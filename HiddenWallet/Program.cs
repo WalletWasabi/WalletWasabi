5a4ecb0b1f8f679a5cb68081196356757b5ec1db
nopara73 <adam.ficsor73@gmail.com>
2016-03-23 08:05:43 +0100

    -

--- HiddenWallet/Program.cs
+++ HiddenWallet/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using HiddenWallet.UserInterface;
 
 namespace HiddenWallet
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///     The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
