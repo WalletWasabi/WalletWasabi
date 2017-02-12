a6e36e7db609390553ce9223499ded3ac604ce8c
nopara73 <adam.ficsor73@gmail.com>
2017-02-12 10:52:07 +0900

    Start login form

--- src/HiddenWallet/UI/WindowAskPassword.cs
+++ src/HiddenWallet/UI/WindowAskPassword.cs
@@ -8,19 +8,42 @@ namespace HiddenWallet.UI
 {
     public class WindowAskPassword : Window
 	{
-		private Tab _tab;
+		private Form _form;
+		private PasswordEntry _passwordEntry;
+		private PasswordEntry _PasswordEntryConfirm;
 
-		public WindowAskPassword(string title = "Please choose a password:", int width = 300, int height = 100, bool hasMenubar = false) : base(title, width, height, hasMenubar)
+		private VerticalBox _verticalBoxButtons;
+		private HorizontalBox _horizontalBoxButtons;
+		private Button _buttonOk;
+
+		public WindowAskPassword(string title = "Generate a wallet", int width = 300, int height = 100, bool hasMenubar = false) : base(title, width, height, hasMenubar)
 		{
 			StartPosition = WindowStartPosition.CenterScreen;
+			AllowMargins = true;
 
 			InitializeComponent();
 		}
 
 		private void InitializeComponent()
 		{
-			_tab = new Tab();
-			Child = _tab;
+			_verticalBoxButtons = new VerticalBox { AllowPadding = true };
+			Child = _verticalBoxButtons;
+
+			_form = new Form { AllowPadding = true };
+			_verticalBoxButtons.Children.Add(_form);
+
+			_passwordEntry = new PasswordEntry();
+			_form.Children.Add("Password:", _passwordEntry);
+			_PasswordEntryConfirm = new PasswordEntry();
+			_form.Children.Add("Confirm password:", _PasswordEntryConfirm);
+
+			_horizontalBoxButtons = new HorizontalBox {AllowPadding = true};
+			_verticalBoxButtons.Children.Add(_horizontalBoxButtons);
+
+			_buttonOk = new Button("Generate wallet");
+			_horizontalBoxButtons.Children.Add(_buttonOk);
+			_buttonOk = new Button("Recover another wallet...");
+			_horizontalBoxButtons.Children.Add(_buttonOk);
 		}
 	}
 }
