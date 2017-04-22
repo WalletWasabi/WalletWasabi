74e0817c5c94f53de1997bd13850e0bf1e810de3
nopara73 <adam.ficsor73@gmail.com>
2017-04-22 12:22:52 +0800

    Add bootstrap

--- HiddenWallet/HiddenWallet.GUI/main.js
+++ HiddenWallet/HiddenWallet.GUI/main.js
@@ -13,7 +13,7 @@ let mainWindow;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow = new BrowserWindow({ width: 640, height: 360, resizable: false });
 
     // and load the index.html of the app.
     mainWindow.loadURL(url.format({
