64aa7afa426a56803d9c3894a478b36cf4280ce7
nopara73 <adam.ficsor73@gmail.com>
2017-10-22 23:14:33 +0100

    Add Tumbler phases

---
+++ HiddenWallet/ChaumianCoinJoin/TumblerPhase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HiddenWallet.ChaumianCoinJoin
+{
+    public enum TumblerPhase
+    {
+		InputRegistration,
+		InputConfirmation,
+		OutputRegistration,
+		Signing
+	}
+}
