58f7b00728fc054e6fdb53abb593e1c835219aab
nopara73 <adam.ficsor73@gmail.com>
2017-10-22 21:12:51 +0100

    Prepare Tumbler project

---
+++ HiddenWallet.ChaumianCoinJoin.Tumbler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace HiddenWallet.ChaumianCoinJoin.Tumbler
+{
+    public class Program
+    {
+#pragma warning disable IDE1006 // Naming Styles
+		public static async Task Main(string[] args)
+#pragma warning restore IDE1006 // Naming Styles
+		{
+			var host = WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.Build();
+
+			await host.RunAsync();
+        }
+    }
+}
