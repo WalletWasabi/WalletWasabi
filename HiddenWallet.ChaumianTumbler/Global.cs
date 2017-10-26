a3c7262f6a939b2970bb118af38c834d9cf8c8c2
nopara73 <adam.ficsor73@gmail.com>
2017-10-26 10:10:30 -0400

    Add datadir to ChaumianTumbler

---
+++ HiddenWallet.ChaumianTumbler/Global.cs
@@ -0,0 +1,24 @@
+﻿using HiddenWallet.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenWallet.ChaumianTumbler
+{
+	public static class Global
+	{
+		private static string _dataDir = null;
+		public static string DataDir
+		{
+			get
+			{
+				if (_dataDir != null) return _dataDir;
+
+				_dataDir = EnvironmentHelpers.GetDataDir("ChaumianTumbler");
+
+				return _dataDir;
+			}
+		}
+	}
+}
