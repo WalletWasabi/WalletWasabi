c9dbc0dd0b8ae0a7c47fd14f4842319b5265ef4e
nopara73 <adam.ficsor73@gmail.com>
2017-11-07 17:28:17 -0800

    Prepare phase execution

--- HiddenWallet.FullSpvWallet/ChaumianCoinJoin/CoinJoinService.cs
+++ HiddenWallet.FullSpvWallet/ChaumianCoinJoin/CoinJoinService.cs
@@ -1,14 +1,19 @@
 ﻿using HiddenWallet.ChaumianCoinJoin;
+using HiddenWallet.ChaumianCoinJoin.Models;
 using HiddenWallet.FullSpv;
 using HiddenWallet.KeyManagement;
+using HiddenWallet.Models;
 using Microsoft.AspNetCore.SignalR.Client;
 using NBitcoin;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using static HiddenWallet.FullSpv.WalletJob;
 
 namespace HiddenWallet.FullSpvWallet.ChaumianCoinJoin
 {
@@ -19,8 +24,14 @@ namespace HiddenWallet.FullSpvWallet.ChaumianCoinJoin
 		public ChaumianTumblerClient TumblerClient { get; private set; }
 		public string BaseAddress { get; private set; }
 
+		public SafeAccount From { get; private set; }
+		public SafeAccount To { get; private set; }
+
+		private volatile bool _tumblingInProcess;
+
 		public CoinJoinService(WalletJob walletJob)
 		{
+			_tumblingInProcess = false;
 			TumblerConnection = null;
 			WalletJob = walletJob ?? throw new ArgumentNullException(nameof(walletJob));
 		}
@@ -41,9 +52,18 @@ namespace HiddenWallet.FullSpvWallet.ChaumianCoinJoin
 						.WithUrl(BaseAddress + "ChaumianTumbler")
 						.Build();
 
-				TumblerConnection.On<string>("PhaseChange", data =>
+				TumblerConnection.On<string>("PhaseChange", async data =>
 				{
-					Debug.WriteLine($"Received: {data}");
+					var jObject = JObject.Parse(data);
+					var phaseString = jObject.Value<string>("Phase");
+
+					var phase = TumblerPhaseHelpers.GetTumblerPhase(phaseString);
+					if (phase != TumblerPhase.InputRegistration) // input registration is executed from tumbling function
+					{
+						await ExecutePhaseAsync(phase);
+					}
+
+					Debug.WriteLine($"New Phase: {phaseString}");
 				});
 
 				await TumblerConnection.StartAsync();
@@ -63,24 +83,98 @@ namespace HiddenWallet.FullSpvWallet.ChaumianCoinJoin
 		}
 
 		#endregion
-		
-		#region Tumbling
 
+		#region Tumbling
+		
 		/// <summary>
 		/// Participates one tumbling round
 		/// </summary>
-		public async Task TumbleAsync(SafeAccount from, SafeAccount to)
+		public async Task TumbleAsync(SafeAccount from, SafeAccount to, CancellationToken cancel)
 		{
-			if (from == null) throw new ArgumentNullException(nameof(from));
-			if (to == null) throw new ArgumentNullException(nameof(to));
-			
-			// if blocks are not synced yet throw
-			if(WalletJob.State < WalletState.SyncingMemPool)
+			try
 			{
-				throw new InvalidOperationException("Blocks are not synced");
-			}
+				From = from ?? throw new ArgumentNullException(nameof(from));
+				To = to ?? throw new ArgumentNullException(nameof(to));
 
-			throw new NotImplementedException();
+				// if blocks are not synced yet throw
+				var headerHeightResult = await WalletJob.TryGetHeaderHeightAsync();
+				if (!headerHeightResult.Success)
+				{
+					throw new InvalidOperationException("Cannot get header height");
+				}
+				Height headerHeight = headerHeightResult.Height;
+				Height blockHeight = await WalletJob.GetBestHeightAsync();
+
+				if (headerHeight.Value - 2 > blockHeight) // tolerate being maximum two blocks behind
+				{
+					throw new InvalidOperationException("Blocks are not synced");
+				}
+
+				if(TumblerConnection == null)
+				{
+					await SubscribePhaseChangeAsync();
+
+					if (TumblerConnection == null)
+					{
+						throw new HttpRequestException("Tumbler is offline");
+					}
+				}
+
+				StatusResponse status = await TumblerClient.GetStatusAsync(cancel);
+
+				var phase = TumblerPhaseHelpers.GetTumblerPhase(status.Phase);
+				
+				// wait for input registration
+				while(phase != TumblerPhase.InputRegistration)
+				{
+					await Task.Delay(100, cancel);
+				}
+
+				_tumblingInProcess = true;
+				await ExecutePhaseAsync(phase);
+
+				// wait while input registration
+				while (phase == TumblerPhase.InputRegistration)
+				{
+					await Task.Delay(100, cancel);
+				}
+
+				// wait for input registration again, this is the end of the round (beginning of the next round)
+				while (phase != TumblerPhase.InputRegistration)
+				{
+					await Task.Delay(100, cancel);
+				}
+				_tumblingInProcess = false;
+			}
+			catch(OperationCanceledException)
+			{
+				return;
+			}
+		}
+
+		private async Task ExecutePhaseAsync(TumblerPhase phase)
+		{
+			if (_tumblingInProcess)
+			{
+				if (phase == TumblerPhase.InputRegistration)
+				{
+					var getBalanceResult = await WalletJob.GetBalanceAsync(From);
+					throw new NotImplementedException();
+				}
+				else if (phase == TumblerPhase.ConnectionConfirmation)
+				{
+					throw new NotImplementedException();
+				}
+				else if (phase == TumblerPhase.OutputRegistration)
+				{
+					throw new NotImplementedException();
+				}
+				else if (phase == TumblerPhase.Signing)
+				{
+					throw new NotImplementedException();
+				}
+				else throw new NotSupportedException("This should never happen");
+			}
 		}
 
 		#endregion
--- HiddenWallet/ChaumianCoinJoin/TumblerPhase.cs
+++ HiddenWallet/ChaumianCoinJoin/TumblerPhase.cs
@@ -11,4 +11,22 @@ namespace HiddenWallet.ChaumianCoinJoin
 		OutputRegistration,
 		Signing,
 	}
+
+	public static class TumblerPhaseHelpers
+	{
+		public static TumblerPhase GetTumblerPhase(string phase)
+		{
+			if (phase == null) throw new ArgumentNullException(nameof(phase));
+
+			foreach (TumblerPhase p in Enum.GetValues(typeof(TumblerPhase)))
+			{
+				if (phase.Equals(p.ToString(), StringComparison.OrdinalIgnoreCase))
+				{
+					return p;
+				}
+			}
+
+			throw new NotSupportedException($"Phase does not exist: {phase}");
+		}
+	}
 }
