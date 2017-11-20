47df4e40e82f1dab7ba6e3bedbb2f6dda84eda02
nopara73 <adam.ficsor73@gmail.com>
2017-11-20 15:21:21 -0400

    Use ip for default websocket address instead of domain (more stable)

--- HiddenWallet.Daemon/Config.cs
+++ HiddenWallet.Daemon/Config.cs
@@ -63,7 +63,7 @@ namespace HiddenWallet.Daemon
 			CanSpendUnconfirmed = false;
 			ChaumianTumblerTestNetAddress = "http://uitzert24go64ure.onion/";
 			ChaumianTumblerMainAddress = "http://i4dmqjdw2nljsyt3.onion/"; // TODO: change it when active tumbler had been set up
-			ChaumianTumblerTestNetNotificationAddress = "http://hiddenwallet.org:37126/";
+			ChaumianTumblerTestNetNotificationAddress = "http://139.59.208.212:37126/";
 			ChaumianTumblerMainNotificationAddress = "http://localhost:37126/"; // TODO: change it when active tumbler had been set up
 
 			if (!File.Exists(path))
