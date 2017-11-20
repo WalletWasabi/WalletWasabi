ee7720b6b8b5a6bcf9c87bcc8febc93d7e723919
nopara73 <adam.ficsor73@gmail.com>
2017-11-19 22:42:28 -0400

    expose kestrel to internet

--- HiddenWallet.ChaumianTumbler/Program.cs
+++ HiddenWallet.ChaumianTumbler/Program.cs
@@ -28,11 +28,14 @@ namespace HiddenWallet.ChaumianTumbler
 			try
 			{
 				await Global.InitializeAsync();
-
-				var endPoint = "http://0.0.0.0:80/";
+				
 				using (var host = WebHost.CreateDefaultBuilder(args)
 					.UseStartup<Startup>()
-					.UseUrls(endPoint)
+					.UseKestrel(options =>
+					{
+						// listen to requests from outside the local machine   
+						options.Listen(System.Net.IPAddress.Any, 80);
+					})
 					.Build())
 				{
 					await host.RunAsync();
