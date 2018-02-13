cd7a425a0f2b436d85dee999df7306241220417c
nopara73 <adam.ficsor73@gmail.com>
2018-02-13 10:39:12 +0100

    Add Tests

---
+++ MagicalCryptoWallet.Tests/CryptoTests.cs
@@ -0,0 +1,53 @@
+﻿using MagicalCryptoWallet.Crypto;
+using MagicalCryptoWallet.Logging;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using Xunit;
+
+namespace MagicalCryptoWallet.Tests
+{
+	public class CryptoTests : IClassFixture<SharedFixture>
+	{
+		private SharedFixture SharedFixture { get; }
+
+		public CryptoTests(SharedFixture fixture)
+		{
+			SharedFixture = fixture;
+		}
+
+		[Fact]
+		public void CipherTests()
+		{
+			var toEncrypt = "hello";
+			var password = "password";
+			var encypted = StringCipher.Encrypt(toEncrypt, password);
+			Assert.NotEqual(toEncrypt, encypted);
+			var decrypted = StringCipher.Decrypt(encypted, password);
+			Assert.Equal(toEncrypt, decrypted);
+
+			var builder = new StringBuilder();
+			for (int i = 0; i < 1000000; i++) // check 10MB
+			{
+				builder.Append("0123456789");
+			}
+
+			toEncrypt = builder.ToString();
+			encypted = StringCipher.Encrypt(toEncrypt, password);
+			Assert.NotEqual(toEncrypt, encypted);
+			decrypted = StringCipher.Decrypt(encypted, password);
+			Assert.Equal(toEncrypt, decrypted);
+			Assert.Throws<CryptographicException>(() => StringCipher.Decrypt(encypted, ""));
+			Assert.Throws<CryptographicException>(() => StringCipher.Decrypt(encypted, "wrongpassword"));
+
+			toEncrypt = "foo@éóüö";
+			password = "";
+			encypted = StringCipher.Encrypt(toEncrypt, password);
+			Assert.NotEqual(toEncrypt, encypted);
+			decrypted = StringCipher.Decrypt(encypted, password);
+			Assert.Equal(toEncrypt, decrypted);
+			Assert.Throws<CryptographicException>(() => StringCipher.Decrypt(encypted, "wrongpassword"));
+		}
+	}
+}
---
+++ MagicalCryptoWallet.Tests/SharedFixture.cs
@@ -0,0 +1,24 @@
+﻿using MagicalCryptoWallet.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagicalCryptoWallet.Tests
+{
+	public class SharedFixture : IDisposable
+	{
+		public SharedFixture()
+		{
+			// Initialize tests...
+
+			Logger.SetMinimumLevel(LogLevel.Debug);
+			Logger.SetModes(LogMode.Debug, LogMode.File);
+			Logger.SetFilePath("TestLogs.txt");
+		}
+
+		public void Dispose()
+		{
+			// Cleanup tests...
+		}
+	}
+}
