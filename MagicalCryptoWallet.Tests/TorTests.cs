ba3707a802d37c3b480a362e797ecf0f376f79f5
nopara73 <adam.ficsor73@gmail.com>
2018-02-13 15:13:37 +0100

    Add post tests

--- MagicalCryptoWallet.Tests/TorTests.cs
+++ MagicalCryptoWallet.Tests/TorTests.cs
@@ -113,6 +113,52 @@ namespace MagicalCryptoWallet.Tests
 			}
 		}
 
+		[Fact]
+		public async Task CanDoBasicPostRequestAsync()
+		{
+			using (var client = new TorHttpClient(new Uri("http://httpbin.org")))
+			{
+				HttpContent content = new FormUrlEncodedContent(new[]
+				{
+					new KeyValuePair<string, string>("foo", "bar@98")
+				});
+
+				HttpResponseMessage message = await client.SendAsync(HttpMethod.Post, "post", content);
+				var responseContentString = await message.Content.ReadAsStringAsync();
+
+				Assert.Contains("bar@98", responseContentString);
+			}
+		}
+
+		[Fact]
+		public async Task CanDoBasicPostRequestWithNonAsciiCharsAsync()
+		{
+			using (var client = new TorHttpClient(new Uri("http://httpbin.org")))
+			{
+				string json = "Hello ñ";
+				var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+				HttpResponseMessage message = await client.SendAsync(HttpMethod.Post, "post", httpContent);
+				var responseContentString = await message.Content.ReadAsStringAsync();
+
+				Assert.Contains(@"Hello \u00f1", responseContentString);
+			}
+		}
+
+		[Fact]
+		public async Task CanDoBasicPostHttpsRequestAsync()
+		{
+			using (var client = new TorHttpClient(new Uri("https://api.smartbit.com.au")))
+			{
+				HttpContent content = new StringContent("{\"hex\": \"01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff2d03a58605204d696e656420627920416e74506f6f6c20757361311f10b53620558903d80272a70c0000724c0600ffffffff010f9e5096000000001976a9142ef12bd2ac1416406d0e132e5bc8d0b02df3861b88ac00000000\"}");
+
+				HttpResponseMessage message = await client.SendAsync(HttpMethod.Post, "/v1/blockchain/decodetx", content);
+				var responseContentString = await message.Content.ReadAsStringAsync();
+
+				Assert.Equal("{\"success\":true,\"transaction\":{\"Version\":\"1\",\"LockTime\":\"0\",\"Vin\":[{\"TxId\":null,\"Vout\":null,\"ScriptSig\":null,\"CoinBase\":\"03a58605204d696e656420627920416e74506f6f6c20757361311f10b53620558903d80272a70c0000724c0600\",\"TxInWitness\":null,\"Sequence\":\"4294967295\"}],\"Vout\":[{\"Value\":25.21865743,\"N\":0,\"ScriptPubKey\":{\"Asm\":\"OP_DUP OP_HASH160 2ef12bd2ac1416406d0e132e5bc8d0b02df3861b OP_EQUALVERIFY OP_CHECKSIG\",\"Hex\":\"76a9142ef12bd2ac1416406d0e132e5bc8d0b02df3861b88ac\",\"ReqSigs\":1,\"Type\":\"pubkeyhash\",\"Addresses\":[\"15HCzh8AoKRnTWMtmgAsT9TKUPrQ6oh9HQ\"]}}],\"TxId\":\"a02b9bd4264ab5d7c43ee18695141452b23b230b2a8431b28bbe446bf2b2f595\"}}", responseContentString);
+			}
+		}
+
 		private static async Task<List<string>> QBitTestAsync(TorHttpClient client, int times, bool alterRequests = false)
 		{
 			var relativetUri = "/whatisit/what%20is%20my%20future";
