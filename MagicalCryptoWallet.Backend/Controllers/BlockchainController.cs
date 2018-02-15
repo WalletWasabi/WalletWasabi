b98c1f5a5fa33d268424c8d3723d01bc787a1f62
nopara73 <adam.ficsor73@gmail.com>
2018-02-15 15:47:26 +0100

    Advance API

--- MagicalCryptoWallet.Backend/Controllers/BlockchainController.cs
+++ MagicalCryptoWallet.Backend/Controllers/BlockchainController.cs
@@ -6,12 +6,31 @@ using MagicalCryptoWallet.Backend.Models;
 using MagicalCryptoWallet.Backend.Models.Responses;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NBitcoin;
 
 namespace MagicalCryptoWallet.Backend.Controllers
 {
     [Route("api/v1/btc/[controller]")]
     public class BlockchainController : Controller
-    {
+	{
+		[HttpGet("fees")]
+		public IDictionary<int, FeeEstimationPair> GetFees(IEnumerable<int> confirmationTargets) // ToDo: make it comma separated, currently: ?confirmationTargets=1&confirmationTargets=2
+		{
+			var feeEstimations = new SortedDictionary<int, FeeEstimationPair>();
+			foreach (var target in confirmationTargets)
+			{
+				feeEstimations.Add(target, new FeeEstimationPair() { Conservative = 200, Economical = 199 });
+			}
+
+			return feeEstimations;
+		}
+
+		[HttpPost("broadcast")]
+		public IActionResult Broadcast([FromBody]string hex)
+		{
+			return Ok();
+		}
+
 		[HttpGet("exchange-rates")]
 		public IEnumerable<ExchangeRate> GetExchangeRates()
 		{
@@ -23,5 +42,17 @@ namespace MagicalCryptoWallet.Backend.Controllers
 
 			return exchangeRates;
 		}
-    }
+
+		[HttpGet("filters/{blockHash}")]
+		public IEnumerable<BlockHashFilterPair> GetFilters(uint256 blockHash)
+		{
+			var filters = new List<BlockHashFilterPair>
+			{
+				new BlockHashFilterPair(){BlockHash = uint256.Zero, FilterHex = "foo"},
+				new BlockHashFilterPair(){BlockHash = uint256.One, FilterHex = "bar"},
+			};
+
+			return filters;
+		}
+	}
 }
---
+++ MagicalCryptoWallet/Backend/Models/BlockHashFilterPair.cs
@@ -0,0 +1,13 @@
+﻿using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagicalCryptoWallet.Backend.Models
+{
+    public class BlockHashFilterPair
+    {
+		public uint256 BlockHash { get; set; }
+		public string FilterHex { get; set; }
+	}
+}
