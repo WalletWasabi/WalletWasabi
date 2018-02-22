8c1e53dec942c0cb96882d7ad93de1b6db5f78f7
nopara73 <adam.ficsor73@gmail.com>
2018-02-22 12:33:26 +0100

    Add connection to the Bitcoin network test

--- MagicalCryptoWallet.Tests/WalletTests.cs
+++ MagicalCryptoWallet.Tests/WalletTests.cs
@@ -1,10 +1,13 @@
 ﻿using MagicalCryptoWallet.KeyManagement;
+using MagicalCryptoWallet.Logging;
 using MagicalCryptoWallet.Services;
 using NBitcoin;
+using NBitcoin.Protocol;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -20,15 +23,38 @@ namespace MagicalCryptoWallet.Tests
 		}
 
 		[Fact]
-		public async Task BasicWalletTestAsync()
+		public async Task CanConnectToNodesTestAsync()
 		{
 			var manager = KeyManager.CreateNew(out Mnemonic mnemonic, "password");
-			var dataFolder = Path.Combine(SharedFixture.DataDir, nameof(BasicWalletTestAsync));
+			var dataFolder = Path.Combine(SharedFixture.DataDir, nameof(CanConnectToNodesTestAsync));
 			using (var wallet = new WalletService(dataFolder, Network.Main, manager))
 			{
+
+				wallet.Nodes.ConnectedNodes.Added += ConnectedNodes_Added;
+				wallet.Nodes.ConnectedNodes.Removed += ConnectedNodes_Removed;
 				wallet.Start();
-				await Task.Delay(1000);
+				// Using the interlocked, not because it makes sense in this context, but to
+				// set an example that these values are often concurrency sensitive
+				while (Interlocked.Read(ref _nodeCount) < 3) 
+				{
+					await Task.Delay(100);
+				}
 			}
+		}
+
+		private long _nodeCount = 0;
+		private void ConnectedNodes_Added(object sender, NodeEventArgs e)
+		{
+			var nodes = sender as NodesCollection;
+			Interlocked.Increment(ref _nodeCount);
+			Logger.LogInfo<WalletTests>($"Node count:{Interlocked.Read(ref _nodeCount)}");
+		}
+		private void ConnectedNodes_Removed(object sender, NodeEventArgs e)
+		{
+			var nodes = sender as NodesCollection;
+			Interlocked.Decrement(ref _nodeCount);
+			// Trace is fine here, building the connections is more exciting than removing them.
+			Logger.LogTrace<WalletTests>($"Node count:{Interlocked.Read(ref _nodeCount)}"); 
 		}
 	}
 }
