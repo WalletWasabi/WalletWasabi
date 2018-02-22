af4e470db9f1939104e955060d5c365c8a6a3dfc
nopara73 <adam.ficsor73@gmail.com>
2018-02-22 10:12:34 +0100

    Start writing tests

---
+++ MagicalCryptoWallet.Tests/WalletTests.cs
@@ -0,0 +1,34 @@
+﻿using MagicalCryptoWallet.KeyManagement;
+using MagicalCryptoWallet.Services;
+using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MagicalCryptoWallet.Tests
+{
+    public class WalletTests : IClassFixture<SharedFixture>
+	{
+		private SharedFixture SharedFixture { get; }
+
+		public WalletTests(SharedFixture fixture)
+		{
+			SharedFixture = fixture;
+		}
+
+		[Fact]
+		public async Task BasicWalletTestAsync()
+		{
+			var manager = KeyManager.CreateNew(out Mnemonic mnemonic, "password");
+			var dataFolder = Path.Combine(SharedFixture.DataDir, nameof(BasicWalletTestAsync));
+			using (var wallet = new WalletService(dataFolder, Network.TestNet, manager))
+			{
+				wallet.Start();
+				await Task.Delay(1000);
+			}
+		}
+	}
+}
