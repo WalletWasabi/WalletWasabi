1bb811eec6d6662af566a5a51a7c56b3b0ddbf90
nopara73 <adam.ficsor73@gmail.com>
2018-03-28 14:39:54 +0200

    Make ToHex 2x faster, but unsafe

--- MagicalCryptoWallet/Helpers/ByteHelpers.cs
+++ MagicalCryptoWallet/Helpers/ByteHelpers.cs
@@ -1,10 +1,11 @@
 ﻿using MagicalCryptoWallet.Helpers;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace System
 {
-	public static class ByteHelpers
+	unsafe public static class ByteHelpers
     {
 		// https://stackoverflow.com/questions/415291/best-way-to-combine-two-or-more-byte-arrays-in-c-sharp
 		/// <summary>
@@ -67,6 +68,23 @@ namespace System
 			}
 		}
 
+		private static readonly uint[] _lookup32Unsafe = CreateLookup32Unsafe();
+		private static readonly uint* _lookup32UnsafeP = (uint*)GCHandle.Alloc(_lookup32Unsafe, GCHandleType.Pinned).AddrOfPinnedObject();
+
+		private static uint[] CreateLookup32Unsafe()
+		{
+			var result = new uint[256];
+			for (int i = 0; i < 256; i++)
+			{
+				string s = i.ToString("X2");
+				if (BitConverter.IsLittleEndian)
+					result[i] = ((uint)s[0]) + ((uint)s[1] << 16);
+				else
+					result[i] = ((uint)s[1]) + ((uint)s[0] << 16);
+			}
+			return result;
+		}
+
 		// https://stackoverflow.com/a/5919521/2061103
 		// https://stackoverflow.com/a/10048895/2061103
 		/// <summary>
@@ -76,16 +94,18 @@ namespace System
 		{
 			Guard.NotNullOrEmpty(nameof(bytes), bytes);
 
-			var result = new StringBuilder(bytes.Length * 2);
-			var hexAlphabet = "0123456789ABCDEF";
-
-			foreach (byte b in bytes)
+			var lookupP = _lookup32UnsafeP;
+			var result = new string((char)0, bytes.Length * 2);
+			fixed (byte* bytesP = bytes)
+			fixed (char* resultP = result)
 			{
-				result.Append(hexAlphabet[b >> 4]);
-				result.Append(hexAlphabet[b & 0xF]);
+				uint* resultP2 = (uint*)resultP;
+				for (int i = 0; i < bytes.Length; i++)
+				{
+					resultP2[i] = lookupP[bytesP[i]];
+				}
 			}
-
-			return result.ToString();
+			return result;
 		}
 
 		// https://stackoverflow.com/a/5919521/2061103
