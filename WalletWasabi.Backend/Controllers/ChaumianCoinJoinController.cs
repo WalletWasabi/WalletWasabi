87fd7f1ef646c08c21407811fad1b8af4cdecaa3
nopara73 <adam.ficsor73@gmail.com>
2018-04-24 05:00:34 +0800

    Add blinded data array size debug log.

--- WalletWasabi.Backend/Controllers/ChaumianCoinJoinController.cs
+++ WalletWasabi.Backend/Controllers/ChaumianCoinJoinController.cs
@@ -212,6 +212,7 @@ namespace WalletWasabi.Backend.Controllers
 					{
 						return BadRequest("Invalid blinded output hex.");
 					}
+					Logger.LogDebug<ChaumianCoinJoinController>($"Blinded data array size: {blindedData.Length}");
 					byte[] signature = RsaKey.SignBlindedData(blindedData);
 
 					// Check if phase changed since.
