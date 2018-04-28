b41ef00ed18222c48c642a823214688dda6661c0
nopara73 <adam.ficsor73@gmail.com>
2018-04-28 20:05:02 +0700

    Remove httpbin test, stopped working

--- WalletWasabi.Tests/TorTests.cs
+++ WalletWasabi.Tests/TorTests.cs
@@ -104,21 +104,6 @@ namespace WalletWasabi.Tests
 		}
 
 		[Fact]
-		public async Task CanDoBasicPostRequestWithNonAsciiCharsAsync()
-		{
-			using (var client = new TorHttpClient(new Uri("http://httpbin.org")))
-			{
-				string json = "Hello ñ";
-				var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
-
-				HttpResponseMessage message = await client.SendAsync(HttpMethod.Post, "post", httpContent);
-				var responseContentString = await message.Content.ReadAsStringAsync();
-
-				Assert.Contains(@"Hello \u00f1", responseContentString);
-			}
-		}
-
-		[Fact]
 		public async Task CanDoBasicPostHttpsRequestAsync()
 		{
 			using (var client = new TorHttpClient(new Uri("https://api.smartbit.com.au")))
