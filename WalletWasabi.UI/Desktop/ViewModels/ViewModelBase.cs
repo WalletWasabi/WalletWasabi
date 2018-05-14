4d377841516861705a2b967ab4d6b130234ac29d
Lucas Ontivero <lucasontivero@gmail.com>
2018-05-14 01:00:36 -0300

    First commit for Wallet.UI Desktop

---
+++ WalletWasabi.UI/Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace WalletWasabi.UI.Desktop.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ WalletWasabi.UI/Desktop/Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.UI.Desktop.Views
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
