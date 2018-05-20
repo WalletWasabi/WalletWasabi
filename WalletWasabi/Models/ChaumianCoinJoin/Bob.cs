6852f4f5255822f9f3e17aa436582936aca63d2c
Lucas Ontivero <lucasontivero@gmail.com>
2018-05-20 20:02:22 -0300

    Removes scriptPubkey length checks

--- WalletWasabi/Models/ChaumianCoinJoin/Bob.cs
+++ WalletWasabi/Models/ChaumianCoinJoin/Bob.cs
@@ -12,14 +12,7 @@ namespace WalletWasabi.Models.ChaumianCoinJoin
 
 		public Bob(BitcoinAddress activeOutputAddress)
 		{
-			Guard.NotNull(nameof(activeOutputAddress), activeOutputAddress);
-			// 33 bytes maximum: https://bitcoin.stackexchange.com/a/46379/26859
-			int byteCount = activeOutputAddress.ScriptPubKey.ToBytes().Length;
-			if (byteCount > 33)
-			{
-				throw new ArgumentOutOfRangeException(nameof(activeOutputAddress), byteCount, $"Can be maximum 33 bytes.");
-			}
-			ActiveOutputAddress = activeOutputAddress;
+			ActiveOutputAddress = Guard.NotNull(nameof(activeOutputAddress), activeOutputAddress);
 		}
 	}
 }
