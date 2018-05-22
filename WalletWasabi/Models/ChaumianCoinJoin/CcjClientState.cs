acccf1ea8c1a5dfbc5be171e6556a6e18aa6be65
Lucas Ontivero <lucasontivero@gmail.com>
2018-05-22 16:51:58 -0300

    Fix meaningless OrderBy chain

--- WalletWasabi/Models/ChaumianCoinJoin/CcjClientState.cs
+++ WalletWasabi/Models/ChaumianCoinJoin/CcjClientState.cs
@@ -114,7 +114,7 @@ namespace WalletWasabi.Models.ChaumianCoinJoin
 				foreach (SmartCoin coin in WaitingList
 								.Where(x => x.Confirmed || x.Label.StartsWith("ZeroLink", StringComparison.Ordinal)) // Where our label contains CoinJoin, CoinJoins can be registered even if not confirmed, our label will likely be CoinJoin only if it was a previous CoinJoin, otherwise the server will refuse us.
 								.OrderByDescending(y => y.Amount) // First order by amount.
-								.OrderByDescending(z => z.Confirmed)) // Then order by the amount ordered ienumerable by confirmation, so first try to register confirmed coins.
+								.ThenByDescending(z => z.Confirmed)) // Then order by the amount ordered ienumerable by confirmation, so first try to register confirmed coins.
 				{
 					coinsToRegister.Add(coin);
 
