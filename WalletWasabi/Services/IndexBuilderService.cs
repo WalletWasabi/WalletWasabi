a9cfd7a2878b0b133a69bc6a586118a558a3dc19
Lucas Ontivero <lucasontivero@gmail.com>
2018-06-03 22:29:57 -0300

    Update filters parameters for compatibility
    Update the filters parameters for keeping compatibility with the
    already built filters (testnet and mainnet).
    
    This code should be merged once we update the NBitcoin version
    containing the changes in the filter builder.
    
    **This has not been tested yet.**

--- WalletWasabi/Services/IndexBuilderService.cs
+++ WalletWasabi/Services/IndexBuilderService.cs
@@ -318,6 +318,8 @@ namespace WalletWasabi.Services
 							if (scripts.Count != 0)
 							{
 								filter = new GolombRiceFilterBuilder()
+									.SetP(20)
+									.SetM(1U << 20)
 									.SetKey(block.GetHash())
 									.AddEntries(scripts.Select(x => x.ToCompressedBytes()))
 									.Build();
