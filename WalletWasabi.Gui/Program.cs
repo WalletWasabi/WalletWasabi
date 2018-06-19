7508092e79d436b50c2ebe5b8b3b2f3c07f23999
Dan Walmsley <dan@walms.co.uk>
2018-06-19 18:12:09 +0100

    run codemaid.

--- WalletWasabi.Gui/Program.cs
+++ WalletWasabi.Gui/Program.cs
@@ -2,11 +2,11 @@
 
 namespace WalletWasabi.Gui
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
 }
