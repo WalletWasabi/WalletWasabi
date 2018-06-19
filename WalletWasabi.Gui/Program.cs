dac22f80b2d2b44026569ef759833dd6f55cda10
Dan Walmsley <dan@walms.co.uk>
2018-06-19 18:02:29 +0100

    Add empty project for Gui app.

---
+++ WalletWasabi.Gui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WalletWasabi.Gui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
