fb91289432f70d6a26f7a3c1fe77041ce3c5549a
nopara73 <adam.ficsor73@gmail.com>
2018-06-20 11:58:18 +0100

    Improve Logging

--- WalletWasabi.Gui/Program.cs
+++ WalletWasabi.Gui/Program.cs
@@ -12,24 +12,31 @@ namespace WalletWasabi.Gui
 	{
 		private static void Main(string[] args)
 		{
-			Logger.SetFilePath(Path.Combine(Global.DataDir, "Logs.txt"));
+			try
+			{
+				Logger.SetFilePath(Path.Combine(Global.DataDir, "Logs.txt"));
 #if RELEASE
 				Logger.SetMinimumLevel(LogLevel.Info);
 				Logger.SetModes(LogMode.File);
 #else
-			Logger.SetMinimumLevel(LogLevel.Debug);
-			Logger.SetModes(LogMode.Debug, LogMode.Console, LogMode.File);
+				Logger.SetMinimumLevel(LogLevel.Debug);
+				Logger.SetModes(LogMode.Debug, LogMode.Console, LogMode.File);
 #endif
 
-			BuildAvaloniaApp().AfterSetup(async builder =>
-			{
-				var configFilePath = Path.Combine(Global.DataDir, "Config.json");
-				var config = new Config(configFilePath);
-				await config.LoadOrCreateDefaultFileAsync();
-				Logger.LogInfo<Config>("Config is successfully initialized.");
+				BuildAvaloniaApp().AfterSetup(async builder =>
+				{
+					var configFilePath = Path.Combine(Global.DataDir, "Config.json");
+					var config = new Config(configFilePath);
+					await config.LoadOrCreateDefaultFileAsync();
+					Logger.LogInfo<Config>("Config is successfully initialized.");
 
-				await Global.InitializeAsync(config);
-			}).StartShellApp("Wasabi Wallet");
+					await Global.InitializeAsync(config);
+				}).StartShellApp("Wasabi Wallet");
+			}
+			catch (Exception ex)
+			{
+				Logger.LogCritical<Program>(ex);
+			}
 		}
 
 		private static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>().UsePlatformDetect().UseReactiveUI();
