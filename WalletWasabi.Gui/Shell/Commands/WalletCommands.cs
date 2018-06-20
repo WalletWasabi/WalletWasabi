d582ca3629c9c16bbf62b17a4871025f4461a107
Dan Walmsley <dan@walms.co.uk>
2018-06-20 12:52:05 +0100

    Implement file menu.

---
+++ WalletWasabi.Gui/Shell/Commands/WalletCommands.cs
@@ -0,0 +1,21 @@
+﻿using AvalonStudio.Commands;
+using ReactiveUI;
+
+namespace WalletWasabi.Gui.Shell.Commands
+{
+	internal class WalletCommands
+	{
+		[ExportCommandDefinition("File.GenerateWallet")]
+		public CommandDefinition GenerateWalletCommand { get; }
+
+		public WalletCommands()
+		{
+			GenerateWalletCommand = new CommandDefinition(
+				"Generate Wallet", null, ReactiveCommand.Create(OnGenerateWallet));
+		}
+
+		private void OnGenerateWallet()
+		{
+		}
+	}
+}
---
+++ WalletWasabi.Gui/Shell/MainMenu/FileMainMenuItems.cs
@@ -0,0 +1,42 @@
+﻿using AvalonStudio.MainMenu;
+using AvalonStudio.Menus;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Text;
+
+namespace WalletWasabi.Gui.Shell.MainMenu
+{
+	internal class FileMainMenuItems
+	{
+		private IMenuItemFactory _menuItemFactory;
+
+		[ImportingConstructor]
+		public FileMainMenuItems(IMenuItemFactory menuItemFactory)
+		{
+			_menuItemFactory = menuItemFactory;
+		}
+
+		[ExportMainMenuItem("File")]
+		[DefaultOrder(0)]
+		public IMenuItem File => _menuItemFactory.CreateHeaderMenuItem("File", null);
+
+		[ExportMainMenuDefaultGroup("File", "Wallet")]
+		[DefaultOrder(0)]
+		public object WalletGroup => null;
+
+		[ExportMainMenuItem("File", "Generate Wallet")]
+		[DefaultOrder(0)]
+		[DefaultGroup("Wallet")]
+		public IMenuItem GenerateWallet => _menuItemFactory.CreateCommandMenuItem("File.GenerateWallet");
+
+		[ExportMainMenuDefaultGroup("File", "Exit")]
+		[DefaultOrder(1000)]
+		public object ExitGroup => null;
+
+		[ExportMainMenuItem("File", "Exit")]
+		[DefaultOrder(0)]
+		[DefaultGroup("Exit")]
+		public IMenuItem Exit => _menuItemFactory.CreateCommandMenuItem("File.Exit");
+	}
+}
