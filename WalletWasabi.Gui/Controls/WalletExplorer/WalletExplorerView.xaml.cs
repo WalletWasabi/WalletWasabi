b2c709928f932321e0524f03170e5f3a265489c1
Dan Walmsley <dan@walms.co.uk>
2018-06-21 16:45:55 +0100

    begin wallet explorer.

---
+++ WalletWasabi.Gui/Controls/WalletExplorer/WalletExplorerView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Gui.Controls.WalletExplorer
+{
+	public class WalletExplorerView : UserControl
+	{
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
---
+++ WalletWasabi.Gui/Controls/WalletExplorer/WalletExplorerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using AvalonStudio.Extensibility;
+using AvalonStudio.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Text;
+
+namespace WalletWasabi.Gui.Controls.WalletExplorer
+{
+	[Export(typeof(IExtension))]
+	[ExportToolControl]
+	[Shared]
+	public class WalletExplorerViewModel : ToolViewModel, IExtension
+	{
+		public override Location DefaultLocation => Location.Right;
+
+		public WalletExplorerViewModel()
+		{
+			Title = "Wallet Explorer";
+		}
+	}
+}
