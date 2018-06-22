2dc2b20e2556da931009cf05d778981838c5d7c1
Lucas Ontivero <lucasontivero@gmail.com>
2018-06-22 13:04:31 +0100

    Adds Privacy Policy and Terms and Conditions tabs

---
+++ WalletWasabi.Gui/Tabs/PrivacyPolicyViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Diagnostics.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WalletWasabi.Gui.ViewModels;
+
+namespace WalletWasabi.Gui.Tabs
+{
+	internal class PrivacyPolicyViewModel : DocumentTabViewModel
+	{
+		public PrivacyPolicyViewModel() : base("Privacy Policy")
+		{
+			PrivacyPolicy = "This is the privacy policy *****";
+		}
+
+		public string PrivacyPolicy { get; }
+
+	}
+}
