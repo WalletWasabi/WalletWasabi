b7af5fa020fa86cd47ac172b1ea2fd8aa1cd5082
Dan Walmsley <dan@walms.co.uk>
2018-06-22 14:34:07 +0100

    implement generate button logic.

--- WalletWasabi.Gui/Tabs/WalletManager/GenerateWalletViewModel.cs
+++ WalletWasabi.Gui/Tabs/WalletManager/GenerateWalletViewModel.cs
@@ -1,4 +1,6 @@
-﻿using ReactiveUI;
+﻿using AvalonStudio.Extensibility;
+using AvalonStudio.Shell;
+using ReactiveUI;
 using System.Collections.ObjectModel;
 using WalletWasabi.Gui.ViewModels;
 
@@ -9,9 +11,14 @@ namespace WalletWasabi.Gui.Tabs.WalletManager
 		private string _password;
 		private string _passwordConfirmation;
 		private string _walletName;
+		private bool _termsAccepted;
 
 		public GenerateWalletViewModel() : base("Generate Wallet")
 		{
+			GenerateCommand = ReactiveCommand.Create(() =>
+			{
+			},
+			this.WhenAnyValue(x => x.TermsAccepted));
 		}
 
 		public string Password
@@ -30,6 +37,22 @@ namespace WalletWasabi.Gui.Tabs.WalletManager
 		{
 			get { return _walletName; }
 			set { this.RaiseAndSetIfChanged(ref _walletName, value); }
+		}
+
+		public bool TermsAccepted
+		{
+			get { return _termsAccepted; }
+			set { this.RaiseAndSetIfChanged(ref _termsAccepted, value); }
+		}
+
+		public ReactiveCommand GenerateCommand { get; }
+
+		public void OnTermsClicked()
+		{
+		}
+
+		public void OnPrivacyClicked()
+		{
 		}
 	}
 }
