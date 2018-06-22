13ac140ef782de568a6e0ce7a250950e252c48c9
Lucas Ontivero <lucasontivero@gmail.com>
2018-06-22 19:44:55 +0100

    WIP - Context Menu for copy selected text

--- WalletWasabi.Gui/Tabs/ReportBugViewModel.cs
+++ WalletWasabi.Gui/Tabs/ReportBugViewModel.cs
@@ -1,4 +1,9 @@
-﻿using WalletWasabi.Gui.ViewModels;
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using WalletWasabi.Gui.ViewModels;
 
 namespace WalletWasabi.Gui.Tabs
 {
@@ -6,6 +11,40 @@ namespace WalletWasabi.Gui.Tabs
 	{
 		public ReportBugViewModel() : base("Report Bug")
 		{
+		}
+
+		public void CopySelection(TextBox textBox)
+		{
+			SetClipboardText(GetSelection(textBox));
+		}
+
+		private string GetSelection(TextBox textBox)
+		{
+			var text = textBox.Text;
+			if (string.IsNullOrEmpty(text))
+				return "";
+			var selectionStart = textBox.SelectionStart;
+			var selectionEnd = textBox.SelectionEnd;
+			var start = Math.Min(selectionStart, selectionEnd);
+			var end = Math.Max(selectionStart, selectionEnd);
+			if (start == end || (textBox.Text?.Length ?? 0) < end)
+			{
+				return "";
+			}
+			return text.Substring(start, end - start);
+		}
+
+		private static void SetClipboardText(string text)
+		{
+			try
+			{
+				Application.Current.Clipboard.SetTextAsync(text).GetAwaiter().GetResult();
+			}
+			catch (Exception)
+			{
+				// Apparently this exception sometimes happens randomly.
+				// The MS controls just ignore it, so we'll do the same.
+			}
 		}
 	}
 }
