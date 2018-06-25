e2357ddadf8e4db7312a0624cf4cab9b685aa765
varsnotwars <luiss@hush.ai>
2018-06-25 10:22:44 +1000

    Added menu icons
    Added the xaml format of icons for the remaining list items.
    Also renamed the settings item to have the correct name.

--- WalletWasabi.Gui/Shell/Commands/HelpCommands.cs
+++ WalletWasabi.Gui/Shell/Commands/HelpCommands.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls;
+﻿using System.Composition;
+using Avalonia.Controls;
 using Avalonia.Diagnostics;
 using AvalonStudio.Commands;
 using AvalonStudio.Extensibility;
@@ -14,61 +15,77 @@ namespace WalletWasabi.Gui.Shell.Commands
 {
 	internal class HelpCommands
 	{
-		public HelpCommands()
+		[ImportingConstructor]
+		public HelpCommands(CommandIconService commandIconService)
 		{
 			AboutCommand = new CommandDefinition(
-				"About", null, ReactiveCommand.Create(() =>
+				"About",
+				commandIconService.GetCompletionKindImage("About"),
+				ReactiveCommand.Create(() =>
 				{
 					IoC.Get<IShell>().AddOrSelectDocument<AboutViewModel>(new AboutViewModel());
 				}));
 
 			CustomerSupportCommand = new CommandDefinition(
-				"Customer Support", null, ReactiveCommand.Create(() =>
+				"Customer Support",
+				commandIconService.GetCompletionKindImage("CustomerSupport"),
+				ReactiveCommand.Create(() =>
 				{
 					IoC.Get<IShell>().AddOrSelectDocument<CustomerSupportViewModel>(new CustomerSupportViewModel());
 				}));
 
 			ReportBugCommand = new CommandDefinition(
-				"Report Bug", null, ReactiveCommand.Create(() =>
+				"Report Bug",
+				commandIconService.GetCompletionKindImage("ReportBug"),
+				ReactiveCommand.Create(() =>
 				{
 					IoC.Get<IShell>().AddOrSelectDocument<ReportBugViewModel>(new ReportBugViewModel());
 				}));
 
 			PrivacyPolicyCommand = new CommandDefinition(
-				"Privacy Policy", null, ReactiveCommand.Create(() =>
+				"Privacy Policy",
+				commandIconService.GetCompletionKindImage("PrivacyPolicy"),
+				ReactiveCommand.Create(() =>
 				{
 					IoC.Get<IShell>().AddOrSelectDocument<PrivacyPolicyViewModel>(new PrivacyPolicyViewModel());
 				}));
 
 			TermsAndConditionsCommand = new CommandDefinition(
-				"Terms and Conditions", null, ReactiveCommand.Create(() =>
+				"Terms and Conditions",
+				commandIconService.GetCompletionKindImage("TermsAndConditions"),
+				ReactiveCommand.Create(() =>
 				{
 					IoC.Get<IShell>().AddOrSelectDocument<TermsAndConditionsViewModel>(new TermsAndConditionsViewModel());
 				}));
 
 			LegalIssuesCommand = new CommandDefinition(
-				"Legal Issues", null, ReactiveCommand.Create(() =>
+				"Legal Issues",
+				commandIconService.GetCompletionKindImage("LegalIssues"),
+				ReactiveCommand.Create(() =>
 				{
 					IoC.Get<IShell>().AddOrSelectDocument<LegalIssuesViewModel>(new LegalIssuesViewModel());
 				}));
 
-			DevToolsCommand = new CommandDefinition("Dev Tools", null, ReactiveCommand.Create(() =>
-			{
-				var devTools = new DevTools(Window.OpenWindows.FirstOrDefault());
-
-				var devToolsWindow = new Window
+			DevToolsCommand = new CommandDefinition(
+				"Dev Tools",
+				commandIconService.GetCompletionKindImage("DevTools"),
+				ReactiveCommand.Create(() =>
 				{
-					Width = 1024,
-					Height = 512,
-					Content = devTools,
-					DataTemplates =
+					var devTools = new DevTools(Window.OpenWindows.FirstOrDefault());
+
+					var devToolsWindow = new Window
+					{
+						Width = 1024,
+						Height = 512,
+						Content = devTools,
+						DataTemplates =
 						{
 							new ViewLocator<Avalonia.Diagnostics.ViewModels.ViewModelBase>(),
 						}
-				};
+					};
 
-				devToolsWindow.Show();
-			}));
+					devToolsWindow.Show();
+				}));
 		}
 
 		private void OnGenerateWallet()
