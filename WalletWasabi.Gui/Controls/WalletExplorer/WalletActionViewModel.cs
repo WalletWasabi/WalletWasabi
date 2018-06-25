34767889eb82dd638db4ec5f407a63cc98773e0e
nopara73 <adam.ficsor73@gmail.com>
2018-06-25 18:30:29 +0100

    Move WalletViewModel to its own file.

---
+++ WalletWasabi.Gui/Controls/WalletExplorer/WalletActionViewModel.cs
@@ -0,0 +1,29 @@
+﻿using AvalonStudio.Controls;
+using AvalonStudio.Extensibility;
+using AvalonStudio.Shell;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalletWasabi.Gui.Controls.WalletExplorer
+{
+	public class WalletActionViewModel : DocumentTabViewModel
+	{
+		public WalletViewModel Wallet { get; }
+
+		public WalletActionViewModel(string title, WalletViewModel walletViewModel)
+			: base(title)
+		{
+			Wallet = walletViewModel;
+			DoItCommand = ReactiveCommand.Create(DisplayActionTab);
+		}
+
+		public ReactiveCommand DoItCommand { get; }
+
+		public void DisplayActionTab()
+		{
+			IoC.Get<IShell>().AddOrSelectDocument(this);
+		}
+	}
+}
--- WalletWasabi.Gui/Controls/WalletExplorer/WalletViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/WalletViewModel.cs
@@ -38,25 +38,6 @@ namespace WalletWasabi.Gui.Controls.WalletExplorer
 		}
 	}
 
-	public class WalletActionViewModel : DocumentTabViewModel
-	{
-		public WalletViewModel Wallet { get; }
-
-		public WalletActionViewModel(string title, WalletViewModel walletViewModel)
-			: base(title)
-		{
-			Wallet = walletViewModel;
-			DoItCommand = ReactiveCommand.Create(DisplayActionTab);
-		}
-
-		public ReactiveCommand DoItCommand { get; }
-
-		public void DisplayActionTab()
-		{
-			IoC.Get<IShell>().AddOrSelectDocument(this);
-		}
-	}
-
 	public class SendActionViewModel : WalletActionViewModel
 	{
 		public SendActionViewModel(WalletViewModel walletViewModel)
