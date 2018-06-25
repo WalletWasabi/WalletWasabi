bbf787412a6fb5a464434703a3a4d6211e1b026d
varsnotwars <luiss@hush.ai>
2018-06-26 02:21:20 +1000

    WalletExplorer code cleanup
    Moved classes to own file.
    Fixed spacing.
    Using object initializer in WalletViewModel ctor

---
+++ WalletWasabi.Gui/Controls/WalletExplorer/WalletActionViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AvalonStudio.Extensibility;
+using AvalonStudio.Shell;
+using ReactiveUI;
+using WalletWasabi.Gui.ViewModels;
+
+namespace WalletWasabi.Gui.Controls.WalletExplorer
+{
+	public class WalletActionViewModel : DocumentTabViewModel
+	{
+		public WalletViewModel Wallet { get; }
+
+		public WalletActionViewModel(string title, WalletViewModel walletViewModel)
+			: base(title)
+		{
+			Wallet = walletViewModel;
+			DoItCommand = ReactiveCommand.Create(DisplayActionTab);
+		}
+
+		public ReactiveCommand DoItCommand { get; }
+
+		public void DisplayActionTab()
+		{
+			if (Title == "Send")
+			{
+				IoC.Get<IShell>().AddOrSelectDocument<SendActionViewModel>(this);
+			}
+			else if (Title == "Receive")
+			{
+				IoC.Get<IShell>().AddOrSelectDocument<ReceiveActionViewModel>(this);
+			}
+			else if (Title == "CoinJoin")
+			{
+				IoC.Get<IShell>().AddOrSelectDocument<CoinJoinActionViewModel>(this);
+			}
+			else if (Title == "History")
+			{
+				IoC.Get<IShell>().AddOrSelectDocument<HistoryActionViewModel>(this);
+			}
+		}
+	}
+}
