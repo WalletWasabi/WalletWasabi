44032fd0a917894e949d3013e1041f11a2d669c0
varsnotwars <luiss@hush.ai>
2018-06-27 05:17:40 +1000

    Created converter to toggle the text

---
+++ WalletWasabi.Gui/Converters/StatusColorConvertor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using AvalonStudio.Extensibility.Theme;
+using WalletWasabi.Models;
+
+namespace WalletWasabi.Gui.Converters
+{
+	public class StatusColorConvertor : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			bool isTor = Enum.TryParse(value.ToString(), out TorStatus tor);
+			if (isTor && tor == TorStatus.NotRunning)
+			{
+				return Brushes.Red;
+			}
+
+			bool isBackend = Enum.TryParse(value.ToString(), out BackendStatus backend);
+			if (isBackend && backend == BackendStatus.NotConnected)
+			{
+				return Brushes.Red;
+			}
+
+			return ColorTheme.CurrentTheme.Foreground;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
