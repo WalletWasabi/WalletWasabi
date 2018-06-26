81664111a5d6bced6f73a3310833aabb192218cd
nopara73 <adam.ficsor73@gmail.com>
2018-06-26 23:05:14 +0100

    Let selections to lose focus

--- WalletWasabi.Gui/Controls/WalletExplorer/HistoryTabView.xaml.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/HistoryTabView.xaml.cs
@@ -4,16 +4,23 @@ using Avalonia.Markup.Xaml;
 
 namespace WalletWasabi.Gui.Controls.WalletExplorer
 {
-    public class HistoryTabView : UserControl
-    {
-        public HistoryTabView()
-        {
-            this.InitializeComponent();
-        }
+	public class HistoryTabView : UserControl
+	{
+		public HistoryTabView()
+		{
+			InitializeComponent();
+			LostFocus += ReceiveTabView_LostFocus;
+		}
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
-    }
+		private void ReceiveTabView_LostFocus(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+		{
+			var model = DataContext as HistoryTabViewModel;
+			model.SelectedTransaction = null;
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
 }
--- WalletWasabi.Gui/Controls/WalletExplorer/ReceiveTabView.xaml.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/ReceiveTabView.xaml.cs
@@ -4,16 +4,23 @@ using Avalonia.Markup.Xaml;
 
 namespace WalletWasabi.Gui.Controls.WalletExplorer
 {
-    public class ReceiveTabView : UserControl
-    {
-        public ReceiveTabView()
-        {
-            this.InitializeComponent();
-        }
+	public class ReceiveTabView : UserControl
+	{
+		public ReceiveTabView()
+		{
+			InitializeComponent();
+			LostFocus += ReceiveTabView_LostFocus;
+		}
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
-    }
+		private void ReceiveTabView_LostFocus(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+		{
+			var model = DataContext as ReceiveTabViewModel;
+			model.SelectedAddress = null;
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
 }
