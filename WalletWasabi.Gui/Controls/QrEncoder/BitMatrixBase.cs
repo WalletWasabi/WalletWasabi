dfee680bf2cc69330f481e2332f255dd210b039e
varsnotwars <luiss@hush.ai>
2018-06-28 04:48:51 +1000

    Created base class for shared logic

---
+++ WalletWasabi.Gui/Controls/QrEncoder/BitMatrixBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Gma.QrCodeNet.Encoding
+{
+    public abstract class BitMatrixBase : BitMatrix
+    {
+	    public readonly bool[,] M_InternalArray;
+
+	    public readonly int M_Width;
+
+	    protected BitMatrixBase(int width, bool[,] internalArray)
+	    {
+			M_Width = width;
+		    M_InternalArray = internalArray;
+	    }
+
+	    protected BitMatrixBase(bool[,] internalArray)
+	    {
+			M_InternalArray = internalArray;
+		    int width = internalArray.GetLength(0);
+		    M_Width = width;
+		}
+	    public static bool CanCreate(bool[,] internalArray)
+	    {
+		    if (internalArray == null)
+			    return false;
+
+		    if (internalArray.GetLength(0) == internalArray.GetLength(1))
+		    {
+			    return true;
+		    }
+		    else
+		    {
+			    return false;
+		    }
+		}
+	    /// <summary>
+	    /// Return value will be deep copy of array.
+	    /// </summary>
+	    public override bool[,] InternalArray
+	    {
+		    get
+		    {
+			    bool[,] deepCopyArray = new bool[M_Width, M_Width];
+			    for (int x = 0; x < M_Width; x++)
+			    for (int y = 0; y < M_Width; y++)
+				    deepCopyArray[x, y] = M_InternalArray[x, y];
+			    return deepCopyArray;
+		    }
+	    }
+
+	}
+}
--- WalletWasabi.Gui/Controls/QrEncoder/TriStateMatrix.cs
+++ WalletWasabi.Gui/Controls/QrEncoder/TriStateMatrix.cs
@@ -2,59 +2,29 @@
 
 namespace Gma.QrCodeNet.Encoding
 {
-	public class TriStateMatrix : BitMatrix
+	public class TriStateMatrix : BitMatrixBase
 	{
 		private readonly StateMatrix M_stateMatrix;
 
-		private readonly bool[,] M_InternalArray;
-
-		private readonly int M_Width;
-
-		public TriStateMatrix(int width)
+		public TriStateMatrix(int width) : base(width, new bool[width, width])
 		{
 			M_stateMatrix = new StateMatrix(width);
-			M_InternalArray = new bool[width, width];
-			M_Width = width;
 		}
 
 		public static bool CreateTriStateMatrix(bool[,] internalArray, out TriStateMatrix triStateMatrix)
 		{
 			triStateMatrix = null;
-			if (internalArray == null)
-				return false;
-
-			if (internalArray.GetLength(0) == internalArray.GetLength(1))
+			if (CanCreate(internalArray))
 			{
 				triStateMatrix = new TriStateMatrix(internalArray);
 				return true;
 			}
-			else
-			{
-				return false;
-			}
-		}
 
-		/// <summary>
-		/// Return value will be deep copy of array.
-		/// </summary>
-		public override bool[,] InternalArray
-		{
-			get
-			{
-				bool[,] deepCopyArray = new bool[M_Width, M_Width];
-				for (int x = 0; x < M_Width; x++)
-					for (int y = 0; y < M_Width; y++)
-						deepCopyArray[x, y] = M_InternalArray[x, y];
-				return deepCopyArray;
-			}
+			return false;
 		}
-
-		internal TriStateMatrix(bool[,] internalArray)
+		internal TriStateMatrix(bool[,] internalArray) : base(internalArray)
 		{
-			M_InternalArray = internalArray;
-			int width = internalArray.GetLength(0);
-			M_stateMatrix = new StateMatrix(width);
-			M_Width = width;
+			M_stateMatrix = new StateMatrix(internalArray.GetLength(0));
 		}
 
 		public override bool this[int i, int j]
