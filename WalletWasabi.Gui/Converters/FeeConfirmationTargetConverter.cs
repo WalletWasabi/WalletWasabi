f6b873e3f227eeb4bed393f0fa4796cda45817bf
Dan Walmsley <dan@walms.co.uk>
2018-06-27 19:24:22 +0100

    Add tooltip for coin list details.
    implement sent tab controls

---
+++ WalletWasabi.Gui/Converters/FeeConfirmationTargetConverter.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace WalletWasabi.Gui.Converters
+{
+	public class FeeConfirmationTargetConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is int integer)
+			{
+				return "20 minutes - 7 days";
+			}
+
+			throw new InvalidOperationException();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
