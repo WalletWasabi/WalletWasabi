9d567ccd783831aa74aa41c613f67bd9c5ba17d1
Dan Walmsley <dan@walms.co.uk>
2018-06-29 01:47:02 +0100

    fix coinlist vm we no longer need to have a SelectedCoins list.

--- WalletWasabi.Gui/Controls/WalletExplorer/CoinListViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/CoinListViewModel.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using ReactiveUI;
 using System.Collections.ObjectModel;
 using WalletWasabi.Models;
-using System.Linq;
+using WalletWasabi.Gui.ViewModels;
 
 namespace WalletWasabi.Gui.Controls.WalletExplorer
 {
-	public class CoinListViewModel : CoinListViewModelBase
+	public class CoinListViewModel : ViewModelBase
 	{
-		private ObservableCollection<CoinViewModel> _coins;
+		private IReactiveDerivedList<CoinViewModel> _coins;
 
-		public CoinListViewModel(IEnumerable<SmartCoin> coins)
+		public CoinListViewModel(IEnumerable<CoinViewModel> coins)
 		{
-			Coins = new ObservableCollection<CoinViewModel>(coins.Select(c => new CoinViewModel(this, c)));
+			Coins = coins.CreateDerivedCollection(c => c, null, (first, second) => first.Amount.CompareTo(second.Amount), RxApp.MainThreadScheduler);
 		}
 
-		public CoinListViewModel()
-		{
-			Coins = new ObservableCollection<CoinViewModel>();
-		}
-
-		public ObservableCollection<CoinViewModel> Coins
+		public IReactiveDerivedList<CoinViewModel> Coins
 		{
 			get { return _coins; }
 			set { this.RaiseAndSetIfChanged(ref _coins, value); }
