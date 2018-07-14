0ff52aaaf71dce1402f1468916baa68dd3230a74
Dan Walmsley <dan@walms.co.uk>
2018-07-14 22:13:28 +0100

    implement update ui.

---
+++ WalletWasabi.Gui/Behaviors/CommandOnClickBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using System.Reactive.Disposables;
+
+namespace WalletWasabi.Gui.Behaviors
+{
+	public class CommandOnClickBehavior : CommandBasedBehavior<InputElement>
+    {
+        private CompositeDisposable _disposables;
+
+        protected override void OnAttached()
+        {
+            _disposables = new CompositeDisposable();
+
+            base.OnAttached();
+
+            _disposables.Add(AssociatedObject.AddHandler(InputElement.PointerPressedEvent, (sender, e) =>
+            {
+                e.Handled = ExecuteCommand();
+            }));
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            _disposables.Dispose();
+        }
+    }
+}
