2646482985f070086ccf6dea9846dc200594a281
Luis Silva <luiss@hush.ai>
2018-07-15 12:21:24 +0700

    Cursor changes based on update availability
    Created converter to convert bool to Cursor.

---
+++ WalletWasabi.Gui/Converters/ShowCursorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Avalonia.Data.Converters;
+using Avalonia.Input;
+
+namespace WalletWasabi.Gui.Converters
+{
+    public class ShowCursorConverter : IValueConverter
+    {
+	    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	    {
+		    if (value is bool boolean && boolean)
+		    {
+			    return new Cursor(StandardCursorType.Hand);
+		    }
+
+		    return new Cursor(StandardCursorType.Arrow);
+	    }
+
+	    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	    {
+		    throw new NotSupportedException();
+	    }
+    }
+}
