baf2c0f63b057532ec2a144c0b6e8d0a39ff9636
Lucas Ontivero <lucasontivero@gmail.com>
2018-07-16 14:09:12 -0300

    Use shield icons for anonimity set

--- WalletWasabi.Gui/Converters/PrivacyLevelValueConverter.cs
+++ WalletWasabi.Gui/Converters/PrivacyLevelValueConverter.cs
@@ -1,31 +1,59 @@
-﻿using Avalonia.Data.Converters;
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using AvalonStudio.Commands;
 using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Drawing;
 using System.Globalization;
 
 namespace WalletWasabi.Gui.Converters
 {
 	public class PrivacyLevelValueConverter : IValueConverter
 	{
+        private readonly static Dictionary<string, DrawingGroup> Cache = new Dictionary<string, DrawingGroup>();
+
+        public DrawingGroup GetIconByName(string icon)
+        {
+            if (!Cache.TryGetValue(icon, out var image))
+            {
+                if (Application.Current.Styles.TryGetResource(icon.ToString(), out object resource))
+                {
+                    image = resource as DrawingGroup;
+                    Cache.Add(icon, image);
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Icon {icon} not found");
+                }
+            }
+
+            return image;
+        }
+
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
 			if (value is int integer)
 			{
+				var shield = string.Empty;
 				if (integer <= 1)
 				{
-					return "None";
+					shield = "None";
 				}
 				else if (integer < 21)
 				{
-					return "Some";
+					shield = "Some";
 				}
 				else if (integer < 49)
 				{
-					return "Fine";
+					shield = "Fine";
 				}
 				else
 				{
-					return "Strong";
+					shield = "Strong";
 				}
+				return GetIconByName("PrivacyWarning");
 			}
 
 			throw new InvalidOperationException();
