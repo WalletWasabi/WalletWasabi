d815d57f0b47891a74b6d7035f30ec6cd6955a02
Dan Walmsley <dan@walms.co.uk>
2018-07-16 22:45:28 +0100

    implement validation

---
+++ WalletWasabi.Gui/ViewModels/Validation/ValidateMethodAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalletWasabi.Gui.ViewModels.Validation
+{
+	[AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+	public sealed class ValidateMethodAttribute : Attribute
+	{
+		private readonly string _methodName;
+
+		public ValidateMethodAttribute(string methodName)
+		{
+			_methodName = methodName;
+		}
+
+		public string MethodName
+		{
+			get { return _methodName; }
+		}
+	}
+}
