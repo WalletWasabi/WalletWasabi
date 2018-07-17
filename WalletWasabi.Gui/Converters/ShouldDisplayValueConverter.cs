ee5da8ff2192114982752ff0611525d9d2a08840
nopara73 <adam.ficsor73@gmail.com>
2018-07-17 11:59:46 +0100

    Hide unneccessary data

---
+++ WalletWasabi.Gui/Converters/ShouldDisplayValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Avalonia.Data.Converters;
+
+namespace WalletWasabi.Gui.Converters
+{
+	public class ShouldDisplayValueConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is int integer && integer <= 0)
+			{
+				return false;
+			}
+
+			return true;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
