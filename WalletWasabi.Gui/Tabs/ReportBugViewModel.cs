dfd5f9a797c9c211c5083071296c441436007bc6
Dan Walmsley <dan@walms.co.uk>
2018-07-17 19:03:50 +0100

    implements url copy from context menu.

--- WalletWasabi.Gui/Tabs/ReportBugViewModel.cs
+++ WalletWasabi.Gui/Tabs/ReportBugViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using WalletWasabi.Gui.ViewModels;
+using ReactiveUI;
+using Avalonia;
+using System;
 
 namespace WalletWasabi.Gui.Tabs
 {
@@ -6,6 +9,22 @@ namespace WalletWasabi.Gui.Tabs
 	{
 		public ReportBugViewModel() : base("Report Bug")
 		{
+			CopyUrlCommand = ReactiveCommand.Create(() =>
+			{
+				try
+				{
+					Application.Current.Clipboard.SetTextAsync(IssuesURL).GetAwaiter().GetResult();
+				}
+				catch (Exception)
+				{
+					// Apparently this exception sometimes happens randomly.
+					// The MS controls just ignore it, so we'll do the same.
+				}
+			});
 		}
+
+		public string IssuesURL => "http://github.com/zkSNACKs/WalletWasabi/issues";
+
+		public ReactiveCommand CopyUrlCommand { get; }
 	}
 }
