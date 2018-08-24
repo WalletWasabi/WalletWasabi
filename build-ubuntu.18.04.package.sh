5a1806fb6a756a0f252c060940883442eef51363
Lucas Ontivero <lucasontivero@gmail.com>
2018-08-24 16:57:03 -0300

    Do not publish self-contained application

--- build-ubuntu.18.04.package.sh
+++ build-ubuntu.18.04.package.sh
@@ -1,51 +1,62 @@
-FOLDER=wasabi
+SOLUTION_FOLDER=$(pwd)
+PACKAGE_FOLDER=$SOLUTION_FOLDER/wasabi
+BIN_FOLDER=/usr/local/bin
+PACKAGE_BIN_FOLDER=$PACKAGE_FOLDER$BIN_FOLDER/wasabi.d
+INSTALLATION_FOLDER=$BIN_FOLDER/wasabi.d
+DEBIAN_FOLDER=$PACKAGE_FOLDER/DEBIAN
+CONTROL_FILE=$DEBIAN_FOLDER/control
+POST_INST_FILE=$DEBIAN_FOLDER/postinst
+PRE_RM_FILE=$DEBIAN_FOLDER/prerm
 
-dotnet clean
-rm -rf $FOLDER
+
+rm -rf $PACKAGE_FOLDER
+rm wasabi.linux_all.deb
+rm -rf $SOLUTION_FOLDER/**/bin $SOLUTION_FOLDER/**/obj
+
+(
 cd WalletWasabi.Gui
-dotnet publish -c Release -r ubuntu.18.04-x64 --self-contained
-cd ..
+dotnet publish --force \
+	--configuration Release \
+	--output $PACKAGE_BIN_FOLDER
+#	--runtime ubuntu.18.04-x64 \
+)
 
-mkdir $FOLDER && mkdir $FOLDER/DEBIAN
-mkdir -p $FOLDER/usr/local/bin
-cp -r WalletWasabi.Gui/bin/Release/netcoreapp2.1/ubuntu.18.04-x64/publish/. $FOLDER/usr/local/bin/wasabiwallet.d/
+mkdir -p $PACKAGE_BIN_FOLDER $DEBIAN_FOLDER
 
-cat <<EOT >> $FOLDER/DEBIAN/control
+cat <<EOT > $CONTROL_FILE
 Package: WasabiWallet
-Version: 1.0.0
+Version: 1.0.0-beta
 Maintainer: Adam Ficzor <adam.ficzor@gmail.com>
 Architecture: all
-Depends: libc6, libgcc1, libgssapi-krb5-2, liblttng-ust0, libssl0.9.8 | libssl1.0.0 | libssl1.0.1 | libssl1.0.2, libstdc++6, libunwind8, libuuid1, zlib1g
+Homepage: https://www.wasabiwallet.io/
 Description: The privacy-oriented light bitcoin wallet
  is an open source wallet distributed under MIT license.
 EOT
 
-cat <<EOT >> $FOLDER/DEBIAN/postinst
+cat <<EOT > $POST_INST_FILE
 #!/bin/sh
 set -e
-cat <<IEOT >> /usr/local/bin/wasabi 
+cat <<IEOT > $BIN_FOLDER/wasabi
 #!/bin/sh
 
-dotnet /usr/local/bin/wasabiwallet.d/WalletWasabi.Gui.dll
+dotnet $INSTALLATION_FOLDER/WalletWasabi.Gui.dll
 IEOT
 
-chmod +x /usr/local/bin/wasabi
+chmod +x $BIN_FOLDER/wasabi
 
 echo Installed!!!!
 EOT
 
 
-cat <<EOT >> $FOLDER/DEBIAN/prerm
-
+cat <<EOT > $PRE_RM_FILE
 #!/bin/sh
 set -e
-rm /ust/local/bin/wasabi
+rm -f $BIN_FOLDER/wasabi
+rm -rf $INSTALLATION_FOLDER
 echo Removed!!!
 EOT
 
-find ./$FOLDER/DEBIAN -type f | xargs chmod 755
+chmod -R 0775 $DEBIAN_FOLDER
+dpkg-deb --build $PACKAGE_FOLDER
 
-dpkg-deb --build $FOLDER
-
-mv $FOLDER.deb wasabi.linux_all.deb
-
+mv $PACKAGE_FOLDER.deb wasabi.linux_all.deb
