f9e844896c68fc6599161d3b253e06f7d26420c5
Lucas Ontivero <lucasontivero@gmail.com>
2018-09-02 00:35:49 -0300

    Update contant  P2pkhInputSize

--- WalletWasabi/Helpers/Constants.cs
+++ WalletWasabi/Helpers/Constants.cs
@@ -14,7 +14,7 @@ namespace WalletWasabi.Helpers
 		public const uint ProtocolVersion_WITNESS_VERSION = 70012;
 
 		public const int P2wpkhInputSizeInBytes = 41;
-		public const int P2pkhInputSizeInBytes = 146;
+		public const int P2pkhInputSizeInBytes = 145;
 		public const int OutputSizeInBytes = 33;
 
 		// https://en.bitcoin.it/wiki/Bitcoin
