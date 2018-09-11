fd8db5fc21c8040253d0f9f401d6e5829e32e9ed
nopara73 <adam.ficsor73@gmail.com>
2018-09-11 20:38:15 +0200

    [Skip CI] Remove unused references

--- WalletWasabi.Tests/LiveServerTests.cs
+++ WalletWasabi.Tests/LiveServerTests.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using NBitcoin;
-using WalletWasabi.Backend.Models;
 using WalletWasabi.Backend.Models.Responses;
 using WalletWasabi.Services;
 using WalletWasabi.Tests.XunitConfiguration;
