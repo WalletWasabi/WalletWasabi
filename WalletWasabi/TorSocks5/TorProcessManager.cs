c5a0dd16147ae95beeb4747c8efb74d82a72d7d6
nopara73 <adam.ficsor73@gmail.com>
2018-09-11 20:12:53 +0200

    [Skip CI] PGP sign test - command line

--- WalletWasabi/TorSocks5/TorProcessManager.cs
+++ WalletWasabi/TorSocks5/TorProcessManager.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.Exceptions;
