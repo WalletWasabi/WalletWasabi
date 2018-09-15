b730163edef7ed7a43ef27980086437740556304
nopara73 <adam.ficsor73@gmail.com>
2018-09-15 11:03:35 +0200

    Prepare some needed strings

--- WalletWasabi.Packager/Program.cs
+++ WalletWasabi.Packager/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace WalletWasabi.Packager
 {
@@ -6,7 +8,21 @@ namespace WalletWasabi.Packager
 	{
 		private static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string packagerProjectDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\"));
+			string solutionDirectory = Path.GetFullPath(Path.Combine(packagerProjectDirectory, "..\\"));
+			string guiProjectDirectory = Path.GetFullPath(Path.Combine(solutionDirectory, "WalletWasabi.Gui\\"));
+
+			string version = Helpers.Constants.ClientVersion.ToString();
+
+			Console.WriteLine($"{nameof(solutionDirectory)}:\t\t{solutionDirectory}");
+			Console.WriteLine($"{nameof(packagerProjectDirectory)}:\t{packagerProjectDirectory}");
+			Console.WriteLine($"{nameof(guiProjectDirectory)}:\t\t{guiProjectDirectory}");
+			Console.WriteLine();
+			Console.WriteLine($"{nameof(version)}:\t\t\t{version}");
+
+			Console.WriteLine();
+			Console.WriteLine("FINISHED! Press key to exit...");
+			Console.ReadKey();
 		}
 	}
 }
