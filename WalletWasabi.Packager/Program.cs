d0137a9fbbad691ca9b9763c3c50f00e887766b4
nopara73 <adam.ficsor73@gmail.com>
2018-09-15 11:21:56 +0200

    [Skip CI] Specify targets

--- WalletWasabi.Packager/Program.cs
+++ WalletWasabi.Packager/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace WalletWasabi.Packager
@@ -11,14 +13,38 @@ namespace WalletWasabi.Packager
 			string packagerProjectDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\"));
 			string solutionDirectory = Path.GetFullPath(Path.Combine(packagerProjectDirectory, "..\\"));
 			string guiProjectDirectory = Path.GetFullPath(Path.Combine(solutionDirectory, "WalletWasabi.Gui\\"));
-
-			string version = Helpers.Constants.ClientVersion.ToString();
-
 			Console.WriteLine($"{nameof(solutionDirectory)}:\t\t{solutionDirectory}");
 			Console.WriteLine($"{nameof(packagerProjectDirectory)}:\t{packagerProjectDirectory}");
 			Console.WriteLine($"{nameof(guiProjectDirectory)}:\t\t{guiProjectDirectory}");
+
+			string version = Helpers.Constants.ClientVersion.ToString();
 			Console.WriteLine();
 			Console.WriteLine($"{nameof(version)}:\t\t\t{version}");
+
+			// https://docs.microsoft.com/en-us/dotnet/articles/core/rid-catalog
+			// Tor supports: win32, linux32, linux64, osx64
+			// .NET Core supports: win32, win64, linux64, osx64
+			// We'll only support x64, if someone complains, we can come back to it.
+			var targets = new List<string>
+			{
+				"win-x64",
+				"linux-x64",
+				"osx-x64"
+			};
+			Console.WriteLine();
+			Console.Write($"{nameof(targets)}:\t\t\t");
+			targets.ForEach(x =>
+			{
+				if (targets.Last() != x)
+				{
+					Console.Write($"{x}, ");
+				}
+				else
+				{
+					Console.Write(x);
+				}
+			});
+			Console.WriteLine();
 
 			Console.WriteLine();
 			Console.WriteLine("FINISHED! Press key to exit...");
