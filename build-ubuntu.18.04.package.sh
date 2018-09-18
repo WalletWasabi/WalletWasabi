4158108732816ec180210d0372d260667f5f1c0f
Lucas Ontivero <lucasontivero@gmail.com>
2018-09-18 11:55:49 -0300

    Add Icon

--- build-ubuntu.18.04.package.sh
+++ build-ubuntu.18.04.package.sh
@@ -1,13 +1,18 @@
 SOLUTION_FOLDER=$(pwd)
 PACKAGE_FOLDER=$SOLUTION_FOLDER/wasabi
 BIN_FOLDER=/usr/local/bin
+APP_FOLDER=/usr/share/applications
+ICO_FOLDER=/usr/share/icons/hicolor/32x32/apps
+
 PACKAGE_BIN_FOLDER=$PACKAGE_FOLDER$BIN_FOLDER/wasabi.d
+PACKAGE_APP_FOLDER=$PACKAGE_FOLDER$APP_FOLDER
+PACKAGE_ICO_FOLDER=$PACKAGE_FOLDER$ICO_FOLDER
 INSTALLATION_FOLDER=$BIN_FOLDER/wasabi.d
 DEBIAN_FOLDER=$PACKAGE_FOLDER/DEBIAN
 CONTROL_FILE=$DEBIAN_FOLDER/control
 POST_INST_FILE=$DEBIAN_FOLDER/postinst
 PRE_RM_FILE=$DEBIAN_FOLDER/prerm
-
+DESKTOP_FILE=$PACKAGE_APP_FOLDER/wasabi.desktop
 
 rm -rf $PACKAGE_FOLDER
 rm wasabi.linux_all.deb
@@ -17,11 +22,16 @@ rm -rf $SOLUTION_FOLDER/**/bin $SOLUTION_FOLDER/**/obj
 cd WalletWasabi.Gui
 dotnet publish --force \
 	--configuration Release \
-	--output $PACKAGE_BIN_FOLDER
-#	--runtime ubuntu.18.04-x64 \
+	--output $PACKAGE_BIN_FOLDER \
+	--runtime ubuntu.18.04-x64 
 )
 
-mkdir -p $PACKAGE_BIN_FOLDER $DEBIAN_FOLDER
+mkdir -p $PACKAGE_BIN_FOLDER 
+mkdir -p $DEBIAN_FOLDER
+mkdir -p $PACKAGE_APP_FOLDER
+mkdir -p $PACKAGE_ICO_FOLDER
+
+cp WalletWasabi.Gui/Assets/WasabiLogo.png $PACKAGE_ICO_FOLDER/wasabi.png
 
 cat <<EOT > $CONTROL_FILE
 Package: WasabiWallet
@@ -54,6 +64,17 @@ set -e
 rm -f $BIN_FOLDER/wasabi
 rm -rf $INSTALLATION_FOLDER
 echo Removed!!!
+EOT
+
+cat <<EOT > $DESKTOP_FILE
+[Desktop Entry]
+Type=Application
+Name=Wasabi Wallet
+Comment=Launches Wasabi Bitcoin Wallet
+Icon=wasabi
+Terminal=false
+Exec=wasabi
+Categories=wallet
 EOT
 
 chmod -R 0775 $DEBIAN_FOLDER
