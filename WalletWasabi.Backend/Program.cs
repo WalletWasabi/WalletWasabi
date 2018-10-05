a2ab3387c20f7b069eecdacb01c22d26374f9012
nopara73 <adam.ficsor73@gmail.com>
2018-10-05 18:42:45 +0200

    -

--- WalletWasabi.Backend/Program.cs
+++ WalletWasabi.Backend/Program.cs
@@ -40,6 +40,8 @@ namespace WalletWasabi.Backend
 
 				await Global.InitializeAsync(config, roundConfig, rpc);
 
+				UnversionedWebBuilder.CreateClientVersionHtml();
+
 				var endPoint = "http://localhost:37127/";
 
 				using (var host = WebHost.CreateDefaultBuilder(args)
