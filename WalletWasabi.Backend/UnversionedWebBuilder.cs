c799999e238123c5f081445efa805b901c73e127
nopara73 <adam.ficsor73@gmail.com>
2018-10-05 18:43:35 +0200

    -

---
+++ WalletWasabi.Backend/UnversionedWebBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalletWasabi.Backend
+{
+	public static class UnversionedWebBuilder
+	{
+		public static string UnversionedFolder { get; } = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\wwwroot\\unversioned"));
+
+		public static string CreateFilePath(string fileName) => Path.Combine(UnversionedFolder, fileName);
+
+		public static void CreateClientVersionHtml()
+		{
+			var filePath = CreateFilePath("client-version.html");
+			var content = "<link href=\"txtstyle.css\" rel=\"stylesheet\" type=\"text/css\" />" + Environment.NewLine;
+			content += Helpers.Constants.ClientVersion.ToString();
+
+			File.WriteAllText(filePath, content);
+		}
+	}
+}
