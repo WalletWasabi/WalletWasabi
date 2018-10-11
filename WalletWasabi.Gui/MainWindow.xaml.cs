6676e6a7b5e26862459377b0eed0d22c5f2531f8
Dan Walmsley <dan@walms.co.uk>
2018-10-11 22:22:18 +0100

    update avalonia native with some osx specific customizations
    to make window management integrated with osx rather than looking like a simulated windows style app.

--- WalletWasabi.Gui/MainWindow.xaml.cs
+++ WalletWasabi.Gui/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using AvalonStudio.Extensibility;
+using AvalonStudio.Extensibility.Theme;
 using AvalonStudio.Shell;
 using AvalonStudio.Shell.Controls;
 using WalletWasabi.Gui.Tabs.WalletManager;
@@ -18,6 +21,15 @@ namespace WalletWasabi.Gui
 #if DEBUG
 			this.AttachDevTools();
 #endif
+
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				HasSystemDecorations = true;
+
+				// This will need implementing properly once this is supported by avalonia itself.
+				var color = (ColorTheme.CurrentTheme.Background as SolidColorBrush).Color;
+				(PlatformImpl as Avalonia.Native.WindowImpl).SetTitleBarColor(color);
+			}
 		}
 
 		private void InitializeComponent()
