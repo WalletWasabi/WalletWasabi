031c092fd7dc99cff1da6621c73cf9fb0d101dfb
JuniperTonic <cornfedweasle@protonmail.com>
2018-11-03 10:48:59 -0600

    # 780 Improve Send Error Feedback

--- WalletWasabi/Exceptions/InsufficientBalanceException.cs
+++ WalletWasabi/Exceptions/InsufficientBalanceException.cs
@@ -5,7 +5,7 @@ namespace WalletWasabi.Exceptions
 {
 	public class InsufficientBalanceException : Exception
 	{
-		public InsufficientBalanceException(Money minimum, Money actual) : base($"Needed: {minimum.ToString(false, true)} BTC, got only: {actual.ToString(false, true)} BTC.")
+		public InsufficientBalanceException(Money minimum, Money actual) : base($"Needed: {minimum.ToString(false, true)} BTC for this transaction but only: {actual.ToString(false, true)} BTC was selected to send.")
 		{
 		}
 	}
