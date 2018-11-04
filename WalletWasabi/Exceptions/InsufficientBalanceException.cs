ddc76d509a58e34240423c69e2a1ad5a80b6a12c
JuniperTonic <cornfedweasle@protonmail.com>
2018-11-04 13:46:39 -0700

    "Need X BTC for this transaction, but only Y BTC is selected to send."

--- WalletWasabi/Exceptions/InsufficientBalanceException.cs
+++ WalletWasabi/Exceptions/InsufficientBalanceException.cs
@@ -5,7 +5,7 @@ namespace WalletWasabi.Exceptions
 {
 	public class InsufficientBalanceException : Exception
 	{
-		public InsufficientBalanceException(Money minimum, Money actual) : base($"Needed: {minimum.ToString(false, true)} BTC for this transaction but only: {actual.ToString(false, true)} BTC was selected to send")
+		public InsufficientBalanceException(Money minimum, Money actual) : base($"Need {minimum.ToString(false, true)} BTC for this transaction, but only {actual.ToString(false, true)} BTC is selected to send.")
 		{
 		}
 	}
