4a0d8df4630bcaadeb3bd18ccc58c75831be86b6
nopara73 <adam.ficsor73@gmail.com>
2018-11-18 23:00:21 +0700

    [skip ci] Add tooltips

---
+++ WalletWasabi.Gui/Converters/CoinStatusToolTipConverter.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using WalletWasabi.Gui.Controls.WalletExplorer;
+using WalletWasabi.Gui.Models;
+using WalletWasabi.Models.ChaumianCoinJoin;
+
+namespace WalletWasabi.Gui.Converters
+{
+	public class CoinStatusToolTipConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is SmartCoinStatus status)
+			{
+				switch (status)
+				{
+					case SmartCoinStatus.Confirmed: return "This coin is confirmed.";
+					case SmartCoinStatus.Unconfirmed: return "This coin is unconfirmed.";
+					case SmartCoinStatus.MixingOnWaitingList: return "This coin is waiting for its turn to be coinjoined.";
+					case SmartCoinStatus.MixingBanned: return "The coordinator banned this coin from participation until ToDo:{}.";
+					case SmartCoinStatus.MixingInputRegistration: return "This coin is registered for coinjoin.";
+					case SmartCoinStatus.MixingConnectionConfirmation: return "This coin is currently in Connection Confirmation phase.";
+					case SmartCoinStatus.MixingOutputRegistration: return "This coin is currently in Output Registration phase.";
+					case SmartCoinStatus.MixingSigning: return "This coin is currently in Signing phase.";
+					case SmartCoinStatus.SpentAccordingToBackend: return "According to the backend, this coin has been spent, but Wasabi's mempool do not know about it. Correct wallet state will appear after confirmation.";
+					case SmartCoinStatus.MixingWaitingForConfirmation: return "Coinjoining unconfirmed coins is not allowed.";
+				}
+			}
+
+			throw new InvalidOperationException();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
