ffe388f030ef6682d1d34062422f46d5c6047489
Lucas Ontivero <lucasontivero@gmail.com>
2018-11-30 20:04:59 -0300

    Fix concurrency problem in enumerator

--- WalletWasabi/Models/ConcurrentHashSet.cs
+++ WalletWasabi/Models/ConcurrentHashSet.cs
@@ -9,29 +9,73 @@ using WalletWasabi.Helpers;
 namespace WalletWasabi.Models
 {
 	[DebuggerDisplay("Count = {Count}")]
-    public class ConcurrentHashSet<T> : IReadOnlyCollection<T>, IEnumerable<T>, IEnumerable
-    {
-        private readonly ConcurrentDictionary<T, byte> _dict = new ConcurrentDictionary<T, byte>();
+	public class ConcurrentHashSet<T> : IReadOnlyCollection<T>, IEnumerable<T>, IEnumerable
+	{
+		private const int DefaultConcurrencyLevel = 4;
 
-        public int Count 
-			=> _dict.Count;
+		private const int DefaultCapacity = 4_000;
 
-        public void Clear()
-			=> _dict.Clear();
+		private readonly ConcurrentDictionary<T, byte> _dictionary = new ConcurrentDictionary<T, byte>(DefaultConcurrencyLevel, DefaultCapacity);
 
-        public bool Contains(T item)
-			=> _dict.ContainsKey(item);
+		public int Count
+			=> _dictionary.Count;
 
-        public bool TryAdd(T item)
-            => _dict.TryAdd(item, 0);
+		public void Clear()
+			=> _dictionary.Clear();
 
-        public bool TryRemove(T item)
-			=> _dict.TryRemove(item, out byte dontCare);
+		public bool Contains(T item)
+			=> _dictionary.ContainsKey(item);
 
-        public IEnumerator<T> GetEnumerator()
-            => _dict.Keys.GetEnumerator();
+		public bool TryAdd(T item)
+			=> _dictionary.TryAdd(item, 0);
 
-        IEnumerator IEnumerable.GetEnumerator()
-            => _dict.Keys.GetEnumerator();
-    }
+		public bool TryRemove(T item)
+			=> _dictionary.TryRemove(item, out byte dontCare);
+
+		public KeyEnumerator GetEnumerator()
+			=> new KeyEnumerator(_dictionary);
+
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+			=> GetEnumeratorImpl();
+
+		IEnumerator IEnumerable.GetEnumerator()
+			=> GetEnumeratorImpl();
+
+		private IEnumerator<T> GetEnumeratorImpl()
+		{
+			foreach (var kvp in _dictionary)
+			{
+				yield return kvp.Key;
+			}
+		}
+
+
+		public struct KeyEnumerator : IEnumerator<T>
+		{
+			private readonly IEnumerator<KeyValuePair<T, byte>> _kvpEnumerator;
+
+			internal KeyEnumerator(IEnumerable<KeyValuePair<T, byte>> data)
+			{
+				_kvpEnumerator = data.GetEnumerator();
+			}
+
+			public T Current => _kvpEnumerator.Current.Key;
+
+			object IEnumerator.Current => _kvpEnumerator.Current.Key;
+
+			public void Dispose()
+			{
+			}
+
+			public bool MoveNext()
+			{
+				return _kvpEnumerator.MoveNext();
+			}
+
+			public void Reset()
+			{
+				_kvpEnumerator.Reset();
+			}
+		}
+	}
 }
