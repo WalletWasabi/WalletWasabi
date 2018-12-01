138e2af647e38bdb750e445d3bb914047cfc280a
nopara73 <adam.ficsor73@gmail.com>
2018-12-01 19:29:18 +0700

    [skip ci] dump version

--- WalletWasabi/Helpers/Constants.cs
+++ WalletWasabi/Helpers/Constants.cs
@@ -8,7 +8,7 @@ namespace WalletWasabi.Helpers
 {
 	public static class Constants
 	{
-		public static readonly Version ClientVersion = new Version(1, 0, 1);
+		public static readonly Version ClientVersion = new Version(1, 0, 2);
 		public const string BackendMajorVersion = "2";
 		public static readonly VersionsResponse VersionsResponse = new VersionsResponse { ClientVersion = ClientVersion.ToString(), BackenMajordVersion = BackendMajorVersion };
 
