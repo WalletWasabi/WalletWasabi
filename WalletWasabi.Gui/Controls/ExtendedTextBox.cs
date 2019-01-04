94015733261c9249cac190a3c134575a3085205f
MDavid <molnardavid84@gmail.com>
2019-01-04 12:09:54 +0100

    IsReadOnly propertyChange fix

--- WalletWasabi.Gui/Controls/ExtendedTextBox.cs
+++ WalletWasabi.Gui/Controls/ExtendedTextBox.cs
@@ -13,7 +13,7 @@ namespace WalletWasabi.Gui.Controls
 {
 	public class ExtendedTextBox : TextBox, IStyleable
 	{
-		private MenuItem _pasteItem;
+		private MenuItem _pasteItem = null;
 
 		public ExtendedTextBox()
 		{
@@ -27,25 +27,25 @@ namespace WalletWasabi.Gui.Controls
 				PasteAsync();
 			});
 
-			this.GetObservable(IsReadOnlyProperty).Subscribe(ro =>
+			this.GetObservable(IsReadOnlyProperty).Subscribe(isReadOnly =>
 			{
-				if (!(_pasteItem is null))
-				{
-					var items = ContextMenu.Items as Avalonia.Controls.Controls;
+				if (ContextMenu == null) return;
+				var items = ContextMenu.Items as Avalonia.Controls.Controls;
 
-					if (ro)
+				if (isReadOnly)
+				{
+					if (items.Contains(_pasteItem))
 					{
-						if (items.Contains(_pasteItem))
-						{
-							items.Remove(_pasteItem);
-						}
+						items.Remove(_pasteItem);
+						_pasteItem = null;
 					}
-					else
+				}
+				else
+				{
+					if (!items.Contains(_pasteItem))
 					{
-						if (!items.Contains(_pasteItem))
-						{
-							items.Add(_pasteItem);
-						}
+						CreatePasteItem();
+						items.Add(_pasteItem);
 					}
 				}
 			});
@@ -136,20 +136,27 @@ namespace WalletWasabi.Gui.Controls
 
 			if (!IsReadOnly)
 			{
-				var pastePresenter = new DrawingPresenter
-				{
-					Drawing = new GeometryDrawing
-					{
-						Brush = Brushes.LightGray,
-						Geometry = Geometry.Parse(
-							@"M19,20H5V4H7V7H17V4H19M12,2A1,1 0 0,1 13,3A1,1 0 0,1 12,4A1,1 0 0,1 11,3A1,1 0 0,1 12,2M19,2H14.82C14.4,0.84
-							13.3,0 12,0C10.7,0 9.6,0.84 9.18,2H5A2,2 0 0,0 3,4V20A2,2 0 0,0 5,22H19A2,2 0 0,0 21,20V4A2,2 0 0,0 19,2Z")
-					},
-					Width = 16,
-					Height = 16,
-				};
-				(ContextMenu.Items as Avalonia.Controls.Controls).Add(new MenuItem { Header = "Paste", Command = PasteCommand, Icon = pastePresenter });
+				CreatePasteItem();
+				(ContextMenu.Items as Avalonia.Controls.Controls).Add(_pasteItem);
 			}
+		}
+
+		private void CreatePasteItem()
+		{
+			if (_pasteItem != null) return;
+			var pastePresenter = new DrawingPresenter
+			{
+				Drawing = new GeometryDrawing
+				{
+					Brush = Brushes.LightGray,
+					Geometry = Geometry.Parse(
+			@"M19,20H5V4H7V7H17V4H19M12,2A1,1 0 0,1 13,3A1,1 0 0,1 12,4A1,1 0 0,1 11,3A1,1 0 0,1 12,2M19,2H14.82C14.4,0.84
+							13.3,0 12,0C10.7,0 9.6,0.84 9.18,2H5A2,2 0 0,0 3,4V20A2,2 0 0,0 5,22H19A2,2 0 0,0 21,20V4A2,2 0 0,0 19,2Z")
+				},
+				Width = 16,
+				Height = 16,
+			};
+			_pasteItem = new MenuItem { Header = "Paste", Command = PasteCommand, Icon = pastePresenter };
 		}
 	}
 }
