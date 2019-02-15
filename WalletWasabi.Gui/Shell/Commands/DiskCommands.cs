ae2403bff8855c555e50c07148e0234f7ca8e00c
nopara73 <adam.ficsor73@gmail.com>
2019-02-15 09:24:52 +0100

    Finish Open Menu

--- WalletWasabi.Gui/Shell/Commands/DiskCommands.cs
+++ WalletWasabi.Gui/Shell/Commands/DiskCommands.cs
@@ -17,8 +17,28 @@ namespace WalletWasabi.Gui.Shell.Commands
 		{
 			OpenDataFolderCommand = new CommandDefinition(
 				"Data Folder",
-				commandIconService.GetCompletionKindImage("DataFolder"),
+				commandIconService.GetCompletionKindImage("FolderOpen"),
 				ReactiveCommand.Create(OnOpenDataFolder));
+
+			OpenWalletsFolderCommand = new CommandDefinition(
+				"Wallets Folder",
+				commandIconService.GetCompletionKindImage("FolderOpen"),
+				ReactiveCommand.Create(OnOpenWalletsFolder));
+
+			OpenLogFileCommand = new CommandDefinition(
+				"Log File",
+				commandIconService.GetCompletionKindImage("Log"),
+				ReactiveCommand.Create(OnOpenLogFile));
+
+			OpenTorLogFileCommand = new CommandDefinition(
+				"Tor Log File",
+				commandIconService.GetCompletionKindImage("Log"),
+				ReactiveCommand.Create(OnOpenTorLogFile));
+
+			OpenConfigFileCommand = new CommandDefinition(
+				"Config File",
+				commandIconService.GetCompletionKindImage("Settings"),
+				ReactiveCommand.Create(OnOpenConfigFile));
 		}
 
 		private void OnOpenDataFolder()
@@ -26,7 +46,39 @@ namespace WalletWasabi.Gui.Shell.Commands
 			IoHelpers.OpenFolderInFileExplorer(Global.DataDir);
 		}
 
+		private void OnOpenWalletsFolder()
+		{
+			IoHelpers.OpenFolderInFileExplorer(Global.WalletsDir);
+		}
+
+		private void OnOpenLogFile()
+		{
+			IoHelpers.OpenFileInTextEditor(Logging.Logger.FilePath);
+		}
+
+		private void OnOpenTorLogFile()
+		{
+			IoHelpers.OpenFileInTextEditor(Global.TorLogsFile);
+		}
+
+		private void OnOpenConfigFile()
+		{
+			IoHelpers.OpenFileInTextEditor(Global.Config.FilePath);
+		}
+
 		[ExportCommandDefinition("File.Open.DataFolder")]
 		public CommandDefinition OpenDataFolderCommand { get; }
+
+		[ExportCommandDefinition("File.Open.WalletsFolder")]
+		public CommandDefinition OpenWalletsFolderCommand { get; }
+
+		[ExportCommandDefinition("File.Open.LogFile")]
+		public CommandDefinition OpenLogFileCommand { get; }
+
+		[ExportCommandDefinition("File.Open.TorLogFile")]
+		public CommandDefinition OpenTorLogFileCommand { get; }
+
+		[ExportCommandDefinition("File.Open.ConfigFile")]
+		public CommandDefinition OpenConfigFileCommand { get; }
 	}
 }
--- WalletWasabi.Gui/Shell/MainMenu/FileMainMenuItems.cs
+++ WalletWasabi.Gui/Shell/MainMenu/FileMainMenuItems.cs
@@ -21,44 +21,64 @@ namespace WalletWasabi.Gui.Shell.MainMenu
 		[DefaultOrder(0)]
 		public IMenuItem File => MenuItemFactory.CreateHeaderMenuItem("File", null);
 
-		[ExportMainMenuDefaultGroup("File", "Disk")]
-		[DefaultOrder(0)]
-		public object DiskGroup => null;
-
-		[ExportMainMenuItem("File", "Open")]
-		[DefaultOrder(0)]
-		public IMenuItem Open => MenuItemFactory.CreateHeaderMenuItem("Open", null);
-
-		[ExportMainMenuItem("File", "Open", "Data Folder")]
-		[DefaultOrder(1)]
-		[DefaultGroup("Disk")]
-		public IMenuItem OpenDataFolder => MenuItemFactory.CreateCommandMenuItem("File.Open.DataFolder");
-
 		[ExportMainMenuDefaultGroup("File", "Wallet")]
-		[DefaultOrder(1)]
+		[DefaultOrder(0)]
 		public object WalletGroup => null;
 
 		[ExportMainMenuItem("File", "Generate Wallet")]
-		[DefaultOrder(10)]
+		[DefaultOrder(1)]
 		[DefaultGroup("Wallet")]
 		public IMenuItem GenerateWallet => MenuItemFactory.CreateCommandMenuItem("File.GenerateWallet");
 
 		[ExportMainMenuItem("File", "Recover Wallet")]
-		[DefaultOrder(11)]
+		[DefaultOrder(2)]
 		[DefaultGroup("Wallet")]
 		public IMenuItem Recover => MenuItemFactory.CreateCommandMenuItem("File.RecoverWallet");
 
 		[ExportMainMenuItem("File", "Load Wallet")]
-		[DefaultOrder(12)]
+		[DefaultOrder(3)]
 		[DefaultGroup("Wallet")]
 		public IMenuItem LoadWallet => MenuItemFactory.CreateCommandMenuItem("File.LoadWallet");
+
+		[ExportMainMenuDefaultGroup("File", "Disk")]
+		[DefaultOrder(10)]
+		public object DiskGroup => null;
+
+		[ExportMainMenuItem("File", "Open")]
+		[DefaultOrder(11)]
+		public IMenuItem Open => MenuItemFactory.CreateHeaderMenuItem("Open", null);
+
+		[ExportMainMenuItem("File", "Open", "Data Folder")]
+		[DefaultOrder(111)]
+		[DefaultGroup("Disk")]
+		public IMenuItem OpenDataFolder => MenuItemFactory.CreateCommandMenuItem("File.Open.DataFolder");
+
+		[ExportMainMenuItem("File", "Open", "Wallets Folder")]
+		[DefaultOrder(112)]
+		[DefaultGroup("Disk")]
+		public IMenuItem OpenWalletsFolder => MenuItemFactory.CreateCommandMenuItem("File.Open.WalletsFolder");
+
+		[ExportMainMenuItem("File", "Open", "Log File")]
+		[DefaultOrder(113)]
+		[DefaultGroup("Disk")]
+		public IMenuItem OpenLogFile => MenuItemFactory.CreateCommandMenuItem("File.Open.LogFile");
+
+		[ExportMainMenuItem("File", "Open", "Tor Log File")]
+		[DefaultOrder(114)]
+		[DefaultGroup("Disk")]
+		public IMenuItem OpenTorLogFile => MenuItemFactory.CreateCommandMenuItem("File.Open.TorLogFile");
+
+		[ExportMainMenuItem("File", "Open", "Config File")]
+		[DefaultOrder(115)]
+		[DefaultGroup("Disk")]
+		public IMenuItem OpenConfigFile => MenuItemFactory.CreateCommandMenuItem("File.Open.ConfigFile");
 
 		[ExportMainMenuDefaultGroup("File", "Exit")]
 		[DefaultOrder(1000)]
 		public object ExitGroup => null;
 
 		[ExportMainMenuItem("File", "Exit")]
-		[DefaultOrder(0)]
+		[DefaultOrder(1001)]
 		[DefaultGroup("Exit")]
 		public IMenuItem Exit => MenuItemFactory.CreateCommandMenuItem("File.Exit");
 	}
