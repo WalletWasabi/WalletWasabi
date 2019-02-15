8981d06bee6fd38a387d5b15cb56ee37634a6770
nopara73 <adam.ficsor73@gmail.com>
2019-02-15 09:04:20 +0100

    Place data folder open to file open

---
+++ WalletWasabi.Gui/Shell/Commands/DiskCommands.cs
@@ -0,0 +1,32 @@
+﻿using AvalonStudio.Commands;
+using AvalonStudio.Documents;
+using AvalonStudio.Extensibility;
+using AvalonStudio.Shell;
+using ReactiveUI;
+using System.Composition;
+using System.IO;
+using System.Linq;
+using WalletWasabi.Gui.Tabs.WalletManager;
+
+namespace WalletWasabi.Gui.Shell.Commands
+{
+	internal class DiskCommands
+	{
+		[ImportingConstructor]
+		public DiskCommands(CommandIconService commandIconService)
+		{
+			OpenDataFolderCommand = new CommandDefinition(
+				"Data Folder",
+				commandIconService.GetCompletionKindImage("DataFolder"),
+				ReactiveCommand.Create(OnOpenDataFolder));
+		}
+
+		private void OnOpenDataFolder()
+		{
+			IoHelpers.OpenFolderInFileExplorer(Global.DataDir);
+		}
+
+		[ExportCommandDefinition("File.Open.DataFolder")]
+		public CommandDefinition OpenDataFolderCommand { get; }
+	}
+}
--- WalletWasabi.Gui/Shell/MainMenu/FileMainMenuItems.cs
+++ WalletWasabi.Gui/Shell/MainMenu/FileMainMenuItems.cs
@@ -21,22 +21,35 @@ namespace WalletWasabi.Gui.Shell.MainMenu
 		[DefaultOrder(0)]
 		public IMenuItem File => MenuItemFactory.CreateHeaderMenuItem("File", null);
 
-		[ExportMainMenuDefaultGroup("File", "Wallet")]
+		[ExportMainMenuDefaultGroup("File", "Disk")]
 		[DefaultOrder(0)]
+		public object DiskGroup => null;
+
+		[ExportMainMenuItem("File", "Open")]
+		[DefaultOrder(0)]
+		public IMenuItem Open => MenuItemFactory.CreateHeaderMenuItem("Open", null);
+
+		[ExportMainMenuItem("File", "Open", "Data Folder")]
+		[DefaultOrder(1)]
+		[DefaultGroup("Disk")]
+		public IMenuItem OpenDataFolder => MenuItemFactory.CreateCommandMenuItem("File.Open.DataFolder");
+
+		[ExportMainMenuDefaultGroup("File", "Wallet")]
+		[DefaultOrder(1)]
 		public object WalletGroup => null;
 
 		[ExportMainMenuItem("File", "Generate Wallet")]
-		[DefaultOrder(0)]
+		[DefaultOrder(10)]
 		[DefaultGroup("Wallet")]
 		public IMenuItem GenerateWallet => MenuItemFactory.CreateCommandMenuItem("File.GenerateWallet");
 
 		[ExportMainMenuItem("File", "Recover Wallet")]
-		[DefaultOrder(10)]
+		[DefaultOrder(11)]
 		[DefaultGroup("Wallet")]
 		public IMenuItem Recover => MenuItemFactory.CreateCommandMenuItem("File.RecoverWallet");
 
 		[ExportMainMenuItem("File", "Load Wallet")]
-		[DefaultOrder(20)]
+		[DefaultOrder(12)]
 		[DefaultGroup("Wallet")]
 		public IMenuItem LoadWallet => MenuItemFactory.CreateCommandMenuItem("File.LoadWallet");
 
