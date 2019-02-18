d632e6730f75818a267beed64051518028b3e270
MDavid <molnardavid84@gmail.com>
2019-02-18 12:16:49 +0100

    fix catch mechanism

--- WalletWasabi.Gui/Utils.cs
+++ WalletWasabi.Gui/Utils.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia.Threading;
 using System;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace WalletWasabi.Gui
 {
@@ -19,16 +20,34 @@ namespace WalletWasabi.Gui
 			throw new NotSupportedException("This is impossible.");
 		}
 
+		public static void PostLogException(this Dispatcher dispatcher, Func<Task> action, DispatcherPriority priority = DispatcherPriority.Normal)
+		{
+			dispatcher.Post(async () =>
+			{
+				try
+				{
+					await action();
+				}
+				catch (Exception ex)
+				{
+					Logging.Logger.LogDebug<Dispatcher>(ex);
+				}
+			}, priority);
+		}
+
 		public static void PostLogException(this Dispatcher dispatcher, Action action, DispatcherPriority priority = DispatcherPriority.Normal)
 		{
-			try
+			dispatcher.Post(() =>
 			{
-				dispatcher.Post(action, priority);
-			}
-			catch (Exception ex)
-			{
-				Logging.Logger.LogDebug<Dispatcher>(ex);
-			}
+				try
+				{
+					action();
+				}
+				catch (Exception ex)
+				{
+					Logging.Logger.LogDebug<Dispatcher>(ex);
+				}
+			}, priority);
 		}
 	}
 }
