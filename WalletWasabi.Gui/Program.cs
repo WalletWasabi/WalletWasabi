0139e7cf4fddedeb6b5767c8e165da65201b013d
Dan Walmsley <dan@walms.co.uk>
2019-02-19 18:36:39 +0000

    trigger garbage collection once uithread starts.
    This cuts almost 200MB from managed heap, which otherwise would be long lived.

--- WalletWasabi.Gui/Program.cs
+++ WalletWasabi.Gui/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using Avalonia.Threading;
 using AvalonStudio.Shell;
 using AvalonStudio.Shell.Extensibility.Platforms;
 using NBitcoin;
@@ -46,6 +47,11 @@ namespace WalletWasabi.Gui
 						{
 							MainWindowViewModel.Instance.Title += $" - {Global.Synchronizer.Network}";
 						}
+
+						Dispatcher.UIThread.Post(() =>
+						{
+							GC.Collect();
+						});
 					}).StartShellApp<AppBuilder, MainWindow>("Wasabi Wallet", null, () => MainWindowViewModel.Instance);
 			}
 			catch (Exception ex)
