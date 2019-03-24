7ff83e88e2ffc738fbbda5d42be8ff15e0dbd86a
lontivero <lucasontivero@gmail.com>
2019-03-24 16:48:51 -0300

    Improve constrast

--- WalletWasabi.Gui/Converters/NotificationTypeColorConverter.cs
+++ WalletWasabi.Gui/Converters/NotificationTypeColorConverter.cs
@@ -16,20 +16,21 @@ namespace WalletWasabi.Gui.Converters
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
+			var background = (parameter.ToString() == "Background");
 			IBrush brush = Brush.Parse("#2d2d30");
 			switch ((NotificationTypeEnum)value)
 			{
 				case NotificationTypeEnum.Info: 
-					brush = Brush.Parse("#2d2d30");
+					brush = Brush.Parse(background ? "#2d2d30" : "#FFFFFF");
 					break;
 				case NotificationTypeEnum.Error: 
-					brush = Brush.Parse("#E34937");
+					brush = Brush.Parse(background ? "#E34937" : "#D1E337");
 					break;
 				case NotificationTypeEnum.Warning: 
-					brush = Brush.Parse("#D78A04");
+					brush = Brush.Parse(background ? "#D78A04" : "#E0331F");
 					break;
 				case NotificationTypeEnum.Success: 
-					brush = Brush.Parse("#008800");
+					brush = Brush.Parse(background ? "#008800" : "#E4E64C");
 					break;
 			}
 
