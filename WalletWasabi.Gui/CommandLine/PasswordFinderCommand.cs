2feaf8dba94fbb712106a9fb9796530f63f92742
lontivero <lucasontivero@gmail.com>
2019-04-13 15:29:08 -0300

    Fix command usage doc

--- WalletWasabi.Gui/CommandLine/PasswordFinderCommand.cs
+++ WalletWasabi.Gui/CommandLine/PasswordFinderCommand.cs
@@ -19,12 +19,12 @@ namespace WalletWasabi.Gui.CommandLine
 			Language = "en";
 
 			Options = new OptionSet () {
-				"usage: findpassword --secret:encrypted-secret --language:lang --numbers:[TRUE|FALSE] --symbold:[TRUE|FALSE]",
+				"usage: findpassword --wallet:walet-file-path --language:lang --numbers:[TRUE|FALSE] --symbold:[TRUE|FALSE]",
 				"",
 				"Tries to find typing mistakes in the user password by brute forcing it char by char.",
-				"eg: findpassword --secret:6PYSeErf23ArQL7xXUWPKa3VBin6cuDaieSdABvVyTA51dS4Mxrtg1CpGN --numbers:false --symbold:true",
+				"eg: .wassabee findpassword --wallet:/home/user/.wasabiwallet/client/Wallets/my-wallet.json --numbers:false --symbold:true",
 				"",
-				{ "w|wallet=", "The specified wallet file.", 
+				{ "w|wallet=", "The path to the wallet file.", 
 					x =>  Wallet = x },
 				{ "l|language=", "The charset to use: en, es, it, fr, pt. Default=en.",
 					v => Language = v },
