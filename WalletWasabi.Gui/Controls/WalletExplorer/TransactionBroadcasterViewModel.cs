3a75f9254f412f5e2793f897f228db495b53548b
MDavid <molnardavid84@gmail.com>
2019-04-19 12:04:24 +0200

    Tx Broadcaster binding

--- WalletWasabi.Gui/Controls/WalletExplorer/TransactionBroadcasterViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/TransactionBroadcasterViewModel.cs
@@ -1,14 +1,49 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Reactive.Disposables;
 
 namespace WalletWasabi.Gui.Controls.WalletExplorer
 {
 	public class TransactionBroadcasterViewModel : WalletActionViewModel
 	{
+		private string _transactionString;
+		private string _errorMessage;
+		private string _successMessage;
+
 		private CompositeDisposable Disposables { get; set; }
+
+		public string TransactionString
+		{
+			get => _transactionString;
+			set => this.RaiseAndSetIfChanged(ref _transactionString, value);
+		}
+
+		public string ErrorMessage
+		{
+			get => _errorMessage;
+			set => this.RaiseAndSetIfChanged(ref _errorMessage, value);
+		}
+
+		public string SuccessMessage
+		{
+			get => _successMessage;
+			set => this.RaiseAndSetIfChanged(ref _successMessage, value);
+		}
 
 		public TransactionBroadcasterViewModel(WalletViewModel walletViewModel) : base("Transaction Broadcaster", walletViewModel)
 		{
+		}
+
+		private static bool IsValidTransaction(string txstring)
+		{
+			return false;
+		}
+
+		private void OnTransactionTextChanged()
+		{
+			if (!IsValidTransaction(null))
+			{
+			}
 		}
 
 		public override void OnOpen()
