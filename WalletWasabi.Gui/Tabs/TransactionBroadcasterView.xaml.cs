c95797413cb32bd260ee250095309117e28eeeb6
MDavid <molnardavid84@gmail.com>
2019-04-19 08:58:00 +0200

    Tx broadcaster Tab basic

---
+++ WalletWasabi.Gui/Tabs/TransactionBroadcasterView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Gui.Tabs
+{
+	public class TransactionBroadcasterView : UserControl
+	{
+		public TransactionBroadcasterView()
+		{
+			InitializeComponent();
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
---
+++ WalletWasabi.Gui/Tabs/TransactionBroadcasterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Text;
+using WalletWasabi.Gui.ViewModels;
+
+namespace WalletWasabi.Gui.Tabs
+{
+	public class TransactionBroadcasterViewModel : WasabiDocumentTabViewModel
+	{
+		private CompositeDisposable Disposables { get; set; }
+
+		public TransactionBroadcasterViewModel() : base("TransactionBroadcaster")
+		{
+		}
+
+		public override void OnOpen()
+		{
+			if (Disposables != null)
+			{
+				throw new Exception("TransactionBroadcaster was opened before it was closed.");
+			}
+
+			Disposables = new CompositeDisposable();
+
+			base.OnOpen();
+		}
+
+		public override bool OnClose()
+		{
+			Disposables?.Dispose();
+			Disposables = null;
+
+			return base.OnClose();
+		}
+	}
+}
