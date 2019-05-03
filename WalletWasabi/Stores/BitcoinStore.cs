fce209e7964ce01014fe7e50f4d3b905159851ba
nopara73 <adam.ficsor73@gmail.com>
2019-05-03 11:48:26 +0200

    Add comment.

--- WalletWasabi/Stores/BitcoinStore.cs
+++ WalletWasabi/Stores/BitcoinStore.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace WalletWasabi.Stores
 {
+	/// <summary>
+	/// The purpose of this class is to safely and performantly manage all the Bitcoin related data
+	/// that's being serialized to disk, like transactions, wallet files, keys, blocks, index files, etc...
+	/// </summary>
 	public class BitcoinStore
 	{
 	}
