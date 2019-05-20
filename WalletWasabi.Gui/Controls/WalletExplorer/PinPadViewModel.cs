5c382af9d9693cecd820ac0409bc0819cde6555d
molnard <molnardavid84@gmail.com>
2019-05-20 20:47:50 +0200

    Add commands.

--- WalletWasabi.Gui/Controls/WalletExplorer/PinPadViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/PinPadViewModel.cs
@@ -1,7 +1,10 @@
 using Avalonia.Threading;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Text;
 
 namespace WalletWasabi.Gui.Controls.WalletExplorer
@@ -9,6 +12,10 @@ namespace WalletWasabi.Gui.Controls.WalletExplorer
 	internal class PinPadViewModel : WalletActionViewModel
 	{
 		private CompositeDisposable Disposables { get; set; }
+		private string _maskedPin;
+
+		public ReactiveCommand<Unit, Unit> SendPinCommand { get; }
+		public ReactiveCommand<string, Unit> KeyPadCommand { get; }
 
 		/*
 		 * 7 8 9
@@ -16,8 +23,27 @@ namespace WalletWasabi.Gui.Controls.WalletExplorer
 		 * 1 2 3
 		 */
 
+		public string MaskedPin
+		{
+			get => _maskedPin;
+			set => this.RaiseAndSetIfChanged(ref _maskedPin, value);
+		}
+
 		public PinPadViewModel(WalletViewModel walletViewModel) : base("Pin Pad", walletViewModel)
 		{
+			SendPinCommand = ReactiveCommand.CreateFromTask(async () =>
+			{
+			},
+			this.WhenAny(x => x.MaskedPin, (maskedPin) => (!string.IsNullOrWhiteSpace(maskedPin.Value))));
+
+			KeyPadCommand = ReactiveCommand.Create<string>((arg) =>
+			{
+				MaskedPin += arg;
+			});
+
+			Observable.Merge(SendPinCommand.ThrownExceptions)
+			.Merge(KeyPadCommand.ThrownExceptions)
+			.Subscribe(OnException);
 		}
 
 		public override void OnOpen()
