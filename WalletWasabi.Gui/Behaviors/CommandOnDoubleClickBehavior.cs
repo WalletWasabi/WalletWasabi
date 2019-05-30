3d8b99d34afafd2c4ec1d28aaece708bbd728b54
lontivero <lucasontivero@gmail.com>
2019-05-30 10:40:13 -0300

    Open wallet file with double click

---
+++ WalletWasabi.Gui/Behaviors/CommandOnDoubleClickBehavior.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia.Controls;
+using System.Reactive.Disposables;
+
+namespace WalletWasabi.Gui.Behaviors
+{
+    public class CommandOnDoubleClickBehavior : CommandBasedBehavior<Control>
+    {
+        private CompositeDisposable _disposables;
+
+        protected override void OnAttached()
+        {
+            _disposables = new CompositeDisposable();
+
+            base.OnAttached();
+
+            _disposables.Add(AssociatedObject.AddHandler(Control.PointerPressedEvent, (sender, e) =>
+            {
+                if(e.ClickCount == 2)
+                {
+                    e.Handled = ExecuteCommand();
+                }
+            }));
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            _disposables.Dispose();
+        }
+    }
+}
