b9b2e67d2e244fb64d443078f68f41751ddb5ab4
nicolas.dorier <nicolas.dorier@gmail.com>
2019-06-18 18:26:34 +0900

    Revert "Revert "Fix hanging block download""
    This reverts commit 16bfa32ceb13b44591c6ee5e32d61a0d747923fd.

--- WalletWasabi/Services/WalletService.cs
+++ WalletWasabi/Services/WalletService.cs
@@ -806,16 +806,12 @@ namespace WalletWasabi.Services
 
 							Block blockFromLocalNode = null;
 							// Should timeout faster. Not sure if it should ever fail though. Maybe let's keep like this later for remote node connection.
-							using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(64))) // 1/2 ADSL	512 kbit/s	00:00:32
+							using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(RuntimeParams.Instance.NetworkNodeTimeout)))
 							{
-								blockFromLocalNode = LocalBitcoinCoreNode.GetBlocks(new uint256[] { hash }, cts.Token)?.Single();
+								blockFromLocalNode = await LocalBitcoinCoreNode.DownloadBlockAsync(hash, cts.Token);
 							}
 
-							if (blockFromLocalNode is null)
-							{
-								throw new InvalidOperationException($"Disconnected local node, because couldn't parse received block.");
-							}
-							else if (!blockFromLocalNode.Check())
+							if (!blockFromLocalNode.Check())
 							{
 								throw new InvalidOperationException($"Disconnected node, because block invalid block received!");
 							}
@@ -863,14 +859,7 @@ namespace WalletWasabi.Services
 						{
 							using (var cts = new CancellationTokenSource(TimeSpan.FromSeconds(RuntimeParams.Instance.NetworkNodeTimeout))) // 1/2 ADSL	512 kbit/s	00:00:32
 							{
-								block = node.GetBlocks(new uint256[] { hash }, cts.Token)?.Single();
-							}
-
-							if (block is null)
-							{
-								Logger.LogInfo<WalletService>($"Disconnected node: {node.RemoteSocketAddress}, because couldn't parse received block.");
-								node.DisconnectAsync("Couldn't parse block.");
-								continue;
+								block = await node.DownloadBlockAsync(hash, cts.Token);
 							}
 
 							if (!block.Check())
@@ -1580,7 +1569,9 @@ namespace WalletWasabi.Services
 				Encoding.UTF8);
 		}
 
-		// Current timeout used when downloading a block from the remote node. It is defined in seconds.
+		/// <summary>
+		/// Current timeout used when downloading a block from the remote node. It is defined in seconds.
+		/// </summary>
 		private async Task NodeTimeoutsAsync(bool increaseDecrease)
 		{
 			if (increaseDecrease)
