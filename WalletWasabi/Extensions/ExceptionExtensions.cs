12d0f2851691570a51187bfb2c6ae652b5c943bc
nopara73 <adam.ficsor73@gmail.com>
2019-06-28 12:39:11 +0200

    Fix ExceptionExtensions

--- WalletWasabi/Extensions/ExceptionExtensions.cs
+++ WalletWasabi/Extensions/ExceptionExtensions.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Net.Http;
+using WalletWasabi.Helpers;
 
 namespace System
 {
@@ -7,27 +8,45 @@ namespace System
 	{
 		public static string ToTypeMessageString(this Exception ex)
 		{
-			return $"{ex.GetType().Name}: {ex.Message}";
+			var trimmed = Guard.Correct(ex.Message);
+			if (trimmed == "")
+			{
+				return ex.GetType().Name;
+			}
+			else
+			{
+				return $"{ex.GetType().Name}: {ex.Message}";
+			}
 		}
+
+		public static Dictionary<string, string> BitcoinCoreTransalations { get; } = new Dictionary<string, string> {
+			["too-long-mempool-chain"] = "At least one coin you are trying to spend is part of long chain of unconfirmed transactions. You must wait for some previous transactions to confirm.",
+			["bad-txns-inputs-missingorspent"] = "At least one coin you are trying to spend is already spent.",
+			["missing-inputs"] = "At least one coin you are trying to spend is already spent.",
+			["bad-txns-inputs-duplicate"] = "The transaction contains duplicated inputs.",
+			["bad-txns-nonfinal"] = "The transaction is not final and cannot be broadcasted.",
+			["bad-txns-oversize"] = "The transaction is too big."
+		};
 
 		public static string ToUserFriendlyString(this HttpRequestException ex)
 		{
-			var transalations = new Dictionary<string, string>
+			var trimmed = Guard.Correct(ex.Message);
+			if (trimmed == "")
 			{
-				["too-long-mempool-chain"] = "At least one coin you are trying to spend is part of long chain of unconfirmed transactions. You must wait for some previous transactions to confirm.",
-				["bad-txns-inputs-missingorspent"] = "At least one coin you are trying to spend is already spent.",
-				["missing-inputs"] = "At least one coin you are trying to spend is already spent.",
-				["bad-txns-inputs-duplicate"] = "The transaction contains duplicated inputs.",
-				["bad-txns-nonfinal"] = "The transaction is not final and cannot be broadcasted.",
-				["bad-txns-oversize"] = "The transaction is too big."
-			};
-
-			var msg = ex.Message.Substring(0, ex.Message.IndexOf(','));
-			if(transalations.ContainsKey(msg))
-			{
-				return transalations[msg];
+				return ex.ToTypeMessageString();
 			}
-			return ex.ToTypeMessageString();
+			else
+			{
+				foreach (KeyValuePair<string, string> pair in BitcoinCoreTransalations)
+				{
+					if (trimmed.Contains(pair.Key))
+					{
+						return pair.Value;
+					}
+				}
+
+				return ex.ToTypeMessageString();
+			}
 		}
 	}
 }
