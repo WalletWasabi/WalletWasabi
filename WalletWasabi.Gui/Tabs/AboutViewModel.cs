1cb08d1f61ec44e13fe005dd42428f7829253d93
Ben Carman <benthecarman@live.com>
2019-07-01 15:50:29 -0500

    Add functionality to about page links

--- WalletWasabi.Gui/Tabs/AboutViewModel.cs
+++ WalletWasabi.Gui/Tabs/AboutViewModel.cs
@@ -1,5 +1,7 @@
 using Avalonia.Diagnostics.ViewModels;
 using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Text;
 using WalletWasabi.Gui.ViewModels;
@@ -16,5 +18,65 @@ namespace WalletWasabi.Gui.Tabs
 		public Version Version { get; }
 
 		public string VersionText => $"v{Version.ToString()}";
+
+		public string ClearnetLink => "https://wasabiwallet.io/";
+
+		public string TorLink => "http://wasabiukrxmkdgve5kynjztuovbg43uxcbcxn6y2okcrsg7gb6jdmbad.onion";
+
+		public string SourceCodeLink => "https://github.com/zkSNACKs/WalletWasabi/";
+
+		public string CustomerSupportLink => "https://www.reddit.com/r/WasabiWallet/";
+
+		public string BugReportLink => "https://github.com/zkSNACKs/WalletWasabi/issues/";
+
+		public string FAQLink => "https://github.com/zkSNACKs/WalletWasabi/blob/master/WalletWasabi.Documentation/FAQ.md";
+
+		public void OnClearnetClicked()
+		{
+			OpenLink(ClearnetLink);
+		}
+
+		public void OnSourceCodeClicked()
+		{
+			OpenLink(SourceCodeLink);
+		}
+
+		public void OnCustomerSupportClicked()
+		{
+			OpenLink(CustomerSupportLink);
+		}
+
+		public void OnBugReportClicked()
+		{
+			OpenLink(BugReportLink);
+		}
+
+		public void OnFAQClicked()
+		{
+			OpenLink(FAQLink);
+		}
+
+		public void OpenLink(string url)
+		{
+			try
+			{
+				Process.Start(url);
+			}
+			catch
+			{
+				if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+				{
+					Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+				}
+				else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+				{
+					Process.Start("xdg-open", url);
+				}
+				else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+				{
+					Process.Start("open", url);
+				}
+			}
+		}
 	}
 }
