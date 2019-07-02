403035915bb37b0ef91b017c5f93c2b92d15078a
Jumar Macato <hikari.netto23@gmail.com>
2019-07-02 19:26:22 +0800

    Add necessary UiConfig fields.

---
+++ WalletWasabi.Gui/Models/LockScreenType.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalletWasabi.Gui.Models
+{
+	/// <summary>
+	/// Order matter: piority the lower.
+	/// </summary>
+	public enum LockScreenType
+	{
+		SlideLock,
+		PINLock
+	}
+}
--- WalletWasabi.Gui/UiConfig.cs
+++ WalletWasabi.Gui/UiConfig.cs
@@ -16,174 +16,216 @@ using WalletWasabi.JsonConverters;
 
 namespace WalletWasabi.Gui
 {
-	[JsonObject(MemberSerialization.OptIn)]
-	public class UiConfig : ReactiveObject, IConfig
-	{
-		private bool? _lurkingWifeMode;
+    [JsonObject(MemberSerialization.OptIn)]
+    public class UiConfig : ReactiveObject, IConfig
+    {
+        private bool? _lurkingWifeMode;
+        private bool? _lockScreenActive;
+		private LockScreenType _lockScreenType;
 
-		/// <inheritdoc />
-		public string FilePath { get; private set; }
+        /// <inheritdoc />
+        public string FilePath { get; private set; }
 
-		[JsonProperty(PropertyName = "WindowState")]
-		[JsonConverter(typeof(WindowStateAfterSartJsonConverter))]
-		public WindowState? WindowState { get; internal set; }
+        [JsonProperty(PropertyName = "WindowState")]
+        [JsonConverter(typeof(WindowStateAfterSartJsonConverter))]
+        public WindowState? WindowState { get; internal set; }
 
-		[JsonProperty(PropertyName = "Height")]
-		public double? Height { get; internal set; }
+        [JsonProperty(PropertyName = "Height")]
+        public double? Height { get; internal set; }
 
-		[JsonProperty(PropertyName = "Width")]
-		public double? Width { get; internal set; }
+        [JsonProperty(PropertyName = "Width")]
+        public double? Width { get; internal set; }
 
-		[JsonProperty(PropertyName = "FeeTarget")]
-		public int? FeeTarget { get; internal set; }
+        [JsonProperty(PropertyName = "FeeTarget")]
+        public int? FeeTarget { get; internal set; }
 
-		[JsonProperty(PropertyName = "FeeDisplayFormat")]
-		public int? FeeDisplayFormat { get; internal set; }
+        [JsonProperty(PropertyName = "FeeDisplayFormat")]
+        public int? FeeDisplayFormat { get; internal set; }
 
-		[JsonProperty(PropertyName = "Autocopy")]
-		public bool? Autocopy { get; internal set; }
+        [JsonProperty(PropertyName = "Autocopy")]
+        public bool? Autocopy { get; internal set; }
 
-		[JsonProperty(PropertyName = "LurkingWifeMode")]
-		public bool? LurkingWifeMode
-		{
-			get => _lurkingWifeMode;
-			set => this.RaiseAndSetIfChanged(ref _lurkingWifeMode, value);
-		}
+        [JsonProperty(PropertyName = "LurkingWifeMode")]
+        public bool? LurkingWifeMode
+        {
+            get => _lurkingWifeMode;
+            set => this.RaiseAndSetIfChanged(ref _lurkingWifeMode, value);
+        }
 
-		public UiConfig()
-		{
-		}
+        [JsonProperty(PropertyName = "LockScreenActive")]
+        public bool? LockScreenActive
+        {
+            get => _lockScreenActive;
+            set => this.RaiseAndSetIfChanged(ref _lockScreenActive, value);
+        }
 
-		public UiConfig(string filePath)
-		{
-			SetFilePath(filePath);
-		}
+        [JsonProperty(PropertyName = "LockScreenType")]
+        public LockScreenType LockScreenType
+        {
+            get => _lockScreenType;
+            set => this.RaiseAndSetIfChanged(ref _lockScreenType, value);
+        }
 
-		public UiConfig(WindowState windowState, double height, double width, int feeTarget, int feeDisplayFormat, bool autocopy, bool lurkingWifeMode)
-		{
-			WindowState = Guard.NotNull(nameof(windowState), windowState);
-			Height = Guard.NotNull(nameof(height), height);
-			Width = Guard.NotNull(nameof(width), width);
-			FeeTarget = Guard.NotNull(nameof(feeTarget), feeTarget);
-			FeeDisplayFormat = Guard.NotNull(nameof(feeDisplayFormat), feeDisplayFormat);
-			Autocopy = Guard.NotNull(nameof(autocopy), autocopy);
-			LurkingWifeMode = Guard.NotNull(nameof(lurkingWifeMode), lurkingWifeMode);
-		}
+        [JsonProperty(PropertyName = "LockScreenPinHash")]
+        public string LockScreenPinHash { get; internal set; }
 
-		/// <inheritdoc />
-		public async Task ToFileAsync()
-		{
-			AssertFilePathSet();
+        public UiConfig()
+        {
+        }
 
-			string jsonString = JsonConvert.SerializeObject(this, Formatting.Indented);
-			await File.WriteAllTextAsync(FilePath,
-			jsonString,
-			Encoding.UTF8);
-		}
+        public UiConfig(string filePath)
+        {
+            SetFilePath(filePath);
+        }
 
-		/// <inheritdoc />
-		public async Task LoadOrCreateDefaultFileAsync()
-		{
-			AssertFilePathSet();
+        public UiConfig(WindowState windowState, double height, double width, int feeTarget,
+                        int feeDisplayFormat, bool autocopy, bool lurkingWifeMode,
+                        bool LockScreenActive, string lockScreenPinHash, LockScreenType lockScreenType)
+        {
+            WindowState = Guard.NotNull(nameof(windowState), windowState);
+            Height = Guard.NotNull(nameof(height), height);
+            Width = Guard.NotNull(nameof(width), width);
+            FeeTarget = Guard.NotNull(nameof(feeTarget), feeTarget);
+            FeeDisplayFormat = Guard.NotNull(nameof(feeDisplayFormat), feeDisplayFormat);
+            Autocopy = Guard.NotNull(nameof(autocopy), autocopy);
+            LurkingWifeMode = Guard.NotNull(nameof(lurkingWifeMode), lurkingWifeMode);
+            LockScreenActive = Guard.NotNull(nameof(LockScreenActive), LockScreenActive);
+            LockScreenType = Guard.NotNull(nameof(LockScreenType), lockScreenType);
+            LockScreenPinHash = Guard.NotNull(nameof(LockScreenPinHash), lockScreenPinHash);
+        }
 
-			WindowState = Avalonia.Controls.WindowState.Maximized;
-			Height = 530;
-			Width = 1100;
-			FeeTarget = 2;
-			FeeDisplayFormat = 0;
-			Autocopy = true;
-			LurkingWifeMode = false;
+        /// <inheritdoc />
+        public async Task ToFileAsync()
+        {
+            AssertFilePathSet();
 
-			if (!File.Exists(FilePath))
-			{
-				Logging.Logger.LogInfo<Config>($"{nameof(Config)} file did not exist. Created at path: `{FilePath}`.");
-			}
-			else
-			{
-				await LoadFileAsync();
-			}
+            string jsonString = JsonConvert.SerializeObject(this, Formatting.Indented);
+            await File.WriteAllTextAsync(FilePath,
+            jsonString,
+            Encoding.UTF8);
+        }
 
-			await ToFileAsync();
-		}
+        /// <inheritdoc />
+        public async Task LoadOrCreateDefaultFileAsync()
+        {
+            AssertFilePathSet();
 
-		public async Task LoadFileAsync()
-		{
-			string jsonString = await File.ReadAllTextAsync(FilePath, Encoding.UTF8);
-			var config = JsonConvert.DeserializeObject<UiConfig>(jsonString);
+            WindowState = Avalonia.Controls.WindowState.Maximized;
+            Height = 530;
+            Width = 1100;
+            FeeTarget = 2;
+            FeeDisplayFormat = 0;
+            Autocopy = true;
+            LurkingWifeMode = false;
+			LockScreenActive = false;
+			LockScreenPinHash = "";
+			LockScreenType = LockScreenType.SlideLock;
 
-			WindowState = config.WindowState ?? WindowState;
-			Height = config.Height ?? Height;
-			Width = config.Width ?? Width;
-			FeeTarget = config.FeeTarget ?? FeeTarget;
-			FeeDisplayFormat = config.FeeDisplayFormat ?? FeeDisplayFormat;
-			Autocopy = config.Autocopy ?? Autocopy;
-			LurkingWifeMode = config.LurkingWifeMode ?? LurkingWifeMode;
-		}
+            if (!File.Exists(FilePath))
+            {
+                Logging.Logger.LogInfo<Config>($"{nameof(Config)} file did not exist. Created at path: `{FilePath}`.");
+            }
+            else
+            {
+                await LoadFileAsync();
+            }
 
-		/// <inheritdoc />
-		public async Task<bool> CheckFileChangeAsync()
-		{
-			AssertFilePathSet();
+            await ToFileAsync();
+        }
 
-			if (!File.Exists(FilePath))
-			{
-				throw new FileNotFoundException($"{nameof(Config)} file did not exist at path: `{FilePath}`.");
-			}
+        public async Task LoadFileAsync()
+        {
+            string jsonString = await File.ReadAllTextAsync(FilePath, Encoding.UTF8);
+            var config = JsonConvert.DeserializeObject<UiConfig>(jsonString);
 
-			string jsonString = await File.ReadAllTextAsync(FilePath, Encoding.UTF8);
-			var config = JsonConvert.DeserializeObject<UiConfig>(jsonString);
+            WindowState = config.WindowState ?? WindowState;
+            Height = config.Height ?? Height;
+            Width = config.Width ?? Width;
+            FeeTarget = config.FeeTarget ?? FeeTarget;
+            FeeDisplayFormat = config.FeeDisplayFormat ?? FeeDisplayFormat;
+            Autocopy = config.Autocopy ?? Autocopy;
+            LurkingWifeMode = config.LurkingWifeMode ?? LurkingWifeMode;
+        }
 
-			if (WindowState != config.WindowState)
-			{
-				return true;
-			}
+        /// <inheritdoc />
+        public async Task<bool> CheckFileChangeAsync()
+        {
+            AssertFilePathSet();
 
-			if (Height != config.Height)
-			{
-				return true;
-			}
+            if (!File.Exists(FilePath))
+            {
+                throw new FileNotFoundException($"{nameof(Config)} file did not exist at path: `{FilePath}`.");
+            }
 
-			if (Width != config.Width)
-			{
-				return true;
-			}
+            string jsonString = await File.ReadAllTextAsync(FilePath, Encoding.UTF8);
+            var config = JsonConvert.DeserializeObject<UiConfig>(jsonString);
 
-			if (FeeTarget != config.FeeTarget)
-			{
-				return true;
-			}
+            if (WindowState != config.WindowState)
+            {
+                return true;
+            }
 
-			if (FeeDisplayFormat != config.FeeDisplayFormat)
-			{
-				return true;
-			}
+            if (Height != config.Height)
+            {
+                return true;
+            }
 
-			if (Autocopy != config.Autocopy)
-			{
-				return true;
-			}
+            if (Width != config.Width)
+            {
+                return true;
+            }
 
-			if (LurkingWifeMode != config.LurkingWifeMode)
-			{
-				return true;
-			}
+            if (FeeTarget != config.FeeTarget)
+            {
+                return true;
+            }
 
-			return false;
-		}
+            if (FeeDisplayFormat != config.FeeDisplayFormat)
+            {
+                return true;
+            }
 
-		/// <inheritdoc />
-		public void SetFilePath(string path)
-		{
-			FilePath = Guard.NotNullOrEmptyOrWhitespace(nameof(path), path, trim: true);
-		}
+            if (Autocopy != config.Autocopy)
+            {
+                return true;
+            }
 
-		/// <inheritdoc />
-		public void AssertFilePathSet()
-		{
-			if (FilePath is null)
-			{
-				throw new NotSupportedException($"{nameof(FilePath)} is not set. Use {nameof(SetFilePath)} to set it.");
-			}
-		}
-	}
+            if (LurkingWifeMode != config.LurkingWifeMode)
+            {
+                return true;
+            }
+
+            if (LockScreenActive != config.LockScreenActive)
+            {
+                return true;
+            }
+
+            if (LockScreenType != config.LockScreenType)
+            {
+                return true;
+            }
+
+            if (LockScreenPinHash != config.LockScreenPinHash)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc />
+        public void SetFilePath(string path)
+        {
+            FilePath = Guard.NotNullOrEmptyOrWhitespace(nameof(path), path, trim: true);
+        }
+
+        /// <inheritdoc />
+        public void AssertFilePathSet()
+        {
+            if (FilePath is null)
+            {
+                throw new NotSupportedException($"{nameof(FilePath)} is not set. Use {nameof(SetFilePath)} to set it.");
+            }
+        }
+    }
 }
