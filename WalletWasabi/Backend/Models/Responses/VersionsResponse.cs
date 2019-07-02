6d914d9f176ffc407ee87ae153aa620771aa167e
nopara73 <adam.ficsor73@gmail.com>
2019-07-02 07:08:02 +0200

    Add back typo with JsonProperty and explain in comment why it's necessary.

--- WalletWasabi/Backend/Models/Responses/VersionsResponse.cs
+++ WalletWasabi/Backend/Models/Responses/VersionsResponse.cs
@@ -1,9 +1,13 @@
-﻿namespace WalletWasabi.Backend.Models.Responses
+using Newtonsoft.Json;
+
+namespace WalletWasabi.Backend.Models.Responses
 {
 	public class VersionsResponse
 	{
 		public string ClientVersion { get; set; }
 
+		// KEEP THE TYPO IN IT! Otherwise the response would not be backwards compatible.
+		[JsonProperty(PropertyName = "BackenMajordVersion")]
 		public string BackendMajorVersion { get; set; }
 	}
 }
