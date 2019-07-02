2fc246052086c7fb7c3bbd1a1fc57cd39ea44d67
Jumar Macato <hikari.netto23@gmail.com>
2019-07-02 19:26:51 +0800

    fix LockScreenCommands.

--- WalletWasabi.Gui/Shell/Commands/LockScreenCommands.cs
+++ WalletWasabi.Gui/Shell/Commands/LockScreenCommands.cs
@@ -6,27 +6,31 @@ using System.Composition;
 
 namespace WalletWasabi.Gui.Shell.Commands
 {
-	internal class LockScreenCommands
-	{
-		[ExportCommandDefinition("File.LockScreen")]
-		public CommandDefinition LockScreenCommand { get; }
+    internal class LockScreenCommands
+    {
+        public Global Global { get; }
 
-		[ImportingConstructor]
-		public LockScreenCommands(CommandIconService commandIconService)
-		{
-			var lockScreen = ReactiveCommand.Create(OnLockScreen);
+        [ExportCommandDefinition("File.LockScreen")]
+        public CommandDefinition LockScreenCommand { get; }
 
-			lockScreen.ThrownExceptions.Subscribe(ex => Logging.Logger.LogWarning<LockScreenCommands>(ex));
+        [ImportingConstructor]
+        public LockScreenCommands(CommandIconService commandIconService, AvaloniaGlobalComponent global)
+        {
+			Global = global.Global;
 
-			LockScreenCommand = new CommandDefinition(
-			   "Lock Screen",
-			   commandIconService.GetCompletionKindImage("Lock"),
-			   lockScreen);
-		}
+            var lockScreen = ReactiveCommand.Create(OnLockScreen);
 
-		private void OnLockScreen()
-		{
+            lockScreen.ThrownExceptions.Subscribe(ex => Logging.Logger.LogWarning<LockScreenCommands>(ex));
 
-		}
-	}
+            LockScreenCommand = new CommandDefinition(
+               "Lock Screen",
+               commandIconService.GetCompletionKindImage("Lock"),
+               lockScreen);
+        }
+
+        private void OnLockScreen()
+        {
+			Global.UiConfig.LockScreenActive = true;
+        }
+    }
 }
