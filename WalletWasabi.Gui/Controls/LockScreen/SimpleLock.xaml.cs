be045df8f90dae3fe0e5a5ad231989ba155e2140
Jumar Macato <hikari.netto23@gmail.com>
2019-07-02 22:37:57 +0800

    Made the simple lock work

--- WalletWasabi.Gui/Controls/LockScreen/SimpleLock.xaml.cs
+++ WalletWasabi.Gui/Controls/LockScreen/SimpleLock.xaml.cs
@@ -7,14 +7,44 @@ using Avalonia.Markup.Xaml;
 using Avalonia.Controls.Primitives;
 using ReactiveUI;
 using Avalonia;
+using Avalonia.Interactivity;
 
 namespace WalletWasabi.Gui.Controls.LockScreen
 {
     internal class SimpleLock : LockScreenImpl
-    { 
+    {
+        public Grid Shade { get; }
+
         public SimpleLock()
         {
-            InitializeComponent(); 
+            InitializeComponent();
+            var unlockButton = this.FindControl<Button>("UnlockButton");
+
+            this.Shade = this.FindControl<Grid>("Shade");
+
+            unlockButton.Click += unlockButton_Clicked;
+
+            this.WhenAnyValue(x => x.IsLocked)
+                .Subscribe(IsLockedChanged);
+        }
+
+        private void IsLockedChanged(bool obj)
+        {
+            if (obj)
+            {
+                Shade.Classes.Add("Locked");
+                Shade.Classes.Remove("Unlocked");
+            }
+            else
+            {
+                Shade.Classes.Add("Unlocked");
+                Shade.Classes.Remove("Locked");
+            }
+        }
+
+        private void unlockButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.IsLocked = false;
         }
 
         private void InitializeComponent()
