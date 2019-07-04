839a93a849735e7f13ab6ff5a891a9ce0ffef1d9
lontivero <lucasontivero@gmail.com>
2019-07-04 13:35:11 -0300

    Boolean-String converter impl in SettingsView

---
+++ WalletWasabi.Gui/Converters/BooleanStringConverter.cs
@@ -0,0 +1,48 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace WalletWasabi.Gui.Converters
+{
+	public class BooleanStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is bool on))
+			{
+				throw new TypeArgumentException(value, typeof(bool), nameof(value));
+			}
+			if (!(parameter is string str))
+			{
+				throw new TypeArgumentException(parameter, typeof(string), nameof(parameter));
+			}
+
+			var options = str.Split(':');
+			if (options.Length < 2)
+			{
+				throw new ArgumentException("Two options are required by the converter.", nameof(parameter));
+			}
+			
+			return on ? options[0] : options[1];
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
+
+namespace WalletWasabi.Gui.Converters
+{
+	public static class BoolConverters
+	{
+		public static readonly IValueConverter Not =
+			new FuncValueConverter<bool, bool>(x => !x);
+
+		public static IValueConverter Stringify(string trueString, string falseString)
+			=> new FuncValueConverter<bool, string>(x => x ? trueString : falseString);
+
+		public static readonly IValueConverter OnOff = Stringify("On", "Off");
+	}
+}
