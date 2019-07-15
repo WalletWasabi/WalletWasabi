d4b051df921d816498e553ce1a332bd6066345fe
Jumar Macato <hikari.netto23@gmail.com>
2019-07-15 12:42:30 +0800

    Addendum

--- WalletWasabi.Packager/NSubsys/NSubsysUtil.cs
+++ WalletWasabi.Packager/NSubsys/NSubsysUtil.cs
@@ -22,16 +22,17 @@ namespace NSubsys
 				var subsysOffset = peFile.MainHeaderOffset;
 
 				subsysVal = (SubSystemType)peFile.OptionalHeader.Subsystem;
-				subsysOffset += Marshal.OffsetOf<IMAGE_OPTIONAL_HEADER>("Subsystem").ToInt32();
+				subsysOffset += Marshal.OffsetOf<IMAGE_OPTIONAL_HEADER>(nameof(IMAGE_OPTIONAL_HEADER.Subsystem))
+									   .ToInt32();
 
 				switch (subsysVal)
 				{
 					case PeUtility.SubSystemType.IMAGE_SUBSYSTEM_WINDOWS_GUI:
-						Console.WriteLine("Executable file is already a Win32 App!");
+						Console.WriteLine("NSubsys: Executable file is already a Win32 App!");
 						return true;
 					case PeUtility.SubSystemType.IMAGE_SUBSYSTEM_WINDOWS_CUI:
-						Console.WriteLine("Console app detected...");
-						Console.WriteLine("Converting...");
+						Console.WriteLine("NSubsys: Console app detected...");
+						Console.WriteLine("NSubsys: Converting...");
 
 						var subsysSetting = BitConverter.GetBytes((ushort)SubSystemType.IMAGE_SUBSYSTEM_WINDOWS_GUI);
 
@@ -42,17 +43,17 @@ namespace NSubsys
 						{
 							peFile.Stream.Seek(subsysOffset, SeekOrigin.Begin);
 							peFile.Stream.Write(subsysSetting, 0, subsysSetting.Length);
-							Console.WriteLine("Conversion Complete...");
+							Console.WriteLine("NSubsys: Conversion Complete...");
 						}
 						else
 						{
-							Console.WriteLine("Can't write changes!");
-							Console.WriteLine("Conversion Failed...");
+							Console.WriteLine("NSubsys: Can't write changes!");
+							Console.WriteLine("NSubsys: Conversion Failed...");
 						}
 
 						return true;
 					default:
-						Console.WriteLine($"Unsupported subsystem : {Enum.GetName(typeof(SubSystemType), subsysVal)}.");
+						Console.WriteLine($"NSubsys: Unsupported subsystem number: {subsysVal}.");
 						return false;
 				}
 			}
