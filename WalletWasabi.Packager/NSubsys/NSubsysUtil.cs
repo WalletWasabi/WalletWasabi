b2d7f642112d9dd1568e2c0ed0d61a8de9b4b1e4
Jumar Macato <hikari.netto23@gmail.com>
2019-07-15 12:39:57 +0800

    Update NSubsys

--- WalletWasabi.Packager/NSubsys/NSubsysUtil.cs
+++ WalletWasabi.Packager/NSubsys/NSubsysUtil.cs
@@ -16,57 +16,43 @@ namespace NSubsys
 		{
 			Console.WriteLine("NSubsys: Subsystem Changer for Windows PE files.");
 			Console.WriteLine($"NSubsys: Target EXE {exeFilePath}.");
-
 			using (var peFile = new PeUtility(exeFilePath))
 			{
 				SubSystemType subsysVal;
 				var subsysOffset = peFile.MainHeaderOffset;
-				var headerType = peFile.Is32BitHeader ? typeof(IMAGE_OPTIONAL_HEADER32) : typeof(IMAGE_OPTIONAL_HEADER64);
 
-				if (peFile.Is32BitHeader)
-				{
-					subsysVal = (SubSystemType)peFile.OptionalHeader32.Subsystem;
-				}
-				else
-				{
-					subsysVal = (SubSystemType)peFile.OptionalHeader64.Subsystem;
-				}
-
-				subsysOffset += Marshal.OffsetOf(headerType, "Subsystem").ToInt32();
+				subsysVal = (SubSystemType)peFile.OptionalHeader.Subsystem;
+				subsysOffset += Marshal.OffsetOf<IMAGE_OPTIONAL_HEADER>("Subsystem").ToInt32();
 
 				switch (subsysVal)
 				{
-					case SubSystemType.IMAGE_SUBSYSTEM_WINDOWS_GUI:
-						Console.WriteLine("NSubsys: Executable file is already a Win32 App!");
+					case PeUtility.SubSystemType.IMAGE_SUBSYSTEM_WINDOWS_GUI:
+						Console.WriteLine("Executable file is already a Win32 App!");
 						return true;
-
-					case SubSystemType.IMAGE_SUBSYSTEM_WINDOWS_CUI:
-						Console.WriteLine("NSubsys: Console app detected...");
-						Console.WriteLine("NSubsys: Converting...");
+					case PeUtility.SubSystemType.IMAGE_SUBSYSTEM_WINDOWS_CUI:
+						Console.WriteLine("Console app detected...");
+						Console.WriteLine("Converting...");
 
 						var subsysSetting = BitConverter.GetBytes((ushort)SubSystemType.IMAGE_SUBSYSTEM_WINDOWS_GUI);
 
 						if (!BitConverter.IsLittleEndian)
-						{
 							Array.Reverse(subsysSetting);
-						}
 
 						if (peFile.Stream.CanWrite)
 						{
 							peFile.Stream.Seek(subsysOffset, SeekOrigin.Begin);
 							peFile.Stream.Write(subsysSetting, 0, subsysSetting.Length);
-							Console.WriteLine("NSubsys: Conversion Complete...");
+							Console.WriteLine("Conversion Complete...");
 						}
 						else
 						{
-							Console.WriteLine("NSubsys: Can't write changes!");
-							Console.WriteLine("NSubsys: Conversion Failed...");
+							Console.WriteLine("Can't write changes!");
+							Console.WriteLine("Conversion Failed...");
 						}
 
 						return true;
-
 					default:
-						Console.WriteLine($"NSubsys: Unsupported subsystem : {Enum.GetName(typeof(SubSystemType), subsysVal)}.");
+						Console.WriteLine($"Unsupported subsystem : {Enum.GetName(typeof(SubSystemType), subsysVal)}.");
 						return false;
 				}
 			}
