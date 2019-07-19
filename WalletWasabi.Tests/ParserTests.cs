f5b48dc127c4009521862aa87c5083560a416de6
nopara73 <adam.ficsor73@gmail.com>
2019-07-19 14:59:14 +0200

    Add unit tests for parser

--- WalletWasabi.Tests/ParserTests.cs
+++ WalletWasabi.Tests/ParserTests.cs
@@ -6,48 +6,61 @@ using Xunit;
 
 namespace WalletWasabi.Tests
 {
-	public class ParserTests
-	{
-		[Theory]
-		[InlineData("localhost")]
-		//[InlineData("127.0.0.1")]
-		//[InlineData("192.168.56.1")]
-		//[InlineData("foo.com")]
-		//[InlineData("foo.onion")]
-		public void EndPointParserTests(string host)
-		{
-			var inputs = new[]
-			{
-				host,
-				$"{host} ",
-				$" {host}",
-				$" {host} ",
-				$"{host}:",
-				$"{host}: ",
-				$"{host} :",
-				$"{host} : ",
-				$" {host} : ",
-				$"{host}:5000",
-				$" {host}:5000",
-				$"{host} :5000",
-				$" {host}:5000",
-				$"{host}: 5000",
-				$" {host} : 5000 "
-			};
+    public class ParserTests
+    {
+        [Theory]
+        [InlineData("localhost")]
+        [InlineData("127.0.0.1")]
+        [InlineData("192.168.56.1")]
+        [InlineData("foo.com")]
+        [InlineData("foo.onion")]
+        public void EndPointParserTests(string host)
+        {
+            var inputs = new[]
+            {
+                host,
+                $"{host} ",
+                $" {host}",
+                $" {host} ",
+                $"{host}:",
+                $"{host}: ",
+                $"{host} :",
+                $"{host} : ",
+                $" {host} : ",
+                $"{host}:5000",
+                $" {host}:5000",
+                $"{host} :5000",
+                $" {host}:5000",
+                $"{host}: 5000",
+                $" {host} : 5000 ",
+                $"{host}/",
+                $"{host}/ ",
+                $" {host}/ ",
+                $"{host}/:5000",
+                $"{host}/:5000/",
+                $"{host}/:5000/ ",
+                $"{host}/: 5000/",
+                $"{host}/ :5000/ ",
+                $"{host}/ : 5000/",
+                $"{host}/ : 5000/ ",
+                $"         {host}/              :             5000/           "
+            };
 
-			foreach (var inputString in inputs)
-			{
-				var success = EndPointParser.TryParse(inputString, 5000, out EndPoint ep);
-				Assert.True(success);
-				Assert.Equal(5000, ep.GetPortOrDefault());
-				var actualHost = ep.GetHostOrDefault();
-				var expectedHost = host;
-				if (host == "localhost")
-				{
-					expectedHost = "127.0.0.1";
-				}
-				Assert.Equal(expectedHost, actualHost);
-			}
-		}
-	}
+            foreach (var inputString in inputs)
+            {
+                var success = EndPointParser.TryParse(inputString, 5000, out EndPoint ep);
+                Assert.True(success);
+                var actualPort = ep.GetPortOrDefault();
+                Assert.Equal(5000, actualPort);
+                var actualHost = ep.GetHostOrDefault();
+                var expectedHost = host;
+                if (host == "localhost")
+                {
+                    expectedHost = "127.0.0.1";
+                }
+                Assert.Equal(expectedHost, actualHost);
+                Assert.Equal($"{actualHost}:{actualPort}", ep.ToString(5000));
+            }
+        }
+    }
 }
--- WalletWasabi/Helpers/EndPointParser.cs
+++ WalletWasabi/Helpers/EndPointParser.cs
@@ -1,93 +1,116 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using WalletWasabi.Helpers;
 
 namespace System.Net
 {
-	public static class EndPointParser
-	{
-		public static string ToString(this EndPoint me, int defaultPort)
-		{
-			string host;
-			int port;
-			if (me is DnsEndPoint dnsEndPoint)
-			{
-				host = dnsEndPoint.Host;
-				port = dnsEndPoint.Port;
-			}
-			else if (me is IPEndPoint ipEndPoint)
-			{
-				host = ipEndPoint.Address.ToString();
-				port = ipEndPoint.Port;
-			}
-			else
-			{
-				throw new FormatException($"Invalid endpoint: {me.ToString()}");
-			}
+    public static class EndPointParser
+    {
+        public static string ToString(this EndPoint me, int defaultPort)
+        {
+            string host;
+            int port;
+            if (me is DnsEndPoint dnsEndPoint)
+            {
+                host = dnsEndPoint.Host;
+                port = dnsEndPoint.Port;
+            }
+            else if (me is IPEndPoint ipEndPoint)
+            {
+                host = ipEndPoint.Address.ToString();
+                port = ipEndPoint.Port;
+            }
+            else
+            {
+                throw new FormatException($"Invalid endpoint: {me.ToString()}");
+            }
 
-			if (port == 0)
-			{
-				port = defaultPort;
-			}
+            if (port == 0)
+            {
+                port = defaultPort;
+            }
 
-			var endPointString = $"{host}:{port}";
+            var endPointString = $"{host}:{port}";
 
-			return endPointString;
-		}
+            return endPointString;
+        }
 
-		/// <param name="defaultPort">If set to -1 and it's needed to use, then this function returns false.</param>
-		public static bool TryParse(string endPointString, int defaultPort, out EndPoint endPoint)
-		{
-			endPoint = null;
+        /// <param name="defaultPort">If set to -1 and it's needed to use, then this function returns false.</param>
+        public static bool TryParse(string endPointString, int defaultPort, out EndPoint endPoint)
+        {
+            endPoint = null;
 
-			endPointString = Guard.Correct(endPointString);
-			endPointString = endPointString.TrimEnd('/');
-			endPointString = endPointString.TrimEnd(':');
+            if (string.IsNullOrWhiteSpace(endPointString))
+            {
+                return false;
+            }
 
-			var lastIndex = endPointString.LastIndexOf(':');
+            endPointString = Guard.Correct(endPointString);
+            endPointString = endPointString.TrimEnd(':', '/');
+            var parts = endPointString.Split(':', StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim().TrimEnd('/').TrimEnd()).ToArray();
 
-			string portString = "";
-			if (lastIndex != -1)
-			{
-				portString = endPointString.Substring(endPointString.LastIndexOf(':') + 1);
-			}
+            int port;
+            if (parts.Length == 0)
+            {
+                return false;
+            }
+            else if (parts.Length == 1)
+            {
+                if (defaultPort == -1)
+                {
+                    return false;
+                }
+                else
+                {
+                    port = defaultPort;
+                }
+            }
+            else if (parts.Length == 2)
+            {
+                var portString = parts[1];
+                if (int.TryParse(portString, out int p))
+                {
+                    port = p;
+                }
+                else
+                {
+                    if (defaultPort == -1)
+                    {
+                        return false;
+                    }
 
-			if (portString is null || !int.TryParse(portString, out int port))
-			{
-				if (defaultPort == -1)
-				{
-					return false;
-				}
+                    port = defaultPort;
+                }
+            }
+            else
+            {
+                return false;
+            }
 
-				port = defaultPort;
-			}
+            string host = parts[0];
+            if (host == "localhost")
+            {
+                endPoint = new IPEndPoint(IPAddress.Loopback, port);
+            }
+            else if (IPAddress.TryParse(host, out IPAddress addr))
+            {
+                endPoint = new IPEndPoint(addr, port);
+            }
+            else
+            {
+                try
+                {
+                    endPoint = new DnsEndPoint(host, port);
+                }
+                catch
+                {
+                    return false;
+                }
+            }
 
-			var host = endPointString.TrimEnd(portString, StringComparison.OrdinalIgnoreCase);
-
-			host = host.TrimEnd(':');
-
-			if (host == "localhost")
-			{
-				endPoint = new IPEndPoint(IPAddress.Loopback, port);
-			}
-			else if (IPAddress.TryParse(host, out IPAddress addr))
-			{
-				endPoint = new IPEndPoint(addr, port);
-			}
-			else
-			{
-				try
-				{
-					endPoint = new DnsEndPoint(host, port);
-				}
-				catch
-				{
-					return false;
-				}
-			}
-
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 }
