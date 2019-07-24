dbe9366f50fe8591d9865b8a98cfa49056d986bb
Dan Walmsley <dan@walms.co.uk>
2019-07-24 18:05:58 +0100

    fix spacing on osx

--- WalletWasabi.Gui/ManagedDialogs/ManagedFileDialog.xaml.cs
+++ WalletWasabi.Gui/ManagedDialogs/ManagedFileDialog.xaml.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Runtime.InteropServices;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Native;
+using AvalonStudio.Extensibility.Theme;
 using AvalonStudio.Shell.Controls;
 
 namespace WalletWasabi.Gui.ManagedDialogs
@@ -14,6 +18,15 @@ namespace WalletWasabi.Gui.ManagedDialogs
 #if DEBUG
 			this.AttachDevTools();
 #endif
+			
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				HasSystemDecorations = true;
+
+				// This will need implementing properly once this is supported by avalonia itself.
+				var color = (ColorTheme.CurrentTheme.Background as SolidColorBrush).Color;
+				(PlatformImpl as WindowImpl).SetTitleBarColor(color);
+			}
 		}
 	}
 }
