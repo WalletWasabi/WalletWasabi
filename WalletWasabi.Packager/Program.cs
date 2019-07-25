6d75d1ce4257e8d0b362126e18338cd81390f66c
lontivero <lucasontivero@gmail.com>
2019-07-25 17:12:02 -0300

    Fix permissions on tar.gz file

--- WalletWasabi.Packager/Program.cs
+++ WalletWasabi.Packager/Program.cs
@@ -749,14 +749,24 @@ namespace WalletWasabi.Packager
 					Directory.Move(publishedFolder, newFolderPath);
 					publishedFolder = newFolderPath;
 
+					var commands = new[] {
+						"cd ~",
+						"sudo umount /mnt/c",
+						"sudo mount -t drvfs C: /mnt/c -o metadata",
+						$"cd /mnt/c/Users/lucas/Desktop/WalletWasabi/WalletWasabi.Gui/bin/dist",
+						$"sudo find ./{newFolderName} -type f -not -name 'wassabee' -exec chmod 644 {{}} \\;",
+						$"tar -pczvf {newFolderName}.tar.gz {newFolderName}"
+					};
+					string arguments = string.Join(" && ", commands);
+
 					using (var process = Process.Start(new ProcessStartInfo
 					{
-						FileName = "cmd",
+						FileName = "wsl",
+						Arguments = arguments,
 						RedirectStandardInput = true,
 						WorkingDirectory = BinDistDirectory
 					}))
 					{
-						process.StandardInput.WriteLine($"wsl tar -pczvf {newFolderName}.tar.gz {newFolderName} && exit");
 						process.WaitForExit();
 					}
 
@@ -839,7 +849,7 @@ namespace WalletWasabi.Packager
 					string debDestopFileLinuxPath = Tools.LinuxPathCombine(debUsrAppFolderRelativePath, $"{ExecutableName}.desktop");
 					var wasabiStarterScriptLinuxPath = Tools.LinuxPathCombine(debUsrLocalBinFolderRelativePath, $"{ExecutableName}");
 
-					var commands = new[] {
+					commands = new[] {
 						"cd ~",
 						"sudo umount /mnt/c",
 						"sudo mount -t drvfs C: /mnt/c -o metadata",
@@ -850,7 +860,7 @@ namespace WalletWasabi.Packager
 						$"sudo chmod -R 0644 {debDestopFileLinuxPath}",
 						$"dpkg --build {Tools.LinuxPath(debFolderRelativePath)} $(pwd)"
 					};
-					string arguments = string.Join(" && ", commands);
+					arguments = string.Join(" && ", commands);
 					using (var process = Process.Start(new ProcessStartInfo
 					{
 						FileName = "wsl",
