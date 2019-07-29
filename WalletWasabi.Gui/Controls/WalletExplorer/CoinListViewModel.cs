d538daa54f40e9358e54f116433e15d89b161caf
nopara73 <adam.ficsor73@gmail.com>
2019-07-29 13:09:32 +0200

    Improve left out OnOpen exception.

--- WalletWasabi.Gui/Controls/WalletExplorer/CoinListViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/CoinListViewModel.cs
@@ -377,12 +377,7 @@ namespace WalletWasabi.Gui.Controls.WalletExplorer
 
 		private void OnOpen()
 		{
-			if (Disposables != null)
-			{
-				throw new Exception("CoinList was opened before last one closed.");
-			}
-
-			Disposables = new CompositeDisposable();
+			Disposables = Disposables is null ? new CompositeDisposable() : throw new NotSupportedException($"Cannot open {GetType().Name} before closing it.");
 
 			foreach (var sc in Global.WalletService.Coins.Where(sc => sc.Unspent))
 			{
