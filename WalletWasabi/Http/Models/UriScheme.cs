cd39d501d6a8447abe834895cd796162c9665302
Yahia Chiheb <chihebyahia@gmail.com>
2019-08-06 18:57:12 +0100

    Remove unnecessary pragma warnings, commas, and use PascalCase with enumerators

--- WalletWasabi/Http/Models/UriScheme.cs
+++ WalletWasabi/Http/Models/UriScheme.cs
@@ -3,13 +3,9 @@ namespace WalletWasabi.Http.Models
 	// https://tools.ietf.org/html/rfc7230#section-2.7.3
 	// The scheme and host are case-insensitive and normally provided in lowercase;
 	// all other components are compared in a case-sensitive manner.
-#pragma warning disable IDE1006 // Naming Styles
-
 	public enum UriScheme
 	{
-		http,
-		https
+		Http,
+		Https
 	}
-
-#pragma warning restore IDE1006 // Naming Styles
 }
