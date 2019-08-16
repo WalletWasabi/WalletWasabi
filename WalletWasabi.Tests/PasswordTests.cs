53d33cf5538b73c0f3533fcdc0efc647548d2337
molnard <molnardavid84@gmail.com>
2019-08-16 11:56:40 +0200

    Add tests.

--- WalletWasabi.Tests/PasswordTests.cs
+++ WalletWasabi.Tests/PasswordTests.cs
@@ -12,7 +12,7 @@ namespace WalletWasabi.Tests
 	public class PasswordTests
 	{
 		[Fact]
-		public void PasswordClipboardCutTest()
+		public void ClipboardCutTest()
 		{
 			string original = "    w¾3AÍ-dCdï×¾M\\Øò¹ãÔÕýÈÝÁÐ9oEp¨}r:SR¦·ßNó±¥*W!¢ê#ikÇå<ðtÇf·a\\]§,à±H7«®È4nèNmæo4.qØ-¾ûda¯ºíö¾,¥¢½\\¹õèKeÁìÍSÈ@r±ØÙ2[r©UQÞ¶xN\"?:Ö@°&\n";
 
@@ -34,20 +34,65 @@ namespace WalletWasabi.Tests
 		}
 
 		[Fact]
-		public void GetMasterExtKeyTest()
+		public void FormattingTest()
 		{
 			string buggy = "    w¾3AÍ-dCdï×¾M\\Øò¹ãÔÕýÈÝÁÐ9oEp¨}r:SR¦·ßNó±¥*W!¢ê#ikÇå<ðtÇf·a\\]§,à±H7«®È4nèNmæo4.qØ-¾ûda¯";
 			string original = "    w¾3AÍ-dCdï×¾M\\Øò¹ãÔÕýÈÝÁÐ9oEp¨}r:SR¦·ßNó±¥*W!¢ê#ikÇå<ðtÇf·a\\]§,à±H7«®È4nèNmæo4.qØ-¾ûda¯ºíö¾,¥¢½\\¹õèKeÁìÍSÈ@r±ØÙ2[r©UQÞ¶xN\"?:Ö@°&\n";
 
-			var keyManager = KeyManager.CreateNew(out _, buggy); // Creating a wallet with buggy password.
+			// Creating a wallet with buggy password.
+			var keyManager = KeyManager.CreateNew(out _, buggy);
 
-			Assert.Throws<FormatException>(() => PasswordHelper.GetMasterExtKey(keyManager, original, out _)); // Password should be formatted, before entering here.
+			// Password should be formatted, before entering here.
+			Assert.Throws<FormatException>(() => PasswordHelper.GetMasterExtKey(keyManager, original, out _));
 
-			// This should not throw format exception.
+			// This should not throw format exception but pw is not correct.
 			Assert.Throws<SecurityException>(() => PasswordHelper.GetMasterExtKey(keyManager, RandomString.Generate(PasswordHelper.MaxPasswordLength), out _));
 
-			Assert.Throws<FormatException>(() => PasswordHelper.GetMasterExtKey(keyManager,RandomString.Generate(PasswordHelper.MaxPasswordLength +1 ),out _)); // Password should be formatted, before entering here.
+			// Password should be formatted, before entering here.
+			Assert.Throws<FormatException>(() => PasswordHelper.GetMasterExtKey(keyManager, RandomString.Generate(PasswordHelper.MaxPasswordLength + 1), out _));
 
+			// Too long password with extra spaces.
+			var badPassword = $"   {RandomString.Generate(PasswordHelper.MaxPasswordLength + 1)}   ";
+
+			// Password should be formatted, before entering here.
+			Assert.Throws<FormatException>(() => PasswordHelper.GetMasterExtKey(keyManager, badPassword, out _));
+
+			Assert.True(PasswordHelper.IsTrimable(badPassword, out badPassword));
+
+			// Still too long.
+			Assert.Throws<FormatException>(() => PasswordHelper.GetMasterExtKey(keyManager, badPassword, out _));
+
+			Assert.True(PasswordHelper.IsTooLong(badPassword, out badPassword));
+
+			// This should not throw format exception but pw is not correct.
+			Assert.Throws<SecurityException>(() => PasswordHelper.GetMasterExtKey(keyManager, badPassword, out _));
+		}
+
+		[Fact]
+		public void CompatibilityTest()
+		{
+			string buggy = "    w¾3AÍ-dCdï×¾M\\Øò¹ãÔÕýÈÝÁÐ9oEp¨}r:SR¦·ßNó±¥*W!¢ê#ikÇå<ðtÇf·a\\]§,à±H7«®È4nèNmæo4.qØ-¾ûda¯";
+			string original = "    w¾3AÍ-dCdï×¾M\\Øò¹ãÔÕýÈÝÁÐ9oEp¨}r:SR¦·ßNó±¥*W!¢ê#ikÇå<ðtÇf·a\\]§,à±H7«®È4nèNmæo4.qØ-¾ûda¯ºíö¾,¥¢½\\¹õèKeÁìÍSÈ@r±ØÙ2[r©UQÞ¶xN\"?:Ö@°&\n";
+
+			Assert.Throws<FormatException>(() => PasswordHelper.Guard(buggy));
+
+			Assert.True(PasswordHelper.IsTrimable(buggy, out buggy));
+
+			// Creating a wallet with buggy password.
+			var keyManager = KeyManager.CreateNew(out _, buggy);
+
+			Assert.True(PasswordHelper.IsTrimable(original, out original));
+
+			Assert.False(PasswordHelper.TryPassword(keyManager, "falsepassword", out _));
+
+			// This should pass
+			Assert.NotNull(PasswordHelper.GetMasterExtKey(keyManager, original, out _));
+
+			Assert.True(PasswordHelper.TryPassword(keyManager, buggy, out string compatiblePasswordNotUsed));
+			Assert.Null(compatiblePasswordNotUsed);
+
+			Assert.True(PasswordHelper.TryPassword(keyManager, original, out string compatiblePassword));
+			Assert.Equal(buggy, compatiblePassword);
 		}
 	}
 }
