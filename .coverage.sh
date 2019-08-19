2127cffd236ee8865d1df1008f3f03644ff60ddd
nopara73 <adam.ficsor73@gmail.com>
2019-08-19 22:59:27 +0200

    pullpanda test

--- .coverage.sh
+++ .coverage.sh
@@ -2,7 +2,7 @@ COLLECT_COVERAGE="//p:CollectCoverage=true"
 OUTPUT_FORMAT="//p:CoverletOutputFormat=lcov"
 FILE_NAME="//p:CoverletOutputName=lcov"
 
-dotnet restore && dotnet build
+foo bar buz
 
 cd WalletWasabi.Tests
 dotnet test --no-build $COLLECT_COVERAGE $OUTPUT_FORMAT $FILE_NAME
