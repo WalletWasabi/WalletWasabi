62cec5bfda0098a43cb1072b5abb17b701686fd8
Yahia Chiheb <chihebyahia@gmail.com>
2019-08-26 04:32:58 +0100

    Change X to O

--- WalletWasabi/Gma/QrCodeNet/Encoding/Positioning/Stencils/PositionDetectionPattern.cs
+++ WalletWasabi/Gma/QrCodeNet/Encoding/Positioning/Stencils/PositionDetectionPattern.cs
@@ -13,7 +13,7 @@ namespace Gma.QrCodeNet.Encoding.Positioning.Stencils
 																			{ O, X, X, X, X, X, X, X, O },
 																			{ O, X, O, O, O, O, O, X, O },
 																			{ O, X, O, X, X, X, O, X, O },
-																			{ O, X, O, X, X, X, O, X, O },
+																			{ O, X, O, X, O, X, O, X, O },
 																			{ O, X, O, X, X, X, O, X, O },
 																			{ O, X, O, O, O, O, O, X, O },
 																			{ O, X, X, X, X, X, X, X, O },
