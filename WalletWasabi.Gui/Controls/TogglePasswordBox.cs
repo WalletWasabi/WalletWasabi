0ffd3a4b6803179390bfcd300ec77c8a1ef8c9ac
MDavid <molnardavid84@gmail.com>
2019-08-29 10:57:29 +0200

    Codefactor

--- WalletWasabi.Gui/Controls/TogglePasswordBox.cs
+++ WalletWasabi.Gui/Controls/TogglePasswordBox.cs
@@ -8,56 +8,55 @@ using ReactiveUI;
 using System.Reactive.Linq;
 
 namespace WalletWasabi.Gui.Controls
-
 {
-    public class TogglePasswordBox : ExtendedTextBox, IStyleable
-    {
-        Type IStyleable.StyleKey => typeof(TogglePasswordBox);
+	public class TogglePasswordBox : ExtendedTextBox, IStyleable
+	{
+		Type IStyleable.StyleKey => typeof(TogglePasswordBox);
 
-        public static readonly StyledProperty<bool> IsPasswordVisibleProperty =
-            AvaloniaProperty.Register<TogglePasswordBox, bool>(nameof(IsPasswordVisible), defaultBindingMode: BindingMode.TwoWay);
+		public static readonly StyledProperty<bool> IsPasswordVisibleProperty =
+			AvaloniaProperty.Register<TogglePasswordBox, bool>(nameof(IsPasswordVisible), defaultBindingMode: BindingMode.TwoWay);
 
-        public bool IsPasswordVisible
-        {
-            get => GetValue(IsPasswordVisibleProperty);
-            set => SetValue(IsPasswordVisibleProperty, value);
-        }
+		public bool IsPasswordVisible
+		{
+			get => GetValue(IsPasswordVisibleProperty);
+			set => SetValue(IsPasswordVisibleProperty, value);
+		}
 
-        public TogglePasswordBox()
-        {
-            UseFloatingWatermark = true;
-            Watermark = "Password";
+		public TogglePasswordBox()
+		{
+			UseFloatingWatermark = true;
+			Watermark = "Password";
 
-            this.GetObservable(IsPasswordVisibleProperty)
-                .ObserveOn(RxApp.MainThreadScheduler)
-                .Subscribe(x =>
-            {
-                IsPasswordVisible = x;
-            });
+			this.GetObservable(IsPasswordVisibleProperty)
+				.ObserveOn(RxApp.MainThreadScheduler)
+				.Subscribe(x =>
+			{
+				IsPasswordVisible = x;
+			});
 
-            this.WhenAnyValue(x => x.IsPasswordVisible)
-                .ObserveOn(RxApp.MainThreadScheduler)
-                .Subscribe(x =>
-            {
-                PasswordChar = x ? '\0' : '*';
-            });
-        }
+			this.WhenAnyValue(x => x.IsPasswordVisible)
+				.ObserveOn(RxApp.MainThreadScheduler)
+				.Subscribe(x =>
+			{
+				PasswordChar = x ? '\0' : '*';
+			});
+		}
 
-        protected override bool IsCopyEnabled => false;
+		protected override bool IsCopyEnabled => false;
 
-        protected override void OnTemplateApplied(TemplateAppliedEventArgs e)
-        {
-            base.OnTemplateApplied(e);
+		protected override void OnTemplateApplied(TemplateAppliedEventArgs e)
+		{
+			base.OnTemplateApplied(e);
 
-            var maskedButton = e.NameScope.Get<Button>("PART_MaskedButton");
-            maskedButton.WhenAnyValue(x => x.IsPressed)
-                .Where(x => x)
-                .ObserveOn(RxApp.MainThreadScheduler)
+			var maskedButton = e.NameScope.Get<Button>("PART_MaskedButton");
+			maskedButton.WhenAnyValue(x => x.IsPressed)
+				.Where(x => x)
+				.ObserveOn(RxApp.MainThreadScheduler)
 
-                .Subscribe(_ =>
-                {
-                    IsPasswordVisible = !IsPasswordVisible;
-                });
-        }
-    }
+				.Subscribe(_ =>
+				{
+					IsPasswordVisible = !IsPasswordVisible;
+				});
+		}
+	}
 }
