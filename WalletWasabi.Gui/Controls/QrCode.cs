7f09abde1c1d2e696f02bd9eb1ead2a235cc3207
Jumar Macato <hikari.netto23@gmail.com>
2019-09-20 17:03:28 +0800

    Initial code for Saving QR codes.

--- WalletWasabi.Gui/Controls/QrCode.cs
+++ WalletWasabi.Gui/Controls/QrCode.cs
@@ -1,16 +1,82 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
+using Avalonia.Media.Imaging;
 using Avalonia.Metadata;
+using AvalonStudio.Extensibility.Theme;
+using ReactiveUI;
 using System;
+using System.IO;
+using System.Reactive;
+using System.Threading.Tasks;
 
 namespace WalletWasabi.Gui.Controls
 {
 	public class QrCode : Control
 	{
+		private static readonly Geometry SaveIcon = Geometry.Parse(
+				"M13,9V3.5L18.5,9M6,2C4.89,2 4,2.89 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2H6Z");
+
+		private ReactiveCommand<Unit, Unit> SaveCommand { get; }
+
+		private static DrawingPresenter GetSavePresenter()
+		{
+			return new DrawingPresenter
+			{
+				Drawing = new GeometryDrawing
+				{
+					Brush = Brush.Parse("#22B14C"),
+					Geometry = SaveIcon
+				},
+				Width = 16,
+				Height = 16
+			};
+		}
+
 		static QrCode()
 		{
 			AffectsMeasure<QrCode>(MatrixProperty);
+		}
+
+		public QrCode()
+		{
+			SaveCommand = ReactiveCommand.CreateFromTask(async () =>
+			{
+				await SaveAsync();
+			});
+
+			SaveCommand.ThrownExceptions.Subscribe(ex => Logging.Logger.LogWarning(ex));
+		}
+
+		private async Task SaveAsync()
+		{
+			var source = Matrix;
+			if (source is null) return;
+
+			var curBounds = new Size(Bounds.Width, Bounds.Height);
+			var pixelBounds = PixelSize.FromSize(curBounds, 1);
+
+			var sfd = new SaveFileDialog();
+			var fileFullName = await sfd.ShowAsync(Application.Current.MainWindow, fallBack: true);
+
+			if (!string.IsNullOrWhiteSpace(fileFullName))
+			{
+				var ext = Path.GetExtension(fileFullName);
+
+				if (string.IsNullOrWhiteSpace(ext))
+				{
+					fileFullName = $"{fileFullName}.png";
+				}
+
+				if (!File.Exists(fileFullName))
+					File.Create(fileFullName).Close();
+
+				using (var rtb = new RenderTargetBitmap(new PixelSize((int)this.Width, (int)this.Height)))
+				{
+					rtb.Render(this);
+					rtb.Save(fileFullName);
+				}
+			}
 		}
 
 		public static readonly DirectProperty<QrCode, bool[,]> MatrixProperty =
@@ -28,30 +94,49 @@ namespace WalletWasabi.Gui.Controls
 			set => SetAndRaise(MatrixProperty, ref _matrix, value);
 		}
 
+		protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+		{
+			ContextMenu = new ContextMenu
+			{
+				DataContext = this,
+				Items = new Avalonia.Controls.Controls()
+			};
+
+			var menuItems = (ContextMenu.Items as Avalonia.Controls.Controls);
+
+			menuItems.Add(new MenuItem
+			{
+				Header = "Save",
+				Foreground = ColorTheme.CurrentTheme.Foreground,
+				Command = SaveCommand,
+				Icon = GetSavePresenter()
+			});
+		}
+
 		public override void Render(DrawingContext context)
 		{
 			var source = Matrix;
 
-			if (source != null)
+			if (source is null) return;
+
+			var h = source.GetUpperBound(0) + 1;
+			var w = source.GetUpperBound(1) + 1;
+
+			var minDimension = Math.Min(w, h);
+			var minBound = Math.Min(Bounds.Width, Bounds.Height);
+			var factor = (float)minBound / minDimension;
+
+			for (var i = 0; i < h; i++)
 			{
-				var h = source.GetUpperBound(0) + 1;
-				var w = source.GetUpperBound(1) + 1;
-
-				var minDimension = Math.Min(w, h);
-				var minBound = Math.Min(Bounds.Width, Bounds.Height);
-				var factor = (float)minBound / minDimension;
-
-				for (var i = 0; i < h; i++)
+				for (var j = 0; j < w; j++)
 				{
-					for (var j = 0; j < w; j++)
-					{
-						var cellValue = source[i, j];
-						var rect = new Rect(i * factor, j * factor, factor, factor);
-						var color = cellValue ? Brushes.Black : Brushes.White;
-						context.FillRectangle(color, rect);
-					}
+					var cellValue = source[i, j];
+					var rect = new Rect(i * factor, j * factor, factor, factor);
+					var color = cellValue ? Brushes.Black : Brushes.White;
+					context.FillRectangle(color, rect);
 				}
 			}
+
 		}
 
 		protected override Size MeasureOverride(Size availableSize)
