3f3f7e2567ed7ce497b17273cb97db7710c0cea8
nopara73 <adam.ficsor73@gmail.com>
2019-10-20 22:38:23 +0800

    Fix incorrect message. Use correct punctuation.

--- WalletWasabi/Models/ChaumianCoinJoin/CcjRound.cs
+++ WalletWasabi/Models/ChaumianCoinJoin/CcjRound.cs
@@ -725,7 +725,7 @@ namespace WalletWasabi.Models.ChaumianCoinJoin
 				FeeRate currentFeeRate = null;
 				if (fee is null)
 				{
-					Logger.LogError($"Cannot calculate CoinJoin transaction fee. (some spent coins are missing or {nameof(spentCoins)} is null)");
+					Logger.LogError($"Cannot calculate CoinJoin transaction fee. Some spent coins are missing.");
 				}
 				else if (fee <= Money.Zero)
 				{
