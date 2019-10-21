102352e0a52cef5d606dd88fe158745d0527281a
molnard <molnardavid84@gmail.com>
2019-10-21 11:40:55 +0200

    Prevent NRE

--- WalletWasabi.Gui/Controls/WalletExplorer/CoinViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/CoinViewModel.cs
@@ -24,89 +24,97 @@ namespace WalletWasabi.Gui.Controls.WalletExplorer
 		private ObservableAsPropertyHelper<bool> _confirmed;
 		private ObservableAsPropertyHelper<bool> _unavailable;
 		private CoinListViewModel _owner;
+		private object DisposablesLock { get; }
 		public Global Global => _owner.Global;
 
 		public CoinViewModel(CoinListViewModel owner, SmartCoin model)
 		{
 			Model = model;
 			_owner = owner;
+			DisposablesLock = new object();
 		}
 
 		public void SubscribeEvents()
 		{
-			if (Disposables != null)
+			lock (DisposablesLock)
 			{
-				throw new Exception("Please report to Dan");
-			}
-
-			Disposables = new CompositeDisposable();
-
-			//TODO defer subscription to when accessed (will be faster in ui.)
-			_coinJoinInProgress = Model.WhenAnyValue(x => x.CoinJoinInProgress)
-				.ToProperty(this, x => x.CoinJoinInProgress, scheduler: RxApp.MainThreadScheduler)
-				.DisposeWith(Disposables);
-
-			_unspent = Model.WhenAnyValue(x => x.Unspent)
-				.ToProperty(this, x => x.Unspent, scheduler: RxApp.MainThreadScheduler)
-				.DisposeWith(Disposables);
-
-			_confirmed = Model.WhenAnyValue(x => x.Confirmed)
-				.ToProperty(this, x => x.Confirmed, scheduler: RxApp.MainThreadScheduler)
-				.DisposeWith(Disposables);
-
-			_unavailable = Model.WhenAnyValue(x => x.Unavailable)
-				.ToProperty(this, x => x.Unavailable, scheduler: RxApp.MainThreadScheduler)
-				.DisposeWith(Disposables);
-
-			this.WhenAnyValue(x => x.Status)
-				.Subscribe(_ => this.RaisePropertyChanged(nameof(ToolTip)));
-
-			this.WhenAnyValue(x => x.Confirmed, x => x.CoinJoinInProgress, x => x.Confirmations)
-				.ObserveOn(RxApp.MainThreadScheduler)
-				.Subscribe(_ => RefreshSmartCoinStatus());
-
-			this.WhenAnyValue(x => x.IsSelected)
-				.ObserveOn(RxApp.MainThreadScheduler)
-				.Subscribe(_ => _owner.OnCoinIsSelectedChanged(this));
-
-			this.WhenAnyValue(x => x.Status)
-				.ObserveOn(RxApp.MainThreadScheduler)
-				.Subscribe(_ => _owner.OnCoinStatusChanged());
-
-			this.WhenAnyValue(x => x.Unspent)
-				.ObserveOn(RxApp.MainThreadScheduler)
-				.Subscribe(_ => _owner.OnCoinUnspentChanged(this));
-
-			Model.WhenAnyValue(x => x.IsBanned, x => x.SpentAccordingToBackend)
-				.ObserveOn(RxApp.MainThreadScheduler)
-				.Subscribe(_ => RefreshSmartCoinStatus())
-				.DisposeWith(Disposables);
-
-			Observable.FromEventPattern(
-				Global.ChaumianClient,
-				nameof(Global.ChaumianClient.StateUpdated))
-				.ObserveOn(RxApp.MainThreadScheduler)
-				.Subscribe(_ => RefreshSmartCoinStatus())
-				.DisposeWith(Disposables);
-
-			Global.BitcoinStore.HashChain.WhenAnyValue(x => x.ServerTipHeight)
-				.ObserveOn(RxApp.MainThreadScheduler)
-				.Subscribe(_ => this.RaisePropertyChanged(nameof(Confirmations)))
-				.DisposeWith(Disposables);
-
-			Global.UiConfig.WhenAnyValue(x => x.LurkingWifeMode)
-				.ObserveOn(RxApp.MainThreadScheduler)
-				.Subscribe(_ =>
+				if (Disposables != null)
 				{
-					this.RaisePropertyChanged(nameof(AmountBtc));
-					this.RaisePropertyChanged(nameof(Clusters));
-				}).DisposeWith(Disposables);
+					throw new Exception("Please report to Dan");
+				}
+
+				Disposables = new CompositeDisposable();
+
+				//TODO defer subscription to when accessed (will be faster in ui.)
+				_coinJoinInProgress = Model.WhenAnyValue(x => x.CoinJoinInProgress)
+					.ToProperty(this, x => x.CoinJoinInProgress, scheduler: RxApp.MainThreadScheduler)
+					.DisposeWith(Disposables);
+
+				_unspent = Model.WhenAnyValue(x => x.Unspent)
+					.ToProperty(this, x => x.Unspent, scheduler: RxApp.MainThreadScheduler)
+					.DisposeWith(Disposables);
+
+				_confirmed = Model.WhenAnyValue(x => x.Confirmed)
+					.ToProperty(this, x => x.Confirmed, scheduler: RxApp.MainThreadScheduler)
+					.DisposeWith(Disposables);
+
+				_unavailable = Model.WhenAnyValue(x => x.Unavailable)
+					.ToProperty(this, x => x.Unavailable, scheduler: RxApp.MainThreadScheduler)
+					.DisposeWith(Disposables);
+
+				this.WhenAnyValue(x => x.Status)
+					.Subscribe(_ => this.RaisePropertyChanged(nameof(ToolTip)));
+
+				this.WhenAnyValue(x => x.Confirmed, x => x.CoinJoinInProgress, x => x.Confirmations)
+					.ObserveOn(RxApp.MainThreadScheduler)
+					.Subscribe(_ => RefreshSmartCoinStatus());
+
+				this.WhenAnyValue(x => x.IsSelected)
+					.ObserveOn(RxApp.MainThreadScheduler)
+					.Subscribe(_ => _owner.OnCoinIsSelectedChanged(this));
+
+				this.WhenAnyValue(x => x.Status)
+					.ObserveOn(RxApp.MainThreadScheduler)
+					.Subscribe(_ => _owner.OnCoinStatusChanged());
+
+				this.WhenAnyValue(x => x.Unspent)
+					.ObserveOn(RxApp.MainThreadScheduler)
+					.Subscribe(_ => _owner.OnCoinUnspentChanged(this));
+
+				Model.WhenAnyValue(x => x.IsBanned, x => x.SpentAccordingToBackend)
+					.ObserveOn(RxApp.MainThreadScheduler)
+					.Subscribe(_ => RefreshSmartCoinStatus())
+					.DisposeWith(Disposables);
+
+				Observable.FromEventPattern(
+					Global.ChaumianClient,
+					nameof(Global.ChaumianClient.StateUpdated))
+					.ObserveOn(RxApp.MainThreadScheduler)
+					.Subscribe(_ => RefreshSmartCoinStatus())
+					.DisposeWith(Disposables);
+
+				Global.BitcoinStore.HashChain.WhenAnyValue(x => x.ServerTipHeight)
+					.ObserveOn(RxApp.MainThreadScheduler)
+					.Subscribe(_ => this.RaisePropertyChanged(nameof(Confirmations)))
+					.DisposeWith(Disposables);
+
+				Global.UiConfig.WhenAnyValue(x => x.LurkingWifeMode)
+					.ObserveOn(RxApp.MainThreadScheduler)
+					.Subscribe(_ =>
+					{
+						this.RaisePropertyChanged(nameof(AmountBtc));
+						this.RaisePropertyChanged(nameof(Clusters));
+					}).DisposeWith(Disposables);
+			}
 		}
 
 		public void UnsubscribeEvents()
 		{
-			Disposables.Dispose();
-			Disposables = null;
+			lock (DisposablesLock)
+			{
+				Disposables?.Dispose();
+				Disposables = null;
+			}
 		}
 
 		public SmartCoin Model { get; }
