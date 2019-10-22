e7d82a0f8c3ae9d0e7ffeb92463560313e1a605b
nopara73 <adam.ficsor73@gmail.com>
2019-10-23 05:45:54 +0800

    Bring AllFeeEstimateAsync from RegTests to BitcoinCore unit tests.

--- WalletWasabi.Tests/IntegrationTests/RegTests.cs
+++ WalletWasabi.Tests/IntegrationTests/RegTests.cs
@@ -225,28 +225,6 @@ namespace WalletWasabi.Tests.IntegrationTests
 			}
 		}
 
-		[Fact]
-		public async Task AllFeeEstimateRpcAsync()
-		{
-			(string password, RPCClient rpc, Network network, Coordinator coordinator, ServiceConfiguration serviceConfiguration, BitcoinStore bitcoinStore, Backend.Global global) = await InitializeTestEnvironmentAsync(1);
-
-			var estimations = await rpc.EstimateAllFeeAsync(EstimateSmartFeeMode.Conservative, simulateIfRegTest: true, tolerateBitcoinCoreBrainfuck: true);
-			Assert.Equal(Constants.OneDayConfirmationTarget, estimations.Estimations.Count);
-			Assert.True(estimations.Estimations.First().Key < estimations.Estimations.Last().Key);
-			Assert.True(estimations.Estimations.First().Value > estimations.Estimations.Last().Value);
-			Assert.Equal(EstimateSmartFeeMode.Conservative, estimations.Type);
-			estimations = await rpc.EstimateAllFeeAsync(EstimateSmartFeeMode.Economical, simulateIfRegTest: true, tolerateBitcoinCoreBrainfuck: true);
-			Assert.Equal(145, estimations.Estimations.Count);
-			Assert.True(estimations.Estimations.First().Key < estimations.Estimations.Last().Key);
-			Assert.True(estimations.Estimations.First().Value > estimations.Estimations.Last().Value);
-			Assert.Equal(EstimateSmartFeeMode.Economical, estimations.Type);
-			estimations = await rpc.EstimateAllFeeAsync(EstimateSmartFeeMode.Economical, simulateIfRegTest: true, tolerateBitcoinCoreBrainfuck: false);
-			Assert.Equal(145, estimations.Estimations.Count);
-			Assert.True(estimations.Estimations.First().Key < estimations.Estimations.Last().Key);
-			Assert.True(estimations.Estimations.First().Value > estimations.Estimations.Last().Value);
-			Assert.Equal(EstimateSmartFeeMode.Economical, estimations.Type);
-		}
-
 		#endregion BackendTests
 
 		#region ServicesTests
