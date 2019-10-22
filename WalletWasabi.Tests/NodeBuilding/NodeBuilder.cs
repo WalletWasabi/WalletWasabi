fcfbb675343c116b2699768597e09b29171bf332
nopara73 <adam.ficsor73@gmail.com>
2019-10-23 03:33:28 +0800

    Remove redundant nesting.

--- WalletWasabi.Tests/NodeBuilding/NodeBuilder.cs
+++ WalletWasabi.Tests/NodeBuilding/NodeBuilder.cs
@@ -21,59 +21,49 @@ namespace WalletWasabi.Tests.NodeBuilding
 {
 	public static class NodeBuilder
 	{
-		private static int Last { get; set; }
-
 		public static async Task<CoreNode> CreateNodeAsync([CallerMemberName]string caller = null)
 		{
-			var root = Path.Combine(Global.Instance.DataDir, caller);
-			var child = Path.Combine(root, Last.ToString());
-			Last++;
-			try
-			{
-				var cfgPath = Path.Combine(child, "data", "bitcoin.conf");
-				if (File.Exists(cfgPath))
-				{
-					var config = await NodeConfigParameters.LoadAsync(cfgPath);
-					var rpcPort = config["regtest.rpcport"];
-					var rpcUser = config["regtest.rpcuser"];
-					var rpcPassword = config["regtest.rpcpassword"];
-					var pidFileName = config["regtest.pid"];
-					var credentials = new NetworkCredential(rpcUser, rpcPassword);
-					try
-					{
-						var rpc = new RPCClient(credentials, new Uri("http://127.0.0.1:" + rpcPort + "/"), Network.RegTest);
-						await rpc.StopAsync();
+			var dataDir = Path.Combine(Global.Instance.DataDir, caller);
 
-						var pidFile = Path.Combine(child, "data", "regtest", pidFileName);
-						if (File.Exists(pidFile))
-						{
-							var pid = await File.ReadAllTextAsync(pidFile);
-							using var process = Process.GetProcessById(int.Parse(pid));
-							await process.WaitForExitAsync(CancellationToken.None);
-						}
-						else
-						{
-							var allProcesses = Process.GetProcesses();
-							var bitcoindProcesses = allProcesses.Where(x => x.ProcessName.Contains("bitcoind"));
-							if (bitcoindProcesses.Count() == 1)
-							{
-								var bitcoind = bitcoindProcesses.First();
-								await bitcoind.WaitForExitAsync(CancellationToken.None);
-							}
-						}
-					}
-					catch (Exception)
+			var cfgPath = Path.Combine(dataDir, "bitcoin.conf");
+			if (File.Exists(cfgPath))
+			{
+				var config = await NodeConfigParameters.LoadAsync(cfgPath);
+				var rpcPort = config["regtest.rpcport"];
+				var rpcUser = config["regtest.rpcuser"];
+				var rpcPassword = config["regtest.rpcpassword"];
+				var pidFileName = config["regtest.pid"];
+				var credentials = new NetworkCredential(rpcUser, rpcPassword);
+				try
+				{
+					var rpc = new RPCClient(credentials, new Uri("http://127.0.0.1:" + rpcPort + "/"), Network.RegTest);
+					await rpc.StopAsync();
+
+					var pidFile = Path.Combine(dataDir, "regtest", pidFileName);
+					if (File.Exists(pidFile))
 					{
+						var pid = await File.ReadAllTextAsync(pidFile);
+						using var process = Process.GetProcessById(int.Parse(pid));
+						await process.WaitForExitAsync(CancellationToken.None);
+					}
+					else
+					{
+						var allProcesses = Process.GetProcesses();
+						var bitcoindProcesses = allProcesses.Where(x => x.ProcessName.Contains("bitcoind"));
+						if (bitcoindProcesses.Count() == 1)
+						{
+							var bitcoind = bitcoindProcesses.First();
+							await bitcoind.WaitForExitAsync(CancellationToken.None);
+						}
 					}
 				}
-				await IoHelpers.DeleteRecursivelyWithMagicDustAsync(child);
-				await IoHelpers.DeleteRecursivelyWithMagicDustAsync(root);
-				IoHelpers.EnsureDirectoryExists(root);
+				catch (Exception)
+				{
+				}
 			}
-			catch (DirectoryNotFoundException)
-			{
-			}
-			var node = new CoreNode(child);
+
+			await IoHelpers.DeleteRecursivelyWithMagicDustAsync(dataDir);
+			var node = new CoreNode(dataDir);
 			await node.StartAsync();
 			return node;
 		}
