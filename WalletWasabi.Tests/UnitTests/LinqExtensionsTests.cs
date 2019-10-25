30d43ccc6b9c1519628e87889db7ed0d07a6b2e7
nopara73 <adam.ficsor73@gmail.com>
2019-10-25 09:45:12 +0800

    fix naming rule violation

--- WalletWasabi.Tests/UnitTests/LinqExtensionsTests.cs
+++ WalletWasabi.Tests/UnitTests/LinqExtensionsTests.cs
@@ -9,63 +9,63 @@ namespace WalletWasabi.Tests.UnitTests
 		public void CombinationsWithoutRepetitionOfFixedLength()
 		{
 			var combinations = Enumerable.Range(0, 5).CombinationsWithoutRepetition(ofLength: 3);
-			var AsString = combinations.Select(x => string.Join(", ", x)).ToArray();
+			var asString = combinations.Select(x => string.Join(", ", x)).ToArray();
 
-			Assert.Equal(10, AsString.Length);
+			Assert.Equal(10, asString.Length);
 			Assert.Equal(combinations.Count(), combinations.Distinct().Count());
 
-			Assert.Equal("0, 1, 2", AsString[0]);
-			Assert.Equal("0, 1, 3", AsString[1]);
-			Assert.Equal("0, 1, 4", AsString[2]);
-			Assert.Equal("0, 2, 3", AsString[3]);
-			Assert.Equal("0, 2, 4", AsString[4]);
-			Assert.Equal("0, 3, 4", AsString[5]);
-			Assert.Equal("1, 2, 3", AsString[6]);
-			Assert.Equal("1, 2, 4", AsString[7]);
-			Assert.Equal("1, 3, 4", AsString[8]);
-			Assert.Equal("2, 3, 4", AsString[9]);
+			Assert.Equal("0, 1, 2", asString[0]);
+			Assert.Equal("0, 1, 3", asString[1]);
+			Assert.Equal("0, 1, 4", asString[2]);
+			Assert.Equal("0, 2, 3", asString[3]);
+			Assert.Equal("0, 2, 4", asString[4]);
+			Assert.Equal("0, 3, 4", asString[5]);
+			Assert.Equal("1, 2, 3", asString[6]);
+			Assert.Equal("1, 2, 4", asString[7]);
+			Assert.Equal("1, 3, 4", asString[8]);
+			Assert.Equal("2, 3, 4", asString[9]);
 		}
 
 		[Fact]
 		public void CombinationsWithoutRepetitionUpToLenth()
 		{
 			var combinations = Enumerable.Range(0, 5).CombinationsWithoutRepetition(ofLength: 1, upToLength: 4);
-			var AsString = combinations.Select(x => string.Join(", ", x)).ToArray();
+			var asString = combinations.Select(x => string.Join(", ", x)).ToArray();
 
-			Assert.Equal(30, AsString.Length);
+			Assert.Equal(30, asString.Length);
 			Assert.Equal(combinations.Count(), combinations.Distinct().Count());
 
 			var i = 0;
-			Assert.Equal("0", AsString[i++]);
-			Assert.Equal("1", AsString[i++]);
-			Assert.Equal("2", AsString[i++]);
-			Assert.Equal("3", AsString[i++]);
-			Assert.Equal("4", AsString[i++]);
-			Assert.Equal("0, 1", AsString[i++]);
-			Assert.Equal("0, 2", AsString[i++]);
-			Assert.Equal("0, 3", AsString[i++]);
-			Assert.Equal("0, 4", AsString[i++]);
-			Assert.Equal("1, 2", AsString[i++]);
-			Assert.Equal("1, 3", AsString[i++]);
-			Assert.Equal("1, 4", AsString[i++]);
-			Assert.Equal("2, 3", AsString[i++]);
-			Assert.Equal("2, 4", AsString[i++]);
-			Assert.Equal("3, 4", AsString[i++]);
-			Assert.Equal("0, 1, 2", AsString[i++]);
-			Assert.Equal("0, 1, 3", AsString[i++]);
-			Assert.Equal("0, 1, 4", AsString[i++]);
-			Assert.Equal("0, 2, 3", AsString[i++]);
-			Assert.Equal("0, 2, 4", AsString[i++]);
-			Assert.Equal("0, 3, 4", AsString[i++]);
-			Assert.Equal("1, 2, 3", AsString[i++]);
-			Assert.Equal("1, 2, 4", AsString[i++]);
-			Assert.Equal("1, 3, 4", AsString[i++]);
-			Assert.Equal("2, 3, 4", AsString[i++]);
-			Assert.Equal("0, 1, 2, 3", AsString[i++]);
-			Assert.Equal("0, 1, 2, 4", AsString[i++]);
-			Assert.Equal("0, 1, 3, 4", AsString[i++]);
-			Assert.Equal("0, 2, 3, 4", AsString[i++]);
-			Assert.Equal("1, 2, 3, 4", AsString[i++]);
+			Assert.Equal("0", asString[i++]);
+			Assert.Equal("1", asString[i++]);
+			Assert.Equal("2", asString[i++]);
+			Assert.Equal("3", asString[i++]);
+			Assert.Equal("4", asString[i++]);
+			Assert.Equal("0, 1", asString[i++]);
+			Assert.Equal("0, 2", asString[i++]);
+			Assert.Equal("0, 3", asString[i++]);
+			Assert.Equal("0, 4", asString[i++]);
+			Assert.Equal("1, 2", asString[i++]);
+			Assert.Equal("1, 3", asString[i++]);
+			Assert.Equal("1, 4", asString[i++]);
+			Assert.Equal("2, 3", asString[i++]);
+			Assert.Equal("2, 4", asString[i++]);
+			Assert.Equal("3, 4", asString[i++]);
+			Assert.Equal("0, 1, 2", asString[i++]);
+			Assert.Equal("0, 1, 3", asString[i++]);
+			Assert.Equal("0, 1, 4", asString[i++]);
+			Assert.Equal("0, 2, 3", asString[i++]);
+			Assert.Equal("0, 2, 4", asString[i++]);
+			Assert.Equal("0, 3, 4", asString[i++]);
+			Assert.Equal("1, 2, 3", asString[i++]);
+			Assert.Equal("1, 2, 4", asString[i++]);
+			Assert.Equal("1, 3, 4", asString[i++]);
+			Assert.Equal("2, 3, 4", asString[i++]);
+			Assert.Equal("0, 1, 2, 3", asString[i++]);
+			Assert.Equal("0, 1, 2, 4", asString[i++]);
+			Assert.Equal("0, 1, 3, 4", asString[i++]);
+			Assert.Equal("0, 2, 3, 4", asString[i++]);
+			Assert.Equal("1, 2, 3, 4", asString[i++]);
 		}
 	}
 }
