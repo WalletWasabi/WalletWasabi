33d772dec6644fe50421326279b55ef054369b64
nopara73 <adam.ficsor73@gmail.com>
2019-10-29 13:02:40 +0800

    Make static

--- WalletWasabi.Tests/Helpers/TestNodeBuilder.cs
+++ WalletWasabi.Tests/Helpers/TestNodeBuilder.cs
@@ -9,7 +9,7 @@ using WalletWasabi.Helpers;
 
 namespace WalletWasabi.Tests.Helpers
 {
-	public class TestNodeBuilder
+	public static class TestNodeBuilder
 	{
 		public static async Task<CoreNode> CreateAsync([CallerFilePath]string callerFilePath = null, [CallerMemberName]string callerMemberName = null, string additionalFolder = null)
 			=> await CoreNode.CreateAsync(Path.Combine(Global.Instance.DataDir, EnvironmentHelpers.ExtractFileName(callerFilePath), callerMemberName, additionalFolder ?? ""));
