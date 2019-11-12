4550d606045bec74ecef3f40eb49e53bab1edb02
Jumar Macato <hikari.netto23@gmail.com>
2019-11-12 18:09:35 +0800

    Enable link opening only when there's a new version.

--- WalletWasabi.Gui/ViewModels/StatusBarViewModel.cs
+++ WalletWasabi.Gui/ViewModels/StatusBarViewModel.cs
@@ -192,7 +192,10 @@ namespace WalletWasabi.Gui.ViewModels
 			{
 				try
 				{
-					IoHelpers.OpenBrowser("https://wasabiwallet.io/#download");
+					if (UpdateAvailable)
+					{
+						IoHelpers.OpenBrowser("https://wasabiwallet.io/#download");
+					}
 				}
 				catch (Exception ex)
 				{
