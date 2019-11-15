efd5df46a0f5b973bd91debd153c0f7936ee31a7
Dan Walmsley <dan@walms.co.uk>
2019-11-15 15:47:01 +0000

    improve notification helper api

--- WalletWasabi.Gui/Helpers/NotificationHelpers.cs
+++ WalletWasabi.Gui/Helpers/NotificationHelpers.cs
@@ -13,17 +13,22 @@ namespace WalletWasabi.Gui.Helpers
 			return Locator.Current.GetService<INotificationManager>();
 		}
 
-		public static void Information (string title, string message)
+		public static void Success (string message, string title = "Success!")
 		{
-			GetNotificationManager().Show(new Notification(title, message, NotificationType.Information));
+			GetNotificationManager().Show(new Notification(title, message, NotificationType.Success));
 		}
 
-		public static void Warning (string title, string message)
+		public static void Information (string message, string title = "Info!")
 		{
 			GetNotificationManager().Show(new Notification(title, message, NotificationType.Warning));
 		}
 
-		public static void Error(string title, string message)
+		public static void Warning (string message, string title = "Warning!")
+		{
+			GetNotificationManager().Show(new Notification(title, message, NotificationType.Warning));
+		}
+
+		public static void Error(string message, string title = "Error!")
 		{
 			GetNotificationManager().Show(new Notification(title, message, NotificationType.Error));
 		}
