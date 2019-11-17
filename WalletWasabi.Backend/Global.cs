ddfe592a195bed9c86fc880506e5529279004311
nopara73 <adam.ficsor73@gmail.com>
2019-11-17 13:03:17 +0800

    fix dispose bug

--- WalletWasabi.Backend/Global.cs
+++ WalletWasabi.Backend/Global.cs
@@ -101,9 +101,9 @@ namespace WalletWasabi.Backend
 			Guard.NotNull(nameof(endPoint), endPoint);
 
 			// We have to find it, because it's cloned by the node and not perfectly cloned (event handlers cannot be cloned.)
-			var p2pNode = new P2pNode(network, endPoint, new MempoolService(), $"/WasabiCoordinator:{Constants.BackendMajorVersion.ToString()}/");
-			await p2pNode.ConnectAsync(cancel).ConfigureAwait(false);
-			BlockNotifier = new BlockNotifier(TimeSpan.FromSeconds(7), RpcClient, p2pNode);
+			P2pNode = new P2pNode(network, endPoint, new MempoolService(), $"/WasabiCoordinator:{Constants.BackendMajorVersion.ToString()}/");
+			await P2pNode.ConnectAsync(cancel).ConfigureAwait(false);
+			BlockNotifier = new BlockNotifier(TimeSpan.FromSeconds(7), RpcClient, P2pNode);
 			BlockNotifier.Start();
 		}
 
