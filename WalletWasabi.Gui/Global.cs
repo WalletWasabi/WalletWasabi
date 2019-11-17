e5d97bb834afd995f529e662147aaf3efee070c9
Dan Walmsley <dan@walms.co.uk>
2019-11-17 11:08:16 +0000

    fix compile issue.

--- WalletWasabi.Gui/Global.cs
+++ WalletWasabi.Gui/Global.cs
@@ -8,14 +8,10 @@ using NBitcoin.Protocol.Connectors;
 using Nito.AsyncEx;
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.BitcoinCore;
@@ -26,16 +22,14 @@ using WalletWasabi.Blockchain.Keys;
 using WalletWasabi.Blockchain.TransactionBroadcasting;
 using WalletWasabi.Blockchain.TransactionOutputs;
 using WalletWasabi.CoinJoin.Client.Clients;
-using WalletWasabi.Crypto;
-using WalletWasabi.Gui.Dialogs;
 using WalletWasabi.Gui.Helpers;
-using WalletWasabi.Gui.ViewModels;
 using WalletWasabi.Helpers;
 using WalletWasabi.Hwi.Models;
 using WalletWasabi.Logging;
 using WalletWasabi.Services;
 using WalletWasabi.Stores;
 using WalletWasabi.TorSocks5;
+using Utils = NBitcoin.Utils;
 
 namespace WalletWasabi.Gui
 {
