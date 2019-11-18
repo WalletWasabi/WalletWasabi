50a6e7aaf13bf3dbab142a3470ce4b94d4d2100f
molnard <molnardavid84@gmail.com>
2019-11-18 16:36:13 +0800

    HostBuilder replaces WebHostBuilder

--- WalletWasabi.Backend/Program.cs
+++ WalletWasabi.Backend/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using NBitcoin;
 using NBitcoin.RPC;
 using System;
@@ -23,10 +24,12 @@ namespace WalletWasabi.Backend
 			{
 				var endPoint = "http://localhost:37127/";
 
-				using var host = WebHost.CreateDefaultBuilder(args)
-					.UseStartup<Startup>()
-					.UseUrls(endPoint)
+				var host = Host.CreateDefaultBuilder(args)
+					.ConfigureWebHostDefaults(webBuilder => webBuilder
+							.UseStartup<Startup>()
+							.UseUrls(endPoint))
 					.Build();
+
 				await host.RunWithTasksAsync();
 			}
 			catch (Exception ex)
