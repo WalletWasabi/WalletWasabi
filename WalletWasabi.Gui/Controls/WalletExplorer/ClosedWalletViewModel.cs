8f0135e4c37385a15748ccc3abf9d9c943738604
Dan Walmsley <dan@walms.co.uk>
2019-11-26 15:29:02 +0000

    Fix code factor issue.

--- WalletWasabi.Gui/Controls/WalletExplorer/ClosedWalletViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/ClosedWalletViewModel.cs
@@ -1,35 +1,7 @@
-using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Reactive;
-using System.Text;
-using WalletWasabi.Gui.ViewModels;
 
 namespace WalletWasabi.Gui.Controls.WalletExplorer
 {
-	public class WalletViewModelBase : ViewModelBase, IComparable<WalletViewModelBase>
-	{
-		private string _title;
-
-		public string Title
-		{
-			get => _title;
-			set => this.RaiseAndSetIfChanged(ref _title, value);
-		}
-
-		public int CompareTo([AllowNull] WalletViewModelBase other)
-		{
-			if(other is null)
-			{
-				return -1;
-			}
-
-			return Title.CompareTo(other.Title);
-		}
-	}
-
 	class ClosedWalletViewModel : WalletViewModelBase
 	{
 		public ClosedWalletViewModel(string walletFile)
---
+++ WalletWasabi.Gui/Controls/WalletExplorer/WalletViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using ReactiveUI;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using WalletWasabi.Gui.ViewModels;
+
+namespace WalletWasabi.Gui.Controls.WalletExplorer
+{
+	public class WalletViewModelBase : ViewModelBase, IComparable<WalletViewModelBase>
+	{
+		private string _title;
+
+		public string Title
+		{
+			get => _title;
+			set => this.RaiseAndSetIfChanged(ref _title, value);
+		}
+
+		public int CompareTo([AllowNull] WalletViewModelBase other)
+		{
+			if(other is null)
+			{
+				return -1;
+			}
+
+			return Title.CompareTo(other.Title);
+		}
+	}
+}
