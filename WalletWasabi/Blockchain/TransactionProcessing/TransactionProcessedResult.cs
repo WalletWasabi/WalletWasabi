1c17f99729f93512c9c7007c12511fd303de8f20
nopara73 <adam.ficsor73@gmail.com>
2019-11-29 11:39:03 +0800

    Comment

--- WalletWasabi/Blockchain/TransactionProcessing/TransactionProcessedResult.cs
+++ WalletWasabi/Blockchain/TransactionProcessing/TransactionProcessedResult.cs
@@ -11,8 +11,14 @@ namespace WalletWasabi.Blockchain.TransactionProcessing
 	public class TransactionProcessedResult
 	{
 		public SmartTransaction Transaction { get; }
-		public bool IsWalletRelevant => IsLikelyOwnCoinJoin || SuccessfullyDoubleSpentCoins.Any();
+		public bool IsWalletRelevant => SuccessfullyDoubleSpentCoins.Any();
 		public bool IsLikelyOwnCoinJoin { get; set; } = false;
+
+		/// <summary>
+		/// Coins those we previously had in the mempool, but this confirmed
+		/// transaction has successfully invalidated them, because it spends
+		/// some of the same inputs.
+		/// </summary>
 		public List<SmartCoin> SuccessfullyDoubleSpentCoins { get; set; } = new List<SmartCoin>();
 
 		public TransactionProcessedResult(SmartTransaction transaction)
