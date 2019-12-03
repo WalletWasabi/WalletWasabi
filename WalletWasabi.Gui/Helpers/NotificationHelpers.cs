c4d38c7f20869773af3cc62f93b9fbceb33d369d
nopara73 <adam.ficsor73@gmail.com>
2019-12-03 18:33:21 +0800

    Fix memory leak

--- WalletWasabi.Gui/Helpers/NotificationHelpers.cs
+++ WalletWasabi.Gui/Helpers/NotificationHelpers.cs
@@ -17,35 +17,35 @@ namespace WalletWasabi.Gui.Helpers
 
 		public static void Notify(string message, string title, NotificationType type, Action onClick = null)
 		{
-			RxApp.MainThreadScheduler
+			using var disposable = RxApp.MainThreadScheduler
 				.Schedule(() => GetNotificationManager()
 				.Show(new Notification(title, message, type, TimeSpan.FromSeconds(7), onClick)));
 		}
 
 		public static void Success(string message, string title = "Success!")
 		{
-			RxApp.MainThreadScheduler
+			using var disposable = RxApp.MainThreadScheduler
 				.Schedule(() => GetNotificationManager()
 				.Show(new Notification(title, message, NotificationType.Success, TimeSpan.FromSeconds(7))));
 		}
 
 		public static void Information(string message, string title = "Info")
 		{
-			RxApp.MainThreadScheduler
+			using var disposable = RxApp.MainThreadScheduler
 				.Schedule(() => GetNotificationManager()
 				.Show(new Notification(title, message, NotificationType.Information, TimeSpan.FromSeconds(7))));
 		}
 
 		public static void Warning(string message, string title = "Warning!")
 		{
-			RxApp.MainThreadScheduler
+			using var disposable = RxApp.MainThreadScheduler
 				.Schedule(() => GetNotificationManager()
 				.Show(new Notification(title, message, NotificationType.Warning, TimeSpan.FromSeconds(7))));
 		}
 
 		public static void Error(string message, string title = "Error")
 		{
-			RxApp.MainThreadScheduler
+			using var disposable = RxApp.MainThreadScheduler
 				.Schedule(() => GetNotificationManager()
 				.Show(new Notification(title, message, NotificationType.Error, TimeSpan.FromSeconds(7))));
 		}
