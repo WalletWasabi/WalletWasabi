dd46b9a2a73bc1bbcaa51581e43c2a2a71e9af14
lontivero <lucasontivero@gmail.com>
2019-12-06 13:41:58 -0300

    Display a notification when file is not found

--- WalletWasabi.Gui/Tabs/SettingsViewModel.cs
+++ WalletWasabi.Gui/Tabs/SettingsViewModel.cs
@@ -374,7 +374,7 @@ namespace WalletWasabi.Gui.Tabs
 
 		private void OpenConfigFile()
 		{
-			IoHelpers.OpenFileInTextEditor(Global.Config.FilePath);
+			FileHelpers.OpenFileInTextEditor(Global.Config.FilePath);
 		}
 
 		#region Validation
