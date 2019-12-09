3a74764fc3c36dfd42b7adce57fe7a32908b004f
Yahia Chiheb <chihebyahia@gmail.com>
2019-12-09 04:53:50 +0100

    Edit generation wallet workflow

--- WalletWasabi.Gui/Tabs/WalletManager/GenerateWalletSuccessViewModel.cs
+++ WalletWasabi.Gui/Tabs/WalletManager/GenerateWalletSuccessViewModel.cs
@@ -5,6 +5,10 @@ using WalletWasabi.Gui.ViewModels;
 using WalletWasabi.Logging;
 using System;
 using System.Reactive.Linq;
+using WalletWasabi.Helpers;
+using WalletWasabi.Blockchain.Keys;
+using WalletWasabi.Models;
+using WalletWasabi.Gui.Helpers;
 
 namespace WalletWasabi.Gui.Tabs.WalletManager
 {
@@ -12,15 +16,33 @@ namespace WalletWasabi.Gui.Tabs.WalletManager
 	{
 		private string _mnemonicWords;
 
-		public GenerateWalletSuccessViewModel(WalletManagerViewModel owner, Mnemonic mnemonic) : base("Wallet Generated Successfully!")
+		public GenerateWalletSuccessViewModel(WalletManagerViewModel owner, string password, string walletFilePath, Global global) : base("Wallet Generated Successfully!")
 		{
+			// Here we are not letting anything that will be autocorrected later. We need to generate the wallet exactly with the entered password bacause of compatibility.
+			PasswordHelper.Guard(password);
+
+			var keyManager = KeyManager.CreateNew(out Mnemonic mnemonic, password);
 			_mnemonicWords = mnemonic.ToString();
 
-			ConfirmCommand = ReactiveCommand.Create(() => owner.SelectTestPassword());
+			ConfirmCommand = ReactiveCommand.Create(() =>
+				{
+					DoConfirmCommand(keyManager, walletFilePath, global);
+					owner.SelectTestPassword();
+				});
 
 			ConfirmCommand.ThrownExceptions
 				.ObserveOn(RxApp.TaskpoolScheduler)
 				.Subscribe(ex => Logger.LogError(ex));
+		}
+
+		private void DoConfirmCommand(KeyManager keyManager, string walletFilePath, Global global)
+		{
+			keyManager.SetNetwork(global.Network);
+			keyManager.SetBestHeight(new Height(global.BitcoinStore.SmartHeaderChain.TipHeight));
+			keyManager.SetFilePath(walletFilePath);
+			keyManager.ToFile();
+
+			NotificationHelpers.Success("Wallet is successfully generated!");
 		}
 
 		public string MnemonicWords
