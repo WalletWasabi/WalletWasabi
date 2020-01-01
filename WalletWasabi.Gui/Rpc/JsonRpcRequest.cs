638f9a2ec8cea4046c693aa97ad8542b003f7e49
lontivero <lucasontivero@gmail.com>
2020-01-01 18:06:30 -0300

    Add documentation (a bit redundant)

--- WalletWasabi.Gui/Rpc/JsonRpcRequest.cs
+++ WalletWasabi.Gui/Rpc/JsonRpcRequest.cs
@@ -6,6 +6,15 @@ namespace WalletWasabi.Gui.Rpc
 {
 	/// <summary>
 	/// A rpc call is represented by sending a Request object to a Server. The Request object has the following members:
+	/// + jsonrpc - A String specifying the version of the JSON-RPC protocol. MUST be exactly "2.0".
+	/// + method  - A String containing the name of the method to be invoked. Method names that begin with the word rpc
+	///             followed by a period character (U+002E or ASCII 46) are reserved for rpc-internal methods and extensions
+	///             and MUST NOT be used for anything else.
+	/// + params  - A Structured value that holds the parameter values to be used during the invocation of the method. This
+	///             member MAY be omitted.
+	/// + id      - An identifier established by the Client that MUST contain a String, Number, or NULL value if included. 
+	///             If it is not included it is assumed to be a notification. The value SHOULD normally not be Null [1] and
+	///             Numbers SHOULD NOT contain fractional parts [2]
 	/// </summary>
 	public class JsonRpcRequest
 	{
