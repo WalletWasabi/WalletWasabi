3435cff01bb56d55ef5d5dfbd300a81b6aee3a52
lontivero <lucasontivero@gmail.com>
2020-01-03 15:39:32 -0300

    Throw exception when wallet has no fingerprint

--- WalletWasabi.Gui/Tabs/WalletManager/LoadWalletViewModel.cs
+++ WalletWasabi.Gui/Tabs/WalletManager/LoadWalletViewModel.cs
@@ -477,6 +477,10 @@ namespace WalletWasabi.Gui.Tabs.WalletManager
 							await EnumerateHardwareWalletsAsync();
 							selectedWallet = Wallets.FirstOrDefault(x => x.HardwareWalletInfo.Model == selectedWallet.HardwareWalletInfo.Model && x.HardwareWalletInfo.Path == selectedWallet.HardwareWalletInfo.Path);
 						}
+						if (!selectedWallet.HardwareWalletInfo.Fingerprint.HasValue)
+						{
+							throw new InvalidOperationException("Wallet doesn't contain the fingerprint.");
+						}
 						KeyManager.CreateNewHardwareWalletWatchOnly(selectedWallet.HardwareWalletInfo.Fingerprint.Value, extPubKey, path);
 					}
 				}
