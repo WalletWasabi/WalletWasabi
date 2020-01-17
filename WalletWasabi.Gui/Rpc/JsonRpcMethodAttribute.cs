7bb057d1b1a309e46366eee5e8b8ddca46fc9550
nopara73 <adam.ficsor73@gmail.com>
2020-01-17 10:28:51 -0400

    Make attribute sealed. https://docs.microsoft.com/en-gb/visualstudio/code-quality/ca1813?view=vs-2019

--- WalletWasabi.Gui/Rpc/JsonRpcMethodAttribute.cs
+++ WalletWasabi.Gui/Rpc/JsonRpcMethodAttribute.cs
@@ -6,7 +6,7 @@ namespace WalletWasabi.Gui.Rpc
 	/// Class used to decorate service methods and map them with their rpc method name.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
-	public class JsonRpcMethodAttribute : Attribute
+	public sealed class JsonRpcMethodAttribute : Attribute
 	{
 		public string Name { get; }
 
