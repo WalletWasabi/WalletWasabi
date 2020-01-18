9156e6184e755af95e3a7b37ea703e9365548b51
Yahia Chiheb <chihebyahia@gmail.com>
2020-01-18 02:16:50 +0100

    Use expression body for lambda expressions

--- WalletWasabi.Gui/Controls/WalletExplorer/WalletAdvancedViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/WalletAdvancedViewModel.cs
@@ -25,7 +25,9 @@ namespace WalletWasabi.Gui.Controls.WalletExplorer
 		{
 			Items = new ObservableCollection<WalletActionViewModel>();
 
+#pragma warning disable IDE0053 // Use expression body for lambda expressions
 			ExpandItCommand = ReactiveCommand.Create(() => { IsExpanded = !IsExpanded; });
+#pragma warning restore IDE0053 // Use expression body for lambda expressions
 
 			ExpandItCommand.ThrownExceptions
 				.ObserveOn(RxApp.TaskpoolScheduler)
