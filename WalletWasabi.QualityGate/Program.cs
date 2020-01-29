c9be53d417fa3ea7a4158efa42639c24744c2726
nopara73 <adam.ficsor73@gmail.com>
2020-01-29 12:56:56 -0400

    Add skeleton project.

---
+++ WalletWasabi.QualityGate/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace WalletWasabi.QualityGate
+{
+	// This software runs on CI and fails if the quality of a pull request does not conform to our rules.
+	public class Program
+	{
+		private static void Main()
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
