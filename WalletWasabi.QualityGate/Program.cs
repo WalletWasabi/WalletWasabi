c1378a6318a41c09e5deb84699766bcf0e8efad5
nopara73 <adam.ficsor73@gmail.com>
2020-01-29 13:05:23 -0400

    Test passing quality gate

--- WalletWasabi.QualityGate/Program.cs
+++ WalletWasabi.QualityGate/Program.cs
@@ -5,9 +5,18 @@ namespace WalletWasabi.QualityGate
 	// This software runs on CI and fails if the quality of a pull request does not conform to our rules.
 	public class Program
 	{
-		private static void Main()
+		private static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			if (args[0] == "foo")
+			{
+				Console.WriteLine("Failing gate.");
+				throw new Exception("foo");
+			}
+			else
+			{
+				Console.WriteLine("Passing gate.");
+				return;
+			}
 		}
 	}
 }
