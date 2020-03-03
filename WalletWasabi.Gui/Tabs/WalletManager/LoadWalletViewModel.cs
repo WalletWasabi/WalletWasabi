403d009ae127ab555017b90e87d851009b18b0cc
nopara73 <adam.ficsor73@gmail.com>
2020-03-03 10:00:43 +0100

    Bring out walletservice to a variable.

--- WalletWasabi.Gui/Tabs/WalletManager/LoadWalletViewModel.cs
+++ WalletWasabi.Gui/Tabs/WalletManager/LoadWalletViewModel.cs
@@ -632,17 +632,20 @@ namespace WalletWasabi.Gui.Tabs.WalletManager
 				try
 				{
 					await Task.Run(async () => await Global.InitializeWalletServiceAsync(keyManager));
+
+					var walletService = Global.WalletService;
+
 					// Successfully initialized.
 					Owner.OnClose();
 					// Open Wallet Explorer tabs
-					if (Global.WalletService.Coins.Any())
+					if (walletService.Coins.Any())
 					{
 						// If already have coins then open the last active tab first.
-						IoC.Get<WalletExplorerViewModel>().OpenWallet(Global.WalletService, receiveDominant: false);
+						IoC.Get<WalletExplorerViewModel>().OpenWallet(walletService, receiveDominant: false);
 					}
 					else // Else open with Receive tab first.
 					{
-						IoC.Get<WalletExplorerViewModel>().OpenWallet(Global.WalletService, receiveDominant: true);
+						IoC.Get<WalletExplorerViewModel>().OpenWallet(walletService, receiveDominant: true);
 					}
 				}
 				catch (Exception ex)
