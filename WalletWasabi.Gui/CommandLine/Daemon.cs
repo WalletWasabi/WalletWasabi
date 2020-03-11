a8abb2a61fa372c9f84254fab8b807d14cbaa061
nopara73 <adam.ficsor73@gmail.com>
2020-03-11 10:23:34 +0100

    Improve comment.

--- WalletWasabi.Gui/CommandLine/Daemon.cs
+++ WalletWasabi.Gui/CommandLine/Daemon.cs
@@ -103,7 +103,7 @@ namespace WalletWasabi.Gui.CommandLine
 					bool anyCoinsQueued = WalletService.ChaumianClient.State.AnyCoinsQueued();
 					if (!anyCoinsQueued && keepMixAlive) // If no coins queued and mixing is asked to be kept alive then try to queue coins.
 					{
-						// Don't do mixall here, the mixall says all the coins has to be mixed all once, not all the time.
+						// Don't do mixall here, the mixall says all the coins has to be mixed once, it doesn't says it has to be requeued all the time.
 						await TryQueueCoinsToMixAsync(mixAll: false, password);
 					}
 
