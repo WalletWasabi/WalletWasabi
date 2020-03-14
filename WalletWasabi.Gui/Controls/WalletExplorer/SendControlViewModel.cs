3cbf35318d7a115f68f6d94ec911a2cb04e9adc5
nopara73 <adam.ficsor73@gmail.com>
2020-03-14 17:29:11 +0100

    IsNullOrWhiteSpace

--- WalletWasabi.Gui/Controls/WalletExplorer/SendControlViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/SendControlViewModel.cs
@@ -839,10 +839,10 @@ namespace WalletWasabi.Gui.Controls.WalletExplorer
 
 			return new ErrorDescriptors(new ErrorDescriptor(ErrorSeverity.Error, "Invalid address."));
 		}
-		
+
 		public ErrorDescriptors ValidateBIP79Url()
 		{
-			if (string.IsNullOrEmpty(BIP79Url))
+			if (string.IsNullOrWhiteSpace(BIP79Url))
 			{
 				return ErrorDescriptors.Empty;
 			}
@@ -891,7 +891,7 @@ namespace WalletWasabi.Gui.Controls.WalletExplorer
 			get => _customChangeAddress;
 			set => this.RaiseAndSetIfChanged(ref _customChangeAddress, value?.Trim());
 		}
-		
+
 		[ValidateMethod(nameof(ValidateBIP79Url))]
 		public string BIP79Url
 		{
