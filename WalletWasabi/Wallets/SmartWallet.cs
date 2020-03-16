03d8725ded8b4ddf751de08caf32bd709c0de2c6
David Molnar <molnardavid84@gmail.com>
2020-03-16 10:22:47 +0100

    Add SmartWallet class

---
+++ WalletWasabi/Wallets/SmartWallet.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.Hosting;
+using NBitcoin.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WalletWasabi.BitcoinCore;
+using WalletWasabi.Blockchain.Analysis.FeesEstimation;
+using WalletWasabi.Blockchain.Keys;
+using WalletWasabi.Helpers;
+using WalletWasabi.Models;
+using WalletWasabi.Services;
+using WalletWasabi.Stores;
+
+namespace WalletWasabi.Wallets
+{
+	public class SmartWallet : BackgroundService
+	{
+		public SmartWallet(string filePath)
+		{
+			KeyManager = KeyManager.FromFile(filePath);
+		}
+
+		public SmartWallet(KeyManager keyManager)
+		{
+			KeyManager = Guard.NotNull(nameof(keyManager), keyManager);
+		}
+
+		public KeyManager KeyManager { get; }
+		private WalletService Wallet { get; set; }
+
+		public void InitializeWalletService(BitcoinStore bitcoinStore, WasabiSynchronizer synchronizer, NodesGroup nodes, string dataDir, ServiceConfiguration serviceConfiguration, IFeeProvider feeProvider, CoreNode bitcoinCoreNode)
+		{
+			Wallet = new WalletService(bitcoinStore, KeyManager, synchronizer, nodes, dataDir, serviceConfiguration, feeProvider, bitcoinCoreNode);
+		}
+
+		/// <inheritdoc />
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			await Wallet.StartAsync(stoppingToken).ConfigureAwait(false);
+		}
+
+		public override async Task StopAsync(CancellationToken cancellationToken)
+		{
+			await base.StopAsync(cancellationToken).ConfigureAwait(false);
+
+			var wallet = Wallet;
+			if (wallet is { })
+			{
+				await wallet.StopAsync(cancellationToken).ConfigureAwait(false);
+			}
+		}
+	}
+}
