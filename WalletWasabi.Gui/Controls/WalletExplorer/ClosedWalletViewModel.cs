f817b2896a15cde32e9463d17057f0cbb4cd5d2d
Dan Walmsley <dan@walms.co.uk>
2020-03-19 15:54:44 -0300

    protect wallet loading with try catch, and clear isbusy in case of exceptions.

--- WalletWasabi.Gui/Controls/WalletExplorer/ClosedWalletViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/ClosedWalletViewModel.cs
@@ -1,11 +1,14 @@
 using AvalonStudio.Extensibility;
 using ReactiveUI;
 using Splat;
+using System;
 using System.IO;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading;
+using WalletWasabi.Gui.Helpers;
+using WalletWasabi.Logging;
 
 namespace WalletWasabi.Gui.Controls.WalletExplorer
 {
@@ -15,45 +18,54 @@ namespace WalletWasabi.Gui.Controls.WalletExplorer
 		{
 			OpenWalletCommand = ReactiveCommand.CreateFromTask(async () =>
 			{
-				IsBusy = true;
-
-				var global = Locator.Current.GetService<Global>();
-
-				if (!await global.WaitForInitializationCompletedAsync(CancellationToken.None))
+				try
 				{
-					return;
+					IsBusy = true;
+
+					var global = Locator.Current.GetService<Global>();
+
+					if (!await global.WaitForInitializationCompletedAsync(CancellationToken.None))
+					{
+						return;
+					}
+
+					var walletManager = global.WalletManager;
+
+					var keyManager = global.LoadKeyManager(Title);
+
+					if (keyManager is null)
+					{
+						return;
+					}
+
+					var wallet = await walletManager.CreateAndStartWalletAsync(keyManager);
+
+					var walletExplorer = IoC.Get<WalletExplorerViewModel>();
+
+					var select = walletExplorer.SelectedItem == this;
+
+					walletExplorer.RemoveWallet(this);
+
+					if (wallet.Coins.Any())
+					{
+						// If already have coins then open the last active tab first.
+						walletExplorer.OpenWallet(wallet, receiveDominant: false, select: select);
+					}
+					else // Else open with Receive tab first.
+					{
+						walletExplorer.OpenWallet(wallet, receiveDominant: true, select: select);
+					}
 				}
-
-				var walletManager = global.WalletManager;
-
-				var keyManager = global.LoadKeyManager(Title);
-
-				if (keyManager is null)
+				catch (Exception e)
+				{
+					NotificationHelpers.Error($"Error loading Wallet: {Title}");
+					Logger.LogError(e.Message);
+				}
+				finally
 				{
 					IsBusy = false;
-					return;
 				}
-
-				var walletService = await walletManager.CreateAndStartWalletAsync(keyManager);
-
-				var walletExplorer = IoC.Get<WalletExplorerViewModel>();
-
-				var select = walletExplorer.SelectedItem == this;
-
-				walletExplorer.RemoveWallet(this);
-
-				if (walletService.Coins.Any())
-				{
-					// If already have coins then open the last active tab first.
-					walletExplorer.OpenWallet(walletService, receiveDominant: false, select: select);
-				}
-				else // Else open with Receive tab first.
-				{
-					walletExplorer.OpenWallet(walletService, receiveDominant: true, select: select);
-				}
-
-				IsBusy = false;
-			}, this.WhenAnyValue(x=>x.IsBusy).Select(x=>!x));
+			}, this.WhenAnyValue(x => x.IsBusy).Select(x => !x));
 		}
 
 		public ReactiveCommand<Unit, Unit> OpenWalletCommand { get; }
