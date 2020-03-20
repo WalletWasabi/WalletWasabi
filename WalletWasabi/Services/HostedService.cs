cec9c4150b294505fabe9826484e67041d65deef
Yahia Chiheb <chihebyahia@gmail.com>
2020-03-20 21:23:30 +0100

    Order elements in some classes

--- WalletWasabi/Services/HostedService.cs
+++ WalletWasabi/Services/HostedService.cs
@@ -9,13 +9,13 @@ namespace WalletWasabi.Services
 {
 	public class HostedService
 	{
-		public IHostedService Service { get; }
-		public string FriendlyName { get; }
-
 		public HostedService(IHostedService service, string friendlyName)
 		{
 			Service = Guard.NotNull(nameof(service), service);
 			FriendlyName = Guard.NotNull(nameof(friendlyName), friendlyName);
 		}
+
+		public IHostedService Service { get; }
+		public string FriendlyName { get; }
 	}
 }
