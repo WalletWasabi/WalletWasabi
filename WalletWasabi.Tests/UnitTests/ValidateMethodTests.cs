547728f77bff813859060df9f80cec1c3561fd0f
Yahia Chiheb <chihebyahia@gmail.com>
2020-04-19 16:16:52 +0100

    Order elements in some classes

--- WalletWasabi.Tests/UnitTests/ValidateMethodTests.cs
+++ WalletWasabi.Tests/UnitTests/ValidateMethodTests.cs
@@ -13,20 +13,6 @@ namespace WalletWasabi.Tests.UnitTests
 {
 	public class ValidateMethodTests
 	{
-		public class ValidateTestClass
-		{
-			public ErrorDescriptors ValidateProperty()
-			{
-				return new ErrorDescriptors(new ErrorDescriptor(ErrorSeverity.Warning, "My warning error descriptor"), new ErrorDescriptor(ErrorSeverity.Error, "My error error descriptor"));
-			}
-
-			[ValidateMethod(nameof(ValidateProperty))]
-			public bool BooleanProperty { get; set; }
-
-			[ValidateMethod(nameof(ValidateProperty))]
-			public string StringProperty { get; set; }
-		}
-
 		[Fact]
 		public void PropertiesWithValidationTest()
 		{
@@ -34,6 +20,20 @@ namespace WalletWasabi.Tests.UnitTests
 
 			var validator = Validator.PropertiesWithValidation(testClass);
 			Assert.Equal(2, validator.Count());
+		}
+
+		public class ValidateTestClass
+		{
+			[ValidateMethod(nameof(ValidateProperty))]
+			public bool BooleanProperty { get; set; }
+
+			[ValidateMethod(nameof(ValidateProperty))]
+			public string StringProperty { get; set; }
+
+			public ErrorDescriptors ValidateProperty()
+			{
+				return new ErrorDescriptors(new ErrorDescriptor(ErrorSeverity.Warning, "My warning error descriptor"), new ErrorDescriptor(ErrorSeverity.Error, "My error error descriptor"));
+			}
 		}
 	}
 }
