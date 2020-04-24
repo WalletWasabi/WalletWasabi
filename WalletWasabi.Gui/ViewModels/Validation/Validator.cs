b8c5a9c829e8649181c4d53351e9aa400ca7a3bd
Dan Walmsley <dan@walms.co.uk>
2020-04-24 13:56:06 -0300

    Remove validator methods, no longer needed.

--- WalletWasabi.Gui/ViewModels/Validation/Validator.cs
+++ WalletWasabi.Gui/ViewModels/Validation/Validator.cs
@@ -6,53 +6,5 @@ using WalletWasabi.Models;
 
 namespace WalletWasabi.Gui.ViewModels.Validation
 {
-	public delegate void ValidateMethod(IErrorList errors);	
-
-	public static class Validator
-	{
-		//public static IEnumerable<(string propertyName, ErrorDescriptors errors)> ValidateAllProperties(Dictionary<string, ValidateMethod> validationMethodCache)
-		//{
-		//	if(validationMethodCache is null || validationMethodCache.Count == 0)
-		//	{
-		//		throw new Exception("Cant call ValidateAllProperties on ViewModels with no ValidateAttributes");
-		//	}
-
-		//	var result = new List<(string propertyName, ErrorDescriptors errors)>();
-
-		//	foreach (var propertyName in validationMethodCache.Keys)
-		//	{
-			
-		//		var invokeRes = (ErrorDescriptors)validationMethodCache[propertyName].Invoke();
-
-		//		result.Add((propertyName, invokeRes));
-		//	}
-
-		//	return result;
-		//}
-
-		//public static ErrorDescriptors ValidateProperty(ViewModelBase viewModelBase, string propertyName,
-		//	Dictionary<string, MethodInfo> validationMethodCache)
-		//{
-		//	if (validationMethodCache is null)
-		//	{
-		//		return ErrorDescriptors.Empty;
-		//	}
-
-		//	ErrorDescriptors result = null;
-
-		//	if(validationMethodCache.ContainsKey(propertyName))
-		//	{
-		//		var invokeRes = (ErrorDescriptors)validationMethodCache[propertyName].Invoke(viewModelBase, null);
-
-		//		if (result is null)
-		//		{
-		//			result = new ErrorDescriptors();
-		//		}
-
-		//		result.AddRange(invokeRes);
-		//	}
-
-		//	return result ?? ErrorDescriptors.Empty;
-		//}
-	}
+	public delegate void ValidateMethod(IErrorList errors);		
 }
