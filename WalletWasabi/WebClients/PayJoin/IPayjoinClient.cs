c731e60453bd26ec618ef51d880dac550f4d1307
Kukks <evilkukka@gmail.com>
2020-05-06 13:30:33 +0200

    format interface

--- WalletWasabi/WebClients/PayJoin/IPayjoinClient.cs
+++ WalletWasabi/WebClients/PayJoin/IPayjoinClient.cs
@@ -8,9 +8,6 @@ namespace WalletWasabi.WebClients.PayJoin
 	public interface IPayjoinClient
 	{
 		Uri PaymentUrl { get; }
-
-		// Task<PSBT> RequestPayjoin(PSBT originalTx, IHDKey accountKey, RootedKeyPath rootedKeyPath, CancellationToken cancellationToken);
 		Task<PSBT> TryNegotiatePayjoin(Func<PSBT, Task<PSBT>> sign, PSBT psbt, KeyManager keyManager);
-
 	}
 }
