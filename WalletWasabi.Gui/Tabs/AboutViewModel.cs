9721de12040ea95de297eed43ba8f6b1dda28007
MDavid <molnardavid84@gmail.com>
2020-05-14 12:22:57 +0200

    better naming

--- WalletWasabi.Gui/Tabs/AboutViewModel.cs
+++ WalletWasabi.Gui/Tabs/AboutViewModel.cs
@@ -29,7 +29,7 @@ namespace WalletWasabi.Gui.Tabs
 		public ReactiveCommand<string, Unit> OpenBrowserCommand { get; }
 
 		public Version ClientVersion => Constants.ClientVersion;
-		public string BackendCompatibleVersion => $"{Constants.BackendMajorVersion},{Constants.ClientCompatibleWithBackendVersion}";
+		public string BackendCompatibleVersions => $"{Constants.BackendMajorVersion},{Constants.ClientCompatibleWithBackendVersion}";
 		public string CurrentBackendMajorVersion => WasabiClient.CurrentBackendMajorVersion;
 		public Version BitcoinCoreVersion => Constants.BitcoinCoreVersion;
 		public Version HwiVersion => Constants.HwiVersion;
