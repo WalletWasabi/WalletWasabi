f73a18b2be6f603634a2902f996b1b80b441f252
nopara73 <adam.ficsor73@gmail.com>
2020-05-31 12:27:03 +0200

    properties after constructors

--- WalletWasabi/CoinJoin/Common/Models/PublicNonceWithIndex.cs
+++ WalletWasabi/CoinJoin/Common/Models/PublicNonceWithIndex.cs
@@ -6,17 +6,17 @@ namespace WalletWasabi.CoinJoin.Common.Models
 {
 	public class PublicNonceWithIndex
 	{
-		[JsonProperty]
-		public int N { get; set; }
-		
-		[JsonProperty]
-		[JsonConverter(typeof(PubKeyJsonConverter))]
-		public PubKey R { get; set; }
-
 		public PublicNonceWithIndex(int n, PubKey rPubKey)
 		{
 			N = n;
 			R = rPubKey;
 		}
+
+		[JsonProperty]
+		public int N { get; set; }
+
+		[JsonProperty]
+		[JsonConverter(typeof(PubKeyJsonConverter))]
+		public PubKey R { get; set; }
 	}
 }
