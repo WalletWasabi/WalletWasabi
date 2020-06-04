ede0b28ee49b8974fe3a2f0c3a477399c469ffd5
nopara73 <adam.ficsor73@gmail.com>
2020-06-04 10:41:30 +0200

    Merge update status equality tests

--- WalletWasabi.Tests/UnitTests/UpdateStatusTests.cs
+++ WalletWasabi.Tests/UnitTests/UpdateStatusTests.cs
@@ -19,18 +19,27 @@ namespace WalletWasabi.Tests.UnitTests
 			var x = new UpdateStatus(backendCompatible, clientUpToDate, legalVersion, backendVersion);
 			var y = new UpdateStatus(backendCompatible, clientUpToDate, legalVersion, backendVersion);
 			Assert.Equal(x, y);
+			Assert.Equal(x.GetHashCode(), y.GetHashCode());
 
 			y = new UpdateStatus(true, clientUpToDate, legalVersion, backendVersion);
 			Assert.NotEqual(x, y);
+			Assert.NotEqual(x.GetHashCode(), y.GetHashCode());
 
 			y = new UpdateStatus(backendCompatible, true, legalVersion, backendVersion);
 			Assert.NotEqual(x, y);
+			Assert.NotEqual(x.GetHashCode(), y.GetHashCode());
 
 			y = new UpdateStatus(backendCompatible, clientUpToDate, new Version(2, 2), backendVersion);
 			Assert.NotEqual(x, y);
+			Assert.NotEqual(x.GetHashCode(), y.GetHashCode());
+
+			y = new UpdateStatus(backendCompatible, clientUpToDate, new Version(1, 1, 1), backendVersion);
+			Assert.NotEqual(x, y);
+			Assert.NotEqual(x.GetHashCode(), y.GetHashCode());
 
 			y = new UpdateStatus(backendCompatible, clientUpToDate, legalVersion, 2);
 			Assert.NotEqual(x, y);
+			Assert.NotEqual(x.GetHashCode(), y.GetHashCode());
 		}
 	}
 }
