9398c07ec81075e352f6eb1b93206570a0bc1de8
David Molnar <molnardavid84@gmail.com>
2020-06-11 14:20:41 +0200

    Add multitexbox

--- WalletWasabi.Gui/CrashReport/ViewModels/CrashReportWindowViewModel.cs
+++ WalletWasabi.Gui/CrashReport/ViewModels/CrashReportWindowViewModel.cs
@@ -14,7 +14,7 @@ namespace WalletWasabi.Gui.CrashReport.ViewModels
 		public int MinWidth => 370;
 		public int MinHeight => 180;
 		public string Title => "Wasabi Wallet - Crash Reporting";
-		public string Details => CrashReporter.GetException().ToString();
+		public string Details => "Wasabi has crashed. You can check the details here, open the log file, or report the crash to the support team. Please always consider your privacy before sharing any information!";
 		public string Message => CrashReporter.GetException().Message;
 	}
 }
