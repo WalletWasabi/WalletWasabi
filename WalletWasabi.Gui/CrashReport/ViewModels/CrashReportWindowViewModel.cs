21decc43cb193906acde05c1247741b3e96aee68
David Molnar <molnardavid84@gmail.com>
2020-06-11 14:28:30 +0200

    Add button functionality

--- WalletWasabi.Gui/CrashReport/ViewModels/CrashReportWindowViewModel.cs
+++ WalletWasabi.Gui/CrashReport/ViewModels/CrashReportWindowViewModel.cs
@@ -1,4 +1,11 @@
+using ReactiveUI;
 using Splat;
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using WalletWasabi.Gui.Controls.WalletExplorer;
+using WalletWasabi.Gui.Helpers;
+using WalletWasabi.Logging;
 
 namespace WalletWasabi.Gui.CrashReport.ViewModels
 {
@@ -8,6 +15,24 @@ namespace WalletWasabi.Gui.CrashReport.ViewModels
 		{
 			var global = Locator.Current.GetService<Global>();
 			CrashReporter = global.CrashReporter;
+
+			OpenLogCommand = ReactiveCommand.CreateFromTask(async () =>
+			{
+				await FileHelpers.OpenFileInTextEditorAsync(Logger.FilePath);
+			});
+
+			OkCommand = ReactiveCommand.Create(() =>
+			{
+			});
+
+			Observable
+				.Merge(OpenLogCommand.ThrownExceptions)
+				.Merge(OkCommand.ThrownExceptions)
+				.ObserveOn(RxApp.TaskpoolScheduler)
+				.Subscribe(ex =>
+				{
+					Logger.LogError(ex);
+				});
 		}
 
 		private CrashReporter CrashReporter { get; }
@@ -16,5 +41,8 @@ namespace WalletWasabi.Gui.CrashReport.ViewModels
 		public string Title => "Wasabi Wallet - Crash Reporting";
 		public string Details => "Wasabi has crashed. You can check the details here, open the log file, or report the crash to the support team. Please always consider your privacy before sharing any information!";
 		public string Message => CrashReporter.GetException().Message;
+
+		public ReactiveCommand<Unit, Unit> OpenLogCommand { get; }
+		public ReactiveCommand<Unit, Unit> OkCommand { get; }
 	}
 }
