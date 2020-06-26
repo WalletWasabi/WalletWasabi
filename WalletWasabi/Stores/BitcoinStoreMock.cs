5f31793b7c28d8d6ed16ca0c6beb5087e33efcd3
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2020-06-26 19:13:51 +0800

    Update WalletWasabi/Stores/BitcoinStoreMock.cs

--- WalletWasabi/Stores/BitcoinStoreMock.cs
+++ WalletWasabi/Stores/BitcoinStoreMock.cs
@@ -2,7 +2,7 @@ namespace WalletWasabi.Stores
 {
 	/// <summary>
 	/// This class provides a Mock version of <see cref="BitcoinStore"/> for
-	/// Unit-tests that only need a dummy version of the class.
+	/// Unit tests that only need a dummy version of the class.
 	/// </summary>
 	public class BitcoinStoreMock : BitcoinStore
 	{
