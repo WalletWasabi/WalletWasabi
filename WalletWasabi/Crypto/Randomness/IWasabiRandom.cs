ff3d2f99f59437f99b0319d8b2a70fb9f2540c79
nopara73 <adam.ficsor73@gmail.com>
2020-07-07 13:52:17 +0200

    Implement randomness

--- WalletWasabi/Crypto/Randomness/IWasabiRandom.cs
+++ WalletWasabi/Crypto/Randomness/IWasabiRandom.cs
@@ -7,5 +7,11 @@ namespace WalletWasabi.Crypto.Randomness
 {
 	public interface IWasabiRandom : IRandom
 	{
+		public byte[] GetBytes(int length)
+		{
+			var buffer = new byte[length];
+			GetBytes(buffer);
+			return buffer;
+		}
 	}
 }
