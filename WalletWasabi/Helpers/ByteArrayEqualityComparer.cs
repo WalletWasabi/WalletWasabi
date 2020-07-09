2a7f5640fc9d4d49ad52dd3bd32b5c58de4b78c0
nopara73 <adam.ficsor73@gmail.com>
2020-07-09 12:38:36 +0200

    CodeMaid

--- WalletWasabi/Helpers/ByteArrayEqualityComparer.cs
+++ WalletWasabi/Helpers/ByteArrayEqualityComparer.cs
@@ -6,10 +6,10 @@ using WalletWasabi.Crypto;
 
 namespace WalletWasabi.Helpers
 {
-    public class ByteArrayEqualityComparer : IEqualityComparer<byte[]>
-    {
-        public bool Equals([AllowNull] byte[] x, [AllowNull] byte[] y) => ByteHelpers.CompareFastUnsafe(x, y);
+	public class ByteArrayEqualityComparer : IEqualityComparer<byte[]>
+	{
+		public bool Equals([AllowNull] byte[] x, [AllowNull] byte[] y) => ByteHelpers.CompareFastUnsafe(x, y);
 
-        public int GetHashCode([DisallowNull] byte[] obj) => HashHelpers.ComputeHashCode(obj);
-    }
+		public int GetHashCode([DisallowNull] byte[] obj) => HashHelpers.ComputeHashCode(obj);
+	}
 }
