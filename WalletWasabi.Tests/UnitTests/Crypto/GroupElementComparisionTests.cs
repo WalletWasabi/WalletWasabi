df3a43f9180a06ebe00a4520d5c9e9f4adadc33b
nopara73 <adam.ficsor73@gmail.com>
2020-07-21 15:06:26 +0200

    Remove magnitude and normalization tests, as they should be internal.

--- WalletWasabi.Tests/UnitTests/Crypto/GroupElementComparisionTests.cs
+++ WalletWasabi.Tests/UnitTests/Crypto/GroupElementComparisionTests.cs
@@ -57,40 +57,6 @@ namespace WalletWasabi.Tests.UnitTests.Crypto
 		}
 
 		[Fact]
-		public void MagnitudeDoesntMatter()
-		{
-			// ToDo: is this ok?
-			var one = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 0, normalized: false);
-			var one2 = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 1, normalized: false);
-			var a = new GE(one, one);
-			var b = new GE(one2, one);
-			Assert.True(Secp256k1Helpers.Equals(a, b));
-
-			one = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 0, normalized: false);
-			one2 = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 2, normalized: false);
-			a = new GE(one, one);
-			b = new GE(one2, one);
-			Assert.True(Secp256k1Helpers.Equals(a, b));
-
-			one = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 1, normalized: false);
-			one2 = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 2, normalized: false);
-			a = new GE(one, one);
-			b = new GE(one2, one);
-			Assert.True(Secp256k1Helpers.Equals(a, b));
-		}
-
-		[Fact]
-		public void NormalizationDoesntMatter()
-		{
-			// ToDo: is this ok?
-			var one = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 0, normalized: false);
-			var one2 = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 0, normalized: true);
-			var a = new GE(one, one);
-			var b = new GE(one2, one);
-			Assert.True(Secp256k1Helpers.Equals(a, b));
-		}
-
-		[Fact]
 		public void OneOneOneEqualsOneOneOneJacobian()
 		{
 			var one = new FE(1);
@@ -136,40 +102,6 @@ namespace WalletWasabi.Tests.UnitTests.Crypto
 			var two = new FE(2);
 			var a = new GEJ(one, one, one, infinity: true);
 			var b = new GEJ(two, one, one, infinity: true);
-			Assert.True(Secp256k1Helpers.Equals(a, b));
-		}
-
-		[Fact]
-		public void MagnitudeDoesntMatterJacobian()
-		{
-			// ToDo: is this ok?
-			var one = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 0, normalized: false);
-			var one2 = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 1, normalized: false);
-			var a = new GEJ(one, one, one);
-			var b = new GEJ(one2, one, one);
-			Assert.True(Secp256k1Helpers.Equals(a, b));
-
-			one = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 0, normalized: false);
-			one2 = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 2, normalized: false);
-			a = new GEJ(one, one, one);
-			b = new GEJ(one2, one, one);
-			Assert.True(Secp256k1Helpers.Equals(a, b));
-
-			one = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 1, normalized: false);
-			one2 = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 2, normalized: false);
-			a = new GEJ(one, one, one);
-			b = new GEJ(one2, one, one);
-			Assert.True(Secp256k1Helpers.Equals(a, b));
-		}
-
-		[Fact]
-		public void NormalizationDoesntMatterJacobian()
-		{
-			// ToDo: is this ok?
-			var one = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 0, normalized: false);
-			var one2 = new FE(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, magnitude: 0, normalized: true);
-			var a = new GEJ(one, one, one);
-			var b = new GEJ(one2, one, one);
 			Assert.True(Secp256k1Helpers.Equals(a, b));
 		}
 	}
