90d384525f277c43a9281c44b4f3dead9567ab51
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2020-07-23 14:44:49 +0800

    Update WalletWasabi.Gui/Controls/WalletExplorer/SendControlViewModel.cs

--- WalletWasabi.Gui/Controls/WalletExplorer/SendControlViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/SendControlViewModel.cs
@@ -60,7 +60,7 @@ namespace WalletWasabi.Gui.Controls.WalletExplorer
 		private string _amountTip;
 
 		private const string WaitingForHardwareWalletButtonTextString = "Waiting for Hardware Wallet...";
-		private readonly long MaxSatoshisSupplyLimit = Constants.MaxSatoshisSupply * 1_000; 
+		private readonly long MaxSatoshisSupplyLimit = Constants.MaxSatoshisSupply / 1_000; 
 		private FeeDisplayFormat _feeDisplayFormat;
 		private bool _isSliderFeeUsed = true;
 		private double _feeControlOpacity;
