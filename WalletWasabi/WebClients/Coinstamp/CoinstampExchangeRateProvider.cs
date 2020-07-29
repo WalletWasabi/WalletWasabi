e14d3fc21abde9d372ab34119366da3303fe6bf3
lontivero <lucasontivero@gmail.com>
2020-07-29 15:51:37 -0300

    Add Coinstamp as price provider

---
+++ WalletWasabi/WebClients/Coinstamp/CoinstampExchangeRateProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WalletWasabi.Backend.Models;
+using WalletWasabi.Interfaces;
+
+namespace WalletWasabi.WebClients.BlockchainInfo
+{
+	public class CoinstampExchangeRateProvider : IExchangeRateProvider
+	{
+		public async Task<List<ExchangeRate>> GetExchangeRateAsync()
+		{
+			using var httpClient = new HttpClient();
+			httpClient.BaseAddress = new Uri("https://www.bitstamp.net");
+			using var response = await httpClient.GetAsync("/api/v2/ticker/btcusd");
+			using var content = response.Content;
+			var rate = await content.ReadAsJsonAsync<CoinstampExchangeRate>();
+
+			var exchangeRates = new List<ExchangeRate>
+				{
+					new ExchangeRate { Rate = rate.Rate, Ticker = "USD" }
+				};
+
+			return exchangeRates;
+		}
+
+		public class CoinstampExchangeRate
+		{
+
+			[JsonProperty(PropertyName = "bid")]
+			public decimal Rate { get; set; }
+		}
+	}
+}
