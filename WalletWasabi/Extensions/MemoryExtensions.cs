c66c36b53727bbafedfe9fe6472c5a8db88dd6b4
nopara73 <adam.ficsor73@gmail.com>
2020-07-30 18:47:00 +0200

    Make sure to clean my dic.

--- WalletWasabi/Extensions/MemoryExtensions.cs
+++ WalletWasabi/Extensions/MemoryExtensions.cs
@@ -1,6 +1,7 @@
 using Nito.AsyncEx;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Microsoft.Extensions.Caching.Memory
 	public static class MemoryExtensions
 	{
 		private static Dictionary<object, AsyncLock> AsyncLocks { get; } = new Dictionary<object, AsyncLock>();
+
 		private static object AsyncLocksLock { get; } = new object();
 
 		public static async Task<TItem> AtomicGetOrCreateAsync<TItem>(this IMemoryCache cache, object key, Func<ICacheEntry, Task<TItem>> factory)
@@ -21,6 +23,12 @@ namespace Microsoft.Extensions.Caching.Memory
 			AsyncLock asyncLock;
 			lock (AsyncLocksLock)
 			{
+				// Cleanup the evicted asynclocks first.
+				foreach (var toRemove in AsyncLocks.Keys.Where(x => !cache.TryGetValue(x, out _)).ToList())
+				{
+					AsyncLocks.Remove(toRemove);
+				}
+
 				if (!AsyncLocks.TryGetValue(key, out asyncLock))
 				{
 					asyncLock = new AsyncLock();
