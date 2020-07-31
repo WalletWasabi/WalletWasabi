c1c9ccb7a764d1b978fe56479fc557a5d490d72c
Jumar Macato <hikari.netto23@gmail.com>
2020-07-31 17:18:32 +0800

    add git info class

---
+++ WalletWasabi/GitInfo/GitInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System;
+using System.Reflection;
+
+namespace WalletWasabi
+{
+	public class GitInfo
+	{
+		public static string BuildCommitHash { get; }
+
+		public static string BuildBranch { get; }
+
+		static GitInfo()
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			BuildCommitHash = GetStringFromResourceStream(assembly, "WalletWasabi.GitInfo.currentcommit.txt");
+			BuildBranch = GetStringFromResourceStream(assembly, "WalletWasabi.GitInfo.currentbranch.txt");
+		}
+
+		private static string GetStringFromResourceStream(Assembly assembly, string ID)
+		{
+			try
+			{
+				using var reader = new StreamReader(assembly.GetManifestResourceStream(ID));
+				return reader.ReadToEnd().Trim().Trim('\r').Trim('\n');
+			}
+			catch (Exception _)
+			{
+				return "";
+			}
+		}
+	}
+}
