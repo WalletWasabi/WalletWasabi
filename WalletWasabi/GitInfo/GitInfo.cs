492c9706eb42e3f95c5826e1fabb749ed6f75d49
Jumar Macato <hikari.netto23@gmail.com>
2020-07-31 17:24:08 +0800

    update

--- WalletWasabi/GitInfo/GitInfo.cs
+++ WalletWasabi/GitInfo/GitInfo.cs
@@ -13,15 +13,15 @@ namespace WalletWasabi
 		static GitInfo()
 		{
 			var assembly = Assembly.GetExecutingAssembly();
-			BuildCommitHash = GetStringFromResourceStream(assembly, "WalletWasabi.GitInfo.currentcommit.txt");
-			BuildBranch = GetStringFromResourceStream(assembly, "WalletWasabi.GitInfo.currentbranch.txt");
+			BuildCommitHash = GetStringFromResourceStream(assembly, "WalletWasabi.GitInfo.git-info-commit.txt");
+			BuildBranch = GetStringFromResourceStream(assembly, "WalletWasabi.GitInfo.git-info-branch.txt");
 		}
 
-		private static string GetStringFromResourceStream(Assembly assembly, string ID)
+		private static string GetStringFromResourceStream(Assembly assembly, string resId)
 		{
 			try
 			{
-				using var reader = new StreamReader(assembly.GetManifestResourceStream(ID));
+				using var reader = new StreamReader(assembly.GetManifestResourceStream(resId));
 				return reader.ReadToEnd().Trim().Trim('\r').Trim('\n');
 			}
 			catch (Exception _)
