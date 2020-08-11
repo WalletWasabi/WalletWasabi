2bccc7e84339d6e2bc5a053e510d6ff3fb77a158
Kiminuo <kiminuo@protonmail.com>
2020-08-11 15:52:42 +0200

    Invert if.

--- WalletWasabi/Bases/LastExceptionTracker.cs
+++ WalletWasabi/Bases/LastExceptionTracker.cs
@@ -13,19 +13,19 @@ namespace WalletWasabi.Bases
 		public ExceptionInfo? Process(Exception currentException)
 		{
 			// Only log one type of exception once.
-			if (LastException is null // If the exception never came.
-				|| currentException.GetType() != LastException.Exception.GetType() // Or the exception has different type from previous exception.
-				|| currentException.Message != LastException.Exception.Message) // Or the exception has different message from previous exception.
-			{
-				var previousException = LastException;
-				LastException = new ExceptionInfo(currentException, exceptionCount: 1, firstAppeared: DateTimeOffset.UtcNow);
-				return previousException;
-			}
-			else
+			if (!(LastException is null)
+				&& currentException.GetType() == LastException.Exception.GetType()
+				&& currentException.Message == LastException.Exception.Message)
 			{
 				// Increment the counter.
 				LastException.ExceptionCount++;
 				return null;
+			}
+			else
+			{
+				var previousException = LastException;
+				LastException = new ExceptionInfo(currentException, exceptionCount: 1, firstAppeared: DateTimeOffset.UtcNow);
+				return previousException;
 			}
 		}
 	}
