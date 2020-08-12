fbebe7436433f29b6e6f8b4b7f25e6dff1484e17
lontivero <lucasontivero@gmail.com>
2020-08-12 20:32:43 -0300

    Move colors to App.xaml

--- WalletWasabi.Gui/Converters/NetworkColorConverter.cs
+++ WalletWasabi.Gui/Converters/NetworkColorConverter.cs
@@ -13,12 +13,11 @@ namespace WalletWasabi.Gui.Converters
 	public class NetworkColorConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
-			Brush.Parse(value switch
+			value switch
 			{
-				Network network when network == Network.TestNet => "#318522",
-				Network network when network == Network.RegTest => "#AE6200",
-				_ => "#007ACC"
-			});
+				Network network => Application.Current.Resources[network.Name],
+				_ => Application.Current.Resources[Network.Main.Name]
+			};
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
 			throw new NotSupportedException();
