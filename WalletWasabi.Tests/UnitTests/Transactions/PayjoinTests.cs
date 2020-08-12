415fa01e72d9c23c31c3e5e8b1c7110497f49ae0
Kiminuo <kiminuo@protonmail.com>
2020-08-12 22:16:19 +0200

    Clean up tests.

--- WalletWasabi.Tests/UnitTests/Transactions/PayjoinTests.cs
+++ WalletWasabi.Tests/UnitTests/Transactions/PayjoinTests.cs
@@ -463,10 +463,9 @@ namespace WalletWasabi.Tests.UnitTests.Transactions
 				}
 			}
 			var coinsView = new CoinsView(scoins);
+			var transactionStore = new AllTransactionStoreMock(workFolderPath: ".", Network.Main);
 
-			var bitcoinStore = new BitcoinStore(null!, new AllTransactionStoreMock(workFolderPath: ".", Network.Main), null!);
-
-			return new TransactionFactory(Network.Main, keyManager, coinsView, bitcoinStore, password, allowUnconfirmed);
+			return new TransactionFactory(Network.Main, keyManager, coinsView, transactionStore, password, allowUnconfirmed);
 		}
 	}
 }
