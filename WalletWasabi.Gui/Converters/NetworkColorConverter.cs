5821c55fcf4902cea0448c19444c41e5f7fcd98d
David Molnar <molnardavid84@gmail.com>
2020-08-13 13:48:19 +0200

    Get back to the original solution.

--- WalletWasabi.Gui/Converters/NetworkColorConverter.cs
+++ WalletWasabi.Gui/Converters/NetworkColorConverter.cs
@@ -14,15 +14,12 @@ namespace WalletWasabi.Gui.Converters
 {
 	public class NetworkColorConverter : IValueConverter
 	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			if (Application.Current.TryFindResource(value.ToString(), out var color))
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+			value switch
 			{
-				return color;
-			}
-
-			return "#007ACC";
-		}
+				Network network => Application.Current.Resources[network.Name],
+				_ => Application.Current.Resources[Network.Main.Name]
+			};
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
 			throw new NotSupportedException();
