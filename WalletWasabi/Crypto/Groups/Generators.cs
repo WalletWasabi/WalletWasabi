d4987b6f948f2b9c060e22eabba586e8e3e4bcd8
nopara73 <adam.ficsor73@gmail.com>
2020-08-13 11:22:06 +0200

    Trigger lazy CodeFactor

--- WalletWasabi/Crypto/Groups/Generators.cs
+++ WalletWasabi/Crypto/Groups/Generators.cs
@@ -8,52 +8,52 @@ namespace WalletWasabi.Crypto.Groups
 	public static class Generators
 	{
 		/// <summary>
-		/// The base point defined in the secp256k1 standard used in ECDSA public key derivation.
+		/// Base point defined in the secp256k1 standard used in ECDSA public key derivation.
 		/// </summary>
 		public static GroupElement G { get; } = new GroupElement(EC.G);
 
 		/// <summary>
-		/// Generator for MAC and Show.
+		/// Generator point for MAC and Show.
 		/// </summary>
 		public static GroupElement Gw { get; } = GroupElement.FromText("Gw");
 
 		/// <summary>
-		/// Generator for MAC and Show.
+		/// Generator point for MAC and Show.
 		/// </summary>
 		public static GroupElement Gwp { get; } = GroupElement.FromText("Gwp");
 
 		/// <summary>
-		/// Generator for MAC and Show.
+		/// Generator point for MAC and Show.
 		/// </summary>
 		public static GroupElement Gx0 { get; } = GroupElement.FromText("Gx0");
 
 		/// <summary>
-		/// Generator for MAC and Show.
+		/// Generator point for MAC and Show.
 		/// </summary>
 		public static GroupElement Gx1 { get; } = GroupElement.FromText("Gx1");
 
 		/// <summary>
-		/// Generator for MAC and Show.
+		/// Generator point for MAC and Show.
 		/// </summary>
 		public static GroupElement GV { get; } = GroupElement.FromText("GV");
 
 		/// <summary>
-		/// Generator for Pedersen commitments.
+		/// Generator point for Pedersen commitments.
 		/// </summary>
 		public static GroupElement Gg { get; } = GroupElement.FromText("Gg");
 
 		/// <summary>
-		/// Generator for Pedersen commitments.
+		/// Generator point for Pedersen commitments.
 		/// </summary>
 		public static GroupElement Gh { get; } = GroupElement.FromText("Gh");
 
 		/// <summary>
-		/// Generator for attributes M_{ai}.
+		/// Generator point for attributes M_{ai}.
 		/// </summary>
 		public static GroupElement Ga { get; } = GroupElement.FromText("Ga");
 
 		/// <summary>
-		/// Generator for serial numbers.
+		/// Generator point for serial numbers.
 		/// </summary>
 		public static GroupElement Gs { get; } = GroupElement.FromText("Gs");
 
