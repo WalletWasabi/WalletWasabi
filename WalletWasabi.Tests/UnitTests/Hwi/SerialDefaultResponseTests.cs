e6a6401c3a5766cb1e90764aeeafd275ee13ad0b
Kiminuo <kiminuo@protonmail.com>
2020-08-17 15:55:45 +0200

    Move test.

--- WalletWasabi.Tests/UnitTests/Hwi/SerialDefaultResponseTests.cs
+++ WalletWasabi.Tests/UnitTests/Hwi/SerialDefaultResponseTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.Hwi.ProcessBridge;
+using WalletWasabi.Microservices;
 using Xunit;
 
 namespace WalletWasabi.Tests.UnitTests.Hwi
@@ -19,7 +20,7 @@ namespace WalletWasabi.Tests.UnitTests.Hwi
 		[Fact]
 		public async Task HwiProcessBridgeTestAsync()
 		{
-			HwiProcessBridge pb = new HwiProcessBridge();
+			HwiProcessBridge pb = new HwiProcessBridge(MicroserviceHelpers.GetBinaryPath("hwi"), new ProcessInvoker());
 
 			using var cts = new CancellationTokenSource(ReasonableRequestTimeout);
 			var res = await pb.SendCommandAsync("version", false, cts.Token);
@@ -29,6 +30,18 @@ namespace WalletWasabi.Tests.UnitTests.Hwi
 			res = await pb.SendCommandAsync("version", false, cts.Token, (sw) => stdInputActionCalled = true);
 			Assert.NotEmpty(res.response);
 			Assert.True(stdInputActionCalled);
+		}
+
+		[Fact]
+		public async void HwiProcessHelpTestAsync()
+		{
+			using var cts = new CancellationTokenSource();
+
+			var processBridge = new HwiProcessBridge(MicroserviceHelpers.GetBinaryPath("hwi"), new ProcessInvoker());
+			(string response, int exitCode) p = await processBridge.SendCommandAsync("--help", openConsole: false, cts.Token);
+
+			Assert.Equal(0, p.exitCode);
+			Assert.Equal("{\"error\": \"Help text requested\", \"code\": -17}" + Environment.NewLine, p.response);
 		}
 	}
 }
