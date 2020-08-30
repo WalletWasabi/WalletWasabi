a3ebd5fcbc2de01286f14827a5b24ed12a206bf4
lontivero <lucasontivero@gmail.com>
2020-08-30 15:56:08 -0300

    Coordinators key and params

---
+++ WalletWasabi/Crypto/CoordinatorParameters.cs
@@ -0,0 +1,16 @@
+﻿using WalletWasabi.Crypto.Groups;
+
+namespace WalletWasabi.Crypto
+{
+	public class CoordinatorParameters
+	{
+		public CoordinatorParameters(GroupElement Cw, GroupElement I)
+		{
+			this.Cw = CryptoGuard.NotInfinity(nameof(Cw), Cw);
+			this.I = CryptoGuard.NotInfinity(nameof(I), I);
+		}
+
+		public GroupElement Cw { get; }
+		public GroupElement I { get; }
+	}
+}
---
+++ WalletWasabi/Crypto/CoordinatorSecretKey.cs
@@ -0,0 +1,35 @@
+﻿using NBitcoin.Secp256k1;
+using WalletWasabi.Crypto.Groups;
+using WalletWasabi.Crypto.Randomness;
+
+namespace WalletWasabi.Crypto
+{
+	public class CoordinatorSecretKey
+	{
+		public CoordinatorSecretKey(WasabiRandom rng)
+			: this(rng.GetScalar(), rng.GetScalar(), rng.GetScalar(), rng.GetScalar(), rng.GetScalar())
+		{}
+
+		private CoordinatorSecretKey(Scalar w, Scalar wp, Scalar x0, Scalar x1, Scalar ya)
+		{
+			this.w  = w;
+			this.wp = wp;
+			this.x0 = x0;
+			this.x1 = x1;
+			this.ya = ya;
+		}
+
+		public Scalar w { get; }
+		public Scalar wp { get; }
+		public Scalar x0 { get; }
+		public Scalar x1 { get; }
+		public Scalar ya { get; }
+
+		public CoordinatorParameters ComputeCoordinatorParameters() =>
+			new CoordinatorParameters((w * Generators.Gw + wp * Generators.Gwp),
+				(x0.Negate() * Generators.Gx0) +
+				(x1.Negate() * Generators.Gx1) +
+				(ya.Negate() * Generators.Ga ) +
+				Generators.GV );
+	}
+}
