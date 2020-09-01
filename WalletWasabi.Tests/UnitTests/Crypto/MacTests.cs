f468fadce5b2e453821a5b9a39e7104be15387e2
lontivero <lucasontivero@gmail.com>
2020-09-01 14:58:44 -0300

    One more test for Mac

--- WalletWasabi.Tests/UnitTests/Crypto/MacTests.cs
+++ WalletWasabi.Tests/UnitTests/Crypto/MacTests.cs
@@ -37,5 +37,29 @@ namespace WalletWasabi.Tests.UnitTests.Crypto
 
 			Assert.True(mac.VerifyMAC(sk, attribute));
 		}
+
+		[Fact]
+		[Trait("UnitTest", "UnitTest")]
+		public void CanDetectInvalidMAC()
+		{
+			var rnd = new SecureRandom();
+			var sk = new CoordinatorSecretKey(rnd);
+
+			var attribute = rnd.GetScalar() * Generators.G;  // any random point
+			var differentAttribute = rnd.GetScalar() * Generators.G;  // any other random point
+			var t = rnd.GetScalar();
+
+			// Create MAC for realAttribute and verify with fake/wrong attribute
+			var mac = MAC.ComputeMAC(sk, attribute, t);
+			Assert.False(mac.VerifyMAC(sk, differentAttribute));
+
+			var differentT = rnd.GetScalar();
+			var differentMac = MAC.ComputeMAC(sk, attribute, differentT);
+			Assert.NotEqual(mac, differentMac);
+
+			mac = MAC.ComputeMAC(sk, attribute, differentT);
+			var differentSk = new CoordinatorSecretKey(rnd);
+			Assert.False(mac.VerifyMAC(differentSk, attribute));
+		}
 	}
 }
