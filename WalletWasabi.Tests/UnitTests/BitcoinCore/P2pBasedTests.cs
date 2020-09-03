a169f460da099b8168ae59c7368e09efb6ef9e98
Kiminuo <kiminuo@protonmail.com>
2020-09-03 21:42:43 +0200

    Rename SerialP2pBasedTests back to P2pBasedTests.

--- WalletWasabi.Tests/UnitTests/BitcoinCore/P2pBasedTests.cs
+++ WalletWasabi.Tests/UnitTests/BitcoinCore/P2pBasedTests.cs
@@ -18,13 +18,9 @@ using Xunit;
 
 namespace WalletWasabi.Tests.UnitTests.BitcoinCore
 {
-	/// <summary>
-	/// The tests in this collection are time-sensitive, therefore this test collection is run in a special way:
-	/// Parallel-capable test collections will be run first (in parallel), followed by parallel-disabled test collections (run sequentially) like this one.
-	/// </summary>
-	/// <seealso href="https://xunit.net/docs/running-tests-in-parallel.html#parallelism-in-test-frameworks"/>
+	/// <seealso cref="XunitConfiguration.SerialCollectionDefinition"/>
 	[Collection("Serial unit tests collection")]
-	public class SerialP2pBasedTests
+	public class P2pBasedTests
 	{
 		[Fact]
 		public async Task MempoolNotifiesAsync()
