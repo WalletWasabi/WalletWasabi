35a6327b43fc6676ba7f20980295faf57608f4df
Yuval Kogman <nothingmuch@woobling.org>
2020-09-07 07:42:18 +0000

    Use xmldoc for IStatement and IKnowledge documentation
    Co-authored-by: kiminuo <58662979+kiminuo@users.noreply.github.com>

--- WalletWasabi/Crypto/ZeroKnowledge/NonInteractive/IKnowledge.cs
+++ WalletWasabi/Crypto/ZeroKnowledge/NonInteractive/IKnowledge.cs
@@ -2,13 +2,35 @@ using WalletWasabi.Crypto.Randomness;
 
 namespace WalletWasabi.Crypto.ZeroKnowledge.NonInteractive
 {
-	// IKnowledge splits proving into 3 phases
-	// The second phase is to generate and commit to all the nonces
+	/// <summary>
+	/// This delegate represents the second phase of proof creation with <see
+	/// cref="IKnowledge"/>.
+	/// </summary>
 	public delegate RespondToChallenge ProverCommitToNonces(WasabiRandom random);
 
-	// The third phase is to generate challenges and respond to them
+	/// <summary>
+	/// This delegate represents the third phase of proof creation with <see
+	/// cref="IKnowledge"/>.
+	/// </summary>
 	public delegate Proof RespondToChallenge();
 
+	/// <summary>
+	/// <see cref="IKnowledge"/> supports creation of a non interactive proof
+	/// based on a strong variant of the Fiat-Shamir transform
+	/// <see cref="Transcript"/> by splitting the process into into three phases:
+	/// <list type="number">
+	/// <item>The first phase is to commit to all the statements and their public
+	/// inputs using <see cref="CommitToStatements(Transcript)"/>, so that
+	/// synthetic nonce generation for every sub-proof depends on the statement as
+	/// a whole.</item>
+	/// <item>The second phase is to generate and commit all the public nonces
+	/// using <see cref="ProverCommitToNonces"/>, so that the challenges for each
+	/// sub-proof depends on the nonces of all other statements.</item>
+	/// <item>The third phase is to generate challenges based on the transcript
+	/// and compute the responses to them using
+	/// <see cref="RespondToChallenge"/>.</item>
+	/// </list>
+	/// </summary>
 	public interface IKnowledge
 	{
 		// The first phase is to commit to all the statements, so that synthetic
--- WalletWasabi/Crypto/ZeroKnowledge/NonInteractive/IStatement.cs
+++ WalletWasabi/Crypto/ZeroKnowledge/NonInteractive/IStatement.cs
@@ -3,17 +3,41 @@ using System.Collections.Generic;
 
 namespace WalletWasabi.Crypto.ZeroKnowledge.NonInteractive
 {
-	// IStatement splits verification into 3 phases
-	// The second phase is to generate and commit to all the nonces
+	/// <summary>
+	/// This delegate represents the second phase of proof verification with <see
+	/// cref="IStatement"/>.
+	/// </summary>
 	public delegate VerifyResponse VerifierCommitToNonces(Proof proof);
 
-	// The third phase is to generate challenges and respond to them
+	/// <summary>
+	/// This delegate represents the third phase of proof verification with <see
+	/// cref="IStatement"/>.
+	/// </summary>
 	public delegate bool VerifyResponse();
 
+	/// <summary>
+	/// <see cref="IStatement"/> splits verification of a non interactive proof using
+	/// into three phases corresponding to those of <see cref="IKnowledge"/>:
+	/// <list type="number">
+
+	/// <see cref="IStatement"/> supports the verification of a non-interactive
+	/// proof created by a corresponding <see cref="IKnowledge"/> by splitting the
+	/// process into into the same three phase structure:
+	/// <list type="number">
+	/// <item>The first phase is to commit to all the statements and their public
+	/// inputs using <see cref="CommitToStatements(Transcript)"/>, so that
+	/// the prover's synthetic nonce generation for every sub-proof depends on the
+	/// statement as a whole.</item>
+	/// <item>The second phase is to commit all the public nonces using
+	/// <see cref="VerifierCommitToNonces"/>, so that the challenges for each
+	/// sub-proof depends on the nonces of all other statements.</item>
+	/// <item>The third phase is to generate challenges based on the transcript
+	/// and verify the responses to those challenges with respect to the public
+	/// nonce commitments using <see cref="VerifyResponse"/>.</item>
+	/// </list>
+	/// </summary>
 	public interface IStatement
 	{
-		// The first phase is to commit to all the statements, so that synthetic
-		// nonce generation for every sub-proof depends on the statement as a whole
 		VerifierCommitToNonces CommitToStatements(Transcript transcript);
 
 		// Verifiers must be convertible to corresponding provers
