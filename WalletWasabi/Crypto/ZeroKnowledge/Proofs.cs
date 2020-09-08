15aa7c00d7ad8aa5da937cd6fea3d5e3440fb98c
Yuval Kogman <nothingmuch@woobling.org>
2020-09-08 22:55:01 +0000

    Remove fully qualified use of Prover, Verifier

--- WalletWasabi/Crypto/ZeroKnowledge/Proofs.cs
+++ WalletWasabi/Crypto/ZeroKnowledge/Proofs.cs
@@ -5,13 +5,13 @@ using WalletWasabi.Crypto.Groups;
 using WalletWasabi.Crypto.Randomness;
 using WalletWasabi.Crypto.ZeroKnowledge.LinearRelation;
 using WalletWasabi.Crypto.ZeroKnowledge.NonInteractive;
-using FS = WalletWasabi.Crypto.ZeroKnowledge.NonInteractive.FiatShamirTransform;
+using WalletWasabi.Crypto.ZeroKnowledge.NonInteractive.FiatShamirTransform;
 
 namespace WalletWasabi.Crypto.ZeroKnowledge
 {
 	public static class Proofs
 	{
-		public static bool CheckProof(FS.Verifier verifier, Proof proof)
+		public static bool CheckProof(Verifier verifier, Proof proof)
 		{
 			var transcript = new Transcript(new byte[] { }); // FIXME label
 
@@ -20,7 +20,7 @@ namespace WalletWasabi.Crypto.ZeroKnowledge
 			return VerifyResponse();
 		}
 
-		public static Proof CreateProof(FS.Prover prover, WasabiRandom random)
+		public static Proof CreateProof(Prover prover, WasabiRandom random)
 		{
 			var transcript = new Transcript(new byte[] { }); // FIXME label
 
@@ -37,23 +37,23 @@ namespace WalletWasabi.Crypto.ZeroKnowledge
 		}
 
 		// Syntactic sugar
-		public static Proof CreateProof(FS.Verifier statement, Scalar witness, WasabiRandom random)
+		public static Proof CreateProof(Verifier statement, Scalar witness, WasabiRandom random)
 			=> CreateProof(statement, new ScalarVector(new[] { witness }), random);
 
-		public static Proof CreateProof(FS.Verifier statement, ScalarVector witness, WasabiRandom random)
+		public static Proof CreateProof(Verifier statement, ScalarVector witness, WasabiRandom random)
 			=> CreateProof(statement, new[] { witness }, random);
 
-		public static Proof CreateProof(FS.Verifier statement, IEnumerable<ScalarVector> witness, WasabiRandom random)
+		public static Proof CreateProof(Verifier statement, IEnumerable<ScalarVector> witness, WasabiRandom random)
 			=> CreateProof(statement.ToProver(witness), random);
 
-		public static FS.Verifier DiscreteLog(GroupElement publicPoint, GroupElement generator)
+		public static Verifier DiscreteLog(GroupElement publicPoint, GroupElement generator)
 			=> Representation(publicPoint, generator);
 
-		public static FS.Verifier Representation(GroupElement publicPoint, params GroupElement[] generators)
+		public static Verifier Representation(GroupElement publicPoint, params GroupElement[] generators)
 			=> Representation(publicPoint, new GroupElementVector(generators));
 
-		public static FS.Verifier Representation(GroupElement publicPoint, GroupElementVector generators)
-			=> new FS.Verifier(LinearRelation(publicPoint, generators));
+		public static Verifier Representation(GroupElement publicPoint, GroupElementVector generators)
+			=> new Verifier(LinearRelation(publicPoint, generators));
 
 		private static Statement LinearRelation(GroupElement publicPoint, GroupElementVector generators)
 			=> new Statement(new Equation(publicPoint, generators));
