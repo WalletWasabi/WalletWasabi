7421b5a874b47d40ec3a77a5f2f8575ff1b5d628
MDavid <molnardavid84@gmail.com>
2020-09-15 19:00:04 +0200

    Keep OS system alive when CoinJoining

--- WalletWasabi.Gui/Helpers/Utils.cs
+++ WalletWasabi.Gui/Helpers/Utils.cs
@@ -1,6 +1,7 @@
 using Avalonia.Threading;
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace WalletWasabi.Gui.Helpers
@@ -24,6 +25,28 @@ namespace WalletWasabi.Gui.Helpers
 			}
 
 			return false;
+		}
+
+		[FlagsAttribute]
+		public enum EXECUTION_STATE : uint
+		{
+			ES_AWAYMODE_REQUIRED = 0x00000040,
+			ES_CONTINUOUS = 0x80000000,
+			ES_DISPLAY_REQUIRED = 0x00000002,
+			ES_SYSTEM_REQUIRED = 0x00000001
+			// Legacy flag, should not be used.
+			// ES_USER_PRESENT = 0x00000004
+		}
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		private static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+		public static void KeepSystemAwake()
+		{
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED);
+			}
 		}
 	}
 }
