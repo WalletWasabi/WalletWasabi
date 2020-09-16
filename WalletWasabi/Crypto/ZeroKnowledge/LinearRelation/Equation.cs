3fb0eac8194fe4ef98b5feb3380a1ee77c7fb1f1
Yuval Kogman <nothingmuch@woobling.org>
2020-09-16 10:43:48 +0000

    add additional Equation constructor for tests

--- WalletWasabi/Crypto/ZeroKnowledge/LinearRelation/Equation.cs
+++ WalletWasabi/Crypto/ZeroKnowledge/LinearRelation/Equation.cs
@@ -22,6 +22,11 @@ namespace WalletWasabi.Crypto.ZeroKnowledge.LinearRelation
 	// representation of a specific point.
 	public class Equation
 	{
+		public Equation(GroupElement publicPoint, params GroupElement[] generators)
+			: this(publicPoint, new GroupElementVector(generators))
+		{
+		}
+
 		public Equation(GroupElement publicPoint, GroupElementVector generators)
 		{
 			Guard.NotNullOrEmpty(nameof(generators), generators);
