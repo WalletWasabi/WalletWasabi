34408ee80ba8d0427bc18e52fbb4831581031431
Yuval Kogman <nothingmuch@woobling.org>
2020-09-16 19:02:57 +0000

    Refactor ProofSystem & Fiat Shamir transform
    - Eliminate delegates in Fiat Shamir transform (needs tests), now uses a
      simplified one-shot batch API
    
    - Move FiatShamirTransform.{Prover,Verifier} to NonInteractive namespace, since
      there will only be one kind of non interactive proof (no more ring
      signatures).
    
    - add Prove() Verify() helpers to ProofSystem
    
    - additional syntactic sugar used in tests
    
    - refactor issuer params to work with GroupElement[,] constructor

--- WalletWasabi/Crypto/ZeroKnowledge/ProofSystem.cs
+++ WalletWasabi/Crypto/ZeroKnowledge/ProofSystem.cs
@@ -1,42 +1,43 @@
 using NBitcoin.Secp256k1;
 using System.Linq;
-using System.Text;
+using System.Collections.Generic;
 using WalletWasabi.Crypto.Groups;
+using WalletWasabi.Crypto.Randomness;
 using WalletWasabi.Crypto.ZeroKnowledge.LinearRelation;
+using WalletWasabi.Crypto.ZeroKnowledge.NonInteractive;
 
 namespace WalletWasabi.Crypto.ZeroKnowledge
 {
 	public static class ProofSystem
 	{
-		private static GroupElement Inf = GroupElement.Infinity;
-		private static Transcript Transcript => new Transcript(Encoding.UTF8.GetBytes("proof-of-parameters"));
+		private static GroupElement O = GroupElement.Infinity;
 
-		public static LinearRelation.Statement CreateStatement(CoordinatorParameters coordinatorParameters, GroupElement V, GroupElement Ma, Scalar t) =>
-			new LinearRelation.Statement(
-				new GroupElementVector(coordinatorParameters.Cw, Generators.GV - coordinatorParameters.I, V)
-					.Zip(ProofOfParametersGenerators(MAC.GenerateU(t), Ma, t), (publicPoint, groupElement) => new Equation(publicPoint, groupElement)));
+		public static bool Verify(LinearRelation.Statement statement, Proof proof)
+				=> NonInteractive.Verifier.Verify(new Transcript(new byte[0]), new[] { statement }, new[] { proof });
 
-		public static NonInteractive.FiatShamirTransform.VerifierCommitToNonces CreateVerifier(LinearRelation.Statement statement) =>
-			new NonInteractive.FiatShamirTransform.Verifier(statement).CommitToStatements(Transcript);
+		public static Proof Prove(Knowledge knowledge, WasabiRandom random)
+			=> NonInteractive.Prover.Prove(new Transcript(new byte[0]), new[] { knowledge }, random).First();
 
-		public static NonInteractive.FiatShamirTransform.ProverCommitToNonces CreateProver(LinearRelation.Statement statement, CoordinatorSecretKey coordinatorSecretKey) =>
-			new NonInteractive.FiatShamirTransform.Prover(statement.ToKnowledge(coordinatorSecretKey)).CommitToStatements(Transcript);
+		// Syntactic sugar used in tests
+		public static Proof Prove(LinearRelation.Statement statement, Scalar witness, WasabiRandom random)
+			=> Prove(statement, new ScalarVector(witness), random);
 
-		private static GroupElementVector[] ProofOfParametersGenerators(GroupElement U, GroupElement Ma, Scalar t) =>
-			new GroupElementVector[]
+		public static Proof Prove(LinearRelation.Statement statement, ScalarVector witness, WasabiRandom random)
+			=> Prove(new Knowledge(statement, witness), random);
+
+		public static Knowledge IssuerParameters(MAC mac, GroupElement ma, CoordinatorSecretKey sk)
+			=> new Knowledge(IssuerParameters(sk.ComputeCoordinatorParameters(), mac, ma), new ScalarVector(sk.W, sk.Wp, sk.X0, sk.X1, sk.Ya));
+
+		public static LinearRelation.Statement IssuerParameters(CoordinatorParameters iparams, MAC mac, GroupElement ma)
+		{
+			return new LinearRelation.Statement(new GroupElement[,]
 			{
-				// coordinator's iparams W, Wp, X0, X1, Ya
-				new GroupElementVector( Generators.Gw, Generators.Gwp, Inf, Inf, Inf ),
-				new GroupElementVector( Inf, Inf, Generators.Gx0, Generators.Gx1, Generators.Ga ),
-				new GroupElementVector( Generators.Gw, Inf, U, t * U, Ma )
-			};
-
-		private static LinearRelation.Knowledge ToKnowledge(this LinearRelation.Statement statement, CoordinatorSecretKey coordinatorSecretKey) =>
-			statement.ToKnowledge(new ScalarVector(
-				coordinatorSecretKey.W,
-				coordinatorSecretKey.Wp,
-				coordinatorSecretKey.X0,
-				coordinatorSecretKey.X1,
-				coordinatorSecretKey.Ya));
+				// public                                             Witness terms:
+				// point                     w,             wp,             x0,             x1,             ya
+				{ mac.V,                     Generators.Gw, O,              mac.U,          mac.T * mac.U,  ma },
+				{ Generators.GV - iparams.I, O,             O,              Generators.Gx0, Generators.Gx1, Generators.Ga },
+				{ iparams.Cw,                Generators.Gw, Generators.Gwp, O,              O,              O },
+			});
+		}
 	}
 }
