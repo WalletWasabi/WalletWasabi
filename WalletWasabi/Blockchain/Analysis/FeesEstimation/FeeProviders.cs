7381a5831a09f5a0ca438c3e7209f983637ca494
Dávid Molnár <molnardavid84@gmail.com>
2020-09-23 14:58:45 +0200

    Remove unused usings

--- WalletWasabi/Blockchain/Analysis/FeesEstimation/FeeProviders.cs
+++ WalletWasabi/Blockchain/Analysis/FeesEstimation/FeeProviders.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using WalletWasabi.BitcoinCore.Monitoring;
-using WalletWasabi.Services;
 
 namespace WalletWasabi.Blockchain.Analysis.FeesEstimation
 {
