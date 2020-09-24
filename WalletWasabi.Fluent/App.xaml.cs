2fbf639710abb075504c5248e96d7c7a79d6e5dd
Dan Walmsley <dan@walms.co.uk>
2020-09-24 12:30:05 +0100

    Add initial project structure.

---
+++ WalletWasabi.Fluent/App.xaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using WalletWasabi.Fluent.ViewModels;
+using WalletWasabi.Fluent.Views;
+
+namespace WalletWasabi.Fluent
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ WalletWasabi.Fluent/Views/MainView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Views
+{
+	public class MainView : UserControl
+	{
+		public MainView()
+		{
+			this.InitializeComponent();
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
