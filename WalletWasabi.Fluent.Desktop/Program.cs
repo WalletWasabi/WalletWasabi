9241343a3dac7a775ced48e13cfe6f8d91aff623
Dan Walmsley <dan@walms.co.uk>
2020-09-24 12:44:02 +0100

    fix codefactor issue.

--- WalletWasabi.Fluent.Desktop/Program.cs
+++ WalletWasabi.Fluent.Desktop/Program.cs
@@ -3,7 +3,7 @@ using Avalonia.ReactiveUI;
 
 namespace WalletWasabi.Fluent.Desktop
 {
-	class Program
+	public class Program
     {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
