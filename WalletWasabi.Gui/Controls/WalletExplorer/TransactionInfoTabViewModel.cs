e274e1046e233979ef68149df4d6498fdadf573a
Roland Soós <soos.roland93@gmail.com>
2020-09-29 10:42:22 +0200

    Fix lurking wife mode on transaction id in details

--- WalletWasabi.Gui/Controls/WalletExplorer/TransactionInfoTabViewModel.cs
+++ WalletWasabi.Gui/Controls/WalletExplorer/TransactionInfoTabViewModel.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using ReactiveUI;
+using Splat;
 using WalletWasabi.Gui.Controls.TransactionDetails.ViewModels;
 using WalletWasabi.Gui.ViewModels;
 
@@ -7,9 +12,22 @@ namespace WalletWasabi.Gui.Controls.WalletExplorer
 	{
 		public TransactionInfoTabViewModel(TransactionDetailsViewModel transaction) : base("")
 		{
+			Global = Locator.Current.GetService<Global>();
 			Transaction = transaction;
 			Title = $"Transaction ({transaction.TransactionId[0..10]}) Details";
 		}
+
+		public override void OnOpen(CompositeDisposable disposables)
+		{
+			Global.UiConfig.WhenAnyValue(x => x.LurkingWifeMode)
+				.ObserveOn(RxApp.MainThreadScheduler)
+				.Subscribe(x => Transaction.RaisePropertyChanged(nameof(Transaction.TransactionId)))
+				.DisposeWith(disposables);
+
+			base.OnOpen(disposables);
+		}
+
+		protected Global Global { get; }
 
 		public TransactionDetailsViewModel Transaction { get; }
 	}
