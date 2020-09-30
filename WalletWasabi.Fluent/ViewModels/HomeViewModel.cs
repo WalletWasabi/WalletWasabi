ddb10f4c2ad2ea764653712dc9ff0ec6b1db230b
Roland Soós <soos.roland93@gmail.com>
2020-09-30 13:49:12 +0200

    Add navigaton back button and working WhenNavigatedTo()

--- WalletWasabi.Fluent/ViewModels/HomeViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/HomeViewModel.cs
@@ -1,18 +1,12 @@
 using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace WalletWasabi.Fluent.ViewModels
 {
-	public class HomeViewModel : ViewModelBase, IRoutableViewModel
+	public class HomeViewModel : ViewModelBase
 	{
-		public HomeViewModel(IScreen screen) => HostScreen = screen;
-
-		// Reference to IScreen that owns the routable view model.
-		public IScreen HostScreen { get; }
-
-		// Unique identifier for the routable view model.
-		public string UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);		
+		public HomeViewModel(IScreen screen)
+		{
+			HostScreen = screen;
+		}
 	}
 }
