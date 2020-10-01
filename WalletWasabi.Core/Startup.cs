466d84e67221e7ec5917369c68871f12c6b95b7c
David Molnar <molnardavid84@gmail.com>
2020-10-01 15:53:19 +0200

    Fix CF

--- WalletWasabi.Core/Startup.cs
+++ WalletWasabi.Core/Startup.cs
@@ -42,13 +42,15 @@ namespace WalletWasabi.Core
 			// Register the Swagger generator, defining one or more Swagger documents
 			services.AddSwaggerGen(c =>
 			{
-				c.SwaggerDoc($"v{Constants.CoreMajorVersion}", new OpenApiInfo
-				{
-					Version = $"v{Constants.CoreMajorVersion}",
-					Title = "Wasabi Wallet Core",
-					Description = "Privacy focused Bitcoin wallet.",
-					License = new OpenApiLicense { Name = "Use under MIT.", Url = new Uri("https://github.com/zkSNACKs/WalletWasabi/blob/master/LICENSE.md") }
-				});
+				c.SwaggerDoc(
+					$"v{Constants.CoreMajorVersion}",
+					new OpenApiInfo
+					{
+						Version = $"v{Constants.CoreMajorVersion}",
+						Title = "Wasabi Wallet Core",
+						Description = "Privacy focused Bitcoin wallet.",
+						License = new OpenApiLicense { Name = "Use under MIT.", Url = new Uri("https://github.com/zkSNACKs/WalletWasabi/blob/master/LICENSE.md") }
+					});
 
 				// Set the comments path for the Swagger JSON and UI.
 				var basePath = AppContext.BaseDirectory;
