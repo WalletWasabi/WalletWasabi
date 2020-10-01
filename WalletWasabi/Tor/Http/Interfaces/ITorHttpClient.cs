f15d4f30a636d134516160b17a962f9b60d4c7c6
Kiminuo <kiminuo@protonmail.com>
2020-10-01 12:12:16 +0200

    Remove TorHttpClientExtensions and move the code to ITorHttpClient.

--- WalletWasabi/Tor/Http/Interfaces/ITorHttpClient.cs
+++ WalletWasabi/Tor/Http/Interfaces/ITorHttpClient.cs
@@ -17,5 +17,35 @@ namespace WalletWasabi.Tor.Http.Interfaces
 		Task<HttpResponseMessage> SendAsync(HttpMethod method, string relativeUri, HttpContent? content = null, CancellationToken cancel = default);
 
 		Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancel = default);
+
+		/// <remarks>
+		/// Throws <see cref="OperationCanceledException"/> if <paramref name="cancel"/> is set.
+		/// </remarks>
+		public async Task<HttpResponseMessage?> SendAndRetryAsync(HttpMethod method, HttpStatusCode expectedCode, string relativeUri, int retry = 2, HttpContent? content = null, CancellationToken cancel = default)
+		{
+			HttpResponseMessage? response = null;
+
+			while (retry-- > 0)
+			{
+				response?.Dispose();
+				cancel.ThrowIfCancellationRequested();
+				response = await SendAsync(method, relativeUri, content, cancel: cancel).ConfigureAwait(false);
+
+				if (response.StatusCode == expectedCode)
+				{
+					break;
+				}
+
+				try
+				{
+					await Task.Delay(1000, cancel).ConfigureAwait(false);
+				}
+				catch (TaskCanceledException ex)
+				{
+					throw new OperationCanceledException(ex.Message, ex, cancel);
+				}
+			}
+			return response;
+		}
 	}
 }
