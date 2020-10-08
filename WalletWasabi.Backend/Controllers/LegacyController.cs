085dbe9afc2a0b65a2dbe1d87d383cafd4477c27
nopara73 <adam.ficsor73@gmail.com>
2020-10-08 14:37:25 +0200

    Add legacy exchange-rate API

--- WalletWasabi.Backend/Controllers/LegacyController.cs
+++ WalletWasabi.Backend/Controllers/LegacyController.cs
@@ -13,16 +13,22 @@ namespace WalletWasabi.Backend.Controllers
 	[Produces("application/json")]
 	public class LegacyController : ControllerBase
 	{
-		public LegacyController(BlockchainController blockchainController)
+		public LegacyController(BlockchainController blockchainController, OffchainController offchainController)
 		{
 			BlockchainController = blockchainController;
+			OffchainController = offchainController;
 		}
 
 		public BlockchainController BlockchainController { get; }
+		public OffchainController OffchainController { get; }
 
 		[HttpGet("api/v3/btc/Blockchain/all-fees")]
 		[ResponseCache(Duration = 300, Location = ResponseCacheLocation.Client)]
 		public async Task<IActionResult> GetAllFeesV3Async([FromQuery, Required] string estimateSmartFeeMode)
 			=> await BlockchainController.GetAllFeesAsync(estimateSmartFeeMode);
+
+		[HttpGet("api/v3/btc/Offchain/exchange-rates")]
+		public async Task<IActionResult> GetExchangeRatesV3Async()
+			=> await OffchainController.GetExchangeRatesAsync();
 	}
 }
