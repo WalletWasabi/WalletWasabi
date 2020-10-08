1d7bb64b069e181071767c34ecd78a2ec665cd8b
nopara73 <adam.ficsor73@gmail.com>
2020-10-08 14:17:43 +0200

    Add legacy getfees

---
+++ WalletWasabi.Backend/Controllers/LegacyController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalletWasabi.Backend.Controllers
+{
+	[Produces("application/json")]
+	public class LegacyController : ControllerBase
+	{
+		public LegacyController(BlockchainController blockchainController)
+		{
+			BlockchainController = blockchainController;
+		}
+
+		public BlockchainController BlockchainController { get; }
+
+		[HttpGet("api/v3/btc/Blockchain/all-fees")]
+		[ResponseCache(Duration = 300, Location = ResponseCacheLocation.Client)]
+		public async Task<IActionResult> GetAllFeesV3Async([FromQuery, Required] string estimateSmartFeeMode)
+			=> await BlockchainController.GetAllFeesAsync(estimateSmartFeeMode);
+	}
+}
