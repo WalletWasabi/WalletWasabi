f791ddea94188ed733302081884e6e6aef2df1a5
Jumar Macato <hikari.netto23@gmail.com>
2020-10-13 13:49:47 +0800

    dialog now returns from showdialogasync when closed from overlay/button

--- WalletWasabi.Fluent/ViewModels/Dialog/DialogViewModelBase.cs
+++ WalletWasabi.Fluent/ViewModels/Dialog/DialogViewModelBase.cs
@@ -1,42 +1,42 @@
 ﻿using System;
+using System.Threading.Tasks;
 using ReactiveUI;
 
 namespace WalletWasabi.Fluent.ViewModels.Dialog
 {
-	/// <summary>
-	/// Foundational class for <see cref="DialogViewModelBase{TResult}"/>.
-	/// Don't use this class directly since it doesn't provide the
-	/// functionality required for Dialogs.
-	/// </summary>	
-	public abstract class DialogViewModelBase : ViewModelBase
-	{
-		private bool _isDialogOpen;
- 
-		public bool IsDialogOpen
-		{
-			get => _isDialogOpen;
-			set => this.RaiseAndSetIfChanged(ref _isDialogOpen, value);
-		}
- 
-		/// <summary>
-		/// Method that is triggered when the dialog
-		/// is about to close.
-		/// </summary>
-		protected abstract void OnDialogClosed();
+    /// <summary>
+    /// Foundational class for <see cref="DialogViewModelBase{TResult}"/>.
+    /// Don't use this class directly since it doesn't provide the
+    /// functionality required for Dialogs.
+    /// </summary>	
+    public abstract class DialogViewModelBase : ViewModelBase
+    { 
+        private bool _isDialogOpen;
+        private bool _dialogReturnedWithValue;
 
-		/// <summary>
-		/// Method to be called when the dialog intends to close
-		/// without returning a value.
-		/// </summary>
-		public void CloseDialog()
-		{
-			if (IsDialogOpen is false)
-			{
-				throw new InvalidOperationException("Dialog was already closed.");
-			} 
+        /// <summary>
+        /// Gets or sets if the dialog is opened/closed.
+        /// </summary>
+        public bool IsDialogOpen
+        {
+            get => _isDialogOpen;
+            set => this.RaiseAndSetIfChanged(ref _isDialogOpen, value);
+        }
 
-			IsDialogOpen = false;
-			OnDialogClosed();
-		}
-	}
+        /// <summary>
+        /// Gets or sets if the dialog returned with a value or not.
+        /// </summary>
+        public bool DialogReturnedWithValue
+        {
+            get => _dialogReturnedWithValue;
+            set => this.RaiseAndSetIfChanged(ref _dialogReturnedWithValue, value);
+        }
+
+        /// <summary>
+        /// Method that is triggered when the dialog
+        /// is about to close.
+        /// </summary>
+        protected abstract void OnDialogClosed();
+
+    }
 }
--- WalletWasabi.Fluent/ViewModels/Dialog/DialogViewModelBaseOfTResult.cs
+++ WalletWasabi.Fluent/ViewModels/Dialog/DialogViewModelBaseOfTResult.cs
@@ -1,52 +1,90 @@
 using System;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
+using ReactiveUI;
 
 namespace WalletWasabi.Fluent.ViewModels.Dialog
 {
-	/// <summary>
-	/// Base ViewModel class for Dialogs that returns a value back.
-	/// </summary>
-	/// <typeparam name="TResult">The type of the value to be returned when the dialog is finished.</typeparam>
-	public abstract class DialogViewModelBase<TResult> : DialogViewModelBase
-	{
-		private TaskCompletionSource<TResult> CurrentTaskCompletionSource { get; set; }
+    /// <summary>
+    /// Base ViewModel class for Dialogs that returns a value back.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the value to be returned when the dialog is finished.</typeparam>
+    public abstract class DialogViewModelBase<TResult> : DialogViewModelBase
+    {
+        protected DialogViewModelBase()
+        {
+            this.WhenAnyValue(x => x.IsDialogOpen)
+                .DistinctUntilChanged()
+                .Subscribe(OnIsDialogOpenChanged);
+        }
 
-		/// <summary>
-		/// Method to be called when the dialog intends to close
-		/// and ready to pass a value back to the caller.
-		/// </summary>
-		/// <param name="value">The return value of the dialog</param>
-		public void CloseDialog(TResult value)
-		{
-			if (CurrentTaskCompletionSource is null)
-			{
-				throw new InvalidOperationException("CloseDialog with value return method failed due to missing TCS.");
-			}
+        private void OnIsDialogOpenChanged(bool obj)
+        {
+			// Trigger when closed abruptly (via the Overlay or the back button).
+            if (!obj & CurrentTaskCompletionSource is { } & !DialogReturnedWithValue)
+            {
+				 Close();
+            }
+        }
 
-			CurrentTaskCompletionSource.SetResult(value);
-			CurrentTaskCompletionSource = null;
+        private TaskCompletionSource<TResult> CurrentTaskCompletionSource { get; set; }
 
-			CloseDialog();
-		}
+        /// <summary>
+        /// Method to be called when the dialog intends to close
+        /// without returning a value.
+        /// </summary>
+        protected void Close()
+        {
+            if (CurrentTaskCompletionSource is null)
+            {
+                throw new InvalidOperationException("CloseDialog with value return method failed due to missing TCS.");
+            }
 
-		/// <summary>
-		/// Shows the dialog.
-		/// </summary>
-		/// <returns>The value to be returned when the dialog is finished.</returns>
-		public Task<TResult> ShowDialogAsync(IDialogHost host)
-		{
-			host.CurrentDialog = this;
+            CurrentTaskCompletionSource.SetResult(default);
+            CurrentTaskCompletionSource = null;
+            IsDialogOpen = false;
 
-			if (CurrentTaskCompletionSource is { })
-			{
-				throw new InvalidOperationException("Can't open a new dialog since there's already one active.");
-			}
+            OnDialogClosed();
+        }
 
-			CurrentTaskCompletionSource = new TaskCompletionSource<TResult>();
- 
-			IsDialogOpen = true;
+        /// <summary>
+        /// Method to be called when the dialog intends to close
+        /// and ready to pass a value back to the caller.
+        /// </summary>
+        /// <param name="value">The return value of the dialog</param>
+        protected void Close(TResult value)
+        {
+            if (CurrentTaskCompletionSource is null)
+            {
+                throw new InvalidOperationException("CloseDialog with value return method failed due to missing TCS.");
+            }
 
-			return CurrentTaskCompletionSource.Task;
-		}
-	}
+            CurrentTaskCompletionSource.SetResult(value);
+            CurrentTaskCompletionSource = null;
+            DialogReturnedWithValue = true;
+            IsDialogOpen = false;
+
+            OnDialogClosed();
+        }
+
+        /// <summary>
+        /// Shows the dialog.
+        /// </summary>
+        /// <returns>The value to be returned when the dialog is finished.</returns>
+        public Task<TResult> ShowDialogAsync(IDialogHost host)
+        {
+            if (CurrentTaskCompletionSource is { })
+            {
+                throw new InvalidOperationException("Can't open a new dialog since there's already one active.");
+            }
+
+            CurrentTaskCompletionSource = new TaskCompletionSource<TResult>();
+
+            host.CurrentDialog = this;
+            IsDialogOpen = true;
+            DialogReturnedWithValue = false;
+
+            return CurrentTaskCompletionSource.Task;
+        }
+    }
 }
