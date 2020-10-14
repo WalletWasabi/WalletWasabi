7703ec8de2950407cef62cb5c3ef936dbb15ef63
Roland Soós <soos.roland93@gmail.com>
2020-10-14 12:04:56 +0200

    change theme button

--- WalletWasabi.Fluent/ViewModels/SettingsPageViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/SettingsPageViewModel.cs
@@ -1,5 +1,8 @@
+using System;
 using ReactiveUI;
 using System.Windows.Input;
+using Avalonia;
+using Avalonia.Markup.Xaml.Styling;
 
 namespace WalletWasabi.Fluent.ViewModels
 {
@@ -10,9 +13,24 @@ namespace WalletWasabi.Fluent.ViewModels
 			Title = "Settings";
 
 			NextCommand = ReactiveCommand.Create(() => screen.Router.Navigate.Execute(new HomePageViewModel(screen)));
+			ChangeThemeCommand = ReactiveCommand.Create(() =>
+			{
+				var light = new StyleInclude(new Uri("avares://WalletWasabi.Fluent/App.xaml"))
+				{
+					Source = new Uri("avares://WalletWasabi.Fluent/Styles/Themes/BaseLight.xaml")
+				};
+				var dark = new StyleInclude(new Uri("avares://WalletWasabi.Fluent/App.xaml"))
+				{
+					Source = new Uri("avares://WalletWasabi.Fluent/Styles/Themes/BaseDark.xaml")
+				};
+
+				Application.Current.Styles[0] = ((StyleInclude)Application.Current.Styles[0]).Source.AbsolutePath == light.Source.AbsolutePath ? dark : light;
+			});
+
 		}
 
 		public ICommand NextCommand { get; }
+		public ICommand ChangeThemeCommand { get; }
 
 		public override string IconName => "settings_regular";
 	}
