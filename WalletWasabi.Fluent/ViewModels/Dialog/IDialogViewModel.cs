52ccec7f296fb41627a73b11a5b075d82eb09901
Jumar Macato <hikari.netto23@gmail.com>
2020-10-14 18:31:43 +0800

    edit xml summary

--- WalletWasabi.Fluent/ViewModels/Dialog/IDialogViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Dialog/IDialogViewModel.cs
@@ -1,7 +1,13 @@
 namespace WalletWasabi.Fluent.ViewModels.Dialog
 {
+    /// <summary>
+    /// Interface that abstracts <see cref="DialogViewModelBase{TResult}"/>.
+    /// </summary>
     public interface IDialogViewModel
     {
+        /// <summary>
+        /// Gets or sets if the dialog is opened/closed.
+        /// </summary>
         bool IsDialogOpen { get; set; }
     }
 }
