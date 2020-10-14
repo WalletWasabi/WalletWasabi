a3c7514ff45ddae40de4a081a045a7a07ef7e568
Dan Walmsley <dan@walms.co.uk>
2020-10-14 13:40:39 +0100

    fix code factor.

--- WalletWasabi.Fluent/ViewModels/Dialogs/IDialogHost.cs
+++ WalletWasabi.Fluent/ViewModels/Dialogs/IDialogHost.cs
@@ -1,9 +1,9 @@
 namespace WalletWasabi.Fluent.ViewModels.Dialogs
 {
-    /// <summary>
-    /// Interface for ViewModels that can host a modal dialog.
-    /// </summary>
-    public interface IDialogHost
+	/// <summary>
+	/// Interface for ViewModels that can host a modal dialog.
+	/// </summary>
+	public interface IDialogHost
 	{
 		/// <summary>
 		/// The currently active dialog. The modal dialog UI should close when this is null.
