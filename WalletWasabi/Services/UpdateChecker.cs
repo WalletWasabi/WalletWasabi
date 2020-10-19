5742e0f4fc8e516b328a4e160619829b026ba263
Yahia Chiheb <chihebyahia@gmail.com>
2020-10-19 18:54:43 +0100

    Remove unused using

--- WalletWasabi/Services/UpdateChecker.cs
+++ WalletWasabi/Services/UpdateChecker.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.Bases;
-using WalletWasabi.Helpers;
 using WalletWasabi.Models;
 using WalletWasabi.WebClients.Wasabi;
 
