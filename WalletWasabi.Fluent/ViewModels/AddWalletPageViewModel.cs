f90cb2a0096658d42933281e19d79d51f37a204a
Dan Walmsley <dan@walms.co.uk>
2020-10-22 15:39:47 +0100

    add walletname property.

--- WalletWasabi.Fluent/ViewModels/AddWalletPageViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/AddWalletPageViewModel.cs
@@ -1,14 +1,24 @@
+using Avalonia.Input;
 using ReactiveUI;
+using WalletWasabi.Gui.Validation;
 
 namespace WalletWasabi.Fluent.ViewModels
 {
 	public class AddWalletPageViewModel : NavBarItemViewModel
 	{
+		private string _walletName = "";
+
 		public AddWalletPageViewModel(IScreen screen) : base(screen)
 		{
 			Title = "Add Wallet";
 		}
 
-		public override string IconName => "add_circle_regular";
+		public override string IconName => "add_circle_regular";		
+
+		public string WalletName
+		{
+			get => _walletName;
+			set => this.RaiseAndSetIfChanged(ref _walletName, value);
+		}
 	}
 }
