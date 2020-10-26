2dd940e06068dc4bf6d7b26c74969007d99ec19c
nopara73 <adam.ficsor73@gmail.com>
2020-10-26 15:22:24 +0100

    No need for Tor identities for non-coinjoins.

--- WalletWasabi/WebClients/Wasabi/WasabiClientFactory.cs
+++ WalletWasabi/WebClients/Wasabi/WasabiClientFactory.cs
@@ -30,28 +30,15 @@ namespace WalletWasabi.WebClients.Wasabi
 		public bool IsTorEnabled => TorEndpoint is { };
 
 		/// <summary>
-		/// Creates a new instance of <see cref="WasabiClient"/> which is predefined with <paramref name="baseUri"/> parameter.
-		/// </summary>
-		/// <remarks>New instances are better to increase privacy level.</remarks>
-		/// <param name="baseUri">Base URI used in all HTTP requests created by <see cref="WasabiClient"/> class.</param>
-		/// <param name="isolateStream">Whether new Tor identity should be used for all HTTP requests.</param>
-		public WasabiClient Create(Uri baseUri, bool isolateStream = true)
-		{
-			TorHttpClient torHttpClient = new TorHttpClient(baseUri, TorEndpoint, isolateStream);
-			return new WasabiClient(torHttpClient);
-		}
-
-		/// <summary>
 		/// Creates new <see cref="WasabiClient"/> instance with correctly set backend URL.
 		/// </summary>
 		/// <remarks>
 		/// For privacy reasons, it is advisable to create a new instance of <see cref="WasabiClient"/>
 		/// for each HTTP request instead of reusing one instance for multiple HTTP requests.
 		/// </remarks>
-		/// <param name="isolateStream">Whether a new Tor identity should be used for this <see cref="WasabiClient"/> instance.</param>
-		public WasabiClient NewBackendClient(bool isolateStream = true)
+		public WasabiClient NewBackendClient()
 		{
-			TorHttpClient torHttpClient = new TorHttpClient(BackendUriGetter.Invoke(), TorEndpoint, isolateStream);
+			TorHttpClient torHttpClient = new TorHttpClient(BackendUriGetter.Invoke(), TorEndpoint, isolateStream: false);
 			return new WasabiClient(torHttpClient);
 		}
 	}
