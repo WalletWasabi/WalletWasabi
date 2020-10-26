b66ae8a00d7046c2ed0a91734806dd1380195331
Yahia Chiheb <chihebyahia@gmail.com>
2020-10-26 16:42:46 +0100

    Place field before constructor

--- WalletWasabi/Tor/Socks5/Models/Fields/OctetFields/AuthStatusField.cs
+++ WalletWasabi/Tor/Socks5/Models/Fields/OctetFields/AuthStatusField.cs
@@ -4,12 +4,12 @@ namespace WalletWasabi.Tor.Socks5.Models.Fields.OctetFields
 {
 	public class AuthStatusField : OctetSerializableBase
 	{
+		public static AuthStatusField Success = new AuthStatusField(0x00);
+
 		public AuthStatusField(byte value)
 		{
 			ByteValue = value;
 		}
-
-		public static AuthStatusField Success = new AuthStatusField(0x00);
 
 		public bool IsSuccess() => ByteValue == Success.ByteValue;
 	}
