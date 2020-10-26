61a5aefc0b1b95f31c050dfb8d87402fcc114d10
Yahia Chiheb <chihebyahia@gmail.com>
2020-10-26 16:55:40 +0100

    Refactor code

--- WalletWasabi/Tor/Socks5/Models/Fields/OctetFields/AuthVerField.cs
+++ WalletWasabi/Tor/Socks5/Models/Fields/OctetFields/AuthVerField.cs
@@ -1,21 +1,15 @@
-using WalletWasabi.Helpers;
 using WalletWasabi.Tor.Socks5.Models.Bases;
 
 namespace WalletWasabi.Tor.Socks5.Models.Fields.OctetFields
 {
 	public class AuthVerField : OctetSerializableBase
 	{
+		public static AuthVerField Version1 = new AuthVerField(0x01);
+
 		public AuthVerField(byte value)
 		{
 			ByteValue = value;
 		}
-
-		public AuthVerField(int value)
-		{
-			ByteValue = (byte)Guard.InRangeAndNotNull(nameof(value), value, 0, 255);
-		}
-
-		public static AuthVerField Version1 => new AuthVerField(1);
 
 		public int Value => ByteValue;
 	}
