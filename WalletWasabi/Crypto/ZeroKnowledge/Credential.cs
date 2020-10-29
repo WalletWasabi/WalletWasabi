1dc8f17c010bcc67fa4826ab308fa2bbefa93ad6
lontivero <lucasontivero@gmail.com>
2020-10-29 15:19:40 -0300

    More documentation

--- WalletWasabi/Crypto/ZeroKnowledge/Credential.cs
+++ WalletWasabi/Crypto/ZeroKnowledge/Credential.cs
@@ -3,8 +3,17 @@ using WalletWasabi.Crypto.Groups;
 
 namespace WalletWasabi.Crypto.ZeroKnowledge
 {
+	/// <summary>
+	/// Represents an anonymous credential and its represented data.
+	/// </summary>
 	public class Credential
 	{
+		/// <summary>
+		/// Initializes a new Credential instance.
+		/// </summary>
+		/// <param name="amount">The amount represented by the credential.</param>
+		/// <param name="randomness">The randomness used as blinding factor in the pedersen committed amount.</param>
+		/// <param name="mac">The algebraic MAC represented the anonymous credential issued by the coordinator.</param>
 		public Credential(Scalar amount, Scalar randomness, MAC mac)
 		{
 			Amount = amount;
@@ -12,11 +21,27 @@ namespace WalletWasabi.Crypto.ZeroKnowledge
 			Mac = mac;
 		}
 
+		/// <summary>
+		/// Amount in represented by the credential.
+		/// </summary>
 		public Scalar Amount { get; }
+
+		/// <summary>
+		/// Randomness used as blinding factor for the pedersen committed Amount.
+		/// </summary>
 		public Scalar Randomness { get; }
+
+		/// <summary>
+		/// Algebraic MAC represented the anonymous credential issued by the coordinator.
+		/// </summary>
 		public MAC Mac { get; }
 
-		public CredentialPresentation Present(Scalar z)
+		/// <summary>
+		/// Randomizes the credential using a randomization scalar z.
+		/// </summary>
+		/// <param name="z">The randomization scalar.</param>
+		/// <returns>A randomized credential ready to be presented to the coordinator.</returns>
+		internal CredentialPresentation Present(Scalar z)
 		{
 			GroupElement Randomize(GroupElement G, GroupElement M) => M + z * G;
 			return new CredentialPresentation(
