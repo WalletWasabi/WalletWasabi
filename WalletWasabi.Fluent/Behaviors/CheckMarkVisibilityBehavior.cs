c725cbda43a83e62fd227fe788b24b6c17c2e585
Roland Soós <soos.roland93@gmail.com>
2020-10-30 11:48:18 +0100

    POC checkmark visibility

---
+++ WalletWasabi.Fluent/Behaviors/CheckMarkVisibilityBehavior.cs
@@ -0,0 +1,67 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace WalletWasabi.Fluent.Behaviors
+{
+	public class CheckMarkVisibilityBehavior : AttachedToVisualTreeBehavior<PathIcon>
+	{
+		public static readonly StyledProperty<bool> HasErrorsProperty =
+			AvaloniaProperty.Register<CheckMarkVisibilityBehavior, bool>(nameof(HasErrors), false);
+
+		public static readonly StyledProperty<bool> IsFocusedProperty =
+			AvaloniaProperty.Register<CheckMarkVisibilityBehavior, bool>(nameof(IsFocused), false);
+
+		public static readonly StyledProperty<string> TextProperty =
+			AvaloniaProperty.Register<CheckMarkVisibilityBehavior, string>(nameof(Text), "");
+
+		public bool HasErrors
+		{
+			get => GetValue(HasErrorsProperty);
+			set => SetValue(HasErrorsProperty, value);
+		}
+
+		public bool IsFocused
+		{
+			get => GetValue(IsFocusedProperty);
+			set => SetValue(IsFocusedProperty, value);
+		}
+
+		public string Text
+		{
+			get => GetValue(TextProperty);
+			set => SetValue(TextProperty, value);
+		}
+
+		public static readonly StyledProperty<TextBox> OwnerTextBoxProperty =
+			AvaloniaProperty.Register<CheckMarkVisibilityBehavior, TextBox>(nameof(OwnerTextBox), null!);
+
+		public TextBox OwnerTextBox
+		{
+			get => GetValue(OwnerTextBoxProperty);
+			set => SetValue(OwnerTextBoxProperty, value);
+		}
+
+		protected override void OnAttachedToVisualTree()
+		{
+			this.WhenAnyValue(
+				x => x.HasErrors,
+				x => x.IsFocused,
+				x => x.Text)
+				.Subscribe(_ => AssociatedObject!.IsVisible = !HasErrors && !IsFocused && !string.IsNullOrEmpty(Text));
+
+			Observable
+				.FromEventPattern<AvaloniaPropertyChangedEventArgs>(OwnerTextBox, nameof(OwnerTextBox.PropertyChanged))
+				.Select(x => x.EventArgs)
+				.Where(x => x.Property.Name == "HasErrors")
+				.Subscribe(x => HasErrors = (bool)x.NewValue!);
+		}
+	}
+}
