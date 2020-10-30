0d6b5abe2d9f259b3484d1f8691280963fcb6b84
Biig <bandi.istvan@gmail.com>
2020-10-30 16:08:52 +0100

    Test app ready

--- WindowsFormsTest/Form1.Designer.cs
+++ WindowsFormsTest/Form1.Designer.cs
@@ -30,37 +30,93 @@ namespace WindowsFormsTest
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.BtnQRCodeDetection = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbContinous = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Location = new System.Drawing.Point(18, 19);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(611, 388);
+            this.pictureBox1.Size = new System.Drawing.Size(784, 479);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(668, 19);
+            this.button1.Location = new System.Drawing.Point(21, 35);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(109, 54);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Capture";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // BtnQRCodeDetection
+            // 
+            this.BtnQRCodeDetection.Location = new System.Drawing.Point(21, 107);
+            this.BtnQRCodeDetection.Name = "BtnQRCodeDetection";
+            this.BtnQRCodeDetection.Size = new System.Drawing.Size(109, 54);
+            this.BtnQRCodeDetection.TabIndex = 1;
+            this.BtnQRCodeDetection.Text = "QR code detection";
+            this.BtnQRCodeDetection.UseVisualStyleBackColor = true;
+            this.BtnQRCodeDetection.Click += new System.EventHandler(this.BtnQRCodeDetection_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // cbContinous
+            // 
+            this.cbContinous.AutoSize = true;
+            this.cbContinous.Location = new System.Drawing.Point(21, 227);
+            this.cbContinous.Name = "cbContinous";
+            this.cbContinous.Size = new System.Drawing.Size(79, 19);
+            this.cbContinous.TabIndex = 3;
+            this.cbContinous.Text = "continous";
+            this.cbContinous.UseVisualStyleBackColor = true;
+            this.cbContinous.CheckedChanged += new System.EventHandler(this.CbContinous_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.cbContinous);
+            this.groupBox1.Controls.Add(this.BtnQRCodeDetection);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(823, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 486);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1065, 517);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +125,10 @@ namespace WindowsFormsTest
 
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button BtnQRCodeDetection;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.CheckBox cbContinous;
+		private System.Windows.Forms.GroupBox groupBox1;
 	}
 }
 
--- WindowsFormsTest/Form1.cs
+++ WindowsFormsTest/Form1.cs
@@ -9,36 +9,68 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using System.Windows.Forms;
 
 namespace WindowsFormsTest
 {
 	public partial class Form1 : Form
 	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-
 		private Emgu.CV.Image<Rgb, byte> _testCvImage;
 		private VideoCapture _videocapture;
 		private Mat _frame;
+		private QRCodeDetector _qrCodeDetector;
+		private Timer _timer;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			_qrCodeDetector = new Emgu.CV.QRCodeDetector();
+			_frame = new Mat();
+			_videocapture = new VideoCapture();
+			_videocapture.ImageGrabbed += VideocaptureImageGrabbed;
+			_timer = new Timer();
+			_timer.Tick += Timer_Tick;
+			_timer.Interval = 100;
+		}
+
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			_videocapture.Grab();
+		}
+
+		private void VideocaptureImageGrabbed(object sender, EventArgs e)
+		{
+			VideoCapture videocapture = (VideoCapture)sender;
+
+			if (_videocapture != null && _videocapture.Ptr != IntPtr.Zero)
+			{
+				videocapture.Retrieve(_frame, 0);
+				_testCvImage = _frame.ToImage<Rgb, byte>();
+
+				MethodInvoker methodInvokerDelegate = delegate ()
+				{
+					pictureBox1.Image = _testCvImage.ToBitmap();
+
+					//_testCvImage.Save(@"c:\image\lastImage.png");
+					label1.Text = GetQRcodeValueFromImage(_testCvImage);
+				};
+
+				if (InvokeRequired)
+				{
+					Invoke(methodInvokerDelegate);
+				}
+				else
+				{
+					methodInvokerDelegate();
+				}
+			}
+		}
 
 		private void Button1_Click(object sender, EventArgs e)
 		{
-			pictureBox1.Image.Save(@"c:\a.png");
-			bool succ;
-
-			_frame = new Mat();
-			using (_videocapture = new VideoCapture())
-			{
-				_videocapture.ImageGrabbed += ProcessFrame;
-				succ = _videocapture.IsOpened;
-				succ = _videocapture.Grab();
-			}
-			//var a = _videocapture.QueryFrame();
-
-			//_videocapture.Start();
+			_videocapture.Grab();
 		}
 
 		private void ProcessFrame(object? sender, EventArgs e)
@@ -51,6 +83,63 @@ namespace WindowsFormsTest
 
 				_testCvImage = _frame.ToImage<Rgb, byte>();
 				pictureBox1.Image = _testCvImage.ToBitmap();
+				//pictureBox1.Image.Save(@"c:\image\a.png");
+
+				label1.Text = GetQRcodeValueFromImage(_testCvImage);
+			}
+		}
+
+		private void BtnQRCodeDetection_Click(object sender, EventArgs e)
+		{
+			Image<Gray, byte> img = new Image<Emgu.CV.Structure.Gray, byte>(@"c:\image\lastImage1.png");
+			Image<Gray, byte> img2 = new Image<Gray, byte>(25, 25);
+			Bitmap temp = img.ToBitmap();
+			//temp.Save(@"c:\image\temp.bmp");
+			GetQRcodeValueFromImage(img);
+		}
+
+		private object _lockObject = new object();
+		private bool _isLocked = false;
+
+		private string GetQRcodeValueFromImage(IInputArray image)
+		{
+			string qrCodeStr = "NA";
+
+			if (_isLocked)
+			{
+				return qrCodeStr;
+			}
+
+			lock (_lockObject)
+			{
+				_isLocked = true;
+
+				bool succ = false;
+
+				IOutputArray result = new Mat();
+				IOutputArray resultImg = new Mat();
+				succ = _qrCodeDetector.Detect(image, result);
+				if (succ)
+				{
+					qrCodeStr = _qrCodeDetector.Decode(image, result, resultImg);
+				}
+				//resultImg.Save(@"c:\image\decodeResultImage.bmp");
+
+				_isLocked = false;
+			}
+
+			return qrCodeStr;
+		}
+
+		private void CbContinous_CheckedChanged(object sender, EventArgs e)
+		{
+			if (cbContinous.Checked)
+			{
+				_timer.Start();
+			}
+			else
+			{
+				_timer.Stop();
 			}
 		}
 	}
