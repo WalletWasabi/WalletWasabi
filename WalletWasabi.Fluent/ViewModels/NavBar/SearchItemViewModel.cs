54c72db118edcb37908c95b685c8225cf8001c9b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-11-09 22:06:26 +0100

    Add search item view model

---
+++ WalletWasabi.Fluent/ViewModels/NavBar/SearchItemViewModel.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace WalletWasabi.Fluent.ViewModels.NavBar
+{
+	public class SearchItemViewModel : RoutableViewModel
+	{
+		public SearchItemViewModel(NavigationStateViewModel navigationState, NavigationTarget navigationTarget, string iconName, string title, Func<RoutableViewModel> createTargetView) : base(navigationState, navigationTarget)
+		{
+			IconName = iconName;
+
+			Title = title;
+
+			OpenCommand = ReactiveCommand.Create(() =>
+			{
+				NavigateToTargetView(navigationState, navigationTarget, createTargetView);
+			});
+		}
+
+		public string IconName { get; }
+
+		public string Title { get; }
+
+		public ICommand OpenCommand { get; }
+
+		private void NavigateToTargetView(NavigationStateViewModel navigationState, NavigationTarget navigationTarget, Func<RoutableViewModel> createTargetView)
+		{
+			switch (navigationTarget)
+			{
+				case NavigationTarget.Default:
+				case NavigationTarget.Home:
+					navigationState.HomeScreen?.Invoke().Router.Navigate.Execute(createTargetView());
+					break;
+
+				case NavigationTarget.Dialog:
+					navigationState.DialogScreen?.Invoke().Router.Navigate.Execute(createTargetView());
+					break;
+			}
+		}
+
+		public override string ToString()
+		{
+			return Title;
+		}
+	}
+}
