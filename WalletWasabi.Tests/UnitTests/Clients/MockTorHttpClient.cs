cfcee012432c4c2f890627bf3514d3322293358f
Kiminuo <kiminuo@protonmail.com>
2020-11-13 21:32:14 +0100

    MockTorHttpClient: Use IRelativeHttpClient.

--- WalletWasabi.Tests/UnitTests/Clients/MockTorHttpClient.cs
+++ WalletWasabi.Tests/UnitTests/Clients/MockTorHttpClient.cs
@@ -4,11 +4,11 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
-using WalletWasabi.Tor.Http.Interfaces;
+using WalletWasabi.Tor.Http;
 
 namespace WalletWasabi.Tests.UnitTests.Clients
 {
-	public class MockTorHttpClient : ITorHttpClient
+	public class MockTorHttpClient : IRelativeHttpClient
 	{
 		public Func<Uri> DestinationUriAction => () => new Uri("https://payment.server.org/pj");
 
