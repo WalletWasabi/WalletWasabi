aff74a7a927e43637ff860d9aa7dcfe77ca7bd76
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2020-11-16 13:54:17 +0000

    Move to user control

---
+++ WalletWasabi.Fluent/Views/NavBar/NavBarSearch.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Views.NavBar
+{
+	public class NavBarSearch : UserControl
+	{
+		public NavBarSearch()
+		{
+			InitializeComponent();
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
