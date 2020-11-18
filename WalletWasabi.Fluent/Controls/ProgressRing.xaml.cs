666beddf966ee20d1a4e40c2a2745776a41f5527
Jumar Macato <hikari.netto23@gmail.com>
2020-11-18 15:36:47 +0800

    add progressring

---
+++ WalletWasabi.Fluent/Controls/ProgressRing.xaml.cs
@@ -0,0 +1,11 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace WalletWasabi.Fluent.Controls
+{
+	public class ProgressRing : TemplatedControl
+	{
+
+	}
+}
