71d14cc171cebc0d79629898c4984a98b128753d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-11-19 20:03:57 +0100

    Close all dialogs so the awaited tasks can complete

--- WalletWasabi.Fluent/ViewModels/Dialogs/DialogScreenViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Dialogs/DialogScreenViewModel.cs
@@ -49,6 +49,16 @@ namespace WalletWasabi.Fluent.ViewModels.Dialogs
 				_isClosing = true;
 				if (Router.NavigationStack.Count >= 1)
 				{
+					foreach (var routable in Router.NavigationStack)
+					{
+						// Close all dialogs so the awaited tasks can complete.
+						// - DialogViewModelBase.ShowDialogAsync()
+						// - DialogViewModelBase.GetDialogResultAsync()
+						if (routable is DialogViewModelBase dialog)
+						{
+							dialog.IsDialogOpen = false;
+						}
+					}
 					Router.NavigationStack.Clear();
 					IsDialogOpen = false;
 				}
