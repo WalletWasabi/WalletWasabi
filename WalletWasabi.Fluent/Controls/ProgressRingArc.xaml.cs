a86dee68466efc7c62e694d5e68410254699a143
Jumar Macato <hikari.netto23@gmail.com>
2020-11-20 22:43:28 +0800

    add default thickness

--- WalletWasabi.Fluent/Controls/ProgressRingArc.xaml.cs
+++ WalletWasabi.Fluent/Controls/ProgressRingArc.xaml.cs
@@ -20,7 +20,7 @@ namespace WalletWasabi.Fluent.Controls
 			AvaloniaProperty.Register<ProgressRingArc, IBrush>(nameof(SegmentColor));
 
 		public static readonly StyledProperty<int> StrokeThicknessProperty =
-			AvaloniaProperty.Register<ProgressRingArc, int>(nameof(StrokeThickness));
+			AvaloniaProperty.Register<ProgressRingArc, int>(nameof(StrokeThickness), 15);
 
 		public static readonly StyledProperty<double> PercentageProperty =
 			AvaloniaProperty.Register<ProgressRingArc, double>(
