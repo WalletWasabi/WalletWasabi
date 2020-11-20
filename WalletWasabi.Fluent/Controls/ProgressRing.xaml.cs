8234df2d1b972689347ca1ca0c54cb54adb3744d
Jumar Macato <hikari.netto23@gmail.com>
2020-11-20 13:11:45 +0800

    properly size up and add to settings page

--- WalletWasabi.Fluent/Controls/ProgressRing.xaml.cs
+++ WalletWasabi.Fluent/Controls/ProgressRing.xaml.cs
@@ -6,6 +6,51 @@ namespace WalletWasabi.Fluent.Controls
 {
 	public class ProgressRing : TemplatedControl
 	{
+		public static readonly StyledProperty<bool> IsIndeterminateProperty =
+			AvaloniaProperty.Register<ProgressRing, bool>("IsIndeterminate");
 
+		public static readonly StyledProperty<double> PercentageProperty =
+			AvaloniaProperty.Register<ProgressRing, double>("Percentage");
+
+		public static readonly StyledProperty<double> AngleProperty =
+			AvaloniaProperty.Register<ProgressRing, double>("Angle");
+
+		public static readonly StyledProperty<double> StrokeThicknessProperty =
+			AvaloniaProperty.Register<ProgressRing, double>("StrokeThickness");
+
+		protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+		{
+			base.OnPropertyChanged(change);
+			UpdatePseudoClasses();
+		}
+
+		private void UpdatePseudoClasses()
+		{
+			PseudoClasses.Set(":indeterminate", IsIndeterminate);
+		}
+
+		public bool IsIndeterminate
+		{
+			get => GetValue(IsIndeterminateProperty);
+			set => SetValue(IsIndeterminateProperty, value);
+		}
+
+		public double Percentage
+		{
+			get => GetValue(PercentageProperty);
+			set => SetValue(PercentageProperty, value);
+		}
+
+		public double Angle
+		{
+			get => GetValue(AngleProperty);
+			set => SetValue(AngleProperty, value);
+		}
+
+		public double StrokeThickness
+		{
+			get => GetValue(StrokeThicknessProperty);
+			set => SetValue(StrokeThicknessProperty, value);
+		}
 	}
 }
