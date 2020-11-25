4e12353353f55d478f663ace880914dbdfaab99f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2020-11-25 18:42:27 +0100

    Added AppViewModel

---
+++ WalletWasabi.Fluent/ViewModels/AppViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using WalletWasabi.Gui.ViewModels;
+
+namespace WalletWasabi.Fluent.ViewModels
+{
+	public class AppViewModel : ViewModelBase
+	{
+		public static IObservable<bool>? PrivacyMode { get; set; }
+	}
+}
