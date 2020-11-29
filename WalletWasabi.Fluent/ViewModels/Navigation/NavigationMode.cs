b5077469a0576e68e76994478858d0c1630b8941
Dan Walmsley <dan@walms.co.uk>
2020-11-29 23:26:01 +0000

    code factor.

--- WalletWasabi.Fluent/ViewModels/Navigation/NavigationMode.cs
+++ WalletWasabi.Fluent/ViewModels/Navigation/NavigationMode.cs
@@ -8,16 +8,6 @@ namespace WalletWasabi.Fluent.ViewModels.Navigation
 		Normal,
 
 		/// <summary>
-		/// Navigates to another page without changing the stack.
-		/// </summary>
-		//Skip,
-
-		/// <summary>
-		/// Navigates to another page replacing the current page on the stack.
-		/// </summary>
-		//Swap,
-
-		/// <summary>
 		/// Navigates to another page and clears the stack. The back button will not be available after this.
 		/// </summary>
 		Clear
