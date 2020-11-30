c19298d71c031d1b1b01da5787a05bfbb47459aa
Roland Soós <soos.roland93@gmail.com>
2020-11-30 16:32:47 +0100

    load transaction

---
+++ WalletWasabi.Fluent/Helpers/FileDialogHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+
+namespace WalletWasabi.Fluent.Helpers
+{
+	public static class FileDialogHelper
+	{
+		public static async Task<string?> ShowOpenFileDialogAsync(string title)
+		{
+			var ofd = new OpenFileDialog
+			{
+				AllowMultiple = false,
+				Title = title,
+				Directory = GetDefaultDirectory(),
+			};
+
+			var window = ((IClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime).MainWindow;
+			var selected = await ofd.ShowAsync(window);
+
+			return selected.FirstOrDefault();
+		}
+
+		private static string GetDefaultDirectory()
+		{
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				return Path.Combine("/media", Environment.UserName);
+			}
+
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				return Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			}
+
+			return Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
+		}
+	}
+}
--- WalletWasabi.Fluent/ViewModels/TransactionBroadcaster/TransactionBroadcasterViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/TransactionBroadcaster/TransactionBroadcasterViewModel.cs
@@ -1,14 +1,154 @@
-﻿using WalletWasabi.Fluent.ViewModels.Navigation;
+﻿using System;
+using System.IO;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Avalonia;
+using NBitcoin;
+using ReactiveUI;
+using Splat;
+using WalletWasabi.Blockchain.Transactions;
+using WalletWasabi.Fluent.Helpers;
+using WalletWasabi.Fluent.ViewModels.Navigation;
+using WalletWasabi.Gui;
+using WalletWasabi.Gui.Controls.TransactionDetails.ViewModels;
+using WalletWasabi.Gui.Helpers;
+using WalletWasabi.Logging;
+using WalletWasabi.Stores;
 
 namespace WalletWasabi.Fluent.ViewModels.TransactionBroadcaster
 {
 	public class TransactionBroadcasterViewModel : RoutableViewModel
 	{
+		private SmartTransaction? _finalTransaction;
+
 		public TransactionBroadcasterViewModel()
 		{
+			var global = Locator.Current.GetService<Global>();
 
+			Network = global.Network;
+			BitcoinStore = global.BitcoinStore;
+
+			this.WhenAnyValue(x => x.FinalTransaction)
+				.Where(x => x is { })
+				.ObserveOn(RxApp.MainThreadScheduler)
+				.Subscribe(x =>
+				{
+					try
+					{
+						var res =  TransactionDetailsViewModel.FromBuildTxnResult(BitcoinStore, PSBT.FromTransaction(x!.Transaction, Network));
+					}
+					catch (Exception ex)
+					{
+						// TODO: Notify the user
+						Logger.LogError(ex);
+					}
+				});
+
+			ImportTransactionCommand = ReactiveCommand.CreateFromTask(
+				async () =>
+				{
+					try
+					{
+						var path = await FileDialogHelper.ShowOpenFileDialogAsync("Import Transaction");
+						if (path is { })
+						{
+							FinalTransaction = await ParseTransactionAsync(path);
+						}
+					}
+					catch (Exception ex)
+					{
+						Logger.LogError(ex);
+					}
+				},
+				outputScheduler: RxApp.MainThreadScheduler);
+
+			PasteCommand = ReactiveCommand.CreateFromTask(async () =>
+			{
+				try
+				{
+					var textToPaste = await Application.Current.Clipboard.GetTextAsync();
+
+					if (string.IsNullOrWhiteSpace(textToPaste))
+					{
+						// TODO: Clipboard is empty message
+						return;
+					}
+
+					if (PSBT.TryParse(textToPaste, Network, out var signedPsbt))
+					{
+						if (!signedPsbt.IsAllFinalized())
+						{
+							signedPsbt.Finalize();
+						}
+
+						FinalTransaction = signedPsbt.ExtractSmartTransaction();
+					}
+					else
+					{
+						FinalTransaction = new SmartTransaction(Transaction.Parse(textToPaste, Network), Models.Height.Unknown);
+					}
+				}
+				catch (Exception ex)
+				{
+					// TODO: Notify the user about the error
+					Logger.LogError(ex);
+				}
+			});
 		}
 
+		public BitcoinStore BitcoinStore { get; }
+
+		public SmartTransaction? FinalTransaction
+		{
+			get => _finalTransaction;
+			set => this.RaiseAndSetIfChanged(ref _finalTransaction, value);
+		}
+
+		private Network Network { get; }
+
 		public override NavigationTarget DefaultTarget => NavigationTarget.DialogScreen;
+
+		public ICommand PasteCommand { get; }
+
+		public ICommand ImportTransactionCommand { get; }
+
+		private async Task<SmartTransaction> ParseTransactionAsync(string path)
+		{
+			var psbtBytes = await File.ReadAllBytesAsync(path);
+			PSBT psbt = null;
+			Transaction transaction = null;
+			try
+			{
+				psbt = PSBT.Load(psbtBytes, Network);
+			}
+			catch
+			{
+				var text = await File.ReadAllTextAsync(path);
+				text = text.Trim();
+				try
+				{
+					psbt = PSBT.Parse(text, Network);
+				}
+				catch
+				{
+					transaction = Transaction.Parse(text, Network);
+				}
+			}
+
+			if (psbt is { })
+			{
+				if (!psbt.IsAllFinalized())
+				{
+					psbt.Finalize();
+				}
+
+				return psbt.ExtractSmartTransaction();
+			}
+			else
+			{
+				return new SmartTransaction(transaction, Models.Height.Unknown);
+			}
+		}
 	}
 }
