ef62eba212a50c1ac21cb06e60f55496dfbb0adc
Roland Soós <soos.roland93@gmail.com>
2020-12-01 13:10:35 +0100

    fix namespace

--- WalletWasabi.Fluent/Views/AddWallet/Create/RecoveryWordsView.axaml.cs
+++ WalletWasabi.Fluent/Views/AddWallet/Create/RecoveryWordsView.axaml.cs
@@ -1,7 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace WalletWasabi.Fluent.Views.AddWallet
+namespace WalletWasabi.Fluent.Views.AddWallet.Create
 {
 	public class RecoveryWordsView : UserControl
 	{
