38b924e55eb69d1874304d85276e849f40f7cd51
Roland Soós <soos.roland93@gmail.com>
2020-12-01 11:35:41 +0100

    first attempt to display values

---
+++ WalletWasabi.Fluent/Converters/MoneyConverters.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+using NBitcoin;
+
+namespace WalletWasabi.Fluent.Converters
+{
+	public static class MoneyConverters
+	{
+		public static readonly IValueConverter MoneyToString =
+			new FuncValueConverter<Money, string>(x =>
+			{
+				return x switch
+				{
+					null => "Unknown",
+					{ } => x.ToString(fplus: false, trimExcessZero: true),
+				};
+			});
+
+		public static readonly IValueConverter MoneyToBrush =
+			new FuncValueConverter<Money, ISolidColorBrush>(x => x.ToDecimal(MoneyUnit.BTC) < 0 ? Brushes.IndianRed : Brushes.MediumSeaGreen);
+	}
+}
