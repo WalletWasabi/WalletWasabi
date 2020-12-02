7d9b44b50c2fa349169fff8fc0a3c8e766211ec0
Kiminuo <kiminuo@protonmail.com>
2020-12-02 13:17:39 +0100

    Add new test.

--- WalletWasabi.Tests/UnitTests/Tor/Socks5/Utils/TorHttpRequestMessageSerializerTests.cs
+++ WalletWasabi.Tests/UnitTests/Tor/Socks5/Utils/TorHttpRequestMessageSerializerTests.cs
@@ -1,4 +1,5 @@
 using System.Net.Http;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.Tor.Socks5.Utils;
@@ -12,12 +13,26 @@ namespace WalletWasabi.Tests.UnitTests.Tor.Socks5.Utils
 	public class TorHttpRequestMessageSerializerTests
 	{
 		[Fact]
-		public async Task BasicTestAsync()
+		public async Task GetTestAsync()
 		{
 			using HttpRequestMessage request = new(HttpMethod.Get, "https://postman-echo.com");
 			string plaintext = await TorHttpRequestMessageSerializer.ToStringAsync(request, CancellationToken.None);
 
 			Assert.Equal("GET / HTTP/1.1\r\nHost:postman-echo.com\r\n\r\n", plaintext);
+		}
+
+		[Fact]
+		public async Task PostTestAsync()
+		{
+			// JSON string with HEX content.
+			using var content = new StringContent(content: @"{""key"": ""value""}", Encoding.UTF8, "application/json");
+			using HttpRequestMessage request = new(HttpMethod.Post, "https://postman-echo.com");
+			request.Content = content;
+
+			string actualPlaintext = await TorHttpRequestMessageSerializer.ToStringAsync(request, CancellationToken.None);
+			string expected = "POST / HTTP/1.1\r\nHost:postman-echo.com\r\nContent-Type:application/json; charset=utf-8\r\nContent-Length:16\r\n\r\n{\"key\": \"value\"}";
+
+			Assert.Equal(expected, actualPlaintext);
 		}
 	}
 }
