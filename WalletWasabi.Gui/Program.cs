15a9669a6ffed3d02642cc6445ac04562a86f324
Jumar Macato <hikari.netto23@gmail.com>
2020-12-05 09:51:21 +0800

    revert

--- WalletWasabi.Gui/Program.cs
+++ WalletWasabi.Gui/Program.cs
@@ -116,20 +116,15 @@ namespace WalletWasabi.Gui
 			return executionTask.GetAwaiter().GetResult();
 		}
 
-		private static void SetTheme() =>
-			AvalonStudio.Extensibility.Theme.ColorTheme.LoadTheme(AvalonStudio.Extensibility.Theme.ColorTheme
-				.VisualStudioDark);
+		private static void SetTheme() => AvalonStudio.Extensibility.Theme.ColorTheme.LoadTheme(AvalonStudio.Extensibility.Theme.ColorTheme.VisualStudioDark);
 
 		private static async void AppMainAsync(string[] args)
 		{
 			try
 			{
 				SetTheme();
-				var statusBarViewModel = new StatusBarViewModel(Global.DataDir, Global.Network, Global.Config,
-					Global.HostedServices, Global.BitcoinStore.SmartHeaderChain, Global.Synchronizer,
-					Global.LegalDocuments);
-				MainWindowViewModel.Instance = new MainWindowViewModel(Global.Network, Global.UiConfig,
-					Global.WalletManager, statusBarViewModel, IoC.Get<IShell>());
+				var statusBarViewModel = new StatusBarViewModel(Global.DataDir, Global.Network, Global.Config, Global.HostedServices, Global.BitcoinStore.SmartHeaderChain, Global.Synchronizer, Global.LegalDocuments);
+				MainWindowViewModel.Instance = new MainWindowViewModel(Global.Network, Global.UiConfig, Global.WalletManager, statusBarViewModel, IoC.Get<IShell>());
 
 				await Global.InitializeNoWalletAsync(TerminateService);
 
