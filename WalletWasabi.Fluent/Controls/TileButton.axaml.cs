5be56bf4bc9ef6833603c13aee3aacdc8aeb1965
Roland Soós <soos.roland93@gmail.com>
2020-12-08 10:24:28 +0100

    TileButton control

---
+++ WalletWasabi.Fluent/Controls/TileButton.axaml.cs
@@ -0,0 +1,37 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using WalletWasabi.WebClients.Gemini;
+
+namespace WalletWasabi.Fluent.Controls
+{
+	public class TileButton : Button
+	{
+		public static readonly StyledProperty<int> IconSizeProperty =
+			AvaloniaProperty.Register<TileButton, int>(nameof(IconSize), 80);
+
+		public static readonly StyledProperty<string> TextProperty =
+			AvaloniaProperty.Register<TileButton, string>(nameof(Text));
+
+		public static readonly StyledProperty<Geometry> IconProperty =
+			AvaloniaProperty.Register<TileButton, Geometry>(nameof(Icon));
+
+		public int IconSize
+		{
+			get => GetValue(IconSizeProperty);
+			set => SetValue(IconSizeProperty, value);
+		}
+
+		public string Text
+		{
+			get => GetValue(TextProperty);
+			set => SetValue(TextProperty, value);
+		}
+
+		public Geometry Icon
+		{
+			get => GetValue(IconProperty);
+			set => SetValue(IconProperty, value);
+		}
+	}
+}
