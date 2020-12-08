91b90d10b9b8b1518ee80c7418e44ce4f902340c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2020-12-08 11:03:59 +0000

    Remove old code

--- WalletWasabi.Fluent/ViewLocator.cs
+++ WalletWasabi.Fluent/ViewLocator.cs
@@ -1,6 +1,3 @@
-// Copyright (c) The Avalonia Project. All rights reserved.
-// Licensed under the MIT license. See licence.md file in the project root for full license information.
-
 using System;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
@@ -21,24 +18,6 @@ namespace WalletWasabi.Fluent
 				return func?.Invoke();
 			}
 			throw new Exception($"Unable to create view for type: {type}");
-			/*var name = data.GetType().FullName!.Replace("ViewModel", "View");
-			var type = Type.GetType(name);
-
-			if (type != null)
-			{
-				var result = Activator.CreateInstance(type) as Control;
-
-				if (result is null)
-				{
-					throw new Exception($"Unable to activate type: {type}");
-				}
-
-				return result;
-			}
-			else
-			{
-				return new TextBlock { Text = "Not Found: " + name };
-			}*/
 		}
 
 		public bool Match(object data)
