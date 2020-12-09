5bc87f56e5f92dc0224118cb4d63af2ed143d881
David Molnar <molnardavid84@gmail.com>
2020-12-09 12:24:13 +0100

    Naked PRogram.cs

--- WalletWasabi.Fluent.Desktop/Program.cs
+++ WalletWasabi.Fluent.Desktop/Program.cs
@@ -1,14 +1,7 @@
-namespace WalletWasabi.Fluent.Desktop
-{
-	public class Program
-	{
-		// Initialization code. Don't use any Avalonia, third-party APIs or any
-		// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-		// yet and stuff might break.
-		public static void Main(string[] args)
-		{
-			FluentProgram program = new();
-			program.Run(args);
-		}
-	}
-}
+using WalletWasabi.Fluent.Desktop;
+
+// Initialization code. Don't use any Avalonia, third-party APIs or any
+// SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+// yet and stuff might break.
+FluentProgram program = new();
+program.Run(args);
