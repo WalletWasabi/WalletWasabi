1fae01c98ed8e8746dc5197ab4417e2e43082f32
Kiminuo <kiminuo@protonmail.com>
2020-12-13 19:18:39 +0100

    Unused using.

--- WalletWasabi/CoinJoin/Client/Clients/CoinJoinClient.cs
+++ WalletWasabi/CoinJoin/Client/Clients/CoinJoinClient.cs
@@ -5,7 +5,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
