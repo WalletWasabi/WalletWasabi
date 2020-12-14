43c9a76c738f51b2ab3f7da4a09a29753b2047c7
Dan Walmsley <dan@walms.co.uk>
2020-12-14 12:59:45 +0000

    added wallet page adds to wallet manager when user clicks done.

--- WalletWasabi.Fluent/ViewModels/AddWallet/AddWalletPageViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/AddWallet/AddWalletPageViewModel.cs
@@ -80,10 +80,10 @@ namespace WalletWasabi.Fluent.ViewModels.AddWallet
 						return;
 					}
 
-					var isColdcardJson = await ImportWalletHelper.ImportWalletAsync(walletManager, WalletName, filePath);
+					var (isColdCardJson, keyManager) = await ImportWalletHelper.ImportWalletAsync(walletManager, WalletName, filePath);
 
 					// TODO: get the type from the wallet file
-					Navigate().To(new AddedWalletPageViewModel(WalletName, isColdcardJson ? WalletType.Coldcard : WalletType.Normal));
+					Navigate().To(new AddedWalletPageViewModel(walletManager, keyManager, isColdCardJson ? WalletType.Coldcard : WalletType.Normal));
 				}
 				catch (Exception ex)
 				{
