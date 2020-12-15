78efa931a87d74ff586ae18ca72215bdb2105bca
Roland Soós <soos.roland93@gmail.com>
2020-12-15 12:53:44 +0100

    stretch in wrap panel

---
+++ WalletWasabi.Fluent/Behaviors/HorizontallyStretchWrapPanelChild.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace WalletWasabi.Fluent.Behaviors
+{
+	public class HorizontallyStretchWrapPanelChild : DisposingBehavior<Control>
+	{
+		protected override void OnAttached(CompositeDisposable disposables)
+		{
+			AssociatedObject?.Parent.WhenAnyValue(x => x.Bounds)
+				.DistinctUntilChanged()
+				.Where(_ => AssociatedObject.Parent.VisualChildren.Count > 0)
+				.Subscribe(StretchLastChild)
+				.DisposeWith(disposables);
+		}
+
+		private void StretchLastChild(Rect parentBounds)
+		{
+			if (!(AssociatedObject?.Parent is WrapPanel parent) || AssociatedObject is null)
+			{
+				return;
+			}
+
+			var otherChildrenTotalWidth = parent.Children.Where(x => !Equals(x, AssociatedObject)).Sum(child => child.Margin.Left + child.Margin.Right + child.Bounds.Width);
+
+			AssociatedObject.Width = parentBounds.Width - otherChildrenTotalWidth - AssociatedObject.Margin.Left - AssociatedObject.Margin.Right;
+		}
+	}
+}
