5d240b817f4d3e0d5cef5d793f5a02f9a9b0fdc7
Roland Soós <soos.roland93@gmail.com>
2020-12-21 08:50:21 +0100

    clean never used prop

--- WalletWasabi.Fluent/Controls/LoginControl.axaml.cs
+++ WalletWasabi.Fluent/Controls/LoginControl.axaml.cs
@@ -1,17 +1,8 @@
-using Avalonia;
 using Avalonia.Controls;
 
 namespace WalletWasabi.Fluent.Controls
 {
 	public class LoginControl : ContentControl
 	{
-		public static readonly StyledProperty<bool> IsBusyProperty =
-			AvaloniaProperty.Register<ContentArea, bool>(nameof(IsBusy));
-
-		public bool IsBusy
-		{
-			get => GetValue(IsBusyProperty);
-			set => SetValue(IsBusyProperty, value);
-		}
 	}
 }
