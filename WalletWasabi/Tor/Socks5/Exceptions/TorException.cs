55f8918236fcd1c43faeddfc8e2fde22063b3c7d
Kiminuo <kiminuo@protonmail.com>
2021-01-05 09:35:50 +0100

    Add TorException base class and add a few comments.

---
+++ WalletWasabi/Tor/Socks5/Exceptions/TorException.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net.Http;
+using WalletWasabi.Tor.Http;
+
+namespace WalletWasabi.Tor.Socks5.Exceptions
+{
+	/// <summary>
+	/// A base class for exceptions thrown by the Tor SOCKS5 classes.
+	/// </summary>
+	/// <remarks>
+	/// <see cref="TorException"/> inherits <see cref="HttpRequestException"/> so that <see cref="ClearnetHttpClient"/>,
+	/// <see cref="TorHttpClient"/> and possibly other implementations of <see cref="IHttpClient"/> throws an exception with common ancestor.
+	/// </remarks>
+	public class TorException : HttpRequestException
+	{
+		public TorException(string message) : base(message)
+		{
+		}
+
+		public TorException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
