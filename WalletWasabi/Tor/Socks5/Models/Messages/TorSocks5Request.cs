764f86b1e04ed59b1bf948ec55cfc42a8922e301
David Molnar <molnardavid84@gmail.com>
2021-01-11 16:58:51 +0100

    fix cf

--- WalletWasabi/Tor/Socks5/Models/Messages/TorSocks5Request.cs
+++ WalletWasabi/Tor/Socks5/Models/Messages/TorSocks5Request.cs
@@ -31,11 +31,8 @@ namespace WalletWasabi.Tor.Socks5.Models.Messages
 		public PortField DstPort { get; }
 
 		public override byte[] ToBytes() => ByteHelpers.Combine(
-			new byte[]
-			{
-				Ver.ToByte(), Cmd.ToByte(), Rsv.ToByte(), Atyp.ToByte()
-			}
-			, DstAddr.ToBytes()
-			, DstPort.ToBytes());
+			new byte[] { Ver.ToByte(), Cmd.ToByte(), Rsv.ToByte(), Atyp.ToByte() },
+			DstAddr.ToBytes(),
+			DstPort.ToBytes());
 	}
 }
