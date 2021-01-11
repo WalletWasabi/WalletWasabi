f78bdc0462cac02ac72e88ff6c26bf1750aa3fd2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-01-11 21:43:35 +0100

    Disable separator item hittesting

---
+++ WalletWasabi.Fluent/Converters/NavBarItemSeparatorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using WalletWasabi.Fluent.ViewModels.NavBar;
+
+namespace WalletWasabi.Fluent.Converters
+{
+	public class NavBarItemSeparatorConverter : IValueConverter
+	{
+		public static readonly NavBarItemSeparatorConverter Instance = new NavBarItemSeparatorConverter();
+
+		private NavBarItemSeparatorConverter()
+		{
+		}
+
+		object? IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is NavBarItemSelectionMode mode)
+			{
+				return mode != NavBarItemSelectionMode.None;
+			}
+
+			return null;
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
