c0a4fabefa62a537923dfdb6b0c2ec1ae99683c9
Lucas Ontivero <lucasontivero@gmail.com>
2021-01-19 13:13:43 -0300

    Fix tabulations

--- WalletWasabi.Tests/UnitTests/BestEffortEndpointConnectorTests.cs
+++ WalletWasabi.Tests/UnitTests/BestEffortEndpointConnectorTests.cs
@@ -15,71 +15,72 @@ namespace WalletWasabi.Tests.UnitTests
 	{
 		[Fact]
 		public async Task CanConnectWithDifferentModesAsync()
-        {
-            var connector = new BestEffortEndpointConnector();
-            var nodeConnectionParameters = new NodeConnectionParameters();
-            nodeConnectionParameters.TemplateBehaviors.Add(new SocksSettingsBehavior(new IPEndPoint(IPAddress.Loopback, 8090), onlyForOnionHosts: true, networkCredential: null, streamIsolation: true));
+		{
+			var connector = new BestEffortEndpointConnector();
+			var nodeConnectionParameters = new NodeConnectionParameters();
+			nodeConnectionParameters.TemplateBehaviors.Add(new SocksSettingsBehavior(new IPEndPoint(IPAddress.Loopback, 8090), onlyForOnionHosts: true, networkCredential: null, streamIsolation: true));
 
-            using var nodes = new NodesGroup(Network.TestNet, nodeConnectionParameters);
+			using var nodes = new NodesGroup(Network.TestNet, nodeConnectionParameters);
 
-            async Task ConnectAsync(EndPoint endpoint)
-            {
-                using var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-                await connector.ConnectSocket(socket, endpoint, nodeConnectionParameters, CancellationToken.None);
-            }
+			async Task ConnectAsync(EndPoint endpoint)
+			{
+				using var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+				await connector.ConnectSocket(socket, endpoint, nodeConnectionParameters, CancellationToken.None);
+			}
 
-            Exception ex;
-            // Try to connect to a non-onion address.
-            ex = await Assert.ThrowsAsync<InvalidOperationException>(
-                async () => await ConnectAsync(new IPEndPoint(IPAddress.Loopback, 180)));
-            Assert.Equal("The Endpoint connector is configured to allow only Tor endpoints and the '127.0.0.1:180' enpoint is not one", ex.Message);
-            Assert.Equal(BestEffortEndpointConnector.ConnectionMode.OnionServiceOnly, connector.State.Mode);
+			Exception ex;
 
-            // Try to connect to an onion address (it has to fail because there is no a real socks proxy listening).
-            ex = await Assert.ThrowsAnyAsync<SocketException>(
-                async () => await ConnectAsync(new DnsEndPoint("nec4kn4ghql7p7an.onion", 80)));
-            Assert.Contains("refused", ex.Message);
-            Assert.Equal(BestEffortEndpointConnector.ConnectionMode.OnionServiceOnly, connector.State.Mode);
+			// Try to connect to a non-onion address.
+			ex = await Assert.ThrowsAsync<InvalidOperationException>(
+				async () => await ConnectAsync(new IPEndPoint(IPAddress.Loopback, 180)));
+			Assert.Equal("The Endpoint connector is configured to allow only Tor endpoints and the '127.0.0.1:180' enpoint is not one", ex.Message);
+			Assert.Equal(BestEffortEndpointConnector.ConnectionMode.OnionServiceOnly, connector.State.Mode);
 
-            // Timing out - Mode changes from OnionServiceOnly -> AllowGoingThroughTorExitNodes.
-            connector.State.LastModeChangeTime = DateTimeOffset.UtcNow.AddMinutes(-1.5);
+			// Try to connect to an onion address (it has to fail because there is no a real socks proxy listening).
+			ex = await Assert.ThrowsAnyAsync<SocketException>(
+				async () => await ConnectAsync(new DnsEndPoint("nec4kn4ghql7p7an.onion", 80)));
+			Assert.Contains("refused", ex.Message);
+			Assert.Equal(BestEffortEndpointConnector.ConnectionMode.OnionServiceOnly, connector.State.Mode);
 
-            ex = await Assert.ThrowsAnyAsync<SocketException>(
-                async () => await ConnectAsync(new IPEndPoint(IPAddress.Loopback, 180)));
-            Assert.Contains("refused", ex.Message);
-            Assert.Equal(BestEffortEndpointConnector.ConnectionMode.AllowGoingThroughTorExitNodes, connector.State.Mode);
+			// Timing out - Mode changes from OnionServiceOnly -> AllowGoingThroughTorExitNodes.
+			connector.State.LastModeChangeTime = DateTimeOffset.UtcNow.AddMinutes(-1.5);
 
-            // Simulate there is a connection made (so, it is feasible to have more).
-            connector.UpdateConnectedNodesCounter(1);
-            connector.State.LastModeChangeTime = DateTimeOffset.UtcNow.AddMinutes(-1.5);
+			ex = await Assert.ThrowsAnyAsync<SocketException>(
+				async () => await ConnectAsync(new IPEndPoint(IPAddress.Loopback, 180)));
+			Assert.Contains("refused", ex.Message);
+			Assert.Equal(BestEffortEndpointConnector.ConnectionMode.AllowGoingThroughTorExitNodes, connector.State.Mode);
 
-            ex = await Assert.ThrowsAnyAsync<SocketException>(
-                async () => await ConnectAsync(new IPEndPoint(IPAddress.Loopback, 180)));
-            Assert.Contains("refused", ex.Message);
-            Assert.Equal(BestEffortEndpointConnector.ConnectionMode.AllowGoingThroughTorExitNodes, connector.State.Mode);
+			// Simulate there is a connection made (so, it is feasible to have more).
+			connector.UpdateConnectedNodesCounter(1);
+			connector.State.LastModeChangeTime = DateTimeOffset.UtcNow.AddMinutes(-1.5);
 
-            // Simulate we lost connection.
-            connector.State.ConnectedNodesCount = 0;
+			ex = await Assert.ThrowsAnyAsync<SocketException>(
+				async () => await ConnectAsync(new IPEndPoint(IPAddress.Loopback, 180)));
+			Assert.Contains("refused", ex.Message);
+			Assert.Equal(BestEffortEndpointConnector.ConnectionMode.AllowGoingThroughTorExitNodes, connector.State.Mode);
 
-            ex = await Assert.ThrowsAnyAsync<SocketException>(
-                async () => await ConnectAsync(new DnsEndPoint("nec4kn4ghql7p7an.onion", 180)));
-            Assert.Contains("refused", ex.Message);
-            Assert.Equal(BestEffortEndpointConnector.ConnectionMode.OnionServiceOnly, connector.State.Mode);
+			// Simulate we lost connection.
+			connector.State.ConnectedNodesCount = 0;
 
-            // Simulate there are no connections made.
-            connector.State.LastModeChangeTime = DateTimeOffset.UtcNow.AddHours(-1);
+			ex = await Assert.ThrowsAnyAsync<SocketException>(
+				async () => await ConnectAsync(new DnsEndPoint("nec4kn4ghql7p7an.onion", 180)));
+			Assert.Contains("refused", ex.Message);
+			Assert.Equal(BestEffortEndpointConnector.ConnectionMode.OnionServiceOnly, connector.State.Mode);
 
-            ex = await Assert.ThrowsAnyAsync<SocketException>(
-                async () => await ConnectAsync(new IPEndPoint(IPAddress.Loopback, 180)));
-            Assert.Contains("refused", ex.Message);
-            Assert.Equal(BestEffortEndpointConnector.ConnectionMode.AllowGoingThroughTorExitNodes, connector.State.Mode);
+			// Simulate there are no connections made.
+			connector.State.LastModeChangeTime = DateTimeOffset.UtcNow.AddHours(-1);
 
-            // Simulate there are no connections made (so, go to desperate mode and connect to clearnet).
-            connector.State.LastModeChangeTime = DateTimeOffset.UtcNow.AddHours(-1);
-            ex = await Assert.ThrowsAnyAsync<SocketException>(
-                async () => await ConnectAsync(new IPEndPoint(IPAddress.Loopback, 180)));
-            Assert.Contains("refused", ex.Message);
-            Assert.Equal(BestEffortEndpointConnector.ConnectionMode.ClearNet, connector.State.Mode);
-        }
-    }
+			ex = await Assert.ThrowsAnyAsync<SocketException>(
+				async () => await ConnectAsync(new IPEndPoint(IPAddress.Loopback, 180)));
+			Assert.Contains("refused", ex.Message);
+			Assert.Equal(BestEffortEndpointConnector.ConnectionMode.AllowGoingThroughTorExitNodes, connector.State.Mode);
+
+			// Simulate there are no connections made (so, go to desperate mode and connect to clearnet).
+			connector.State.LastModeChangeTime = DateTimeOffset.UtcNow.AddHours(-1);
+			ex = await Assert.ThrowsAnyAsync<SocketException>(
+				async () => await ConnectAsync(new IPEndPoint(IPAddress.Loopback, 180)));
+			Assert.Contains("refused", ex.Message);
+			Assert.Equal(BestEffortEndpointConnector.ConnectionMode.ClearNet, connector.State.Mode);
+		}
+	}
 }
--- WalletWasabi/BestEffortEndpointConnector.cs
+++ WalletWasabi/BestEffortEndpointConnector.cs
@@ -12,96 +12,96 @@ namespace WalletWasabi
 {
 	public class BestEffortEndpointConnector : IEnpointConnector
 	{
-        public enum ConnectionMode
-        {
-            // Connect to nodes published as onion services only 
-            OnionServiceOnly,
-            // Connect to nodes published on the clearnet through Tor exit nodes
-            AllowGoingThroughTorExitNodes,
-            // Connect to nodes directly on clearnet
-            ClearNet
-        }
+		public enum ConnectionMode
+		{
+			// Connect to nodes published as onion services only
+			OnionServiceOnly,
+			// Connect to nodes published on the clearnet through Tor exit nodes
+			AllowGoingThroughTorExitNodes,
+			// Connect to nodes directly on clearnet
+			ClearNet
+		}
 
-        // A class to share state between all the BestEffortEndpointConnector connectors.
-        // This is necessary because NBitcoin clones the connectors for every new node connection
-        // attempt using the original connector.
+		// A class to share state between all the BestEffortEndpointConnector connectors.
+		// This is necessary because NBitcoin clones the connectors for every new node connection
+		// attempt using the original connector.
 		public class EffortState
-        {
-            private long _connectedNodesCount;
-            private DateTimeOffset _lastModeChangeTime;
-            private bool _lostConnection; 
-            public long ConnectedNodesCount
+		{
+			private long _connectedNodesCount;
+			private DateTimeOffset _lastModeChangeTime;
+			private bool _lostConnection;
+			public long ConnectedNodesCount
 			{
 				get => _connectedNodesCount;
 				set
 				{
-                    _lostConnection = value == 0 && _connectedNodesCount > 0;
-                    _connectedNodesCount = value;
+					_lostConnection = value == 0 && _connectedNodesCount > 0;
+					_connectedNodesCount = value;
 				}
 			}
 
 			public ConnectionMode Mode { get; set; }
-            public DateTimeOffset LastModeChangeTime 
-            { 
-                get => _lastModeChangeTime;
-                set
-                {
-                    _lostConnection = false;
-                    _lastModeChangeTime = value;
-                }
-            }
+			public DateTimeOffset LastModeChangeTime
+			{
+				get => _lastModeChangeTime;
+				set
+				{
+					_lostConnection = false;
+					_lastModeChangeTime = value;
+				}
+			}
 
-            public TimeSpan ElapsedTimeSinceLastModeChange => DateTimeOffset.UtcNow - LastModeChangeTime;
-            
-            public EffortState(ConnectionMode mode, DateTimeOffset lastModeChangeTime)
-            {
-                Mode = mode;
-                LastModeChangeTime = lastModeChangeTime;
-            }
+			public TimeSpan ElapsedTimeSinceLastModeChange => DateTimeOffset.UtcNow - LastModeChangeTime;
 
-            public bool CheckModeUpdate()
-            {
-                var previousMode = Mode;
-                if (_lostConnection)
-                {
-                    Mode = ConnectionMode.OnionServiceOnly;
-                } 
-                else if ( (ElapsedTimeSinceLastModeChange > TimeSpan.FromMinutes(1) && ConnectedNodesCount == 0) 
-                        || (ElapsedTimeSinceLastModeChange > TimeSpan.FromMinutes(2) && ConnectedNodesCount <= 3)
-                        || (ElapsedTimeSinceLastModeChange > TimeSpan.FromMinutes(3) && ConnectedNodesCount <= 5))
-                {
-                    Mode = Mode switch {
-                        ConnectionMode.OnionServiceOnly => ConnectionMode.AllowGoingThroughTorExitNodes,
-                        ConnectionMode.AllowGoingThroughTorExitNodes => ConnectionMode.ClearNet,
-                        ConnectionMode.ClearNet => ConnectionMode.ClearNet,
-                        _ => throw new ArgumentException($"Unknown {nameof(Mode)} value {Mode}.")
-                    };
-                }
-                if (previousMode != Mode)
-                {
-                    Logger.LogInfo($"Update connection mode from {previousMode} to {Mode}.");
-                    LastModeChangeTime = DateTimeOffset.UtcNow;
-                    return true;
-                }
-                return false; 
-            }
-        }
+			public EffortState(ConnectionMode mode, DateTimeOffset lastModeChangeTime)
+			{
+				Mode = mode;
+				LastModeChangeTime = lastModeChangeTime;
+			}
 
-        public DefaultEndpointConnector Connector { get; private set; }
-        public EffortState State { get; private set; }
+			public bool CheckModeUpdate()
+			{
+				var previousMode = Mode;
+				if (_lostConnection)
+				{
+					Mode = ConnectionMode.OnionServiceOnly;
+				}
+				else if ( (ElapsedTimeSinceLastModeChange > TimeSpan.FromMinutes(1) && ConnectedNodesCount == 0)
+						|| (ElapsedTimeSinceLastModeChange > TimeSpan.FromMinutes(2) && ConnectedNodesCount <= 3)
+						|| (ElapsedTimeSinceLastModeChange > TimeSpan.FromMinutes(3) && ConnectedNodesCount <= 5))
+				{
+					Mode = Mode switch {
+						ConnectionMode.OnionServiceOnly => ConnectionMode.AllowGoingThroughTorExitNodes,
+						ConnectionMode.AllowGoingThroughTorExitNodes => ConnectionMode.ClearNet,
+						ConnectionMode.ClearNet => ConnectionMode.ClearNet,
+						_ => throw new ArgumentException($"Unknown {nameof(Mode)} value {Mode}.")
+					};
+				}
+				if (previousMode != Mode)
+				{
+					Logger.LogInfo($"Update connection mode from {previousMode} to {Mode}.");
+					LastModeChangeTime = DateTimeOffset.UtcNow;
+					return true;
+				}
+				return false;
+			}
+		}
 
-        public BestEffortEndpointConnector()
-            : this(
-                new DefaultEndpointConnector(allowOnlyTorEndpoints: true), 
-                new EffortState(ConnectionMode.OnionServiceOnly, DateTimeOffset.UtcNow))
-        {
-        }
+		public DefaultEndpointConnector Connector { get; private set; }
+		public EffortState State { get; private set; }
 
-        private BestEffortEndpointConnector(DefaultEndpointConnector connector, EffortState state)
-        {
-            Connector = connector;
-            State = state;
-        }
+		public BestEffortEndpointConnector()
+			: this(
+				new DefaultEndpointConnector(allowOnlyTorEndpoints: true),
+				new EffortState(ConnectionMode.OnionServiceOnly, DateTimeOffset.UtcNow))
+		{
+		}
+
+		private BestEffortEndpointConnector(DefaultEndpointConnector connector, EffortState state)
+		{
+			Connector = connector;
+			State = state;
+		}
 
 		public void UpdateConnectedNodesCounter(int connectedNodes)
 		{
@@ -115,38 +115,38 @@ namespace WalletWasabi
 
 		public virtual async Task ConnectSocket(Socket socket, EndPoint endPoint, NodeConnectionParameters nodeConnectionParameters, CancellationToken cancellationToken)
 		{
-            if (State.CheckModeUpdate())
-            {
-                var socksSettings = nodeConnectionParameters.TemplateBehaviors.Find<SocksSettingsBehavior>();
+			if (State.CheckModeUpdate())
+			{
+				var socksSettings = nodeConnectionParameters.TemplateBehaviors.Find<SocksSettingsBehavior>();
 
-                switch (State.Mode)
-                {
-                    case ConnectionMode.OnionServiceOnly:
-                        Connector.AllowOnlyTorEndpoints = true;  // throw if endPoint is not a Tor endpoint
-                        if (socksSettings is { })
-                        {
-                            socksSettings.OnlyForOnionHosts = false; // go through Tor always
-                        }
-                        break;
-                    case ConnectionMode.AllowGoingThroughTorExitNodes:
-                        Connector.AllowOnlyTorEndpoints = false; // do not throw if endPoint is not a Tor endpoint
-                        if (socksSettings is { })
-                        {
-                            socksSettings.OnlyForOnionHosts = false; // go through Tor always
-                        }
-                        break;
-                    case ConnectionMode.ClearNet:
-                        Connector.AllowOnlyTorEndpoints = false; // do not throw if endPoint is not a Tor endpoint
-                        if (socksSettings is { })
-                        {
-                            socksSettings.OnlyForOnionHosts = true; // go through Tor always
-                        }
-                        break;
-                }
-            }
+				switch (State.Mode)
+				{
+					case ConnectionMode.OnionServiceOnly:
+						Connector.AllowOnlyTorEndpoints = true;  // throw if endPoint is not a Tor endpoint
+						if (socksSettings is { })
+						{
+							socksSettings.OnlyForOnionHosts = false; // go through Tor always
+						}
+						break;
+					case ConnectionMode.AllowGoingThroughTorExitNodes:
+						Connector.AllowOnlyTorEndpoints = false; // do not throw if endPoint is not a Tor endpoint
+						if (socksSettings is { })
+						{
+							socksSettings.OnlyForOnionHosts = false; // go through Tor always
+						}
+						break;
+					case ConnectionMode.ClearNet:
+						Connector.AllowOnlyTorEndpoints = false; // do not throw if endPoint is not a Tor endpoint
+						if (socksSettings is { })
+						{
+							socksSettings.OnlyForOnionHosts = true; // go through Tor always
+						}
+						break;
+				}
+			}
 
-            await Connector.ConnectSocket(socket, endPoint, nodeConnectionParameters, cancellationToken).ConfigureAwait(false);
-            State.LastModeChangeTime = DateTimeOffset.UtcNow;
+			await Connector.ConnectSocket(socket, endPoint, nodeConnectionParameters, cancellationToken).ConfigureAwait(false);
+			State.LastModeChangeTime = DateTimeOffset.UtcNow;
 		}
 	}
 }
