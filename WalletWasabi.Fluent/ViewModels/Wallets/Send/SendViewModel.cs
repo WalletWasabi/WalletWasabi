3a60076e4b8d2e13d9f78035b8d39c97f2ee9de4
Dan Walmsley <dan@walms.co.uk>
2021-01-27 17:22:52 +0000

    add paste icon to Address box.

--- WalletWasabi.Fluent/ViewModels/Wallets/Send/SendViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Send/SendViewModel.cs
@@ -5,12 +5,14 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Send
 	[NavigationMetaData(
 		Title = "Send",
 		Caption = "",
-		IconName = "wallet_action_receive",
+		IconName = "wallet_action_send",
 		NavBarPosition = NavBarPosition.None,
 		Searchable = false,
 		NavigationTarget = NavigationTarget.HomeScreen)]
 	public partial class SendViewModel : NavBarItemViewModel
 	{
+		[AutoNotify] private string _to;
+
 		public SendViewModel()
 		{
 
