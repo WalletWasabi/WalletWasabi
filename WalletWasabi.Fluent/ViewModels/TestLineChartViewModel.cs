4587d19e4e36331ce88622182d47c00aab7f7f1e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-01-27 08:43:26 +0100

    Add TestLineChartViewModel class

---
+++ WalletWasabi.Fluent/ViewModels/TestLineChartViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace WalletWasabi.Fluent.ViewModels
+{
+    public class TestLineChartViewModel
+    {
+        public List<double> Values => new List<double>()
+        {
+            15,
+            22,
+            44,
+            50,
+            64,
+            68,
+            92,
+            114,
+            118,
+            142,
+            182,
+            222,
+            446,
+            548,
+            600
+        };
+
+        public List<string> Labels => new List<string>()
+        {
+            "6 days",
+            "4 days",
+            "3 days",
+            "1 day",
+            "22 hours",
+            "20 hours",
+            "18 hours",
+            "10 hours",
+            "6 hours",
+            "4 hours",
+            "2 hours",
+            "1 hour",
+            "50 min",
+            "30 min",
+            "20 min"
+        };
+    }
+}
