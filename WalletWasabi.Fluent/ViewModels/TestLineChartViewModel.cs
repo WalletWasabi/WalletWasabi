705bd5f766cce0ace4c2dfe8269c32c33a8e0b02
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-01-28 20:09:34 +0100

    Use new labels

--- WalletWasabi.Fluent/ViewModels/TestLineChartViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/TestLineChartViewModel.cs
@@ -25,21 +25,40 @@ namespace WalletWasabi.Fluent.ViewModels
 
         public List<string> Labels => new List<string>()
         {
-            "6 days",
-            "4 days",
-            "3 days",
-            "1 day",
-            "22 hours",
-            "20 hours",
-            "18 hours",
-            "10 hours",
-            "6 hours",
-            "4 hours",
-            "2 hours",
-            "1 hour",
-            "50 min",
-            "30 min",
-            "20 min"
+            "6d",
+            "4d",
+            "3d",
+            "1d",
+            "22h",
+            "20h",
+            "18h",
+            "10h",
+            "6h",
+            "4h",
+            "2h",
+            "1h",
+            "50m",
+            "30m",
+            "20m"
         };
+
+        // public List<string> Labels => new List<string>()
+        // {
+	    //     "6 days",
+	    //     "4 days",
+	    //     "3 days",
+	    //     "1 day",
+	    //     "22 hours",
+	    //     "20 hours",
+	    //     "18 hours",
+	    //     "10 hours",
+	    //     "6 hours",
+	    //     "4 hours",
+	    //     "2 hours",
+	    //     "1 hour",
+	    //     "50 min",
+	    //     "30 min",
+	    //     "20 min"
+        // };
     }
 }
