e8d14cba8235378462442d466ef1e37ffc88a596
Dan Walmsley <dan@walms.co.uk>
2021-01-29 17:06:03 +0000

    basic working currency entry box.

--- WalletWasabi.Fluent/Controls/CurrencyEntryBox.axaml.cs
+++ WalletWasabi.Fluent/Controls/CurrencyEntryBox.axaml.cs
@@ -1,22 +1,52 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Reactive.Disposables;
-using System.Windows.Input;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
-using Avalonia.Data;
-using Avalonia.Input;
-using Avalonia.Interactivity;
-using Avalonia.Styling;
-using Avalonia.Threading;
 
 namespace WalletWasabi.Fluent.Controls
 {
 	public class CurrencyEntryBox : TextBox
 	{
+		public static readonly StyledProperty<string> ConversionProperty =
+			AvaloniaProperty.Register<CurrencyEntryBox, string>(nameof(Conversion));
+
+		public static readonly StyledProperty<decimal> ConversionRateProperty =
+			AvaloniaProperty.Register<CurrencyEntryBox, decimal>(nameof(ConversionRate));
+
+		public static readonly StyledProperty<string> CurrencyCodeProperty =
+			AvaloniaProperty.Register<CurrencyEntryBox, string>(nameof(CurrencyCodeProperty));
+
+		public CurrencyEntryBox()
+		{
+			this.GetObservable(TextProperty).Subscribe(x =>
+			{
+				if (decimal.TryParse(x, out var result) && ConversionRate > 0)
+				{
+					Conversion = $"≈ {result * ConversionRate :N}"
+					             + (!string.IsNullOrWhiteSpace(CurrencyCode) ? $" {CurrencyCode}" : "");
+				}
+				else
+				{
+					Conversion = string.Empty;
+				}
+			});
+		}
+
+		public string Conversion
+		{
+			get => GetValue(ConversionProperty);
+			set => SetValue(ConversionProperty, value);
+		}
+
+		public decimal ConversionRate
+		{
+			get => GetValue(ConversionRateProperty);
+			set => SetValue(ConversionRateProperty, value);
+		}
+
+		public string CurrencyCode
+		{
+			get => GetValue(CurrencyCodeProperty);
+			set => SetValue(CurrencyCodeProperty, value);
+		}
 	}
 }
