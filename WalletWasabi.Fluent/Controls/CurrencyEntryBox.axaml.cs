964e953bcf96d65a9686b3d3fbbadfc59c4851fb
Dan Walmsley <dan@walms.co.uk>
2021-01-29 14:52:23 +0000

    add currencyentry control.

---
+++ WalletWasabi.Fluent/Controls/CurrencyEntryBox.axaml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Styling;
+using Avalonia.Threading;
+
+namespace WalletWasabi.Fluent.Controls
+{
+	public class CurrencyEntryBox : TextBox
+	{
+	}
+}
