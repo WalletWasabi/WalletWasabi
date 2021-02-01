3aaa7044c0c9ae40600b44712f1a674d619dad3d
nopara73 <adam.ficsor73@gmail.com>
2021-02-01 12:16:35 +0100

    Feed with real data and have exact data points.

--- WalletWasabi.Fluent/ViewModels/TestLineChartViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/TestLineChartViewModel.cs
@@ -1,89 +1,74 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace WalletWasabi.Fluent.ViewModels
 {
-    public class TestLineChartViewModel
-    {
-	    public double XAxisCurrentValue { get; set; } = 36;
+	public class TestLineChartViewModel
+	{
+		public double XAxisCurrentValue { get; set; } = 36;
 
-	    public double XAxisMinValue { get; set; } = 2;
+		public double XAxisMinValue { get; set; } = 2;
 
-	    public double XAxisMaxValue { get; set; } = 864;
+		public double XAxisMaxValue { get; set; } = 864;
 
-        // public List<string> XAxisLabels => new List<string>()
-        // {
-        //     "6 days",
-        //     "4 days",
-        //     "3 days",
-        //     "1 day",
-        //     "22 hours",
-        //     "20 hours",
-        //     "18 hours",
-        //     "10 hours",
-        //     "6 hours",
-        //     "4 hours",
-        //     "2 hours",
-        //     "1 hour",
-        //     "50 min",
-        //     "30 min",
-        //     "20 min"
-        // };
+		// public List<string> XAxisLabels => new List<string>()
+		// {
+		//     "6 days",
+		//     "4 days",
+		//     "3 days",
+		//     "1 day",
+		//     "22 hours",
+		//     "20 hours",
+		//     "18 hours",
+		//     "10 hours",
+		//     "6 hours",
+		//     "4 hours",
+		//     "2 hours",
+		//     "1 hour",
+		//     "50 min",
+		//     "30 min",
+		//     "20 min"
+		// };
 
-        public List<string> XAxisLabels => new List<string>()
-        {
-            "6d",
-            "4d",
-            "3d",
-            "1d",
-            "22h",
-            "20h",
-            "18h",
-            "10h",
-            "6h",
-            "4h",
-            "2h",
-            "1h",
-            "50m",
-            "30m",
-            "20m"
-        };
+		public List<string> XAxisLabels => new List<string>()
+		{
+			"1w",
+			"3d",
+			"1d",
+			"12h",
+			"6h",
+			"3h",
+			"1h",
+			"30m",
+			"20m",
+			"fastest"
+		};
 
-        public List<double> XAxisValues => new List<double>()
-        {
-	        864,
-	        576,
-	        432,
-	        144,
-	        132,
-	        120,
-	        108,
-	        60,
-	        36,
-	        24,
-	        12,
-	        6,
-	        5,
-	        3,
-	        2,
-        };
+		public List<double> XAxisValues => new List<double>()
+		{
+			1008,
+			432,
+			144,
+			72,
+			36,
+			18,
+			6,
+			3,
+			2,
+			1,
+		};
 
-        public List<double> YAxisValues => new List<double>()
-        {
-	        15,
-	        22,
-	        44,
-	        50,
-	        64,
-	        68,
-	        92,
-	        114,
-	        118,
-	        142,
-	        182,
-	        222,
-	        446,
-	        548,
-	        600
-        };
-    }
+		public List<double> YAxisValues => new List<double>()
+		{
+			4,
+			4,
+			7,
+			22,
+			57,
+			97,
+			102,
+			123,
+			123,
+			185
+		};
+	}
 }
