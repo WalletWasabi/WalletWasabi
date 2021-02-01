6b7ee261af6c4003edade9a45837fd2266cf37c4
Yahia Chiheb <chihebyahia@gmail.com>
2021-02-01 14:38:09 +0100

    Move validation folder to Fluent project

---
+++ WalletWasabi.Fluent/Validation/IRegisterValidationMethod.cs
@@ -0,0 +1,7 @@
+namespace WalletWasabi.Fluent.Validation
+{
+	public interface IRegisterValidationMethod
+	{
+		void RegisterValidationMethod(string propertyName, ValidateMethod validateMethod);
+	}
+}
