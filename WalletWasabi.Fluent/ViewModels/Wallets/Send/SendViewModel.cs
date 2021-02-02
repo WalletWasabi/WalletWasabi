8491309dd3af4a925661dc8ce229d9a9fbb58bc7
Dan Walmsley <dan@walms.co.uk>
2021-02-02 16:24:51 +0000

    add line chart to send ux.

--- WalletWasabi.Fluent/ViewModels/Wallets/Send/SendViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Send/SendViewModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using WalletWasabi.Fluent.ViewModels.NavBar;
 
 namespace WalletWasabi.Fluent.ViewModels.Wallets.Send
@@ -17,6 +18,54 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Send
 		{
 
 		}
+
+		public double XAxisCurrentValue { get; set; } = 36;
+
+		public double XAxisMinValue { get; set; } = 1;
+
+		public double XAxisMaxValue { get; set; } = 1008;
+
+		public List<string> XAxisLabels => new List<string>()
+		{
+			"1w",
+			"3d",
+			"1d",
+			"12h",
+			"6h",
+			"3h",
+			"1h",
+			"30m",
+			"20m",
+			"fastest"
+		};
+
+		public List<double> XAxisValues => new List<double>()
+		{
+			1008,
+			432,
+			144,
+			72,
+			36,
+			18,
+			6,
+			3,
+			2,
+			1,
+		};
+
+		public List<double> YAxisValues => new List<double>()
+		{
+			4,
+			4,
+			7,
+			22,
+			57,
+			97,
+			102,
+			123,
+			123,
+			185
+		};
 
 
 	}
