ac91520044aca5eec02da3a3ba1afb620ebaf53b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2021-02-06 19:13:09 +0100

    Format code

--- WalletWasabi.Fluent/MathNet/SplineBoundaryCondition.cs
+++ WalletWasabi.Fluent/MathNet/SplineBoundaryCondition.cs
@@ -29,29 +29,29 @@
 
 namespace MathNet.Numerics.Interpolation
 {
-    /// <summary>
-    /// Left and right boundary conditions.
-    /// </summary>
-    public enum SplineBoundaryCondition
-    {
-        /// <summary>
-        /// Natural Boundary (Zero second derivative).
-        /// </summary>
-        Natural = 0,
+	/// <summary>
+	/// Left and right boundary conditions.
+	/// </summary>
+	public enum SplineBoundaryCondition
+	{
+		/// <summary>
+		/// Natural Boundary (Zero second derivative).
+		/// </summary>
+		Natural = 0,
 
-        /// <summary>
-        /// Parabolically Terminated boundary.
-        /// </summary>
-        ParabolicallyTerminated,
+		/// <summary>
+		/// Parabolically Terminated boundary.
+		/// </summary>
+		ParabolicallyTerminated,
 
-        /// <summary>
-        /// Fixed first derivative at the boundary.
-        /// </summary>
-        FirstDerivative,
+		/// <summary>
+		/// Fixed first derivative at the boundary.
+		/// </summary>
+		FirstDerivative,
 
-        /// <summary>
-        /// Fixed second derivative at the boundary.
-        /// </summary>
-        SecondDerivative
-    }
+		/// <summary>
+		/// Fixed second derivative at the boundary.
+		/// </summary>
+		SecondDerivative
+	}
 }
