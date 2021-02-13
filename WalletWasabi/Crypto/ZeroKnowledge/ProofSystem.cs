58955fc2c92609b37ed4c654ba2d0bba906fa21b
nopara73 <adam.ficsor73@gmail.com>
2021-02-13 09:28:10 +0100

    Fix CF?

--- WalletWasabi/Crypto/ZeroKnowledge/ProofSystem.cs
+++ WalletWasabi/Crypto/ZeroKnowledge/ProofSystem.cs
@@ -132,7 +132,9 @@ namespace WalletWasabi.Crypto.ZeroKnowledge
 			=> new Knowledge(ZeroProofStatement(ma), new ScalarVector(r));
 
 		public static GroupElement PedersenCommitment(Scalar s, Scalar b)
-		 	=> new GroupElement(ECMultContext.Instance.MultBatch(new[] { s, b }, new[] { Generators.Gg.Ge, Generators.Gh.Ge }));
+		 	=> new GroupElement(ECMultContext.Instance.MultBatch(
+				new[] { s, b },
+				new[] { Generators.Gg.Ge, Generators.Gh.Ge }));
 
 		// TODO swap return value order, remove GroupElement argument
 		// expect nonce provider instead of WasabiRandom?
