9374da90623391e97313ed8073da0f03b8e50786
Dan Walmsley <dan@walms.co.uk>
2021-02-24 20:21:29 +0000

    bold and non bold preview text.

--- WalletWasabi.Fluent/ViewModels/Wallets/Send/TransactionPreviewViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Send/TransactionPreviewViewModel.cs
@@ -30,17 +30,41 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Send
 			{
 				labels = string.Join(", ", info.Labels.Take(info.Labels.Count() - 1));
 
-				labels += $" and {info.Labels.Last()}";
+				labels += $" and {info.Labels.Last()} ";
 			}
 
-			Instruction = $"A total of {destinationAmount} bitcoins (≈{(destinationAmount * wallet.Synchronizer.UsdExchangeRate).FormattedFiat()} USD) will be sent to {labels} via {info.Address.ToString()}";
+			BtcAmountText = $"{destinationAmount} bitcoins ";
 
-			Execution =
-				$"Bitcoin miners will work hard to confirm your transaction within ~20 minutes for an additional fee of {fee.ToDecimal(MoneyUnit.Satoshi)} satoshis (≈{(fee.ToDecimal(MoneyUnit.BTC) * wallet.Synchronizer.UsdExchangeRate).FormattedFiat()}) this is a charge equivalent to {transaction.FeePercentOfSent:P}";
+			FiatAmountText = $"({(destinationAmount * wallet.Synchronizer.UsdExchangeRate).FormattedFiat()} USD) ";
+
+			LabelsText = labels;
+
+			AddressText = info.Address.ToString();
+
+			ConfirmationTimeText = "~20 minutes ";
+
+			BtcFeeText = $"{fee.ToDecimal(MoneyUnit.Satoshi)} satoshis ";
+
+			FiatFeeText =
+				$"(≈{(fee.ToDecimal(MoneyUnit.BTC) * wallet.Synchronizer.UsdExchangeRate).FormattedFiat()} USD)";
+
+			PercentFeeText = $"{transaction.FeePercentOfSent:F2}";
 		}
 
-		public string Instruction { get; }
+		public string BtcAmountText { get; }
 
-		public string Execution { get; }
+		public string FiatAmountText { get; }
+
+		public string LabelsText { get; }
+
+		public string AddressText { get; }
+
+		public string ConfirmationTimeText { get; }
+
+		public string BtcFeeText { get; }
+
+		public string FiatFeeText { get; }
+
+		public string PercentFeeText { get; }
 	}
 }
