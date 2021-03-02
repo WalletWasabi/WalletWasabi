42731b98efcbd490701e2eb28fd008b1e274e784
Roland Soós <soos.roland93@gmail.com>
2021-03-02 11:14:02 +0100

    prop after ctor

--- WalletWasabi/Models/FriendlyNameAttribute.cs
+++ WalletWasabi/Models/FriendlyNameAttribute.cs
@@ -5,11 +5,11 @@ namespace WalletWasabi.Models
 	[AttributeUsage(AttributeTargets.Field)]
 	public class FriendlyNameAttribute : Attribute
 	{
-		public string FriendlyName { get; }
-
 		public FriendlyNameAttribute(string friendlyName)
 		{
 			FriendlyName = friendlyName;
 		}
+
+		public string FriendlyName { get; }
 	}
 }
