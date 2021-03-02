e7e843c69e264d55641cede96b83b86e4d269bca
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-03-02 18:23:28 +0100

    Add CommandParameter support

--- WalletWasabi.Fluent/Controls/AnimatedButton.axaml.cs
+++ WalletWasabi.Fluent/Controls/AnimatedButton.axaml.cs
@@ -16,10 +16,19 @@ namespace WalletWasabi.Fluent.Controls
 		public static readonly StyledProperty<Geometry> ClickIconProperty =
 			AvaloniaProperty.Register<AnimatedButton, Geometry>(nameof(ClickIcon));
 
+		public static readonly StyledProperty<object> CommandParameterProperty =
+			AvaloniaProperty.Register<AnimatedButton, object>(nameof(CommandParameter));
+
 		public ICommand Command
 		{
 			get => GetValue(CommandProperty);
 			set => SetValue(CommandProperty, value);
+		}
+
+		public object CommandParameter
+		{
+			get => GetValue(CommandParameterProperty);
+			set => SetValue(CommandParameterProperty, value);
 		}
 
 		public Geometry NormalIcon
