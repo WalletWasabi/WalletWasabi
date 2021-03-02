342e64fd1feb44a0ecc7f5b026503f4685ab9f53
Roland Soós <soos.roland93@gmail.com>
2021-03-02 10:59:23 +0100

    create FriendlyName attribute

---
+++ WalletWasabi/Models/FriendlyNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WalletWasabi.Models
+{
+	[AttributeUsage(AttributeTargets.Field)]
+	public class FriendlyNameAttribute : Attribute
+	{
+		public string FriendlyName { get; }
+
+		public FriendlyNameAttribute(string friendlyName)
+		{
+			FriendlyName = friendlyName;
+		}
+	}
+}
