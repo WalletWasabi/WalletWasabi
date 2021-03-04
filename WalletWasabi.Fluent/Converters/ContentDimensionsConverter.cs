59ef9b1d0e5c5760e16315c982310e310b4dcecc
Jumar Macato <hikari.netto23@gmail.com>
2021-03-04 17:53:15 +0800

    implement converter

--- WalletWasabi.Fluent/Converters/ContentDimensionsConverter.cs
+++ WalletWasabi.Fluent/Converters/ContentDimensionsConverter.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Policy;
 using Avalonia;
 using Avalonia.Data.Converters;
 using Avalonia.Media.Imaging;
@@ -8,14 +12,30 @@ using WalletWasabi.Wallets;
 
 namespace WalletWasabi.Fluent.Converters
 {
-	public static class ContentDimensionsConverter
+	public class ContentDimensionsConverter : IMultiValueConverter
 	{
-		public static readonly IMultiValueConverter Instance =
-			new FuncMultiValueConverter<object, double>(parts =>
-			{
-				var inputs = parts.ToArray();
-				return 700;
-			});
+		public static readonly IMultiValueConverter Instance = new ContentDimensionsConverter();
 
+		public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (values.Count != 3)
+			{
+				return 0;
+			}
+
+			if (values[0] is bool hasCustomSize &&
+			    values[1] is double defaultSize1 &&
+			    values[2] is double customSize)
+			{
+				return hasCustomSize ? customSize : defaultSize1;
+			}
+
+			if (values[1] is double defaultSize2)
+			{
+				return defaultSize2;
+			}
+
+			return 0;
+		}
 	}
 }
