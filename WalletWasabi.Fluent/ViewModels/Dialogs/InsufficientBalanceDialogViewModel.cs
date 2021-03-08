9cb8960da3f822809624e8af961c959abf5281bb
Roland Soós <soos.roland93@gmail.com>
2021-03-08 09:52:53 +0100

    create dialog

---
+++ WalletWasabi.Fluent/ViewModels/Dialogs/InsufficientBalanceDialogViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI;
+using WalletWasabi.Fluent.ViewModels.Dialogs.Base;
+
+namespace WalletWasabi.Fluent.ViewModels.Dialogs
+{
+	[NavigationMetaData(Title = "Insufficient balance")]
+	public partial class InsufficientBalanceDialogViewModel : DialogViewModelBase<bool>
+	{
+		public InsufficientBalanceDialogViewModel()
+		{
+			Text = $"You don't have enough mixed funds in your wallet including the fee. Instead of adding as an extra cost, Wasabi can subtract the fee from the maximum possible amount.\nWould you like Wasabi to do it?";
+
+			NextCommand = ReactiveCommand.Create(() => Close(result: true));
+			CancelCommand = ReactiveCommand.Create(() => Close(DialogResultKind.Cancel));
+		}
+
+		public string Text { get; }
+	}
+}
--- WalletWasabi.Fluent/ViewModels/Wallets/Send/SendViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Send/SendViewModel.cs
@@ -15,6 +15,7 @@ using ReactiveUI;
 using WalletWasabi.Blockchain.Analysis.Clustering;
 using WalletWasabi.Blockchain.TransactionBroadcasting;
 using WalletWasabi.Blockchain.TransactionBuilding;
+using WalletWasabi.Exceptions;
 using WalletWasabi.Fluent.MathNet;
 using WalletWasabi.Fluent.Validation;
 using WalletWasabi.Fluent.ViewModels.NavBar;
@@ -134,6 +135,10 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Send
 						// Private coins are enough.
 						Navigate().To(new OptimisePrivacyViewModel(wallet, transactionInfo, broadcaster, txRes));
 						return;
+					}
+					catch (InsufficientBalanceException ex)
+					{
+
 					}
 					catch (NotEnoughFundsException)
 					{
