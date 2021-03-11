043bb84a0cd7a8e5c9133c1849974a10e0e98037
Yahia Chiheb <chihebyahia@gmail.com>
2021-03-11 11:05:37 +0100

    Correct logging by removing duplicate round id

--- WalletWasabi/WabiSabi/Backend/Rounds/Arena.cs
+++ WalletWasabi/WabiSabi/Backend/Rounds/Arena.cs
@@ -74,7 +74,7 @@ namespace WalletWasabi.WabiSabi.Backend.Rounds
 				if (round.InputCount < Config.MinInputCountByRound)
 				{
 					Rounds.Remove(round.Id);
-					round.LogInfo($"Round {round.Id} removed because the number of inputs was not enough.");
+					round.LogInfo($"Removed because the number of inputs was not enough.");
 				}
 				else
 				{
@@ -106,7 +106,7 @@ namespace WalletWasabi.WabiSabi.Backend.Rounds
 					if (round.InputCount < Config.MinInputCountByRound)
 					{
 						Rounds.Remove(round.Id);
-						round.LogInfo($"Round {round.Id} removed because the number of inputs was not enough.");
+						round.LogInfo($"Removed because the number of inputs was not enough.");
 					}
 					else
 					{
