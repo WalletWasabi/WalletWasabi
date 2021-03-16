31b2c1562892c3826f1ac96f0d697db1631e0d7d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-03-16 19:56:49 +0100

    Reselect already selected NavBarListBox item

--- WalletWasabi.Fluent/Controls/NavBarListBox.cs
+++ WalletWasabi.Fluent/Controls/NavBarListBox.cs
@@ -2,6 +2,8 @@ using Avalonia.Controls;
 using Avalonia.Controls.Generators;
 using Avalonia.Styling;
 using System;
+using Avalonia;
+using Avalonia.Input;
 
 namespace WalletWasabi.Fluent.Controls
 {
@@ -9,12 +11,43 @@ namespace WalletWasabi.Fluent.Controls
 	{
 		Type IStyleable.StyleKey => typeof(ListBox);
 
+		protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+		{
+			base.OnAttachedToVisualTree(e);
+
+			this.GetObservable(SelectedItemProperty).Subscribe(OnSelectedItemPropertyChanged);
+		}
+
+		private void OnSelectedItemPropertyChanged(object? obj)
+		{
+			Console.WriteLine($"OnSelectedItemPropertyChanged {obj?.GetType().Name}");
+		}
+
 		protected override IItemContainerGenerator CreateItemContainerGenerator()
 		{
 			return new ItemContainerGenerator<NavBarItem>(
 				this,
 				ContentControl.ContentProperty,
 				ContentControl.ContentTemplateProperty);
+		}
+
+		protected override void OnPointerPressed(PointerPressedEventArgs e)
+		{
+			var previousSelectedItem = SelectedItem;
+			// Console.WriteLine($"BEFORE OnPointerPressed {SelectedItem?.GetType().Name}");
+
+			base.OnPointerPressed(e);
+
+			// Console.WriteLine($"AFTER OnPointerPressed {SelectedItem?.GetType().Name}");
+
+			var isSameSelectedItem = previousSelectedItem is not null && previousSelectedItem == SelectedItem;
+			Console.WriteLine($"OnPointerPressed {isSameSelectedItem}");
+			if (isSameSelectedItem)
+			{
+				Console.WriteLine($"RESELECT OnPointerPressed");
+				SelectedItem = null;
+				SelectedItem = previousSelectedItem;
+			}
 		}
 	}
 }
