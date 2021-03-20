91bdad069bc9731fd3096ff46bec3a4c6eaeb787
Kiminuo <kiminuo@protonmail.com>
2021-03-20 10:16:30 +0100

    To revert: Turn TRACE logs on for testing.

--- WalletWasabi.Gui/Program.cs
+++ WalletWasabi.Gui/Program.cs
@@ -68,7 +68,7 @@ namespace WalletWasabi.Gui
 			config.CorrectMixUntilAnonymitySet();
 			var walletManager = new WalletManager(config.Network, dataDir, new WalletDirectories(config.Network, dataDir));
 
-			Logger.InitializeDefaults(Path.Combine(dataDir, "Logs.txt"));
+			Logger.InitializeDefaults(Path.Combine(dataDir, "Logs.txt"), Logging.LogLevel.Trace);
 
 			return new Global(dataDir, torLogsFile, config, uiConfig, walletManager);
 		}
