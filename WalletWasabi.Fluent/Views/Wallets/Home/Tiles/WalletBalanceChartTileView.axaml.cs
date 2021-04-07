9c74b619c21c50ed22b0f16d70577d8337feb838
Dan Walmsley <dan@walms.co.uk>
2021-04-07 10:04:06 +0100

    initial tiles ui implementations.

---
+++ WalletWasabi.Fluent/Views/Wallets/Home/Tiles/WalletBalanceChartTileView.axaml.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using WalletWasabi.Fluent.ViewModels;
+
+namespace WalletWasabi.Fluent.Views.Wallets.Home.Tiles
+{
+	public partial class WalletBalanceChartTileViewModel : ViewModelBase
+	{
+		public List<double> YValues { get; } = new List<double> {0.25, 0.34, 0.79, 0.98, 1.2, 0.75, 0.68, 1.57};
+
+		public List<double> XValues { get; } = new List<double> {0.25, 0.34, 0.79, 0.98, 1.2, 0.75, 0.68, 1.57};
+	}
+
+	public class WalletBalanceChartTileView : UserControl
+	{
+		public WalletBalanceChartTileView()
+		{
+			InitializeComponent();
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
