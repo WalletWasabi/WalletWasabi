da0bfb6871c36be30e960caf5f7c9e13a2daec9e
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-04-12 22:00:13 +0200

    Change default values

--- WalletWasabi.Fluent/ViewModels/ResponsivePanelDemo/ResponsivePanelDemoViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/ResponsivePanelDemo/ResponsivePanelDemoViewModel.cs
@@ -4,11 +4,11 @@ namespace WalletWasabi.Fluent.ViewModels.ResponsivePanelDemo
 {
 	public partial class ResponsivePanelDemoViewModel : ViewModelBase
 	{
-		[AutoNotify] private double _itemWidth = 228;
-		[AutoNotify] private double _itemHeight = 126;
-		[AutoNotify] private double _widthSource = double.NaN;
+		[AutoNotify] private double _itemWidth = 330;
+		[AutoNotify] private double _itemHeight = 150;
+		[AutoNotify] private double _widthSource = 990;
 		[AutoNotify] private double _aspectRatio = double.NaN;
-		[AutoNotify] private AvaloniaList<int> _columnHints = new() { 1, 2, 3, 4 };
-		[AutoNotify] private AvaloniaList<double> _widthTriggers = new() { 228, 456, 684, 912 };
+		[AutoNotify] private AvaloniaList<int> _columnHints = new() { 1, 2, 3 };
+		[AutoNotify] private AvaloniaList<double> _widthTriggers = new() { 330, 660, 990 };
 	}
 }
