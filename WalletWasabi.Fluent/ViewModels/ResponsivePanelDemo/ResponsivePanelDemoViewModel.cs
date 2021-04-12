44e29b354b3e89c8cd5a9b17e555bda948e49a99
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-04-12 18:48:56 +0200

    Add ResponsivePanelDemo test view model

---
+++ WalletWasabi.Fluent/ViewModels/ResponsivePanelDemo/ResponsivePanelDemoViewModel.cs
@@ -0,0 +1,14 @@
+using Avalonia.Collections;
+
+namespace WalletWasabi.Fluent.ViewModels.ResponsivePanelDemo
+{
+	public partial class ResponsivePanelDemoViewModel : ViewModelBase
+	{
+		[AutoNotify] private double _itemWidth = 228;
+		[AutoNotify] private double _itemHeight = 126;
+		[AutoNotify] private double _widthSource = double.NaN;
+		[AutoNotify] private double _aspectRatio = double.NaN;
+		[AutoNotify] private AvaloniaList<int> _columnHints = new() {1, 2, 3, 4};
+		[AutoNotify] private AvaloniaList<double> _widthTriggers = new() {228, 456, 684, 912};
+	}
+}
