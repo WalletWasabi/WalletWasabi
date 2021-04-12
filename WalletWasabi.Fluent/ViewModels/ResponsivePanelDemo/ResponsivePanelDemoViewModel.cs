bc323f43de0a475b4888741c8f8a044432897999
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-04-12 19:12:47 +0200

    Fix spacing

--- WalletWasabi.Fluent/ViewModels/ResponsivePanelDemo/ResponsivePanelDemoViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/ResponsivePanelDemo/ResponsivePanelDemoViewModel.cs
@@ -8,7 +8,7 @@ namespace WalletWasabi.Fluent.ViewModels.ResponsivePanelDemo
 		[AutoNotify] private double _itemHeight = 126;
 		[AutoNotify] private double _widthSource = double.NaN;
 		[AutoNotify] private double _aspectRatio = double.NaN;
-		[AutoNotify] private AvaloniaList<int> _columnHints = new() {1, 2, 3, 4};
-		[AutoNotify] private AvaloniaList<double> _widthTriggers = new() {228, 456, 684, 912};
+		[AutoNotify] private AvaloniaList<int> _columnHints = new() { 1, 2, 3, 4 };
+		[AutoNotify] private AvaloniaList<double> _widthTriggers = new() { 228, 456, 684, 912 };
 	}
 }
