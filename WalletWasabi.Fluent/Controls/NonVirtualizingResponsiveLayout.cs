d7a14581857bc9b54b3c044bd8a4255f42e85221
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-04-13 21:34:52 +0200

    Add NonVirtualizingResponsiveLayout stub

---
+++ WalletWasabi.Fluent/Controls/NonVirtualizingResponsiveLayout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Layout;
+
+namespace WalletWasabi.Fluent.Controls
+{
+    public class NonVirtualizingResponsiveLayout : NonVirtualizingLayout
+    {
+	    protected override Size MeasureOverride(NonVirtualizingLayoutContext context, Size availableSize)
+        {
+	        // TODO: Implement MeasureOverride
+	        return Size.Empty;
+        }
+
+        protected override Size ArrangeOverride(NonVirtualizingLayoutContext context, Size finalSize)
+        {
+	        // TODO: Implement ArrangeOverride
+	        return Size.Empty;
+        }
+    }
+}
