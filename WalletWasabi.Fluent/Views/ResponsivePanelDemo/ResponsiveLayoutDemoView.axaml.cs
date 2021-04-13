693fdec16500a840b1312c6df1208497193699fa
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-04-13 22:59:55 +0200

    Added ResponsiveLayoutDemoView

---
+++ WalletWasabi.Fluent/Views/ResponsivePanelDemo/ResponsiveLayoutDemoView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Views.ResponsivePanelDemo
+{
+	public class ResponsiveLayoutDemoView : UserControl
+	{
+		public ResponsiveLayoutDemoView()
+		{
+			InitializeComponent();
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
