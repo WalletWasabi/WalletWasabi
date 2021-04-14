59f2b8884bde2574a6e61579fe937cd9e9e8f6ae
Lucas Ontivero <lucasontivero@gmail.com>
2021-04-13 23:33:08 -0300

    Code review suggestion

--- WalletWasabi/WabiSabi/ProtocolContants.cs
+++ WalletWasabi/WabiSabi/ProtocolContants.cs
@@ -1,13 +1,3 @@
-using NBitcoin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using WalletWasabi.Logging;
-using WalletWasabi.WabiSabi.Backend.Rounds;
-
 namespace WalletWasabi.WabiSabi
 {
 	public static class ProtocolConstants
