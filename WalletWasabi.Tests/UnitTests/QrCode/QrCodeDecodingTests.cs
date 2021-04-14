c9781228612048bc602e751c966ae3360d97cc1b
HaveaKillerDay <45069029+HaveaKillerDay@users.noreply.github.com>
2021-04-14 16:51:08 +0200

    Test refactor

--- WalletWasabi.Tests/UnitTests/QrCode/QrCodeDecodingTests.cs
+++ WalletWasabi.Tests/UnitTests/QrCode/QrCodeDecodingTests.cs
@@ -19,25 +19,24 @@ namespace WalletWasabi.Tests.UnitTests.QrCode
 		public void GetCorrectAddressFromImage()
 		{
 			QRDecoder decoder = new();
+			string expectedAddress = "tb1ql27ya3gufs5h0ptgjhjd0tm52fq6q0xrav7xza";
+			string otherExpectedAddress = "tb1qfas0k9rn8daqggu7wzp2yne9qdd5fr5wf2u478";
 
 			string path = Path.Combine(_commonPartialPath, "AddressTest1.png");
 			using Bitmap qRCodeInputImage = new(path);
-			var dataByteArray = decoder.SearchQrCodes(qRCodeInputImage);
-			Assert.NotEmpty(dataByteArray);
-			string expectedAddress = "tb1ql27ya3gufs5h0ptgjhjd0tm52fq6q0xrav7xza";
-			string address = dataByteArray.First();
-			Assert.Equal(expectedAddress, address);
+			var dataCollection = decoder.SearchQrCodes(qRCodeInputImage);
+
+			Assert.NotEmpty(dataCollection);
+			Assert.Equal(expectedAddress, dataCollection.First());
 
 			string otherPath = Path.Combine(_commonPartialPath, "AddressTest2.png");
 			using Bitmap otherQRCodeInputImage = new(otherPath);
-			var otherDataByteArray = decoder.SearchQrCodes(otherQRCodeInputImage);
-			Assert.NotEmpty(otherDataByteArray);
-			string otherExpectedAddress = "tb1qfas0k9rn8daqggu7wzp2yne9qdd5fr5wf2u478";
-			string address2 = otherDataByteArray.First();
-			Assert.Equal(otherExpectedAddress, address2);
+			var dataCollection2 = decoder.SearchQrCodes(otherQRCodeInputImage);
 
-			Assert.NotEqual(dataByteArray, otherDataByteArray);
-			Assert.NotEqual(expectedAddress, address2);
+			Assert.NotEmpty(dataCollection2);
+			Assert.Equal(otherExpectedAddress, dataCollection2.First());
+
+			Assert.NotEqual(dataCollection, dataCollection2);
 		}
 
 		[Fact]
@@ -45,14 +44,10 @@ namespace WalletWasabi.Tests.UnitTests.QrCode
 		{
 			QRDecoder decoder = new();
 
-			string path = Path.Combine(_commonPartialPath, "AddressTest1.png");
-			using Bitmap qRCodeInputImage = new(path);
-			var dataByteArray = decoder.SearchQrCodes(qRCodeInputImage);
-			Assert.Single(dataByteArray);
-
 			string otherPath = Path.Combine(_commonPartialPath, "NotBitcoinAddress.jpg");
 			using Bitmap notValidInputImage = new(otherPath);
 			var notValidDataByteArray = decoder.SearchQrCodes(notValidInputImage);
+
 			Assert.Empty(notValidDataByteArray);
 		}
 
