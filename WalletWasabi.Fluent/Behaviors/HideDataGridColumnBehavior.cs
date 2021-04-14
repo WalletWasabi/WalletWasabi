30dc3ce03270d5c922b712e703e14cf8475c0c0d
Roland Soós <soos.roland93@gmail.com>
2021-04-14 16:07:43 +0200

    behavior for hiding columns

---
+++ WalletWasabi.Fluent/Behaviors/HideDataGridColumnBehavior.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using System.Reactive.Disposables;
+using Avalonia;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace WalletWasabi.Fluent.Behaviors
+{
+	public class HideDataGridColumnBehavior : DisposingBehavior<DataGrid>
+	{
+		public static readonly StyledProperty<double> HideThresholdWidthProperty =
+			AvaloniaProperty.Register<HideDataGridColumnBehavior, double>(nameof(HideThresholdWidth));
+
+		public static readonly StyledProperty<string> ColumnHeaderProperty =
+			AvaloniaProperty.Register<HideDataGridColumnBehavior, string>(nameof(ColumnHeader));
+
+		public double HideThresholdWidth
+		{
+			get => GetValue(HideThresholdWidthProperty);
+			set => SetValue(HideThresholdWidthProperty, value);
+		}
+
+		public string ColumnHeader
+		{
+			get => GetValue(ColumnHeaderProperty);
+			set => SetValue(ColumnHeaderProperty, value);
+		}
+
+		protected override void OnAttached(CompositeDisposable disposables)
+		{
+			AssociatedObject?
+				.WhenAnyValue(x => x.Bounds.Width)
+				.Subscribe(width =>
+				{
+					var firstOrDefault = AssociatedObject.Columns.FirstOrDefault(x => ReferenceEquals(x.Header, ColumnHeader));
+
+					if (firstOrDefault is { } column)
+					{
+						column.IsVisible = width > HideThresholdWidth;
+					}
+				});
+		}
+	}
+}
