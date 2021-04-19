abf365f86523de296dfb36de037e166fd6e23e75
Dan Walmsley <dan@walms.co.uk>
2021-04-19 22:28:25 +0100

    tmp test code.

--- WalletWasabi.Fluent/Views/MainWindow.axaml.cs
+++ WalletWasabi.Fluent/Views/MainWindow.axaml.cs
@@ -1,6 +1,10 @@
+using System.IO;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using WalletWasabi.Fluent.QRCodeDecoder;
 
 namespace WalletWasabi.Fluent.Views
 {
@@ -15,6 +19,26 @@ namespace WalletWasabi.Fluent.Views
 		{
 			AvaloniaXamlLoader.Load(this);
 			this.AttachDevTools();
+
+			QRDecoder decoder = new();
+			string expectedAddress = "tb1ql27ya3gufs5h0ptgjhjd0tm52fq6q0xrav7xza";
+			string otherExpectedAddress = "tb1qfas0k9rn8daqggu7wzp2yne9qdd5fr5wf2u478";
+
+			using (var fs = File.OpenRead("AddressTest1.png"))
+			{
+				using (var bmp = WriteableBitmap.Decode(fs))
+				{
+					var dataCollection = decoder.SearchQrCodes(bmp);
+				}
+			}
+
+			using (var fs = File.OpenRead("AddressTest2.png"))
+			{
+				using (var bmp = WriteableBitmap.Decode(fs))
+				{
+					var dataCollection = decoder.SearchQrCodes(bmp);
+				}
+			}
 		}
 	}
 }
