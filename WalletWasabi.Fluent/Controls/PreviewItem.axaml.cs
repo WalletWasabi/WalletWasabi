75330f31835add02bab9c4c337f3d43e3c5d3d06
Roland Soós <soos.roland93@gmail.com>
2021-04-19 16:52:10 +0200

    add Copy button to PreviewItem

--- WalletWasabi.Fluent/Controls/PreviewItem.axaml.cs
+++ WalletWasabi.Fluent/Controls/PreviewItem.axaml.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
+using ReactiveUI;
 
 namespace WalletWasabi.Fluent.Controls
 {
@@ -15,6 +18,30 @@ namespace WalletWasabi.Fluent.Controls
 
 		public static readonly StyledProperty<double> IconSizeProperty =
 			AvaloniaProperty.Register<PreviewItem, double>(nameof(IconSize), 24);
+
+		public static readonly StyledProperty<object?> CopyParameterProperty =
+			AvaloniaProperty.Register<PreviewItem, object?>(nameof(CopyParameter));
+
+		public static readonly StyledProperty<ICommand> CopyCommandProperty =
+			AvaloniaProperty.Register<PreviewItem, ICommand>(nameof(CopyCommand));
+
+		public static readonly StyledProperty<bool> CopyButtonVisibilityProperty =
+			AvaloniaProperty.Register<PreviewItem, bool>(nameof(CopyButtonVisibility));
+
+		public PreviewItem()
+		{
+			CopyCommand = ReactiveCommand.CreateFromTask<object>(async obj =>
+			{
+				if (obj.ToString() is { } text)
+				{
+					await Application.Current.Clipboard.SetTextAsync(text);
+				}
+			});
+
+			this.WhenAnyValue(x => x.CopyParameter)
+				.Select(x => !string.IsNullOrEmpty(x?.ToString()))
+				.Subscribe(x => CopyButtonVisibility = x);
+		}
 
 		public string Text
 		{
@@ -32,6 +59,24 @@ namespace WalletWasabi.Fluent.Controls
 		{
 			get => GetValue(IconSizeProperty);
 			set => SetValue(IconSizeProperty, value);
+		}
+
+		public object? CopyParameter
+		{
+			get => GetValue(CopyParameterProperty);
+			set => SetValue(CopyParameterProperty, value);
+		}
+
+		public ICommand CopyCommand
+		{
+			get => GetValue(CopyCommandProperty);
+			set => SetValue(CopyCommandProperty, value);
+		}
+
+		public bool CopyButtonVisibility
+		{
+			get => GetValue(CopyButtonVisibilityProperty);
+			set => SetValue(CopyButtonVisibilityProperty, value);
 		}
 	}
 }
