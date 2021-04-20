94b21c9df616ad22a71417fd9071c1109b0f4124
HaveaKillerDay <45069029+adamPetho@users.noreply.github.com>
2021-04-20 15:29:02 +0200

    Tests fixed

--- WalletWasabi.Fluent/Views/MainWindow.axaml.cs
+++ WalletWasabi.Fluent/Views/MainWindow.axaml.cs
@@ -19,26 +19,6 @@ namespace WalletWasabi.Fluent.Views
 		{
 			AvaloniaXamlLoader.Load(this);
 			this.AttachDevTools();
-
-			QRDecoder decoder = new();
-			string expectedAddress = "tb1ql27ya3gufs5h0ptgjhjd0tm52fq6q0xrav7xza";
-			string otherExpectedAddress = "tb1qfas0k9rn8daqggu7wzp2yne9qdd5fr5wf2u478";
-
-			using (var fs = File.OpenRead("AddressTest1.png"))
-			{
-				using (var bmp = WriteableBitmap.Decode(fs))
-				{
-					var dataCollection = decoder.SearchQrCodes(bmp);
-				}
-			}
-
-			using (var fs = File.OpenRead("AddressTest2.png"))
-			{
-				using (var bmp = WriteableBitmap.Decode(fs))
-				{
-					var dataCollection = decoder.SearchQrCodes(bmp);
-				}
-			}
 		}
 	}
 }
--- WalletWasabi.Tests/UnitTests/QrCode/QrCodeDecodingTests.cs
+++ WalletWasabi.Tests/UnitTests/QrCode/QrCodeDecodingTests.cs
@@ -17,51 +17,51 @@ namespace WalletWasabi.Tests.UnitTests.QrCode
 		[Fact]
 		public void GetCorrectAddressFromImage()
 		{
-			using (var app = Start())
-			{
-				QRDecoder decoder = new();
-				string expectedAddress = "tb1ql27ya3gufs5h0ptgjhjd0tm52fq6q0xrav7xza";
-				string otherExpectedAddress = "tb1qfas0k9rn8daqggu7wzp2yne9qdd5fr5wf2u478";
+			using var app = Start();
+			QRDecoder decoder = new();
+			string expectedAddress = "tb1ql27ya3gufs5h0ptgjhjd0tm52fq6q0xrav7xza";
+			string otherExpectedAddress = "tb1qfas0k9rn8daqggu7wzp2yne9qdd5fr5wf2u478";
 
-				string path = Path.Combine(_commonPartialPath, "AddressTest1.png");
-				using var qRCodeInputImage = LoadBitmap(path);
-				var dataCollection = decoder.SearchQrCodes(qRCodeInputImage);
+			string path = Path.Combine(_commonPartialPath, "AddressTest1.png");
+			using var bmp = LoadBitmap(path);
+			var dataCollection = decoder.SearchQrCodes(bmp);
 
-				Assert.NotEmpty(dataCollection);
-				Assert.Equal(expectedAddress, dataCollection.First());
+			Assert.NotEmpty(dataCollection);
+			Assert.Equal(expectedAddress, dataCollection.First());
 
-				string otherPath = Path.Combine(_commonPartialPath, "AddressTest2.png");
-				using var otherQRCodeInputImage = LoadBitmap(otherPath);
-				var dataCollection2 = decoder.SearchQrCodes(otherQRCodeInputImage);
+			string otherPath = Path.Combine(_commonPartialPath, "AddressTest2.png");
+			using var otherQRCodeInputImage = LoadBitmap(otherPath);
+			var dataCollection2 = decoder.SearchQrCodes(otherQRCodeInputImage);
 
-				Assert.NotEmpty(dataCollection2);
-				Assert.Equal(otherExpectedAddress, dataCollection2.First());
+			Assert.NotEmpty(dataCollection2);
+			Assert.Equal(otherExpectedAddress, dataCollection2.First());
 
-				Assert.NotEqual(dataCollection, dataCollection2);
-			}
+			Assert.NotEqual(dataCollection, dataCollection2);
 		}
 
-		/*[Fact]
+		[Fact]
 		public void IncorrectImageReturnsEmpty()
 		{
+			using var app = Start();
 			QRDecoder decoder = new();
 
-			string otherPath = Path.Combine(_commonPartialPath, "NotBitcoinAddress.jpg");
-			using Bitmap notValidInputImage = new(otherPath);
-			var notValidDataByteArray = decoder.SearchQrCodes(notValidInputImage);
+			string path = Path.Combine(_commonPartialPath, "NotBitcoinAddress.jpg");
+			using var bmp = LoadBitmap(path);
+			var dataCollection = decoder.SearchQrCodes(bmp);
 
-			Assert.Empty(notValidDataByteArray);
+			Assert.Empty(dataCollection);
 		}
 
 		[Fact]
 		public void DecodePictureTakenByPhone()
 		{
+			using var app = Start();
 			QRDecoder decoder = new();
 			string expectedOutput = "tb1qutgpgraaze3hqnvt2xyw5acsmd3urprk3ff27d";
 
 			string path = Path.Combine(_commonPartialPath, "QrByPhone.jpg");
-			using Bitmap inputImage = new(path);
-			var dataCollection = decoder.SearchQrCodes(inputImage);
+			using var bmp = LoadBitmap(path);
+			var dataCollection = decoder.SearchQrCodes(bmp);
 
 			Assert.Single(dataCollection);
 			Assert.Equal(expectedOutput, dataCollection.First());
@@ -70,12 +70,13 @@ namespace WalletWasabi.Tests.UnitTests.QrCode
 		[Fact]
 		public void DecodeDifficultPictureTakenByPhone()
 		{
+			using var app = Start();
 			QRDecoder decoder = new();
 			string expectedOutput = "Top right corner";
 
 			string path = Path.Combine(_commonPartialPath, "QrBrick.jpg");
-			using Bitmap inputImage = new(path);
-			var dataCollection = decoder.SearchQrCodes(inputImage);
+			using var bmp = LoadBitmap(path);
+			var dataCollection = decoder.SearchQrCodes(bmp);
 
 			Assert.Single(dataCollection);
 			Assert.Equal(expectedOutput, dataCollection.First());
@@ -84,12 +85,13 @@ namespace WalletWasabi.Tests.UnitTests.QrCode
 		[Fact]
 		public void DecodePictureWithZebraBackground()
 		{
+			using var app = Start();
 			QRDecoder decoder = new();
 			string expectedOutput = "Let's see a Zebra.";
 
 			string path = Path.Combine(_commonPartialPath, "QRwithZebraBackground.png");
-			using Bitmap inputImage = new(path);
-			var dataCollection = decoder.SearchQrCodes(inputImage);
+			using var bmp = LoadBitmap(path);
+			var dataCollection = decoder.SearchQrCodes(bmp);
 
 			Assert.Single(dataCollection);
 			Assert.Equal(expectedOutput, dataCollection.First());
@@ -98,12 +100,13 @@ namespace WalletWasabi.Tests.UnitTests.QrCode
 		[Fact]
 		public void DecodePictureWithMouseOverTheQRCodeReturnsNull()
 		{
+			using var app = Start();
 			QRDecoder decoder = new();
 			string expectedOutput = "tb1qutgpgraaze3hqnvt2xyw5acsmd3urprk3ff27d";
 
 			string path = Path.Combine(_commonPartialPath, "mouseOverQR.jpg");
-			using Bitmap inputImage = new(path);
-			var dataCollection = decoder.SearchQrCodes(inputImage);
+			using var bmp = LoadBitmap(path);
+			var dataCollection = decoder.SearchQrCodes(bmp);
 
 			Assert.Single(dataCollection);
 			Assert.Equal(expectedOutput, dataCollection.First());
@@ -112,12 +115,13 @@ namespace WalletWasabi.Tests.UnitTests.QrCode
 		[Fact]
 		public void DecodePictureWithImageInsideTheQR()
 		{
+			using var app = Start();
 			QRDecoder decoder = new();
 			string expectedOutput = "bitcoin:bc1q3r0ayktdsh8yd3krk5zkvpc5weeqnmw8ztzsgx?amount=1000&label=GIMME%201000BTC";
 
 			string path = Path.Combine(_commonPartialPath, "Payment_details_included.jpg");
-			using Bitmap inputImage = new(path);
-			var dataCollection = decoder.SearchQrCodes(inputImage);
+			using var bmp = LoadBitmap(path);
+			var dataCollection = decoder.SearchQrCodes(bmp);
 
 			Assert.Equal(expectedOutput, dataCollection.First());
 		}
@@ -125,13 +129,13 @@ namespace WalletWasabi.Tests.UnitTests.QrCode
 		[Fact]
 		public void DecodePictureWithLegacyAddress()
 		{
+			using var app = Start();
 			QRDecoder decoder = new();
 			string expectedOutput = "bitcoin:1EYTGtG4LnFfiMvjJdsU7GMGCQvsRSjYhx";
 
 			string path = Path.Combine(_commonPartialPath, "Random_address_starting_with_1.png");
-			using Bitmap inputImage = new(path);
-
-			var dataCollection = decoder.SearchQrCodes(inputImage);
+			using var bmp = LoadBitmap(path);
+			var dataCollection = decoder.SearchQrCodes(bmp);
 
 			Assert.Equal(expectedOutput, dataCollection.First());
 		}
@@ -139,28 +143,27 @@ namespace WalletWasabi.Tests.UnitTests.QrCode
 		[Fact]
 		public void DecodeQrCodeInsideQrCode()
 		{
+			using var app = Start();
 			QRDecoder decoder = new();
+			string firstExpectedOutput = "Big QR Code";
+			string secondExpectedOutput = "Small QR Code";
+			int expectedLength = 2;
 
 			// If there are two QR Codes on the picture, it will find both, even if they are
 			// stacked on eachother, until the QR Code's quality is okay and is still readable
 			string path = Path.Combine(_commonPartialPath, "DoubleQRCode.png");
-			string firstExpectedOutput = "Big QR Code";
-			string secondExpectedOutput = "Small QR Code";
-			using Bitmap inputImage = new(path);
-			int expectedLength = 2;
-			var dataCollection = decoder.SearchQrCodes(inputImage).ToArray();
+			using var bmp = LoadBitmap(path);
+			var dataCollection = decoder.SearchQrCodes(bmp).ToArray();
 
 			Assert.Equal(expectedLength, dataCollection.Length);
 			Assert.Equal(firstExpectedOutput, dataCollection[0]);
 			Assert.Equal(secondExpectedOutput, dataCollection[1]);
-		}*/
+		}
 
 		private static WriteableBitmap LoadBitmap(string path)
 		{
-			using (var fs = File.OpenRead(path))
-			{
-				return WriteableBitmap.Decode(fs);
-			}
+			using var fs = File.OpenRead(path);
+			return WriteableBitmap.Decode(fs);
 		}
 
 		private static IDisposable Start()
