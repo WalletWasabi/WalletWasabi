4da45270bf393c23e558a30247e5cfe38b7fcb8b
Yahia Chiheb <chihebyahia@gmail.com>
2021-04-25 16:06:13 +0100

    Convert switch statement to expression

--- WalletWasabi.Fluent/ViewModels/Dialogs/InsufficientBalanceDialogViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Dialogs/InsufficientBalanceDialogViewModel.cs
@@ -1,4 +1,4 @@
-﻿using NBitcoin;
+using NBitcoin;
 using ReactiveUI;
 using WalletWasabi.Blockchain.TransactionBuilding;
 using WalletWasabi.Fluent.Helpers;
@@ -22,18 +22,12 @@ namespace WalletWasabi.Fluent.ViewModels.Dialogs
 			var fiatFeeText = fee.ToDecimal(MoneyUnit.BTC).GenerateFiatText(usdExchangeRate, "USD");
 			FeeText = $"{btcFeeText}{fiatFeeText}";
 
-			switch (type)
+			Caption = type switch
 			{
-				case BalanceType.Private:
-					Caption = $"There are not enough private funds to cover the transaction fee. Alternatively you could:";
-					break;
-				case BalanceType.Pocket:
-					Caption = $"There are not enough funds selected to cover the transaction fee. Alternatively you could:";
-					break;
-				default:
-					Caption = $"There are not enough funds available to cover the transaction fee. Alternatively you could:";
-					break;
-			}
+				BalanceType.Private => $"There are not enough private funds to cover the transaction fee. Alternatively you could:",
+				BalanceType.Pocket => $"There are not enough funds selected to cover the transaction fee. Alternatively you could:",
+				_ => $"There are not enough funds available to cover the transaction fee. Alternatively you could:",
+			};
 
 			NextCommand = ReactiveCommand.Create(() => Close(result: true));
 			CancelCommand = ReactiveCommand.Create(() => Close(DialogResultKind.Cancel));
