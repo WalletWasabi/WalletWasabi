4132963bb9243339f167dec2d96af6fc5c947eae
Yahia Chiheb <chihebyahia@gmail.com>
2021-04-27 12:28:17 +0100

    Use tabs

--- WalletWasabi/Crypto/Randomness/SecureRandom.cs
+++ WalletWasabi/Crypto/Randomness/SecureRandom.cs
@@ -3,41 +3,41 @@ using System.Security.Cryptography;
 
 namespace WalletWasabi.Crypto.Randomness
 {
-    public class SecureRandom : WasabiRandom, IDisposable
-    {
-        private bool _disposedValue;
+	public class SecureRandom : WasabiRandom, IDisposable
+	{
+		private bool _disposedValue;
 
-        public SecureRandom()
-        {
-            Random = RandomNumberGenerator.Create();
-        }
+		public SecureRandom()
+		{
+			Random = RandomNumberGenerator.Create();
+		}
 
-        private RandomNumberGenerator Random { get; }
+		private RandomNumberGenerator Random { get; }
 
-        public override void GetBytes(byte[] buffer) => Random.GetBytes(buffer);
+		public override void GetBytes(byte[] buffer) => Random.GetBytes(buffer);
 
-        public override void GetBytes(Span<byte> buffer) => Random.GetBytes(buffer);
+		public override void GetBytes(Span<byte> buffer) => Random.GetBytes(buffer);
 
-        public override int GetInt(int fromInclusive, int toExclusive) => RandomNumberGenerator.GetInt32(fromInclusive, toExclusive);
+		public override int GetInt(int fromInclusive, int toExclusive) => RandomNumberGenerator.GetInt32(fromInclusive, toExclusive);
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!_disposedValue)
-            {
-                if (disposing)
-                {
-                    Random?.Dispose();
-                }
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!_disposedValue)
+			{
+				if (disposing)
+				{
+					Random?.Dispose();
+				}
 
-                _disposedValue = true;
-            }
-        }
+				_disposedValue = true;
+			}
+		}
 
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-    }
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+	}
 }
--- WalletWasabi/Tor/TorProcessManager.cs
+++ WalletWasabi/Tor/TorProcessManager.cs
@@ -11,152 +11,152 @@ using WalletWasabi.Tor.Socks5;
 
 namespace WalletWasabi.Tor
 {
-    /// <summary>
-    /// Manages lifetime of Tor process.
-    /// </summary>
-    /// <seealso href="https://2019.www.torproject.org/docs/tor-manual.html.en"/>
-    public class TorProcessManager
-    {
-        public TorProcessManager(TorSettings settings, EndPoint torSocks5EndPoint)
-        {
-            TorSocks5EndPoint = torSocks5EndPoint;
-            TorProcess = null;
-            Settings = settings;
-            TcpConnectionFactory = new(torSocks5EndPoint);
-        }
+	/// <summary>
+	/// Manages lifetime of Tor process.
+	/// </summary>
+	/// <seealso href="https://2019.www.torproject.org/docs/tor-manual.html.en"/>
+	public class TorProcessManager
+	{
+		public TorProcessManager(TorSettings settings, EndPoint torSocks5EndPoint)
+		{
+			TorSocks5EndPoint = torSocks5EndPoint;
+			TorProcess = null;
+			Settings = settings;
+			TcpConnectionFactory = new(torSocks5EndPoint);
+		}
 
-        private EndPoint TorSocks5EndPoint { get; }
+		private EndPoint TorSocks5EndPoint { get; }
 
-        private ProcessAsync? TorProcess { get; set; }
+		private ProcessAsync? TorProcess { get; set; }
 
-        private TorSettings Settings { get; }
-        private TorTcpConnectionFactory TcpConnectionFactory { get; }
+		private TorSettings Settings { get; }
+		private TorTcpConnectionFactory TcpConnectionFactory { get; }
 
-        private bool _disposed = false;
+		private bool _disposed = false;
 
-        /// <summary>
-        /// Starts Tor process if it is not running already.
-        /// </summary>
-        public async Task<bool> StartAsync()
-        {
-            ThrowIfDisposed();
+		/// <summary>
+		/// Starts Tor process if it is not running already.
+		/// </summary>
+		public async Task<bool> StartAsync()
+		{
+			ThrowIfDisposed();
 
-            try
-            {
-                // Is Tor already running? Either our Tor process from previous Wasabi Wallet run or possibly user's own Tor.
-                bool isAlreadyRunning = await TcpConnectionFactory.IsTorRunningAsync().ConfigureAwait(false);
+			try
+			{
+				// Is Tor already running? Either our Tor process from previous Wasabi Wallet run or possibly user's own Tor.
+				bool isAlreadyRunning = await TcpConnectionFactory.IsTorRunningAsync().ConfigureAwait(false);
 
-                if (isAlreadyRunning)
-                {
-                    string msg = TorSocks5EndPoint is IPEndPoint endpoint
-                        ? $"Tor is already running on {endpoint.Address}:{endpoint.Port}."
-                        : "Tor is already running.";
-                    Logger.LogInfo(msg);
-                    return true;
-                }
+				if (isAlreadyRunning)
+				{
+					string msg = TorSocks5EndPoint is IPEndPoint endpoint
+						? $"Tor is already running on {endpoint.Address}:{endpoint.Port}."
+						: "Tor is already running.";
+					Logger.LogInfo(msg);
+					return true;
+				}
 
-                string torArguments = Settings.GetCmdArguments(TorSocks5EndPoint);
+				string torArguments = Settings.GetCmdArguments(TorSocks5EndPoint);
 
-                var startInfo = new ProcessStartInfo
-                {
-                    FileName = Settings.TorBinaryFilePath,
-                    Arguments = torArguments,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                    RedirectStandardOutput = true,
-                    WorkingDirectory = Settings.TorBinaryDir
-                };
+				var startInfo = new ProcessStartInfo
+				{
+					FileName = Settings.TorBinaryFilePath,
+					Arguments = torArguments,
+					UseShellExecute = false,
+					CreateNoWindow = true,
+					RedirectStandardOutput = true,
+					WorkingDirectory = Settings.TorBinaryDir
+				};
 
-                if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    var env = startInfo.EnvironmentVariables;
+				if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+				{
+					var env = startInfo.EnvironmentVariables;
 
-                    env["LD_LIBRARY_PATH"] = !env.ContainsKey("LD_LIBRARY_PATH") || string.IsNullOrEmpty(env["LD_LIBRARY_PATH"])
-                        ? Settings.TorBinaryDir
-                        : Settings.TorBinaryDir + Path.PathSeparator + env["LD_LIBRARY_PATH"];
+					env["LD_LIBRARY_PATH"] = !env.ContainsKey("LD_LIBRARY_PATH") || string.IsNullOrEmpty(env["LD_LIBRARY_PATH"])
+						? Settings.TorBinaryDir
+						: Settings.TorBinaryDir + Path.PathSeparator + env["LD_LIBRARY_PATH"];
 
-                    Logger.LogDebug($"Environment variable 'LD_LIBRARY_PATH' set to: '{env["LD_LIBRARY_PATH"]}'.");
-                }
+					Logger.LogDebug($"Environment variable 'LD_LIBRARY_PATH' set to: '{env["LD_LIBRARY_PATH"]}'.");
+				}
 
-                TorProcess = new(startInfo);
+				TorProcess = new(startInfo);
 
-                Logger.LogInfo($"Starting Tor process ...");
-                TorProcess.Start();
+				Logger.LogInfo($"Starting Tor process ...");
+				TorProcess.Start();
 
-                // Ensure it's running.
-                int i = 0;
-                while (true)
-                {
-                    i++;
+				// Ensure it's running.
+				int i = 0;
+				while (true)
+				{
+					i++;
 
-                    bool isRunning = await TcpConnectionFactory.IsTorRunningAsync().ConfigureAwait(false);
+					bool isRunning = await TcpConnectionFactory.IsTorRunningAsync().ConfigureAwait(false);
 
-                    if (isRunning)
-                    {
-                        break;
-                    }
+					if (isRunning)
+					{
+						break;
+					}
 
-                    if (TorProcess.HasExited)
-                    {
-                        Logger.LogError("Tor process failed to start!");
-                        return false;
-                    }
+					if (TorProcess.HasExited)
+					{
+						Logger.LogError("Tor process failed to start!");
+						return false;
+					}
 
-                    const int MaxAttempts = 25;
+					const int MaxAttempts = 25;
 
-                    if (i >= MaxAttempts)
-                    {
-                        Logger.LogError($"All {MaxAttempts} attempts to connect to Tor failed.");
-                        return false;
-                    }
+					if (i >= MaxAttempts)
+					{
+						Logger.LogError($"All {MaxAttempts} attempts to connect to Tor failed.");
+						return false;
+					}
 
-                    // Wait 250 milliseconds between attempts.
-                    await Task.Delay(250).ConfigureAwait(false);
-                }
+					// Wait 250 milliseconds between attempts.
+					await Task.Delay(250).ConfigureAwait(false);
+				}
 
-                Logger.LogInfo("Tor is running.");
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError("Could not automatically start Tor. Try running Tor manually.", ex);
-            }
+				Logger.LogInfo("Tor is running.");
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Logger.LogError("Could not automatically start Tor. Try running Tor manually.", ex);
+			}
 
-            return false;
-        }
+			return false;
+		}
 
-        public async Task StopAsync(bool killTor = false)
-        {
-            if (TorProcess is { } && killTor)
-            {
-                Logger.LogInfo($"Killing Tor process.");
+		public async Task StopAsync(bool killTor = false)
+		{
+			if (TorProcess is { } && killTor)
+			{
+				Logger.LogInfo($"Killing Tor process.");
 
-                try
-                {
-                    TorProcess.Kill();
-                    using CancellationTokenSource cts = new(TimeSpan.FromMinutes(1));
-                    await TorProcess.WaitForExitAsync(cts.Token, killOnCancel: true).ConfigureAwait(false);
+				try
+				{
+					TorProcess.Kill();
+					using CancellationTokenSource cts = new(TimeSpan.FromMinutes(1));
+					await TorProcess.WaitForExitAsync(cts.Token, killOnCancel: true).ConfigureAwait(false);
 
-                    Logger.LogInfo($"Tor process killed successfully.");
-                }
-                catch (Exception ex)
-                {
-                    Logger.LogError($"Could not kill Tor process: {ex.Message}.");
-                }
-            }
+					Logger.LogInfo($"Tor process killed successfully.");
+				}
+				catch (Exception ex)
+				{
+					Logger.LogError($"Could not kill Tor process: {ex.Message}.");
+				}
+			}
 
-            // Dispose Tor process resources (does not stop/kill Tor process).
-            TorProcess?.Dispose();
+			// Dispose Tor process resources (does not stop/kill Tor process).
+			TorProcess?.Dispose();
 
-            _disposed = true;
-        }
+			_disposed = true;
+		}
 
-        private void ThrowIfDisposed()
-        {
-            if (_disposed)
-            {
-                throw new ObjectDisposedException(GetType().FullName);
-            }
-        }
-    }
+		private void ThrowIfDisposed()
+		{
+			if (_disposed)
+			{
+				throw new ObjectDisposedException(GetType().FullName);
+			}
+		}
+	}
 }
