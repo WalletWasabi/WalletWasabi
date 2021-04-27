beb68b564f7ef626b11e717fdfb7a3bb9437a425
Lucas Ontivero <lucasontivero@gmail.com>
2021-04-27 12:19:22 -0300

    Move JsonConverters to better place
    Move the new json converters to the models' folder where they are
    closer to the serilizable models and prevent mixing the new converters
    with the old converters.
    
    Additionally move the list of converters to the
    `JsonSerializationOptions` class in order to share the exact same
    settings in the client and server code.

--- WalletWasabi/WabiSabi/Models/Serialization/WitScriptJsonConverter.cs
+++ WalletWasabi/WabiSabi/Models/Serialization/WitScriptJsonConverter.cs
@@ -2,24 +2,21 @@ using NBitcoin;
 using Newtonsoft.Json;
 using System;
 
-namespace WalletWasabi.JsonConverters
+namespace WalletWasabi.WabiSabi.Models.Serialization
 {
 	public class WitScriptJsonConverter : JsonConverter
 	{
-		/// <inheritdoc />
 		public override bool CanConvert(Type objectType)
 		{
 			return objectType == typeof(WitScript);
 		}
 
-		/// <inheritdoc />
 		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
 		{
 			var value = (string)reader.Value;
 			return new WitScript(ByteHelpers.FromHex(value));
 		}
 
-		/// <inheritdoc />
 		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
 		{
 			var bytes = ((WitScript)value).ToBytes();
