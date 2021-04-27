bd031a11b0a4689b3c5e8f7ae6446f4db248ea19
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-04-27 17:42:48 +0200

    Revert to AvaloniaList

--- WalletWasabi.Fluent/ViewModels/ResponsivePanelDemo/ResponsiveLayoutDemoViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/ResponsivePanelDemo/ResponsiveLayoutDemoViewModel.cs
@@ -11,8 +11,8 @@ namespace WalletWasabi.Fluent.ViewModels.ResponsivePanelDemo
 		[AutoNotify] private double _itemHeight = 150;
 		[AutoNotify] private double _widthSource = 990;
 		[AutoNotify] private double _aspectRatio = double.NaN;
-		[AutoNotify] private List<int> _columnHints = new() { 1, 2, 3, 4 };
-		[AutoNotify] private List<double> _widthTriggers = new() { 0, 660, 990, 1320 };
+		[AutoNotify] private AvaloniaList<int> _columnHints = new() { 1, 2, 3, 4 };
+		[AutoNotify] private AvaloniaList<double> _widthTriggers = new() { 0, 660, 990, 1320 };
 		[AutoNotify] private ObservableCollection<TestTileItemViewModel> _items;
 
 		public ResponsiveLayoutDemoViewModel()
