763175289894789db3c182c20f281300f2505782
Szpoti <balage.farago@gmail.com>
2021-05-04 10:15:52 +0200

    Minor cleanup in Bob

--- WalletWasabi/WabiSabi/Backend/Models/Bob.cs
+++ WalletWasabi/WabiSabi/Backend/Models/Bob.cs
@@ -1,20 +1,11 @@
 using NBitcoin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WalletWasabi.Helpers;
 
 namespace WalletWasabi.WabiSabi.Backend.Models
 {
-	public record Bob(
-		Script Script,
-		/// <summary>
-		/// This is slightly larger than the final TXO amount,
-		/// because the fees are coming down from this.
-		/// </summary>
-		long CredentialAmount)
+	/// <summary>
+	/// </summary>
+	/// <param name="CredentialAmount"> This is slightly larger than the final TXO amount,because the fees are coming down from this.</param>
+	public record Bob(Script Script, long CredentialAmount)
 	{
 		public int OutputVsize
 			=> Script.EstimateOutputVsize();
