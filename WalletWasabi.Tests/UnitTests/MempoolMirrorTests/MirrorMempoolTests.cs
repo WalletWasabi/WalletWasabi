2b06403847fb44963a580601d37e647801e3ed10
adamPetho <45069029+adamPetho@users.noreply.github.com>
2021-05-06 15:33:22 +0200

    Added test

--- WalletWasabi.Tests/UnitTests/MempoolMirrorTests/MirrorMempoolTests.cs
+++ WalletWasabi.Tests/UnitTests/MempoolMirrorTests/MirrorMempoolTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using NBitcoin;
 using WalletWasabi.BitcoinCore.Mempool;
@@ -128,6 +129,67 @@ namespace WalletWasabi.Tests.UnitTests.MempoolMirrorTests
 
 				Assert.Single(localMempool);
 				Assert.Contains(tx.GetHash(), localMempool.Keys);
+			}
+			finally
+			{
+				await services.StopAllAsync();
+				await coreNode.TryStopAsync();
+			}
+		}
+
+		[Fact]
+		public async Task CanHandleConfirmationAsync()
+		{
+			var coreNode = await TestNodeBuilder.CreateAsync();
+			using HostedServices services = new();
+			using MempoolMirror mempoolMirror = new(TimeSpan.FromSeconds(2), coreNode.RpcClient, coreNode.P2pNode);
+			services.Register<MempoolMirror>(mempoolMirror, "Mempool Mirror");
+			try
+			{
+				var rpc = coreNode.RpcClient;
+				var network = rpc.Network;
+				await services.StartAllAsync();
+				var mempoolInstance = services.Get<MempoolMirror>();
+
+				var walletName = "RandomWalletName";
+				await rpc.CreateWalletAsync(walletName);
+
+				using var k1 = new Key();
+				var blockId = await rpc.GenerateToAddressAsync(1, k1.PubKey.WitHash.GetAddress(network));
+				var block = await rpc.GetBlockAsync(blockId[0]);
+				var coinBaseTx = block.Transactions[0];
+
+				var tx = Transaction.Create(network);
+				using var k2 = new Key();
+				tx.Inputs.Add(coinBaseTx, 0);
+				tx.Outputs.Add(Money.Coins(49.9999m), k2.PubKey.WitHash.GetAddress(network));
+				tx.Sign(k1.GetBitcoinSecret(network), coinBaseTx.Outputs.AsCoins().First());
+				var valid = tx.Check();
+
+				await rpc.GenerateAsync(101);
+
+				var rpcMempoolBeforeSend = await rpc.GetRawMempoolAsync();
+				var localMempoolBeforeSend = mempoolInstance.GetMempool();
+				Assert.Equal(rpcMempoolBeforeSend.Length, localMempoolBeforeSend.Count);
+
+				await rpc.SendRawTransactionAsync(tx);
+				await mempoolInstance.TriggerAndWaitRoundAsync(TimeSpan.FromSeconds(2));
+				Thread.Sleep(5000);
+
+				var rpcMempoolAfterSend = await rpc.GetRawMempoolAsync();
+				var localMempoolAfterSend = mempoolInstance.GetMempool();
+				Assert.Equal(rpcMempoolAfterSend.Length, localMempoolAfterSend.Count);
+				Assert.Single(localMempoolAfterSend);
+				Assert.Single(rpcMempoolAfterSend);
+
+				await rpc.GenerateAsync(1);
+				await mempoolInstance.TriggerAndWaitRoundAsync(TimeSpan.FromSeconds(2));
+
+				var rpcMempoolAfterBlockMined = await rpc.GetRawMempoolAsync();
+				var localMempoolAfterBlockMined = mempoolInstance.GetMempool();
+				Assert.Equal(rpcMempoolAfterBlockMined.Length, localMempoolAfterBlockMined.Count);
+				Assert.Empty(rpcMempoolAfterBlockMined);
+				Assert.Empty(localMempoolAfterBlockMined);
 			}
 			finally
 			{
