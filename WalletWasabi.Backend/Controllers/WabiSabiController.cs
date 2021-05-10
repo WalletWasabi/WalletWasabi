c63962548dc43c31205c7132441f5d7f82562bf4
nopara73 <adam.ficsor73@gmail.com>
2021-05-10 12:03:22 +0200

    Fix backend

--- WalletWasabi.Backend/Controllers/WabiSabiController.cs
+++ WalletWasabi.Backend/Controllers/WabiSabiController.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WalletWasabi.WabiSabi;
 using WalletWasabi.WabiSabi.Backend.PostRequests;
 using WalletWasabi.WabiSabi.Models;
 
@@ -11,46 +12,46 @@ namespace WalletWasabi.Backend.Controllers
 	[Route("[controller]")]
 	public class WabiSabiController : ControllerBase
 	{
-		private IArenaRequestHandler handler;
+		private IArenaRequestHandler Handler { get; }
 
-		public WabiSabiController(IArenaRequestHandler handler)
+		public WabiSabiController(Global global)
 		{
-			this.handler = handler;
+			Handler = global.HostedServices.Get<WabiSabiCoordinator>().Postman;
 		}
 
 		public Task<ConnectionConfirmationResponse> ConfirmConnectionAsync(ConnectionConfirmationRequest request, CancellationToken cancellableToken)
 		{
-			return handler.ConfirmConnectionAsync(request, cancellableToken);
+			return Handler.ConfirmConnectionAsync(request, cancellableToken);
 		}
 
 		[HttpPost("input-registration")]
 		public Task<InputRegistrationResponse> RegisterInputAsync(InputRegistrationRequest request, CancellationToken cancellableToken)
 		{
-			return handler.RegisterInputAsync(request, cancellableToken);
+			return Handler.RegisterInputAsync(request, cancellableToken);
 		}
 
 		[HttpPost("output-registration")]
 		public Task<OutputRegistrationResponse> RegisterOutputAsync(OutputRegistrationRequest request, CancellationToken cancellableToken)
 		{
-			return handler.RegisterOutputAsync(request, cancellableToken);
+			return Handler.RegisterOutputAsync(request, cancellableToken);
 		}
 
 		[HttpPost("credential-issuance")]
 		public Task<ReissueCredentialResponse> ReissueCredentialAsync(ReissueCredentialRequest request, CancellationToken cancellableToken)
 		{
-			return handler.ReissueCredentialAsync(request, cancellableToken);
+			return Handler.ReissueCredentialAsync(request, cancellableToken);
 		}
 
 		[HttpPost("input-unregistration")]
 		public Task RemoveInputAsync(InputsRemovalRequest request, CancellationToken cancellableToken)
 		{
-			return handler.RemoveInputAsync(request, cancellableToken);
+			return Handler.RemoveInputAsync(request, cancellableToken);
 		}
 
 		[HttpPost("transaction-signature")]
 		public Task SignTransactionAsync(TransactionSignaturesRequest request, CancellationToken cancellableToken)
 		{
-			return handler.SignTransactionAsync(request, cancellableToken);
+			return Handler.SignTransactionAsync(request, cancellableToken);
 		}
 	}
 }
