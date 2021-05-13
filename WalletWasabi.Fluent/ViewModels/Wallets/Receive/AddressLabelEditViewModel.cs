9f42527e43bd1063ef899d1fa0a24a4d37939f9c
Roland Soós <soos.roland93@gmail.com>
2021-05-13 09:56:58 +0200

    make the save work

--- WalletWasabi.Fluent/ViewModels/Wallets/Receive/AddressLabelEditViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Receive/AddressLabelEditViewModel.cs
@@ -1,5 +1,4 @@
 using System.Collections.ObjectModel;
-using System.Linq;
 using ReactiveUI;
 using WalletWasabi.Blockchain.Analysis.Clustering;
 using WalletWasabi.Blockchain.Keys;
@@ -10,22 +9,15 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Receive
 	[NavigationMetaData(Title = "Edit Labels")]
 	public partial class AddressLabelEditViewModel : RoutableViewModel
 	{
-		[AutoNotify] private string _label;
 		[AutoNotify] private ObservableCollection<string> _labels;
 
 		public AddressLabelEditViewModel(HdPubKey hdPubKey, KeyManager keyManager)
 		{
 			_labels = new(hdPubKey.Label);
-			_label = "";
 
 			NextCommand = ReactiveCommand.Create(() =>
 			{
-				hdPubKey = keyManager.GetKeys(x => x == hdPubKey).FirstOrDefault();
-
-				hdPubKey?.SetLabel(new SmartLabel(Label), kmToFile: keyManager);
-
-				// hdPubKey.SetLabel(new SmartLabel(Label), kmToFile: keyManager);
-
+				hdPubKey.SetLabel(new SmartLabel(Labels), kmToFile: keyManager);
 				Navigate().Back();
 			});
 		}
