b2ab91831a1f59b0a265f0ff3ca2de713cc65a92
David Molnar <molnardavid84@gmail.com>
2021-05-17 13:47:55 +0200

    Fix typo

--- WalletWasabi/Tor/Socks5/Pool/Identities/OneOffIdentity.cs
+++ WalletWasabi/Tor/Socks5/Pool/Identities/OneOffIdentity.cs
@@ -2,13 +2,14 @@ using WalletWasabi.Crypto.Randomness;
 
 namespace WalletWasabi.Tor.Socks5.Pool.Identities
 {
-	/// <summary>Random idendity for an HTTP requests which should not be linked with any other HTTP request.</summary>
+	/// <summary>Random identity for an HTTP requests which should not be linked with any other HTTP request.</summary>
 	public class OneOffIdentity : IIdentity
-	{		
+	{
 		public OneOffIdentity()
 		{
 			Name = RandomString.CapitalAlphaNumeric(21);
 		}
+
 		public string Name { get; }
 
 		/// <inheritdoc/>
