6f5f4f6e42b9617cb60843cb0fdd64f5586d6173
Lucas Ontivero <lucasontivero@gmail.com>
2021-05-17 16:11:30 -0300

    Add missing timeout properties to `RoundParameters`

--- WalletWasabi/WabiSabi/Backend/Rounds/RoundParameters.cs
+++ WalletWasabi/WabiSabi/Backend/Rounds/RoundParameters.cs
@@ -19,15 +19,19 @@ namespace WalletWasabi.WabiSabi.Backend.Rounds
 			Random = random;
 			FeeRate = feeRate;
 
-			MaxInputCountByAlice = wabiSabiConfig.MaxInputCountByAlice;
+			MaxInputCountByRound = wabiSabiConfig.MaxInputCountByRound;
+			MinInputCountByRound = wabiSabiConfig.MinInputCountByRound;
 			MinRegistrableAmount = wabiSabiConfig.MinRegistrableAmount;
 			MaxRegistrableAmount = wabiSabiConfig.MaxRegistrableAmount;
+			MaxInputCountByAlice = wabiSabiConfig.MaxInputCountByAlice;
 			PerAliceVsizeAllocation = wabiSabiConfig.PerAliceVsizeAllocation;
 
 			// Note that input registration timeouts can be modified runtime.
+			StandardInputRegistrationTimeout = wabiSabiConfig.StandardInputRegistrationTimeout;
 			ConnectionConfirmationTimeout = wabiSabiConfig.ConnectionConfirmationTimeout;
 			OutputRegistrationTimeout = wabiSabiConfig.OutputRegistrationTimeout;
 			TransactionSigningTimeout = wabiSabiConfig.TransactionSigningTimeout;
+			BlameInputRegistrationTimeout = wabiSabiConfig.BlameInputRegistrationTimeout;
 
 			BlameOf = blameOf;
 			IsBlameRound = BlameOf is not null;
@@ -41,15 +45,19 @@ namespace WalletWasabi.WabiSabi.Backend.Rounds
 		public WasabiRandom Random { get; }
 		public FeeRate FeeRate { get; }
 		public Network Network { get; }
-		public uint MaxInputCountByAlice { get; }
+		public uint MinInputCountByRound { get; }
+		public uint MaxInputCountByRound { get; }
 		public Money MinRegistrableAmount { get; }
 		public Money MaxRegistrableAmount { get; }
+		public uint MaxInputCountByAlice { get; }
 		public uint PerAliceVsizeAllocation { get; }
 		public Round? BlameOf { get; }
 		public bool IsBlameRound { get; }
 		public ISet<OutPoint> BlameWhitelist { get; }
+		public TimeSpan StandardInputRegistrationTimeout { get; }
 		public TimeSpan ConnectionConfirmationTimeout { get; }
 		public TimeSpan OutputRegistrationTimeout { get; }
 		public TimeSpan TransactionSigningTimeout { get; }
+		public TimeSpan BlameInputRegistrationTimeout { get; }
 	}
 }
