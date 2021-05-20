17f38fa9c4ae53125480dd12511a522095559f6e
David Molnar <molnardavid84@gmail.com>
2021-05-20 14:23:19 +0200

    Remove unused.

--- WalletWasabi/WabiSabi/Models/Decomposition/BaseDenomGenerator.cs
+++ WalletWasabi/WabiSabi/Models/Decomposition/BaseDenomGenerator.cs
@@ -2,8 +2,6 @@ using NBitcoin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WalletWasabi.WabiSabi.Models.Decomposition
 {
