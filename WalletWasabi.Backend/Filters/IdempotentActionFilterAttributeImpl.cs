afdb5b04b8b65b768abbc1bd7fe6f56c62e407e4
Lucas Ontivero <lucasontivero@gmail.com>
2021-05-23 15:47:04 -0300

    Fix Idempotent action filter
    This action takes place after the http body stream has already
    been consumed by the model binder and there is not way to get its
    raw value again.
    
    Given the model binder has already created the model and made it
    available, we serialize it again in order to have a text
    representation that can be hashed to be used in combination with
    the request path to generate an unique `cache-key`.

--- WalletWasabi.Backend/Filters/IdempotentActionFilterAttributeImpl.cs
+++ WalletWasabi.Backend/Filters/IdempotentActionFilterAttributeImpl.cs
@@ -2,10 +2,11 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using WalletWasabi.WabiSabi.Models.Serialization;
 
 namespace WalletWasabi.Backend.Filters
 {
@@ -23,18 +24,27 @@ namespace WalletWasabi.Backend.Filters
 		{
 			var request = context.HttpContext.Request;
 
-			var (cacheKey, body) = await GetCacheEntryKeyAsync(request).ConfigureAwait(false);
-
-			if (_cache.TryGetValue<ObjectResult>(cacheKey, out var cachedResponse))
+			if (context.ModelState.IsValid)
 			{
-				context.Result = cachedResponse;
-				context.HttpContext.Items.Remove("cached-key");
-				return;
+				if (context.ActionArguments.TryGetValue("request", out var model))
+				{
+					var cacheKey = GetCacheEntryKey(request.Path, model);
+
+					if (_cache.TryGetValue<ObjectResult>(cacheKey, out var cachedResponse))
+					{
+						context.Result = cachedResponse;
+						context.HttpContext.Items.Remove("cached-key");
+						return;
+					}
+
+					context.HttpContext.Items["cached-key"] = cacheKey;
+				}
+				else
+				{
+					throw new InvalidOperationException("Control actions marked as Idempotent must receive a 'request' argument.");
+				}
 			}
-
-			context.HttpContext.Items["cached-key"] = cacheKey;
-
- 			await next().ConfigureAwait(false);
+			await next().ConfigureAwait(false);
 		}
 
 		public override void OnResultExecuted(ResultExecutedContext context)
@@ -45,16 +55,14 @@ namespace WalletWasabi.Backend.Filters
 			}
 		}
 
-		private async Task<(string, string)> GetCacheEntryKeyAsync(HttpRequest request)
+		private string GetCacheEntryKey(string path, object model)
 		{
-			using var reader = new System.IO.StreamReader(request.Body);
-			var body = await reader.ReadToEndAsync().ConfigureAwait(false);
-
-			var rawKey = string.Join(":", request.Path, body);
+			var json = JsonConvert.SerializeObject(model, JsonSerializationOptions.Default.Settings);
+			var rawKey = string.Join(":", path, json);
 			using var sha256Hash = SHA256.Create();
 			var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawKey));
 
-			return ("arena-request-cache-key: " + ByteHelpers.ToHex(bytes), body);
+			return "arena-request-cache-key: " + ByteHelpers.ToHex(bytes);
 		}
 	}
 }
