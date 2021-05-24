47483e8fc4085a89be8df313d9f5181ac7e69ab9
MDavid <molnardavid84@gmail.com>
2021-05-24 13:25:58 +0200

    Use LINQ

--- WalletWasabi/WabiSabi/Models/Decomposition/BaseDenomGenerator.cs
+++ WalletWasabi/WabiSabi/Models/Decomposition/BaseDenomGenerator.cs
@@ -7,23 +7,10 @@ namespace WalletWasabi.WabiSabi.Models.Decomposition
 {
 	public static class BaseDenominationGenerator
 	{
-		private const long MaxSats = 2099999997690000;
+		private static Money MaxSats = Money.Satoshis(2099999997690000);
 
-		private static IEnumerable<long> Multiple(IEnumerable<int> coefficients, IEnumerable<long> values)
-		{
-			foreach (var v in values)
-			{
-				foreach (var c in coefficients)
-				{
-					var x = c * v;
-					if (x > MaxSats)
-					{
-						break;
-					}
-					yield return x;
-				}
-			}
-		}
+		private static IEnumerable<Money> Multiple(IEnumerable<long> coefficients, IEnumerable<long> values) =>
+			values.SelectMany(v => coefficients.Select(c => Money.Satoshis(c * v))).Where(x => x <= MaxSats);
 
 		public static IEnumerable<Money> Generate()
 		{
@@ -31,10 +18,14 @@ namespace WalletWasabi.WabiSabi.Models.Decomposition
 			var powersOfThree = Enumerable.Range(0, 32).Select(x => (long)Math.Pow(3, x));
 			var powersOfTen = Enumerable.Range(0, 16).Select(x => (long)Math.Pow(10, x));
 
-			var ternary = Multiple(new[] { 1, 2 }, powersOfThree);
-			var preferredValueSeries = Multiple(new[] { 1, 2, 5 }, powersOfTen);
+			var ternary = Multiple(new long[] { 1, 2 }, powersOfThree);
+			var preferredValueSeries = Multiple(new long[] { 1, 2, 5 }, powersOfTen);
 
-			return powersOfTwo.Union(ternary).Union(preferredValueSeries).OrderBy(v => v).Select(v => Money.Satoshis(v));
+			return powersOfTwo
+				.Select(x => Money.Satoshis(x))
+				.Union(ternary)
+				.Union(preferredValueSeries)
+				.OrderBy(v => v);
 		}
 	}
 }
