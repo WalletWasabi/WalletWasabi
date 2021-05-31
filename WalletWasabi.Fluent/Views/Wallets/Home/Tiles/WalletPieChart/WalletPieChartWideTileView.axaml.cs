9d7a63f89d88d99281bc58bf83aee8556a783d82
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-31 15:38:08 +0200

    Added WalletPieChart tile stubs

---
+++ WalletWasabi.Fluent/Views/Wallets/Home/Tiles/WalletPieChart/WalletPieChartWideTileView.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Views.Wallets.Home.Tiles.WalletPieChart
+{
+	public class WalletPieChartWideTileView : UserControl
+	{
+		public WalletPieChartWideTileView()
+		{
+			InitializeComponent();
+		}
+
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+	}
+}
