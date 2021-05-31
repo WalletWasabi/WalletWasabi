55fa42a62bb0d07790a5d40f7a4793d9246ae454
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-31 11:24:38 +0200

    Add SuggestionLabelViewModel class

---
+++ WalletWasabi.Fluent/ViewModels/Wallets/Receive/SuggestionLabelViewModel.cs
@@ -0,0 +1,17 @@
+using System.Windows.Input;
+
+namespace WalletWasabi.Fluent.ViewModels.Wallets.Receive
+{
+	public partial class SuggestionLabelViewModel : ViewModelBase
+	{
+		public SuggestionLabelViewModel(string suggestion, ICommand selectedCommand)
+		{
+			Suggestion = suggestion;
+			SelectedCommand = selectedCommand;
+		}
+
+		public  string Suggestion { get; }
+		
+		public  ICommand SelectedCommand { get; }
+	}
+}
