70101c3c00a192f9bbd92c601d3f7b8f3e8a4cc4
MDavid <molnardavid84@gmail.com>
2021-06-01 22:40:15 +0200

    Remove DynamicProxyGenAssembly2

--- WalletWasabi/AssemblyInfo.cs
+++ WalletWasabi/AssemblyInfo.cs
@@ -1,6 +1,3 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("WalletWasabi.Tests")]
-
-// For moq access to internals of this project.
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
