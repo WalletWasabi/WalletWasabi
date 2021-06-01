397fe5fa9ec2668c2a08ed0496de3be51f267958
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-06-01 18:35:29 +0200

    Add private and non-private balance properties

--- WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/WalletBalanceTileViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/WalletBalanceTileViewModel.cs
@@ -13,6 +13,8 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Home.Tiles
 		private readonly IObservable<Unit> _balanceChanged;
 		[AutoNotify] private string? _balanceBtc;
 		[AutoNotify] private string? _balanceFiat;
+		[AutoNotify] private string? _balancePrivateBtc;
+		[AutoNotify] private string? _balanceNonPrivateBtc;
 
 		public WalletBalanceTileViewModel(Wallet wallet, IObservable<Unit> balanceChanged)
 		{
@@ -38,6 +40,16 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Home.Tiles
 
 			BalanceFiat = _wallet.Coins.TotalAmount().ToDecimal(MoneyUnit.BTC)
 				.GenerateFiatText(_wallet.Synchronizer.UsdExchangeRate, "USD");
+
+			var privateThreshold = _wallet.ServiceConfiguration.GetMixUntilAnonymitySetValue();
+			var privateCoins = _wallet.Coins.FilterBy(x => x.HdPubKey.AnonymitySet >= privateThreshold);
+			var normalCoins = _wallet.Coins.FilterBy(x => x.HdPubKey.AnonymitySet < privateThreshold);
+
+			BalancePrivateBtc = privateCoins.TotalAmount().ToDecimal(MoneyUnit.BTC)
+				.FormattedBtc() + " BTC";
+
+			BalanceNonPrivateBtc = normalCoins.TotalAmount().ToDecimal(MoneyUnit.BTC)
+				.FormattedBtc() + " BTC";
 		}
 	}
 }
