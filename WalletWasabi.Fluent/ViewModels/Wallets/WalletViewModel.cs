df67138c22d58eaa2ff9efe845dc45454f96b3bd
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-06-01 18:41:48 +0200

    Change tiles layout for development

--- WalletWasabi.Fluent/ViewModels/Wallets/WalletViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/WalletViewModel.cs
@@ -77,9 +77,9 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 			{
 				TilePresets = new ObservableCollection<TilePresetViewModel>()
 				{
-					new(0, 0, 1, 1, TileSize.Small),
-					new(0, 0, 1, 1, TileSize.Small),
-					new(0, 0, 1, 1, TileSize.Small)
+					new(3, 0, 1, 1, TileSize.Small),
+					new(0, 1, 1, 1, TileSize.Normal),
+					new(1, 1, 1, 1, TileSize.Normal)
 				},
 				TilePresetIndex = LayoutIndex
 			};
@@ -97,9 +97,9 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 			{
 				TilePresets = new ObservableCollection<TilePresetViewModel>()
 				{
-					new(2, 0, 1, 1, TileSize.Small),
-					new(2, 0, 1, 1, TileSize.Small),
-					new(0, 1, 1, 1, TileSize.Normal)
+					new(0, 0, 1, 1, TileSize.Small),
+					new(0, 0, 1, 1, TileSize.Small),
+					new(0, 0, 1, 1, TileSize.Small)
 				},
 				TilePresetIndex = LayoutIndex
 			};
@@ -107,9 +107,9 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 			{
 				TilePresets = new ObservableCollection<TilePresetViewModel>()
 				{
-					new(3, 0, 1, 1, TileSize.Small),
-					new(0, 1, 1, 1, TileSize.Normal),
-					new(1, 1, 1, 1, TileSize.Normal)
+					new(2, 0, 1, 1, TileSize.Small),
+					new(2, 0, 1, 1, TileSize.Small),
+					new(0, 1, 1, 1, TileSize.Normal)
 				},
 				TilePresetIndex = LayoutIndex
 			};
