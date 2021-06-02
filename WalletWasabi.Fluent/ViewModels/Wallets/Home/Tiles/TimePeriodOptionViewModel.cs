fdd1effb2c8f81ae75c2ef728c35a480e914e421
Roland Soós <soos.roland93@gmail.com>
2021-06-02 13:32:55 +0200

    fix CodeFactor

--- WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/TimePeriodOptionViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/TimePeriodOptionViewModel.cs
@@ -8,7 +8,6 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Home.Tiles
 {
 	public partial class TimePeriodOptionViewModel
 	{
-		public TimePeriodOption Option { get; }
 		[AutoNotify] private bool _isSelected;
 
 		public TimePeriodOptionViewModel(TimePeriodOption option, Action<TimePeriodOptionViewModel> updateAction)
@@ -17,6 +16,8 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Home.Tiles
 			Text = option.FriendlyName();
 			SelectCommand = ReactiveCommand.Create(() => updateAction(this));
 		}
+
+		public TimePeriodOption Option { get; }
 
 		public string Text { get; }
 
