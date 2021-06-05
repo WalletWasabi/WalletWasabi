e90a578990d93b4a8e8dacfc97236531337854e9
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-06-05 14:06:27 +0200

    Fix

--- WalletWasabi.Fluent/ViewModels/Wallets/WalletViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/WalletViewModel.cs
@@ -121,7 +121,7 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 				{
 					new(4, 0, 1, 1, TileSize.Medium),
 					new(1, 1, 2, 1, TileSize.Wide),
-					new(1, 1, 1, 1, TileSize.Normal)
+					new(1, 1, 1, 1, TileSize.Large)
 				},
 				TilePresetIndex = LayoutIndex
 			};
