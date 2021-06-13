455945af6bc064412f65e0d30589cd543ca3a293
Lucas Ontivero <lucasontivero@gmail.com>
2021-06-13 20:11:17 -0300

    Do not regenerate denominations

--- WalletWasabi.Tests/UnitTests/WabiSabi/Models/StandardDenominationsTests.cs
+++ WalletWasabi.Tests/UnitTests/WabiSabi/Models/StandardDenominationsTests.cs
@@ -1,16 +1,15 @@
-using NBitcoin;
 using System.Linq;
 using WalletWasabi.WabiSabi.Models.Decomposition;
 using Xunit;
 
 namespace WalletWasabi.Tests.UnitTests.WabiSabi.Models
 {
-	public class AmountDecompositionTests
+	public class StandardDenominationsTests
 	{
 		[Fact]
 		public void BaseDenomGeneratorTest()
 		{
-			var denoms = BaseDenominationGenerator.Generate();
+			var denoms = StandardDenomination.Values;
 			Assert.Equal(157, denoms.Count());
 			Assert.Equal(denoms, denoms.Distinct());
 
