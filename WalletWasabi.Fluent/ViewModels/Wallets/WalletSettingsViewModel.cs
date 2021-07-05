be9e1f407db538d4ea498d2c89009c21052182ff
Roland Soós <soos.roland93@gmail.com>
2021-07-05 14:20:12 +0200

    move PSBT workflow toggle button into Wallet settings

--- WalletWasabi.Fluent/ViewModels/Wallets/WalletSettingsViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/WalletSettingsViewModel.cs
@@ -1,3 +1,5 @@
+using System;
+using ReactiveUI;
 using WalletWasabi.Fluent.ViewModels.Navigation;
 
 namespace WalletWasabi.Fluent.ViewModels.Wallets
@@ -5,11 +7,27 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 	[NavigationMetaData(Title="Wallet Settings")]
 	public partial class WalletSettingsViewModel : RoutableViewModel
 	{
-		public WalletSettingsViewModel(WalletViewModel walletViewModel)
+		[AutoNotify] private bool _preferPsbtWorkflow;
+
+		public WalletSettingsViewModel(WalletViewModelBase walletViewModelBase)
 		{
+			var wallet = walletViewModelBase.Wallet;
+			_preferPsbtWorkflow = wallet.KeyManager.PreferPsbtWorkflow;
+			IsHardwareWallet = wallet.KeyManager.IsHardwareWallet;
+
 			SetupCancel(enableCancel: false, enableCancelOnEscape: true, enableCancelOnPressed: true);
 
 			NextCommand = CancelCommand;
+
+			this.WhenAnyValue(x => x.PreferPsbtWorkflow)
+				.Subscribe(value =>
+				{
+					wallet.KeyManager.PreferPsbtWorkflow = value;
+					wallet.KeyManager.ToFile();
+					walletViewModelBase.RaisePropertyChanged(nameof(walletViewModelBase.PreferPsbtWorkflow));
+				});
 		}
+
+		public bool IsHardwareWallet { get; }
 	}
 }
