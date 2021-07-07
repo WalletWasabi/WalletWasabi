0aa4d615111c17f3d1a424c52ad9ad7c7d36ad42
Lucas Ontivero <lucasontivero@gmail.com>
2021-07-06 23:45:38 -0300

    Add multi-participants test

---
+++ WalletWasabi.Tests/UnitTests/WabiSabi/Integration/Participant.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NBitcoin;
+using WalletWasabi.BitcoinCore.Rpc;
+using WalletWasabi.Blockchain.Keys;
+using WalletWasabi.WabiSabi.Client;
+using WalletWasabi.Wallets;
+
+namespace WalletWasabi.Tests.UnitTests.WabiSabi.Integration
+{
+	internal class Participant
+	{
+		public Participant(IRPCClient rpc, WabiSabiHttpApiClient apiClient)
+		{
+			Rpc = rpc;
+			ApiClient = apiClient;
+
+			KeyManager = KeyManager.CreateNew(out var _, password: "");
+			KeyManager.AssertCleanKeysIndexed();
+		}
+
+		public KeyManager KeyManager { get; }
+		public List<Coin> Coins { get; } = new ();
+		public IRPCClient Rpc { get; }
+		public WabiSabiHttpApiClient ApiClient { get; }
+
+		public async Task InitializeAsync(int numberOfCoins, CancellationToken cancellationToken)
+		{
+			var keys = KeyManager.GetKeys().Take(numberOfCoins);
+			foreach (var key in keys)
+			{
+				cancellationToken.ThrowIfCancellationRequested();
+				var blockIds = await Rpc.GenerateToAddressAsync(1, key.GetP2wpkhAddress(Network.Main));
+				var block = await Rpc.GetBlockAsync(blockIds.First());
+				var coin = block.Transactions[0].Outputs.GetCoins(key.P2wpkhScript).First();
+				Coins.Add(coin);
+			}
+		}
+
+		public async Task StartParticipatingAsync(CancellationToken cancellationToken)
+		{
+			using var roundStateUpdater = new RoundStateUpdater(TimeSpan.FromSeconds(1), ApiClient);
+			await roundStateUpdater.StartAsync(cancellationToken);
+
+			var kitchen = new Kitchen();
+			kitchen.Cook("");
+
+			var coinJoinClient = new CoinJoinClient(ApiClient, Coins, kitchen, KeyManager, roundStateUpdater);
+
+			// Run the coinjoin client task.
+			await coinJoinClient.StartCoinJoinAsync(cancellationToken);
+
+			await roundStateUpdater.StopAsync(cancellationToken);
+		}
+	}
+}
