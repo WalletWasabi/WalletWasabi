805d4ef0621c2ef1f5b010fdf0c56fc18e1b8692
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-07-08 12:54:03 +0200

    Add RemoveClassAction action
    This is opposite to AddClassAction action.

---
+++ WalletWasabi.Fluent/Behaviors/RemoveClassAction.cs
@@ -0,0 +1,42 @@
+using Avalonia;
+using Avalonia.Xaml.Interactivity;
+
+namespace WalletWasabi.Fluent.Behaviors
+{
+	public class RemoveClassAction : AvaloniaObject, IAction
+	{
+		public static readonly StyledProperty<string> ClassNameProperty =
+			AvaloniaProperty.Register<AddClassAction, string>(nameof(ClassName));
+
+		public static readonly StyledProperty<IStyledElement?> StyledElementProperty =
+			AvaloniaProperty.Register<AddClassAction, IStyledElement?>(nameof(StyledElement));
+
+		public string ClassName
+		{
+			get => GetValue(ClassNameProperty);
+			set => SetValue(ClassNameProperty, value);
+		}
+
+		public IStyledElement? StyledElement
+		{
+			get => GetValue(StyledElementProperty);
+			set => SetValue(StyledElementProperty, value);
+		}
+
+		public object Execute(object? sender, object? parameter)
+		{
+			var target = GetValue(StyledElementProperty) is { } ? StyledElement : sender as IStyledElement;
+			if (target is null || string.IsNullOrEmpty(ClassName))
+			{
+				return false;
+			}
+
+			if (target.Classes.Contains(ClassName))
+			{
+				target.Classes.Remove(ClassName);
+			}
+
+			return true;
+		}
+	}
+}
