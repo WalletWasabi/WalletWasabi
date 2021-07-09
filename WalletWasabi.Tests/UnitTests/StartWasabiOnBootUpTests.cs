6e2f459dd997cf407cafdefd51f8af828aae0a9e
adamPetho <45069029+adamPetho@users.noreply.github.com>
2021-07-09 20:31:46 +0200

    Added simple test case

---
+++ WalletWasabi.Tests/UnitTests/StartWasabiOnBootUpTests.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Runtime.InteropServices;
+using WalletWasabi.Fluent.Helpers;
+using Xunit;
+
+namespace WalletWasabi.Tests.UnitTests
+{
+	public class StartWasabiOnBootUpTests
+	{
+		[Fact]
+		public void ModifyStartupOnDifferentSystemsTest()
+		{
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				StartupHelper.ModifyStartupSetting(true);
+				StartupHelper.ModifyStartupSetting(false);
+			}
+			else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				Assert.Throws<NotImplementedException>(() => StartupHelper.ModifyStartupSetting(true));
+				Assert.Throws<NotImplementedException>(() => StartupHelper.ModifyStartupSetting(false));
+			}
+			else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				Assert.Throws<NotImplementedException>(() => StartupHelper.ModifyStartupSetting(true));
+				Assert.Throws<NotImplementedException>(() => StartupHelper.ModifyStartupSetting(false));
+			}
+		}
+	}
+}
