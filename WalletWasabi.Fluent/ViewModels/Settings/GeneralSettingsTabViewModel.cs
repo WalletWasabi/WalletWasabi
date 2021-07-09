777a831eda1ce9fa0bd1e307be695c9db87e18ad
adamPetho <45069029+adamPetho@users.noreply.github.com>
2021-07-09 15:21:12 +0200

    Better caption for the notification

--- WalletWasabi.Fluent/ViewModels/Settings/GeneralSettingsTabViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Settings/GeneralSettingsTabViewModel.cs
@@ -68,7 +68,7 @@ namespace WalletWasabi.Fluent.ViewModels.Settings
 					catch (Exception ex)
 					{
 						Logger.LogError(ex);
-						await ShowErrorAsync(Title, "Couldn't save your change, please see the logs for further information.", "Something went wrong.");
+						await ShowErrorAsync(Title, "Couldn't save your change, please see the logs for further information.", "Error occured.");
 					}
 				});
 
