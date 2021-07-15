3013cabf628ea0ebf7a3c59a90e3a5b1211517ec
adamPetho <45069029+adamPetho@users.noreply.github.com>
2021-07-15 10:52:50 +0200

    Make appName Constant
    I need the Appname to be a Constant because the feature depends on it.
    If the appname changes in the Packager,
    but not in the StartupHelper, the feature just won't work at all.

--- WalletWasabi.Fluent/Helpers/StartupHelper.cs
+++ WalletWasabi.Fluent/Helpers/StartupHelper.cs
@@ -13,7 +13,7 @@ namespace WalletWasabi.Fluent.Helpers
 		private const string KeyPath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
 
 		// Arguments to add Wasabi to macOS startup settings.
-		private static readonly string AddCmd = $"osascript -e \' tell application \"System Events\" to make new login item at end with properties {{name:\"WasabiWallet\", path:\"/Applications/WasabiWallet.app\",hidden:false}} \'";
+		private static readonly string AddCmd = $"osascript -e \' tell application \"System Events\" to make new login item at end with properties {{name:\"WasabiWallet\", path:\"/Applications/{Constants.AppName}\",hidden:false}} \'";
 
 		// Arguments to delete Wasabi from macOS startup settings.
 		private static readonly string DeleteCmd = "osascript -e \' tell application \"System Events\" to delete login item \"WasabiWallet\" \'";
