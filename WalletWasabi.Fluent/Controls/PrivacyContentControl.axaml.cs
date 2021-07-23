75a612350ea03a98fd8b18aac46cff59a10ac006
Roland Soós <soos.roland93@gmail.com>
2021-07-23 11:08:36 +0200

    add PrivacyContentControl

---
+++ WalletWasabi.Fluent/Controls/PrivacyContentControl.axaml.cs
@@ -0,0 +1,44 @@
+using Avalonia;
+using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace WalletWasabi.Fluent.Controls
+{
+	public class PrivacyContentControl : ContentControl
+	{
+		public static readonly StyledProperty<bool> PrivacyModeEnabledProperty =
+			AvaloniaProperty.Register<PrivacyContentControl, bool>(nameof(PrivacyModeEnabled));
+
+		private bool PrivacyModeEnabled
+		{
+			get => GetValue(PrivacyModeEnabledProperty);
+			set => SetValue(PrivacyModeEnabledProperty, value);
+		}
+
+		private CompositeDisposable? _disposable;
+
+		protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+		{
+			base.OnAttachedToVisualTree(e);
+
+			_disposable = new CompositeDisposable();
+
+			Services.UiConfig
+				.WhenAnyValue(x => x.PrivacyMode)
+				.ObserveOn(RxApp.MainThreadScheduler)
+				.Subscribe(value => PrivacyModeEnabled = value)
+				.DisposeWith(_disposable);
+		}
+
+		protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+		{
+			base.OnDetachedFromVisualTree(e);
+
+			_disposable?.Dispose();
+			_disposable = null;
+		}
+	}
+}
