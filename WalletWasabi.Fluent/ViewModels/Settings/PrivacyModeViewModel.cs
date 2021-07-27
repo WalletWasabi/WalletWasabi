d0b2bf3b07f60cbee5e3d227e4f9ea999c069df7
Roland Soós <soos.roland93@gmail.com>
2021-07-27 12:24:23 +0200

    use incognito icon instead of eye

--- WalletWasabi.Fluent/ViewModels/Settings/PrivacyModeViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Settings/PrivacyModeViewModel.cs
@@ -30,7 +30,7 @@ namespace WalletWasabi.Fluent.ViewModels.Settings
 				});
 		}
 
-		public override string IconName => _privacyMode ? "eye_hide_regular" : "eye_show_regular";
+		public override string IconName => _privacyMode ? "incognito_filled" : "incognito_regular";
 
 		public override void Toggle()
 		{
