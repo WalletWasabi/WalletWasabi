fe57b83f29e08e6861dac12bae9aa4d8cf2804de
Roland Soós <soos.roland93@gmail.com>
2021-08-12 15:24:47 +0200

    Restore original PaymentInfo.cs file.

---
+++ WalletWasabi.Gui/Rpc/PaymentInfo.cs
@@ -0,0 +1,12 @@
+using NBitcoin;
+
+namespace WalletWasabi.Gui.Rpc
+{
+	public class PaymentInfo
+	{
+		public BitcoinAddress Sendto { get; set; }
+		public Money Amount { get; set; }
+		public string Label { get; set; }
+		public bool SubtractFee { get; set; }
+	}
+}
