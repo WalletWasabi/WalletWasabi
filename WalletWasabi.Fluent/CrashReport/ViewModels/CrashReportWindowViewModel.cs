8f6313bc0155982652df32670533138d9a983c19
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2021-08-12 17:24:50 +0800

    fix CrashReportWindowViewModel

--- WalletWasabi.Fluent/CrashReport/ViewModels/CrashReportWindowViewModel.cs
+++ WalletWasabi.Fluent/CrashReport/ViewModels/CrashReportWindowViewModel.cs
@@ -2,19 +2,17 @@ using System;
 using System.IO;
 using ReactiveUI;
 using System.Windows.Input;
+using WalletWasabi.Fluent.ViewModels;
 using WalletWasabi.Fluent.ViewModels.HelpAndSupport;
 using WalletWasabi.Models;
-using WalletWasabi.Gui.ViewModels;
 
 namespace WalletWasabi.Fluent.CrashReport.ViewModels
 {
-	public partial class CrashReportWindowViewModel : ViewModelBase
+	public class CrashReportWindowViewModel : ViewModelBase
 	{
-		[AutoNotify] private SerializableException _serializableException;
-
-		public CrashReportWindowViewModel(SerializableException exception)
+		public CrashReportWindowViewModel(SerializableException serializedException)
 		{
-			SerializableException = exception;
+			SerializedSerializedException = serializedException;
 			CancelCommand = ReactiveCommand.Create(CrashReporter.RestartWasabi);
 			NextCommand = ReactiveCommand.Create(CrashReporter.ShutdownWasabi);
 			OpenGitHubRepoCommand = ReactiveCommand.CreateFromTask(async () =>
@@ -23,14 +21,18 @@ namespace WalletWasabi.Fluent.CrashReport.ViewModels
 			});
 		}
 
+		public SerializableException SerializedSerializedException { get; }
+
 		public ICommand OpenGitHubRepoCommand { get; }
+
 		public ICommand NextCommand { get; }
+
 		public ICommand CancelCommand { get; }
 
 		public string Caption => $"A problem has occurred and Wasabi is unable to continue.";
 
-		public string Trace => $"{_serializableException.Message}{Environment.NewLine}" +
-		                       $"{Environment.NewLine}{_serializableException.StackTrace}";
+		public string Trace => $"{SerializedSerializedException.Message}{Environment.NewLine}" +
+		                       $"{Environment.NewLine}{SerializedSerializedException.StackTrace}";
 
 		public string Title => "Wasabi has crashed.";
 	}
