6d92611212942ac1dbf2684b68629d64453d98ba
adamPetho <45069029+adamPetho@users.noreply.github.com>
2021-08-16 16:12:12 +0200

    Clean up

--- WalletWasabi/Helpers/Startup/StartupChecker.cs
+++ WalletWasabi/Helpers/Startup/StartupChecker.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,26 +9,22 @@ namespace WalletWasabi.Helpers
 	{
 		public static async Task<bool> GetCurrentValueAsync(CancellationToken cancel = default)
 		{
-			bool result = false;
-
 			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 			{
-				result = WindowsStartupChecker.CheckRegistryKeyExists();
+				return WindowsStartupChecker.CheckRegistryKeyExists();
 			}
 			else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
 			{
-				result = await LinuxStartupChecker.CheckDesktopFileAsync().ConfigureAwait(false);
+				return await LinuxStartupChecker.CheckDesktopFileAsync().ConfigureAwait(false);
 			}
 			else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
 			{
-				result = await MacOsStartupChecker.CheckLoginItemExistsAsync().ConfigureAwait(false);
+				return await MacOsStartupChecker.CheckLoginItemExistsAsync().ConfigureAwait(false);
 			}
 			else
 			{
 				throw new NotImplementedException("Your platform is not supported yet.");
 			}
-
-			return result;
 		}
 	}
 }
