031a77548240597f7ff7294f4cfad6b5e59ef5c8
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2021-08-17 22:12:14 +0800

    Remove round status from the tile set in WalletViewModel.cs

--- WalletWasabi.Fluent/ViewModels/Wallets/WalletViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/WalletViewModel.cs
@@ -78,7 +78,7 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 
 			Layouts = new ObservableCollection<TileLayoutViewModel>()
 			{
-				new("Small", "330,330,330,330,330", "150"),
+				new("Small", "330,330,330,330", "150"),
 				new("Normal", "330,330,330", "150,300"),
 				new("Wide", "330,330", "150,300,300")
 			};
@@ -95,22 +95,12 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 				},
 				TilePresetIndex = LayoutIndex
 			};
-			RoundStatusTile = new RoundStatusTileViewModel(wallet)
-			{
-				TilePresets = new ObservableCollection<TilePresetViewModel>()
-				{
-					new(1, 0, 1, 1, TileSize.Medium),
-					new(1, 0, 1, 1, TileSize.Medium),
-					new(1, 0, 1, 1, TileSize.Medium)
-				},
-				TilePresetIndex = LayoutIndex
-			};
 			BtcPriceTile = new BtcPriceTileViewModel(wallet)
 			{
 				TilePresets = new ObservableCollection<TilePresetViewModel>()
 				{
 					new(2, 0, 1, 1, TileSize.Medium),
-					new(2, 0, 1, 1, TileSize.Medium),
+					new(1, 0, 1, 1, TileSize.Medium),
 					new(0, 1, 1, 1, TileSize.Large)
 				},
 				TilePresetIndex = LayoutIndex
@@ -119,7 +109,7 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 			{
 				TilePresets = new ObservableCollection<TilePresetViewModel>()
 				{
-					new(3, 0, 1, 1, TileSize.Medium),
+					new(1, 0, 1, 1, TileSize.Medium),
 					new(0, 1, 1, 1, TileSize.Large),
 					new(1, 1, 1, 1, TileSize.Large)
 				},
@@ -129,7 +119,7 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 			{
 				TilePresets = new ObservableCollection<TilePresetViewModel>()
 				{
-					new(4, 0, 1, 1, TileSize.Medium),
+					new(3, 0, 1, 1, TileSize.Medium),
 					new(1, 1, 2, 1, TileSize.Wide),
 					new(0, 2, 2, 1, TileSize.Wide)
 				},
@@ -139,7 +129,6 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 			_tiles = new List<TileViewModel>
 			{
 				BalanceTile,
-				RoundStatusTile,
 				BtcPriceTile,
 				WalletPieChart,
 				BalanceChartTile
@@ -212,8 +201,6 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 		public HistoryViewModel History { get; }
 
 		public WalletBalanceTileViewModel BalanceTile { get; }
-
-		public RoundStatusTileViewModel RoundStatusTile { get; }
 
 		public BtcPriceTileViewModel BtcPriceTile { get; }
 
