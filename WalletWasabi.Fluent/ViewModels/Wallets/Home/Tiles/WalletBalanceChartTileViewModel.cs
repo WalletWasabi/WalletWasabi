c11c2eb303a6fcb67a34570ae3d4a408b087a660
Roland Soós <soos.roland93@gmail.com>
2021-08-25 15:00:26 +0200

    take timezone into account

--- WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/WalletBalanceChartTileViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/WalletBalanceChartTileViewModel.cs
@@ -135,33 +135,23 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Home.Tiles
 				var range = DateTimeOffset.FromUnixTimeMilliseconds((long)maxX) -
 							DateTimeOffset.FromUnixTimeMilliseconds((long)minX);
 
+				var stringFormatOption = "MMM-d";
+
 				if (range <= TimeSpan.FromDays(1))
 				{
-					XLabels = new List<string>
-					{
-						DateTimeOffset.FromUnixTimeMilliseconds((long) minX).DateTime.ToString("t"),
-						DateTimeOffset.FromUnixTimeMilliseconds((long) halfX).DateTime.ToString("t"),
-						DateTimeOffset.FromUnixTimeMilliseconds((long) maxX).DateTime.ToString("t"),
-					};
+					stringFormatOption = "t";
 				}
 				else if (range <= TimeSpan.FromDays(7))
 				{
-					XLabels = new List<string>
-					{
-						DateTimeOffset.FromUnixTimeMilliseconds((long) minX).DateTime.ToString("ddd MMM-d"),
-						DateTimeOffset.FromUnixTimeMilliseconds((long) halfX).DateTime.ToString("ddd MMM-d"),
-						DateTimeOffset.FromUnixTimeMilliseconds((long) maxX).DateTime.ToString("ddd MMM-d"),
-					};
+					stringFormatOption = "ddd MMM-d";
 				}
-				else
+
+				XLabels = new List<string>
 				{
-					XLabels = new List<string>
-					{
-						DateTimeOffset.FromUnixTimeMilliseconds((long) minX).DateTime.ToString("MMM-d"),
-						DateTimeOffset.FromUnixTimeMilliseconds((long) halfX).DateTime.ToString("MMM-d"),
-						DateTimeOffset.FromUnixTimeMilliseconds((long) maxX).DateTime.ToString("MMM-d"),
-					};
-				}
+					DateTimeOffset.FromUnixTimeMilliseconds((long)minX).ToLocalTime().ToString(stringFormatOption),
+					DateTimeOffset.FromUnixTimeMilliseconds((long)halfX).ToLocalTime().ToString(stringFormatOption),
+					DateTimeOffset.FromUnixTimeMilliseconds((long)maxX).ToLocalTime().ToString(stringFormatOption),
+				};
 			}
 			else
 			{
