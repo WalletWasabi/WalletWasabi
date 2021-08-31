00aad33b48b6816392e02b0467ac64083929c921
David Molnar <molnardavid84@gmail.com>
2021-08-31 20:33:57 +0200

    Better evaluation of CoinJoin in progress

--- WalletWasabi/Wallets/WalletManager.cs
+++ WalletWasabi/Wallets/WalletManager.cs
@@ -439,7 +439,7 @@ namespace WalletWasabi.Wallets
 		{
 			lock (Lock)
 			{
-				return Wallets.Any(x => x.ChaumianClient?.State.AnyCoinsQueued() is true);
+				return Wallets.Any(w => w.Coins.CoinJoinInProcess().Any());
 			}
 		}
 	}
