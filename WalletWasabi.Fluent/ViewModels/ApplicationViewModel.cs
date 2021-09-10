f0499799694614b1a325d2df8913f944f545871d
adamPetho <45069029+adamPetho@users.noreply.github.com>
2021-09-10 13:59:56 +0200

    Use highestCoinJoinClientState

--- WalletWasabi.Fluent/ViewModels/ApplicationViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/ApplicationViewModel.cs
@@ -1,9 +1,19 @@
+using System;
 using WalletWasabi.Fluent.Providers;
+using WalletWasabi.WabiSabi.Client;
 
 namespace WalletWasabi.Fluent.ViewModels
 {
 	public partial class ApplicationViewModel : ViewModelBase, ICanShutdownProvider
 	{
-		bool ICanShutdownProvider.CanShutdown() => Services.WalletManager.AnyCoinJoinInProgress();
+		bool ICanShutdownProvider.CanShutdown()
+		{
+			return Services.HostedServices.Get<CoinJoinManager>().HighestCoinJoinClientState switch
+			{
+				CoinJoinClientState.InCriticalPhase => false,
+				CoinJoinClientState.Idle or CoinJoinClientState.InProgress => true,
+				_ => throw new ArgumentOutOfRangeException(),
+			};
+		}
 	}
 }
