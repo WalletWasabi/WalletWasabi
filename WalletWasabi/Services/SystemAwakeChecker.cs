8c0d93436b2a4e1f21773e32fc5e08281d0f03ba
adamPetho <45069029+adamPetho@users.noreply.github.com>
2021-09-13 17:26:17 +0200

    use LogTrace

--- WalletWasabi/Services/SystemAwakeChecker.cs
+++ WalletWasabi/Services/SystemAwakeChecker.cs
@@ -69,6 +69,7 @@ namespace WalletWasabi.Services
 			switch (CoinJoinManager.HighestCoinJoinClientState)
 			{
 				case CoinJoinClientState.Idle:
+					Logger.LogTrace("Computer idle state is allowed again.");
 					await StopTaskAsync().ConfigureAwait(false);
 					break;
 
