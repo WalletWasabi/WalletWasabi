35b3c63706c7e4e6d627fc3ac9c95bcbd62e1804
Roland Soós <soos.roland93@gmail.com>
2021-09-14 19:36:11 +0200

    tweak wording
    Co-authored-by: Max Hillebrand <30683012+MaxHillebrand@users.noreply.github.com>

--- WalletWasabi.Fluent/ViewModels/HelpAndSupport/BugReportLinkViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/HelpAndSupport/BugReportLinkViewModel.cs
@@ -6,7 +6,7 @@ namespace WalletWasabi.Fluent.ViewModels.HelpAndSupport
 {
 	[NavigationMetaData(
 		Title = "Report a Bug",
-		Caption = "Will open a website in default browser",
+		Caption = "Opens a website in the default browser",
 		Order = 1,
 		Category = "Help & Support",
 		Keywords = new[]
