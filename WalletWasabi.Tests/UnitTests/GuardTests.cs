61e4c59381b54c47611a2ca589079345d440ada1
Kiminuo <kiminuo@protonmail.com>
2021-10-03 15:05:26 +0200

    GuardTests: Fix warnings.

--- WalletWasabi.Tests/UnitTests/GuardTests.cs
+++ WalletWasabi.Tests/UnitTests/GuardTests.cs
@@ -10,7 +10,7 @@ namespace WalletWasabi.Tests.UnitTests
 		public void True()
 		{
 			Guard.True("foo", true);
-			Assert.Throws<ArgumentNullException>(() => Guard.True(null, true));
+			Assert.Throws<ArgumentNullException>(() => Guard.True(null!, true));
 			Assert.Throws<ArgumentException>(() => Guard.True("", true));
 			Assert.Throws<ArgumentException>(() => Guard.True("  ", true));
 			Assert.Throws<ArgumentOutOfRangeException>(() => Guard.True("foo", false));
@@ -21,7 +21,7 @@ namespace WalletWasabi.Tests.UnitTests
 		public void False()
 		{
 			Guard.False("foo", false);
-			Assert.Throws<ArgumentNullException>(() => Guard.False(null, false));
+			Assert.Throws<ArgumentNullException>(() => Guard.False(null!, false));
 			Assert.Throws<ArgumentException>(() => Guard.False("", false));
 			Assert.Throws<ArgumentException>(() => Guard.False("  ", false));
 			Assert.Throws<ArgumentOutOfRangeException>(() => Guard.False("foo", true));
