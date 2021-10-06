14a779c21a13c688d229e2af2a62fa5ae3b51c02
Roland Soós <soos.roland93@gmail.com>
2021-10-06 17:06:27 +0200

    lowecase cj status text

--- WalletWasabi.Fluent/ViewModels/Wallets/WalletViewModelBase.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/WalletViewModelBase.cs
@@ -46,7 +46,7 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets
 					}
 					else if (isCoinJoining)
 					{
-						StatusText = "CoinJoining";
+						StatusText = "Coinjoining";
 					}
 					else
 					{
