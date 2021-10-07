6e306500ab897706d93de146cf9e51de8091a3df
Lucas Ontivero <lucasontivero@gmail.com>
2021-10-07 14:09:25 -0300

    Code Review suggestions

--- WalletWasabi/WabiSabi/Backend/Rounds/TimeFrame.cs
+++ WalletWasabi/WabiSabi/Backend/Rounds/TimeFrame.cs
@@ -13,9 +13,9 @@ namespace WalletWasabi.WabiSabi.Backend.Rounds
 		public static TimeFrame Create(TimeSpan duration) =>
 			new (DateTimeOffset.MinValue, duration);
 
+		private DateTimeOffset EndTime => StartTime + Duration;
 		public DateTimeOffset StartTime { get; init; }
 		public TimeSpan Duration { get; init; }
-		public DateTimeOffset EndTime => StartTime + Duration;
 		public bool HasStarted => StartTime > DateTimeOffset.MinValue && StartTime < DateTimeOffset.UtcNow;
 		public bool HasExpired => HasStarted && EndTime < DateTimeOffset.UtcNow;
 		public TimeFrame StartNow() => this with { StartTime = DateTimeOffset.UtcNow };
