ecee768450018f2c9eed9f9bde4e06e9f237184c
Roland Soós <soos.roland93@gmail.com>
2021-10-19 16:16:29 +0200

    fix build

--- WalletWasabi.Fluent/Views/Wallets/Home/Tiles/PrivacyControlTileView.axaml.cs
+++ WalletWasabi.Fluent/Views/Wallets/Home/Tiles/PrivacyControlTileView.axaml.cs
@@ -1,17 +1,18 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace WalletWasabi.Fluent.Views.Wallets.Home.Tiles;
-
-public class PrivacyControlTileView : UserControl
+namespace WalletWasabi.Fluent.Views.Wallets.Home.Tiles
 {
-	public PrivacyControlTileView()
+	public class PrivacyControlTileView : UserControl
 	{
-		InitializeComponent();
-	}
+		public PrivacyControlTileView()
+		{
+			InitializeComponent();
+		}
 
-	private void InitializeComponent()
-	{
-		AvaloniaXamlLoader.Load(this);
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
 	}
 }
