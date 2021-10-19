0cb1ffcd91032eaa9493de7aaf9154b6fa6fab73
Roland Soós <soos.roland93@gmail.com>
2021-10-19 10:27:17 +0200

    add basic structure

--- WalletWasabi.Fluent/ViewModels/AddWallet/WelcomePageViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/AddWallet/WelcomePageViewModel.cs
@@ -1,4 +1,5 @@
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using ReactiveUI;
 using WalletWasabi.Fluent.ViewModels.Dialogs.Base;
@@ -8,8 +9,15 @@ namespace WalletWasabi.Fluent.ViewModels.AddWallet
 	[NavigationMetaData(Title = "Welcome")]
 	public partial class WelcomePageViewModel : DialogViewModelBase<Unit>
 	{
+		private const int NumberOfPages = 3;
+
+		[AutoNotify] private int _selectedIndex;
+
 		public WelcomePageViewModel(AddWalletPageViewModel addWalletPage)
 		{
+			SetupCancel(enableCancel: false, enableCancelOnEscape: false, enableCancelOnPressed: false);
+			EnableBack = false;
+
 			GetStartedCommand = ReactiveCommand.Create(() =>
 			{
 				if (!Services.WalletManager.HasWallet())
@@ -21,8 +29,15 @@ namespace WalletWasabi.Fluent.ViewModels.AddWallet
 					Close();
 				}
 			});
+
+			SelectedIndex = 0;
+
+			NextCommand = ReactiveCommand.Create(() => SelectedIndex++, this.WhenAnyValue(x => x.SelectedIndex).Select(c => c < NumberOfPages - 1));
+			PrevCommand = ReactiveCommand.Create(() => SelectedIndex--, this.WhenAnyValue(x => x.SelectedIndex).Select(c => c > 0));
 		}
 
 		public ICommand GetStartedCommand { get; }
+
+		public ICommand PrevCommand { get; }
 	}
 }
