63df97d0e5adec40d747164e3f25158249149030
Kiminuo <kiminuo@protonmail.com>
2021-10-21 22:32:29 +0200

    Revert "Remove Chaumian coinjoin clients."
    This reverts commit ef053ff1d6782c84b98f41a7bf96290b077fc308.

--- WalletWasabi.Tests/IntegrationTests/LiveServerTests.cs
+++ WalletWasabi.Tests/IntegrationTests/LiveServerTests.cs
@@ -7,6 +7,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.Backend.Models.Responses;
 using WalletWasabi.Blockchain.BlockFilters;
+using WalletWasabi.CoinJoin.Client.Clients;
+using WalletWasabi.Models;
 using WalletWasabi.Tests.Helpers;
 using WalletWasabi.Tests.XunitConfiguration;
 using WalletWasabi.Tor;
@@ -59,6 +61,17 @@ namespace WalletWasabi.Tests.IntegrationTests
 
 			Assert.NotNull(filtersResponse);
 			Assert.True(filtersResponse!.Filters.Count() == 2);
+		}
+
+		[Theory]
+		[MemberData(nameof(GetNetworks))]
+		public async Task GetAllRoundStatesAsync(Network network)
+		{
+			TorHttpClient torHttpClient = MakeTorHttpClient(network);
+			SatoshiClient client = new(torHttpClient);
+			var states = await client.GetAllRoundStatesAsync();
+			Assert.True(states.NotNullAndNotEmpty());
+			Assert.True(states.Any());
 		}
 
 		[Theory]
