54ae35224fb674a1d9af6477f1e56bd266b33b22
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2021-10-22 19:02:28 +0800

    Implement a new animated graphic for wallet loading screen.

---
+++ WalletWasabi.Fluent/Behaviors/RandomizedOpacityBehavior.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Threading;
+using Avalonia.Xaml.Interactivity;
+
+namespace WalletWasabi.Fluent.Behaviors
+{
+	public class RandomizedOpacityBehavior : Behavior<Control>
+	{
+		private static readonly Random _randomSource = new();
+		private readonly CancellationTokenSource _cts = new();
+
+		protected override void OnDetaching()
+		{
+			base.OnDetaching();
+			_cts?.Dispose();
+		}
+
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+
+			if (AssociatedObject is not null)
+			{
+				Task.Run(async () =>
+				{
+					while (!_cts.IsCancellationRequested)
+					{
+						Dispatcher.UIThread.Post(() =>
+						{
+							AssociatedObject.SetValue(Control.OpacityProperty, 0, BindingPriority.Style);
+						});
+
+						await Task.Delay(TimeSpan.FromSeconds(_randomSource.Next(1,3)));
+
+						Dispatcher.UIThread.Post(() =>
+						{
+							AssociatedObject.SetValue(Control.OpacityProperty, 1, BindingPriority.Style);
+						});
+
+						await Task.Delay(TimeSpan.FromSeconds(_randomSource.Next(1,3)));
+					}
+				});
+			}
+		}
+	}
+}
