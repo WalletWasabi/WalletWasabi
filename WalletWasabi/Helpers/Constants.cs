2d63579f85c4da1f1ac59d2b9ccf4bd127d94589
David Molnar <molnardavid84@gmail.com>
2021-10-25 20:15:55 +0200

    Fix HwiVersion and expected hashes

--- WalletWasabi/Helpers/Constants.cs
+++ WalletWasabi/Helpers/Constants.cs
@@ -57,7 +57,7 @@ namespace WalletWasabi.Helpers
 		public const long MaxSatoshisSupply = 2_100_000_000_000_000L;
 
 		public static readonly Version ClientVersion = new Version(1, 1, 13, 0);
-		public static readonly Version HwiVersion = new Version("2.0.1");
+		public static readonly Version HwiVersion = new Version("2.0.2");
 		public static readonly Version BitcoinCoreVersion = new Version("0.21.1");
 		public static readonly Version LegalDocumentsVersion = new Version(2, 0);
 
