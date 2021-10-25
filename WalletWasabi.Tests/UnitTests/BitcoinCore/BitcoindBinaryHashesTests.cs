42a77ce70e1b562e25f0f74731e89f98f6c820cc
David Molnar <molnardavid84@gmail.com>
2021-10-25 19:49:43 +0200

    Fix bitcoind hash

--- WalletWasabi.Tests/UnitTests/BitcoinCore/BitcoindBinaryHashesTests.cs
+++ WalletWasabi.Tests/UnitTests/BitcoinCore/BitcoindBinaryHashesTests.cs
@@ -22,7 +22,7 @@ namespace WalletWasabi.Tests.UnitTests.BitcoinCore
 
 			Dictionary<OSPlatform, string> expectedHashes = new Dictionary<OSPlatform, string>()
 			{
-				{ OSPlatform.Windows, "e51379c1950092514182494b9998152d9ad6d527358dd730945b2ea4e7ee5a51" },
+				{ OSPlatform.Windows, "957d43d6290ad1bda3e4ee6dc1303a7dac56c378ad4a4b4896610fb02f4fe177" },
 				{ OSPlatform.Linux, "62d849c713c00821dbcca328651c287864c2d489aeafbd8a1a3f8b32870d4d8a" },
 				{ OSPlatform.OSX, "e84db05bef03f62a7da36f1006c4734b9f417320f256cd67aa9bd1fcef5f87a5" },
 			};
