e743cf3b78bf1b8bbca82a90e4218bfb69781203
David Molnar <molnardavid84@gmail.com>
2021-10-25 18:44:19 +0200

    Use ToList() to prevent the collection that is being iterated to get modified #6131

--- WalletWasabi/Blockchain/Keys/KeyManager.cs
+++ WalletWasabi/Blockchain/Keys/KeyManager.cs
@@ -2,6 +2,7 @@ using NBitcoin;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Security;
@@ -530,7 +531,7 @@ namespace WalletWasabi.Blockchain.Keys
 		{
 			lock (HdPubKeyScriptBytesLock)
 			{
-				return HdPubKeyScriptBytes;
+				return HdPubKeyScriptBytes.ToImmutableArray();
 			}
 		}
 
