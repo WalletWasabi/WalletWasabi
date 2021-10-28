e8e77a35c3e223d0809b751a5c1242a07e6ae986
Roland Soós <soos.roland93@gmail.com>
2021-10-28 08:39:55 +0200

    remove unused functions

--- WalletWasabi.Fluent/Helpers/NotificationHelpers.cs
+++ WalletWasabi.Fluent/Helpers/NotificationHelpers.cs
@@ -33,50 +33,6 @@ namespace WalletWasabi.Fluent.Helpers
 			NotificationManager = notificationManager;
 		}
 
-		public static void Notify(string message, string title, NotificationType type, Action? onClick = null, object? sender = null)
-		{
-			List<string> titles = new();
-
-			if (!string.IsNullOrEmpty(title))
-			{
-				titles.Add(title);
-			}
-
-			string walletName = sender switch
-			{
-				Wallet wallet => wallet.WalletName,
-				WalletViewModelBase walletViewModelBase => walletViewModelBase.WalletName,
-				_ => ""
-			};
-
-			if (!string.IsNullOrEmpty(walletName))
-			{
-				titles.Add(walletName);
-			}
-
-			var fullTitle = string.Join(" - ", titles);
-
-			fullTitle = fullTitle.Substring(0, Math.Min(fullTitle.Length, MaxTitleLength));
-
-			RxApp.MainThreadScheduler
-				.Schedule(() => NotificationManager?.Show(new Notification(fullTitle, message, type, TimeSpan.FromSeconds(DefaultNotificationTimeout), onClick)));
-		}
-
-		public static void Information(string message, string title = "Info", object? sender = null)
-		{
-			Notify(message, title, NotificationType.Information, sender: sender);
-		}
-
-		public static void Warning(string message, string title = "Warning!", object? sender = null)
-		{
-			Notify(message, title, NotificationType.Warning, sender: sender);
-		}
-
-		public static void Error(string message, string title = "Error!", object? sender = null)
-		{
-			Notify(message, title, NotificationType.Error, sender: sender);
-		}
-
 		public static void Show(string title, string message, Action? onClick = null)
 		{
 			if (NotificationManager is { } nm)
