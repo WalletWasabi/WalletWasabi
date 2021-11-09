07dc667140aa5d71ba96d0b01fbeb41abfc46f0b
Bitcoin Eagle <hello@bitcoineagle.dev>
2021-11-09 08:50:02 +0100

    continue with tests

--- WalletWasabi.Tests/RegressionTests/RpcServerTests.cs
+++ WalletWasabi.Tests/RegressionTests/RpcServerTests.cs
@@ -16,6 +16,13 @@ using WalletWasabi.Stores;
 using WalletWasabi.Tests.XunitConfiguration;
 using WalletWasabi.WebClients.Wasabi;
 using Xunit;
+using NBitcoin.Protocol;
+using WalletWasabi.Blockchain.Analysis.FeesEstimation;
+using WalletWasabi.Blockchain.Keys;
+using WalletWasabi.Wallets;
+using WalletWasabi.Blockchain.TransactionBroadcasting;
+using WalletWasabi.Helpers;
+using Moq;
 
 namespace WalletWasabi.Tests.RegressionTests
 {
@@ -23,7 +30,7 @@ namespace WalletWasabi.Tests.RegressionTests
 	public class RpcServerTests : IDisposable
 	{
 		private RegTestFixture RegTestFixture { get; }
-		private JsonRpcServer RpcServer { get; }
+		private JsonRpcServer RpcServer { get; set; }
 
 		public RpcServerTests(RegTestFixture regTestFixture)
 		{
@@ -33,6 +40,56 @@ namespace WalletWasabi.Tests.RegressionTests
 		[Fact]
 		public async Task GetStatusTestAsync()
 		{
+			(string password, IRPCClient rpc, Network network, _, ServiceConfiguration serviceConfiguration, BitcoinStore bitcoinStore, Backend.Global global) = await Common.InitializeTestEnvironmentAsync(RegTestFixture, 1);
+
+			bitcoinStore.IndexStore.NewFilter += Common.Wallet_NewFilterProcessed;
+			// Create the services.
+			// 1. Create connection service.
+			NodesGroup nodes = new(global.Config.Network, requirements: Constants.NodeRequirements);
+			nodes.ConnectedNodes.Add(await RegTestFixture.BackendRegTestNode.CreateNewP2pNodeAsync());
+
+			// 2. Create mempool service.
+
+			Node node = await RegTestFixture.BackendRegTestNode.CreateNewP2pNodeAsync();
+			node.Behaviors.Add(bitcoinStore.CreateUntrustedP2pBehavior());
+
+			// 3. Create wasabi synchronizer service.
+			using HttpClientFactory httpClientFactory = new(torEndPoint: null, backendUriGetter: () => new Uri(RegTestFixture.BackendEndPoint));
+			WasabiSynchronizer synchronizer = new(bitcoinStore, httpClientFactory);
+			HybridFeeProvider feeProvider = new(synchronizer, null);
+
+			// 4. Create key manager service.
+			var keyManager = KeyManager.CreateNew(out _, password);
+
+			// 5. Create wallet service.
+			var workDir = Helpers.Common.GetWorkDir();
+
+			CachedBlockProvider blockProvider = new(
+				new P2pBlockProvider(nodes, null, httpClientFactory, serviceConfiguration, network),
+				bitcoinStore.BlockRepository);
+
+			WalletManager walletManager = new(network, workDir, new WalletDirectories(network, workDir));
+			walletManager.RegisterServices(bitcoinStore, synchronizer, serviceConfiguration, feeProvider, blockProvider);
+
+			var wallet = await walletManager.AddAndStartWalletAsync(keyManager);
+
+			TransactionBroadcaster broadcaster = new(network, bitcoinStore, httpClientFactory, walletManager);
+
+			var terminateService = new TerminateService(async () => { });
+			var hostedServices = new HostedServices();
+			var configMock = new Mock<IJsonRpcServerConfiguration>();
+			var config = configMock.Object;
+
+			RpcServer = new JsonRpcServer(config, terminateService,
+				new WasabiJsonRpcService(terminateService)
+				{
+					BitcoinStore = bitcoinStore,
+					HostedServices = hostedServices,
+					Network = network,
+					Synchronizer = synchronizer,
+					TransactionBroadcaster = broadcaster,
+					WalletManager = walletManager
+				});
 		}
 
 		public void Dispose()
