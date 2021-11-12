a687f7659d6ca9885bfd5df94609f9de93740f83
Roland Soós <soos.roland93@gmail.com>
2021-11-12 09:54:22 +0100

    correct exception message

--- WalletWasabi/Exceptions/TransactionFeeOverpaymentException.cs
+++ WalletWasabi/Exceptions/TransactionFeeOverpaymentException.cs
@@ -5,7 +5,7 @@ namespace WalletWasabi.Exceptions
 	public class TransactionFeeOverpaymentException : Exception
 	{
 		public TransactionFeeOverpaymentException(decimal percentageOfOverpayment)
-			: base($"The transaction fee is more than twice the sent amount: {percentageOfOverpayment:0.#}%.")
+			: base($"The transaction fee is more than the sent amount: {percentageOfOverpayment:0.#}%.")
 		{
 			PercentageOfOverpayment = percentageOfOverpayment;
 		}
