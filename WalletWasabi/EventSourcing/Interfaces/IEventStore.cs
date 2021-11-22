28abde69ceac537d1d59f6936e336fc20e5ef990
David Molnar <molnardavid84@gmail.com>
2021-11-22 17:53:51 +0100

    Extracting methods

--- WalletWasabi/EventSourcing/Interfaces/IEventStore.cs
+++ WalletWasabi/EventSourcing/Interfaces/IEventStore.cs
@@ -9,5 +9,7 @@ namespace WalletWasabi.EventSourcing.Interfaces
 	public interface IEventStore
 	{
 		Task ProcessCommandAsync(ICommand command, string aggregateType, string aggregateId);
+
+		Task<IAggregate> GetAggregateAsync(string aggregateType, string aggregateId);
 	}
 }
