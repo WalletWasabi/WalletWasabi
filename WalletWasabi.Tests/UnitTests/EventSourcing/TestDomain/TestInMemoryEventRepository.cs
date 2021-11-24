22f66dab6a843c647484e18fe58d0e42ea2c14dd
Bitcoin Eagle <hello@bitcoineagle.dev>
2021-11-24 15:33:01 +0100

    Use Immutable list

--- WalletWasabi.Tests/UnitTests/EventSourcing/TestDomain/TestInMemoryEventRepository.cs
+++ WalletWasabi.Tests/UnitTests/EventSourcing/TestDomain/TestInMemoryEventRepository.cs
@@ -20,15 +20,11 @@ namespace WalletWasabi.Tests.UnitTests.EventSourcing.TestDomain
 
 		public SemaphoreSlim ValidatedSemaphore { get; } = new SemaphoreSlim(0);
 		public SemaphoreSlim ConflictedSemaphore { get; } = new SemaphoreSlim(0);
-		public SemaphoreSlim LockedSemaphore { get; } = new SemaphoreSlim(0);
 		public SemaphoreSlim AppendedSemaphore { get; } = new SemaphoreSlim(0);
-		public SemaphoreSlim UnlockedSemaphore { get; } = new SemaphoreSlim(0);
 
 		public Action? ValidatedCallback { get; set; }
 		public Action? ConflictedCallback { get; set; }
-		public Action? LockedCallback { get; set; }
 		public Action? AppendedCallback { get; set; }
-		public Action? UnlockedCallback { get; set; }
 
 		protected override void Validated()
 		{
@@ -46,28 +42,12 @@ namespace WalletWasabi.Tests.UnitTests.EventSourcing.TestDomain
 			ConflictedCallback?.Invoke();
 		}
 
-		protected override void Locked()
-		{
-			base.Locked();
-			Output.WriteLine(nameof(Locked));
-			LockedSemaphore.Release();
-			LockedCallback?.Invoke();
-		}
-
 		protected override void Appended()
 		{
 			base.Appended();
 			Output.WriteLine(nameof(Appended));
 			AppendedSemaphore.Release();
 			AppendedCallback?.Invoke();
-		}
-
-		protected override void Unlocked()
-		{
-			base.Unlocked();
-			Output.WriteLine(nameof(Unlocked));
-			UnlockedSemaphore.Release();
-			UnlockedCallback?.Invoke();
 		}
 	}
 }
