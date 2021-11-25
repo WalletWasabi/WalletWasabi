66072cc805cfe233fc610a0858e3528d8030fb74
Dan Walmsley <dan@walms.co.uk>
2021-11-25 11:10:09 +0000

    pastebutton flash behavior takes into account textbox status.

--- WalletWasabi.Fluent/Behaviors/PasteButtonFlashBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/PasteButtonFlashBehavior.cs
@@ -1,9 +1,10 @@
-using System;
 using System.Reactive;
 using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using ReactiveUI;
 using WalletWasabi.Fluent.Controls;
@@ -19,6 +20,16 @@ namespace WalletWasabi.Fluent.Behaviors
 		public static readonly StyledProperty<string> CurrentAddressProperty =
 			AvaloniaProperty.Register<PasteButtonFlashBehavior, string>(nameof(FlashAnimation));
 
+		public static readonly StyledProperty<TextBox?> OwnerTextBoxProperty =
+			AvaloniaProperty.Register<CheckMarkVisibilityBehavior, TextBox?>(nameof(OwnerTextBox));
+
+		[ResolveByName]
+		public TextBox? OwnerTextBox
+		{
+			get => GetValue(OwnerTextBoxProperty);
+			set => SetValue(OwnerTextBoxProperty, value);
+		}
+
 		public string FlashAnimation
 		{
 			get => GetValue(FlashAnimationProperty);
@@ -31,31 +42,50 @@ namespace WalletWasabi.Fluent.Behaviors
 			set => SetValue(CurrentAddressProperty, value);
 		}
 
-		protected override void OnAttachedToVisualTree()
+		protected override void OnAttachedToVisualTree(CompositeDisposable disposables)
 		{
 			RxApp.MainThreadScheduler.Schedule(async () => await CheckClipboardForValidAddressAsync());
 
-			var mainWindow = ((IClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime).MainWindow;
-			Observable
-				.FromEventPattern(mainWindow, nameof(mainWindow.Activated)).Select(_ => Unit.Default)
-				.Merge(this.WhenAnyValue(x => x.CurrentAddress).Select(_ => Unit.Default))
-				.Throttle(TimeSpan.FromMilliseconds(100))
-				.ObserveOn(RxApp.MainThreadScheduler)
-				.Subscribe(async _ => await CheckClipboardForValidAddressAsync());
+			if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime)
+			{
+				var mainWindow  = lifetime.MainWindow;
+
+				Observable
+					.FromEventPattern(mainWindow, nameof(mainWindow.Activated)).Select(_ => Unit.Default)
+					.Merge(this.WhenAnyValue(x => x.CurrentAddress).Select(_ => Unit.Default))
+					.Throttle(TimeSpan.FromMilliseconds(100))
+					.ObserveOn(RxApp.MainThreadScheduler)
+					.Subscribe(async _ => await CheckClipboardForValidAddressAsync())
+					.DisposeWith(disposables);
+			}
 
 			AssociatedObject?.WhenAnyValue(x => x.AnimateIcon)
 				.Where(x => x)
-				.Subscribe(_ => AssociatedObject.Classes.Remove(FlashAnimation));
+				.Subscribe(_ => AssociatedObject.Classes.Remove(FlashAnimation))
+				.DisposeWith(disposables);
+
+			OwnerTextBox?.WhenAnyValue(x => x.IsFocused)
+				.Subscribe(async _ =>
+				{
+					await CheckClipboardForValidAddressAsync();
+				})
+				.DisposeWith(disposables);
 		}
 
 		private async Task CheckClipboardForValidAddressAsync()
 		{
+			var tbFocussed = OwnerTextBox?.IsFocused ?? true;
+
 			AssociatedObject?.Classes.Remove(FlashAnimation);
 
-			var textToPaste = await Application.Current.Clipboard.GetTextAsync();
-			if (AddressStringParser.TryParse(textToPaste, Services.WalletManager.Network, out _) && textToPaste != CurrentAddress)
+			if (tbFocussed)
 			{
-				AssociatedObject?.Classes.Add(FlashAnimation);
+				var textToPaste = await Application.Current.Clipboard.GetTextAsync();
+				if (AddressStringParser.TryParse(textToPaste, Services.WalletManager.Network, out _) &&
+				    textToPaste != CurrentAddress)
+				{
+					AssociatedObject?.Classes.Add(FlashAnimation);
+				}
 			}
 		}
 	}
