f13ab51335728e6b715d7c7ac15173f229887809
Roland Soós <soos.roland93@gmail.com>
2021-12-09 12:48:35 +0100

    simplify `BindableFlyoutOpenBehavior`

--- WalletWasabi.Fluent/Behaviors/BindableFlyoutOpenBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/BindableFlyoutOpenBehavior.cs
@@ -3,17 +3,14 @@ using System.Reactive.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
-using Avalonia.Threading;
 using ReactiveUI;
 
 namespace WalletWasabi.Fluent.Behaviors;
 
-public class BindableFlyoutOpenBehavior : ShowFlyoutOnPointerOverBehavior
+public class BindableFlyoutOpenBehavior : DisposingBehavior<Control>
 {
 	public static readonly StyledProperty<bool> IsOpenProperty =
 		AvaloniaProperty.Register<BindableFlyoutOpenBehavior, bool>(nameof(IsOpen));
-
-	private IDisposable? _pointerEnterDisposable;
 
 	public bool IsOpen
 	{
@@ -21,41 +18,28 @@ public class BindableFlyoutOpenBehavior : ShowFlyoutOnPointerOverBehavior
 		set => SetValue(IsOpenProperty, value);
 	}
 
-	private IDisposable SubscribePointerOver()
-	{
-		_pointerEnterDisposable?.Dispose();
-
-		return _pointerEnterDisposable = Observable
-			.FromEventPattern(AssociatedObject, nameof(AssociatedObject.PointerEnter))
-			.Subscribe(_ =>
-			{
-				_pointerEnterDisposable?.Dispose();
-				_pointerEnterDisposable = null;
-				FlyoutBase.ShowAttachedFlyout(AssociatedObject);
-			});
-	}
-
 	protected override void OnAttached(CompositeDisposable disposable)
 	{
-		SubscribePointerOver()
+		if (AssociatedObject is null)
+		{
+			return;
+		}
+
+		Observable
+			.FromEventPattern(AssociatedObject, nameof(AssociatedObject.PointerEnter))
+			.Subscribe(_ => IsOpen = true)
 			.DisposeWith(disposable);
 
 		this.WhenAnyValue(x => x.IsOpen)
-			.Subscribe(x =>
+			.Subscribe(isOpen =>
 			{
-				if (x)
+				if (isOpen)
 				{
-					FlyoutBase.ShowAttachedFlyout(AssociatedObject!);
+					FlyoutBase.ShowAttachedFlyout(AssociatedObject);
 				}
-				else if (Flyout.GetAttachedFlyout(AssociatedObject) is FlyoutBase flyout)
+				else
 				{
-					Flyout.GetAttachedFlyout(AssociatedObject!)?.Hide();
-
-					Dispatcher.UIThread.Post(() =>
-					{
-						SubscribePointerOver()
-							.DisposeWith(disposable);
-					});
+					FlyoutBase.GetAttachedFlyout(AssociatedObject)?.Hide();
 				}
 			});
 	}
