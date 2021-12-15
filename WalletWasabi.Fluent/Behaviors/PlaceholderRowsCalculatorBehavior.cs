f5aef191ced7c150874a527aef495dc3dddf827a
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2021-12-15 23:32:07 +0800

    dynamically calculate row count instead of being fixed

---
+++ WalletWasabi.Fluent/Behaviors/PlaceholderRowsCalculatorBehavior.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using ReactiveUI;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+public class PlaceholderRowsCalculatorBehavior : DisposingBehavior<DataBox.DataBox>
+{
+	protected override void OnAttached(CompositeDisposable disposables)
+	{
+		if (AssociatedObject is null)
+		{
+			return;
+		}
+
+		AssociatedObject.WhenAnyValue(x => x.Bounds)
+			.Select(x => x.Height)
+			.Where(x => x > 0)
+			.Subscribe(CalculateRows)
+			.DisposeWith(disposables);
+	}
+
+	private void CalculateRows(double Height)
+	{
+		var totalRows = (int) Math.Floor(Math.Max(3, (Height / 42) ));
+		var opacityList = new List<double>();
+		var deltaOpacity = 1d / totalRows;
+
+		foreach (var mult in Enumerable.Range(1, totalRows).Reverse())
+		{
+			opacityList.Add(mult * deltaOpacity);
+		}
+
+		AssociatedObject.Items = opacityList;
+	}
+}
