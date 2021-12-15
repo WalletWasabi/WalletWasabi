48e7d3709afb22cfeadce74001064980d08196dc
Bitcoin Eagle <hello@bitcoineagle.dev>
2021-12-15 20:29:57 +0400

    zkSNACKs 91f4853 (#12)
    * Navigate back is txn building is failed
    
    * add custom exception for transaction fee overpayment
    
    * correct transaction fee if overpayment is detected
    
    * Limit the maximum of the fee chart to the maximum possible fee
    
    * reset some options if amount is changed
    
    * fix issue when coins and txn fee got reseted after navigating back and forth
    
    * fix inaccurate x axis labels in some cases
    
    * make sure the last label is correct
    
    * pop error message if the txn couldn't be built due to high fees
    
    * extract methods
    
    * tiny correction
    
    * correct exception message
    
    * a few correction
    
    * add dialog for label entry
    
    * use the label entry dialog
    
    * tweak wording
    
    Co-authored-by: Max Hillebrand <30683012+MaxHillebrand@users.noreply.github.com>
    
    * make optimized transactions reproducible
    
    * rebuild txn with the label
    
    * hide labels for now
    
    * special navigation to make it feel slightly less disruptive.
    
    * revert xaml changes
    
    * Packager.Program: Convert to async code.
    
    * Narrow down unreachable code section.
    
    * Packager: Generate BUILDINFO.txt
    
    * fix tiny ui freeze when confirming
    
    * Reformatting
    
    * Use chart for privacy control tile on medium size layout
    
    * Add vertical style for Separator
    
    * Adjust medium tile size
    
    * Remove text
    
    * Write JSON
    
    * add Version
    
    * Add summary
    
    * hide labels section when private coins were used
    
    * Fix TagControl's text wrapping
    
    * fix navigation issue when canceling the dialog
    
    * flash when cj happened
    
    * find the txn even if it is a cj group
    
    * shorten sentence for label entry dialog.
    
    * consistent wording on receive view.
    
    * Add try/catch to Arena step methods
    
    * Add one more try/catch
    
    * Show unconfirmed CJ tx too and reverse order in batch list
    
    * Fix time interval
    
    * Better
    
    * tidy compact privacy tile.
    
    * Revert "Merge pull request #6835 from kiminuo/feature/2021-12-09-event-sourcing-formatting"
    
    This reverts commit f52bd08fef9a4b01f7b1c141ce2ae2a0860f8cf4, reversing
    changes made to c1d3dfc700b1c9f73140fee11a37838cfe87c7db.
    
    Co-authored-by: Roland Soós <soos.roland93@gmail.com>
    Co-authored-by: Dan Walmsley <dan@walms.co.uk>
    Co-authored-by: Max Hillebrand <30683012+MaxHillebrand@users.noreply.github.com>
    Co-authored-by: Kiminuo <kiminuo@protonmail.com>
    Co-authored-by: Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
    Co-authored-by: Jumar Macato <16554748+jmacato@users.noreply.github.com>
    Co-authored-by: David Molnar <molnardavid84@gmail.com>

--- WalletWasabi.Fluent/ViewModels/Wallets/Send/TransactionPreviewViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Send/TransactionPreviewViewModel.cs
@@ -248,6 +248,20 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Send
 
 				return await Task.Run(() => TransactionHelpers.BuildTransaction(_wallet, _info));
 			}
+			catch (TransactionFeeOverpaymentException ex)
+			{
+				var result = TrySetMaximumPossibleFee(ex.PercentageOfOverpayment, _wallet, _info);
+
+				if (!result)
+				{
+					await ShowErrorAsync("Transaction Building", "At the moment, it is not possible to select a transaction fee that is less than the payment amount. The transaction cannot be sent.",
+						"Wasabi was unable to create your transaction.");
+
+					return null;
+				}
+
+				return await BuildTransactionAsync();
+			}
 			catch (InsufficientBalanceException)
 			{
 				if (_info.IsPayJoin)
@@ -272,8 +286,31 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Send
 			}
 		}
 
-		private async Task<BuildTransactionResult?> HandleInsufficientBalanceWhenNormalAsync(Wallet wallet,
-			TransactionInfo transactionInfo)
+		private bool TrySetMaximumPossibleFee(decimal percentageOfOverpayment, Wallet wallet, TransactionInfo transactionInfo)
+		{
+			var currentFeeRate = transactionInfo.FeeRate;
+			var maxPossibleFeeRateInSatoshiPerByte = (currentFeeRate.SatoshiPerByte / percentageOfOverpayment) * 100;
+			var maximumPossibleFeeRate = new FeeRate(maxPossibleFeeRateInSatoshiPerByte);
+
+			var feeChartViewModel = new FeeChartViewModel();
+			feeChartViewModel.UpdateFeeEstimates(TransactionFeeHelper.GetFeeEstimates(wallet));
+
+			var blockTarget = feeChartViewModel.GetConfirmationTarget(maximumPossibleFeeRate);
+			var newFeeRate = new FeeRate(feeChartViewModel.GetSatoshiPerByte(blockTarget));
+
+			if (newFeeRate > maximumPossibleFeeRate)
+			{
+				return false;
+			}
+
+			transactionInfo.ConfirmationTimeSpan = TransactionFeeHelper.CalculateConfirmationTime(blockTarget);
+			transactionInfo.FeeRate = newFeeRate;
+			transactionInfo.MaximumPossibleFeeRate = maximumPossibleFeeRate;
+
+			return true;
+		}
+
+		private async Task<BuildTransactionResult?> HandleInsufficientBalanceWhenNormalAsync(Wallet wallet, TransactionInfo transactionInfo)
 		{
 			var dialog = new InsufficientBalanceDialogViewModel(transactionInfo.IsPrivatePocketUsed
 				? BalanceType.Private
@@ -385,33 +422,41 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Send
 
 		private async Task OnConfirmAsync()
 		{
-			if (_transaction is { })
+			var labelDialog = new LabelEntryDialogViewModel(_wallet, _info);
+
+			Navigate(NavigationTarget.CompactDialogScreen).To(labelDialog);
+
+			var result = await labelDialog.GetDialogResultAsync();
+
+			if (result.Result is null)
 			{
-				var transaction = _transaction;
+				Navigate(NavigationTarget.CompactDialogScreen).Back();
+				return;
+			}
 
-				var transactionAuthorizationInfo = new TransactionAuthorizationInfo(transaction);
+			_info.UserLabels = result.Result;
 
-				var authResult = await AuthorizeAsync(transactionAuthorizationInfo);
+			var transaction = await Task.Run(() => TransactionHelpers.BuildTransaction(_wallet, _info));
+			var transactionAuthorizationInfo = new TransactionAuthorizationInfo(transaction);
+			var authResult = await AuthorizeAsync(transactionAuthorizationInfo);
+			if (authResult)
+			{
+				IsBusy = true;
 
-				if (authResult)
+				try
 				{
-					IsBusy = true;
-
-					try
-					{
-						var finalTransaction =
-							await GetFinalTransactionAsync(transactionAuthorizationInfo.Transaction, _info);
-						await SendTransactionAsync(finalTransaction);
-						Navigate().To(new SendSuccessViewModel(_wallet, finalTransaction));
-					}
-					catch (Exception ex)
-					{
-						await ShowErrorAsync("Transaction", ex.ToUserFriendlyString(),
-							"Wasabi was unable to send your transaction.");
-					}
-
-					IsBusy = false;
+					var finalTransaction =
+						await GetFinalTransactionAsync(transactionAuthorizationInfo.Transaction, _info);
+					await SendTransactionAsync(finalTransaction);
+					Navigate().To(new SendSuccessViewModel(_wallet, finalTransaction));
 				}
+				catch (Exception ex)
+				{
+					await ShowErrorAsync("Transaction", ex.ToUserFriendlyString(),
+						"Wasabi was unable to send your transaction.");
+				}
+
+				IsBusy = false;
 			}
 		}
 
@@ -424,7 +469,7 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Send
 			}
 
 			var authDialog = AuthorizationHelpers.GetAuthorizationDialog(_wallet, transactionAuthorizationInfo);
-			var authDialogResult = await NavigateDialogAsync(authDialog, authDialog.DefaultTarget);
+			var authDialogResult = await NavigateDialogAsync(authDialog, authDialog.DefaultTarget, NavigationMode.Clear);
 
 			if (!authDialogResult.Result && authDialogResult.Kind == DialogResultKind.Normal)
 			{
