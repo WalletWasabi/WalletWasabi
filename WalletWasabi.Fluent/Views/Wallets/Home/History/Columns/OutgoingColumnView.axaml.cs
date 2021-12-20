e5da7cc06fb3d616056934548c501af8be6b5df3
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-12-20 18:57:07 +0100

    Add columns user controls

---
+++ WalletWasabi.Fluent/Views/Wallets/Home/History/Columns/OutgoingColumnView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Views.Wallets.Home.History.Columns;
+
+public class OutgoingColumnView : UserControl
+{
+	public OutgoingColumnView()
+	{
+		InitializeComponent();
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+}
