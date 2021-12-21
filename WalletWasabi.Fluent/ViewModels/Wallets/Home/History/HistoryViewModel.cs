d5f6fc92083ce3d953c08eca277c5d758836183b
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-12-21 19:40:25 +0100

    Set column template control height

--- WalletWasabi.Fluent/ViewModels/Wallets/Home/History/HistoryViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/History/HistoryViewModel.cs
@@ -60,12 +60,12 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Home.History
 			// Outgoing			OutgoingColumnView			Outgoing (₿)	Auto		120				150			true
 			// Balance			BalanceColumnView			Balance (₿)		Auto		120				150			true
 
-			IControl IndicatorsColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new IndicatorsColumnView() { MinWidth = 80 };
-			IControl DateColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new DateColumnView() { MinWidth = 150 };
-			IControl LabelsColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new LabelsColumnView() { MinWidth = 75 };
-			IControl IncomingColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new IncomingColumnView() { MinWidth = 120, MaxWidth = 150 };
-			IControl OutgoingColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new OutgoingColumnView() { MinWidth = 120, MaxWidth = 150 };
-			IControl BalanceColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new BalanceColumnView() { MinWidth = 120, MaxWidth = 150 };
+			IControl IndicatorsColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new IndicatorsColumnView() { Height = 37.5, MinWidth = 80 };
+			IControl DateColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new DateColumnView() { Height = 37.5, MinWidth = 150 };
+			IControl LabelsColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new LabelsColumnView() { Height = 37.5, MinWidth = 75 };
+			IControl IncomingColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new IncomingColumnView() { Height = 37.5, MinWidth = 120, MaxWidth = 150 };
+			IControl OutgoingColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new OutgoingColumnView() { Height = 37.5, MinWidth = 120, MaxWidth = 150 };
+			IControl BalanceColumnTemplate(HistoryItemViewModelBase node, INameScope ns) => new BalanceColumnView() { Height = 37.5, MinWidth = 120, MaxWidth = 150 };
 
 			Source = new FlatTreeDataGridSource<HistoryItemViewModelBase>(_transactions)
             {
