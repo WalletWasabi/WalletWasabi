b1fe412eabe230479e6beb9a5377c2957278aaf9
Lucas Ontivero <lucasontivero@gmail.com>
2021-12-24 15:55:55 -0300

    Remove duplicated test

--- WalletWasabi.Tests/UnitTests/WabiSabi/Client/AmountDecomposerTests.cs
+++ WalletWasabi.Tests/UnitTests/WabiSabi/Client/AmountDecomposerTests.cs
@@ -26,37 +26,23 @@ namespace WalletWasabi.Tests.UnitTests.WabiSabi.Client
 		[InlineData(100, 0, 2)]
 		[InlineData(500, 0, 3)]
 		[InlineData(5000, 0, 8)]
-		public void DecompositionsTest(decimal feeRateDecimal, long minOutputAmount, int maxAvailableOutputs)
+		public void DecompositionsInvariantTest(decimal feeRateDecimal, long minOutputAmount, int maxAvailableOutputs)
 		{
 			var availableVsize = maxAvailableOutputs * Constants.P2WPKHOutputSizeInBytes;
 			var feeRate = new FeeRate(feeRateDecimal);
 			var feePerOutput = feeRate.GetFee(Constants.P2WPKHOutputSizeInBytes);
 			var registeredCoins = GenerateRandomCoins().Take(3).ToList();
 			var theirCoins = GenerateRandomCoins().Take(30).ToList();
-			var totalEffectiveValue = registeredCoins.Sum(x => x.EffectiveValue(feeRate));
 
 			var amountDecomposer = new AmountDecomposer(feeRate, minOutputAmount, Constants.P2WPKHOutputSizeInBytes, availableVsize);
 			var outputValues = amountDecomposer.Decompose(registeredCoins, theirCoins);
 
+			var totalEffectiveValue = registeredCoins.Sum(x => x.EffectiveValue(feeRate));
 			var totalEffectiveCost = outputValues.Count() * feePerOutput;
+
 			Assert.InRange(outputValues.Count(), 1, maxAvailableOutputs);
-			Assert.Equal(totalEffectiveValue - totalEffectiveCost, outputValues.Sum());
+			Assert.True(totalEffectiveValue - totalEffectiveCost -minOutputAmount <= outputValues.Sum());
 			Assert.All(outputValues, v => Assert.InRange(v.Satoshi, minOutputAmount, totalEffectiveValue));
-		}
-
-		[Fact]
-		public void MinimumPrivacyTest()
-		{
-			var feeRate = new FeeRate(25.0m);
-			var feePerOutput = feeRate.GetFee(Constants.P2WPKHOutputSizeInBytes);
-			var amountDecomposer = new AmountDecomposer(feeRate, 0L, Constants.P2WPKHOutputSizeInBytes, 255);
-
-			var numberOfInputsPerUser = new[] { 1, 2, 3, 7 };
-			var usersCoins = numberOfInputsPerUser.Select(n => GenerateRandomCoins().Take(n).ToArray()).ToArray();
-			var theirCoins = usersCoins.SelectMany(x => x);
-			var usersDecompositions = usersCoins.Select(coins => amountDecomposer.Decompose(coins, theirCoins)).ToArray();
-
-			Assert.All(usersDecompositions,	d => Assert.True(d.Count() <= 8));
 		}
 
 		private static IEnumerable<Coin> GenerateRandomCoins()
