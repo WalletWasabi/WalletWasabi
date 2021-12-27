23ccc6f18766b10cc3bd87684c556749df5dc1f5
Lucas Ontivero <lucasontivero@gmail.com>
2021-12-27 12:05:14 -0300

    Add UT to make the problem reproducible

--- WalletWasabi.Tests/UnitTests/Crypto/StringCipherTests.cs
+++ WalletWasabi.Tests/UnitTests/Crypto/StringCipherTests.cs
@@ -8,38 +8,6 @@ namespace WalletWasabi.Tests.UnitTests.Crypto
 {
 	public class StringCipherTests
 	{
-		[Fact]
-		public void CipherTests()
-		{
-			var toEncrypt = "hello";
-			var password = "password";
-			var encypted = StringCipher.Encrypt(toEncrypt, password);
-			Assert.NotEqual(toEncrypt, encypted);
-			var decrypted = StringCipher.Decrypt(encypted, password);
-			Assert.Equal(toEncrypt, decrypted);
-
-			var builder = new StringBuilder();
-			for (int i = 0; i < 1000000; i++) // check 10MB
-			{
-				builder.Append("0123456789");
-			}
-
-			toEncrypt = builder.ToString();
-			encypted = StringCipher.Encrypt(toEncrypt, password);
-			Assert.NotEqual(toEncrypt, encypted);
-			decrypted = StringCipher.Decrypt(encypted, password);
-			Assert.Equal(toEncrypt, decrypted);
-
-			toEncrypt = "foo@éóüö";
-			password = "";
-			encypted = StringCipher.Encrypt(toEncrypt, password);
-			Assert.NotEqual(toEncrypt, encypted);
-			decrypted = StringCipher.Decrypt(encypted, password);
-			Assert.Equal(toEncrypt, decrypted);
-			Logger.TurnOff();
-			Assert.Throws<CryptographicException>(() => StringCipher.Decrypt(encypted, "wrongpassword"));
-			Logger.TurnOn();
-		}
 
 		[Fact]
 		public void AuthenticateMessageTest()
@@ -66,6 +34,45 @@ namespace WalletWasabi.Tests.UnitTests.Crypto
 			}
 			var rate = errorCount / (double)count;
 			Assert.True(rate is < 0.000001 and > (-0.000001));
+		}
+
+		[Fact]
+		public void CipherTests()
+		{
+			var password = "password";
+
+			var builder = new StringBuilder();
+			for (int i = 0; i < 1000000; i++) // check 10MB
+			{
+				builder.Append("0123456789");
+			}
+
+			var toEncrypt = builder.ToString();
+			var encypted = StringCipher.Encrypt(toEncrypt, password);
+			Assert.NotEqual(toEncrypt, encypted);
+			var decrypted = StringCipher.Decrypt(encypted, password);
+			Assert.Equal(toEncrypt, decrypted);
+
+			toEncrypt = "foo@éóüö";
+			password = "";
+			encypted = StringCipher.Encrypt(toEncrypt, password);
+			Assert.NotEqual(toEncrypt, encypted);
+			decrypted = StringCipher.Decrypt(encypted, password);
+			Assert.Equal(toEncrypt, decrypted);
+			Logger.TurnOff();
+			Assert.Throws<CryptographicException>(() => StringCipher.Decrypt(encypted, "wrongpassword"));
+			Logger.TurnOn();
+		}
+
+		[Fact]
+		public void LongTextWithPartialReadTests()
+		{
+			var toEncrypt = "hello world, how are you doing todays?";
+			var password = "password";
+			var encypted = StringCipher.Encrypt(toEncrypt, password);
+			Assert.NotEqual(toEncrypt, encypted);
+			var decrypted = StringCipher.Decrypt(encypted, password);
+			Assert.Equal(toEncrypt, decrypted);
 		}
 	}
 }
