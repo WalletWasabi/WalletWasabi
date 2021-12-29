1de6dbb1e1cf42b5452db415e320cbf8e5c55e58
Kiminuo <kiminuo@protonmail.com>
2021-12-29 22:30:27 +0100

    `IHttpClientFactory`: Add convenience methods

--- WalletWasabi/WebClients/Wasabi/IHttpClientFactory.cs
+++ WalletWasabi/WebClients/Wasabi/IHttpClientFactory.cs
@@ -5,6 +5,23 @@ namespace WalletWasabi.WebClients.Wasabi
 {
 	public interface IWasabiHttpClientFactory
 	{
+		PersonCircuit NewHttpClientWithPersonCircuit(out IHttpClient httpClient)
+		{
+			PersonCircuit personCircuit = new();
+			httpClient = NewHttpClient(Mode.SingleCircuitPerLifetime, personCircuit);
+			return personCircuit;
+		}
+
+		IHttpClient NewHttpClientWithDefaultCircuit()
+		{
+			return NewHttpClient(Mode.DefaultCircuit);
+		}
+
+		IHttpClient NewHttpClientWithCircuitPerRequest()
+		{
+			return NewHttpClient(Mode.NewCircuitPerRequest);
+		}
+
 		IHttpClient NewHttpClient(Mode mode, ICircuit? circuit = null);
 	}
 }
