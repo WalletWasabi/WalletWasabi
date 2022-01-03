d332f29e24e68a54b95b6a36bd84a90fb9a7b3c8
Roland Soós <soos.roland93@gmail.com>
2022-01-03 10:28:25 +0100

    tweak wording
    Co-authored-by: yahiheb <52379387+yahiheb@users.noreply.github.com>

--- WalletWasabi.Fluent/ViewModels/AddWallet/RecoverWalletViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/AddWallet/RecoverWalletViewModel.cs
@@ -56,7 +56,7 @@ namespace WalletWasabi.Fluent.ViewModels.AddWallet
 		private async Task OnNextAsync(string? walletName)
 		{
 			var dialogResult = await NavigateDialogAsync(
-				new CreatePasswordDialogViewModel("Type the password of the wallet if exists.")
+				new CreatePasswordDialogViewModel("Type the password of the wallet if you used one.")
 				, NavigationTarget.CompactDialogScreen);
 
 			if (dialogResult.Result is { } password)
