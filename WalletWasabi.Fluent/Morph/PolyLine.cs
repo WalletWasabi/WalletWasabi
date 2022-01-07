7dcb07a30fceb7789d07aa5c78a1b7cfd389e99b
Dan Walmsley <dan@walms.co.uk>
2022-01-07 14:58:58 +0000

    fix nullable issues in 2 files.

--- WalletWasabi.Fluent/Morph/PolyLine.cs
+++ WalletWasabi.Fluent/Morph/PolyLine.cs
@@ -4,15 +4,23 @@ namespace WalletWasabi.Fluent.Morph;
 
 public class PolyLine
 {
+	public PolyLine()
+	{
+		XValues = new ObservableCollection<double>();
+		YValues = new ObservableCollection<double>();
+	}
+
+	public PolyLine(ObservableCollection<double> xValues, ObservableCollection<double> yValues)
+	{
+		XValues = xValues;
+		YValues = yValues;
+	}
+
 	public ObservableCollection<double> XValues { get; set; }
 	public ObservableCollection<double> YValues { get; set; }
 
 	public PolyLine Clone()
 	{
-		return new()
-		{
-			XValues = new ObservableCollection<double>(XValues),
-			YValues = new ObservableCollection<double>(YValues)
-		};
+			return new PolyLine(XValues, YValues);
 	}
 }
