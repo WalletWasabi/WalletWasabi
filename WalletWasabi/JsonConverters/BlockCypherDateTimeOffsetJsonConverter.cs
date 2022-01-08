1744529c672834c479e55978235d9a6e71c113df
Yahia Chiheb <chihebyahia@gmail.com>
2022-01-08 02:02:52 +0100

    Convert to file-scoped namespace

--- WalletWasabi/JsonConverters/BlockCypherDateTimeOffsetJsonConverter.cs
+++ WalletWasabi/JsonConverters/BlockCypherDateTimeOffsetJsonConverter.cs
@@ -2,28 +2,27 @@ using Newtonsoft.Json;
 using System;
 using System.Globalization;
 
-namespace WalletWasabi.JsonConverters
+namespace WalletWasabi.JsonConverters;
+
+public class BlockCypherDateTimeOffsetJsonConverter : JsonConverter<DateTimeOffset>
 {
-	public class BlockCypherDateTimeOffsetJsonConverter : JsonConverter<DateTimeOffset>
+	/// <inheritdoc />
+	public override DateTimeOffset ReadJson(JsonReader reader, Type objectType, DateTimeOffset existingValue, bool hasExistingValue, JsonSerializer serializer)
 	{
-		/// <inheritdoc />
-		public override DateTimeOffset ReadJson(JsonReader reader, Type objectType, DateTimeOffset existingValue, bool hasExistingValue, JsonSerializer serializer)
+		var value = reader.Value as string;
+
+		if (string.IsNullOrWhiteSpace(value))
 		{
-			var value = reader.Value as string;
-
-			if (string.IsNullOrWhiteSpace(value))
-			{
-				return DateTimeOffset.Now;
-			}
-
-			string time = value.Trim();
-			return DateTimeOffset.Parse(time, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+			return DateTimeOffset.Now;
 		}
 
-		/// <inheritdoc />
-		public override void WriteJson(JsonWriter writer, DateTimeOffset value, JsonSerializer serializer)
-		{
-			writer.WriteValue(value.ToString(CultureInfo.InvariantCulture));
-		}
+		string time = value.Trim();
+		return DateTimeOffset.Parse(time, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+	}
+
+	/// <inheritdoc />
+	public override void WriteJson(JsonWriter writer, DateTimeOffset value, JsonSerializer serializer)
+	{
+		writer.WriteValue(value.ToString(CultureInfo.InvariantCulture));
 	}
 }
