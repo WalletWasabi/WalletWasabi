f0fe5315e7fea06dc385bac116356aff45013237
adamPetho <45069029+adamPetho@users.noreply.github.com>
2022-01-10 13:23:18 +0100

    Convert to file-scoped namespace

--- WalletWasabi/Blockchain/TransactionBuilding/BnBManager.cs
+++ WalletWasabi/Blockchain/TransactionBuilding/BnBManager.cs
@@ -6,34 +6,33 @@ using System.Text;
 using System.Threading.Tasks;
 using WalletWasabi.Blockchain.TransactionOutputs;
 
-namespace WalletWasabi.Blockchain.TransactionBuilding
-{
-	public static class BnBManager
-	{
-		public static bool CanCreateChangelessTransaction(List<SmartCoin> availableCoins, long target, [NotNullWhen(true)] out List<SmartCoin>? selectedCoins)
-		{
-			selectedCoins = null;
-			var longs = availableCoins.Select(x => x.Amount.Satoshi).ToList();
-			BranchAndBound branchAndBound = new(longs);
+namespace WalletWasabi.Blockchain.TransactionBuilding;
 
-			if (branchAndBound.TryGetExactMatch(target, out List<long>? bnbResult))
+public static class BnBManager
+{
+	public static bool CanCreateChangelessTransaction(List<SmartCoin> availableCoins, long target, [NotNullWhen(true)] out List<SmartCoin>? selectedCoins)
+	{
+		selectedCoins = null;
+		var longs = availableCoins.Select(x => x.Amount.Satoshi).ToList();
+		BranchAndBound branchAndBound = new(longs);
+
+		if (branchAndBound.TryGetExactMatch(target, out List<long>? bnbResult))
+		{
+			selectedCoins = new();
+			foreach (var coin in availableCoins)
 			{
-				selectedCoins = new();
-				foreach (var coin in availableCoins)
+				foreach (var item in bnbResult)
 				{
-					foreach (var item in bnbResult)
+					if (coin.Amount == item)
 					{
-						if (coin.Amount == item)
-						{
-							selectedCoins.Add(coin);
-							bnbResult.Remove(item);
-							break;
-						}
+						selectedCoins.Add(coin);
+						bnbResult.Remove(item);
+						break;
 					}
 				}
-				return true;
 			}
-			return false;
+			return true;
 		}
+		return false;
 	}
 }
