a0d184b1fe24c0e0bd8e2ae28ecc36ff96386af5
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-10 14:02:55 +0100

    Update HistoryTablePro.axaml.cs

--- WalletWasabi.Fluent/Views/Wallets/Home/History/HistoryTablePro.axaml.cs
+++ WalletWasabi.Fluent/Views/Wallets/Home/History/HistoryTablePro.axaml.cs
@@ -1,18 +1,17 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace WalletWasabi.Fluent.Views.Wallets.Home.History
-{
-	public class HistoryTablePro : UserControl
-	{
-		public HistoryTablePro()
-		{
-			InitializeComponent();
-		}
+namespace WalletWasabi.Fluent.Views.Wallets.Home.History;
 
-		private void InitializeComponent()
-		{
-			AvaloniaXamlLoader.Load(this);
-		}
+public class HistoryTablePro : UserControl
+{
+	public HistoryTablePro()
+	{
+		InitializeComponent();
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
 	}
 }
