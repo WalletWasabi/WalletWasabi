bbbbb641c1caa2d84a76596c5b53fc06242451f2
Roland Soós <soos.roland93@gmail.com>
2022-01-27 12:55:00 +0100

    converter to separate file

--- WalletWasabi.Fluent/Converters/FeeTargetTimeConverter.cs
+++ WalletWasabi.Fluent/Converters/FeeTargetTimeConverter.cs
@@ -6,31 +6,7 @@ using WalletWasabi.Helpers;
 
 namespace WalletWasabi.Fluent.Converters;
 
-public class BoolToBrushConverter : IValueConverter
-{
-	public static BoolToBrushConverter Instance { get; } = new();
 
-	public IBrush? TrueBrush { get; set; }
-
-	public IBrush? FalseBrush { get; set; }
-
-	public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-	{
-		if (value is true)
-		{
-			return TrueBrush;
-		}
-		else
-		{
-			return FalseBrush;
-		}
-	}
-
-	public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-	{
-		throw new NotImplementedException();
-	}
-}
 
 public class FeeTargetTimeConverter : IValueConverter
 {
