f3e87afbfa1a946dc727e10a678e7acbc38da29f
Dan Walmsley <dan@walms.co.uk>
2022-02-04 15:07:50 +0000

    add logic for fading.

--- WalletWasabi.Fluent/ViewModels/Wallets/Send/LabelViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Send/LabelViewModel.cs
@@ -12,11 +12,27 @@ public partial class LabelViewModel : ViewModelBase
 	[AutoNotify(SetterModifier = AccessModifier.Private)]
 	private bool _isHighlighted;
 
+	[AutoNotify(SetterModifier = AccessModifier.Internal)]
+	private bool _isFaded;
+
 	[AutoNotify] private bool _isPointerOver;
 
 	public LabelViewModel(LabelSelectionViewModel owner, string label)
 	{
 		Value = label;
+
+		this.WhenAnyValue(x => x.IsPointerOver)
+			.Subscribe(isPointerOver =>
+			{
+				if (isPointerOver)
+				{
+					owner.Fade(this);
+				}
+				else
+				{
+					owner.ClearFade(this);
+				}
+			});
 
 		this.WhenAnyValue(x => x.IsPointerOver)
 			.Skip(1)
@@ -34,6 +50,8 @@ public partial class LabelViewModel : ViewModelBase
 
 	public void Highlight(LabelViewModel triggerSource)
 	{
+		IsFaded = false;
+
 		triggerSource
 			.WhenAnyValue(x => x.IsPointerOver)
 			.TakeUntil(value => value == false)
