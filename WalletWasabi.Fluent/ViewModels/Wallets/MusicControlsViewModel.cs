f3cf11c4a6984067fe81d6fe90b0b6b04b4cf2b6
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-02-08 23:58:37 +0800

    Add hide/show animation for MusicControls

--- WalletWasabi.Fluent/ViewModels/Wallets/MusicControlsViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/MusicControlsViewModel.cs
@@ -1,6 +1,15 @@
+using System.Reactive.Linq;
+
 namespace WalletWasabi.Fluent.ViewModels.Wallets;
 
-public class MusicControlsViewModel : ViewModelBase
+public partial class MusicControlsViewModel : ViewModelBase
 {
+	[AutoNotify] private bool _isActive;
+
+	public MusicControlsViewModel()
+	{
+		Observable.Interval(TimeSpan.FromMilliseconds(3000))
+			.Subscribe(token => IsActive = !IsActive);
+	}
 
 }
