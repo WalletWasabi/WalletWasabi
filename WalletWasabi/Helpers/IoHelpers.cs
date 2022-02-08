7ba3790bcc8c3c842edc0625a7041946a1dbf078
Example <example@example.com>
2022-02-08 09:56:17 +0100

    file-scoped ns

--- WalletWasabi/Helpers/IoHelpers.cs
+++ WalletWasabi/Helpers/IoHelpers.cs
@@ -7,169 +7,168 @@ using WalletWasabi.Crypto;
 using WalletWasabi.Helpers;
 using WalletWasabi.Logging;
 
-namespace System.IO
+namespace System.IO;
+
+public static class IoHelpers
 {
-	public static class IoHelpers
+	/// <summary>
+	/// Attempts to delete <paramref name="directory"/> with retry feature to allow File Explorer or any other
+	/// application that holds the directory handle of the <paramref name="directory"/> to release it.
+	/// </summary>
+	/// <see href="https://stackoverflow.com/questions/329355/cannot-delete-directory-with-directory-deletepath-true/14933880#44324346"/>
+	public static async Task<bool> TryDeleteDirectoryAsync(string directory, int maxRetries = 10, int millisecondsDelay = 100)
 	{
-		/// <summary>
-		/// Attempts to delete <paramref name="directory"/> with retry feature to allow File Explorer or any other
-		/// application that holds the directory handle of the <paramref name="directory"/> to release it.
-		/// </summary>
-		/// <see href="https://stackoverflow.com/questions/329355/cannot-delete-directory-with-directory-deletepath-true/14933880#44324346"/>
-		public static async Task<bool> TryDeleteDirectoryAsync(string directory, int maxRetries = 10, int millisecondsDelay = 100)
+		Guard.NotNull(nameof(directory), directory);
+
+		if (maxRetries < 1)
 		{
-			Guard.NotNull(nameof(directory), directory);
+			throw new ArgumentOutOfRangeException(nameof(maxRetries));
+		}
 
-			if (maxRetries < 1)
-			{
-				throw new ArgumentOutOfRangeException(nameof(maxRetries));
-			}
+		if (millisecondsDelay < 1)
+		{
+			throw new ArgumentOutOfRangeException(nameof(millisecondsDelay));
+		}
 
-			if (millisecondsDelay < 1)
+		for (int i = 0; i < maxRetries; ++i)
+		{
+			try
 			{
-				throw new ArgumentOutOfRangeException(nameof(millisecondsDelay));
-			}
-
-			for (int i = 0; i < maxRetries; ++i)
-			{
-				try
+				if (Directory.Exists(directory))
 				{
-					if (Directory.Exists(directory))
-					{
-						Directory.Delete(directory, recursive: true);
-					}
-
-					return true;
+					Directory.Delete(directory, recursive: true);
 				}
-				catch (DirectoryNotFoundException e)
-				{
-					Logger.LogDebug($"Directory was not found: {e}");
 
-					// Directory does not exist. So the operation is trivially done.
-					return true;
-				}
-				catch (Exception ex) when (ex is IOException or UnauthorizedAccessException)
-				{
-					await Task.Delay(millisecondsDelay).ConfigureAwait(false);
-				}
+				return true;
 			}
-
-			return false;
-		}
-
-		public static void EnsureContainingDirectoryExists(string fileNameOrPath)
-		{
-			string fullPath = Path.GetFullPath(fileNameOrPath); // No matter if relative or absolute path is given to this.
-			string? dir = Path.GetDirectoryName(fullPath);
-			EnsureDirectoryExists(dir);
-		}
-
-		/// <summary>
-		/// It's like Directory.CreateDirectory, but does not fail when root is given.
-		/// </summary>
-		public static void EnsureDirectoryExists(string? dir)
-		{
-			if (!string.IsNullOrWhiteSpace(dir)) // If root is given, then do not worry.
+			catch (DirectoryNotFoundException e)
 			{
-				Directory.CreateDirectory(dir); // It does not fail if it exists.
+				Logger.LogDebug($"Directory was not found: {e}");
+
+				// Directory does not exist. So the operation is trivially done.
+				return true;
+			}
+			catch (Exception ex) when (ex is IOException or UnauthorizedAccessException)
+			{
+				await Task.Delay(millisecondsDelay).ConfigureAwait(false);
 			}
 		}
 
-		public static void EnsureFileExists(string filePath)
-		{
-			if (!File.Exists(filePath))
-			{
-				EnsureContainingDirectoryExists(filePath);
+		return false;
+	}
 
-				File.Create(filePath)?.Dispose();
-			}
+	public static void EnsureContainingDirectoryExists(string fileNameOrPath)
+	{
+		string fullPath = Path.GetFullPath(fileNameOrPath); // No matter if relative or absolute path is given to this.
+		string? dir = Path.GetDirectoryName(fullPath);
+		EnsureDirectoryExists(dir);
+	}
+
+	/// <summary>
+	/// It's like Directory.CreateDirectory, but does not fail when root is given.
+	/// </summary>
+	public static void EnsureDirectoryExists(string? dir)
+	{
+		if (!string.IsNullOrWhiteSpace(dir)) // If root is given, then do not worry.
+		{
+			Directory.CreateDirectory(dir); // It does not fail if it exists.
+		}
+	}
+
+	public static void EnsureFileExists(string filePath)
+	{
+		if (!File.Exists(filePath))
+		{
+			EnsureContainingDirectoryExists(filePath);
+
+			File.Create(filePath)?.Dispose();
+		}
+	}
+
+	public static void OpenFolderInFileExplorer(string dirPath)
+	{
+		if (Directory.Exists(dirPath))
+		{
+			using var process = Process.Start(new ProcessStartInfo
+			{
+				FileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+					? "explorer.exe"
+					: (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+						? "open"
+						: "xdg-open"),
+				Arguments = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? $"\"{dirPath}\"" : dirPath,
+				CreateNoWindow = true
+			});
+		}
+	}
+
+	public static async Task OpenBrowserAsync(string url)
+	{
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+		{
+			// If no associated application/json MimeType is found xdg-open opens retrun error
+			// but it tries to open it anyway using the console editor (nano, vim, other..)
+			await EnvironmentHelpers.ShellExecAsync($"xdg-open {url}", waitForExit: false).ConfigureAwait(false);
+		}
+		else
+		{
+			using var process = Process.Start(new ProcessStartInfo
+			{
+				FileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? url : "open",
+				Arguments = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? $"-e {url}" : "",
+				CreateNoWindow = true,
+				UseShellExecute = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+			});
+		}
+	}
+
+	public static void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target)
+	{
+		foreach (DirectoryInfo dir in source.GetDirectories())
+		{
+			CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
 		}
 
-		public static void OpenFolderInFileExplorer(string dirPath)
+		foreach (FileInfo file in source.GetFiles())
 		{
-			if (Directory.Exists(dirPath))
-			{
-				using var process = Process.Start(new ProcessStartInfo
-				{
-					FileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-						? "explorer.exe"
-						: (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
-							? "open"
-							: "xdg-open"),
-					Arguments = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? $"\"{dirPath}\"" : dirPath,
-					CreateNoWindow = true
-				});
-			}
+			file.CopyTo(Path.Combine(target.FullName, file.Name));
 		}
+	}
 
-		public static async Task OpenBrowserAsync(string url)
+	public static void CreateOrOverwriteFile(string path)
+	{
+		using var _ = File.Create(path);
+	}
+
+	/// <seealso href="https://stackoverflow.com/questions/17408499/async-wait-for-file-to-be-created/17408819#17408819"/>
+	[SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Watcher is disposed once the file is created.")]
+	public static Task WhenFileCreated(string filePath)
+	{
+		TaskCompletionSource<bool> tcs = new();
+		FileSystemWatcher watcher = new(Path.GetDirectoryName(filePath)!);
+
+		FileSystemEventHandler? createdHandler = null;
+		createdHandler = (s, e) =>
 		{
-			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			if (e.Name == Path.GetFileName(filePath))
 			{
-				// If no associated application/json MimeType is found xdg-open opens retrun error
-				// but it tries to open it anyway using the console editor (nano, vim, other..)
-				await EnvironmentHelpers.ShellExecAsync($"xdg-open {url}", waitForExit: false).ConfigureAwait(false);
-			}
-			else
-			{
-				using var process = Process.Start(new ProcessStartInfo
-				{
-					FileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? url : "open",
-					Arguments = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? $"-e {url}" : "",
-					CreateNoWindow = true,
-					UseShellExecute = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-				});
-			}
-		}
-
-		public static void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target)
-		{
-			foreach (DirectoryInfo dir in source.GetDirectories())
-			{
-				CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name));
-			}
-
-			foreach (FileInfo file in source.GetFiles())
-			{
-				file.CopyTo(Path.Combine(target.FullName, file.Name));
-			}
-		}
-
-		public static void CreateOrOverwriteFile(string path)
-		{
-			using var _ = File.Create(path);
-		}
-
-		/// <seealso href="https://stackoverflow.com/questions/17408499/async-wait-for-file-to-be-created/17408819#17408819"/>
-		[SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Watcher is disposed once the file is created.")]
-		public static Task WhenFileCreated(string filePath)
-		{
-			TaskCompletionSource<bool> tcs = new();
-			FileSystemWatcher watcher = new(Path.GetDirectoryName(filePath)!);
-
-			FileSystemEventHandler? createdHandler = null;
-			createdHandler = (s, e) =>
-			{
-				if (e.Name == Path.GetFileName(filePath))
-				{
-					tcs.TrySetResult(true);
-					watcher.Created -= createdHandler;
-					watcher.Dispose();
-				}
-			};
-
-			watcher.Created += createdHandler;
-			watcher.EnableRaisingEvents = true;
-
-			// This is to avoid race: "FileSystemWatcher is created, @filePath file is created, watcher.EnableRaisingEvents is set to true -> "
-			if (File.Exists(filePath))
-			{
+				tcs.TrySetResult(true);
 				watcher.Created -= createdHandler;
 				watcher.Dispose();
-				return Task.FromResult(true);
 			}
+		};
 
-			return tcs.Task;
+		watcher.Created += createdHandler;
+		watcher.EnableRaisingEvents = true;
+
+		// This is to avoid race: "FileSystemWatcher is created, @filePath file is created, watcher.EnableRaisingEvents is set to true -> "
+		if (File.Exists(filePath))
+		{
+			watcher.Created -= createdHandler;
+			watcher.Dispose();
+			return Task.FromResult(true);
 		}
+
+		return tcs.Task;
 	}
 }
