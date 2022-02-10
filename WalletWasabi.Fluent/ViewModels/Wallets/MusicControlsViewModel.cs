0e641f16cd8dfb2425ce23df1c8ac5a306c02aef
Dan Walmsley <dan@walms.co.uk>
2022-02-10 14:23:12 +0000

    add scrolling status message.

--- WalletWasabi.Fluent/ViewModels/Wallets/MusicControlsViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/MusicControlsViewModel.cs
@@ -1,15 +1,32 @@
 using System.Reactive.Linq;
+using Avalonia.Threading;
+using ReactiveUI;
+using WalletWasabi.WabiSabi.Client;
+using WalletWasabi.Wallets;
 
 namespace WalletWasabi.Fluent.ViewModels.Wallets;
 
 public partial class MusicControlsViewModel : ViewModelBase
 {
 	[AutoNotify] private bool _isActive;
+	[AutoNotify] private TimeSpan _autoStartCountdown;
+	[AutoNotify] private string? _currentStatus;
 
-	public MusicControlsViewModel()
+
+	public MusicControlsViewModel(Wallet wallet)
 	{
-		Observable.Interval(TimeSpan.FromMilliseconds(3000))
-			.Subscribe(token => IsActive = !IsActive);
-	}
+		IsActive = true;
 
+		var coinJoinManager = Services.HostedServices.Get<CoinJoinManager>();
+
+		Observable.Interval(TimeSpan.FromSeconds(1))
+			.ObserveOn(RxApp.MainThreadScheduler)
+			.Subscribe(x =>
+			{
+				CurrentStatus = $"CoinJoin will auto-start in this is a very long message: {(DateTime.Now - coinJoinManager.WhenWalletCanStartAutoCoinJoin(wallet)):mm\\:ss}";
+
+
+
+			});
+	}
 }
--- WalletWasabi.Fluent/ViewModels/Wallets/WalletViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/WalletViewModel.cs
@@ -146,7 +146,11 @@ public partial class WalletViewModel : WalletViewModelBase
 		WalletSettingsCommand = ReactiveCommand.Create(() => Navigate(NavigationTarget.DialogScreen).To(Settings));
 
 		WalletCoinsCommand = ReactiveCommand.Create(() => Navigate(NavigationTarget.DialogScreen).To(new WalletCoinsViewModel(this, balanceChanged)));
+
+		MusicControls = new MusicControlsViewModel(wallet);
 	}
+
+	public MusicControlsViewModel MusicControls { get; }
 
 	public WalletSettingsViewModel Settings { get; }
 
