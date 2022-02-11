aa89769c2489b40e11541253beeb1b0575b7adf3
Dan Walmsley <dan@walms.co.uk>
2022-02-11 13:18:43 +0000

    make spectrum control extendable.

---
+++ WalletWasabi.Fluent/Controls/Spectrum/SplashEffectDataSource.cs
@@ -0,0 +1,32 @@
+﻿namespace WalletWasabi.Fluent.Controls.Spectrum;
+
+public class SplashEffectDataSource : SpectrumDataSource
+{
+	private int _currentEffectIndex;
+
+	public SplashEffectDataSource(int numBins) : base(numBins, 4, TimeSpan.FromSeconds(0.0125))
+	{
+	}
+
+	protected override void OnMixData()
+	{
+		Bins[MidPointBins - _currentEffectIndex] = 1;
+		Bins[MidPointBins + _currentEffectIndex] = 1;
+
+		if (_currentEffectIndex >= 8)
+		{
+			var index = _currentEffectIndex - 8;
+			Bins[MidPointBins - index] = 0;
+			Bins[MidPointBins + index] = 0;
+		}
+
+		_currentEffectIndex++;
+
+		if (_currentEffectIndex >= MidPointBins)
+		{
+			_currentEffectIndex = 0;
+
+			Stop();
+		}
+	}
+}
