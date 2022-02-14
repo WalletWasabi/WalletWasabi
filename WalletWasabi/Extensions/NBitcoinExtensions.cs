e772eb754227496334bb993d64681799992759e1
Yahia Chiheb <chihebyahia@gmail.com>
2022-02-14 16:43:19 +0100

    Improve IsLikelyCoinjoin method

--- WalletWasabi/Extensions/NBitcoinExtensions.cs
+++ WalletWasabi/Extensions/NBitcoinExtensions.cs
@@ -151,7 +151,8 @@ public static class NBitcoinExtensions
 	}
 
 	public static bool IsLikelyCoinjoin(this SmartTransaction me)
-		=> me.Transaction.Inputs.Count != me.WalletInputs.Count // Some inputs must not be ours for it to be a coinjoin.
+		=> me.Transaction.Inputs.Count > 1 // The tx must have more than one input in order to be a coinjoin.
+		&& me.Transaction.Inputs.Count != me.WalletInputs.Count // Some inputs must not be ours for it to be a coinjoin.
 		&& me.Transaction.Outputs.Count != me.WalletOutputs.Count // Some outputs must not be ours for it to be a coinjoin.
 		&& me.Transaction.HasIndistinguishableOutputs(); // The tx must have more than one equal output in order to be a coinjoin.
 
