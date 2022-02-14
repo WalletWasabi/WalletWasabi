4ad1723019a4cb024b7b4c85c36c24dc9088d988
Szpoti <balage.farago@gmail.com>
2022-02-14 13:30:50 +0100

    Remove leftover using

--- WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/PrivacyControlTileViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/PrivacyControlTileViewModel.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Disposables;
-using System.Reactive.Linq;
 using System.Windows.Input;
 using Avalonia.Threading;
 using NBitcoin;
