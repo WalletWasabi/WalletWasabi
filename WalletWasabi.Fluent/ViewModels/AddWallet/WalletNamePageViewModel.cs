699c18ded5918b996a4fb3515f91df7053f8be5b
Yahia Chiheb <chihebyahia@gmail.com>
2022-02-19 19:35:17 +0100

    Remove caption when creating password

--- WalletWasabi.Fluent/ViewModels/AddWallet/WalletNamePageViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/AddWallet/WalletNamePageViewModel.cs
@@ -78,7 +78,7 @@ public partial class WalletNamePageViewModel : RoutableViewModel
 	private async Task CreatePasswordAsync(string walletName)
 	{
 		var dialogResult = await NavigateDialogAsync(
-			new CreatePasswordDialogViewModel("Create Password", "Enter a password for your wallet.", enableEmpty: true),
+			new CreatePasswordDialogViewModel("Create Password", enableEmpty: true),
 			NavigationTarget.CompactDialogScreen);
 
 		if (dialogResult.Result is { } password)
