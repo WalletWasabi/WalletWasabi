4420f8b7e5f59eef7a175e04617eacb0963717d0
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-02-23 16:10:23 +0800

    Added auto open behavior
    But it's a little bit annoying at the moment.

---
+++ WalletWasabi.Fluent/Behaviors/SplitViewHoverBehavior.cs
@@ -0,0 +1,68 @@
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Input.Raw;
+using Avalonia.Interactivity;
+using Avalonia.Threading;
+using Avalonia.VisualTree;
+using ReactiveUI;
+using WalletWasabi.BitcoinCore.Rpc.Models;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+public class SplitViewHoverBehavior : DisposingBehavior<SplitView>
+{
+	public static readonly StyledProperty<Action> ToggleActionProperty =
+		AvaloniaProperty.Register<SplitViewHoverBehavior, Action>(nameof(ToggleAction));
+
+	public static readonly StyledProperty<double> OpenPaneLengthProperty =
+		AvaloniaProperty.Register<SplitViewHoverBehavior, double>(nameof(OpenPaneLength));
+
+	public Action ToggleAction
+	{
+		get => GetValue(ToggleActionProperty);
+		set => SetValue(ToggleActionProperty, value);
+	}
+
+	public double OpenPaneLength
+	{
+		get => GetValue(OpenPaneLengthProperty);
+		set => SetValue(OpenPaneLengthProperty, value);
+	}
+
+	protected override void OnAttached(CompositeDisposable disposables)
+	{
+		if (AssociatedObject is null)
+		{
+			return;
+		}
+
+
+		Observable
+			.FromEventPattern<PointerEventArgs>(AssociatedObject, nameof(InputElement.PointerMoved))
+			.Select(x => x.EventArgs.GetPosition(AssociatedObject))
+			.Subscribe(x =>
+			{
+				var navState = AssociatedObject.IsPaneOpen;
+				if (x.X <= 100)
+				{
+					if (!navState)
+					{
+						AssociatedObject.IsPaneOpen = true;
+					}
+				}
+				else
+				{
+					if (x.X > OpenPaneLength && navState)
+					{
+						AssociatedObject.IsPaneOpen = false;
+					}
+				}
+			})
+			.DisposeWith(disposables);
+	}
+}
