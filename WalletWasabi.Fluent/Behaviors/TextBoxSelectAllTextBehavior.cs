9773794015fbd510e3c5e0cd1f1ea29df7a8b045
ichthus1604 <ichthus1604@gmail.com>
2022-02-23 15:50:30 -0300

    Wallet Name TextBox Select All Text

---
+++ WalletWasabi.Fluent/Behaviors/TextBoxSelectAllTextBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Reactive.Disposables;
+using Avalonia.Controls;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+internal class TextBoxSelectAllTextBehavior : AttachedToVisualTreeBehavior<TextBox>
+{
+	protected override void OnAttachedToVisualTree(CompositeDisposable disposable)
+	{
+		if (AssociatedObject is { })
+		{
+			AssociatedObject.SelectAll();
+		}
+	}
+}
