ad7f7b2ac4fb0653c55e07ca613381194159341f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-02-23 18:02:07 +0100

    Remove debug code

--- WalletWasabi.Fluent/ViewModels/Wallets/Home/History/HistoryViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/History/HistoryViewModel.cs
@@ -212,8 +212,6 @@ public partial class HistoryViewModel : ActivatableViewModel
 
 			lock (_transactionListLock)
 			{
-				// NOTE: Original wasabi code with performance fix
-				//*
 				_transactionSourceList.Edit(x =>
 				{
 					var copyList = Transactions.ToList();
@@ -246,28 +244,6 @@ public partial class HistoryViewModel : ActivatableViewModel
 						}
 					}
 				});
-				//*/
-
-				// NOTE: Repro for TreeDataGrid scrolling issues
-				/*
-				_transactionSourceList.Edit(x =>
-				{
-					var rand = new Random(100);
-
-					for (var i = 0; i < 10_000; i++)
-					{
-						var ts = new TransactionSummary()
-						{
-							DateTime = DateTimeOffset.Now,
-							Label = new SmartLabel($"Label{i++}", $"Label{i++}"),
-							Amount = new Money(rand.NextDouble() < 0.5 ? rand.Next() : -rand.Next()),
-						};
-						var item = new TransactionHistoryItemViewModel(i, ts, _walletViewModel, Money.Zero, _updateTrigger);
-
-						x.Add(item);
-					}
-				});
-				//*/
 
 				if (!IsInitialized)
 				{
