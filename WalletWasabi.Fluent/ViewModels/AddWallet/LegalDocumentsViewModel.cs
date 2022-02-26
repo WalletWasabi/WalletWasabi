72e93183ddf09ef6734f391499f6b7f305f53841
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-02-27 00:24:07 +0800

    update info regular icon names

--- WalletWasabi.Fluent/ViewModels/AddWallet/LegalDocumentsViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/AddWallet/LegalDocumentsViewModel.cs
@@ -8,7 +8,7 @@ namespace WalletWasabi.Fluent.ViewModels.AddWallet;
 	Order = 3,
 	Category = "General",
 	Keywords = new[] { "View", "Legal", "Docs", "Documentation", "Terms", "Conditions", "Help" },
-	IconName = "info_regular",
+	IconName = "info_regular_16",
 	Searchable = true,
 	NavigationTarget = NavigationTarget.DialogScreen)]
 public partial class LegalDocumentsViewModel : RoutableViewModel
