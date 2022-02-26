9976f9c85c79380e3634a88a85fcedd0718811f7
Yahia Chiheb <chihebyahia@gmail.com>
2022-02-26 18:45:23 +0100

    Add unknown pocket to tests

--- WalletWasabi.Tests/UnitTests/UserInterfaceTest/PocketSelectionTests.cs
+++ WalletWasabi.Tests/UnitTests/UserInterfaceTest/PocketSelectionTests.cs
@@ -468,12 +468,14 @@ public class PocketSelectionTests
 		var pockets = new List<Pocket>();
 		pockets.AddPocket(0.7M, out var pocket1, CoinPocketHelper.PrivateFundsText);
 		pockets.AddPocket(0.7M, out var pocket2, "Dan");
+		pockets.AddPocket(0.7M, out var pocket3, CoinPocketHelper.UnlabelledFundsText);
 
 		selection.Reset(pockets.ToArray());
 
 		var output = selection.AutoSelectPockets();
 		Assert.Contains(pocket1, output);
 		Assert.DoesNotContain(pocket2, output);
+		Assert.DoesNotContain(pocket3, output);
 	}
 
 	[Fact]
@@ -485,11 +487,13 @@ public class PocketSelectionTests
 		pockets.AddPocket(0.5M, out var pocket1, CoinPocketHelper.PrivateFundsText);
 		pockets.AddPocket(0.5M, out var pocket2, "Dan");
 		pockets.AddPocket(0.5M, out var pocket3, "Lucas");
+		pockets.AddPocket(0.8M, out var pocket4, CoinPocketHelper.UnlabelledFundsText);
 
 		selection.Reset(pockets.ToArray());
 
 		var output = selection.AutoSelectPockets();
 		Assert.DoesNotContain(pocket1, output);
+		Assert.DoesNotContain(pocket4, output);
 		Assert.Contains(pocket2, output);
 		Assert.Contains(pocket3, output);
 	}
