553b4ac21ca7c99d1799545d25d4085bf508da60
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-03-01 01:47:02 +0800

    Simplify privacy tile control

--- WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/PrivacyControlTileViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/PrivacyControlTileViewModel.cs
@@ -131,7 +131,7 @@ public partial class PrivacyControlTileViewModel : TileViewModel
 		var pcPrivate = totalDecimalAmount == 0M ? 1d : (double)(privateDecimalAmount / totalDecimalAmount);
 		var pcNormal = 1 - pcPrivate;
 
-		PercentText = $"{pcPrivate:P}";
+		PercentText = $"\u205F{pcPrivate*100:N1}\u205F/\u205F{100}";
 
 		FullyMixed = pcPrivate >= 1d;
 
