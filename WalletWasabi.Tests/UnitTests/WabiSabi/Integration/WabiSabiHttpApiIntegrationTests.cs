4ad340f7bf29abec8f1221a9bc5dc7e95db51ae8
David Molnar <molnardavid84@gmail.com>
2022-03-01 18:41:46 +0100

    Fix CoinJoinWithBlameRoundTestAsync

--- WalletWasabi.Tests/UnitTests/WabiSabi/Integration/WabiSabiHttpApiIntegrationTests.cs
+++ WalletWasabi.Tests/UnitTests/WabiSabi/Integration/WabiSabiHttpApiIntegrationTests.cs
@@ -300,9 +300,10 @@ public class WabiSabiHttpApiIntegrationTests : IClassFixture<WabiSabiApiApplicat
 
 		var badCoinsTask = Task.Run(async () => await badCoinJoinClient.StartRoundAsync(badCoins, roundState, cts.Token).ConfigureAwait(false), cts.Token);
 
-		await Task.WhenAll(new Task[] { badCoinsTask, coinJoinTask });
+		// BadCoinsTask will throw.
+		await Assert.ThrowsAsync<AggregateException>(async () => await Task.WhenAll(new Task[] { badCoinsTask, coinJoinTask }));
 
-		Assert.False(badCoinsTask.Result);
+		Assert.True(badCoinsTask.IsFaulted);
 		Assert.True(coinJoinTask.Result);
 
 		var broadcastedTx = await transactionCompleted.Task; // wait for the transaction to be broadcasted.
