792e13e2ced94f1d1e1f538e648abfb0940212c5
José Manuel Nieto <superjmn@outlook.com>
2022-03-25 13:48:19 +0100

    Rescue Dan's SearchBarBehavior (and improve it)

---
+++ WalletWasabi.Fluent/Behaviors/SearchBarBehavior.cs
@@ -0,0 +1,141 @@
+using System.Reactive.Disposables;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+public class SearchBarBehavior : AttachedToVisualTreeBehavior<UserControl>
+{
+	public static readonly StyledProperty<TextBox?> SearchBoxProperty =
+		AvaloniaProperty.Register<SearchBarBehavior, TextBox?>(nameof(SearchBox));
+
+	public static readonly StyledProperty<UserControl?> SearchPanelProperty =
+		AvaloniaProperty.Register<SearchBarBehavior, UserControl?>(nameof(SearchPanel));
+
+	public static readonly StyledProperty<bool> IsSearchPanelOpenProperty =
+		AvaloniaProperty.Register<SearchBarBehavior, bool>(nameof(SearchBox));
+
+	private TopLevel? _topLevel;
+
+	[ResolveByName]
+	public TextBox? SearchBox
+	{
+		get => GetValue(SearchBoxProperty);
+		set => SetValue(SearchBoxProperty, value);
+	}
+
+	[ResolveByName]
+	public UserControl? SearchPanel
+	{
+		get => GetValue(SearchPanelProperty);
+		set => SetValue(SearchPanelProperty, value);
+	}
+
+
+	public bool IsSearchPanelOpen
+	{
+		get => GetValue(IsSearchPanelOpenProperty);
+		set => SetValue(IsSearchPanelOpenProperty, value);
+	}
+
+	protected override void OnAttachedToVisualTree(CompositeDisposable disposables)
+	{
+		this.GetObservable(IsSearchPanelOpenProperty)
+			.Subscribe(ToggleFlyoutOpen);
+
+		if (AssociatedObject is null)
+		{
+			return;
+		}
+
+		if (AssociatedObject.GetVisualRoot() is TopLevel topLevel)
+		{
+			_topLevel = topLevel;
+
+			topLevel.AddHandler(InputElement.PointerPressedEvent, OnTopLevelPointerPressed, RoutingStrategies.Tunnel);
+		}
+
+		if (SearchBox is { } && SearchPanel is { })
+		{
+			SearchBox.GotFocus += SearchBoxOnGotFocus;
+			AssociatedObject.LostFocus += AssociatedObjectOnLostFocus;
+		}
+	}
+
+	protected override void OnDetaching()
+	{
+		base.OnDetaching();
+
+		if (_topLevel is { })
+		{
+			_topLevel.RemoveHandler(InputElement.PointerPressedEvent, OnTopLevelPointerPressed);
+
+			_topLevel = null;
+		}
+
+		if (SearchBox is { })
+		{
+			SearchBox.GotFocus -= SearchBoxOnGotFocus;
+		}
+
+		if (AssociatedObject is { })
+		{
+			AssociatedObject.LostFocus -= AssociatedObjectOnLostFocus;
+		}
+	}
+
+	private void ToggleFlyoutOpen(bool isOpen)
+	{
+		if (isOpen)
+		{
+			ShowFlyout();
+		}
+		else
+		{
+			HideFlyout();
+		}
+	}
+
+	private void SearchBoxOnGotFocus(object? sender, GotFocusEventArgs e)
+	{
+		ShowFlyout();
+	}
+
+	private void ShowFlyout()
+	{
+		FlyoutBase.ShowAttachedFlyout(AssociatedObject);
+		IsSearchPanelOpen = true;
+	}
+
+	private void AssociatedObjectOnLostFocus(object? sender, RoutedEventArgs e)
+	{
+		if (AssociatedObject is { } && SearchPanel is { })
+		{
+			if (!AssociatedObject.IsKeyboardFocusWithin && !SearchPanel.IsKeyboardFocusWithin)
+			{
+				HideFlyout();
+			}
+		}
+	}
+
+	private void HideFlyout()
+	{
+		FlyoutBase.GetAttachedFlyout(AssociatedObject).Hide();
+		IsSearchPanelOpen = false;
+	}
+
+	private void OnTopLevelPointerPressed(object? sender, PointerPressedEventArgs e)
+	{
+		if (AssociatedObject is { } && !AssociatedObject.IsPointerOver)
+		{
+			if (ReferenceEquals(FocusManager.Instance?.Current, SearchBox))
+			{
+				FocusManager.Instance?.Focus(null);
+			}
+		}
+	}
+}
