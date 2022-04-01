dfe461d7d8e590f7794939286962d31d7758d68b
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-04-01 15:18:01 +0800

    use XAML Flag enum syntax instead of codebehind. Thanks @soosr!

--- WalletWasabi.Fluent/Views/MainWindow.axaml.cs
+++ WalletWasabi.Fluent/Views/MainWindow.axaml.cs
@@ -10,7 +10,6 @@ public class MainWindow : Window
 	public MainWindow()
 	{
 		InitializeComponent();
-		ExtendClientAreaChromeHints |= ExtendClientAreaChromeHints.OSXThickTitleBar;
 	}
 
 	private void InitializeComponent()
