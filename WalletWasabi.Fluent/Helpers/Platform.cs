96a043f86b515491b3a530b5a844670836dbd116
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-04-13 13:58:49 +0200

    Add Platform helper class to be used in adaptive xaml styles

---
+++ WalletWasabi.Fluent/Helpers/Platform.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WalletWasabi.Fluent.Helpers;
+
+public static class Platform
+{
+	public static bool Osx => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+	public static bool Linux => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+	public static bool Windows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+}
