48e5147781a2eabfa2194c0996e2dff554e318c6
José Manuel Nieto <superjmn@outlook.com>
2022-04-18 14:57:14 +0200

    Correct settings labels

--- WalletWasabi.Fluent/ViewModels/SearchBar/Sources/SettingsSource.cs
+++ WalletWasabi.Fluent/ViewModels/SearchBar/Sources/SettingsSource.cs
@@ -21,13 +21,13 @@ public class SettingsSource : ISearchItemSource
 
 		return new ISearchItem[]
 		{
-			new NonActionableSearchItem(new DarkThemeSetting(Services.UiConfig), "Dark theme", "Appearance", new List<string>(), "nav_settings_regular"){ IsDefault = false},
-			new NonActionableSearchItem(new Setting<UiConfig, bool>(Services.UiConfig, b => b.Autocopy), "Autocopy Bitcoin address", "Settings", new List<string>(), "nav_settings_regular")  { IsDefault = false },
-			new NonActionableSearchItem(new Setting<UiConfig, bool>(Services.UiConfig, b => b.AutoPaste), "Autopaste Bitcoin address", "Settings", new List<string>(), "nav_settings_regular")  { IsDefault = false },
-			new NonActionableSearchItem(new Setting<UiConfig, bool>(Services.UiConfig, b => b.HideOnClose), "Run in background when closed", "Settings", new List<string>() { "hide" }, "nav_settings_regular") { IsDefault = false },
-			new NonActionableSearchItem(new Setting<UiConfig, bool>(Services.UiConfig, b => b.RunOnSystemStartup), "Run on system startup", "Settings", new List<string>(), "nav_settings_regular") { IsDefault = false },
-			new NonActionableSearchItem(new Setting<TorSettingsTabViewModel, bool>(torSettings, b => b.UseTor, true), "Use Tor", "Settings", new List<string>(), "nav_settings_regular") { IsDefault = false },
-			new NonActionableSearchItem(new Setting<TorSettingsTabViewModel, bool>(torSettings, b => b.TerminateTorOnExit, true), "Terminate Tor on exit", "Settings", new List<string>(), "nav_settings_regular") { IsDefault = false },
+			new NonActionableSearchItem(new DarkThemeSetting(Services.UiConfig), "Dark mode", "Appearance", new List<string> { "Black", "White", "Theme" }, "nav_settings_regular"){ IsDefault = false},
+			new NonActionableSearchItem(new Setting<UiConfig, bool>(Services.UiConfig, b => b.Autocopy), "Auto copy addresses", "Settings", new List<string>(), "nav_settings_regular")  { IsDefault = false },
+			new NonActionableSearchItem(new Setting<UiConfig, bool>(Services.UiConfig, b => b.AutoPaste), "Auto paste addresses", "Settings", new List<string>(), "nav_settings_regular")  { IsDefault = false },
+			new NonActionableSearchItem(new Setting<UiConfig, bool>(Services.UiConfig, b => b.HideOnClose), "Run in background when closed", "Settings", new List<string>() { "hide", "tray" }, "nav_settings_regular") { IsDefault = false },
+			new NonActionableSearchItem(new Setting<UiConfig, bool>(Services.UiConfig, b => b.RunOnSystemStartup), "Run Wasabi when computer starts", "Settings", new List<string>() { "startup", "boot" }, "nav_settings_regular") { IsDefault = false },
+			new NonActionableSearchItem(new Setting<TorSettingsTabViewModel, bool>(torSettings, b => b.UseTor, true), "Network anonymization (Tor)", "Settings", new List<string>(), "nav_settings_regular") { IsDefault = false },
+			new NonActionableSearchItem(new Setting<TorSettingsTabViewModel, bool>(torSettings, b => b.TerminateTorOnExit, true), "Terminate Tor when Wasabi shuts down", "Settings", new List<string>(), "nav_settings_regular") { IsDefault = false },
 			new NonActionableSearchItem(new Setting<BitcoinTabSettingsViewModel, bool>(btSettings, b => b.StartLocalBitcoinCoreOnStartup), "Start local Bitcoin core on startup", "Settings", new List<string>(), "nav_settings_regular") { IsDefault = false },
 			new NonActionableSearchItem(new Setting<BitcoinTabSettingsViewModel, bool>(btSettings, b => b.StopLocalBitcoinCoreOnShutdown), "Stop local Bitcoin core on shutdown", "Settings", new List<string>(), "nav_settings_regular") { IsDefault = false },
 		};
