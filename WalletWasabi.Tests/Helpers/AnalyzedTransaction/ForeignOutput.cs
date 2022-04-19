8da162f71bfa9663aca13116e4bed374dda96f8d
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2022-04-19 16:54:15 +0200

    Introduce test environment for transaction analysis

---
+++ WalletWasabi.Tests/Helpers/AnalyzedTransaction/ForeignOutput.cs
@@ -0,0 +1,29 @@
+using NBitcoin;
+using WalletWasabi.Blockchain.Keys;
+
+namespace WalletWasabi.Tests.Helpers.AnalyzedTransaction;
+
+public record ForeignOutput
+{
+	public Transaction Transaction;
+	public uint Index;
+
+	public ForeignOutput(Transaction transaction, uint index)
+	{
+		Transaction = transaction;
+		Index = index;
+	}
+
+	public OutPoint ToOutPoint()
+	{
+		return new OutPoint(Transaction, Index);
+	}
+
+	public static ForeignOutput Create(Money amount, Script scriptPubKey)
+	{
+		Transaction transaction = Transaction.Create(Network.Main);
+		TxOut txOut = new(amount, scriptPubKey);
+		transaction.Outputs.Add(txOut);
+		return new ForeignOutput(transaction, 0);
+	}
+}
