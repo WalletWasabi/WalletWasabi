0899b0c3ad35899f3765241b95f8320d18190bef
David Molnar <molnardavid84@gmail.com>
2022-04-29 10:45:49 +0200

    Remove Wait too

--- WalletWasabi.Packager/scripts/Wasabi_release.ps1
+++ WalletWasabi.Packager/scripts/Wasabi_release.ps1
@@ -12,10 +12,11 @@ $host.UI.RawUI.BackgroundColor = "Black"
 Read-Host -Prompt 'Remove and Plug the penrive to macOS and run the packager to notarize the files. Starting MSI build [Press ENTER]'
 
 $arguments = $env:userprofile + '\Desktop\WalletWasabi\WalletWasabi.WindowsInstaller\WalletWasabi.WindowsInstaller.wixproj /Build "Release|x64"'
-Start-Process -FilePath 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com' -ArgumentList $arguments -Wait # If -NoNewWindow added devenv will hang forever at the end of the build.
+Start-Process -FilePath 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.com' -ArgumentList $arguments # If -Wait -NoNewWindow added devenv will hang forever at the end of the build.
 
 $host.UI.RawUI.ForegroundColor = "Green"
 $host.UI.RawUI.BackgroundColor = "Black"
+Read-Host -Prompt 'Wait until WiX building the MSI installer, then [Press ENTER]'
 Read-Host -Prompt 'Wait until macOS notarization is done and insert the pendrive to this PC [Press ENTER]'
 dotnet run -- sign
 
