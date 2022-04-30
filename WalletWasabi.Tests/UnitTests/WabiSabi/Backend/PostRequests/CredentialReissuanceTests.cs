acaf4851e3bac4561d47e70df21c1b649f0215ed
Yahia Chiheb <chihebyahia@gmail.com>
2022-04-30 22:11:01 +0100

    Fix CF

--- WalletWasabi.Tests/UnitTests/WabiSabi/Backend/PostRequests/CredentialReissuanceTests.cs
+++ WalletWasabi.Tests/UnitTests/WabiSabi/Backend/PostRequests/CredentialReissuanceTests.cs
@@ -26,8 +26,7 @@ public class CredentialReissuanceTest
 		// Step 1. Create credentials
 		var (amClient, vsClient, amIssuer, vsIssuer, amZeroCredentials, vsZeroCredentials) = WabiSabiFactory.CreateWabiSabiClientsAndIssuers(round);
 
-		var amountsToRequest = new[]
-			{alice.CalculateRemainingAmountCredentials(round.FeeRate, round.CoordinationFeeRate).Satoshi};
+		var amountsToRequest = new[] { alice.CalculateRemainingAmountCredentials(round.FeeRate, round.CoordinationFeeRate).Satoshi };
 		var (amCredentialRequest, amValid) = amClient.CreateRequest(
 			amountsToRequest,
 			amZeroCredentials, // FIXME doesn't make much sense
