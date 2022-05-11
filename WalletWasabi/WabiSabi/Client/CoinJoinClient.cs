e5bf6e23d3b1c0927af9e100a14db6038bead5fc
Dávid Molnár <molnardavid84@gmail.com>
2022-05-11 16:19:25 +0200

    Update WalletWasabi/WabiSabi/Client/CoinJoinClient.cs
    Co-authored-by: Kimi <58662979+kiminuo@users.noreply.github.com>

--- WalletWasabi/WabiSabi/Client/CoinJoinClient.cs
+++ WalletWasabi/WabiSabi/Client/CoinJoinClient.cs
@@ -178,7 +178,7 @@ public class CoinJoinClient
 
 		ImmutableArray<(AliceClient AliceClient, PersonCircuit PersonCircuit)> registeredAliceClientAndCircuits = ImmutableArray<(AliceClient, PersonCircuit)>.Empty;
 
-		// Because of the nature of the protocol, the inputreg and the connconfirm phases are done subsequently thus having a merged timeout.
+		// Because of the nature of the protocol, the input registration and the connection confirmation phases are done subsequently thus they have a merged timeout.
 		var timeUntilOutputReg = (roundState.InputRegistrationEnd - DateTimeOffset.Now) + roundState.CoinjoinState.Parameters.ConnectionConfirmationTimeout;
 
 		try
