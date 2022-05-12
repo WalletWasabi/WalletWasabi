31a99b34e8759e712d1b4e5e08c7991e5be1550a
Roland Soós <soos.roland93@gmail.com>
2022-05-12 09:35:32 +0200

    remove unused usings

--- WalletWasabi.Fluent/Converters/BooleanValueConverters.cs
+++ WalletWasabi.Fluent/Converters/BooleanValueConverters.cs
@@ -1,7 +1,6 @@
 using System.Globalization;
 using Avalonia;
 using Avalonia.Data.Converters;
-using Avalonia.Media;
 
 namespace WalletWasabi.Fluent.Converters;
 
--- WalletWasabi.Fluent/ViewModels/Dialogs/ShuttingDownViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Dialogs/ShuttingDownViewModel.cs
@@ -1,4 +1,3 @@
-using ReactiveUI;
 using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
