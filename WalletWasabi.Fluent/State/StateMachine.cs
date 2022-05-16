b21911d44f69ee5ce7977562ac3ff39121a928e3
Dan Walmsley <dan@walms.co.uk>
2022-05-16 12:40:42 +0100

    Statemachine: Allow custom builders

--- WalletWasabi.Fluent/State/StateMachine.cs
+++ WalletWasabi.Fluent/State/StateMachine.cs
@@ -172,6 +172,11 @@ public class StateMachine<TState, TTrigger> where TTrigger : Enum where TState :
 			return this;
 		}
 
+		public StateContext Custom(Func<StateContext, StateContext> custom)
+		{
+			return custom(this);
+		}
+
 		public StateContext OnTrigger(TTrigger trigger, Action action)
 		{
 			if (_triggerActions.TryGetValue(trigger, out var t))
