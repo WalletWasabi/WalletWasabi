b5c9a0f60b722d45acf5ec531d33c6b8861c71c3
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-16 14:26:14 +0200

    Create HistoryItemTypeClassBehavior.cs

---
+++ WalletWasabi.Fluent/Behaviors/HistoryItemTypeClassBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Reactive.Disposables;
+using Avalonia.Controls.Primitives;
+using ReactiveUI;
+using WalletWasabi.Fluent.ViewModels.Wallets.Home.History.HistoryItems;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+public class HistoryItemTypeClassBehavior : AttachedToVisualTreeBehavior<TreeDataGridRow>
+{
+	protected override void OnAttachedToVisualTree(CompositeDisposable disposable)
+	{
+		AssociatedObject?.WhenAnyValue(x => x.DataContext)
+			.Subscribe(x =>
+			{
+				if (x is TransactionHistoryItemViewModel)
+				{
+					AssociatedObject.Classes.Add("Transaction");
+				}
+				else if (x is CoinJoinHistoryItemViewModel)
+				{
+					AssociatedObject.Classes.Add("CoinJoin");
+				}
+				else if (x is CoinJoinsHistoryItemViewModel)
+				{
+					AssociatedObject.Classes.Add("CoinJoins");
+				}
+			})
+			.DisposeWith(disposable);
+	}
+}
