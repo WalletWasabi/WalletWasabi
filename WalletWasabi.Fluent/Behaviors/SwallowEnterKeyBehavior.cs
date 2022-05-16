0e0c454d4b004079eb63d492904198a761c8114a
José Manuel Nieto <superjmn@outlook.com>
2022-05-16 12:03:44 +0200

    Make SearchBar swallow Enter key

---
+++ WalletWasabi.Fluent/Behaviors/SwallowEnterKeyBehavior.cs
@@ -0,0 +1,21 @@
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia.Input;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+internal class SwallowEnterKeyBehavior : AttachedToVisualTreeBehavior<InputElement>
+{
+	protected override void OnAttachedToVisualTree(CompositeDisposable disposable)
+	{
+		if (AssociatedObject is null)
+		{
+			return;
+		}
+
+		Observable
+			.FromEventPattern<KeyEventArgs>(AssociatedObject, nameof(InputElement.KeyDown))
+			.Subscribe(r => r.EventArgs.Handled = true)
+			.DisposeWith(disposable);
+	}
+}
