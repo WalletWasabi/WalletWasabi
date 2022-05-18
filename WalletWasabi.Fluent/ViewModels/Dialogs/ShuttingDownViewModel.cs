5d9028c1a64f0f8de37cc00318daf1338b8f6954
ichthus1604 <ichthus1604@gmail.com>
2022-05-18 10:47:04 -0300

    Changed ShuttingDownViewModel to a RoutableViewModel

--- WalletWasabi.Fluent/ViewModels/Dialogs/ShuttingDownViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Dialogs/ShuttingDownViewModel.cs
@@ -1,43 +1,33 @@
 using ReactiveUI;
-using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using WalletWasabi.Fluent.ViewModels.Dialogs.Base;
+using WalletWasabi.Fluent.ViewModels.Navigation;
 
 namespace WalletWasabi.Fluent.ViewModels.Dialogs;
 
 [NavigationMetaData(Title = "Please wait to shut down...")]
-public partial class ShuttingDownViewModel : DialogViewModelBase<Unit>
+public partial class ShuttingDownViewModel : RoutableViewModel
 {
 	private readonly ApplicationViewModel _applicationViewModel;
-	private IDisposable? _intervalDisposable;
 
 	public ShuttingDownViewModel(ApplicationViewModel applicationViewModel)
 	{
 		_applicationViewModel = applicationViewModel;
-
-		NextCommand = ReactiveCommand.Create(OnNext);
+		NextCommand = CancelCommand;
 	}
 
 	protected override void OnNavigatedTo(bool isInHistory, CompositeDisposable disposables)
 	{
-		_intervalDisposable =
-			Observable.Interval(TimeSpan.FromSeconds(3))
-					  .ObserveOn(RxApp.MainThreadScheduler)
-					  .Subscribe(_ =>
+		Observable.Interval(TimeSpan.FromSeconds(3))
+				  .ObserveOn(RxApp.MainThreadScheduler)
+				  .Subscribe(_ =>
+				  {
+					  if (_applicationViewModel.CanShutdown())
 					  {
-						  if (_applicationViewModel.CanShutdown())
-						  {
-							  Close();
-							  _applicationViewModel.ShutDown();
-						  }
-					  })
-					  .DisposeWith(disposables);
-	}
-
-	private void OnNext()
-	{
-		_intervalDisposable?.Dispose();
-		Close();
+						  Navigate().Clear();
+						  _applicationViewModel.ShutDown();
+					  }
+				  })
+				  .DisposeWith(disposables);
 	}
 }
