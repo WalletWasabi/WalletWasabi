fa566cfbfb19911aa4ceaa95404b2375f3919361
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-05-23 11:07:51 +0200

    Remove unused converter

--- WalletWasabi.Fluent/Converters/ListCountConverters.cs
+++
@@ -1,9 +0,0 @@
-using Avalonia.Data.Converters;
-
-namespace WalletWasabi.Fluent.Converters;
-
-public static class ListCountConverters
-{
-	public static readonly IValueConverter IsSingleItem =
-		new FuncValueConverter<int, bool>(x => x == 1);
-}
