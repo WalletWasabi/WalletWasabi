0b7c7e594c71b943a59e97a8ba3b992bb16917d0
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-05-25 23:29:49 +0800

    add hasExchangeRate property

--- WalletWasabi.Fluent/ViewModels/ExchangeRateViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/ExchangeRateViewModel.cs
@@ -15,7 +15,12 @@ public class ExchangeRateViewModel : ViewModelBase
 		BtcToUsdExchangeRate = usdExchangeRate
 			.Select(usd => usd.FormattedFiat("N0") + " USD")
 			.ObserveOn(RxApp.MainThreadScheduler);
+
+		HasExchangeRate = usdExchangeRate
+			.Select(usd => usd > 0m)
+			.ObserveOn(RxApp.MainThreadScheduler);
 	}
 
+	public IObservable<bool>? HasExchangeRate { get; }
 	public IObservable<string> BtcToUsdExchangeRate { get; }
 }
