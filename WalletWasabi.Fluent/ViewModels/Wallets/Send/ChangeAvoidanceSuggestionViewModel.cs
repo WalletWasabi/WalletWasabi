c155a3ed781c6b6934cd4bf588becac575d7bd63
Roland Soós <soos.roland93@gmail.com>
2022-05-26 08:15:36 +0200

    make sure BnB uses coins from the same pockets

--- WalletWasabi.Fluent/ViewModels/Wallets/Send/ChangeAvoidanceSuggestionViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Send/ChangeAvoidanceSuggestionViewModel.cs
@@ -1,10 +1,12 @@
 using NBitcoin;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.Blockchain.TransactionBuilding;
+using WalletWasabi.Blockchain.TransactionOutputs;
 using WalletWasabi.Fluent.Helpers;
 using WalletWasabi.Wallets;
 
@@ -41,16 +43,16 @@ public partial class ChangeAvoidanceSuggestionViewModel : SuggestionViewModel
 
 	public BuildTransactionResult TransactionResult { get; }
 
-	public static async IAsyncEnumerable<ChangeAvoidanceSuggestionViewModel> GenerateSuggestionsAsync(
-		TransactionInfo transactionInfo,
+	public static async IAsyncEnumerable<ChangeAvoidanceSuggestionViewModel> GenerateSuggestionsAsync(TransactionInfo transactionInfo,
 		BitcoinAddress destination,
 		Wallet wallet,
+		ImmutableArray<SmartCoin> coinsToUse,
 		int maxInputCount,
 		decimal usdExchangeRate,
 		[EnumeratorCancellation] CancellationToken cancellationToken)
 	{
 		var selections = ChangelessTransactionCoinSelector.GetAllStrategyResultsAsync(
-			transactionInfo.Coins,
+			coinsToUse,
 			transactionInfo.FeeRate,
 			new TxOut(transactionInfo.Amount, destination),
 			maxInputCount,
