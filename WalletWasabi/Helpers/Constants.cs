4357067448a4e95de6eb2ced90fc21047d9d674e
David Molnar <molnardavid84@gmail.com>
2022-05-30 12:46:01 +0200

    Update WW1 Legal docs

--- WalletWasabi/Helpers/Constants.cs
+++ WalletWasabi/Helpers/Constants.cs
@@ -61,7 +61,7 @@ public static class Constants
 
 	public static readonly Version HwiVersion = new("2.0.2");
 	public static readonly Version BitcoinCoreVersion = new("21.2");
-	public static readonly Version LegalDocumentsVersion = new(2, 0);
+	public static readonly Version LegalDocumentsVersion = new(3, 0);
 
 	// Defined in hours. Do not modify these values or the order!
 	public static readonly int[] CoinJoinFeeRateMedianTimeFrames = new[] { 24, 168, 720 };
