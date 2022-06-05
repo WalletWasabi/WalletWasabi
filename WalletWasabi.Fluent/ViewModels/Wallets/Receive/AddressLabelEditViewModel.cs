78511150333d65398152a649106bd4fb7c21978f
ichthus1604 <ichthus1604@gmail.com>
2022-06-05 02:35:03 -0300

    Receive Address - Edit Label - Suggestions

--- WalletWasabi.Fluent/ViewModels/Wallets/Receive/AddressLabelEditViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Receive/AddressLabelEditViewModel.cs
@@ -1,31 +1,28 @@
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Reactive.Linq;
 using ReactiveUI;
 using WalletWasabi.Blockchain.Analysis.Clustering;
 using WalletWasabi.Blockchain.Keys;
 using WalletWasabi.Fluent.ViewModels.Navigation;
+using WalletWasabi.Fluent.ViewModels.Wallets.Labels;
 
 namespace WalletWasabi.Fluent.ViewModels.Wallets.Receive;
 
 [NavigationMetaData(Title = "Edit Labels")]
 public partial class AddressLabelEditViewModel : RoutableViewModel
 {
-	[AutoNotify] private ObservableCollection<string> _labels;
 	[AutoNotify] private SmartLabel? _finalLabel;
 	[AutoNotify] private bool _isCurrentTextValid;
 
-	public AddressLabelEditViewModel(ReceiveAddressesViewModel owner, HdPubKey hdPubKey, KeyManager keyManager, HashSet<string> suggestions)
+	public AddressLabelEditViewModel(ReceiveAddressesViewModel owner, HdPubKey hdPubKey, KeyManager keyManager)
 	{
-		Suggestions = suggestions;
-		_labels = new(hdPubKey.Label);
+		SuggestionLabels = new SuggestionLabelsViewModel(3, hdPubKey.Label);
 
 		SetupCancel(enableCancel: true, enableCancelOnEscape: true, enableCancelOnPressed: true);
 
-		Labels
+		SuggestionLabels.Labels
 			.WhenAnyValue(x => x.Count)
 			.ObserveOn(RxApp.MainThreadScheduler)
-			.Subscribe(_ => FinalLabel = new SmartLabel(Labels));
+			.Subscribe(_ => FinalLabel = new SmartLabel(SuggestionLabels.Labels));
 
 		var canExecute =
 			this.WhenAnyValue(x => x.FinalLabel, x => x.IsCurrentTextValid)
@@ -50,5 +47,5 @@ public partial class AddressLabelEditViewModel : RoutableViewModel
 			canExecute);
 	}
 
-	public HashSet<string> Suggestions { get; }
+	public SuggestionLabelsViewModel SuggestionLabels { get; }
 }
