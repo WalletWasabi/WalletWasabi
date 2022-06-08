0f322b75f3c8e70d869f32902d247e84d9628aff
David Molnar <molnardavid84@gmail.com>
2022-06-08 17:00:53 +0200

    Better equality

--- WalletWasabi.Fluent/ViewModels/CoinJoinProfiles/CoinJoinProfileViewModelBase.cs
+++ WalletWasabi.Fluent/ViewModels/CoinJoinProfiles/CoinJoinProfileViewModelBase.cs
@@ -9,4 +9,24 @@ public abstract class CoinJoinProfileViewModelBase : ViewModelBase
 	public virtual int AnonScoreTarget { get; } = 5;
 
 	public abstract int FeeRateMedianTimeFrameHours { get; }
+
+	public override bool Equals(object? obj)
+	{
+		if (ReferenceEquals(this, obj))
+		{
+			return true;
+		}
+
+		if (obj is not CoinJoinProfileViewModelBase profile)
+		{
+			return false;
+		}
+
+		return profile.AnonScoreTarget == AnonScoreTarget && profile.FeeRateMedianTimeFrameHours == FeeRateMedianTimeFrameHours;
+	}
+
+	public override int GetHashCode()
+	{
+		return HashCode.Combine(AnonScoreTarget, FeeRateMedianTimeFrameHours);
+	}
 }
