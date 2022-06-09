8d02ff58b822eb1c8cf918a04b8db1f90f34cf7e
nopara73 <adam.ficsor73@gmail.com>
2022-06-09 13:41:31 +0200

    Make label suggestions action dependent

--- WalletWasabi.Fluent/ViewModels/Wallets/Labels/SuggestionLabelViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Labels/SuggestionLabelViewModel.cs
@@ -2,13 +2,13 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Labels;
 
 public class SuggestionLabelViewModel : ViewModelBase
 {
-	public SuggestionLabelViewModel(string label, int count)
+	public SuggestionLabelViewModel(string label, int score)
 	{
 		Label = label;
-		Count = count;
+		Score = score;
 	}
 
 	public string Label { get; }
 
-	public int Count { get; }
+	public int Score { get; }
 }
