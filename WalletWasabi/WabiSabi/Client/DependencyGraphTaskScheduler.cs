59b7476035980b41bffdf6c7efe54880578ad68e
Lucas Ontivero <lucasontivero@gmail.com>
2022-06-21 01:05:22 -0300

    Prevent coinjoin process halting

--- WalletWasabi/WabiSabi/Client/DependencyGraphTaskScheduler.cs
+++ WalletWasabi/WabiSabi/Client/DependencyGraphTaskScheduler.cs
@@ -182,6 +182,10 @@ public class DependencyGraphTaskScheduler
 						hdPubKey.SetKeyState(KeyState.Used);
 					}
 				}
+				catch (Exception ex)
+				{
+					Logger.LogDebug($"Output registration error message:'{ex.Message}'.");
+				}
 			}
 		).ToImmutableArray();
 
