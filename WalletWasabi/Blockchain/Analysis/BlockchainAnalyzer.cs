e625e29d8435a8b2388ae63bd6f4a47f33196b63
Lucas Ontivero <lucasontivero@gmail.com>
2022-07-01 19:12:54 -0300

    Do not penalize so hard coins if their values can be also expressed and combination os other outputs

--- WalletWasabi/Blockchain/Analysis/BlockchainAnalyzer.cs
+++ WalletWasabi/Blockchain/Analysis/BlockchainAnalyzer.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using WalletWasabi.Blockchain.Keys;
 using WalletWasabi.Blockchain.Transactions;
+using WalletWasabi.Extensions;
+using WalletWasabi.WabiSabi.Client;
 
 namespace WalletWasabi.Blockchain.Analysis;
 
 public class BlockchainAnalyzer
 {
+	private static long[] StdDenoms = new []{
+		5000L, 6561L, 8192L, 10000L, 13122L, 16384L, 19683L, 20000L, 32768L, 39366L, 50000L, 59049L, 65536L, 100000L, 118098L,
+		131072L, 177147L, 200000L, 262144L, 354294L, 500000L, 524288L, 531441L, 1000000L, 1048576L, 1062882L, 1594323L, 2000000L,
+		2097152L, 3188646L, 4194304L, 4782969L, 5000000L, 8388608L, 9565938L, 10000000L, 14348907L, 16777216L, 20000000L,
+		28697814L, 33554432L, 43046721L, 50000000L, 67108864L, 86093442L, 100000000L, 129140163L, 134217728L, 200000000L,
+		258280326L, 268435456L, 387420489L, 500000000L, 536870912L, 774840978L, 1000000000L, 1073741824L, 1162261467L,
+		2000000000L, 2147483648L, 2324522934L, 3486784401L, 4294967296L, 5000000000L, 6973568802L, 8589934592L, 10000000000L,
+		10460353203L, 17179869184L, 20000000000L, 20920706406L, 31381059609L, 34359738368L, 50000000000L, 62762119218L,
+		68719476736L, 94143178827L, 100000000000L, 137438953472L
+	};
+
 	public BlockchainAnalyzer(int privacyLevelThreshold)
 	{
 		PrivacyLevelThreshold = privacyLevelThreshold;
@@ -115,6 +128,7 @@ public class BlockchainAnalyzer
 			.ToDictionary(x => x.Key, y => y.Count());
 
 		var foreignInputCount = tx.ForeignInputs.Count;
+		var allOutputValues = tx.Transaction.Outputs.Select(x => x.Value.Satoshi).ToArray();			
 
 		foreach (var newCoin in tx.WalletOutputs)
 		{
@@ -122,11 +136,30 @@ public class BlockchainAnalyzer
 			var equalOutputCount = indistinguishableOutputs[output.Value];
 			var ownEqualOutputCount = indistinguishableWalletOutputs[output.Value];
 
+			// Create a list containing the coinjoin's output values that repeat and are also standard.
+			var stdValuesUsedInOutputs = indistinguishableOutputs.Keys.Intersect(StdDenoms);
+			var stdValuesUsedInOutputsSmallerThanCoin = stdValuesUsedInOutputs.Where(x => x < output.Value).ToArray();
+
+			// Compute the decompositions in which the current analyzed txout value can be expressed (in no more than 6 elements,
+			// this is arbitrary) and count how many of those are present in the coinjoin transaction.
+			var newCoinEquivalentCount = Decomposer
+				.Decompose(output.Value, tolerance: 0, maxCount: 8, stdValuesUsedInOutputsSmallerThanCoin)
+				.Select(d =>
+					Decomposer.ToRealValuesArray(d.Decomposition, d.Count, stdValuesUsedInOutputsSmallerThanCoin))
+				.TakeWhile(equivalentCandidate => allOutputValues.IsSuperSetOf(equivalentCandidate))
+				.Take(10) // Arbitrary circuit breaker. If there are at least 10 possible ways to sum up the same coin amount then stop.
+				.Count();
+
+			// if the value of the current output can be expressed using 8 or more combinations also present in the
+			// coinjoin then do not punish it too hard.
+			var shouldBePenalized = newCoinEquivalentCount < 8;
+			var punishmentSeverity = (int)Math.Max(1, shouldBePenalized ? ownEqualOutputCount : ownEqualOutputCount / 3.0);  
+			
 			// Anonset gain cannot be larger than others' input count.
 			var anonset = Math.Min(equalOutputCount - ownEqualOutputCount, foreignInputCount);
 
 			// Picking randomly an output would make our anonset: total/ours.
-			anonset /= ownEqualOutputCount;
+			anonset /= punishmentSeverity;
 
 			// If not anonset gain achieved on the output, then it's best to assume it's change.
 			var startingOutputAnonset = anonset == 0 ? startingNonMixedOutputAnonset : startingMixedOutputAnonset;
