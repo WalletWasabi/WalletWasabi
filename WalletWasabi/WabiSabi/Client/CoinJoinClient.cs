99e95f2d5aa18a229b8f53a7ea9c7b104f8b8ee1
nopara73 <adam.ficsor73@gmail.com>
2022-07-03 15:48:33 +0200

    Remove wrong log entry

--- WalletWasabi/WabiSabi/Client/CoinJoinClient.cs
+++ WalletWasabi/WabiSabi/Client/CoinJoinClient.cs
@@ -199,7 +199,6 @@ public class CoinJoinClient
 
 			if (!registeredAliceClientAndCircuits.Any())
 			{
-				roundState.LogInfo("There are no available Alices to participate with.");
 				return new CoinJoinResult(false);
 			}
 
