b72765fe834e068fa6d8feae83473bc0ef05ccbb
Kimi <58662979+kiminuo@users.noreply.github.com>
2022-07-05 22:40:09 +0200

    Remove WW1 code. (#8639)

--- WalletWasabi/Wallets/WalletManager.cs
+++ WalletWasabi/Wallets/WalletManager.cs
@@ -10,7 +10,6 @@ using WalletWasabi.Blockchain.Keys;
 using WalletWasabi.Blockchain.TransactionOutputs;
 using WalletWasabi.Blockchain.TransactionProcessing;
 using WalletWasabi.Blockchain.Transactions;
-using WalletWasabi.CoinJoin.Client.Clients.Queuing;
 using WalletWasabi.Helpers;
 using WalletWasabi.Logging;
 using WalletWasabi.Models;
