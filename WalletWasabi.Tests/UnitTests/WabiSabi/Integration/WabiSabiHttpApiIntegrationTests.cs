21b720f91ed0eb2ab6946277b1b9cd39e303f994
Yahia Chiheb <chihebyahia@gmail.com>
2022-07-05 14:57:48 +0100

    Move class to its own file

--- WalletWasabi.Tests/UnitTests/WabiSabi/Integration/WabiSabiHttpApiIntegrationTests.cs
+++ WalletWasabi.Tests/UnitTests/WabiSabi/Integration/WabiSabiHttpApiIntegrationTests.cs
@@ -1,5 +1,4 @@
 using System.Collections.Immutable;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using NBitcoin;
@@ -569,27 +568,5 @@ public class WabiSabiHttpApiIntegrationTests : IClassFixture<WabiSabiApiApplicat
 		var (response, _) = await apiClient.RegisterInputAsync(round.Id, coinToRegister.Outpoint, ownershipProof, CancellationToken.None);
 
 		Assert.NotEqual(Guid.Empty, response.Value);
-	}
-}
-
-public class StuttererHttpClient : HttpClientWrapper
-{
-	public StuttererHttpClient(HttpClient httpClient) : base(httpClient)
-	{
-	}
-
-	public override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken token = default)
-	{
-		using HttpRequestMessage requestClone1 = request.Clone();
-		using HttpRequestMessage requestClone2 = request.Clone();
-
-		HttpResponseMessage result1 = await base.SendAsync(requestClone1, token).ConfigureAwait(false);
-		HttpResponseMessage result2 = await base.SendAsync(requestClone2, token).ConfigureAwait(false);
-
-		string content1 = await result1.Content.ReadAsStringAsync(token).ConfigureAwait(false);
-		string content2 = await result2.Content.ReadAsStringAsync(token).ConfigureAwait(false);
-
-		Assert.Equal(content1, content2);
-		return result2;
 	}
 }
