33cb64405516fac340263f5d41a79f2ceb56b74b
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-07-11 12:37:38 +0200

    Set initial selectable pseudo class

--- WalletWasabi.Fluent/Controls/NavBarItem.axaml.cs
+++ WalletWasabi.Fluent/Controls/NavBarItem.axaml.cs
@@ -23,6 +23,7 @@ public class NavBarItem : ListBoxItem
 	public NavBarItem()
 	{
 		UpdateIndicatorOrientationPseudoClasses(IndicatorOrientation);
+		UpdateIsSelectablePseudoClasses(IsSelectable);
 	}
 
 	/// <summary>
