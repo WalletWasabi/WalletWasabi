b172cac996bc706581db0e30c8c4a090af34752f
Dávid Molnár <molnardavid84@gmail.com>
2022-07-14 14:07:34 +0200

    Update WalletWasabi/Exceptions/TransactionSizeException.cs
    Co-authored-by: Kimi <58662979+kiminuo@users.noreply.github.com>

--- WalletWasabi/Exceptions/TransactionSizeException.cs
+++ WalletWasabi/Exceptions/TransactionSizeException.cs
@@ -4,7 +4,8 @@ namespace WalletWasabi.Exceptions;
 
 public class TransactionSizeException : Exception
 {
-	public TransactionSizeException(Money minimum, Money actual) : base($"Transaction size over the limit. Needed: {minimum.ToString(false, true)} BTC. The maximum amount you can spent at once with your coins, is: {actual.ToString(false, true)} BTC.")
+	public TransactionSizeException(Money minimum, Money actual)
+		: base($"Transaction size over the limit. Needed {minimum.ToString(false, true)} BTC. The maximum amount you can spent your coins is {actual.ToString(false, true)} BTC at the moment.")
 	{
 		Minimum = minimum ?? Money.Zero;
 		Actual = actual ?? Money.Zero;
