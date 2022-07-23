f805e1a0dac124107781bcc15c8113be9a1a8969
José Manuel Nieto <superjmn@outlook.com>
2022-07-23 13:59:58 +0200

    Componentalize

---
+++ WalletWasabi.Fluent/Controls/DestinationEntry/AddressEntryControl.axaml.cs
@@ -0,0 +1,25 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Controls.DestinationEntry;
+public partial class AddressEntryControl : UserControl
+{
+	public AddressEntryControl()
+	{
+		InitializeComponent();
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+
+	public static readonly StyledProperty<string> WatermarkProperty = AvaloniaProperty.Register<AddressEntryControl, string>("Watermark");
+
+	public string Watermark
+	{
+		get => GetValue(WatermarkProperty);
+		set => SetValue(WatermarkProperty, value);
+	}
+}
