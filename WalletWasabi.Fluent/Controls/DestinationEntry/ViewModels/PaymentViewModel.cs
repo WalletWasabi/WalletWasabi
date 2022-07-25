925ab9a8d649837140cf4679bfb2be940cde7bf1
José Manuel Nieto <superjmn@outlook.com>
2022-07-25 16:32:06 +0200

    Cleanup

--- WalletWasabi.Fluent/Controls/DestinationEntry/ViewModels/PaymentViewModel.cs
+++ WalletWasabi.Fluent/Controls/DestinationEntry/ViewModels/PaymentViewModel.cs
@@ -1,88 +1,84 @@
-using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using ReactiveUI;
 using ReactiveUI.Validation.Extensions;
 using ReactiveUI.Validation.Helpers;
 
-namespace WalletWasabi.Fluent.Controls.DestinationEntry.ViewModels
+namespace WalletWasabi.Fluent.Controls.DestinationEntry.ViewModels;
+
+public class PaymentViewModel : ReactiveValidationObject, IDisposable
 {
-    public class PaymentViewModel : ReactiveValidationObject, IDisposable
-    {
-        private string address = "";
-        private decimal amount;
-        private readonly CompositeDisposable disposables = new();
+	private string address = "";
+	private decimal amount;
+	private readonly CompositeDisposable disposables = new();
 
-        public PaymentViewModel(IMutableAddressHost mutableAddressHost, ContentChecker<string> contentChecker, Func<decimal, bool> isAmountValid)
-        {
-	        MutableAddressHost = mutableAddressHost;
-	        MutableAddressHost.ParsedAddress
-		        .WhereNotNull()
-		        .Do(Transfer)
-		        .Subscribe()
-		        .DisposeWith(disposables);
+	public PaymentViewModel(IMutableAddressHost mutableAddressHost, ContentChecker<string> contentChecker, Func<decimal, bool> isAmountValid)
+	{
+		MutableAddressHost = mutableAddressHost;
+		MutableAddressHost.ParsedAddress
+			.WhereNotNull()
+			.Do(Transfer)
+			.Subscribe()
+			.DisposeWith(disposables);
 
-	        HasNewContent = ApplicationUtils.IsMainWindowActive.CombineLatest(contentChecker.HasNewContent, (isActive, hasNewContent) => isActive && hasNewContent);
+		HasNewContent = ApplicationUtils.IsMainWindowActive.CombineLatest(contentChecker.HasNewContent, (isActive, hasNewContent) => isActive && hasNewContent);
 
-            if (Services.UiConfig.AutoPaste)
-            {
-	            ApplicationUtils.IsMainWindowActive.CombineLatest(contentChecker.Content)
-		            .Where(a => a.First)
-		            .Select(x => x.Second)
-		            .Subscribe(content => MutableAddressHost.Text = content)
-		            .DisposeWith(disposables);
-            }
+		if (Services.UiConfig.AutoPaste)
+		{
+			ApplicationUtils.IsMainWindowActive.CombineLatest(contentChecker.Content)
+				.Where(a => a.First)
+				.Select(x => x.Second)
+				.Subscribe(content => MutableAddressHost.Text = content)
+				.DisposeWith(disposables);
+		}
 
-            var validAmount = this.WhenAnyValue(x => x.Amount).Select(x => x > 0);
+		var validAmount = this.WhenAnyValue(x => x.Amount).Select(x => x > 0);
 
-            this.ValidationRule(
-                viewModel => viewModel.Amount, validAmount.Skip(1),
-                "Amount should be greater than 0");
+		this.ValidationRule(
+			viewModel => viewModel.Amount, validAmount.Skip(1),
+			"Amount should be greater than 0");
 
-            this.ValidationRule(
-	            x => x.Amount,
-	            isAmountValid,
-	            "Insufficient funds to cover the amount requested");
-        }
+		this.ValidationRule(
+			x => x.Amount,
+			isAmountValid,
+			"Insufficient funds to cover the amount requested");
+	}
 
-        private void Transfer(Address a)
-        {
-	        Address = a.BtcAddress;
+	private void Transfer(Address a)
+	{
+		Address = a.BtcAddress;
 
-	        if (a.Amount is not null)
-	        {
-		        Amount = a.Amount.Value;
-	        }
+		if (a.Amount is not null)
+		{
+			Amount = a.Amount.Value;
+		}
 
-	        if (a.EndPoint is not null)
-	        {
-		        EndPoint = a.EndPoint;
-	        }
-        }
+		if (a.EndPoint is not null)
+		{
+			EndPoint = a.EndPoint;
+		}
+	}
 
-        public Uri? EndPoint { get; set; }
+	public Uri? EndPoint { get; set; }
 
-        public ReactiveCommand<Unit, string> PasteCommand { get; }
+	public IObservable<bool> HasNewContent { get; }
 
-        public IObservable<bool> HasNewContent { get; }
+	public IMutableAddressHost MutableAddressHost { get; }
 
-        public IMutableAddressHost MutableAddressHost { get; }
+	public decimal Amount
+	{
+		get => amount;
+		set => this.RaiseAndSetIfChanged(ref amount, value);
+	}
 
-        public decimal Amount
-        {
-            get => amount;
-            set => this.RaiseAndSetIfChanged(ref amount, value);
-        }
+	public string Address
+	{
+		get => address;
+		set => this.RaiseAndSetIfChanged(ref address, value);
+	}
 
-        public string Address
-        {
-            get => address;
-            set => this.RaiseAndSetIfChanged(ref address, value);
-        }
-
-        public void Dispose()
-        {
-            disposables.Dispose();
-        }
-    }
+	public void Dispose()
+	{
+		disposables.Dispose();
+	}
 }
