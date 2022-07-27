d7ede5814f96a1611d0c69aad73ba975fa44c6e4
David Molnar <molnardavid84@gmail.com>
2022-07-27 13:56:44 +0200

    use for iteration

--- WalletWasabi/WabiSabi/Client/CoinJoinClient.cs
+++ WalletWasabi/WabiSabi/Client/CoinJoinClient.cs
@@ -825,7 +825,7 @@ public class CoinJoinClient
 
 	private static int GetRandomBiasedSameTxAllowance(WasabiRandom rnd, int percent)
 	{
-		foreach (var num in new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 })
+		for (int num = 0; num <= 100; num++)
 		{
 			if (rnd.GetInt(1, 101) <= percent)
 			{
