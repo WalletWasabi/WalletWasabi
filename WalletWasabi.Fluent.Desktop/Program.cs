f88b4fa8ffd38e18e78ae8984e4b048f28c6742e
José Manuel Nieto <superjmn@outlook.com>
2022-07-28 17:19:45 +0200

    Trace all info in exception on app crash

--- WalletWasabi.Fluent.Desktop/Program.cs
+++ WalletWasabi.Fluent.Desktop/Program.cs
@@ -103,7 +103,7 @@ public class Program
 
 					Logger.LogError(ex);
 
-					RxApp.MainThreadScheduler.Schedule(() => throw ex);
+					RxApp.MainThreadScheduler.Schedule(() => throw new ApplicationException("Command execution crashed", ex));
 				});
 
 			Logger.LogSoftwareStarted("Wasabi GUI");
