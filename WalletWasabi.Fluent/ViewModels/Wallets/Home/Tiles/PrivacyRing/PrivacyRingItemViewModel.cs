7104c1c08bbcbc35e1218b1a5b10f548bd9fd1d2
Ichthus1604 <98904713+ichthus1604@users.noreply.github.com>
2022-07-30 15:15:06 -0300

    partial commit

---
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/PrivacyRing/PrivacyRingItemViewModel.cs
@@ -0,0 +1,67 @@
+using Avalonia;
+using WalletWasabi.Blockchain.TransactionOutputs;
+using WalletWasabi.Fluent.Helpers;
+using WalletWasabi.Fluent.ViewModels.Wallets.Advanced.WalletCoins;
+
+namespace WalletWasabi.Fluent.ViewModels.Wallets.Home.Tiles;
+
+public class PrivacyRingItemViewModel : WalletCoinViewModel
+{
+	private const double TotalAngle = 2d * Math.PI;
+	private const double UprightAngle = Math.PI / 2d;
+
+	public PrivacyRingItemViewModel(PrivacyRingViewModel parent, SmartCoin coin, double start, double end) : base(coin)
+	{
+		var outerRadius = parent.OuterRadius;
+		var innerRadius = parent.InnerRadius;
+
+		Arc1Size = new Size(outerRadius, outerRadius);
+		Arc2Size = new Size(innerRadius, innerRadius);
+
+		var margin = 2d;
+
+		var startAngle = (TotalAngle * start) - UprightAngle;
+		var endAngle = (TotalAngle * end) - UprightAngle;
+
+		var outerOffset = outerRadius == 0 ? 0 : (margin / (TotalAngle * outerRadius) * TotalAngle);
+		var innerOffset = innerRadius == 0 ? 0 : (margin / (TotalAngle * innerRadius) * TotalAngle);
+
+		Origin1 = GetAnglePoint(outerRadius, startAngle + outerOffset);
+		Arc1 = GetAnglePoint(outerRadius, endAngle - outerOffset);
+		Origin2 = GetAnglePoint(innerRadius, endAngle - innerOffset);
+		Arc2 = GetAnglePoint(innerRadius, startAngle + innerOffset);
+
+		OuterRadius = outerRadius;
+		IsPrivate = coin.IsPrivate(parent.Wallet.KeyManager.AnonScoreTarget);
+		IsSemiPrivate = !IsPrivate && coin.IsSemiPrivate();
+		IsNonPrivate = !IsPrivate && !IsSemiPrivate;
+	}
+
+	public double OuterRadius { get; }
+
+	public Point Origin1 { get; }
+	public Point Arc1 { get; }
+	public Size Arc1Size { get; }
+
+	public Point Origin2 { get; }
+	public Point Arc2 { get; }
+	public Size Arc2Size { get; }
+
+	public bool IsPrivate { get; }
+	public bool IsSemiPrivate { get; }
+	public bool IsNonPrivate { get; }
+
+	private Point GetAnglePoint(double r, double angle)
+	{
+		var x = r * Math.Cos(angle);
+		var y = r * Math.Sin(angle);
+		return new Point(x, y);
+	}
+}
+
+public enum PrivacyRingItemPrivacyState
+{
+	NonPrivate = 1,
+	SemiPrivate = 2,
+	Private = 3
+}
---
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/PrivacyRing/PrivacyRingViewModel.cs
@@ -0,0 +1,94 @@
+using DynamicData;
+using DynamicData.Binding;
+using ReactiveUI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using WalletWasabi.Fluent.Extensions;
+using WalletWasabi.Fluent.Helpers;
+using WalletWasabi.Fluent.Models;
+using WalletWasabi.Fluent.ViewModels.Navigation;
+using WalletWasabi.Wallets;
+
+namespace WalletWasabi.Fluent.ViewModels.Wallets.Home.Tiles;
+
+[NavigationMetaData(
+	Title = "Wallet Coins",
+	NavigationTarget = NavigationTarget.DialogScreen)]
+public partial class PrivacyRingViewModel : RoutableViewModel
+{
+	private IObservable<Unit> _coinsUpdated;
+	private readonly SourceList<PrivacyRingItemViewModel> _itemsSourceList = new();
+
+	public PrivacyRingViewModel(WalletViewModel walletViewModel, IObservable<Unit> balanceChanged)
+	{
+		Wallet = walletViewModel.Wallet;
+
+		OuterRadius = 250d;
+		InnerRadius = 240d;
+
+		NextCommand = CancelCommand;
+
+		_itemsSourceList
+			.Connect()
+			.ObserveOn(RxApp.MainThreadScheduler)
+			.Bind(Items)
+			.DisposeMany()
+			.Subscribe();
+
+		_coinsUpdated =
+			balanceChanged.ToSignal()
+						  .Merge(walletViewModel
+						  .WhenAnyValue(w => w.IsCoinJoining)
+						  .ToSignal());
+
+		_coinsUpdated
+			.Select(_ => walletViewModel.Wallet.GetPockets())
+			.Subscribe(RefreshCoinsList);
+	}
+
+	public ObservableCollectionExtended<PrivacyRingItemViewModel> Items { get; } = new();
+
+	public double OuterRadius { get; }
+	public double InnerRadius { get; }
+
+	public Wallet Wallet { get; }
+
+	private void RefreshCoinsList(IEnumerable<Pocket> pockets)
+	{
+		_itemsSourceList.Edit(list =>
+		{
+			list.Clear();
+
+			var total = pockets.Sum(x => Math.Abs(x.Amount.ToDecimal(NBitcoin.MoneyUnit.BTC)));
+			var start = 0.0m;
+
+			foreach (var pocket in pockets.OrderByDescending(x => x.Coins.First().HdPubKey.AnonymitySet))
+			{
+				var pocketCoins = pocket.Coins.OrderByDescending(x => x.Amount).ToList();
+
+				foreach (var coin in pocketCoins)
+				{
+					var end = start + (Math.Abs(coin.Amount.ToDecimal(NBitcoin.MoneyUnit.BTC)) / total);
+
+					var item = new PrivacyRingItemViewModel(this, coin, (double)start, (double)end);
+
+					list.Add(item);
+
+					start = end;
+				}
+			}
+		});
+	}
+
+	public void Dispose()
+	{
+		foreach (var item in Items)
+		{
+			item.Dispose();
+		}
+
+		_itemsSourceList.Dispose();
+	}
+}
