a722aa143854da992d36aba1100b9f04fe525b3f
Kiminuo <kiminuo@protonmail.com>
2022-08-08 13:06:56 +0200

    nit

--- WalletWasabi/Tor/Socks5/TcpClientSocks5Connector.cs
+++ WalletWasabi/Tor/Socks5/TcpClientSocks5Connector.cs
@@ -11,7 +11,7 @@ public static class TcpClientSocks5Connector
 	/// <summary>
 	/// Establishes TCP connection with Tor SOCKS5 endpoint.
 	/// </summary>
-	/// <exception cref="ArgumentException">This should never happen.</exception>
+	/// <exception cref="NotSupportedException">This should never happen.</exception>
 	/// <exception cref="TorException">When connection to Tor SOCKS5 endpoint fails.</exception>
 	public static async Task<TcpClient> ConnectAsync(EndPoint endPoint, CancellationToken cancellationToken)
 	{
