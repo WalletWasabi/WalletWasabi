43868236b7d0fe0d58f51515a662cc96878f2850
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-09 14:40:26 +0200

    Inherit directly from LabelsItemsPresenter

--- WalletWasabi.Fluent/Controls/LabelsListBox.axaml.cs
+++ WalletWasabi.Fluent/Controls/LabelsListBox.axaml.cs
@@ -1,25 +1,8 @@
-﻿using System.Collections;
-using Avalonia;
-using Avalonia.Collections;
-using Avalonia.Controls.Primitives;
-using Avalonia.Metadata;
-using Avalonia.Styling;
+﻿using Avalonia.Styling;
 
 namespace WalletWasabi.Fluent.Controls;
 
-public class LabelsListBox : TemplatedControl, IStyleable
+public class LabelsListBox : LabelsItemsPresenter, IStyleable
 {
-	public static readonly DirectProperty<LabelsListBox, IEnumerable> ItemsProperty =
-		AvaloniaProperty.RegisterDirect<LabelsListBox, IEnumerable>(nameof(Items), o => o.Items, (o, v) => o.Items = v);
-
-	private IEnumerable _items = new AvaloniaList<object>();
-
 	Type IStyleable.StyleKey => typeof(LabelsListBox);
-
-	[Content]
-	public IEnumerable Items
-	{
-		get { return _items; }
-		set { SetAndRaise(ItemsProperty, ref _items, value); }
-	}
 }
