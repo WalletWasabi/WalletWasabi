700c12a0f5d86221d11047c27ecbcd0c795d2c9c
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-08-10 02:37:30 +0800

    remove foreach

--- WalletWasabi/Extensions/LinqExtensions.cs
+++ WalletWasabi/Extensions/LinqExtensions.cs
@@ -208,13 +208,4 @@ public static class LinqExtensions
 			}
 		}
 	}
-	
-	public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
-	{
-		foreach (var item in source)
-		{
-			action(item);
-			yield return item;
-		}
-	}
 }
