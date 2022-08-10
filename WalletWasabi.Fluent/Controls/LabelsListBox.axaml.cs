f36bd287996186be50458c6a952e0b286277921d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-10 09:11:40 +0200

    Rename LabelsListBox to LabelsItemsPresenter

--- WalletWasabi.Fluent/Controls/LabelsListBox.axaml.cs
+++
@@ -1,8 +0,0 @@
-﻿using Avalonia.Styling;
-
-namespace WalletWasabi.Fluent.Controls;
-
-public class LabelsListBox : LabelsItemsPresenter, IStyleable
-{
-	Type IStyleable.StyleKey => typeof(LabelsListBox);
-}
