49633e92750fda609ff397afceecd64bdabb7d33
José Manuel Nieto <superjmn@outlook.com>
2022-08-10 23:38:47 +0200

    Add keywords

--- WalletWasabi.Fluent/ViewModels/SearchBar/Sources/TransactionItemsSource.cs
+++ WalletWasabi.Fluent/ViewModels/SearchBar/Sources/TransactionItemsSource.cs
@@ -16,6 +16,7 @@ public class TransactionsSource : ISearchItemSource
 	private static ISearchItem ToSearchItem(TransactionEntry r)
 	{
 		var transactionId = new string(r.HistoryItem.Id.ToString().Trim('0').Take(10).ToArray());
-		return new ActionableItem(transactionId, $"Found in {r.Wallet.WalletName}", async () => r.Parent.SelectTransaction(r.HistoryItem.Id), "Transactions");
+		var keywords = new []{ r.HistoryItem.Id.ToString() };
+		return new ActionableItem(transactionId, $"Found in {r.Wallet.WalletName}", async () => r.Wallet.NavigateAndHighlight(r.HistoryItem.Id), "Transactions", keywords);
 	}
 }
