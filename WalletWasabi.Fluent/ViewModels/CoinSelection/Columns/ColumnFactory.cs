9bb667b22d75d2ed6cdfde1e25730d1a14932bf8
José Manuel Nieto <superjmn@outlook.com>
2022-08-31 13:35:49 +0200

    Add sorting for amount

--- WalletWasabi.Fluent/ViewModels/CoinSelection/Columns/ColumnFactory.cs
+++ WalletWasabi.Fluent/ViewModels/CoinSelection/Columns/ColumnFactory.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using Avalonia.Controls;
 using Avalonia.Controls.Models.TreeDataGrid;
 using WalletWasabi.Blockchain.Analysis.Clustering;
 using WalletWasabi.Fluent.Helpers;
@@ -24,7 +26,12 @@ public static class ColumnFactory
 						WalletCoinViewModel coin => new BehaviorSubject<string>(coin.Amount.ToFormattedString()),
 						_ => Observable.Return("")
 					};
-				}));
+				}), GridLength.Auto,
+			new ColumnOptions<TreeNode>
+			{
+				CompareAscending = SortAscending<WalletCoinViewModel>(model => model.Amount),
+				CompareDescending= SortDescending<WalletCoinViewModel>(model => model.Amount),
+			});
 	}
 
 	public static TemplateColumn<TreeNode> AnonymityScore()
@@ -121,5 +128,37 @@ public static class ColumnFactory
 			new TextColumn<TreeNode, string>("", group => ""),
 			group => group.Children,
 			node => node.Children.Any());
+	}
+
+	private static Comparison<TreeNode?> SortAscending<T>(Func<T, object> selector)
+	{
+		var comparison = new Comparison<TreeNode?>(
+			(node, treeNode) =>
+			{
+				if (node?.Value is T x && treeNode?.Value is T y)
+				{
+					return Comparer<object>.Default.Compare(selector(x), selector(y));
+				}
+
+				return 0;
+			});
+
+		return comparison;
+	}
+
+	private static Comparison<TreeNode?> SortDescending<T>(Func<T, object> selector)
+	{
+		var comparison = new Comparison<TreeNode?>(
+			(node, treeNode) =>
+			{
+				if (node?.Value is T x && treeNode?.Value is T y)
+				{
+					return Comparer<object>.Default.Compare(selector(y), selector(x));
+				}
+
+				return 0;
+			});
+
+		return comparison;
 	}
 }
