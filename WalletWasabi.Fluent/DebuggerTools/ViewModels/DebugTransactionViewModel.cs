33b148a3c4ee3cbcebfcd3bdb36c9a3387cfbdf1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-02 22:53:45 +0200

    Update DebugTransactionViewModel.cs

--- WalletWasabi.Fluent/DebuggerTools/ViewModels/DebugTransactionViewModel.cs
+++ WalletWasabi.Fluent/DebuggerTools/ViewModels/DebugTransactionViewModel.cs
@@ -1,8 +1,13 @@
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Reactive;
+using Avalonia.Controls;
+using Avalonia.Controls.Models.TreeDataGrid;
 using NBitcoin;
+using ReactiveUI;
 using WalletWasabi.Blockchain.Transactions;
 using WalletWasabi.Fluent.ViewModels;
+using WalletWasabi.Models;
 
 namespace WalletWasabi.Fluent.DebuggerTools.ViewModels;
 
@@ -10,6 +15,7 @@ public partial class DebugTransactionViewModel : ViewModelBase
 {
 	private readonly SmartTransaction _transaction;
 	private readonly IObservable<Unit> _updateTrigger;
+	[AutoNotify] private DebugCoinViewModel? _selectedCoin;
 
 	public DebugTransactionViewModel(SmartTransaction transaction, IObservable<Unit> updateTrigger)
 	{
@@ -17,6 +23,8 @@ public partial class DebugTransactionViewModel : ViewModelBase
 		_updateTrigger = updateTrigger;
 
 		Update();
+
+		CreateCoinsSource();
 	}
 
 	private void Update()
@@ -25,12 +33,70 @@ public partial class DebugTransactionViewModel : ViewModelBase
 
 		TransactionId = _transaction.GetHash();
 
-		Coins = new List<DebugCoinViewModel>();
+		Coins = new ObservableCollection<DebugCoinViewModel>();
 	}
+
+	public SmartTransaction Transaction => _transaction;
 
 	public DateTimeOffset FirstSeen { get; private set; }
 
 	public uint256 TransactionId { get; private set; }
 
-	public List<DebugCoinViewModel> Coins { get; private set; }
+	public ObservableCollection<DebugCoinViewModel> Coins { get; private set; }
+
+	public FlatTreeDataGridSource<DebugCoinViewModel> CoinsSource { get; private set; }
+
+	private void CreateCoinsSource()
+	{
+		CoinsSource = new FlatTreeDataGridSource<DebugCoinViewModel>(Coins)
+		{
+			Columns =
+			{
+				new TextColumn<DebugCoinViewModel, DateTimeOffset>(
+					"FirstSeen",
+					x => x.FirstSeen,
+					new GridLength(0, GridUnitType.Auto)),
+				new TextColumn<DebugCoinViewModel, Money>(
+					"Amount",
+					x => x.Amount,
+					new GridLength(0, GridUnitType.Auto)),
+				new TextColumn<DebugCoinViewModel, bool>(
+					"Confirmed",
+					x => x.Confirmed,
+					new GridLength(0, GridUnitType.Auto)),
+				new TextColumn<DebugCoinViewModel, bool>(
+					"CoinJoinInProgress",
+					x => x.CoinJoinInProgress,
+					new GridLength(0, GridUnitType.Auto)),
+				new TextColumn<DebugCoinViewModel, bool>(
+					"IsBanned",
+					x => x.IsBanned,
+					new GridLength(0, GridUnitType.Auto)),
+				new TextColumn<DebugCoinViewModel, DateTimeOffset?>(
+					"BannedUntilUtc",
+					x => x.BannedUntilUtc,
+					new GridLength(0, GridUnitType.Auto)),
+				new TextColumn<DebugCoinViewModel, Height?>(
+					"Height",
+					x => x.Height,
+					new GridLength(0, GridUnitType.Auto)),
+				new TextColumn<DebugCoinViewModel, uint256>(
+					"Transaction",
+					x => x.TransactionId,
+					new GridLength(0, GridUnitType.Auto)),
+				new TextColumn<DebugCoinViewModel, uint256?>(
+					"SpenderTransaction",
+					x => x.SpenderTransactionId,
+					new GridLength(0, GridUnitType.Auto)),
+			}
+		};
+
+		CoinsSource.RowSelection!.SingleSelect = true;
+
+		CoinsSource.RowSelection
+			.WhenAnyValue(x => x.SelectedItem)
+			.Subscribe(x => SelectedCoin = x);
+
+		(CoinsSource as ITreeDataGridSource).SortBy(CoinsSource.Columns[0], ListSortDirection.Descending);
+	}
 }
