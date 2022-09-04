c6228758e2a45c38432952b640b4e2cef450aea4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-04 11:59:57 +0200

    Delete DebugProcessedResultLogItemViewModel.cs

--- WalletWasabi.Fluent/DebuggerTools/ViewModels/Logging/DebugProcessedResultLogItemViewModel.cs
+++
@@ -1,7 +0,0 @@
-using WalletWasabi.Fluent.ViewModels;
-
-namespace WalletWasabi.Fluent.DebuggerTools.ViewModels.Logging;
-
-public abstract partial class DebugProcessedResultLogItemViewModel : ViewModelBase
-{
-}
