01e71ea910f36f6afd1d114f2e663ab6dc725a7a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-12 14:48:55 +0200

    Update DebugTransactionProcessedLogItemViewModel.cs

--- WalletWasabi.Fluent/DebuggerTools/ViewModels/Logging/DebugTransactionProcessedLogItemViewModel.cs
+++ WalletWasabi.Fluent/DebuggerTools/ViewModels/Logging/DebugTransactionProcessedLogItemViewModel.cs
@@ -1,3 +1,5 @@
+using System.Reactive;
+using System.Reactive.Linq;
 using WalletWasabi.Blockchain.TransactionProcessing;
 
 namespace WalletWasabi.Fluent.DebuggerTools.ViewModels.Logging;
@@ -10,6 +12,28 @@ public partial class DebugTransactionProcessedLogItemViewModel : DebugLogItemVie
 	{
 		_processedResult = processedResult;
 
-		// TODO: Add ProcessedResult properties.
+		Transaction = new DebugTransactionViewModel(_processedResult.Transaction, Observable.Empty<Unit>());
+
+		IsNews = _processedResult.IsNews;
+
+		IsOwnCoinJoin = _processedResult.IsOwnCoinJoin;
+
+		// TODO: Add ProcessedResult properties:
+		// ReceivedDusts
+		// ReceivedCoins
+		// NewlyReceivedCoins
+		// NewlyConfirmedReceivedCoins
+		// SpentCoins
+		// NewlySpentCoins
+		// NewlyConfirmedSpentCoins
+		// SuccessfullyDoubleSpentCoins
+		// ReplacedCoins
+		// RestoredCoins
 	}
+
+	public DebugTransactionViewModel Transaction { get; private set; }
+
+	public bool IsNews { get; private set; }
+
+	public bool IsOwnCoinJoin { get; private set; }
 }
