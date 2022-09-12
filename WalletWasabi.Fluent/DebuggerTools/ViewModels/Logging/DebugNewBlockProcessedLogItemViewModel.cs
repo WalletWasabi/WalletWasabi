0b5b891c129a8c51dd7c9b034193c961a3463756
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-12 13:16:08 +0200

    Update DebugNewBlockProcessedLogItemViewModel.cs

--- WalletWasabi.Fluent/DebuggerTools/ViewModels/Logging/DebugNewBlockProcessedLogItemViewModel.cs
+++ WalletWasabi.Fluent/DebuggerTools/ViewModels/Logging/DebugNewBlockProcessedLogItemViewModel.cs
@@ -10,6 +10,20 @@ public partial class DebugNewBlockProcessedLogItemViewModel : DebugLogItemViewMo
 	{
 		_block = block;
 
-		// TODO: Add Block properties.
+		BlockTime = block.Header.BlockTime;
+
+		CoinbaseHeight = block.GetCoinbaseHeight();
+
+		BlockId = block.GetHash();
+
+		// TODO: Add Block properties e.g. Transactions.
 	}
+
+	public Block Block => _block;
+
+	public DateTimeOffset BlockTime { get; private set; }
+
+	public int? CoinbaseHeight { get; private set; }
+
+	public uint256 BlockId { get; private set; }
 }
