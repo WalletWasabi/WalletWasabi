ea013a13cfa8023630aa7c3e398a3e0eeef15b9b
José Manuel Nieto <superjmn@outlook.com>
2022-09-14 14:33:48 +0200

    Fix CF

--- WalletWasabi.Fluent/ViewModels/Wallets/Home/History/TransactionItemNode.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/History/TransactionItemNode.cs
@@ -6,6 +6,18 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Home.History;
 
 internal class TransactionItemNode
 {
+	private TransactionItemNode(int index, HistoryItemViewModelBase item, TransactionItemNode? parent)
+	{
+		Index = index;
+		Item = item;
+		Parent = parent;
+	}
+
+	public int Index { get; }
+	public TransactionItemNode? Parent { get; }
+	public HistoryItemViewModelBase Item { get; }
+	public IEnumerable<TransactionItemNode> Children { get; private set; } = null!;
+
 	public static IEnumerable<TransactionItemNode> Create(IEnumerable<HistoryItemViewModelBase> transactions, TransactionItemNode? parent = null)
 	{
 		return transactions.Select(
@@ -16,17 +28,5 @@ internal class TransactionItemNode
 				parentNode.Children = childNodes;
 				return parentNode;
 			});
-	}
-
-	public int Index { get; }
-	public TransactionItemNode? Parent { get; }
-	public HistoryItemViewModelBase Item { get; }
-	public IEnumerable<TransactionItemNode> Children { get; private set; } = null!;
-
-	private TransactionItemNode(int index, HistoryItemViewModelBase item, TransactionItemNode? parent)
-	{
-		Index = index;
-		Item = item;
-		Parent = parent;
 	}
 }
