4cfaab4af8cf7b40d6bea382a5fc8400c7ef8223
José Manuel Nieto <superjmn@outlook.com>
2022-09-14 17:29:07 +0200

    More progress

---
+++ WalletWasabi.Fluent/Views/Wallets/Home/History/Details/HeaderView.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Views.Wallets.Home.History.Details;
+public class HeaderView : UserControl
+{
+	public HeaderView()
+	{
+		InitializeComponent();
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+}
---
+++ WalletWasabi.Fluent/Views/Wallets/Home/History/Details/InputAndOutputs.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Views.Wallets.Home.History.Details;
+public class InputAndOutputs : UserControl
+{
+	public InputAndOutputs()
+	{
+		InitializeComponent();
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+}
