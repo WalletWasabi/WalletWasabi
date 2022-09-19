8a20d4216a15553af6147118b6f904663b532b44
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-19 13:45:06 +0200

    Check if constraint is not empty

--- WalletWasabi.Fluent/Controls/LineChart.cs
+++ WalletWasabi.Fluent/Controls/LineChart.cs
@@ -65,12 +65,15 @@ public partial class LineChart : Control
 	private static FormattedText CreateFormattedText(string text, Typeface typeface, TextAlignment alignment,
 		double fontSize, Size constraint)
 	{
-		return new FormattedText(text, CultureInfo.CurrentCulture, FlowDirection.LeftToRight, typeface, fontSize,
-			null)
+		var ft = new FormattedText(text, CultureInfo.CurrentCulture, FlowDirection.LeftToRight, typeface, fontSize, null);
+
+		if (constraint != Size.Empty)
 		{
-			MaxTextHeight = constraint.Height,
-			MaxTextWidth = constraint.Width
-		};
+			ft.MaxTextHeight = constraint.Height;
+			ft.MaxTextWidth = constraint.Width;
+		}
+
+		return ft;
 	}
 
 	private void UpdateXAxisCursorPosition(double x)
