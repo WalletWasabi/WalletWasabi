6c15a801dfd2e97aa8f79186ce346fbfcad83cef
Roland Soós <soos.roland93@gmail.com>
2022-09-20 15:11:44 +0200

    add comments

--- WalletWasabi.Fluent/Models/UiTriggers.cs
+++ WalletWasabi.Fluent/Models/UiTriggers.cs
@@ -19,14 +19,29 @@ public class UiTriggers
 		_wallet = _walletViewModel.Wallet;
 	}
 
+	/// <summary>
+	/// Triggers on subscription and when a transaction to the wallet is processed.
+	/// </summary>
 	public IObservable<Unit> WalletRelevantTransactionProcessed =>
 		Observable.FromEventPattern(_wallet.TransactionProcessor, nameof(TransactionProcessor.WalletRelevantTransactionProcessed)).ToSignal().StartWith(Unit.Default);
 
+	/// <summary>
+	/// Triggers on subscription and when the USD exchange rate changed.
+	/// </summary>
 	public IObservable<Unit> UsdExchangeRateChanged => _wallet.Synchronizer.WhenAnyValue(x => x.UsdExchangeRate).ToSignal();
 
+	/// <summary>
+	/// Triggers on subscription and when the anon score target changed.
+	/// </summary>
 	public IObservable<Unit> AnonScoreTargetChanged => _walletViewModel.CoinJoinSettings.WhenAnyValue(x => x.AnonScoreTarget).ToSignal().Throttle(TimeSpan.FromMilliseconds(3100));
 
+	/// <summary>
+	/// Triggers on subscription and when a transaction is processed to the wallet or the USD exchange rate changed.
+	/// </summary>
 	public IObservable<Unit> BalanceUpdateTrigger => WalletRelevantTransactionProcessed.Merge(UsdExchangeRateChanged);
 
+	/// <summary>
+	/// Triggers on subscription and when a transaction is processed to the wallet or the anon score target changed.
+	/// </summary>
 	public IObservable<Unit> PrivacyProgressUpdateTrigger => WalletRelevantTransactionProcessed.Merge(AnonScoreTargetChanged);
 }
