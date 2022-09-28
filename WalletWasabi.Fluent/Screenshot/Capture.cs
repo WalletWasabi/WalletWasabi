9f4729f134ec8512f33ef233e04524b5a9e0f654
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-28 13:35:57 +0200

    Simplify

--- WalletWasabi.Fluent/Screenshot/Capture.cs
+++ WalletWasabi.Fluent/Screenshot/Capture.cs
@@ -4,12 +4,37 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
-using WalletWasabi.Fluent.Screenshot.Renderers;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia.Rendering;
+using Avalonia.Skia.Helpers;
+using SkiaSharp;
 
 namespace WalletWasabi.Fluent.Screenshot;
 
 public static class Capture
 {
+	private class CanvasRenderTarget : IRenderTarget
+	{
+		private readonly SKCanvas _canvas;
+		private readonly double _dpi;
+
+		public CanvasRenderTarget(SKCanvas canvas, double dpi)
+		{
+			_canvas = canvas;
+			_dpi = dpi;
+		}
+
+		public IDrawingContextImpl CreateDrawingContext(IVisualBrushRenderer? visualBrushRenderer)
+		{
+			return DrawingContextHelper.WrapSkiaCanvas(_canvas, new Vector(_dpi, _dpi), visualBrushRenderer);
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+
 	public static void AttachCapture(this TopLevel root)
 	{
 		AttachCapture(root, new(Key.F6));
@@ -57,9 +82,15 @@ public static class Capture
 		}
 	}
 
-	private static void Save(Control? control, Size size, string path)
+	private static void Render(Control target, SKCanvas canvas, double dpi)
 	{
-		if (control is null)
+		using var renderTarget = new CanvasRenderTarget(canvas, dpi);
+		ImmediateRenderer.Render(target, renderTarget);
+	}
+
+	private static void Save(Control? target, Size size, string path)
+	{
+		if (target is null)
 		{
 			return;
 		}
@@ -70,25 +101,48 @@ public static class Capture
 			case ".png":
 			{
 				using var stream = File.Create(path);
-				PngRenderer.Render(control, size, stream);
+				var pixelSize = new PixelSize((int)size.Width, (int)size.Height);
+				var dpiVector = new Vector(96d, 96d);
+				using var bitmap = new RenderTargetBitmap(pixelSize, dpiVector);
+				target.Measure(size);
+				target.Arrange(new Rect(size));
+				bitmap.Render(target);
+				bitmap.Save(stream);
 				break;
 			}
 			case ".svg":
 			{
 				using var stream = File.Create(path);
-				SvgRenderer.Render(control, size, stream);
+				using var managedWStream = new SKManagedWStream(stream);
+				var bounds = SKRect.Create(new SKSize((float)size.Width, (float)size.Height));
+				using var canvas = SKSvgCanvas.Create(bounds, managedWStream);
+				target.Measure(size);
+				target.Arrange(new Rect(size));
+				Render(target, canvas, 96d);
 				break;
 			}
 			case ".pdf":
 			{
 				using var stream = File.Create(path);
-				PdfRenderer.Render(control, size, stream);
+				using var managedWStream = new SKManagedWStream(stream);
+				using var document = SKDocument.CreatePdf(stream, 72f);
+				using var canvas = document.BeginPage((float)size.Width, (float)size.Height);
+				target.Measure(size);
+				target.Arrange(new Rect(size));
+				Render(target, canvas, 72f);
 				break;
 			}
 			case ".skp":
 			{
 				using var stream = File.Create(path);
-				SkpRenderer.Render(control, size, stream);
+				var bounds = SKRect.Create(new SKSize((float)size.Width, (float)size.Height));
+				using var pictureRecorder = new SKPictureRecorder();
+				using var canvas = pictureRecorder.BeginRecording(bounds);
+				target.Measure(size);
+				target.Arrange(new Rect(size));
+				Render(target, canvas, 96d);
+				using var picture = pictureRecorder.EndRecording();
+				picture.Serialize(stream);
 				break;
 			}
 		}
