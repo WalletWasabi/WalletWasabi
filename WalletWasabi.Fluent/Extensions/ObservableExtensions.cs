10a8d44b8e3e8ed446caf6ab72dc793be700fe2b
José Manuel Nieto <superjmn@outlook.com>
2022-09-29 10:12:01 +0200

    Format

--- WalletWasabi.Fluent/Extensions/ObservableExtensions.cs
+++ WalletWasabi.Fluent/Extensions/ObservableExtensions.cs
@@ -6,28 +6,18 @@ namespace WalletWasabi.Fluent.Extensions;
 
 public static class ObservableExtensions
 {
-	public static IDisposable SubscribeAsync<T>(this IObservable<T> source, Func<T, Task> onNextAsync)
-	{
-		return source
+	public static IDisposable SubscribeAsync<T>(this IObservable<T> source, Func<T, Task> onNextAsync) =>
+		source
 			.Select(x => Observable.FromAsync(() => onNextAsync(x)))
 			.Concat()
 			.Subscribe();
-	}
 
-	public static IObservable<Unit> DoAsync<T>(this IObservable<T> source, Func<T, Task> onNextAsync)
-	{
-		return source
+	public static IObservable<Unit> DoAsync<T>(this IObservable<T> source, Func<T, Task> onNextAsync) =>
+		source
 			.Select(x => Observable.FromAsync(() => onNextAsync(x)))
 			.Concat();
-	}
 
-	public static IObservable<Unit> ToSignal<T>(this IObservable<T> source)
-	{
-		return source.Select(_ => Unit.Default);
-	}
+	public static IObservable<Unit> ToSignal<T>(this IObservable<T> source) => source.Select(_ => Unit.Default);
 
-	public static IObservable<T> ReplayLastActive<T>(this IObservable<T> observable)
-	{
-		return observable.Replay(1).RefCount();
-	}
+	public static IObservable<T> ReplayLastActive<T>(this IObservable<T> observable) => observable.Replay(1).RefCount();
 }
