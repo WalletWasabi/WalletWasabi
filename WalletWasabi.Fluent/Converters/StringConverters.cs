706f61a505ad4f0aca69afaa593a55c6c761fe80
José Manuel Nieto <superjmn@outlook.com>
2022-09-29 16:47:35 +0200

    Remove StringConverters

--- WalletWasabi.Fluent/Converters/StringConverters.cs
+++
@@ -1,9 +0,0 @@
-using Avalonia.Data.Converters;
-using WalletWasabi.Fluent.Helpers;
-
-namespace WalletWasabi.Fluent.Converters;
-
-public static class StringConverters
-{
-	public static readonly IValueConverter AddSIfPluralConverter = new FuncValueConverter<int, string>(TextHelpers.AddSIfPlural);
-}
