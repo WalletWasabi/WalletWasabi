50bcaef6a5306923549ecd25f6e20acff76ca7c0
José Manuel Nieto <superjmn@outlook.com>
2022-09-29 17:08:18 +0200

    Remove attributes related to ReSharper

--- WalletWasabi.Fluent/ViewModels/CoinSelection/Core/CommandViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/CoinSelection/Core/CommandViewModel.cs
@@ -1,11 +1,8 @@
-using System.Diagnostics.CodeAnalysis;
 using System.Reactive;
 using ReactiveUI;
 
 namespace WalletWasabi.Fluent.ViewModels.CoinSelection.Core;
 
-[SuppressMessage("ReSharper", "MemberCanBePrivate.Global", Justification = "Used in XAML via Style")]
-[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global", Justification = "Used in XAML via Style")]
 public class CommandViewModel
 {
 	public CommandViewModel(string header, ReactiveCommand<Unit, Unit> command)
