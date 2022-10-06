cccc3bfe735f811043bb16d674914d12c14c593d
Kimi <58662979+kiminuo@users.noreply.github.com>
2022-10-06 03:26:47 +0200

    Revert "Disable Tor's `UseEntryGuards` feature" (#9256)
    This reverts commit a689bee9d8ec34aa15ca7d1095e82c8a0629b824.

--- WalletWasabi/Tor/TorSettings.cs
+++ WalletWasabi/Tor/TorSettings.cs
@@ -98,7 +98,6 @@ public class TorSettings
 			$"--LogTimeGranularity 1",
 			$"--SOCKSPort \"{SocksEndpoint} ExtendedErrors KeepAliveIsolateSOCKSAuth\"",
 			$"--SocksTimeout 30", // Default is 2 minutes.
-			$"--UseEntryGuards 0", // Default is 1 (yes).
 			$"--CookieAuthentication 1",
 			$"--ControlPort {port}",
 			$"--CookieAuthFile \"{CookieAuthFilePath}\"",
