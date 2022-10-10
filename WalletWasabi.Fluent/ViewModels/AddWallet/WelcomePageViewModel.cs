0ba2ba989e921999dd4912fd917e42757a2bfd4f
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-10-10 14:47:35 +0800

    Simplify Welcome Page

--- WalletWasabi.Fluent/ViewModels/AddWallet/WelcomePageViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/AddWallet/WelcomePageViewModel.cs
@@ -10,9 +10,6 @@ public partial class WelcomePageViewModel : DialogViewModelBase<Unit>
 {
 	private const int NumberOfPages = 5;
 	private readonly AddWalletPageViewModel _addWalletPage;
-	[AutoNotify] private int _selectedIndex;
-	[AutoNotify] private string? _nextLabel;
-	[AutoNotify] private bool _enableNextKey = true;
 
 	public WelcomePageViewModel(AddWalletPageViewModel addWalletPage)
 	{
@@ -20,34 +17,12 @@ public partial class WelcomePageViewModel : DialogViewModelBase<Unit>
 
 		SetupCancel(enableCancel: false, enableCancelOnEscape: false, enableCancelOnPressed: false);
 
-		SelectedIndex = 0;
 		NextCommand = ReactiveCommand.Create(OnNext);
-		CanGoBack = this.WhenAnyValue(x => x.SelectedIndex, i => i > 0);
-		BackCommand = ReactiveCommand.Create(() => SelectedIndex--, CanGoBack);
-
-		this.WhenAnyValue(x => x.SelectedIndex)
-			.Subscribe(
-				x =>
-				{
-					NextLabel = x < NumberOfPages - 1 ? "Continue" : "Get Started";
-					EnableNextKey = x < NumberOfPages - 1;
-				});
-
-		this.WhenAnyValue(x => x.IsActive)
-			.Skip(1)
-			.Where(x => !x)
-			.Subscribe(x => EnableNextKey = false);
 	}
-
-	public IObservable<bool> CanGoBack { get; }
 
 	private void OnNext()
 	{
-		if (SelectedIndex < NumberOfPages - 1)
-		{
-			SelectedIndex++;
-		}
-		else if (!Services.WalletManager.HasWallet())
+		if (!Services.WalletManager.HasWallet())
 		{
 			Navigate().To(_addWalletPage);
 		}
