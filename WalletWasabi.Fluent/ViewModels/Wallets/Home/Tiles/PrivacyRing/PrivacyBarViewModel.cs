a455760f385b3f4c48a93a8e430086a1c7a3a724
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-10-11 16:30:56 +0800

    fix review comment by @soosr

--- WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/PrivacyRing/PrivacyBarViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/PrivacyRing/PrivacyBarViewModel.cs
@@ -66,7 +66,7 @@ public partial class PrivacyBarViewModel : ViewModelBase
 
 		var coinCount = pockets.SelectMany(x => x.Coins).Count();
 
-		if (coinCount == 0d)
+		if (Width == 0d || coinCount == 0d)
 		{
 			return;
 		}
