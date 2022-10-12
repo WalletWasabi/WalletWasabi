0ffac4873d6f5aca843d5cf26be4f6018e265cef
José Manuel Nieto <superjmn@outlook.com>
2022-10-12 18:16:11 +0200

    Add number format

--- WalletWasabi.Fluent/Extensions/MoneyExtensions.cs
+++ WalletWasabi.Fluent/Extensions/MoneyExtensions.cs
@@ -10,7 +10,15 @@ public static class MoneyExtensions
 	public static string ToUsdAproxBetweenParens(this decimal n) => $"(≈{ToUsd(n)})";
 	public static string ToUsd(this decimal n)
 	{
-		var amountPart = n < 10 ? n.ToString("N2") : n.RoundToSignificantFigures(5).ToString("N0");
+		var amountPart = n < 10 ? n.ToString("N2", FormatInfo) : n.RoundToSignificantFigures(5).ToString("N0", FormatInfo);
 		return amountPart + " USD";
 	}
+
+	private static readonly NumberFormatInfo FormatInfo = new()
+	{
+		CurrencyGroupSeparator = " ",
+		NumberGroupSeparator = " ",
+		CurrencyDecimalSeparator = ".",
+		NumberDecimalSeparator = "."
+	};
 }
