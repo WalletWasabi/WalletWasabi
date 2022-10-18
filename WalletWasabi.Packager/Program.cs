e1a4d8964dbd69646c7b37e2f4bd45e4beacdb8c
Kiminuo <kiminuo@protonmail.com>
2022-10-18 09:48:25 +0200

    `policykit-1` is the right package

--- WalletWasabi.Packager/Program.cs
+++ WalletWasabi.Packager/Program.cs
@@ -429,7 +429,7 @@ public static class Program
 					$"Architecture: amd64\n" +
 					$"License: Open Source (MIT)\n" +
 					$"Installed-Size: {installedSizeKb}\n" +
-					$"Recommends: policykit-desktop-privileges\n" +
+					$"Recommends: policykit-1\n" +
 					$"Description: open-source, non-custodial, privacy focused Bitcoin wallet\n" +
 					$"  Built-in Tor, coinjoin, payjoin and coin control features.\n";
 
