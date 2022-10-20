5af7f9ada0908d02ee50b916a61d4adbeb9ff519
Kiminuo <kiminuo@protonmail.com>
2022-10-20 09:46:02 +0200

    CF

--- WalletWasabi.Tests/UnitTests/Extensions/MemoryExtensionsTests.cs
+++ WalletWasabi.Tests/UnitTests/Extensions/MemoryExtensionsTests.cs
@@ -28,7 +28,6 @@ public class MemoryExtensionsTests
 		StateObject stateObject = new();
 
 		RunTest(stateObject, taskProvider: (int i) => memoryCache.AtomicGetOrCreateAsync(key: i % 2, cacheOptions, factory: () => stateObject.CounterTaskAsync(i % 2, CancellationToken.None)));
-
 	}
 
 	[Fact]
