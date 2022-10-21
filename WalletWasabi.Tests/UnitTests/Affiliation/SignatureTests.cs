a7cf81c2b6a823053c6f3607a2cfb9e94c761bcd
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2022-10-21 18:34:56 +0200

    Implement affiliation signer

---
+++ WalletWasabi.Tests/UnitTests/Affiliation/SignatureTests.cs
@@ -0,0 +1,26 @@
+using System.Security.Cryptography;
+using NBitcoin.DataEncoders;
+using WalletWasabi.Affiliation;
+using WalletWasabi.Helpers;
+using Xunit;
+
+namespace WalletWasabi.Tests.UnitTests.Affiliation;
+
+public class SignatureTests
+{
+	public (string, string) GenerateKey()
+	{
+		var ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+		var privateKeyBytes = ecdsa.ExportECPrivateKey();
+		var publicKeyBytes = ecdsa.ExportSubjectPublicKeyInfo();
+		var privateKeyHex = Encoders.Hex.EncodeData(privateKeyBytes);
+		var publicKeyHex = Encoders.Hex.EncodeData(publicKeyBytes);
+		return (privateKeyHex, publicKeyHex);
+	}
+
+	[Fact]
+	public void FallbackSignerKeyTest()
+	{
+		Signer signer = new(Constants.FallbackCoordinatorSignerKeyHex);
+	}
+}
---
+++ WalletWasabi/Affiliation/Signer.cs
@@ -0,0 +1,32 @@
+using NBitcoin.DataEncoders;
+using System.Security.Cryptography;
+
+namespace WalletWasabi.Affiliation;
+
+public class Signer
+{
+	private ECDsa ECDsa;
+
+	public Signer(string signingKeyHex)
+	{
+		byte[] signingKeyBytes = Encoders.Hex.DecodeData(signingKeyHex);
+		ECDsa = ECDsa.Create();
+		ECDsa.ImportECPrivateKey(signingKeyBytes, out var readBytes);
+
+		if (signingKeyBytes.Length != readBytes)
+		{
+			throw new ArgumentException("Invalid key.", nameof(signingKeyHex));
+		}
+
+		// ECDSA_P256
+		if (ECDsa.ExportParameters(false).Curve.Oid.Value != "1.2.840.10045.3.1.7")
+		{
+			throw new NotSupportedException("Unsupported curve.");
+		}
+	}
+
+	public byte[] Sign(byte[] digest)
+	{
+		return ECDsa.SignData(digest, HashAlgorithmName.SHA256, DSASignatureFormat.IeeeP1363FixedFieldConcatenation);
+	}
+}
