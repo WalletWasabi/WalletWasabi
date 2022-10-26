bbe343932fb447903da9f765d43c688cc89018a8
ichthus1604 <ichthus1604@gmail.com>
2022-10-26 09:33:14 -0300

    Move PrivacyRing Item details view to it's own UserControl

---
+++ WalletWasabi.Fluent/Views/Wallets/Home/Tiles/PrivacyRing/PrivacyRingItemDetailsView.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Views.Wallets.Home.Tiles.PrivacyRing;
+
+public partial class PrivacyRingItemDetailsView : UserControl
+{
+	public PrivacyRingItemDetailsView()
+	{
+		InitializeComponent();
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+}
