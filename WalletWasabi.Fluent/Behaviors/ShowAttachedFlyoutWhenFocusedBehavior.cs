9d23b40ac7b4f8ed1ecdf00d6002dfc2cef9cb11
ichthus1604 <ichthus1604@gmail.com>
2022-10-26 14:03:34 -0300

    Fix SearchBar flyout issue on theme change

--- WalletWasabi.Fluent/Behaviors/ShowAttachedFlyoutWhenFocusedBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/ShowAttachedFlyoutWhenFocusedBehavior.cs
@@ -56,6 +56,7 @@ public class ShowAttachedFlyoutWhenFocusedBehavior : AttachedToVisualTreeBehavio
 		if (IsFlyoutOpen)
 		{
 			controller.SetIsForcedOpen(false);
+			IsFlyoutOpen = false;
 		}
 	}
 
