72c5ae2cfa5cfd1b5fd11eba7c420cfba7333328
José Manuel Nieto <superjmn@outlook.com>
2022-10-28 12:51:12 +0200

    Restrict paste to left button release

--- WalletWasabi.Fluent/Behaviors/AutoPasteAmountBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/AutoPasteAmountBehavior.cs
@@ -1,9 +1,11 @@
 using System.Globalization;
+using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using WalletWasabi.Fluent.Controls;
 using WalletWasabi.Fluent.Extensions;
 using WalletWasabi.Helpers;
@@ -61,8 +63,8 @@ public class AutoPasteAmountBehavior : AttachedToVisualTreeBehavior<DualCurrency
 	private static decimal CountDecimalPlaces(decimal dec)
 	{
 		var bits = decimal.GetBits(dec);
-		ulong lowInt = (uint)bits[0];
-		ulong midInt = (uint)bits[1];
+		ulong lowInt = (uint) bits[0];
+		ulong midInt = (uint) bits[1];
 		var exponent = (bits[3] & 0x00FF0000) >> 16;
 		var result = exponent;
 		var lowDecimal = lowInt | (midInt << 32);
@@ -75,16 +77,37 @@ public class AutoPasteAmountBehavior : AttachedToVisualTreeBehavior<DualCurrency
 		return result;
 	}
 
-	private IDisposable AutoPaster(TemplatedControl dualCurrencyEntryBox, string templatePartName, Func<string, bool> isValidAmount)
+	private IDisposable AutoPaster(TemplatedControl dualCurrencyEntryBox, string target, Func<string, bool> isValidAmount)
 	{
-		return dualCurrencyEntryBox
-			.OnEvent<TemplateAppliedEventArgs>(nameof(dualCurrencyEntryBox.TemplateApplied))
-			.Select(x => x.EventArgs.NameScope.Find<CurrencyEntryBox>(templatePartName))
-			.Select(entryBox => entryBox.OnEvent(InputElement.GotFocusEvent).Select(_ => entryBox))
-			.Switch()
+		return LeftButtonReleased(dualCurrencyEntryBox, target)
 			.SelectMany(textBox => ApplicationUtils.GetClipboard().Select(str => new { Text = str, TextBox = textBox }))
 			.Where(x => isValidAmount(x.Text) && x.TextBox.Text.Trim() == "")
 			.Do(x => x.TextBox.Text = x.Text)
 			.Subscribe();
+	}
+
+	private IObservable<CurrencyEntryBox> LeftButtonReleased(TemplatedControl root, string childName)
+	{
+		return TemplateApplied(root)
+			.Select(x => Find(childName, x))
+			.Select(entryBox => LeftButtonReleased(entryBox).Select(_ => entryBox))
+			.Switch();
+	}
+
+	private static IObservable<TemplateAppliedEventArgs> TemplateApplied(TemplatedControl dualCurrencyEntryBox)
+	{
+		return dualCurrencyEntryBox
+			.OnEvent<TemplateAppliedEventArgs>(nameof(dualCurrencyEntryBox.TemplateApplied))
+			.Select(x => x.EventArgs);
+	}
+
+	private static CurrencyEntryBox Find(string templatePartName, TemplateAppliedEventArgs templateAppliedEventArgs)
+	{
+		return templateAppliedEventArgs.NameScope.Find<CurrencyEntryBox>(templatePartName);
+	}
+
+	private IObservable<EventPattern<PointerReleasedEventArgs>> LeftButtonReleased(IInteractive entryBox)
+	{
+		return entryBox.OnEvent(InputElement.PointerReleasedEvent, RoutingStrategies.Tunnel).Where(x => x.EventArgs.InitialPressMouseButton == MouseButton.Left);
 	}
 }
