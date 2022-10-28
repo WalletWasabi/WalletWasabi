48dfbcc8a077e6f4b3fbf6fae933a282a97ba9a9
José Manuel Nieto <superjmn@outlook.com>
2022-10-28 12:51:12 +0200

    Restrict paste to left button release

--- WalletWasabi.Fluent/Behaviors/ApplicationUtils.cs
+++ WalletWasabi.Fluent/Behaviors/ApplicationUtils.cs
@@ -1,33 +1,11 @@
 using System.Reactive.Linq;
 using System.Reactive.Threading.Tasks;
 using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
 
 namespace WalletWasabi.Fluent.Behaviors;
 
 public static class ApplicationUtils
 {
-	public static IObservable<bool> MainWindowIsActive()
-	{
-		if (Application.Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime app)
-		{
-			return Observable.Return(true);
-		}
-
-		var isActive = Observable
-			.FromEventPattern(app.MainWindow, nameof(Window.Activated))
-			.Select(_ => true);
-
-		var isInactive = Observable
-			.FromEventPattern(app.MainWindow, nameof(Window.Deactivated))
-			.Select(_ => false);
-
-		return isActive
-			.Merge(isInactive)
-			.StartWith(app.MainWindow.IsActive);
-	}
-
 	public static IObservable<string> GetClipboard()
 	{
 		if (Application.Current is { Clipboard: { } clipboard })
