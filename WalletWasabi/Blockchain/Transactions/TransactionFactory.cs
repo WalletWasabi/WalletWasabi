76221a2fdc5b702c9fe8d0619e3dbdbd4446b1eb
Yahia Chiheb <chihebyahia@gmail.com>
2022-10-28 22:56:53 +0100

    Edit log

--- WalletWasabi/Blockchain/Transactions/TransactionFactory.cs
+++ WalletWasabi/Blockchain/Transactions/TransactionFactory.cs
@@ -270,7 +270,7 @@ public class TransactionFactory
 			}
 		}
 
-		Logger.LogDebug($"Built: {totalOutgoingAmountNoFee.ToString(fplus: false, trimExcessZero: true)} BTC. Fee: {fee.Satoshi} sats. Vsize: {vSize} vBytes. Fee/Total ratio: {feePercentage:0.#}%. Tx hash: {tx.GetHash()}.");
+		Logger.LogDebug($"Built tx: {totalOutgoingAmountNoFee.ToString(fplus: false, trimExcessZero: true)} BTC. Fee: {fee.Satoshi} sats. Vsize: {vSize} vBytes. Fee/Total ratio: {feePercentage:0.#}%. Tx hash: {tx.GetHash()}.");
 
 		var sign = !KeyManager.IsWatchOnly;
 		return new BuildTransactionResult(smartTransaction, psbt, sign, fee, feePercentage);
