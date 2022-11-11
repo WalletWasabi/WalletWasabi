767d05ae1236434e549e858ee987d19727fa863c
José Manuel Nieto <superjmn@outlook.com>
2022-11-11 16:57:53 +0100

    Fix CF

--- WalletWasabi.Fluent/Controls/TileControl.axaml.cs
+++ WalletWasabi.Fluent/Controls/TileControl.axaml.cs
@@ -7,13 +7,19 @@ public class TileControl : ContentControl
 {
 	public static readonly StyledProperty<string> TitleProperty = AvaloniaProperty.Register<TileControl, string>("Title");
 
+	public static readonly StyledProperty<object> BottomContentProperty = AvaloniaProperty.Register<TileControl, object>("BottomContent");
+
+	public static readonly StyledProperty<bool> IsBottomContentVisibleProperty = AvaloniaProperty.Register<TileControl, bool>("IsBottomContentVisible", true);
+
+	public static readonly StyledProperty<Thickness> SeparatorMarginProperty = AvaloniaProperty.Register<TileControl, Thickness>("SeparatorMargin");
+
+	public static readonly StyledProperty<double> BottomPartHeightProperty = AvaloniaProperty.Register<TileControl, double>("BottomPartHeight");
+
 	public string Title
 	{
 		get => GetValue(TitleProperty);
 		set => SetValue(TitleProperty, value);
 	}
-
-	public static readonly StyledProperty<object> BottomContentProperty = AvaloniaProperty.Register<TileControl, object>("BottomContent");
 
 	public object BottomContent
 	{
@@ -21,23 +27,17 @@ public class TileControl : ContentControl
 		set => SetValue(BottomContentProperty, value);
 	}
 
-	public static readonly StyledProperty<bool> IsBottomContentVisibleProperty = AvaloniaProperty.Register<TileControl, bool>("IsBottomContentVisible", true);
-
 	public bool IsBottomContentVisible
 	{
 		get => GetValue(IsBottomContentVisibleProperty);
 		set => SetValue(IsBottomContentVisibleProperty, value);
 	}
 
-	public static readonly StyledProperty<Thickness> SeparatorMarginProperty = AvaloniaProperty.Register<TileControl, Thickness>("SeparatorMargin");
-
 	public Thickness SeparatorMargin
 	{
 		get => GetValue(SeparatorMarginProperty);
 		set => SetValue(SeparatorMarginProperty, value);
 	}
-
-	public static readonly StyledProperty<double> BottomPartHeightProperty = AvaloniaProperty.Register<TileControl, double>("BottomPartHeight");
 
 	public double BottomPartHeight
 	{
