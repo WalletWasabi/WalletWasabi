782b7e7f8263fec9c450055f5a0c15dbf0426d56
José Manuel Nieto <superjmn@outlook.com>
2022-11-14 16:58:17 +0100

    Refineme from review

--- WalletWasabi.Fluent/Helpers/ApplicationHelper.cs
+++ WalletWasabi.Fluent/Helpers/ApplicationHelper.cs
@@ -1,3 +1,5 @@
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
@@ -7,4 +9,14 @@ namespace WalletWasabi.Fluent.Helpers;
 public static class ApplicationHelper
 {
 	public static Window? MainWindow => (Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow;
+
+	public static IObservable<string> ClipBoardTextChanged()
+	{
+		if (Application.Current is { Clipboard: { } clipboard })
+		{
+			return clipboard.GetTextAsync().ToObservable().Select(x => x ?? "");
+		}
+
+		return Observable.Return("");
+	}
 }
