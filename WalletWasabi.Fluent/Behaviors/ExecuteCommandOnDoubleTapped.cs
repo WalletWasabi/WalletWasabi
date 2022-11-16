2a380e77507a87b9f72ba4f82d07a1c6722fcf1d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-16 13:49:11 +0100

    Add custom behavior ExecuteCommandOnDoubleTapped

---
+++ WalletWasabi.Fluent/Behaviors/ExecuteCommandOnDoubleTapped.cs
@@ -0,0 +1,40 @@
+﻿using System.Reactive.Disposables;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+public class ExecuteCommandOnDoubleTapped : DisposingBehavior<Control>
+{
+	public static readonly StyledProperty<ICommand?> CommandProperty =
+		AvaloniaProperty.Register<ExecuteCommandOnDoubleTapped, ICommand?>(nameof(Command));
+
+	public ICommand? Command
+	{
+		get => GetValue(CommandProperty);
+		set => SetValue(CommandProperty, value);
+	}
+
+	protected override void OnAttached(CompositeDisposable disposables)
+	{
+		if (AssociatedObject is { })
+		{
+			Gestures.DoubleTappedEvent.AddClassHandler<InputElement>(
+					(x, _) =>
+					{
+						if (Equals(x, AssociatedObject))
+						{
+							if (Command is { } cmd && cmd.CanExecute(default))
+							{
+								cmd.Execute(default);
+							}
+						}
+					},
+					RoutingStrategies.Tunnel | RoutingStrategies.Bubble)
+				.DisposeWith(disposables);
+		}
+	}
+}
