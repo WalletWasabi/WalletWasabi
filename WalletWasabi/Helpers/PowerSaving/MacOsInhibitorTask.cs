f8197f6bac29faa7d7fe4264ef0f030e6b5e3e05
Kiminuo <kiminuo@protonmail.com>
2022-11-16 08:39:26 +0100

    Add the macOS inhibitor task

---
+++ WalletWasabi/Helpers/PowerSaving/MacOsInhibitorTask.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using WalletWasabi.Logging;
+using WalletWasabi.Microservices;
+
+namespace WalletWasabi.Helpers.PowerSaving;
+
+/// <summary>
+/// Inhibitor based on <c>caffeinate</c> command.
+/// </summary>
+public class MacOsInhibitorTask : BaseInhibitorTask
+{
+	/// <remarks>Use the constructor only in tests.</remarks>
+	internal MacOsInhibitorTask(TimeSpan period, string reason, ProcessAsync process)
+		: base(period, reason, process)
+	{
+	}
+
+	public static MacOsInhibitorTask Create(TimeSpan basePeriod, string reason)
+	{
+		// TODO: Make sure that the caffeinate is terminated once the parent process (WW) finishes.
+		string command = $"caffeinate";
+		string arguments = $"-i";
+
+		Logger.LogTrace($"Command to invoke: {command} {arguments}");
+		ProcessStartInfo startInfo = GetProcessStartInfo(command, arguments);
+
+		ProcessAsync process = new(startInfo);
+		process.Start();
+		MacOsInhibitorTask task = new(basePeriod, reason, process);
+
+		return task;
+	}
+}
