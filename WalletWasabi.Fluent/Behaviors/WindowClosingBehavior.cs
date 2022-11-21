8b34f78e8a2da16485bc2323e3bd57d880b76d4c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-21 12:54:23 +0100

    Add CancelWindowClosing property

--- WalletWasabi.Fluent/Behaviors/WindowClosingBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/WindowClosingBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using Avalonia;
 using Avalonia.Controls;
 using ReactiveUI;
 
@@ -8,6 +9,15 @@ namespace WalletWasabi.Fluent.Behaviors;
 
 public class WindowClosingBehavior : DisposingBehavior<Window>
 {
+	public static readonly StyledProperty<bool> CancelWindowClosingProperty =
+		AvaloniaProperty.Register<WindowClosingBehavior, bool>(nameof(CancelWindowClosing));
+
+	public bool CancelWindowClosing
+	{
+		get => GetValue(CancelWindowClosingProperty);
+		set => SetValue(CancelWindowClosingProperty, value);
+	}
+
 	protected override void OnAttached(CompositeDisposable disposables)
 	{
 		if (AssociatedObject is null)
