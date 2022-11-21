78803ccb8f41878132371a202105f0709c7b1ddf
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-11-21 12:48:10 +0100

    Add WindowClosingBehavior stub

---
+++ WalletWasabi.Fluent/Behaviors/WindowClosingBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia.Controls;
+using ReactiveUI;
+using WalletWasabi.Fluent.Helpers;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+public class WindowClosingBehavior : DisposingBehavior<Window>
+{
+	protected override void OnAttached(CompositeDisposable disposables)
+	{
+		if (AssociatedObject is null)
+		{
+			return;
+		}
+
+		Observable
+			.FromEventPattern(AssociatedObject, nameof(AssociatedObject.Closing))
+			.ObserveOn(RxApp.MainThreadScheduler)
+			.Subscribe(_ =>
+			{
+				// TODO: Prevent window closing when dialog is open.
+			})
+			.DisposeWith(disposables);
+	}
+}
