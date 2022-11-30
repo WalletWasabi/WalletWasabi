f3ad207c9c079cedd02082e208870ba395cb817d
José Manuel Nieto <superjmn@outlook.com>
2022-11-30 12:28:27 +0100

    Include suggested changes. Thanks @soosr!

--- WalletWasabi.Fluent/ViewModels/CoinControl/Core/PocketCoinControlItemViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/CoinControl/Core/PocketCoinControlItemViewModel.cs
@@ -7,15 +7,12 @@ public class PocketCoinControlItemViewModel : CoinControlItemViewModelBase
 {
 	public PocketCoinControlItemViewModel(Pocket pocket)
 	{
-		var coinCount = pocket.Coins.Count();
 		var unconfirmedCount = pocket.Coins.Count(x => !x.Confirmed);
-		var confirmedCount = coinCount - unconfirmedCount;
-		var allConfirmed = coinCount == confirmedCount;
-		ConfirmationStatus = allConfirmed ? "All coins are confirmed" : $"{unconfirmedCount} coins are waiting for confirmation";
+		IsConfirmed = unconfirmedCount == 0;
+		ConfirmationStatus = IsConfirmed ? "All coins are confirmed" : $"{unconfirmedCount} coins are waiting for confirmation";
 		IsBanned = pocket.Coins.Any(x => x.IsBanned);
 		BannedUntilUtcToolTip = IsBanned ? "Some coins can't participate in coinjoin" : null;
 		Amount = pocket.Amount;
-		IsConfirmed = allConfirmed;
 		IsCoinjoining = pocket.Coins.Any(x => x.CoinJoinInProgress);
 		AnonymityScore = (int) pocket.Coins.Max(x => x.HdPubKey.AnonymitySet);
 		Labels = pocket.Labels;
