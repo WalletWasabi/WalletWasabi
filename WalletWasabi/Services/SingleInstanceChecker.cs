bf27ed2a397bea501d05a015faa9435ae95be4f3
Kimi <58662979+kiminuo@users.noreply.github.com>
2022-12-11 21:00:10 +0100

    Fix namespace of `TaskExtensions` (#9749)

--- WalletWasabi/Services/SingleInstanceChecker.cs
+++ WalletWasabi/Services/SingleInstanceChecker.cs
@@ -6,6 +6,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using WalletWasabi.Extensions;
 using WalletWasabi.Logging;
 
 namespace WalletWasabi.Services;
