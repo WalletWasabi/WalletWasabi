407eaa0711578fe4c687ee0962d9a0ec4bae8383
José Manuel Nieto <superjmn@outlook.com>
2022-12-22 11:41:02 +0100

    Rename Hint => Suggestion

--- WalletWasabi.Fluent/Behaviors/Suggestion.cs
+++ WalletWasabi.Fluent/Behaviors/Suggestion.cs
@@ -8,10 +8,10 @@ public class Suggestion : ReactiveObject
 	public Suggestion(string text, Action onAccept)
 	{
 		Text = text;
-		AcceptHintCommand = ReactiveCommand.Create(onAccept);
+		AcceptCommand = ReactiveCommand.Create(onAccept);
 	}
 
 	public string Text { get; }
 
-	public ReactiveCommand<Unit, Unit> AcceptHintCommand { get; set; }
+	public ReactiveCommand<Unit, Unit> AcceptCommand { get; set; }
 }
