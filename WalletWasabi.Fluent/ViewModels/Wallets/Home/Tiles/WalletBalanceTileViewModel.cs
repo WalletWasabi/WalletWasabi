347450c47b355054edc4dd59d5941e07c1735236
José Manuel Nieto <superjmn@outlook.com>
2022-12-30 00:05:35 +0100

    Add ability to copy balance on right click over tile

--- WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/WalletBalanceTileViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Home/Tiles/WalletBalanceTileViewModel.cs
@@ -1,12 +1,21 @@
+using System.Reactive;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Input;
+using Avalonia.VisualTree;
 using NBitcoin;
+using ReactiveUI;
 using WalletWasabi.Fluent.Extensions;
 using WalletWasabi.Fluent.Helpers;
+using WalletWasabi.Wallets;
 
 namespace WalletWasabi.Fluent.ViewModels.Wallets.Home.Tiles;
 
 public class WalletBalanceTileViewModel : ActivatableViewModel
 {
+	private readonly ObservableAsPropertyHelper<bool> _isCopyActive;
+
 	public WalletBalanceTileViewModel(WalletViewModel walletVm)
 	{
 		var wallet = walletVm.Wallet;
@@ -15,14 +24,45 @@ public class WalletBalanceTileViewModel : ActivatableViewModel
 			.Select(_ => wallet.Coins.TotalAmount());
 
 		BalanceBtc = balance
-			.Select(money => $"{money.ToFormattedString()} BTC");
+			.Select(FormatMoney);
 
 		BalanceFiat = balance
 			.Select(money => money.BtcToUsd(wallet.Synchronizer.UsdExchangeRate));
 
 		HasBalance = balance
 			.Select(money => money > Money.Zero);
+
+		CopyBalanceCommand = ReactiveCommand.CreateFromObservable<PointerEventArgs, Unit>(eventArgs =>
+		{
+			if (!eventArgs.GetCurrentPoint((IVisual?) eventArgs.Source).Properties.IsRightButtonPressed)
+			{
+				return Observable.Empty(Unit.Default);
+			}
+
+			return Observable.FromAsync(() => SetTextAsync(wallet))
+				.ToSignal()
+				.Concat(Observable.Timer(TimeSpan.FromSeconds(2)).ToSignal());
+		});
+
+		_isCopyActive = CopyBalanceCommand.IsExecuting.ToProperty(this, x => x.IsCopyRunning);
 	}
+
+	private static string FormatMoney(Money money)
+	{
+		return $"{money.ToFormattedString()} BTC";
+	}
+
+	public bool IsCopyRunning => _isCopyActive.Value;
+
+	private static async Task SetTextAsync(Wallet wallet)
+	{
+		if (Application.Current is { Clipboard: { } clipboard })
+		{
+			await clipboard.SetTextAsync(FormatMoney(wallet.Coins.TotalAmount()));
+		}
+	}
+
+	public ReactiveCommand<PointerEventArgs, Unit> CopyBalanceCommand { get; set; }
 
 	public IObservable<bool> HasBalance { get; }
 
