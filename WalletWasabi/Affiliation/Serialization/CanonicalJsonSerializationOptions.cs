bc22b1c825eb3af41e5ffa199bcd96078e5b7613
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2023-01-04 17:53:17 +0100

    fixup! Implement canonical serialization

--- WalletWasabi/Affiliation/Serialization/CanonicalJsonSerializationOptions.cs
+++ WalletWasabi/Affiliation/Serialization/CanonicalJsonSerializationOptions.cs
@@ -1,17 +1,28 @@
-using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WalletWasabi.Affiliation.Serialization;
 
 public static class CanonicalJsonSerializationOptions
 {
-	public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings() { ContractResolver = new OrderedContractResolver(), Converters = JsonSerializationOptions.Converters, Formatting = Formatting.None };
+	/// <summary>
+	/// JSON settings that enforces JSON's objects' properties' to be serialized in alphabetical order.
+	/// </summary>
+	public static readonly JsonSerializerSettings Settings = new()
+	{
+		ContractResolver = new OrderedContractResolver(),
+		Converters = AffiliationJsonSerializationOptions.Converters,
 
-	// Taken from https://stackoverflow.com/a/11309106/3744182
+		// Intentionally enforced default value.
+		Formatting = Formatting.None
+	};
+
+	/// <seealso href="https://stackoverflow.com/a/11309106/3744182"/>
 	private class OrderedContractResolver : DefaultContractResolver
 	{
-		protected override System.Collections.Generic.IList<JsonProperty> CreateProperties(System.Type type, MemberSerialization memberSerialization)
+		protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
 		{
 			return base.CreateProperties(type, memberSerialization).OrderBy(p => p.PropertyName, StringComparer.Ordinal).ToList();
 		}
