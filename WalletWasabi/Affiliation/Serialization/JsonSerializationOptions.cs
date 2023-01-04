ea7e2e1f6177d2b440ffa2f6ac05a31d5cc137d1
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2023-01-04 17:32:38 +0100

    fixup! Implement json serialization
    Co-authored-by: Kimi <58662979+kiminuo@users.noreply.github.com>

--- WalletWasabi/Affiliation/Serialization/JsonSerializationOptions.cs
+++ WalletWasabi/Affiliation/Serialization/JsonSerializationOptions.cs
@@ -5,7 +5,10 @@ namespace WalletWasabi.Affiliation.Serialization;
 
 public static class JsonSerializationOptions
 {
-	public static readonly List<JsonConverter> Converters = new List<JsonConverter>() { new ByteArrayJsonConverter(), new FeeRateJsonConverter() };
+	private static readonly List<JsonConverter> Converters = new() {
+		new ByteArrayJsonConverter(),
+		new FeeRateJsonConverter()
+	};
 
-	public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings() { Converters = Converters };
+	public static readonly JsonSerializerSettings Settings = new() { Converters = Converters };
 }
