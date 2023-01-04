2d6397ca3cbfea0aa31cfd196f3bc90b18f28916
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2023-01-04 17:52:28 +0100

    fixup! Implement json serialization

--- WalletWasabi/Affiliation/Serialization/AffiliationJsonSerializationOptions.cs
+++ WalletWasabi/Affiliation/Serialization/AffiliationJsonSerializationOptions.cs
@@ -3,11 +3,11 @@ using Newtonsoft.Json;
 
 namespace WalletWasabi.Affiliation.Serialization;
 
-public static class JsonSerializationOptions
+public static class AffiliationJsonSerializationOptions
 {
-	private static readonly List<JsonConverter> Converters = new() {
-		new ByteArrayJsonConverter(),
-		new FeeRateJsonConverter()
+	public static readonly List<JsonConverter> Converters = new() {
+		new AffiliationByteArrayJsonConverter(),
+		new AffiliationFeeRateJsonConverter()
 	};
 
 	public static readonly JsonSerializerSettings Settings = new() { Converters = Converters };
