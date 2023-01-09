25dba14fb7c779b990e5cd63cb97330151adf53e
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2023-01-09 17:33:27 +0100

    fixup! Implement affiliation signer

--- WalletWasabi.Tests/UnitTests/Affiliation/AffiliationMessageSignerTests.cs
+++ WalletWasabi.Tests/UnitTests/Affiliation/AffiliationMessageSignerTests.cs
@@ -6,7 +6,7 @@ using Xunit;
 
 namespace WalletWasabi.Tests.UnitTests.Affiliation;
 
-public class SignatureTests
+public class AffiliationMessageSignerTests
 {
 	public (string, string) GenerateKey()
 	{
