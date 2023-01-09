883c38b4374689311e1f075c758129249d63a3e2
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2023-01-09 17:23:17 +0100

    fixup! Implement affiliation manager
    Co-authored-by: Kimi <58662979+kiminuo@users.noreply.github.com>

--- WalletWasabi/Affiliation/AffiliationManager.cs
+++ WalletWasabi/Affiliation/AffiliationManager.cs
@@ -7,7 +7,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.Tor.Http;
 using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
 
 namespace WalletWasabi.Affiliation;
 
