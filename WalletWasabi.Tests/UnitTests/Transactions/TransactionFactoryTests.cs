52de1c77d823fcf295119c8295ffd7e9a364807c
adamPetho <45069029+adamPetho@users.noreply.github.com>
2023-01-12 13:54:58 +0100

    add unit test

--- WalletWasabi.Tests/UnitTests/Transactions/TransactionFactoryTests.cs
+++ WalletWasabi.Tests/UnitTests/Transactions/TransactionFactoryTests.cs
@@ -55,6 +55,43 @@ public class TransactionFactoryTests
 	}
 
 	[Fact]
+	public void NoAvailableCoinsTest()
+	{
+		var password = "foo";
+		var keyManager = ServiceFactory.CreateKeyManager(password);
+
+		HdPubKey NewKey(string label) => keyManager.GenerateNewKey(label, KeyState.Used, true);
+		var scoins = new[]
+		{
+				BitcoinFactory.CreateSmartCoin(NewKey("Pablo"), 0.9m),
+				BitcoinFactory.CreateSmartCoin(NewKey("Daniel"), 0.9m),
+				BitcoinFactory.CreateSmartCoin(NewKey("Adam"), 0.9m),
+				BitcoinFactory.CreateSmartCoin(NewKey("Maria"), 0.9m),
+				BitcoinFactory.CreateSmartCoin(NewKey("Ding"), 0.9m),
+				BitcoinFactory.CreateSmartCoin(NewKey("Joseph"), 0.9m),
+				BitcoinFactory.CreateSmartCoin(NewKey("Eve"), 0.9m),
+				BitcoinFactory.CreateSmartCoin(NewKey("Julio"), 0.9m),
+				BitcoinFactory.CreateSmartCoin(NewKey("Donald, Jean, Lee, Onur"), 0.9m),
+				BitcoinFactory.CreateSmartCoin(NewKey("Satoshi"), 0.9m)
+		};
+
+		foreach (var coin in scoins)
+		{
+			coin.CoinJoinInProgress = true;
+		}
+
+		var coinsView = new CoinsView(scoins.ToArray());
+		var mockTransactionStore = new Mock<AllTransactionStore>(".", Network.Main);
+		var transactionFactory = new TransactionFactory(Network.Main, keyManager, coinsView, mockTransactionStore.Object, password);
+
+		using Key key1 = new();
+		var payment = new PaymentIntent(key1, Money.Coins(1.75m), label: "Sophie");
+		var feeRate = new FeeRate(2m);
+
+		Assert.Throws<ArgumentException>(() => transactionFactory.BuildTransaction(payment, feeRate));
+	}
+
+	[Fact]
 	public void SelectMostPrivateIndependentlyOfCluster()
 	{
 		var transactionFactory = ServiceFactory.CreateTransactionFactory(new[]
