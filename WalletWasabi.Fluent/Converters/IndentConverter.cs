f2c9efabdd2231c8ae8fea5b91df54f2cb07090f
José Manuel Nieto <superjmn@outlook.com>
2023-01-15 22:55:19 +0100

    Apply indentation to coins in pockets

---
+++ WalletWasabi.Fluent/Converters/IndentConverter.cs
@@ -0,0 +1,33 @@
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace WalletWasabi.Fluent.Converters;
+
+public class IndentConverter : AvaloniaObject, IValueConverter
+{
+	public static readonly StyledProperty<double> MultiplierProperty = AvaloniaProperty.Register<IndentConverter, double>(nameof(Multiplier));
+
+	public static Avalonia.Controls.Converters.IndentConverter Instance { get; } = new();
+
+	public double Multiplier
+	{
+		get => GetValue(MultiplierProperty);
+		set => SetValue(MultiplierProperty, value);
+	}
+
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		if (value is int indent)
+		{
+			return new Thickness(indent * Multiplier, 0, 0, 0);
+		}
+
+		return new Thickness();
+	}
+
+	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
