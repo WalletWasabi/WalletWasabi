9ec7ae6c8bd907ed3e2a836bbf039b6ee8798b9c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-01-16 20:31:11 +0100

    Update NavBarBox.cs

--- WalletWasabi.Fluent/Views/NavBar/NavBarBox.cs
+++ WalletWasabi.Fluent/Views/NavBar/NavBarBox.cs
@@ -1,45 +1,56 @@
-using Avalonia;
+using System.Linq;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
 using Avalonia.Input;
-using Avalonia.Input.Platform;
 using Avalonia.VisualTree;
 
 namespace WalletWasabi.Fluent.Views.NavBar;
 
 public class NavBarBox : SelectingItemsControl
 {
+	private static readonly FuncTemplate<IPanel> DefaultPanel =
+		new FuncTemplate<IPanel>(() => new StackPanel());
+
+	static NavBarBox()
+	{
+		SelectionModeProperty.OverrideDefaultValue<NavBarBox>(SelectionMode.AlwaysSelected);
+		ItemsPanelProperty.OverrideDefaultValue<NavBarBox>(DefaultPanel);
+	}
+
+	/// <inheritdoc/>
 	protected override void OnGotFocus(GotFocusEventArgs e)
 	{
 		base.OnGotFocus(e);
 
 		if (e.NavigationMethod == NavigationMethod.Directional)
 		{
-			e.Handled = UpdateSelectionFromEventSource(
-				e.Source,
-				true,
-				e.KeyModifiers.HasAllFlags(KeyModifiers.Shift),
-				e.KeyModifiers.HasAllFlags(KeyModifiers.Control));
+			e.Handled = UpdateSelectionFromEventSource(e.Source);
 		}
 	}
 
+	/// <inheritdoc/>
 	protected override void OnPointerPressed(PointerPressedEventArgs e)
 	{
 		base.OnPointerPressed(e);
 
-		if (e.Source is IVisual source)
+		if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed && e.Pointer.Type == PointerType.Mouse)
 		{
-			var point = e.GetCurrentPoint(source);
-
-			if (point.Properties.IsLeftButtonPressed || point.Properties.IsRightButtonPressed)
-			{
-				e.Handled = UpdateSelectionFromEventSource(
-					e.Source,
-					true,
-					e.KeyModifiers.HasAllFlags(KeyModifiers.Shift),
-					e.KeyModifiers.HasAllFlags(AvaloniaLocator.Current.GetRequiredService<PlatformHotkeyConfiguration>().CommandModifiers),
-					point.Properties.IsRightButtonPressed);
-			}
+			e.Handled = UpdateSelectionFromEventSource(e.Source);
 		}
 	}
 
+	protected override void OnPointerReleased(PointerReleasedEventArgs e)
+	{
+		if (e.InitialPressMouseButton == MouseButton.Left && e.Pointer.Type != PointerType.Mouse)
+		{
+			var container = GetContainerFromEventSource(e.Source);
+			if (container != null
+			    && container.GetVisualsAt(e.GetPosition(container))
+				    .Any(c => container == c || container.IsVisualAncestorOf(c)))
+			{
+				e.Handled = UpdateSelectionFromEventSource(e.Source);
+			}
+		}
+	}
 }
