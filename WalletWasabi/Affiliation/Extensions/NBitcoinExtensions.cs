c56061169f7a8f2484e5836382d0fe58ec100b8f
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2023-01-17 14:57:53 +0100

    fixup! Implement affiliation round data

--- WalletWasabi/Affiliation/Extensions/NBitcoinExtensions.cs
+++ WalletWasabi/Affiliation/Extensions/NBitcoinExtensions.cs
@@ -7,7 +7,7 @@ public static class NBitcoinExtensions
 	public static long ToSlip44CoinType(this Network me) => me.Name switch
 	{
 		"Main" => 0,
-		"RegTest" => 0,
+		"RegTest" => 1,
 		"TestNet" => 1,
 		_ => throw new NotImplementedException()
 	};
