024b4acec510ea27414881b6ad41796da8bb5633
José Manuel Nieto <superjmn@outlook.com>
2023-01-24 16:30:03 +0100

    Don't support ConvertBack

--- WalletWasabi.Fluent/Converters/IndentConverter.cs
+++ WalletWasabi.Fluent/Converters/IndentConverter.cs
@@ -26,6 +26,6 @@ public class IndentConverter : AvaloniaObject, IValueConverter
 
 	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
 	{
-		throw new NotImplementedException();
+		throw new NotSupportedException();
 	}
 }
