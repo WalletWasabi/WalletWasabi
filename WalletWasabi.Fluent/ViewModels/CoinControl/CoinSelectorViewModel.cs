5d45564033f5facdcc10dbf1b3288cf984771311
José Manuel Nieto <superjmn@outlook.com>
2023-01-24 09:50:58 +0100

    Group all column generators

--- WalletWasabi.Fluent/ViewModels/CoinControl/CoinSelectorViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/CoinControl/CoinSelectorViewModel.cs
@@ -86,25 +86,6 @@ public class CoinSelectorViewModel : ViewModelBase, IDisposable
 			.ToList();
 	}
 
-	private static IColumn<CoinControlItemViewModelBase> ChildrenColumn()
-	{
-		return new HierarchicalExpanderColumn<CoinControlItemViewModelBase>(
-			SelectionColumn(),
-			group => group.Children,
-			node => node.Children.Count > 1,
-			node => node.IsExpanded);
-	}
-
-	private static TemplateColumn<CoinControlItemViewModelBase> SelectionColumn()
-	{
-		return new TemplateColumn<CoinControlItemViewModelBase>(
-			"",
-			new FuncDataTemplate<CoinControlItemViewModelBase>(
-				(_, _) => new SelectionCellView(),
-				true),
-			GridLength.Auto);
-	}
-
 	private static void SyncSelectedItems(IEnumerable<PocketCoinControlItemViewModel> items, IEnumerable<SmartCoin> selectedCoins)
 	{
 		var allCoins = GetAllCoinItems(items);
@@ -113,58 +94,6 @@ public class CoinSelectorViewModel : ViewModelBase, IDisposable
 		{
 			viewModel.IsSelected = true;
 		}
-	}
-
-	private static IColumn<CoinControlItemViewModelBase> AmountColumn()
-	{
-		return new PlainTextColumn<CoinControlItemViewModelBase>(
-			"Amount",
-			node => node.Amount.ToFormattedString(),
-			GridLength.Auto,
-			new ColumnOptions<CoinControlItemViewModelBase>
-			{
-				CompareAscending = SortAscending<CoinControlItemViewModelBase, Money>(x => x.Amount),
-				CompareDescending = SortDescending<CoinControlItemViewModelBase, Money>(x => x.Amount)
-			});
-	}
-
-	private static IColumn<CoinControlItemViewModelBase> IndicatorsColumn()
-	{
-		return new TemplateColumn<CoinControlItemViewModelBase>(
-			"",
-			new FuncDataTemplate<CoinControlItemViewModelBase>((_, _) => new IndicatorsCellView(), true),
-			GridLength.Auto,
-			new ColumnOptions<CoinControlItemViewModelBase>
-			{
-				CompareAscending = SortAscending<CoinControlItemViewModelBase, int>(GetIndicatorPriority),
-				CompareDescending = SortDescending<CoinControlItemViewModelBase, int>(GetIndicatorPriority)
-			});
-	}
-
-	private static IColumn<CoinControlItemViewModelBase> AnonymityScoreColumn()
-	{
-		return new PlainTextColumn<CoinControlItemViewModelBase>(
-			new AnonymityScoreHeaderView(),
-			node => node.AnonymityScore.ToString(),
-			GridLength.Auto,
-			new TextColumnOptions<CoinControlItemViewModelBase>
-			{
-				CompareAscending = SortAscending<CoinControlItemViewModelBase, int?>(b => b.AnonymityScore),
-				CompareDescending = SortDescending<CoinControlItemViewModelBase, int?>(b => b.AnonymityScore)
-			});
-	}
-
-	private static IColumn<CoinControlItemViewModelBase> PocketColumn()
-	{
-		return new TemplateColumn<CoinControlItemViewModelBase>(
-			"Pocket",
-			new FuncDataTemplate<CoinControlItemViewModelBase>((_, _) => new LabelsCellView(), true),
-			GridLength.Star,
-			new ColumnOptions<CoinControlItemViewModelBase>
-			{
-				CompareAscending = SortAscending<CoinControlItemViewModelBase, int>(GetLabelPriority),
-				CompareDescending = SortDescending<CoinControlItemViewModelBase, int>(GetLabelPriority)
-			});
 	}
 
 	private static int GetLabelPriority(CoinControlItemViewModelBase coin)
@@ -217,5 +146,76 @@ public class CoinSelectorViewModel : ViewModelBase, IDisposable
 	private static Comparison<TSource?> SortDescending<TSource, TProperty>(Func<TSource, TProperty> selector)
 	{
 		return (x, y) => Comparer<TProperty>.Default.Compare(selector(y!), selector(x!));
+	}
+
+	private static IColumn<CoinControlItemViewModelBase> ChildrenColumn()
+	{
+		return new HierarchicalExpanderColumn<CoinControlItemViewModelBase>(
+			SelectionColumn(),
+			group => group.Children,
+			node => node.Children.Count > 1,
+			node => node.IsExpanded);
+	}
+
+	private static TemplateColumn<CoinControlItemViewModelBase> SelectionColumn()
+	{
+		return new TemplateColumn<CoinControlItemViewModelBase>(
+			"",
+			new FuncDataTemplate<CoinControlItemViewModelBase>(
+				(_, _) => new SelectionCellView(),
+				true),
+			GridLength.Auto);
+	}
+
+	private static IColumn<CoinControlItemViewModelBase> AmountColumn()
+	{
+		return new PlainTextColumn<CoinControlItemViewModelBase>(
+			"Amount",
+			node => node.Amount.ToFormattedString(),
+			GridLength.Auto,
+			new ColumnOptions<CoinControlItemViewModelBase>
+			{
+				CompareAscending = SortAscending<CoinControlItemViewModelBase, Money>(x => x.Amount),
+				CompareDescending = SortDescending<CoinControlItemViewModelBase, Money>(x => x.Amount)
+			});
+	}
+
+	private static IColumn<CoinControlItemViewModelBase> IndicatorsColumn()
+	{
+		return new TemplateColumn<CoinControlItemViewModelBase>(
+			"",
+			new FuncDataTemplate<CoinControlItemViewModelBase>((_, _) => new IndicatorsCellView(), true),
+			GridLength.Auto,
+			new ColumnOptions<CoinControlItemViewModelBase>
+			{
+				CompareAscending = SortAscending<CoinControlItemViewModelBase, int>(GetIndicatorPriority),
+				CompareDescending = SortDescending<CoinControlItemViewModelBase, int>(GetIndicatorPriority)
+			});
+	}
+
+	private static IColumn<CoinControlItemViewModelBase> AnonymityScoreColumn()
+	{
+		return new PlainTextColumn<CoinControlItemViewModelBase>(
+			new AnonymityScoreHeaderView(),
+			node => node.AnonymityScore.ToString(),
+			GridLength.Auto,
+			new TextColumnOptions<CoinControlItemViewModelBase>
+			{
+				CompareAscending = SortAscending<CoinControlItemViewModelBase, int?>(b => b.AnonymityScore),
+				CompareDescending = SortDescending<CoinControlItemViewModelBase, int?>(b => b.AnonymityScore)
+			});
+	}
+
+	private static IColumn<CoinControlItemViewModelBase> PocketColumn()
+	{
+		return new TemplateColumn<CoinControlItemViewModelBase>(
+			"Pocket",
+			new FuncDataTemplate<CoinControlItemViewModelBase>((_, _) => new LabelsCellView(), true),
+			GridLength.Star,
+			new ColumnOptions<CoinControlItemViewModelBase>
+			{
+				CompareAscending = SortAscending<CoinControlItemViewModelBase, int>(GetLabelPriority),
+				CompareDescending = SortDescending<CoinControlItemViewModelBase, int>(GetLabelPriority)
+			});
 	}
 }
