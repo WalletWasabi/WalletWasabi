dae74dc4adb34e38fc6f17bdd210914c5baaa013
adamPetho <45069029+adamPetho@users.noreply.github.com>
2023-01-25 12:40:08 +0100

    add Reason enum

--- WalletWasabi/WabiSabi/Backend/Banning/CoinVerifyResult.cs
+++ WalletWasabi/WabiSabi/Backend/Banning/CoinVerifyResult.cs
@@ -2,4 +2,15 @@ using NBitcoin;
 
 namespace WalletWasabi.WabiSabi.Backend.Banning;
 
-public record CoinVerifyResult(Coin Coin, bool ShouldBan, bool ShouldRemove);
+public record CoinVerifyResult(Coin Coin, bool ShouldBan, bool ShouldRemove, Reason Reason, ApiResponseItem? ApiResponseItem = null);
+
+public enum Reason
+{
+	Remix,
+	Whitelisted,
+	OneHop,
+	RemoteApiChecked,
+	NotChecked,
+	Inmature,
+	Exception
+}
