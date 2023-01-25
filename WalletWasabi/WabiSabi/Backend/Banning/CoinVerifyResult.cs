75e7e2f20934f28f986dbc9b9de8b9c9a7a07ebc
adamPetho <45069029+adamPetho@users.noreply.github.com>
2023-01-25 14:58:18 +0100

    add exception to results

--- WalletWasabi/WabiSabi/Backend/Banning/CoinVerifyResult.cs
+++ WalletWasabi/WabiSabi/Backend/Banning/CoinVerifyResult.cs
@@ -2,7 +2,7 @@ using NBitcoin;
 
 namespace WalletWasabi.WabiSabi.Backend.Banning;
 
-public record CoinVerifyResult(Coin Coin, bool ShouldBan, bool ShouldRemove, Reason Reason, ApiResponseItem? ApiResponseItem = null);
+public record CoinVerifyResult(Coin Coin, bool ShouldBan, bool ShouldRemove, Reason Reason, ApiResponseItem? ApiResponseItem = null, Exception? Exception = null);
 
 public enum Reason
 {
