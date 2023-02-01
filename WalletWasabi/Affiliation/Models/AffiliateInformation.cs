403a36c77bdb41607dbabd2b0a063bab7958e986
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2023-02-01 17:09:33 +0100

    fixup! Implement affiliation information

--- WalletWasabi/Affiliation/Models/AffiliateInformation.cs
+++ WalletWasabi/Affiliation/Models/AffiliateInformation.cs
@@ -8,8 +8,8 @@ namespace WalletWasabi.Affiliation.Models;
 public record AffiliateInformation
 (
 	ImmutableArray<AffiliationFlag> RunningAffiliateServers,
-	ImmutableDictionary<uint256, ImmutableDictionary<AffiliationFlag, byte[]>> CoinjoinRequests
+	ImmutableDictionary<string, ImmutableDictionary<AffiliationFlag, byte[]>> CoinjoinRequests
 )
 {
-	public static readonly AffiliateInformation Empty = new(ImmutableArray<AffiliationFlag>.Empty, ImmutableDictionary<uint256, ImmutableDictionary<AffiliationFlag, byte[]>>.Empty);
+	public static readonly AffiliateInformation Empty = new(ImmutableArray<AffiliationFlag>.Empty, ImmutableDictionary<string, ImmutableDictionary<AffiliationFlag, byte[]>>.Empty);
 }
