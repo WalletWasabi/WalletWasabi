c87bba1b2d0231387e0d9d96ea74651fadf08d16
Lucas Ontivero <lucasontivero@gmail.com>
2023-02-05 17:47:28 -0300

    Fix error found during CR

--- WalletWasabi/Blockchain/TransactionProcessing/TransactionProcessor.cs
+++ WalletWasabi/Blockchain/TransactionProcessing/TransactionProcessor.cs
@@ -281,7 +281,7 @@ public class TransactionProcessor
 	private bool CanBeConsideredDustAttack(TxOut output, HdPubKey hdPubKey, bool weAreAmongTheSender) =>
 		output.Value <= DustThreshold // the value received is under the dust threshold
 		&& !weAreAmongTheSender // we are not one of the senders (it is not a self-spending tx or coinjoin)
-		&& hdPubKey.KeyState == KeyState.Used; // the destination address has already been used (address reuse)
+		&& Coins.Any(c => c.HdPubKey == hdPubKey); // the destination address has already been used (address reuse)
 	
 	public void UndoBlock(Height blockHeight)
 	{
