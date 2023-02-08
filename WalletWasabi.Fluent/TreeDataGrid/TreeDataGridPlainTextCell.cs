c09d416742d418133188828792dd20f44f26574d
Yahia Chiheb <chihebyahia@gmail.com>
2023-02-08 10:55:24 +0100

    Use field

--- WalletWasabi.Fluent/TreeDataGrid/TreeDataGridPlainTextCell.cs
+++ WalletWasabi.Fluent/TreeDataGrid/TreeDataGridPlainTextCell.cs
@@ -9,17 +9,16 @@ namespace WalletWasabi.Fluent.TreeDataGrid;
 internal class TreeDataGridPlainTextCell : TreeDataGridCell
 {
 	private FormattedText? _formattedText;
-
-	private string? Text { get; set; }
+	private string? _text;
 
 	public override void Realize(IElementFactory factory, ICell model, int columnIndex, int rowIndex)
 	{
 		var plainTextCell = (PlainTextCell)model;
 		var text = plainTextCell.Value;
 
-		if (text != Text)
+		if (text != _text)
 		{
-			Text = text;
+			_text = text;
 			_formattedText = null;
 		}
 
@@ -37,7 +36,7 @@ internal class TreeDataGridPlainTextCell : TreeDataGridCell
 
 	protected override Size MeasureOverride(Size availableSize)
 	{
-		if (string.IsNullOrWhiteSpace(Text))
+		if (string.IsNullOrWhiteSpace(_text))
 		{
 			return default;
 		}
@@ -45,7 +44,7 @@ internal class TreeDataGridPlainTextCell : TreeDataGridCell
 		if (availableSize != _formattedText?.Constraint)
 		{
 			_formattedText = new FormattedText(
-				Text,
+				_text,
 				new Typeface(FontFamily, FontStyle, FontWeight),
 				FontSize,
 				TextAlignment.Left,
