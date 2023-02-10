d980d4df26384d98eaacdf2959461df43a068ce1
Kiminuo <kiminuo@protonmail.com>
2023-02-10 13:11:40 +0100

    AffiliationMessageSignerTests: Move method and assert its output

--- WalletWasabi.Tests/UnitTests/Affiliation/AffiliationMessageSignerTests.cs
+++ WalletWasabi.Tests/UnitTests/Affiliation/AffiliationMessageSignerTests.cs
@@ -1,5 +1,5 @@
-using System.Security.Cryptography;
 using NBitcoin.DataEncoders;
+using System.Security.Cryptography;
 using WalletWasabi.Affiliation;
 using WalletWasabi.Helpers;
 using Xunit;
@@ -8,17 +8,6 @@ namespace WalletWasabi.Tests.UnitTests.Affiliation;
 
 public class AffiliationMessageSignerTests
 {
-	/// <summary>The method shows how to generate affiliation message signer key</summary>
-	public (string, string) GenerateKey()
-	{
-		using var ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256);
-		var privateKeyBytes = ecdsa.ExportECPrivateKey();
-		var publicKeyBytes = ecdsa.ExportSubjectPublicKeyInfo();
-		var privateKeyHex = Encoders.Hex.EncodeData(privateKeyBytes);
-		var publicKeyHex = Encoders.Hex.EncodeData(publicKeyBytes);
-		return (privateKeyHex, publicKeyHex);
-	}
-
 	[Fact]
 	public void FallbackSignerKeyTest()
 	{
@@ -29,6 +18,22 @@ public class AffiliationMessageSignerTests
 	public void GeneratedKeyTest()
 	{
 		(string privateKey, string publicKey) = GenerateKey();
+		Assert.NotNull(privateKey);
+		Assert.NotNull(publicKey);
+
 		using AffiliationMessageSigner signer = new(privateKey);
+	}
+
+	/// <summary>
+	/// The method shows how to generate affiliation message signer key.
+	/// </summary>
+	private (string, string) GenerateKey()
+	{
+		using var ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+		var privateKeyBytes = ecdsa.ExportECPrivateKey();
+		var publicKeyBytes = ecdsa.ExportSubjectPublicKeyInfo();
+		var privateKeyHex = Encoders.Hex.EncodeData(privateKeyBytes);
+		var publicKeyHex = Encoders.Hex.EncodeData(publicKeyBytes);
+		return (privateKeyHex, publicKeyHex);
 	}
 }
