5fea3339934369cca3b97cba9386d42bc7ed71c4
Kiminuo <kiminuo@protonmail.com>
2023-02-10 13:11:39 +0100

    `AffiliateInformation`: Readability

--- WalletWasabi/Affiliation/Models/AffiliateInformation.cs
+++ WalletWasabi/Affiliation/Models/AffiliateInformation.cs
@@ -7,5 +7,7 @@ public record AffiliateInformation
 	ImmutableArray<AffiliationFlag> RunningAffiliateServers,
 	ImmutableDictionary<string, ImmutableDictionary<AffiliationFlag, byte[]>> CoinjoinRequests)
 {
-	public static readonly AffiliateInformation Empty = new(ImmutableArray<AffiliationFlag>.Empty, ImmutableDictionary<string, ImmutableDictionary<AffiliationFlag, byte[]>>.Empty);
+	public static readonly AffiliateInformation Empty = new(
+		RunningAffiliateServers: ImmutableArray<AffiliationFlag>.Empty,
+		CoinjoinRequests: ImmutableDictionary<string, ImmutableDictionary<AffiliationFlag, byte[]>>.Empty);
 }
