e6d1f5d2f1a6753d9efdeae06a66a900a61d91eb
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2023-02-16 19:14:14 +0800

    Hack fix for a bug in avalonia's selection.

---
+++ WalletWasabi.Fluent/Behaviors/ListBoxReselectingBehavior.cs
@@ -0,0 +1,42 @@
+
+using System.Collections.Specialized;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using DynamicData.Binding;
+using ReactiveUI;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+public class ListBoxReselectingBehavior : DisposingBehavior<ListBox>
+{
+	protected override void OnAttached(CompositeDisposable disposables)
+	{
+		if (AssociatedObject is not {   } listBox)
+		{
+			return;
+		}
+
+		listBox.WhenAnyValue(x => x.Items)
+			.Where(x => x is INotifyCollectionChanged)
+			.Select(x => x as INotifyCollectionChanged)
+			.Do(x => RegisterItemListener(listBox, x, disposables))
+			.Subscribe()
+			.DisposeWith(disposables);
+	}
+
+	private static void RegisterItemListener(SelectingItemsControl listBox,
+		INotifyCollectionChanged? walletCollection,
+		CompositeDisposable disposables)
+	{
+		walletCollection?.ObserveCollectionChanges()
+			.Where(x => x.EventArgs.Action == NotifyCollectionChangedAction.Move)
+			.Do(x =>
+			{
+				listBox.SelectedIndex = x.EventArgs.NewStartingIndex;
+			})
+			.Subscribe()
+			.DisposeWith(disposables);
+	}
+}
