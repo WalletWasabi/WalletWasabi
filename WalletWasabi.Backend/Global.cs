4dec0e0b91ca6a7e39a30cf998223c88f99ccbe5
Kiminuo <kiminuo@protonmail.com>
2023-02-22 14:54:19 +0100

    Dispose

--- WalletWasabi.Backend/Global.cs
+++ WalletWasabi.Backend/Global.cs
@@ -66,6 +66,7 @@ public class Global : IDisposable
 	private HttpClient HttpClient { get; }
 
 	public Coordinator? Coordinator { get; private set; }
+	private CoinVerifierApiClient? CoinVerifierApiClient { get; set; }
 	public CoinVerifier? CoinVerifier { get; private set; }
 
 	public Config Config { get; }
@@ -114,9 +115,9 @@ public class Global : IDisposable
 
 				HttpClient.BaseAddress = url;
 
-				var coinVerifierApiClient = new CoinVerifierApiClient(CoordinatorParameters.RuntimeCoordinatorConfig.CoinVerifierApiAuthToken, HttpClient);
+				CoinVerifierApiClient = new CoinVerifierApiClient(CoordinatorParameters.RuntimeCoordinatorConfig.CoinVerifierApiAuthToken, HttpClient);
 				var whitelist = await Whitelist.CreateAndLoadFromFileAsync(CoordinatorParameters.WhitelistFilePath, wabiSabiConfig, cancel).ConfigureAwait(false);
-				coinVerifier = new(CoinJoinIdStore, coinVerifierApiClient, whitelist, CoordinatorParameters.RuntimeCoordinatorConfig, auditsDirectoryPath: Path.Combine(CoordinatorParameters.CoordinatorDataDir, "CoinVerifierAudits"));
+				coinVerifier = new(CoinJoinIdStore, CoinVerifierApiClient, whitelist, CoordinatorParameters.RuntimeCoordinatorConfig, auditsDirectoryPath: Path.Combine(CoordinatorParameters.CoordinatorDataDir, "CoinVerifierAudits"));
 				CoinVerifier = coinVerifier;
 				WhiteList = whitelist;
 				Logger.LogInfo("CoinVerifier created successfully.");
@@ -278,6 +279,11 @@ public class Global : IDisposable
 		if (CoinVerifier is { } coinVerifier)
 		{
 			await coinVerifier.DisposeAsync().ConfigureAwait(false);
+		}
+
+		if (CoinVerifierApiClient is { } coinVerifierApiClient)
+		{
+			await coinVerifierApiClient.DisposeAsync().ConfigureAwait(false);
 		}
 	}
 
