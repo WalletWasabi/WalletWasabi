159fdb988613c1dd3ad0f1b4fe7a91e162dd0a75
Marnix <93143998+MarnixCroes@users.noreply.github.com>
2023-03-02 10:36:33 +0100

    Update ScriptType.cs
    change P2WPKH from SegWit to NativeSegwit

--- WalletWasabi.Fluent/ViewModels/CoinControl/Core/ScriptType.cs
+++ WalletWasabi.Fluent/ViewModels/CoinControl/Core/ScriptType.cs
@@ -4,6 +4,7 @@ public record ScriptType(string Name, string ShortName)
 {
 	public static readonly ScriptType Unknown = new("Unknown", "?");
 	public static ScriptType SegWit = new("SegWit", "SW");
+	public static ScriptType NativeSegWit = new("Native SegWit", "NS");
 	public static ScriptType Taproot = new("Taproot", "TR");
 
 	public static ScriptType FromEnum(NBitcoin.ScriptType type)
@@ -16,7 +17,7 @@ public record ScriptType(string Name, string ShortName)
 			NBitcoin.ScriptType.P2PK => Unknown,
 			NBitcoin.ScriptType.MultiSig => Unknown,
 			NBitcoin.ScriptType.P2WSH => SegWit,
-			NBitcoin.ScriptType.P2WPKH => SegWit,
+			NBitcoin.ScriptType.P2WPKH => NativeSegWit,
 			NBitcoin.ScriptType.Taproot => Taproot,
 			_ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
 		};
