42f4a7553e601aa9ffc2c7482125989144884f74
José Manuel Nieto <superjmn@outlook.com>
2023-03-07 18:43:35 +0100

    Remove special behavior for single coin

--- WalletWasabi.Fluent/ViewModels/CoinControl/Core/PocketCoinControlItemViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/CoinControl/Core/PocketCoinControlItemViewModel.cs
@@ -22,11 +22,11 @@ public class PocketCoinControlItemViewModel : CoinControlItemViewModelBase, IDis
 		BannedUntilUtcToolTip = IsBanned ? "Some coins can't participate in coinjoin" : null;
 		Amount = pocket.Amount;
 		IsCoinjoining = pocketCoins.Any(x => x.CoinJoinInProgress);
-		AnonymityScore = pocketCoins.Count == 1 ? (int)pocketCoins[0].HdPubKey.AnonymitySet : null;
+		AnonymityScore = null;
 		Labels = pocket.Labels;
 		Children = pocketCoins.OrderByDescending(x => x.Amount).Select(coin => new CoinCoinControlItemViewModel(coin)).ToList();
 		CanBeSelected = true;
-		ScriptType = pocketCoins.Count == 1 ? ScriptType.FromEnum(pocketCoins[0].ScriptType) : null;
+		ScriptType = null;
 
 		Children
 			.AsObservableChangeSet()
