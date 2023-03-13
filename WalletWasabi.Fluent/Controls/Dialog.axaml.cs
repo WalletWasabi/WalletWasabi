6ded4171dcb8e1a2a17dc75b762d45f2499a620b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-13 20:30:27 +0100

    Wait for real window activation

--- WalletWasabi.Fluent/Controls/Dialog.axaml.cs
+++ WalletWasabi.Fluent/Controls/Dialog.axaml.cs
@@ -203,8 +203,6 @@ public class Dialog : ContentControl
 	{
 		base.OnAttachedToVisualTree(e);
 
-		_canCancelActivatedOnPointerPressed = true;
-
 		ApplicationHelper.MainWindowActivated.Subscribe(UpdateActivatedDelay);
 	}
 
