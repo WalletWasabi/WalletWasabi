44543ddec66bef61d33fae829d827e7073faa6fe
ichthus1604 <ichthus1604@gmail.com>
2023-03-18 10:56:44 -0300

    pass UIContext where required

--- WalletWasabi.Fluent/ViewModels/Wallets/WatchOnlyWalletViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/WatchOnlyWalletViewModel.cs
@@ -1,11 +1,12 @@
+using WalletWasabi.Fluent.Models.UI;
 using WalletWasabi.Wallets;
 
 namespace WalletWasabi.Fluent.ViewModels.Wallets;
 
 public class WatchOnlyWalletViewModel : WalletViewModel
 {
-	internal WatchOnlyWalletViewModel(Wallet wallet)
-		: base(wallet)
+	internal WatchOnlyWalletViewModel(UIContext uiContext, Wallet wallet)
+		: base(uiContext, wallet)
 	{
 	}
 }
