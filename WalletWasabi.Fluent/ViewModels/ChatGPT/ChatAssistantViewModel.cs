d760980eb0dff899288637fcf6cfdb52e3f6715c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-23 21:27:41 +0100

    Use C# scripting for commands

--- WalletWasabi.Fluent/ViewModels/ChatGPT/ChatAssistantViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/ChatGPT/ChatAssistantViewModel.cs
@@ -10,6 +10,7 @@ using ChatGPT.Services;
 using ChatGPT.ViewModels.Chat;
 using ChatGPT.ViewModels.Settings;
 using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using ReactiveUI;
@@ -26,7 +27,8 @@ public partial class ChatAssistantViewModel : ReactiveObject
 
 	private string _initialDirections = """
 You are a helpful assistant named Wasabito, you are Wasabi Wallet operator.
-I will write text prompts and you will generate appropriate answers only in json format I have provided.
+I will write text prompts and you will generate appropriate answers
+only in json format I have provided.
 
 The json format for the answers is as follows:
 {
@@ -36,25 +38,23 @@ The json format for the answers is as follows:
 The status and message properties are of type string.
 
 The json response "status" property value can be one of the following:
-- "command":  when wasabi api command is the answer
+- "command":  when wasabi C# scripting api command is the answer
 - "error": when not possible to answer or other problem
 - "message": when answer is only text message but not error
 
 When "status"="command" the "message" value can only be set to
- one of the following wasabi api commands available:
-- send(address, amount): command requires address and amount, calls send method and displays send dialog
-- address receive(): command returns only new BTC address, does not require any params, shows receive dialog with qr code
-- value balance(): command returns BTC or USD balance value, does not require any params nor UI interaction
-e.g.:
+one of the following wasabi api C# scripting commands which will be executed as C# script:
+- public async Task<string> Send(string address, string amount)
+  command requires address and amount parameters
+- public async Task<string> Receive(string[] labels)
+  command requires labels array parameter
+- public async Task<string> Balance()
+  command does not require any parameters
+e.g. for Send command (other follow similar pattern):
 {
   "status": "command",
-  "message": "send|address|amount|void",
+  "message": "await Send("valid BTC address", "valid BTC amount")",
 }
-When message is api command write it using | separator between command name
-and command parameters, last item is return value e.g.:
-- send|address|amount|void
-- receive|address
-- balance|value
 
 If user does not provide valid param to execute api command please set status=error and ask followup question to provide that info:
 e.g.:
@@ -120,6 +120,83 @@ Do not add additional text before or after json.
 
 	public ObservableCollection<MessageViewModel> Messages { get; }
 
+	private class Globals
+	{
+		public ChatAssistantViewModel Chat { get; set; }
+
+		public MainViewModel Main { get; set; }
+
+		public async Task<string> Send(string address, string amount)
+		{
+			// TODO:
+
+			return $"Sending {amount} to {address}...";
+		}
+
+		public async Task<string> Receive(string[] labels)
+		{
+			// TODO: Show receive dialog but QR cod progress view.
+
+			var resultMessage = "";
+
+			if (MainViewModel.Instance.CurrentWallet is { } currentWallet)
+			{
+				var currentWalletValue = await currentWallet.LastOrDefaultAsync();
+				if (currentWalletValue is { })
+				{
+					// TODO:
+					if (currentWalletValue.ReceiveCommand.CanExecute(null))
+					{
+						currentWalletValue.ReceiveCommand.Execute(null);
+						resultMessage = "Generating receive address...";
+					}
+					else
+					{
+						resultMessage = "Can't generate receive address.";
+					}
+				}
+				else
+				{
+					resultMessage = "Please select current wallet.";
+				}
+			}
+			else
+			{
+				resultMessage = "Can't generate receive address.";
+			}
+
+			return resultMessage;
+		}
+
+		public async Task<string> Balance()
+		{
+			// TODO:
+
+			var resultMessage = "";
+
+			if (MainViewModel.Instance.CurrentWallet is { } currentWallet)
+			{
+				var currentWalletValue = await currentWallet.LastOrDefaultAsync();
+				if (currentWalletValue is { })
+				{
+					var balance = currentWalletValue.Wallet.Coins.TotalAmount().ToFormattedString();
+
+					resultMessage = $"{currentWalletValue.Wallet.WalletName} balance is {balance}";
+				}
+				else
+				{
+					resultMessage = "Please select current wallet.";
+				}
+			}
+			else
+			{
+				resultMessage = "Can not provide wallet balance.";
+			}
+
+			return resultMessage;
+		}
+	}
+
 	private async Task SendAsync(string input)
 	{
 		try
@@ -152,6 +229,17 @@ Do not add additional text before or after json.
 
 						if (assistantResult.Status == "command")
 						{
+							if (message is { })
+							{
+								var globals = new Globals
+								{
+									Chat = this,
+									Main = MainViewModel.Instance
+								};
+								resultMessage = await CSharpScript.EvaluateAsync<string>(message, globals: globals);
+							}
+
+							/*
 							if (message is { })
 							{
 								var args = message.Split(new char[] { '|' });
@@ -241,6 +329,7 @@ Do not add additional text before or after json.
 								// TODO:
 								resultMessage = message;
 							}
+							*/
 						}
 						else if (assistantResult.Status == "error")
 						{
