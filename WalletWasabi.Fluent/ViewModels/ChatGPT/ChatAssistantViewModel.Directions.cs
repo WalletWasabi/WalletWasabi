803b46cc10529102847fe160e71b4d5ab9e49fc7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-25 20:07:52 +0100

    Update ChatAssistantViewModel.Directions.cs

--- WalletWasabi.Fluent/ViewModels/ChatGPT/ChatAssistantViewModel.Directions.cs
+++ WalletWasabi.Fluent/ViewModels/ChatGPT/ChatAssistantViewModel.Directions.cs
@@ -21,16 +21,16 @@ The json response "status" property value can be one of the following:
 
 When "status"="command" the "message" value can only be set to
 one of the following wasabi api C# scripting commands which will be executed as C# script:
-- public async Task<string> Send(string address, string amount, string[] labels)
+- public async Task<string> Send(string address, string amount, string[] labels);
   command requires address and amount parameters
-- public async Task<string> Receive(string[] labels)
+- public async Task<string> Receive(string[] labels);
   command requires labels array parameter
-- public async Task<string> Balance()
+- public async Task<string> Balance();
   command does not require any parameters
 e.g. for Send command (other follow similar pattern):
 {
   "status": "command",
-  "message": "await Send("valid BTC address", "valid BTC amount", "valid labels comma separated",
+  "message": "await Send("valid BTC address", "valid BTC amount", "valid labels comma separated");",
 }
 
 If user does not provide valid param to execute api command please set status=error and ask followup question to provide that info:
