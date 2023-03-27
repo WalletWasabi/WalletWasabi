302de692828be5c34ab13b704328b8dde2fdfe27
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-27 23:07:18 +0200

    Update ChatAssistantScriptGlobals.cs

--- WalletWasabi.Fluent/ViewModels/ChatGPT/ChatAssistantScriptGlobals.cs
+++ WalletWasabi.Fluent/ViewModels/ChatGPT/ChatAssistantScriptGlobals.cs
@@ -1,8 +1,14 @@
 using System.Threading.Tasks;
+using NBitcoin;
 using WalletWasabi.Blockchain.Analysis.Clustering;
+using WalletWasabi.Blockchain.Transactions;
+using WalletWasabi.Extensions;
 using WalletWasabi.Fluent.Helpers;
+using WalletWasabi.Fluent.Models;
 using WalletWasabi.Fluent.ViewModels.ChatGPT.Messages.Actions;
 using WalletWasabi.Fluent.ViewModels.Wallets;
+using WalletWasabi.Fluent.ViewModels.Wallets.Send;
+using WalletWasabi.Logging;
 
 namespace WalletWasabi.Fluent.ViewModels.ChatGPT;
 
@@ -12,16 +18,93 @@ public class ChatAssistantScriptGlobals
 
 	public MainViewModel Main { get; set; }
 
-	public async Task<string> Send(string address, string amount, string[] labels)
+	public async Task<string> Send(string address, decimal amountBtc, string[] labels)
 	{
-		var resultMessage = $"Sending {amount} to {address}, {labels}...";
+		var resultMessage = "";
 
-		Chat.Messages.Add(new SendActionMessageViewModel()
+		if (MainViewModel.Instance.MainScreen.CurrentPage is WalletViewModel currentWallet)
 		{
-			Message = resultMessage
-		});
+			Chat.Messages.Add(new SendActionMessageViewModel()
+			{
+				Message = $"Sending {amountBtc} to {address}, {labels}..."
+			});
 
-		// TODO:
+			Chat.IsBusy = true;
+
+			var IsFixedAmount = false;
+			var IsPayJoin = false;
+
+			// TODO:
+			var amount = new Money(amountBtc, MoneyUnit.BTC);
+			var transactionInfo = new TransactionInfo(BitcoinAddress.Create(address, currentWallet.Wallet.Network), currentWallet.Wallet.AnonScoreTarget)
+			{
+				Amount = amount,
+				Recipient = new SmartLabel(labels),
+				// TODO: PayJoinClient = GetPayjoinClient(PayJoinEndPoint),
+				PayJoinClient = null,
+				IsFixedAmount = IsFixedAmount,
+				SubtractFee = amount == currentWallet.Wallet.Coins.TotalAmount() && !(IsFixedAmount || IsPayJoin)
+			};
+
+			var transaction = await Task.Run(() => TransactionHelpers.BuildTransaction(currentWallet.Wallet, transactionInfo));
+			var transactionAuthorizationInfo = new TransactionAuthorizationInfo(transaction);
+			// TODO:
+			//var authResult = await AuthorizeAsync(transactionAuthorizationInfo);
+			//if (authResult)
+			{
+				try
+				{
+					var finalTransaction =
+						await GetFinalTransactionAsync(transactionAuthorizationInfo.Transaction, transactionInfo);
+					await SendTransactionAsync(finalTransaction);
+					// _cancellationTokenSource?.Cancel();
+					//Navigate().To(new SendSuccessViewModel(currentWallet.Wallet, finalTransaction));
+					Chat.Messages.Add(new SendActionMessageViewModel()
+					{
+						Message = $"Sent {amountBtc} to {address}, {labels}."
+					});
+				}
+				catch (Exception ex)
+				{
+					Logger.LogError(ex);
+					// TODO:
+					//await ShowErrorAsync("Transaction", ex.ToUserFriendlyString(),
+					//	"Wasabi was unable to send your transaction.");
+				}
+			}
+
+			async Task<SmartTransaction> GetFinalTransactionAsync(SmartTransaction transaction,
+				TransactionInfo transactionInfo)
+			{
+				if (transactionInfo.PayJoinClient is { })
+				{
+					try
+					{
+						var payJoinTransaction = await Task.Run(() =>
+							TransactionHelpers.BuildTransaction(currentWallet.Wallet, transactionInfo, isPayJoin: true));
+						return payJoinTransaction.Transaction;
+					}
+					catch (Exception ex)
+					{
+						Logger.LogError(ex);
+					}
+				}
+
+				return transaction;
+			}
+
+			async Task SendTransactionAsync(SmartTransaction transaction)
+			{
+				await Services.TransactionBroadcaster.SendTransactionAsync(transaction);
+			}
+
+
+			Chat.IsBusy = false;
+		}
+		else
+		{
+			resultMessage = "Can't generate receive address, please login into wallet.";
+		}
 
 		return resultMessage;
 	}
