b80e406b0e54ac559ba52870fd2b0fbebef5347c
Lucas Ontivero <lucasontivero@gmail.com>
2023-03-27 11:42:42 -0300

    Fix serialization

--- WalletWasabi/WabiSabi/Crypto/Serialization/IssuanceRequestJsonConverter.cs
+++ WalletWasabi/WabiSabi/Crypto/Serialization/IssuanceRequestJsonConverter.cs
@@ -11,8 +11,8 @@ public class IssuanceRequestJsonConverter : JsonConverter<IssuanceRequest>
 	public override IssuanceRequest? ReadJson(JsonReader reader, Type objectType, IssuanceRequest? existingValue, bool hasExistingValue, JsonSerializer serializer)
 	{
 		reader.Expect(JsonToken.StartObject);
-		var ma = reader.ReadProperty<GroupElement>(serializer, "Ma");
-		var bitCommitments = reader.ReadProperty<IEnumerable<GroupElement>>(serializer, "BitCommitments");
+		var ma = reader.ReadProperty<GroupElement>(serializer, "ma");
+		var bitCommitments = reader.ReadProperty<IEnumerable<GroupElement>>(serializer, "bitCommitments");
 		reader.Read();
 		reader.Expect(JsonToken.EndObject);
 		return ReflectionUtils.CreateInstance<IssuanceRequest>(new object[] { ma, bitCommitments });
@@ -26,8 +26,8 @@ public class IssuanceRequestJsonConverter : JsonConverter<IssuanceRequest>
 			throw new ArgumentException($"No valid {nameof(IssuanceRequest)}.", nameof(ir));
 		}
 		writer.WriteStartObject();
-		writer.WriteProperty("Ma", ir.Ma, serializer);
-		writer.WriteProperty("BitCommitments", ir.BitCommitments, serializer);
+		writer.WriteProperty("ma", ir.Ma, serializer);
+		writer.WriteProperty("bitCommitments", ir.BitCommitments, serializer);
 		writer.WriteEndObject();
 	}
 }
