853027ac7390465474dbb01c34d81d9f889c8c8f
José Manuel Nieto <superjmn@outlook.com>
2023-03-29 12:14:06 +0200

    Disable null warning and add explanatory comment

--- WalletWasabi.Fluent/Models/UI/UIContext.cs
+++ WalletWasabi.Fluent/Models/UI/UIContext.cs
@@ -7,12 +7,14 @@ public class UIContext
 {
 	public UIContext(IQrCodeGenerator qrCodeGenerator, IClipboard clipboard)
 	{
-		QrCodeGenerator = qrCodeGenerator;
-		Clipboard = clipboard;
+		QrCodeGenerator = qrCodeGenerator ?? throw new ArgumentNullException(nameof(qrCodeGenerator));
+		Clipboard = clipboard ?? throw new ArgumentNullException(nameof(clipboard));
 	}
 
 	public IClipboard Clipboard { get; }
 	public IQrCodeGenerator QrCodeGenerator { get; }
 
-	public static UIContext Default => new(new QrGenerator(), Application.Current?.Clipboard);
+	// The use of this property is a temporary workaround until we finalize the refactoring of all ViewModels (to be testable)
+	// Application.Current should never be null
+	public static UIContext Default => new(new QrGenerator(), Application.Current?.Clipboard!);
 }
