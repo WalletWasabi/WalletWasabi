e3445e6a0a588129bdd542e0d7d6c32b0ef7f4fc
ichthus1604 <ichthus1604@gmail.com>
2023-04-03 09:18:37 -0300

    Change NavigateDialogAsync() to the implementation

--- WalletWasabi.Fluent/ViewModels/Navigation/NavigationState.cs
+++ WalletWasabi.Fluent/ViewModels/Navigation/NavigationState.cs
@@ -1,6 +1,8 @@
 using ReactiveUI;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 using WalletWasabi.Fluent.Models.UI;
+using WalletWasabi.Fluent.ViewModels.Dialogs.Base;
 
 namespace WalletWasabi.Fluent.ViewModels.Navigation;
 
@@ -81,5 +83,11 @@ public class NavigationState : ReactiveObject, INavigate
 		}
 
 		page.IsActive = true;
+	}
+
+	public async Task<DialogResult<TResult>> NavigateDialogAsync<TResult>(DialogViewModelBase<TResult> dialog, NavigationTarget target = NavigationTarget.Default, NavigationMode navigationMode = NavigationMode.Normal)
+	{
+		target = NavigationExtensions.GetTarget(dialog, target);
+		return await Navigate(target).NavigateDialogAsync(dialog, navigationMode);
 	}
 }
