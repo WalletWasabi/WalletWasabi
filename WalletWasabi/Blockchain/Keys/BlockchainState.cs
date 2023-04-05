bd49753f799f80ca6021b4f529b3fec80026662d
Turbolay <clement.rouquier@gmail.com>
2023-04-05 02:22:19 -0600

    Whitespace
    Co-authored-by: Kimi <58662979+kiminuo@users.noreply.github.com>

--- WalletWasabi/Blockchain/Keys/BlockchainState.cs
+++ WalletWasabi/Blockchain/Keys/BlockchainState.cs
@@ -29,7 +29,7 @@ public class BlockchainState
 	}
 
 	[JsonProperty]
-	[JsonConverter(typeof(NetworkJsonConverter))]	
+	[JsonConverter(typeof(NetworkJsonConverter))]
 	public Network Network { get; set; }
 
 	[JsonProperty]
