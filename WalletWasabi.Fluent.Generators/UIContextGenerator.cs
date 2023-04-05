92cd8937f41349502dc4673f31d09433dd0a90d7
ichthus1604 <ichthus1604@gmail.com>
2023-04-05 02:37:33 -0300

    fix parameterless ctor based sourcegen

--- WalletWasabi.Fluent.Generators/UIContextGenerator.cs
+++ WalletWasabi.Fluent.Generators/UIContextGenerator.cs
@@ -107,7 +107,7 @@ public class UiContextGenerator : ISourceGenerator
 				var ctorString =
 					hasCtorArgs
 					? $": this({ctorArgsString})"
-					: "";
+					: $": this()";
 
 				var parameterUsings =
 					ctor.ParameterList.Parameters
