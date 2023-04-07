a08f031e99c26aeec87056ad1222cc08cb98b2d4
ichthus1604 <ichthus1604@gmail.com>
2023-04-07 14:11:27 -0300

    Part4 - NavBarViewModel

--- WalletWasabi.Fluent/ViewModels/NavBar/NavBarViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/NavBar/NavBarViewModel.cs
@@ -24,13 +24,31 @@ public partial class NavBarViewModel : ViewModelBase
 			.ObserveOn(RxApp.MainThreadScheduler)
 			.Do(x => x?.Activate())
 			.Subscribe();
+		this.WhenAnyValue(x => x.SelectedWallet)
+			.Buffer(2, 1)
+			.Select(buffer => (OldValue: buffer[0], NewValue: buffer[1]))
+			.ObserveOn(RxApp.MainThreadScheduler)
+			.Do(x =>
+			{
+				if (x.OldValue is { } a)
+				{
+					a.IsSelected = false;
+				}
+
+				if (x.NewValue is { } b)
+				{
+					b.IsSelected = true;
+					b.Activate();
+				}
+			})
+			.Subscribe();
 	}
 
 	public ObservableCollection<NavBarItemViewModel> BottomItems { get; }
 
 	public ObservableCollection<NavBarWalletStateViewModel> Wallets => UiServices.WalletManager.Wallets;
 
-	[AutoNotify] private NavBarWalletStateViewModel _selectedWallet;
+	[AutoNotify] private NavBarWalletStateViewModel? _selectedWallet;
 
 	private IObservable<NavBarItemViewModel> WhenItemSelected(IObservable<IChangeSet<NavBarItemViewModel>> observable)
 	{
@@ -42,7 +60,9 @@ public partial class NavBarViewModel : ViewModelBase
 
 	private void SetDefaultSelection()
 	{
-		var walletToSelect = Wallets.FirstOrDefault(item => item.Wallet.WalletName == Services.UiConfig.LastSelectedWallet) ?? Wallets.FirstOrDefault();
+		var walletToSelect =
+			Wallets.FirstOrDefault(item => item.Wallet.WalletName == Services.UiConfig.LastSelectedWallet)
+			?? Wallets.FirstOrDefault();
 		//
 		//if (walletToSelect is { } && walletToSelect.OpenCommand.CanExecute(default))
 		//{
