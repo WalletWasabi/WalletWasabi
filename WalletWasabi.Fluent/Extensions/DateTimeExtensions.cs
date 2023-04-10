3677b80f3ac67b7bb8f60d34eac31e6d6c0777fd
Yoh Deadfall <yoh.deadfall@hotmail.com>
2023-04-10 20:26:15 +0300

    Removed redundant semicolon

--- WalletWasabi.Fluent/Extensions/DateTimeExtensions.cs
+++ WalletWasabi.Fluent/Extensions/DateTimeExtensions.cs
@@ -2,6 +2,6 @@ namespace WalletWasabi.Fluent.Extensions;
 
 public static class DateTimeExtensions
 {
-	public static string ToUserFacingString(this DateTime value) => value.ToString("yyyy-MM-dd HH::mm");
+	public static string ToUserFacingString(this DateTime value) => value.ToString("yyyy-MM-dd HH:mm");
 	public static string ToUserFacingDateString(this DateTime value) => value.ToString("yyyy-MM-dd");
 }
