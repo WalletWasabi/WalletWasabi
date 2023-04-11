26a737a3f3f5258723b71c0f5eb26631447d9521
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-04-11 22:31:05 +0200

    Fix CF

--- WalletWasabi.Fluent/Controls/TagsBoxTopItemsPresenter.axaml.cs
+++ WalletWasabi.Fluent/Controls/TagsBoxTopItemsPresenter.axaml.cs
@@ -11,13 +11,16 @@ public class TagsBoxTopItemsPresenter : TemplatedControl
 	private IEnumerable<string>? _topItems;
 
 	public static readonly DirectProperty<TagsBoxTopItemsPresenter, IEnumerable<string>?> TopItemsProperty =
-		AvaloniaProperty.RegisterDirect<TagsBoxTopItemsPresenter, IEnumerable<string>?>(nameof(TopItems),
+		AvaloniaProperty.RegisterDirect<TagsBoxTopItemsPresenter, IEnumerable<string>?>(
+			nameof(TopItems),
 			o => o.TopItems,
 			(o, v) => o.TopItems = v);
 
 	public static readonly DirectProperty<TagsBoxTopItemsPresenter, ICommand?> AddTagCommandProperty =
-		AvaloniaProperty.RegisterDirect<TagsBoxTopItemsPresenter, ICommand?>(nameof(AddTagCommand),
-			o => o.AddTagCommand, (o, v) => o.AddTagCommand = v);
+		AvaloniaProperty.RegisterDirect<TagsBoxTopItemsPresenter, ICommand?>(
+			nameof(AddTagCommand),
+			o => o.AddTagCommand,
+			(o, v) => o.AddTagCommand = v);
 
 	public IEnumerable<string>? TopItems
 	{
