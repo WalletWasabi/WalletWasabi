6e56b06acbd79d3ea78e21b6aade36405143f4f8
ichthus1604 <ichthus1604@gmail.com>
2023-04-17 01:18:25 -0300

    Implement IComparable and IEquatable in IWalletModel

--- WalletWasabi.Fluent/Models/Wallets/IWalletModel.cs
+++ WalletWasabi.Fluent/Models/Wallets/IWalletModel.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using DynamicData;
 using NBitcoin;
 using WalletWasabi.Blockchain.Transactions;
+using WalletWasabi.Fluent.ViewModels.Wallets;
 using WalletWasabi.Fluent.ViewModels.Wallets.Labels;
 
 namespace WalletWasabi.Fluent.Models.Wallets;
@@ -10,7 +11,7 @@ namespace WalletWasabi.Fluent.Models.Wallets;
 /// This interface serves the purpose of enabling Mocks for unit testing of the ViewModels that consume it.
 /// It belongs to the Model part in the Model-View-ViewModel pattern
 /// </summary>
-public interface IWalletModel
+public interface IWalletModel : IEquatable<IWalletModel>, IComparable<IWalletModel>
 {
 	public string Name { get; }
 
@@ -25,4 +26,26 @@ public interface IWalletModel
 	IEnumerable<(string Label, int Score)> GetMostUsedLabels(Intent intent);
 
 	bool IsHardwareWallet();
+
+	bool IEquatable<IWalletModel>.Equals(IWalletModel? other)
+	{
+		return Name == other?.Name;
+	}
+
+	int IComparable<IWalletModel>.CompareTo(IWalletModel? other)
+	{
+		if (other is null)
+		{
+			return -1;
+		}
+
+		var result = other.IsLoggedIn.CompareTo(IsLoggedIn);
+
+		if (result == 0)
+		{
+			result = string.Compare(Name, other.Name, StringComparison.Ordinal);
+		}
+
+		return result;
+	}
 }
