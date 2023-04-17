a66286f56edd2c75d51411f60091b650c09b9e23
Yoh Deadfall <yoh.deadfall@hotmail.com>
2023-04-17 22:36:36 +0300

    Removed BigInteger

--- Dependencies/ZXing.Net/Source/lib/common/BigInteger/Sign.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BigIntegerLibrary
-{
-
-    /// <summary>
-    /// The number's sign, where Positive also stands for the number zero.
-    /// </summary>
-    enum Sign { Positive, Negative };
-
-}
