854cf5a49f05eb57111e16c9a5f06e2c30a937e0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-04-18 18:46:25 +0200

    Add initial Android project

---
+++ WalletWasabi.Fluent.Android/MainActivity.cs
@@ -0,0 +1,10 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia.Android;
+
+namespace WalletWasabi.Fluent.Android;
+
+[Activity(Label = "Wasabi Wallet", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+public class MainActivity : AvaloniaMainActivity
+{
+}
---
+++ WalletWasabi.Fluent.Android/SplashActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Avalonia;
+using Avalonia.Android;
+using Avalonia.Fonts.Inter;
+using Application = Android.App.Application;
+
+namespace WalletWasabi.Fluent.Android;
+
+[Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+public class SplashActivity : AvaloniaSplashActivity<App>
+{
+	protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont()
+            .AfterSetup(_ =>
+            {
+	            // TODO;
+            });
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+
+        StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        Finish();
+    }
+}
