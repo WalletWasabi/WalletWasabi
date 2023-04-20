bfecce9369d677a8e868d74fa5e01966806118ee
Yoh Deadfall <yoh.deadfall@hotmail.com>
2023-04-20 22:17:29 +0300

    Predictable resource management

--- WalletWasabi.Fluent/Models/UI/QrCodeReader.cs
+++ WalletWasabi.Fluent/Models/UI/QrCodeReader.cs
@@ -7,6 +7,8 @@ using System.Reactive.Threading.Tasks;
 using System.Runtime.InteropServices;
 using ZXing.QrCode;
 using SkiaSharp;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WalletWasabi.Fluent.Models.UI;
 
@@ -20,28 +22,34 @@ public class QrCodeReader : IQrCodeReader
 
 	public IObservable<(string decoded, Bitmap bitmap)> Read()
 	{
-		return new CaptureDevices()
-			.EnumerateDescriptors()
-			.ToObservable()
-			.Skip(1)
-			.SelectMany(static d => d.Characteristics, static (d, c) => new { d, c })
-			.FirstOrDefaultAsync()
-			.Select(static d => d ?? throw new InvalidOperationException("Could not find a device."))
-			.Select(static d => d.d
-				.AsObservableAsync(d.c)
-				.ToObservable()
-				.Select(static d => d
-					.StartAsync()
-					.ToObservable()
-					.CombineLatest(d, static (_, scope) => scope))
-				.Switch())
-			.Switch()
-			.Select(scope =>
+		return Observable.Create(
+			async (IObserver<(string, Bitmap)> result, CancellationToken ct) =>
 			{
-				var decoded = Decode(scope);
-				var bitmap = new Bitmap(scope.Buffer.ReferImage().AsStream());
+				var devices = new CaptureDevices();
+				var device = devices
+					.EnumerateDescriptors()
+					.SelectMany(static d => d.Characteristics, static (d, c) => new { d, c })
+					.FirstOrDefault() ?? throw new InvalidOperationException("Could not find a device.");
 
-				return (decoded, bitmap);
+				await using var capture = await device.d
+					.OpenAsync(
+						device.c,
+						ct: ct,
+						pixelBufferArrived: scope =>
+						{
+							var decoded = Decode(scope);
+							var bitmap = new Bitmap(scope.Buffer.ReferImage().AsStream());
+
+							result.OnNext((decoded, bitmap));
+						})
+					.ConfigureAwait(false);
+
+				var tcs = new TaskCompletionSource<object?>();
+
+				ct.Register(() => tcs.TrySetResult(default));
+
+				await capture.StartAsync(ct).ConfigureAwait(false);
+				await tcs.Task.ConfigureAwait(false);
 			});
 	}
 
