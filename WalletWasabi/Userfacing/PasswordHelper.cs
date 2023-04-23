3ed6086611413ee9e18b38d5999fb91f1b289f47
Yahia Chiheb <chihebyahia@gmail.com>
2023-04-24 00:58:36 +0100

    Fix CodeFactor

--- WalletWasabi/Userfacing/PasswordHelper.cs
+++ WalletWasabi/Userfacing/PasswordHelper.cs
@@ -14,10 +14,10 @@ public static class PasswordHelper
 {
 	public const int MaxPasswordLength = 150;
 	public const string CompatibilityPasswordWarnMessage = "Compatibility password was used! Please consider generating a new wallet to ensure recoverability!";
-	public static readonly string PasswordTooLongMessage = $"Password is too long.";
 	public const string TrimWarnMessage = "Leading and trailing white spaces will be removed!";
 	public const string MatchingMessage = "Passwords don't match.";
 	public const string WhitespaceMessage = "Leading and trailing white spaces are not allowed!";
+	public static readonly string PasswordTooLongMessage = $"Password is too long.";
 
 	public static string[] GetPossiblePasswords(string? originalPassword)
 	{
