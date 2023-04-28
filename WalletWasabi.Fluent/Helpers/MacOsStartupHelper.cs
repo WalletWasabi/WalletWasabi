af3d92ffaa78977022e85fe5ab9536e3a33b103b
Szpoti <balage.farago@gmail.com>
2023-04-28 15:01:14 +0200

    Remove redundant parameter

--- WalletWasabi.Fluent/Helpers/MacOsStartupHelper.cs
+++ WalletWasabi.Fluent/Helpers/MacOsStartupHelper.cs
@@ -5,8 +5,8 @@ namespace WalletWasabi.Fluent.Helpers;
 
 public static class MacOsStartupHelper
 {
-	private static readonly string AddCmd = $"osascript -e \' tell application \"System Events\" to make new login item at end with properties {{name:\"{Constants.AppName}\", path:\"/Applications/{Constants.AppName}.app/Contents/MacOS/Microservices/Binaries/osx64/{Constants.SilentExecutableName}.app\", hidden:true}} \'";
-	private static readonly string DeleteCmd = $"osascript -e \' tell application \"System Events\" to delete login item \"{Constants.AppName}\" \'";
+	private static readonly string AddCmd = $"osascript -e \' tell application \"System Events\" to make new login item at end with properties {{path:\"/Applications/{Constants.AppName}.app/Contents/MacOS/Microservices/Binaries/osx64/{Constants.SilentExecutableName}.app\", hidden:true}} \'";
+	private static readonly string DeleteCmd = $"osascript -e \' tell application \"System Events\" to delete login item \"{Constants.SilentExecutableName}\" \'";
 
 	public static async Task AddOrRemoveLoginItemAsync(bool runOnSystemStartup)
 	{
