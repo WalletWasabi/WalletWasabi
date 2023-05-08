878e2cbad88f2f8cba854f920c21b0b27cf55783
ichthus1604 <ichthus1604@gmail.com>
2023-05-08 15:07:07 -0300

    fix tests compilation

--- WalletWasabi.Tests/UnitTests/ViewModels/SuggestionLabelsViewModelTests.cs
+++ WalletWasabi.Tests/UnitTests/ViewModels/SuggestionLabelsViewModelTests.cs
@@ -163,6 +163,10 @@ public class SuggestionLabelsViewModelTests
 
 		public WalletType WalletType => throw new NotImplementedException();
 
+		public IWalletAuthModel Auth => throw new NotImplementedException();
+
+		public IWalletLoadWorkflow Loader => throw new NotImplementedException();
+
 		public IAddress GetNextReceiveAddress(IEnumerable<string> destinationLabels)
 		{
 			throw new NotSupportedException();
