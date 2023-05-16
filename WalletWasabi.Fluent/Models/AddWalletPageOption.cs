3dc16c7255994026c13d94d8abd26cb1708656c0
Yoh Deadfall <yoh.deadfall@hotmail.com>
2023-05-16 16:41:26 +0300

    Fixed namespace

--- WalletWasabi.Fluent/Models/AddWalletPageOption.cs
+++ WalletWasabi.Fluent/Models/AddWalletPageOption.cs
@@ -1,7 +1,7 @@
 using ReactiveUI;
 using WalletWasabi.Fluent.Models;
 
-namespace WalletWasabi.Fluent.Models.AddWallet;
+namespace WalletWasabi.Fluent.Models;
 
 public partial class AddWalletPageOption : ReactiveObject
 {
