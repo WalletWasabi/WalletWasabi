062f089d5105b9c26458daa228a817c0f3a34555
ichthus1604 <ichthus1604@gmail.com>
2023-05-18 13:01:57 -0300

    Ignore namespace mismatch warning for NavBar types

--- WalletWasabi.Fluent/ViewModels/NavBar/INavBarItem.cs
+++ WalletWasabi.Fluent/ViewModels/NavBar/INavBarItem.cs
@@ -1,3 +1,5 @@
+#pragma warning disable IDE0130 // Namespace does not match folder structure (see https://github.com/zkSNACKs/WalletWasabi/pull/10576#issuecomment-1552750543)
+
 using System.ComponentModel;
 
 namespace WalletWasabi.Fluent;
