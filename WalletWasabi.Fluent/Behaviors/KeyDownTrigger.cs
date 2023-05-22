fa3e030d64758475d723dc7e9492a563e6cdaf51
José Manuel Nieto <superjmn@outlook.com>
2023-05-22 11:23:44 +0200

    Use single disposable

--- WalletWasabi.Fluent/Behaviors/KeyDownTrigger.cs
+++ WalletWasabi.Fluent/Behaviors/KeyDownTrigger.cs
@@ -14,7 +14,7 @@ public class KeyDownTrigger : Trigger
 	public static readonly StyledProperty<RoutingStrategies> EventRoutingStrategyProperty = AvaloniaProperty.Register<KeyDownTrigger, RoutingStrategies>(nameof(EventRoutingStrategy));
 
 	public static readonly StyledProperty<Key> KeyProperty = AvaloniaProperty.Register<KeyDownTrigger, Key>(nameof(Key));
-	private readonly CompositeDisposable _disposables = new();
+	private IDisposable _subscription = Disposable.Empty;
 
 	public RoutingStrategies EventRoutingStrategy
 	{
@@ -34,18 +34,17 @@ public class KeyDownTrigger : Trigger
 
 		if (AssociatedObject is InputElement element)
 		{
-			this.WhenAnyValue(x => x.EventRoutingStrategy, x => x.Key, (strategy, key) => new { Key = key, RoutingStrategy = strategy })
+			_subscription = this.WhenAnyValue(x => x.EventRoutingStrategy, x => x.Key, (strategy, key) => new { Key = key, RoutingStrategy = strategy })
 				.Select(args => element.OnEvent(InputElement.KeyDownEvent, args.RoutingStrategy).Where(x => x.EventArgs.Key == args.Key))
 				.Switch()
 				.Do(_ => Interaction.ExecuteActions(AssociatedObject, Actions, null))
-				.Subscribe()
-				.DisposeWith(_disposables);
+				.Subscribe();
 		}
 	}
 
 	protected override void OnDetaching()
 	{
 		base.OnDetaching();
-		_disposables.Dispose();
+		_subscription.Dispose();
 	}
 }
