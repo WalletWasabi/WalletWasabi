6dd658051a74ef4474659ea8a4a0b7f64f218f83
José Manuel Nieto <superjmn@outlook.com>
2023-05-22 11:12:05 +0200

    Remove unused class

--- WalletWasabi/Blockchain/Transactions/Summary/Feature.cs
+++
@@ -1,8 +0,0 @@
-namespace WalletWasabi.Blockchain.Transactions.Summary;
-
-public record Feature(string Name)
-{
-	public static Feature Taproot = new("Taproot");
-	public static Feature RBF = new("RBF");
-	public static Feature SegWit = new("SegWit");
-}
