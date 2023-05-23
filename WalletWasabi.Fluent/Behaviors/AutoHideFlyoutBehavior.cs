34bc27f2a2c16433e08b7f90ba71c191ff6fc68d
Yoh Deadfall <yoh.deadfall@hotmail.com>
2023-05-23 20:41:47 +0300

    Close flyouts on window focus lose or resize

---
+++ WalletWasabi.Fluent/Behaviors/AutoHideFlyoutBehavior.cs
@@ -0,0 +1,27 @@
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+public class AutoHideFlyoutBehavior : AttachedFlyoutBehavior<Window>
+{
+	protected override void OnAttached(CompositeDisposable disposables)
+	{
+		base.OnAttached(disposables);
+		
+		AssociatedObject!
+			.WhenAnyValue(x => x.IsActive, x => x.IsPointerOver, (isActive, isPointerOver) => !isActive && !isPointerOver)
+			.Where(x => x)
+			.Subscribe(_ => CloseFlyouts())
+			.DisposeWith(disposables);
+
+		Observable
+			.FromEventPattern<PixelPointEventArgs>(
+				handler => AssociatedObject!.PositionChanged += handler,
+				handler => AssociatedObject!.PositionChanged -= handler)
+			.Subscribe(_ => AssociatedObject?.Focus())
+			.DisposeWith(disposables);
+	}
+}
