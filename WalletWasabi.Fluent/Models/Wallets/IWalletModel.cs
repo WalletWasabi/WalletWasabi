ccdadfaafa32e7e3ed0e250c001cfcc452129aae
ichthus1604 <ichthus1604@gmail.com>
2023-05-31 15:09:26 -0300

    Remove IComparable and IEquatable from IWalletModel

--- WalletWasabi.Fluent/Models/Wallets/IWalletModel.cs
+++ WalletWasabi.Fluent/Models/Wallets/IWalletModel.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using DynamicData;
 using NBitcoin;
 using WalletWasabi.Blockchain.Transactions;
-using WalletWasabi.Fluent.ViewModels.Wallets;
 using WalletWasabi.Fluent.ViewModels.Wallets.Labels;
 using WalletWasabi.Wallets;
 
@@ -13,7 +12,7 @@ namespace WalletWasabi.Fluent.Models.Wallets;
 /// This interface serves the purpose of enabling Mocks for unit testing of the ViewModels that consume it.
 /// It belongs to the Model part in the Model-View-ViewModel pattern
 /// </summary>
-public interface IWalletModel : IEquatable<IWalletModel>, IComparable<IWalletModel>
+public interface IWalletModel
 {
 	public string Name { get; }
 
@@ -40,26 +39,4 @@ public interface IWalletModel : IEquatable<IWalletModel>, IComparable<IWalletMod
 	void Logout();
 
 	IEnumerable<(string Label, int Score)> GetMostUsedLabels(Intent intent);
-
-	bool IEquatable<IWalletModel>.Equals(IWalletModel? other)
-	{
-		return Name == other?.Name;
-	}
-
-	int IComparable<IWalletModel>.CompareTo(IWalletModel? other)
-	{
-		if (other is null)
-		{
-			return -1;
-		}
-
-		var result = other.IsLoggedIn.CompareTo(IsLoggedIn);
-
-		if (result == 0)
-		{
-			result = string.Compare(Name, other.Name, StringComparison.Ordinal);
-		}
-
-		return result;
-	}
 }
