8e710d819a5e2f757e181d6c5ecad9128fbf55fa
Lucas Ontivero <lucasontivero@gmail.com>
2023-06-05 09:57:58 -0300

    Do not pay too much
    Do not pay too much more than what is necessary for confirming in 2 blocks.

--- WalletWasabi/Extensions/RPCClientExtensions.cs
+++ WalletWasabi/Extensions/RPCClientExtensions.cs
@@ -82,7 +82,9 @@ public static class RPCClientExtensions
 
 		var minEstimations = GetFeeEstimationsFromMempoolInfo(mempoolInfo);
 		var minEstimationFor260Mb = new FeeRate((decimal)minEstimations.GetValueOrDefault(260 / 4));
-		var sanityFeeRate = FeeRate.Max(minEstimationFor260Mb, mempoolInfo.GetSanityFeeRate());
+		var minSanityFeeRate = FeeRate.Max(minEstimationFor260Mb, mempoolInfo.GetSanityFeeRate());
+		var maxEstimationFor3Mb = new FeeRate((decimal)minEstimations.GetValueOrDefault(2));
+		var maxSanityFeeRate = maxEstimationFor3Mb;
 			
 		var fixedEstimations = smartEstimations
 			.GroupJoin(
@@ -92,9 +94,15 @@ public static class RPCClientExtensions
 				(outer, inner) => new { Estimation = outer, MinimumFromMemPool = inner })
 			.SelectMany(
 				x => x.MinimumFromMemPool.DefaultIfEmpty(),
-				(a, b) => (
-					Target: a.Estimation.Key,
-					FeeRate: Math.Max((int)sanityFeeRate.SatoshiPerByte, Math.Max(a.Estimation.Value, b.Value))))
+				(a, b) =>
+				{
+					var maxLowerBound = Math.Max(a.Estimation.Value, b.Value);
+					var maxMinFeeRate = Math.Max((int) minSanityFeeRate.SatoshiPerByte, maxLowerBound);
+					var minMaxFeeRate = Math.Min((int) maxSanityFeeRate.SatoshiPerByte, maxLowerBound);
+					return (
+						Target: a.Estimation.Key,
+						FeeRate: minMaxFeeRate);
+				})
 			.ToDictionary(x => x.Target, x => x.FeeRate);
 
 		return new AllFeeEstimate(
