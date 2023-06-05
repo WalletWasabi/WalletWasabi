563ad5dccec6c8479f3d3c10c5ee34adc4575864
Roland Soós <soos.roland93@gmail.com>
2023-06-05 13:34:59 +0200

    format

--- WalletWasabi.Fluent/Models/Wallets/IWalletModel.cs
+++ WalletWasabi.Fluent/Models/Wallets/IWalletModel.cs
@@ -24,6 +24,7 @@ public interface IWalletModel
 	IObservable<IChangeSet<TransactionSummary, uint256>> Transactions { get; }
 
 	IObservable<IChangeSet<IAddress, string>> Addresses { get; }
+
 	IWalletBalancesModel Balances { get; }
 
 	bool IsHardwareWallet { get; }
