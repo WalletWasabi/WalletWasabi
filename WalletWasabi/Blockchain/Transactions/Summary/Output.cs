420309f9daca57cdd2217fe0854cb3266264f7a8
José Manuel Nieto <superjmn@outlook.com>
2023-06-15 16:48:27 +0200

    Format

--- WalletWasabi/Blockchain/Transactions/Summary/Output.cs
+++ WalletWasabi/Blockchain/Transactions/Summary/Output.cs
@@ -2,16 +2,6 @@ using NBitcoin;
 
 namespace WalletWasabi.Blockchain.Transactions.Summary;
 
-public class OwnOutput : Output
-{
-	public bool IsInternal { get; }
-
-	public OwnOutput(Money amount, BitcoinAddress destinationAddress, bool isInternal) : base(amount, destinationAddress)
-	{
-		IsInternal = isInternal;
-	}
-}
-
 public abstract class Output
 {
 	protected Output(Money amount, BitcoinAddress destinationAddress)
@@ -21,12 +11,6 @@ public abstract class Output
 	}
 
 	public Money Amount { get; }
-	public BitcoinAddress DestinationAddress { get; }
-}
 
-public class ForeignOutput : Output
-{
-	public ForeignOutput(Money amount, BitcoinAddress destinationAddress) : base(amount, destinationAddress)
-	{
-	}
+	public BitcoinAddress DestinationAddress { get; }
 }
