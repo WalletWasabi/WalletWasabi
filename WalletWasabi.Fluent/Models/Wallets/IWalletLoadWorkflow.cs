334b706203265adacde3d9b4f07f617c8d962499
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-06-16 09:29:38 +0200

    Fix CF

--- WalletWasabi.Fluent/Models/Wallets/IWalletLoadWorkflow.cs
+++ WalletWasabi.Fluent/Models/Wallets/IWalletLoadWorkflow.cs
@@ -5,8 +5,12 @@ namespace WalletWasabi.Fluent.Models.Wallets;
 public interface IWalletLoadWorkflow
 {
 	bool IsLoading { get; }
+
 	IObservable<(double PercentComplete, TimeSpan TimeRemaining)> Progress { get; }
+
 	IObservable<Unit> LoadCompleted { get; }
+
 	void Start();
+
 	void Stop();
 }
