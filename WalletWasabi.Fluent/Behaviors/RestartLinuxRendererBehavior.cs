9bc9fb47856d03c67e37bcf7e03b7c6f774fb690
José Manuel Nieto <superjmn@outlook.com>
2023-06-22 11:30:17 +0200

    Move logic to Behavior

---
+++ WalletWasabi.Fluent/Behaviors/RestartLinuxRendererBehavior.cs
@@ -0,0 +1,29 @@
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+// TODO: This is a workaround for this Avalonia issue (https://github.com/AvaloniaUI/Avalonia/issues/11850)
+// Please, remove this Behavior and it's unique usage after it's been fixed.
+public class RestartRendererBehavior : AttachedToVisualTreeBehavior<Window>
+{
+	protected override void OnAttachedToVisualTree(CompositeDisposable disposable)
+	{
+		if (AssociatedObject is null)
+		{
+			return;
+		}
+
+		this.WhenAnyValue(x => x.AssociatedObject!.WindowState)
+			.Where(state => state == WindowState.Normal)
+			.Where(_ => OperatingSystem.IsLinux())
+			.Do(_ =>
+			{
+				AssociatedObject.Renderer.Start();
+				AssociatedObject.Activate();
+			})
+			.Subscribe();
+	}
+}
--- WalletWasabi.Fluent/Views/MainWindow.axaml.cs
+++ WalletWasabi.Fluent/Views/MainWindow.axaml.cs
@@ -1,8 +1,6 @@
-using System.Reactive.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using ReactiveUI;
 using WalletWasabi.Fluent.Diagnostics;
 using WalletWasabi.Fluent.Screenshot;
 
@@ -13,23 +11,6 @@ public class MainWindow : Window
 	public MainWindow()
 	{
 		InitializeComponent();
-
-		// TODO: This is a workaround for this Avalonia issue (https://github.com/AvaloniaUI/Avalonia/issues/11850)
-		// Please, remove after it's been fixed.
-		ApplyRendererWorkaround();
-	}
-
-	private void ApplyRendererWorkaround()
-	{
-		this.WhenAnyValue(x => x.WindowState)
-			.Where(state => state == WindowState.Normal)
-			.Where(_ => OperatingSystem.IsLinux())
-			.Do(_ =>
-			{
-				Renderer.Start();
-				Activate();
-			})
-			.Subscribe();
 	}
 
 	private void InitializeComponent()
