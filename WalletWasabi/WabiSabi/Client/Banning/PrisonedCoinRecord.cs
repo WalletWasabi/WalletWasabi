b6ac2e5c3ec5e8407891c0e04a8ba2b38e88f006
Szpoti <balage.farago@gmail.com>
2023-06-29 18:19:39 +0200

    Add CoinPrison and related record to hold data

---
+++ WalletWasabi/WabiSabi/Client/Banning/PrisonedCoinRecord.cs
@@ -0,0 +1,21 @@
+using NBitcoin;
+using Newtonsoft.Json;
+using WalletWasabi.Affiliation.Models.CoinJoinNotification;
+using WalletWasabi.JsonConverters;
+
+namespace WalletWasabi.WabiSabi.Client.Banning;
+
+public record PrisonedCoinRecord
+{
+	public PrisonedCoinRecord(OutPoint outpoint, DateTimeOffset bannedUntil)
+	{
+		Outpoint = outpoint;
+		BannedUntil = bannedUntil;
+	}
+
+	[JsonProperty]
+	[JsonConverter(typeof(OutPointJsonConverter))]
+	public OutPoint Outpoint { get; set; }
+
+	public DateTimeOffset BannedUntil { get; set; }
+}
