a4b91c9ae2eb5383642aaf014e2ddecc67c403e2
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2023-07-07 17:33:47 +0200

    WSCL/SelectInputsForRound: Introduce min reasonable output amount

--- WalletWasabi.WabiSabiClientLibrary/Controllers/Helpers/SelectInputsForRoundHelper.cs
+++ WalletWasabi.WabiSabiClientLibrary/Controllers/Helpers/SelectInputsForRoundHelper.cs
@@ -6,6 +6,7 @@ using WabiSabi.Crypto.Randomness;
 using WalletWasabi.Blockchain.TransactionOutputs;
 using WalletWasabi.WabiSabi.Backend.Rounds;
 using WalletWasabi.WabiSabi.Client;
+using WalletWasabi.WabiSabi.Models;
 using WalletWasabi.WabiSabiClientLibrary.Models;
 using WalletWasabi.WabiSabiClientLibrary.Models.SelectInputsForRound;
 
@@ -15,7 +16,8 @@ public class SelectInputsForRoundHelper
 {
 	public static SelectInputsForRoundResponse SelectInputsForRound(SelectInputsForRoundRequest request, WasabiRandom random)
 	{
-		UtxoSelectionParameters utxoSelectionParameters = new(request.AllowedInputAmounts, request.AllowedOutputAmounts, request.CoordinationFeeRate, request.MiningFeeRate, request.AllowedInputTypes.ToImmutableSortedSet());
+		MoneyRange reasonableOutputAmountRange = new(RoundParameters.CalculateSmallestReasonableEffectiveDenomination(request.AllowedOutputAmounts.Min, request.AllowedOutputAmounts.Max, request.MiningFeeRate, ScriptType.Taproot, random), request.AllowedOutputAmounts.Max);
+		UtxoSelectionParameters utxoSelectionParameters = new(request.AllowedInputAmounts, reasonableOutputAmountRange, request.CoordinationFeeRate, request.MiningFeeRate, request.AllowedInputTypes.ToImmutableSortedSet());
 		CoinJoinCoinSelectorRandomnessGenerator randomnessGenerator = new(CoinJoinCoinSelector.MaxInputsRegistrableByWallet, random);
 		IEnumerable<Utxo> utxos = request.DoNotSelectPrivateCoins ? request.Utxos.Where(x => x.AnonymitySet < request.AnonScoreTarget) : request.Utxos;
 		CoinJoinCoinSelector coinJoinCoinSelector = new(request.ConsolidationMode, request.AnonScoreTarget, request.SemiPrivateThreshold, randomnessGenerator);
@@ -26,7 +28,7 @@ public class SelectInputsForRoundHelper
 			.ToDictionary(x => x.Item1, x => x.i);
 
 		// Find corresponding indices for the found coins.
-		int[] indices = coins.Select(c => coinIndices[c]).ToArray();
+		int[] indices = coins.Select(c => coinIndices[c]).Order().ToArray();
 
 		return new SelectInputsForRoundResponse(indices);
 	}
