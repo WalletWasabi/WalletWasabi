f80515b95a8bf2242ae4956d33fdad8fd7e23b7a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-07-17 10:25:06 +0200

    Fix FadeOutTextBlock

--- WalletWasabi.Fluent/Controls/FadeOutTextBlock.cs
+++ WalletWasabi.Fluent/Controls/FadeOutTextBlock.cs
@@ -1,27 +1,11 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
-using Avalonia.Media.TextFormatting;
-using Avalonia.Styling;
-using System.Reactive.Disposables;
-using Avalonia.Layout;
 
 namespace WalletWasabi.Fluent.Controls;
 
 public class FadeOutTextBlock : TextBlock
 {
-	private TextLayout? _trimmedLayout;
- 	private bool _cutOff;
-    private TextLayout? _noTrimLayout;
-    private Size _constraint;
-
-	public FadeOutTextBlock()
-	{
-		TextWrapping = TextWrapping.NoWrap;
-	}
-
-	protected override Type StyleKeyOverride => typeof(TextBlock);
-
 	private static readonly IBrush FadeoutOpacityMask = new LinearGradientBrush
 	{
 		StartPoint = new RelativePoint(0, 0, RelativeUnit.Relative),
@@ -34,123 +18,26 @@ public class FadeOutTextBlock : TextBlock
 		}
 	}.ToImmutable();
 
+	internal TextBlock? TrimmedTextBlock { get; set; }
+
 	protected override void RenderTextLayout(DrawingContext context, Point origin)
 	{
-		var bounds = Bounds;
-
-		if (_trimmedLayout is null || _noTrimLayout is null)
+		if (TrimmedTextBlock is null)
 		{
-			return;
+			base.RenderTextLayout(context, origin);
 		}
-
-		var width = bounds.Size.Width;
-
-		var centerOffset = TextAlignment switch
+		else
 		{
-			TextAlignment.Center => (width - _trimmedLayout.Width) / 2.0,
-			TextAlignment.Right => width - _trimmedLayout.Width,
-			_ => 0.0
-		};
-
-		var (left, yPosition, _, _) = Padding;
-
-		using var a = context.PushTransform(Matrix.CreateTranslation(left, yPosition));
-		using var b = _cutOff ? context.PushOpacityMask(FadeoutOpacityMask, bounds) : Disposable.Empty;
-		_noTrimLayout.Draw(context, origin + new Point(_noTrimLayout.OverhangLeading, 0));
-	}
-	private readonly record struct SimpleTextSource : ITextSource
-	{
-		private readonly string _text;
-		private readonly TextRunProperties _defaultProperties;
-
-		public SimpleTextSource(string text, TextRunProperties defaultProperties)
-		{
-			_text = text;
-			_defaultProperties = defaultProperties;
-		}
-
-		public TextRun? GetTextRun(int textSourceIndex)
-		{
-			if (textSourceIndex > _text.Length)
+			var hasCollapsed = TrimmedTextBlock.TextLayout.TextLines[0].HasCollapsed;
+			if (hasCollapsed)
 			{
-				return new TextEndOfParagraph();
+				using var _ = context.PushOpacityMask(FadeoutOpacityMask, Bounds);
+				TextLayout.Draw(context, origin + new Point(TextLayout.OverhangLeading, 0));
 			}
-
-			var runText = _text.AsMemory(textSourceIndex);
-
-			if (runText.IsEmpty)
+			else
 			{
-				return new TextEndOfParagraph();
+				TextLayout.Draw(context, origin + new Point(TextLayout.OverhangLeading, 0));
 			}
-
-			return new TextCharacters(runText, _defaultProperties);
 		}
-	}
-	private void NewCreateTextLayout(Size constraint, string? text)
-	{
-		if (constraint == default)
-		{
-			_trimmedLayout = null;
-		}
-
-		var text1 = text ?? "";
-
-		var typeface = new Typeface(FontFamily, FontStyle, FontWeight, FontStretch);
-
-		var defaultProperties = new GenericTextRunProperties(
-			typeface,
-			FontSize,
-			TextDecorations,
-			Foreground);
-
-		var paragraphProperties = new GenericTextParagraphProperties(FlowDirection, TextAlignment, true, false,
-			defaultProperties, TextWrapping, LineHeight, 0, LetterSpacing);
-
-		var textSource = new SimpleTextSource(text1 ?? "", defaultProperties);
-
-		_noTrimLayout = new TextLayout(
-			textSource,
-			paragraphProperties,
-			TextTrimming.None,
-			_constraint.Width,
-			_constraint.Height,
-			MaxLines);
-
-		_trimmedLayout = new TextLayout(
-			textSource,
-			paragraphProperties,
-			TextTrimming.CharacterEllipsis,
-			_constraint.Width,
-			_constraint.Height,
-			MaxLines);
-
-		_cutOff = _trimmedLayout.TextLines[0].HasCollapsed;
-	}
-
-	protected override Size MeasureOverride(Size availableSize)
-	{
-		var scale = LayoutHelper.GetLayoutScale(this);
-		var padding = LayoutHelper.RoundLayoutThickness(Padding, scale, scale);
-
-		_constraint = availableSize.Deflate(padding);
-
-		if (string.IsNullOrEmpty(Text))
-		{
-			return new Size();
-		}
-
-		var sizeNoTrim = _noTrimLayout is null ? default : new Size(_noTrimLayout.Width, _noTrimLayout.Height);
-
-		if (availableSize != sizeNoTrim)
-		{
-			_constraint = availableSize;
-			NewCreateTextLayout(_constraint, Text);
-		}
-
-		var sizeTrimmed = _trimmedLayout is null ? default : new Size(_trimmedLayout.Width, _trimmedLayout.Height);
-
-		OpacityMask = _cutOff ? FadeoutOpacityMask : Brushes.White;
-
-		return sizeTrimmed.Inflate(padding);
 	}
 }
