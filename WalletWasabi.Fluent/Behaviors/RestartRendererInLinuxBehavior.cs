75ac97afc658d3259caa8f9a1a0d9b1c7b64fc7e
Roland Soós <soos.roland93@gmail.com>
2023-07-17 12:36:36 +0200

    rename

--- WalletWasabi.Fluent/Behaviors/RestartRendererInLinuxBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/RestartRendererInLinuxBehavior.cs
@@ -1,14 +1,13 @@
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Avalonia.Controls;
-using Avalonia.Xaml.Interactivity;
 using ReactiveUI;
 
 namespace WalletWasabi.Fluent.Behaviors;
 
 // TODO: This is a workaround for this Avalonia issue (https://github.com/AvaloniaUI/Avalonia/issues/11850)
 // Please, remove this Behavior and it's unique usage after it's been fixed.
-public class RestartRendererBehavior : DisposingBehavior<Window>
+public class RestartRendererInLinuxBehavior : DisposingBehavior<Window>
 {
 	protected override void OnAttached(CompositeDisposable disposables)
 	{
