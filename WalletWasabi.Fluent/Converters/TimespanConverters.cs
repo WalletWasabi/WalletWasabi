e16d1d7514b10c776c280756718228cc129927f9
José Manuel Nieto <superjmn@outlook.com>
2023-07-24 14:25:43 +0200

    Remove TimeSpanConverters

--- WalletWasabi.Fluent/Converters/TimespanConverters.cs
+++
@@ -1,9 +0,0 @@
-using Avalonia.Data.Converters;
-using WalletWasabi.Fluent.Helpers;
-
-namespace WalletWasabi.Fluent.Converters;
-
-public static class TimespanConverters
-{
-	public static readonly IValueConverter ToFriendlyString = new FuncValueConverter<TimeSpan, string>(TextHelpers.TimeSpanToFriendlyString);
-}
