3a52b03db9d5ffc30b1d2113c3a21bf6cd5db01e
adamPetho <45069029+adamPetho@users.noreply.github.com>
2023-07-25 11:45:25 +0200

    update hwi version in Constants.cs

--- WalletWasabi/Helpers/Constants.cs
+++ WalletWasabi/Helpers/Constants.cs
@@ -75,7 +75,7 @@ public static class Constants
 
 	public static readonly Version ClientVersion = new(2, 0, 3, 0);
 
-	public static readonly Version HwiVersion = new("2.2.1");
+	public static readonly Version HwiVersion = new("2.3.0");
 	public static readonly Version BitcoinCoreVersion = new("21.2");
 	public static readonly Version Ww1LegalDocumentsVersion = new(3, 0);
 	public static readonly Version Ww2LegalDocumentsVersion = new(1, 0);
