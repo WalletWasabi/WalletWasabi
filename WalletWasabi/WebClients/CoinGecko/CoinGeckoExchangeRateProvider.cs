117442cb6f3fe6b48afa7e872c48637c517fda21
Lucas Ontivero <lucasontivero@gmail.com>
2023-07-31 11:43:32 -0300

    Revert "remove coingecko, their api is not public anymore"
    This reverts commit 50fb403a0cc016f0d79c35ae3126243c0b178276.

---
+++ WalletWasabi/WebClients/CoinGecko/CoinGeckoExchangeRateProvider.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using WalletWasabi.Backend.Models;
+using WalletWasabi.Interfaces;
+using WalletWasabi.Tor.Http.Extensions;
+
+namespace WalletWasabi.WebClients.CoinGecko;
+
+public class CoinGeckoExchangeRateProvider : IExchangeRateProvider
+{
+	public async Task<IEnumerable<ExchangeRate>> GetExchangeRateAsync(CancellationToken cancellationToken)
+	{
+		using var httpClient = new HttpClient
+		{
+			BaseAddress = new Uri("https://api.coingecko.com")
+		};
+		using var response = await httpClient.GetAsync("api/v3/coins/markets?vs_currency=usd&ids=bitcoin", cancellationToken).ConfigureAwait(false);
+		using var content = response.Content;
+		var rates = await content.ReadAsJsonAsync<CoinGeckoExchangeRate[]>().ConfigureAwait(false);
+
+		var exchangeRates = new List<ExchangeRate>
+			{
+				new ExchangeRate { Rate = rates[0].Rate, Ticker = "USD" }
+			};
+
+		return exchangeRates;
+	}
+}
