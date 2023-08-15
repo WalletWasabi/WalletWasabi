3c349b9e5d656d2f982873ab136905d3f38c862c
Roland Soós <soos.roland93@gmail.com>
2023-08-15 15:38:23 +0200

    do not bind to model

--- WalletWasabi.Fluent/ViewModels/Wallets/Advanced/WalletInfoViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Advanced/WalletInfoViewModel.cs
@@ -15,13 +15,15 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Advanced;
 	NavigationTarget = NavigationTarget.DialogScreen)]
 public partial class WalletInfoViewModel : RoutableViewModel
 {
+	private readonly IWalletInfoModel _model;
+
 	[AutoNotify] private bool _showSensitiveData;
 	[AutoNotify] private string _showButtonText = "Show sensitive data";
 	[AutoNotify] private string _lockIconString = "eye_show_regular";
 
 	private WalletInfoViewModel(IWalletModel wallet)
 	{
-		Model = wallet.GetWalletInfo();
+		_model = wallet.GetWalletInfo();
 		IsHardwareWallet = wallet.IsHardwareWallet;
 
 		SetupCancel(enableCancel: true, enableCancelOnEscape: true, enableCancelOnPressed: true);
@@ -38,7 +40,31 @@ public partial class WalletInfoViewModel : RoutableViewModel
 		});
 	}
 
-	public IWalletInfoModel Model { get; }
+	public string SegWitExtendedAccountPublicKey => _model.SegWitExtendedAccountPublicKey;
+
+	public string? TaprootExtendedAccountPublicKey => _model.TaprootExtendedAccountPublicKey;
+
+	public string SegWitAccountKeyPath => _model.SegWitAccountKeyPath;
+
+	public string TaprootAccountKeyPath => _model.TaprootAccountKeyPath;
+
+	public string? MasterKeyFingerprint => _model.MasterKeyFingerprint;
+
+	public string? ExtendedMasterPrivateKey => _model.ExtendedMasterPrivateKey;
+
+	public string? ExtendedAccountPrivateKey => _model.ExtendedAccountPrivateKey;
+
+	public string? ExtendedMasterZprv => _model.ExtendedMasterZprv;
+
+	public bool HasOutputDescriptors => _model.WpkhOutputDescriptors is not null;
+
+	public string? PublicExternalOutputDescriptor => _model.WpkhOutputDescriptors?.PublicExternal.ToString();
+
+	public string? PublicInternalOutputDescriptor => _model.WpkhOutputDescriptors?.PublicInternal.ToString();
+
+	public string? PrivateExternalOutputDescriptor => _model.WpkhOutputDescriptors?.PrivateExternal;
+
+	public string? PrivateInternalOutputDescriptor => _model.WpkhOutputDescriptors?.PrivateInternal;
 
 	public bool IsHardwareWallet { get; }
 }
