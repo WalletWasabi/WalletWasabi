336a7d876d4429dbaf76f05878ebf65fef4266ea
Lucas Ontivero <lontivero@users.noreply.github.com>
2023-08-17 14:51:34 -0300

    Hardcode delay before signing (#11169)
    * Hardcode delay before signing
    
    * Make SamplePoisson pure
    
    * Fix delays
    
    * CR suggestions

--- WalletWasabi.Tests/Helpers/WabiSabiFactory.cs
+++ WalletWasabi.Tests/Helpers/WabiSabiFactory.cs
@@ -334,8 +334,8 @@ public static class WabiSabiFactory
 			TimeSpan.Zero);
 
 		// Overwrite Maximum Request Delay parameter but still use the original method.
-		mock.Setup(m => m.GetScheduledDates(It.IsAny<int>(), It.IsAny<DateTimeOffset>(), It.IsNotIn(TimeSpan.FromSeconds(1))))
-			.Returns((int howMany, DateTimeOffset endTime, TimeSpan maximumRequestDelay) => mock.Object.GetScheduledDates(howMany, endTime, TimeSpan.FromSeconds(1)));
+		mock.Setup(m => m.GetScheduledDates(It.IsAny<int>(), It.IsAny<DateTimeOffset>(), It.IsAny<DateTimeOffset>(), It.IsNotIn(TimeSpan.FromSeconds(1))))
+			.Returns((int howMany, DateTimeOffset startTime, DateTimeOffset endTime, TimeSpan maximumRequestDelay) => mock.Object.GetScheduledDates(howMany, startTime, endTime, TimeSpan.FromSeconds(1)));
 
 		mock.CallBase = true;
 
