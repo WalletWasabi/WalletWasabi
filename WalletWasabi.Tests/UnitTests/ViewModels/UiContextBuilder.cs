d34e2ee89ebe8fb93b841bd913c11457cd1e5d8f
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2023-08-22 13:51:49 +0200

    [VDG] Decouple view models from Avalonia platform interface IClipboard  (#11308)
    * Refactor clipboard usage
    
    * Fix unit tests
    
    * Use 0.10.x Application.Current.Clipboard
    
    * Fix CreateUiContext
    
    * Fix CF
    
    * Small corrections
    
    ---------
    
    Co-authored-by: ichthus1604 <ichthus1604@gmail.com>

--- WalletWasabi.Tests/UnitTests/ViewModels/UiContextBuilder.cs
+++ WalletWasabi.Tests/UnitTests/ViewModels/UiContextBuilder.cs
@@ -1,4 +1,3 @@
-using Avalonia.Input.Platform;
 using Moq;
 using WalletWasabi.Fluent.Models.ClientConfig;
 using WalletWasabi.Fluent.Models.FileSystem;
@@ -15,7 +14,7 @@ public class UiContextBuilder
 	public INavigate Navigate { get; private set; } = Mock.Of<INavigate>();
 	public IQrCodeGenerator QrGenerator { get; } = Mock.Of<IQrCodeGenerator>();
 	public IQrCodeReader QrReader { get; } = Mock.Of<IQrCodeReader>();
-	public IClipboard Clipboard { get; private set; } = Mock.Of<IClipboard>();
+	public IUiClipboard Clipboard { get; private set; } = Mock.Of<IUiClipboard>();
 	public IWalletRepository WalletRepository { get; private set; } = new NullWalletRepository();
 	public IHardwareWalletInterface HardwareWalletInterface { get; private set; } = new NullHardwareWalletInterface();
 	public IFileSystem FileSystem { get; private set; } = new NullFileSystem();
@@ -27,7 +26,7 @@ public class UiContextBuilder
 		return this;
 	}
 
-	public UiContextBuilder WithClipboard(IClipboard clipboard)
+	public UiContextBuilder WithClipboard(IUiClipboard clipboard)
 	{
 		Clipboard = clipboard;
 		return this;
