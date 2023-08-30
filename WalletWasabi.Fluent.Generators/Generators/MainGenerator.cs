eca268bde03e4a385b7ba999551cead1102a1cdc
ichthus1604 <ichthus1604@gmail.com>
2023-08-30 17:07:33 -0300

    AutoNotifyGenerator

--- WalletWasabi.Fluent.Generators/Generators/MainGenerator.cs
+++ WalletWasabi.Fluent.Generators/Generators/MainGenerator.cs
@@ -8,6 +8,8 @@ internal class MainGenerator : CombinedGenerator
 	public MainGenerator()
 	{
 		AddStaticFileGenerator<AutoInterfaceAttributeGenerator>();
+		AddStaticFileGenerator<AutoNotifyAttributeGenerator>();
+		Add<AutoNotifyGenerator>();
 		Add<AutoInterfaceGenerator>();
 		Add<UiContextConstructorGenerator>();
 		Add<FluentNavigationGenerator>();
