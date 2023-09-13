7529e3382a91756d09b869df73e6c2ff6103d684
Kimi <58662979+kiminuo@users.noreply.github.com>
2023-09-13 21:59:54 +0200

    [refactoring] Move methods out of `TransactionHistoryBuilder` (#11471)
    * Move `GetInputs` and `GetOutputs` from `TransactionHistoryBuilder` to `SmartTransactionExtensions`.
    
    Co-authored-by: Turbolay <clement.ogame@gmail.com>
    
    * Move `GetDestinationAddresses` to `SmartTransactionExtensions`
    
    Co-authored-by: Turbolay <clement.ogame@gmail.com>
    
    * feedback
    
    ---------
    
    Co-authored-by: Turbolay <clement.ogame@gmail.com>

--- WalletWasabi/Blockchain/Transactions/TransactionHistoryBuilder.cs
+++ WalletWasabi/Blockchain/Transactions/TransactionHistoryBuilder.cs
@@ -42,11 +42,8 @@ public class TransactionHistoryBuilder
 			}
 			else
 			{
-				var outputs = GetOutputs(containingTransaction, wallet.Network).ToList();
-				var inputs = GetInputs(containingTransaction).ToList();
-				var destinationAddresses = GetDestinationAddresses(inputs, outputs);
-
-				txRecordList.Add(new TransactionSummary(containingTransaction, coin.Amount, GetInputs(containingTransaction), outputs, destinationAddresses));
+				var destinationAddresses = containingTransaction.GetDestinationAddresses(wallet.Network, out List<IInput> inputs, out List<Output> outputs);
+				txRecordList.Add(new TransactionSummary(containingTransaction, coin.Amount, inputs, outputs, destinationAddresses));
 			}
 
 			var spenderTransaction = coin.SpenderTransaction;
@@ -62,79 +59,12 @@ public class TransactionHistoryBuilder
 				}
 				else
 				{
-					var outputs = GetOutputs(spenderTransaction, wallet.Network).ToList();
-					var inputs = GetInputs(spenderTransaction).ToList();
-					var destinationAddresses = GetDestinationAddresses(inputs, outputs);
-          
-					txRecordList.Add(new TransactionSummary(spenderTransaction, Money.Zero - coin.Amount, GetInputs(spenderTransaction), outputs, destinationAddresses));
+					var destinationAddresses = spenderTransaction.GetDestinationAddresses(wallet.Network, out List<IInput> inputs, out List<Output> outputs);
+					txRecordList.Add(new TransactionSummary(spenderTransaction, Money.Zero - coin.Amount, inputs, outputs, destinationAddresses));
 				}
 			}
 		}
 		txRecordList = txRecordList.OrderByBlockchain().ToList();
 		return txRecordList;
-	}
-
-	private IEnumerable<BitcoinAddress> GetDestinationAddresses(ICollection<IInput> inputs, ICollection<Output> outputs)
-	{
-		var myOwnInputs = inputs.OfType<KnownInput>().ToList();
-		var foreignInputs = inputs.OfType<ForeignInput>().ToList();
-		var myOwnOutputs = outputs.OfType<OwnOutput>().ToList();
-		var foreignOutputs = outputs.OfType<ForeignOutput>().ToList();
-
-		// All inputs and outputs are my own, transaction is a self-spend.
-		if (!foreignInputs.Any() && !foreignOutputs.Any())
-		{
-			// Classic self-spend to one or more external addresses.
-			if (myOwnOutputs.Any(x => !x.IsInternal))
-			{
-				// Destinations are the external addresses.
-				return myOwnOutputs.Where(x => !x.IsInternal).Select(x => x.DestinationAddress);
-			}
-
-			// Edge-case: self-spend to one or more internal addresses.
-			// We can't know the destinations, return all the outputs.
-			return myOwnOutputs.Select(x => x.DestinationAddress);
-		}
-
-		// All inputs are foreign but some outputs are my own, someone is sending coins to me.
-		if (!myOwnInputs.Any() && myOwnOutputs.Any())
-		{
-			// All outputs that are my own are the destinations.
-			return myOwnOutputs.Select(x => x.DestinationAddress);
-		}
-
-		// I'm sending a transaction to someone else.
-		// All outputs that are not my own are the destinations.
-		return foreignOutputs.Select(x => x.DestinationAddress);
-	}
-
-	private IEnumerable<Output> GetOutputs(SmartTransaction smartTransaction, Network network)
-	{
-		var known = smartTransaction.WalletOutputs.Select(coin =>
-		{
-			var address = coin.TxOut.ScriptPubKey.GetDestinationAddress(network)!;
-			return new OwnOutput(coin.TxOut.Value, address, coin.HdPubKey.IsInternal);
-		}).Cast<Output>();
-
-		var unknown = smartTransaction.ForeignOutputs.Select(coin =>
-		{
-			var address = coin.TxOut.ScriptPubKey.GetDestinationAddress(network)!;
-			return new ForeignOutput(coin.TxOut.Value, address);
-		}).Cast<Output>();
-
-		return known.Concat(unknown);
-	}
-
-	private static IEnumerable<IInput> GetInputs(SmartTransaction transaction)
-	{
-		var known = transaction.WalletInputs
-			.Select(x => new KnownInput(x.Amount))
-			.OfType<IInput>();
-
-		var unknown = transaction.ForeignInputs
-			.Select(_ => new ForeignInput())
-			.OfType<IInput>();
-
-		return known.Concat(unknown);
-	}
+	}	
 }
