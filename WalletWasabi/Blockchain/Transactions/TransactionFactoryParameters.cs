8d37bff85fcaaa8e6abcba2c8551fe7a914c3ead
Szpoti <balage.farago@gmail.com>
2023-10-10 13:05:28 +0200

    Add parameters file

---
+++ WalletWasabi/Blockchain/Transactions/TransactionFactoryParameters.cs
@@ -0,0 +1,22 @@
+using NBitcoin;
+using System.Collections.Generic;
+
+namespace WalletWasabi.Blockchain.Transactions;
+
+public class TransactionFactoryParameters
+{
+	public TransactionFactoryParameters(Func<FeeRate> feeRateFetcher, bool allowUnconfirmed = false, bool allowDoubleSpend = false, IEnumerable<OutPoint>? allowedInputs = null, bool tryToSign = true)
+	{
+		FeeRateFetcher = feeRateFetcher;
+		AllowUnconfirmed = allowUnconfirmed;
+		AllowDoubleSpend = allowDoubleSpend;
+		AllowedInputs = allowedInputs;
+		TryToSign = tryToSign;
+	}
+
+	public Func<FeeRate> FeeRateFetcher { get; }
+	public bool AllowUnconfirmed { get; }
+	public bool AllowDoubleSpend { get; }
+	public IEnumerable<OutPoint>? AllowedInputs { get; }
+	public bool TryToSign { get; }
+}
