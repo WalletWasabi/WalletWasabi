413726b6927fcc68742cee3dd4efed87628431c3
Szpoti <balage.farago@gmail.com>
2023-10-17 12:47:58 +0200

    Add TransactionFactoryParams

---
+++ WalletWasabi/Blockchain/Transactions/TransactionFactoryParameters.cs
@@ -0,0 +1,11 @@
+using NBitcoin;
+using System.Collections.Generic;
+
+namespace WalletWasabi.Blockchain.Transactions;
+
+public record TransactionFactoryParameters(
+	Func<FeeRate> FeeRateFetcher,
+	bool AllowUnconfirmed = false,
+	bool AllowDoubleSpend = false,
+	IEnumerable<OutPoint>? AllowedInputs = null,
+	bool TryToSign = true);
