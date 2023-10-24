6d9f98e910587ce28e910a97ec23aa86c61155e0
adamPetho <45069029+adamPetho@users.noreply.github.com>
2023-10-24 11:37:21 +0200

    remove first extension method

--- WalletWasabi/Extensions/TaskExtensions.cs
+++ WalletWasabi/Extensions/TaskExtensions.cs
@@ -7,22 +7,6 @@ namespace WalletWasabi.Extensions;
 public static class TaskExtensions
 {
 	/// <summary>
-	/// Implements method that behaves as <see cref="Task{T}.WaitAsync(CancellationToken)"/> but
-	/// it throws <see cref="OperationCanceledException"/> instead of <see cref="TimeoutException"/>.
-	/// </summary>
-	public static async Task<T> WithAwaitCancellationAsync<T>(this Task<T> task, CancellationToken cancellationToken)
-	{
-		try
-		{
-			return await task.WaitAsync(cancellationToken).ConfigureAwait(false);
-		}
-		catch (TimeoutException e)
-		{
-			throw new OperationCanceledException("Timed out.", innerException: e);
-		}
-	}
-
-	/// <summary>
 	/// Implements method that behaves as <see cref="Task.WaitAsync(CancellationToken)"/> but
 	/// it throws <see cref="OperationCanceledException"/> instead of <see cref="TimeoutException"/>.
 	/// </summary>
