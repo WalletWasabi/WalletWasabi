68ca8e14060862108b826d4d7067138318de5974
ichthus1604 <ichthus1604@gmail.com>
2023-10-26 12:44:40 -0300

    remove unneeded usings

--- WalletWasabi.Fluent/ViewModels/Wallets/Notifications/WalletNotificationsViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Notifications/WalletNotificationsViewModel.cs
@@ -1,10 +1,6 @@
 using DynamicData;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using WalletWasabi.Blockchain.TransactionProcessing;
 using WalletWasabi.Fluent.Extensions;
