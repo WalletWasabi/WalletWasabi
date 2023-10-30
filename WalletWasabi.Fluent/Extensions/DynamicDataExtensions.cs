c530b1225a1f8edc9f255c4efa145f792f921cf0
José Manuel Nieto <superjmn@outlook.com>
2023-10-30 17:06:06 +0100

    Cleanup

--- WalletWasabi.Fluent/Extensions/DynamicDataExtensions.cs
+++ WalletWasabi.Fluent/Extensions/DynamicDataExtensions.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Reactive.Linq;
 using DynamicData;
 
 namespace WalletWasabi.Fluent.Extensions;
@@ -11,17 +10,12 @@ public static class DynamicDataExtensions
 		return contents.Subscribe(list => sourceCache.Edit(updater => updater.Load(list)));
 	}
 
-	public static IObservable<bool> HasAny<TObject, TKey>(IObservable<IChangeSet<TObject, TKey>> changeSet) where TKey : notnull
-	{
-		return changeSet.AsObservableCache().CountChanged.Select(n => n > 0);
-	}
-
-	public static IObservable<IReadOnlyCollection<TObject>> ToCollectionStartWithEmpty<TObject, TKey>(this IObservable<IChangeSet<TObject, TKey>> changeSet) where TKey : notnull
+	public static IObservable<IReadOnlyCollection<TObject>> ToCollectionStartWithEmpty<TObject, TKey>(this IObservable<IChangeSet<TObject, TKey>> changeSet) where TKey : notnull where TObject : notnull
 	{
 		return changeSet.ToCollection().StartWithEmpty();
 	}
 
-	public static IObservable<IReadOnlyCollection<TObject>> ToCollectionStartWithEmpty<TObject>(this IObservable<IChangeSet<TObject>> changeSet)
+	public static IObservable<IReadOnlyCollection<TObject>> ToCollectionStartWithEmpty<TObject>(this IObservable<IChangeSet<TObject>> changeSet) where TObject : notnull
 	{
 		return changeSet.ToCollection().StartWithEmpty();
 	}
