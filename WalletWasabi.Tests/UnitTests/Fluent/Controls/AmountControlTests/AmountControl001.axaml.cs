3b9e1471cca3a11cabd5c72c9e2b6e1b22318e9b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-10-31 17:43:22 +0100

    Add AmountControl control test

---
+++ WalletWasabi.Tests/UnitTests/Fluent/Controls/AmountControlTests/AmountControl001.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using WalletWasabi.Fluent.Models.Wallets;
+
+namespace WalletWasabi.Tests.UnitTests.Fluent.Controls.AmountControlTests;
+
+public partial class AmountControl001 : Window
+{
+    public AmountControl001()
+    {
+        InitializeComponent();
+
+        DataContext = Amount.Zero;
+    }
+}
---
+++ WalletWasabi.Tests/UnitTests/Fluent/Controls/AmountControlTests/AmountControlTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Avalonia.Headless.XUnit;
+using VerifyXunit;
+using WalletWasabi.Fluent.Models.Wallets;
+using Xunit;
+
+namespace WalletWasabi.Tests.UnitTests.Fluent.Controls.AmountControlTests;
+
+[UsesVerify]
+public class AmountControlTests
+{
+    [AvaloniaFact]
+    public Task AmountControl_001()
+    {
+        var window = new AmountControl001();
+        window.Show();
+
+        Assert.Equal(window.DataContext, Amount.Zero);
+
+        return Verifier.Verify(window);
+    }
+}
