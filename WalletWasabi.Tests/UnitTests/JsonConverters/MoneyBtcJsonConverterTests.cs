320a268a533a67cb69151b116a2901f3b9c3e056
Kiminuo <kiminuo@protonmail.com>
2023-10-31 08:56:33 +0100

    Modify test

--- WalletWasabi.Tests/UnitTests/JsonConverters/MoneyBtcJsonConverterTests.cs
+++ WalletWasabi.Tests/UnitTests/JsonConverters/MoneyBtcJsonConverterTests.cs
@@ -1,112 +1,113 @@
 using NBitcoin;
-using System.Diagnostics;
-using System.IO;
-using WalletWasabi.Helpers;
 using WalletWasabi.JsonConverters.Bitcoin;
-using WalletWasabi.Tests.Helpers;
-using WalletWasabi.WabiSabi.Models.Serialization;
 using Xunit;
+using JsonConvertNew = System.Text.Json.JsonSerializer;
+using JsonConvertOld = Newtonsoft.Json.JsonConvert;
 
 namespace WalletWasabi.Tests.UnitTests.JsonConverters;
 
-public class MoneyBtcJsonConverterCompareTests
+/// <summary>
+/// Tests for <see cref="MoneyBtcJsonConverter"/> and <see cref="MoneyBtcJsonConverterNg"/> classes.
+/// </summary>
+public class MoneyBtcJsonConverterTests
 {
+	/// <summary>
+	/// Tests that JSON converter based on <c>System.Text.Json</c> and the one based on NewtonSoft.JSON works the same.
+	/// </summary>
 	[Fact]
-	public void STJCOnverterWorksLikeNewtonsoft()
+	public void FunctionalParity()
 	{
-		string directory = Common.GetWorkDir();
-		IoHelpers.EnsureDirectoryExists(directory);
+		TestData testObject = new();
 
-		string newtonsoftFilePath = Path.Combine(directory, "newtonsoft.json");
-		string stjFilePath = Path.Combine(directory, "stj.json");
+		string json = AssertSerializedEqually(testObject);
+		Assert.Equal("""{"Half":"0.50","One":"1.00","Zeros":"0.000001","None":null,"NotAnnotated":null}""", json);
 
-		var stjClass = new STJJsonSerializableClass();
-		var newtonClass = new NewtonsoftJsonSerializableClass();
+		//string newtonJson = JsonConvertOld.SerializeObject(testObject);
+		//var deserializedNewtonClass = JsonConvertOld.DeserializeObject<TestClass>(newtonJson);
 
-		// Measure Newtonsoft's serilize and deserialize.
-		var stopwatch = Stopwatch.StartNew();
+		//// Sanity checks.
+		//Assert.Equal(Money.Coins(1m), deserializedNewtonClass.OneBTC);
+		//Assert.Equal(Money.Coins(0.5m), deserializedNewtonClass.HalfBTC);
+		//Assert.Equal(Money.Satoshis(1000), deserializedNewtonClass.SomeSats);
 
-		string newtonJson = Newtonsoft.Json.JsonConvert.SerializeObject(newtonClass);
-		File.WriteAllText(newtonsoftFilePath, newtonJson);
+		//string stjJson = JsonConvertNew.Serialize(stjClass);
+		//// string stjJson = System.Text.Json.JsonSerializer.Serialize(stjClass, typeof(STJJsonSerializableClass)); -> Doesn't make a difference in current state.
 
-		newtonJson = File.ReadAllText(newtonsoftFilePath);
-		var deserializedNewtonClass = Newtonsoft.Json.JsonConvert.DeserializeObject<NewtonsoftJsonSerializableClass>(newtonJson);
-		stopwatch.Stop();
-		var newtontime = stopwatch.ElapsedMilliseconds;
+		//var deserializedJstClass = JsonConvertNew.Deserialize<TestClass>(stjJson);
 
-		// Sanity checks.
-		Assert.Equal(Money.Coins(1m), deserializedNewtonClass.OneBTC);
-		Assert.Equal(Money.Coins(0.5m), deserializedNewtonClass.HalfBTC);
-		Assert.Equal(Money.Satoshis(1000), deserializedNewtonClass.SomeSats);
+		//// newtontime = 7-8ms | stjtime = 41-52ms.
+		//Assert.Equal(Money.Coins(1m), deserializedJstClass.OneBTC);
+		//Assert.Equal(Money.Coins(0.5m), deserializedJstClass.HalfBTC);
+		//Assert.Equal(Money.Satoshis(1000), deserializedJstClass.SomeSats);
 
-		// Measure Microsoft's serilize and deserialize.
-		stopwatch = Stopwatch.StartNew();
+		//// Error handling.(Neither of these throws exception)
+		//// SomeSats property is mistyped:
+		//string badJson = """{"HalfBTC":"0.50","OneBTC":"1.00","SomeS":"0.00002"}""";
+		//var des = JsonConvertOld.DeserializeObject<TestClass>(badJson); // SomeSats is null.
+		//var des2 = JsonConvertNew.Deserialize<TestClass>(badJson); // SomeSats is 0.00001. STJ fills up the property with the init value if it can't find the new value in json.
 
-		string stjJson = System.Text.Json.JsonSerializer.Serialize(stjClass);
-		// string stjJson = System.Text.Json.JsonSerializer.Serialize(stjClass, typeof(STJJsonSerializableClass)); -> Doesn't make a difference in current state.
-		File.WriteAllText(stjFilePath, stjJson);
+		//// A bonus property is in the middle of the json:
+		//badJson = """{"HalfBTC":"0.50","OneBTC":"1.00","FakeSomeSats":"0.00003","SomeSats":"0.00002"}""";
+		//des = JsonConvertOld.DeserializeObject<TestClass>(badJson); // Ignores FakeSomeSats, SomeSats is 0.00002.
+		//des2 = JsonConvertNew.Deserialize<TestClass>(badJson); // Ignores FakeSomeSats, SomeSats is 0.00002.
 
-		stjJson = File.ReadAllText(stjFilePath);
-		var deserializedJstClass = System.Text.Json.JsonSerializer.Deserialize<STJJsonSerializableClass>(stjJson);
-		stopwatch.Stop();
-		var stjtime = stopwatch.ElapsedMilliseconds;
+		//// SomeSats is null:
+		//badJson = """{"HalfBTC":"0.50","OneBTC":"1.00","SomeSats":null}""";
+		//des = JsonConvertOld.DeserializeObject<TestClass>(badJson); // SomeSats is null.
+		//des2 = JsonConvertNew.Deserialize<TestClass>(badJson); // SomeSats is null.
 
-		// newtontime = 7-8ms | stjtime = 41-52ms.
-		Assert.Equal(Money.Coins(1m), deserializedJstClass.OneBTC);
-		Assert.Equal(Money.Coins(0.5m), deserializedJstClass.HalfBTC);
-		Assert.Equal(Money.Satoshis(1000), deserializedJstClass.SomeSats);
-
-		// Error handling.(Neither of these throws exception)
-		// SomeSats property is mistyped:
-		string badJson = "{\"HalfBTC\":\"0.50\",\"OneBTC\":\"1.00\",\"SomeS\":\"0.00002\"}";
-		var des = Newtonsoft.Json.JsonConvert.DeserializeObject<NewtonsoftJsonSerializableClass>(badJson); // SomeSats is null.
-		var des2 = System.Text.Json.JsonSerializer.Deserialize<STJJsonSerializableClass>(badJson); // SomeSats is 0.00001. STJ fills up the property with the init value if it can't find the new value in json.
-
-		// A bonus property is in the middle of the json:
-		badJson = "{\"HalfBTC\":\"0.50\",\"OneBTC\":\"1.00\",\"FakeSomeSats\":\"0.00003\",\"SomeSats\":\"0.00002\"}";
-		des = Newtonsoft.Json.JsonConvert.DeserializeObject<NewtonsoftJsonSerializableClass>(badJson); // Ignores FakeSomeSats, SomeSats is 0.00002.
-		des2 = System.Text.Json.JsonSerializer.Deserialize<STJJsonSerializableClass>(badJson); // Ignores FakeSomeSats, SomeSats is 0.00002.
-
-		// SomeSats is null:
-		badJson = "{\"HalfBTC\":\"0.50\",\"OneBTC\":\"1.00\",\"SomeSats\":null}";
-		des = Newtonsoft.Json.JsonConvert.DeserializeObject<NewtonsoftJsonSerializableClass>(badJson); // SomeSats is null.
-		des2 = System.Text.Json.JsonSerializer.Deserialize<STJJsonSerializableClass>(badJson); // SomeSats is null.
-
-		// OneBTC has an extra zero and misses number after decimal point:
-		badJson = "{\"HalfBTC\":\"0.50\",\"OneBTC\":\"01.\",\"SomeSats\":null}";
-		des = Newtonsoft.Json.JsonConvert.DeserializeObject<NewtonsoftJsonSerializableClass>(badJson); // OneBTC is 1.00000000.
-		des2 = System.Text.Json.JsonSerializer.Deserialize<STJJsonSerializableClass>(badJson); // OneBTC is 1.00000000.
+		//// OneBTC has an extra zero and misses number after decimal point:
+		//badJson = """{"HalfBTC":"0.50","OneBTC":"01.","SomeSats":null}""";
+		//des = JsonConvertOld.DeserializeObject<TestClass>(badJson); // OneBTC is 1.00000000.
+		//des2 = JsonConvertNew.Deserialize<TestClass>(badJson); // OneBTC is 1.00000000.
 	}
 
-	// This example template class only uses System.Text.Json functions and attributes for serializing.
-	private class STJJsonSerializableClass
+	/// <summary>
+	/// Asserts that object <paramref name="o"/> is serialized to the same JSON by both Newtonsoft library and STJ library.
+	/// </summary>
+	/// <returns>JSON representation of <paramref name="o"/>.</returns>
+	private static string AssertSerializedEqually<T>(T o)
 	{
-		[System.Text.Json.Serialization.JsonConverter(typeof(MoneyBtcJsonConverterNg))]
-		[System.Text.Json.Serialization.JsonPropertyName("HalfBTC")]
-		public Money HalfBTC { get; set; } = Money.Coins(0.500m);
+		string newtonsoftJson = JsonConvertOld.SerializeObject(o);
+		string stjJson = JsonConvertNew.Serialize(o);
 
-		[System.Text.Json.Serialization.JsonConverter(typeof(MoneyBtcJsonConverterNg))]
-		[System.Text.Json.Serialization.JsonPropertyName("OneBTC")]
-		public Money OneBTC { get; set; } = Money.Coins(1m);
+		Assert.NotNull(newtonsoftJson);
+		Assert.NotNull(stjJson);
+		Assert.Equal(newtonsoftJson, stjJson);
 
-		[System.Text.Json.Serialization.JsonConverter(typeof(MoneyBtcJsonConverterNg))]
-		[System.Text.Json.Serialization.JsonPropertyName("SomeSats")]
-		public Money SomeSats { get; set; } = Money.Satoshis(1000);
+		return stjJson;
 	}
 
-	// This example template class only uses Newtonsoft.Json functions and attributes for serializing.
-	private class NewtonsoftJsonSerializableClass
+	/// <summary>
+	/// Record with both STJ and Newtonsoft attributes.
+	/// </summary>
+	private record TestData
 	{
-		[Newtonsoft.Json.JsonProperty(PropertyName = "HalfBTC", DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Populate)]
+		[Newtonsoft.Json.JsonProperty(PropertyName = nameof(Half))]
 		[Newtonsoft.Json.JsonConverter(typeof(MoneyBtcJsonConverter))]
-		public Money HalfBTC { get; set; } = Money.Coins(0.500m);
+		[System.Text.Json.Serialization.JsonConverter(typeof(MoneyBtcJsonConverterNg))]
+		[System.Text.Json.Serialization.JsonPropertyName(nameof(Half))]
+		public Money Half { get; set; } = Money.Coins(0.500m);
 
-		[Newtonsoft.Json.JsonProperty(PropertyName = "OneBTC", DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Populate)]
+		[System.Text.Json.Serialization.JsonConverter(typeof(MoneyBtcJsonConverterNg))]
+		[System.Text.Json.Serialization.JsonPropertyName(nameof(One))]
+		[Newtonsoft.Json.JsonProperty(PropertyName = nameof(One))]
 		[Newtonsoft.Json.JsonConverter(typeof(MoneyBtcJsonConverter))]
-		public Money OneBTC { get; set; } = Money.Coins(1m);
+		public Money One { get; set; } = Money.Coins(1m);
 
-		[Newtonsoft.Json.JsonProperty(PropertyName = "SomeSats", DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Populate)]
+		[System.Text.Json.Serialization.JsonConverter(typeof(MoneyBtcJsonConverterNg))]
+		[System.Text.Json.Serialization.JsonPropertyName(nameof(Zeros))]
+		[Newtonsoft.Json.JsonProperty(PropertyName = nameof(Zeros))]
 		[Newtonsoft.Json.JsonConverter(typeof(MoneyBtcJsonConverter))]
-		public Money SomeSats { get; set; } = Money.Satoshis(1000);
+		public Money Zeros { get; set; } = Money.Coins(0.000001m);
+
+		[System.Text.Json.Serialization.JsonConverter(typeof(MoneyBtcJsonConverterNg))]
+		[System.Text.Json.Serialization.JsonPropertyName(nameof(None))]
+		[Newtonsoft.Json.JsonProperty(PropertyName = nameof(None))]
+		[Newtonsoft.Json.JsonConverter(typeof(MoneyBtcJsonConverter))]
+		public Money? None { get; set; } = null;
+
+		/// <summary>Not annotated properties are also included by both Newtonsoft and STJ by default.</summary>
+		public Money? NotAnnotated { get; set; } = null;
 	}
 }
