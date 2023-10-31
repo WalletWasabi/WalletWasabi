25d97382b64f1406b0f920d7d42618a368197aa0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-10-31 17:06:42 +0100

    Add VerifyTests ModuleInit

---
+++ WalletWasabi.Tests/UnitTests/Fluent/ModuleInit.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using VerifyTests;
+
+namespace WalletWasabi.Tests.UnitTests.Fluent;
+
+public static class ModuleInit
+{
+    [ModuleInitializer]
+    public static void InitOther()
+    {
+        VerifierSettings.InitializePlugins();
+        VerifyImageMagick.RegisterComparers(0.04);
+        VerifierSettings.UniqueForOSPlatform();
+    }
+}
