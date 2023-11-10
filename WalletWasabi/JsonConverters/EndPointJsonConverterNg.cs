34040b85e1dfb699b92f0c806479376a50d6f250
Kiminuo <kiminuo@protonmail.com>
2023-11-10 15:22:21 +0100

    Usings

--- WalletWasabi/JsonConverters/EndPointJsonConverterNg.cs
+++ WalletWasabi/JsonConverters/EndPointJsonConverterNg.cs
@@ -1,7 +1,6 @@
 using System.Net;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using WalletWasabi.Helpers;
 using WalletWasabi.Userfacing;
 
 namespace WalletWasabi.JsonConverters;
