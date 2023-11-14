6cc38df23ad6470602cd1b932334b33f5536bdc9
Szpoti <balage.farago@gmail.com>
2023-11-14 12:08:22 +0100

    Remove redundant else

--- WalletWasabi/JsonConverters/Timing/TimeSpanJsonConverterNg.cs
+++ WalletWasabi/JsonConverters/Timing/TimeSpanJsonConverterNg.cs
@@ -23,18 +23,16 @@ public class TimeSpanJsonConverterNg : JsonConverter<TimeSpan>
 		{
 			return default;
 		}
-		else
-		{
-			var daysParts = stringValue.Split('d');
-			var days = int.Parse(daysParts[0].Trim());
-			var hoursParts = daysParts[1].Split('h');
-			var hours = int.Parse(hoursParts[0].Trim());
-			var minutesParts = hoursParts[1].Split('m');
-			var minutes = int.Parse(minutesParts[0].Trim());
-			var secondsParts = minutesParts[1].Split('s');
-			var seconds = int.Parse(secondsParts[0].Trim());
-			return new TimeSpan(days, hours, minutes, seconds);
-		}
+
+		var daysParts = stringValue.Split('d');
+		var days = int.Parse(daysParts[0].Trim());
+		var hoursParts = daysParts[1].Split('h');
+		var hours = int.Parse(hoursParts[0].Trim());
+		var minutesParts = hoursParts[1].Split('m');
+		var minutes = int.Parse(minutesParts[0].Trim());
+		var secondsParts = minutesParts[1].Split('s');
+		var seconds = int.Parse(secondsParts[0].Trim());
+		return new TimeSpan(days, hours, minutes, seconds);
 	}
 
 	/// <inheritdoc />
