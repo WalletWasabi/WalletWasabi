51453332973458d8b7513321cd7651e59d0c0f6c
José Manuel Nieto <superjmn@outlook.com>
2023-11-15 14:01:41 +0100

    Fixes copy button issues

---
+++ WalletWasabi.Fluent/Behaviors/AdornerBehavior.cs
@@ -0,0 +1,59 @@
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+public class AdornerBehavior : AttachedToVisualTreeBehavior<Control>
+{
+	protected override void OnAttachedToVisualTree(CompositeDisposable disposables)
+	{
+		if (AssociatedObject is null)
+		{
+			return;
+		}
+
+		var layer = AdornerLayer.GetAdornerLayer(AssociatedObject);
+
+		if (layer is null || Adorner is null)
+		{
+			return;
+		}
+
+		Adorner.DataContext = AssociatedObject.TemplatedParent;
+
+		layer.Children.Add(Adorner);
+
+		Observable.FromEventPattern(
+				handler => AssociatedObject.LayoutUpdated += handler,
+				handler =>
+				{
+					if (AssociatedObject != null)
+					{
+						AssociatedObject.LayoutUpdated -= handler;
+					}
+				})
+			.Do(_ => ArrangeAdorner(layer, AssociatedObject))
+			.Subscribe()
+			.DisposeWith(disposables);
+
+		ArrangeAdorner(layer, AssociatedObject);
+	}
+
+	private void ArrangeAdorner(Visual layer, Visual associatedObject)
+	{
+		var translated = associatedObject.TranslatePoint(associatedObject.Bounds.TopLeft, layer);
+		if (!translated.HasValue)
+		{
+			return;
+		}
+
+		var finalRect = associatedObject.Bounds.Translate(translated.Value);
+		Canvas.SetLeft(Adorner!, finalRect.Right);
+		Canvas.SetTop(Adorner!, finalRect.Y + (finalRect.Height / 2 - Adorner!.Bounds.Height / 2));
+	}
+
+	public Control? Adorner { get; set; }
+}
