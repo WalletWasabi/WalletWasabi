70b25b91f16f2a837ce711dc49922f54c033c638
José Manuel Nieto <superjmn@outlook.com>
2023-11-20 09:55:28 +0100

    Rename properties

--- WalletWasabi.Fluent/Controls/NewPrivacyBar.axaml.cs
+++ WalletWasabi.Fluent/Controls/NewPrivacyBar.axaml.cs
@@ -1,39 +1,35 @@
-using System.Reactive.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using ReactiveUI;
-using WalletWasabi.Fluent.Extensions;
 
 namespace WalletWasabi.Fluent.Controls;
-public partial class NewPrivacyBar : UserControl
+
+public class NewPrivacyBar : UserControl
 {
+	public static readonly DirectProperty<NewPrivacyBar, decimal> PrivateAndSemiPrivateAmountProperty =
+		AvaloniaProperty.RegisterDirect<NewPrivacyBar, decimal>(
+			nameof(PrivateAndSemiPrivateAmount),
+			o => o.PrivateAndSemiPrivateAmount);
+
+	public static readonly StyledProperty<decimal> TotalAmountProperty = AvaloniaProperty.Register<NewPrivacyBar, decimal>(nameof(TotalAmount));
+
+	public static readonly StyledProperty<decimal> PrivateAmountProperty = AvaloniaProperty.Register<NewPrivacyBar, decimal>(nameof(PrivateAmount));
+
+	public static readonly StyledProperty<decimal> SemiPrivateAmountProperty = AvaloniaProperty.Register<NewPrivacyBar, decimal>(nameof(SemiPrivateAmount));
+
+	private decimal _privateAndSemiPrivateAmount;
+
 	public NewPrivacyBar()
 	{
 		InitializeComponent();
 	}
 
-	private void InitializeComponent()
+	public decimal PrivateAndSemiPrivateAmount
 	{
-		AvaloniaXamlLoader.Load(this);
-
-		this.WhenAnyValue(x => x.SemiPrivateAmount, x => x.PrivateAmount, selector: (a, b) => a + b).BindTo(this, x => x.SemiPrivateOverlay);
+		get => _privateAndSemiPrivateAmount;
+		private set => SetAndRaise(PrivateAndSemiPrivateAmountProperty, ref _privateAndSemiPrivateAmount, value);
 	}
-
-	public static readonly DirectProperty<NewPrivacyBar, decimal> SemiPrivateOverlayProperty =
-		AvaloniaProperty.RegisterDirect<NewPrivacyBar, decimal>(
-			nameof(SemiPrivateOverlay),
-			o => o.SemiPrivateOverlay);
-
-	private decimal _semiPrivateOverlay;
-
-	public decimal SemiPrivateOverlay
-	{
-		get { return _semiPrivateOverlay; }
-		private set { SetAndRaise(SemiPrivateOverlayProperty, ref _semiPrivateOverlay, value); }
-	}
-
-	public static readonly StyledProperty<decimal> TotalAmountProperty = AvaloniaProperty.Register<NewPrivacyBar, decimal>("TotalAmount");
 
 	public decimal TotalAmount
 	{
@@ -41,19 +37,22 @@ public partial class NewPrivacyBar : UserControl
 		set => SetValue(TotalAmountProperty, value);
 	}
 
-	public static readonly StyledProperty<decimal> PrivateAmountProperty = AvaloniaProperty.Register<NewPrivacyBar, decimal>("PrivateAmount");
-
 	public decimal PrivateAmount
 	{
 		get => GetValue(PrivateAmountProperty);
 		set => SetValue(PrivateAmountProperty, value);
 	}
 
-	public static readonly StyledProperty<decimal> SemiPrivateAmountProperty = AvaloniaProperty.Register<NewPrivacyBar, decimal>("SemiPrivateAmount");
-
 	public decimal SemiPrivateAmount
 	{
 		get => GetValue(SemiPrivateAmountProperty);
 		set => SetValue(SemiPrivateAmountProperty, value);
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
+
+		this.WhenAnyValue(x => x.SemiPrivateAmount, x => x.PrivateAmount, selector: (a, b) => a + b).BindTo(this, x => x.PrivateAndSemiPrivateAmount);
 	}
 }
