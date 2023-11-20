deb14258f4d39e698e31b081c5d5e9d65b19c78a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-11-20 22:51:08 +0100

    Add try/catch to main

--- WalletWasabi.Fluent.iOS/Application.cs
+++ WalletWasabi.Fluent.iOS/Application.cs
@@ -1,3 +1,4 @@
+using System;
 using UIKit;
 
 namespace WalletWasabi.Fluent.iOS;
@@ -7,8 +8,15 @@ public class Application
     // This is the main entry point of the application.
     static void Main(string[] args)
     {
-        // if you want to use a different Application Delegate class from "AppDelegate"
-        // you can specify it here.
-        UIApplication.Main(args, null, typeof(AppDelegate));
+	    try
+	    {
+		    // if you want to use a different Application Delegate class from "AppDelegate"
+		    // you can specify it here.
+		    UIApplication.Main(args, null, typeof(AppDelegate));
+	    }
+	    catch (Exception e)
+        {
+	        Log.Error("WASABI",$"{e}");
+        }
     }
 }
