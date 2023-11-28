de39ddaa2928ac25665a838c0ac3949325fc9568
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-11-28 16:44:52 +0100

    Initial attempt to auto-scroll messages list

--- WalletWasabi.Fluent/Views/Wallets/Buy/OrderMessagesView.axaml.cs
+++ WalletWasabi.Fluent/Views/Wallets/Buy/OrderMessagesView.axaml.cs
@@ -1,13 +1,44 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml;
 
 namespace WalletWasabi.Fluent.Views.Wallets.Buy;
 
 public partial class OrderMessagesView : UserControl
 {
+	private bool _scrollToEnd;
+
 	public OrderMessagesView()
 	{
 		InitializeComponent();
+
+		var messagesItemsControl = this.FindControl<SelectingItemsControl>("MessagesItemsControl");
+		var messagesScrollViewer = this.FindControl<ScrollViewer>("MessagesScrollViewer");
+
+		messagesItemsControl
+			.GetObservable(SelectingItemsControl.SelectedItemProperty)
+			.Subscribe(_ =>
+			{
+				_scrollToEnd = true;
+			});
+
+		messagesScrollViewer
+			.GetObservable(ScrollViewer.ExtentProperty)
+			.Subscribe(_ =>
+			{
+				if (_scrollToEnd)
+				{
+					messagesScrollViewer.ScrollToEnd();
+					_scrollToEnd = false;
+				}
+			});
+
+		this.GetObservable(BoundsProperty)
+			.Subscribe(_ =>
+			{
+				messagesScrollViewer.ScrollToEnd();
+			});
 	}
 
 	private void InitializeComponent()
