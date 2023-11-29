9df93d01b11ec55e9ab35fe38ccd540898b5dd86
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-11-29 10:13:39 +0100

    Add IsUnread property to message

--- WalletWasabi.Fluent/ViewModels/Wallets/Buy/Messages/MessageViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Buy/Messages/MessageViewModel.cs
@@ -5,4 +5,5 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Buy.Messages;
 public abstract partial class MessageViewModel : ReactiveObject
 {
 	[AutoNotify] private string? _message;
+	[AutoNotify] private bool? _isUnread;
 }
