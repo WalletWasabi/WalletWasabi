a121820d2bb44ac6db295defad9245137fcc66ab
José Manuel Nieto <superjmn@outlook.com>
2023-12-06 11:27:00 +0100

    Rollback

--- WalletWasabi.Fluent/ViewModels/Wallets/Buy/OrderViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Buy/OrderViewModel.cs
@@ -254,11 +254,6 @@ public partial class OrderViewModel : ReactiveObject
 				break;
 			}
 
-			if (_workflowManager.CurrentWorkflow.CurrentStep is { RequiresUserInput: true })
-			{
-				break;
-			}
-
 			var nextStep = _workflowManager.CurrentWorkflow.ExecuteNextStep();
 			if (nextStep is null)
 			{
