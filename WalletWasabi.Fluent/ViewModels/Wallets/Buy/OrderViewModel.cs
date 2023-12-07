5fe32364c6cc9df9e99807d04c901473b2be2037
Roland Soós <soos.roland93@gmail.com>
2023-12-07 10:25:42 +0100

    save to disk when message is read

--- WalletWasabi.Fluent/ViewModels/Wallets/Buy/OrderViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Buy/OrderViewModel.cs
@@ -74,6 +74,12 @@ public partial class OrderViewModel : ReactiveObject
 
 		// TODO: Remove this once we use newer version of DynamicData
 		HasUnreadMessagesObs.BindTo(this, x => x.HasUnreadMessages);
+
+		// IsUnread flags changed so save it to the disk
+		this.WhenAnyValue(x => x.HasUnreadMessages)
+			.Where(x => x == false)
+			.DoAsync(async _ => await WorkflowManager.SendChatHistoryAsync(GetChatMessages(), cancellationToken))
+			.Subscribe();
 	}
 
 	public IObservable<bool> HasUnreadMessagesObs { get; }
