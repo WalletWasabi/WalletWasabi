fd053488996e914b1e37ab1a8c326d4daa1ce934
Roland Soós <soos.roland93@gmail.com>
2023-12-11 15:32:37 +0100

    country name is enough

--- WalletWasabi.Fluent/ViewModels/Wallets/Buy/OrderViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Buy/OrderViewModel.cs
@@ -410,7 +410,7 @@ public partial class OrderViewModel : ReactiveObject
 				var houseNumber = GetMessageByTag(ChatMessageMetaData.ChatMessageTag.HouseNumber);
 				var postalCode = GetMessageByTag(ChatMessageMetaData.ChatMessageTag.PostalCode);
 				var city = GetMessageByTag(ChatMessageMetaData.ChatMessageTag.City);
-				var country = _countries.FirstOrDefault(x => x.Name == GetMessageByTag(ChatMessageMetaData.ChatMessageTag.Country));
+				var country = GetMessageByTag(ChatMessageMetaData.ChatMessageTag.Country);
 
 					if (firstName is not { } ||
 						lastName is not { } ||
@@ -435,7 +435,7 @@ public partial class OrderViewModel : ReactiveObject
 						postalCode,
 						city,
 						state is not null ? state.Id : "",
-						country.Name,
+						country,
 						cancellationToken);
 					break;
 				}
