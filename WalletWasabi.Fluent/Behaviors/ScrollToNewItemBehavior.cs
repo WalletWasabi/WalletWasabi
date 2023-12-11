3807ec23b97336da4a60cd463ecbb712a4423979
José Manuel Nieto <superjmn@outlook.com>
2023-12-11 16:05:41 +0100

    Improve readability

--- WalletWasabi.Fluent/Behaviors/ScrollToNewItemBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/ScrollToNewItemBehavior.cs
@@ -13,16 +13,19 @@ public class ScrollToNewItemBehavior : AttachedToVisualTreeBehavior<ItemsControl
 {
 	protected override void OnAttachedToVisualTree(CompositeDisposable disposable)
 	{
-		this.WhenAnyValue(x => x.AssociatedObject, x => x.AssociatedObject.DataContext, x => x.AssociatedObject.Items, (control, o, i) => (control, o, i))
-			.Select(tuple => tuple.control)
+		var itemCollectionChanges = this.WhenAnyValue(x => x.AssociatedObject, x => x.AssociatedObject!.DataContext, x => x.AssociatedObject!.Items, (associateControl, _, _) => associateControl)
 			.WhereNotNull()
 			.Select(x => x.Items as INotifyCollectionChanged)
-			.Select(incc => incc?.ObserveCollectionChanges())
 			.WhereNotNull()
-			.Switch()
+			.Select(incc => incc.ObserveCollectionChanges())
+			.Switch();
+
+		var newItem = itemCollectionChanges
 			.Where(pattern => pattern.EventArgs.Action == NotifyCollectionChangedAction.Add)
 			.Select(eventPattern => eventPattern.EventArgs.NewItems?.Cast<object>().FirstOrDefault())
-			.WhereNotNull()
+			.WhereNotNull();
+
+		newItem
 			.Do(ScrollTo)
 			.Subscribe()
 			.DisposeWith(disposable);
@@ -30,7 +33,7 @@ public class ScrollToNewItemBehavior : AttachedToVisualTreeBehavior<ItemsControl
 
 	private void ScrollTo(object obj)
 	{
-		// Avalonia doesn't provide us with any method to scroll to a given item, so we just scroll to end.
+		// Avalonia doesn't provide any method to scroll to a given item, so we just scroll to end.
 		var scrollViewer = AssociatedObject.FindDescendantOfType<ScrollViewer>() ?? AssociatedObject.FindAncestorOfType<ScrollViewer>() ?? throw new InvalidOperationException("We can't find any ScrollViewer we can scroll.");
 		scrollViewer.ScrollToEnd();
 	}
