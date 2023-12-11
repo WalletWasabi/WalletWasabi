57259967bda72ea21cf4a2b50ab4538112ee9789
Kiminuo <kiminuo@protonmail.com>
2023-12-11 10:17:03 +0100

    Disable warnings

--- WalletWasabi.Daemon/BitcoinCore/CoreNode.cs
+++ WalletWasabi.Daemon/BitcoinCore/CoreNode.cs
@@ -24,7 +24,9 @@ namespace WalletWasabi.Daemon.BitcoinCore;
 
 public class CoreNode
 {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 	public CoreNode(string dataDir, Network network, MempoolService mempoolService, CoreConfig config, EndPoint p2pEndPoint, EndPoint rpcEndPoint, IRPCClient rpcClient)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 	{
 		DataDir = dataDir;
 		Network = network;
