33d5557e82e397982acc25929ae978c159a4e4c1
Roland Soós <soos.roland93@gmail.com>
2023-12-12 10:12:33 +0100

    Fix reply time estimation

--- WalletWasabi.Fluent/ViewModels/Wallets/Buy/Workflows/ShopinBit/Initial/Validators/ProductInputValidator.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Buy/Workflows/ShopinBit/Initial/Validators/ProductInputValidator.cs
@@ -8,16 +8,18 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Buy.Workflows.ShopinBit;
 
 public partial class ProductInputValidator : InputValidator
 {
+	private readonly InitialWorkflow _initialWorkflow;
 	private readonly BuyAnythingClient.Product[] _productsEnum;
 
 	[AutoNotify] private ObservableCollection<string> _products;
 	[AutoNotify] private string? _product;
 
-	public ProductInputValidator(
-		WorkflowState workflowState,
+	public ProductInputValidator(WorkflowState workflowState,
+		InitialWorkflow initialWorkflow,
 		ChatMessageMetaData.ChatMessageTag tag)
 		: base(workflowState, null, "Enter your location...", "Send", tag)
 	{
+		_initialWorkflow = initialWorkflow;
 		_productsEnum = Enum.GetValues<BuyAnythingClient.Product>();
 
 		_products = new ObservableCollection<string>(_productsEnum.Select(ProductHelper.GetDescription));
@@ -36,6 +38,10 @@ public partial class ProductInputValidator : InputValidator
 	{
 		if (IsValid())
 		{
+			// TODO: Ugly
+			(BuyAnythingClient.Product, string)? product = _productsEnum.Select(x => (Product: x, Desc: ProductHelper.GetDescription(x))).FirstOrDefault(x => x.Desc == _product);
+			_initialWorkflow.Product = product.Value.Item1;
+
 			return _product;
 		}
 
