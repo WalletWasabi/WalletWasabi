9a0a846d95ff455f0ef2b0248aab851f73461c23
Roland Soós <soos.roland93@gmail.com>
2023-12-16 23:36:45 +0100

    remove todo

--- WalletWasabi.Fluent/ViewModels/Wallets/Buy/Workflows/WorkflowStep.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Buy/Workflows/WorkflowStep.cs
@@ -52,8 +52,7 @@ public abstract partial class WorkflowStep<TValue> : ReactiveObject, IWorkflowSt
 
 		// if this step already contains data previously stored in the Conversation (retrieved by RetrieveData),
 		// then set the Step as completed so the parent workflow can move on.
-		// TODO: Bool values are false by default so this would set the workflow completed. ValidateUserValue(_value) prevents it, fix it properly.
-		if (ValidateInitialValue(_value) && ValidateUserValue(_value))
+		if (ValidateInitialValue(_value))
 		{
 			SetCompleted();
 		}
