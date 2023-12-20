79026320207cb73684a3904d25db593d406b59eb
David Molnar <molnardavid84@gmail.com>
2023-12-20 15:03:56 +0100

    Bump version to v205

--- WalletWasabi/Helpers/Constants.cs
+++ WalletWasabi/Helpers/Constants.cs
@@ -79,15 +79,15 @@ public static class Constants
 
 	public static readonly Money MaximumNumberOfBitcoinsMoney = Money.Coins(MaximumNumberOfBitcoins);
 
-	public static readonly Version ClientVersion = new(2, 0, 4, 0);
+	public static readonly Version ClientVersion = new(2, 0, 5, 0);
 
 	public static readonly Version HwiVersion = new("2.3.1");
 	public static readonly Version BitcoinCoreVersion = new("21.2");
 	public static readonly Version Ww1LegalDocumentsVersion = new(3, 0);
 	public static readonly Version Ww2LegalDocumentsVersion = new(1, 0);
 
-	public static readonly FeeRate MinRelayFeeRate = new (1m);
-	public static readonly FeeRate AbsurdlyHighFeeRate = new (10_000m);
+	public static readonly FeeRate MinRelayFeeRate = new(1m);
+	public static readonly FeeRate AbsurdlyHighFeeRate = new(10_000m);
 
 	// Defined in hours. Do not modify these values or the order!
 	public static readonly int[] CoinJoinFeeRateMedianTimeFrames = new[] { 24, 168, 720 };
