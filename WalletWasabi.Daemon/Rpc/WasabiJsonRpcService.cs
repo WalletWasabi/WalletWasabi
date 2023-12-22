26e5cbd93e3beda0ffeee4422e7e5802c4fc5152
adamPetho <45069029+adamPetho@users.noreply.github.com>
2023-12-22 12:39:16 +0100

    revert RPC service changes

--- WalletWasabi.Daemon/Rpc/WasabiJsonRpcService.cs
+++ WalletWasabi.Daemon/Rpc/WasabiJsonRpcService.cs
@@ -221,7 +221,7 @@ public class WasabiJsonRpcService : IJsonRpcService
 	}
 
 	[JsonRpcMethod("build")]
-	public string BuildTransaction(PaymentInfo[] payments, OutPoint[] coins, int? feeTarget = null, decimal? feeRate = null, string? password = null, bool overrideFeeOverpaymentProtection = false)
+	public string BuildTransaction(PaymentInfo[] payments, OutPoint[] coins, int? feeTarget = null, decimal? feeRate = null, string? password = null)
 	{
 		Guard.NotNull(nameof(payments), payments);
 		Guard.NotNull(nameof(coins), coins);
@@ -250,8 +250,7 @@ public class WasabiJsonRpcService : IJsonRpcService
 			payment,
 			feeStrategy,
 			allowUnconfirmed: true,
-			allowedInputs: coins,
-			overrideFeeOverpaymentProtection: overrideFeeOverpaymentProtection);
+			allowedInputs: coins);
 		var smartTx = result.Transaction;
 
 		return smartTx.Transaction.ToHex();
