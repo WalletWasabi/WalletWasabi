22de663c1f92c912bf2f48f3edd46e37fb27d415
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-01-08 22:34:02 +0100

    Add initial file picker api

--- WalletWasabi.Fluent/Helpers/FileDialogHelper.cs
+++ WalletWasabi.Fluent/Helpers/FileDialogHelper.cs
@@ -6,11 +6,162 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Avalonia.Platform.Storage;
+using Avalonia.VisualTree;
 
 namespace WalletWasabi.Fluent.Helpers;
 
 public static class FileDialogHelper
 {
+	private static FilePickerFileType All { get; } = new("All")
+    {
+        Patterns = new[] { "*.*" },
+        MimeTypes = new[] { "*/*" }
+    };
+
+    private static FilePickerFileType Json { get; } = new("Json")
+    {
+        Patterns = new[] { "*.json" },
+        AppleUniformTypeIdentifiers = new[] { "public.json" },
+        MimeTypes = new[] { "application/json" }
+    };
+
+    private static FilePickerFileType Text { get; } = new("Text")
+    {
+        Patterns = new[] { "*.txt" },
+        AppleUniformTypeIdentifiers = new[] { "public.text" },
+        MimeTypes = new[] { "text/plain" }
+    };
+
+    private static FilePickerFileType Psbt { get; } = new("psbt")
+    {
+	    Patterns = new[] { "*.psbt" },
+	    MimeTypes = new[] { "*/*" }
+    };
+
+    private static FilePickerFileType Txn { get; } = new("txn")
+    {
+	    Patterns = new[] { "*.txn" },
+	    MimeTypes = new[] { "*/*" }
+    };
+
+    public static FilePickerFileType Png { get; } = new("Png")
+    {
+	    Patterns = new[] { "*.png" },
+	    AppleUniformTypeIdentifiers = new[] { "public.png" },
+	    MimeTypes = new[] { "image/png" }
+    };
+
+    private static IStorageProvider? GetStorageProvider()
+    {
+        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime { MainWindow: { } window })
+        {
+            return window.StorageProvider;
+        }
+
+        if (Application.Current?.ApplicationLifetime is ISingleViewApplicationLifetime { MainView: { } mainView })
+        {
+            var visualRoot = mainView.GetVisualRoot();
+            if (visualRoot is TopLevel topLevel)
+            {
+                return topLevel.StorageProvider;
+            }
+        }
+
+        return null;
+    }
+
+    private static List<FilePickerFileType> GetFilePickerFileTypes(List<string> fileTypes)
+    {
+        var fileTypeFilters = new List<FilePickerFileType>();
+
+        foreach (var fileType in fileTypes)
+        {
+            switch (fileType)
+            {
+                case "All":
+                {
+                    fileTypeFilters.Add(All);
+                    break;
+                }
+                case "Json":
+                {
+                    fileTypeFilters.Add(Json);
+                    break;
+                }
+                case "Text":
+                {
+                    fileTypeFilters.Add(Text);
+                    break;
+                }
+                case "psbt":
+                {
+	                fileTypeFilters.Add(Psbt);
+	                break;
+                }
+                case "txn":
+                {
+	                fileTypeFilters.Add(Txn);
+	                break;
+                }
+                case "Png":
+                {
+	                fileTypeFilters.Add(Png);
+	                break;
+                }
+            }
+        }
+
+        return fileTypeFilters;
+    }
+
+    public static async Task OpenFileAsync(Func<Stream, Task> callback, List<string> fileTypes, string title)
+    {
+        var storageProvider = GetStorageProvider();
+        if (storageProvider is null)
+        {
+            return;
+        }
+
+        var result = await storageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = title,
+            FileTypeFilter = GetFilePickerFileTypes(fileTypes),
+            AllowMultiple = false
+        });
+
+        var file = result.FirstOrDefault();
+        if (file is not null)
+        {
+            await using var stream = await file.OpenReadAsync();
+            await callback(stream);
+        }
+    }
+
+    public static async Task SaveFileAsync(Func<Stream, Task> callback, List<string> fileTypes, string title, string fileName, string defaultExtension)
+    {
+        var storageProvider = GetStorageProvider();
+        if (storageProvider is null)
+        {
+            return;
+        }
+
+        var file = await storageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+        {
+            Title = title,
+            FileTypeChoices = GetFilePickerFileTypes(fileTypes),
+            SuggestedFileName = fileName,
+            DefaultExtension = defaultExtension,
+            ShowOverwritePrompt = true
+        });
+
+        if (file is not null)
+        {
+            await using var stream = await file.OpenWriteAsync();
+            await callback(stream);
+        }
+    }
+
 	public static async Task<string?> ShowOpenFileDialogAsync(string title)
 	{
 		var ofd = CreateOpenFileDialog(title);
