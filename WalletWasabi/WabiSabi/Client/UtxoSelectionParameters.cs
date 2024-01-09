dd83b3f9dbc82c786e73848e5c67d9d3da8adfe2
Lucas Ontivero <lucasontivero@gmail.com>
2024-01-09 13:38:28 -0300

    Hardcode wasabi script types (temporarily)

--- WalletWasabi/WabiSabi/Client/UtxoSelectionParameters.cs
+++ WalletWasabi/WabiSabi/Client/UtxoSelectionParameters.cs
@@ -18,12 +18,43 @@ public record UtxoSelectionParameters(
 	public static UtxoSelectionParameters FromRoundParameters(RoundParameters roundParameters, WasabiRandom? random = null)
 	{
 		random ??= SecureRandom.Instance;
-
 		return new(
 			roundParameters.AllowedInputAmounts,
-			roundParameters.CalculateReasonableOutputAmountRange(random),
+			ReasonableOutputAmountRange(roundParameters, random),
 			roundParameters.CoordinationFeeRate,
 			roundParameters.MiningFeeRate,
 			roundParameters.AllowedInputTypes);
+	}
+
+	private static MoneyRange ReasonableOutputAmountRange(RoundParameters roundParameters, WasabiRandom random)
+	{
+		var scriptTypesSupportedByWallet= new[] { ScriptType.P2WPKH, ScriptType.Taproot }; // I doubt this will change
+		var outputTypes = roundParameters.AllowedOutputTypes.Intersect(scriptTypesSupportedByWallet);
+		var maxVsizeInputOutputPairScriptType = outputTypes.MaxBy(x => x.EstimateInputVsize() + x.EstimateOutputVsize());
+		var smallestEffectiveDenom = DenominationBuilder.CreateDenominations(
+				roundParameters.CalculateMinReasonableOutputAmount(),
+				roundParameters.AllowedOutputAmounts.Max,
+				roundParameters.MiningFeeRate,
+				[maxVsizeInputOutputPairScriptType],
+				random)
+			.Min(x => x.EffectiveCost);
+		var smallestReasonableEffectiveDenomination =
+			smallestEffectiveDenom
+		    ?? throw new InvalidOperationException("Something's wrong with the denomination creation or with the parameters it got.");
+
+		return roundParameters.AllowedOutputAmounts with { Min = smallestReasonableEffectiveDenomination };
+	}
+}
+
+public static class RoundParametersExtensions
+{
+	/// <returns>Min: must be larger than the smallest economical denom. Max: max allowed in the round.</returns>
+	/// <returns>Min output amount that's economically reasonable to be registered with current network conditions.</returns>
+	/// <remarks>It won't be smaller than min allowed output amount.</remarks>
+	public static Money CalculateMinReasonableOutputAmount(this RoundParameters roundParameters)
+	{
+		var maxVsizeInputOutputPair = roundParameters.AllowedOutputTypes.Max(x => x.EstimateInputVsize() + x.EstimateOutputVsize());
+		var minEconomicalOutput = roundParameters.MiningFeeRate.GetFee(maxVsizeInputOutputPair);
+		return Math.Max(minEconomicalOutput, roundParameters.AllowedOutputAmounts.Min);
 	}
 }
