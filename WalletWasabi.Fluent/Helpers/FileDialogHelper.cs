7e9cda69b061fd6047af272426fb25c9897b2158
Yahia Chiheb <chihebyahia@gmail.com>
2024-01-11 09:30:28 +0100

    Fix formatting

--- WalletWasabi.Fluent/Helpers/FileDialogHelper.cs
+++ WalletWasabi.Fluent/Helpers/FileDialogHelper.cs
@@ -14,149 +14,149 @@ namespace WalletWasabi.Fluent.Helpers;
 public static class FileDialogHelper
 {
 	private static FilePickerFileType All { get; } = new("All files")
-    {
-        Patterns = new[] { "*.*" },
-        MimeTypes = new[] { "*/*" }
-    };
+	{
+		Patterns = new[] { "*.*" },
+		MimeTypes = new[] { "*/*" }
+	};
 
-    private static FilePickerFileType Json { get; } = new("JSON files")
-    {
-        Patterns = new[] { "*.json" },
-        AppleUniformTypeIdentifiers = new[] { "public.json" },
-        MimeTypes = new[] { "application/json" }
-    };
+	private static FilePickerFileType Json { get; } = new("JSON files")
+	{
+		Patterns = new[] { "*.json" },
+		AppleUniformTypeIdentifiers = new[] { "public.json" },
+		MimeTypes = new[] { "application/json" }
+	};
 
-    private static FilePickerFileType Text { get; } = new("TXT files")
-    {
-        Patterns = new[] { "*.txt" },
-        AppleUniformTypeIdentifiers = new[] { "public.text" },
-        MimeTypes = new[] { "text/plain" }
-    };
+	private static FilePickerFileType Text { get; } = new("TXT files")
+	{
+		Patterns = new[] { "*.txt" },
+		AppleUniformTypeIdentifiers = new[] { "public.text" },
+		MimeTypes = new[] { "text/plain" }
+	};
 
-    private static FilePickerFileType Psbt { get; } = new("PSBT files")
-    {
-	    Patterns = new[] { "*.psbt" },
-	    MimeTypes = new[] { "*/*" }
-    };
+	private static FilePickerFileType Psbt { get; } = new("PSBT files")
+	{
+		Patterns = new[] { "*.psbt" },
+		MimeTypes = new[] { "*/*" }
+	};
 
-    private static FilePickerFileType Txn { get; } = new("TXN files")
-    {
-	    Patterns = new[] { "*.txn" },
-	    MimeTypes = new[] { "*/*" }
-    };
+	private static FilePickerFileType Txn { get; } = new("TXN files")
+	{
+		Patterns = new[] { "*.txn" },
+		MimeTypes = new[] { "*/*" }
+	};
 
-    private static FilePickerFileType Png { get; } = new("PNG files")
-    {
-	    Patterns = new[] { "*.png" },
-	    AppleUniformTypeIdentifiers = new[] { "public.png" },
-	    MimeTypes = new[] { "image/png" }
-    };
+	private static FilePickerFileType Png { get; } = new("PNG files")
+	{
+		Patterns = new[] { "*.png" },
+		AppleUniformTypeIdentifiers = new[] { "public.png" },
+		MimeTypes = new[] { "image/png" }
+	};
 
-    private static IStorageProvider? GetStorageProvider()
-    {
-        if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime { MainWindow: { } window })
-        {
-            return window.StorageProvider;
-        }
+	private static IStorageProvider? GetStorageProvider()
+	{
+		if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime { MainWindow: { } window })
+		{
+			return window.StorageProvider;
+		}
 
-        if (Application.Current?.ApplicationLifetime is ISingleViewApplicationLifetime { MainView: { } mainView })
-        {
-            var visualRoot = mainView.GetVisualRoot();
-            if (visualRoot is TopLevel topLevel)
-            {
-                return topLevel.StorageProvider;
-            }
-        }
+		if (Application.Current?.ApplicationLifetime is ISingleViewApplicationLifetime { MainView: { } mainView })
+		{
+			var visualRoot = mainView.GetVisualRoot();
+			if (visualRoot is TopLevel topLevel)
+			{
+				return topLevel.StorageProvider;
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    private static List<FilePickerFileType> GetFilePickerFileTypes(string[] filterExtTypes)
-    {
-        var fileTypeFilters = new List<FilePickerFileType>();
+	private static List<FilePickerFileType> GetFilePickerFileTypes(string[] filterExtTypes)
+	{
+		var fileTypeFilters = new List<FilePickerFileType>();
 
-        foreach (var fileType in filterExtTypes)
-        {
-            switch (fileType)
-            {
-                case "*":
-                {
-                    fileTypeFilters.Add(All);
-                    break;
-                }
-                case "json":
-                {
-                    fileTypeFilters.Add(Json);
-                    break;
-                }
-                case "txt":
-                {
-                    fileTypeFilters.Add(Text);
-                    break;
-                }
-                case "psbt":
-                {
-	                fileTypeFilters.Add(Psbt);
-	                break;
-                }
-                case "txn":
-                {
-	                fileTypeFilters.Add(Txn);
-	                break;
-                }
-                case "png":
-                {
-	                fileTypeFilters.Add(Png);
-	                break;
-                }
-            }
-        }
+		foreach (var fileType in filterExtTypes)
+		{
+			switch (fileType)
+			{
+				case "*":
+					{
+						fileTypeFilters.Add(All);
+						break;
+					}
+				case "json":
+					{
+						fileTypeFilters.Add(Json);
+						break;
+					}
+				case "txt":
+					{
+						fileTypeFilters.Add(Text);
+						break;
+					}
+				case "psbt":
+					{
+						fileTypeFilters.Add(Psbt);
+						break;
+					}
+				case "txn":
+					{
+						fileTypeFilters.Add(Txn);
+						break;
+					}
+				case "png":
+					{
+						fileTypeFilters.Add(Png);
+						break;
+					}
+			}
+		}
 
-        return fileTypeFilters;
-    }
+		return fileTypeFilters;
+	}
 
-    public static async Task<IStorageFile?> OpenFileAsync(string title, string[] filterExtTypes, string? initialFileName = null, string? directory = null)
-    {
-	    // TODO: initialFileName is not supported, remove the parameter
-        var storageProvider = GetStorageProvider();
-        if (storageProvider is null)
-        {
-            return null;
-        }
+	public static async Task<IStorageFile?> OpenFileAsync(string title, string[] filterExtTypes, string? initialFileName = null, string? directory = null)
+	{
+		// TODO: initialFileName is not supported, remove the parameter
+		var storageProvider = GetStorageProvider();
+		if (storageProvider is null)
+		{
+			return null;
+		}
 
-        var suggestedStartLocation = await GetSuggestedStartLocationAsync(directory, storageProvider);
+		var suggestedStartLocation = await GetSuggestedStartLocationAsync(directory, storageProvider);
 
-        var result = await storageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
-        {
-            Title = title,
-            SuggestedStartLocation = suggestedStartLocation,
-            FileTypeFilter = GetFilePickerFileTypes(filterExtTypes),
-            AllowMultiple = false
-        });
+		var result = await storageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+		{
+			Title = title,
+			SuggestedStartLocation = suggestedStartLocation,
+			FileTypeFilter = GetFilePickerFileTypes(filterExtTypes),
+			AllowMultiple = false
+		});
 
-        return result.FirstOrDefault();
-    }
+		return result.FirstOrDefault();
+	}
 
-    public static async Task<IStorageFile?> SaveFileAsync(string title, string[] filterExtTypes, string? initialFileName = null, string? directory = null)
-    {
-        var storageProvider = GetStorageProvider();
-        if (storageProvider is null)
-        {
-            return null;
-        }
+	public static async Task<IStorageFile?> SaveFileAsync(string title, string[] filterExtTypes, string? initialFileName = null, string? directory = null)
+	{
+		var storageProvider = GetStorageProvider();
+		if (storageProvider is null)
+		{
+			return null;
+		}
 
-        var suggestedStartLocation = await GetSuggestedStartLocationAsync(directory, storageProvider);
+		var suggestedStartLocation = await GetSuggestedStartLocationAsync(directory, storageProvider);
 
-        return await storageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
-        {
-            Title = title,
-            FileTypeChoices = GetFilePickerFileTypes(filterExtTypes),
-            SuggestedFileName = initialFileName,
-            DefaultExtension = filterExtTypes.FirstOrDefault(),
-            SuggestedStartLocation = suggestedStartLocation,
-            ShowOverwritePrompt = true
-        });
-    }
+		return await storageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+		{
+			Title = title,
+			FileTypeChoices = GetFilePickerFileTypes(filterExtTypes),
+			SuggestedFileName = initialFileName,
+			DefaultExtension = filterExtTypes.FirstOrDefault(),
+			SuggestedStartLocation = suggestedStartLocation,
+			ShowOverwritePrompt = true
+		});
+	}
 
 	private static async Task<IStorageFolder?> GetSuggestedStartLocationAsync(string? directory, IStorageProvider storageProvider)
 	{
@@ -176,6 +176,5 @@ public static class FileDialogHelper
 		}
 
 		return null;
-
 	}
 }
