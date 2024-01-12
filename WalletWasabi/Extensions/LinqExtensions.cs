81e628c62af2e3aa35b7687954721f35f1964c39
Kimi <58662979+kiminuo@users.noreply.github.com>
2024-01-12 09:51:08 +0100

    Remove `LinqExtensions.Batch` (#12235)

--- WalletWasabi/Extensions/LinqExtensions.cs
+++ WalletWasabi/Extensions/LinqExtensions.cs
@@ -7,37 +7,6 @@ namespace WalletWasabi.Extensions;
 
 public static class LinqExtensions
 {
-	public static IEnumerable<IEnumerable<T>> Batch<T>(
-	   this IEnumerable<T> source, int size)
-	{
-		T[]? bucket = null;
-		var count = 0;
-
-		foreach (var item in source)
-		{
-			bucket ??= new T[size];
-
-			bucket[count++] = item;
-
-			if (count != size)
-			{
-				continue;
-			}
-
-			yield return bucket.Select(x => x);
-
-			bucket = null;
-			count = 0;
-		}
-
-		// Return the last bucket with all remaining elements
-		if (bucket is { } && count > 0)
-		{
-			Array.Resize(ref bucket, count);
-			yield return bucket.Select(x => x);
-		}
-	}
-
 	public static T? RandomElement<T>(this IEnumerable<T> source, WasabiRandom random)
 	{
 		T? current = default;
