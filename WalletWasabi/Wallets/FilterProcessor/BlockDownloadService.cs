5a50ac13446a4fa463c8a2fa5f6f05612c0a2388
Kiminuo <kiminuo@protonmail.com>
2024-01-12 09:14:25 +0100

    Add results of operations

--- WalletWasabi/Wallets/FilterProcessor/BlockDownloadService.cs
+++ WalletWasabi/Wallets/FilterProcessor/BlockDownloadService.cs
@@ -9,7 +9,7 @@ using WalletWasabi.Logging;
 namespace WalletWasabi.Wallets.FilterProcessor;
 
 /// <summary>
-/// Service that prioritizes blocks downloads.
+/// Service that downloads blocks downloads.
 /// </summary>
 public class BlockDownloadService : BackgroundService
 {
@@ -43,10 +43,10 @@ public class BlockDownloadService : BackgroundService
 	/// <summary>
 	/// Add a block hash to the queue to be downloaded.
 	/// </summary>
-	public TaskCompletionSource Enqueue(uint256 blockHash, Priority priority) =>
-		Enqueue(new Request(blockHash, priority, 1, new TaskCompletionSource()));
+	public TaskCompletionSource<IResult> Enqueue(uint256 blockHash, Priority priority) =>
+		Enqueue(new Request(blockHash, priority, 1, new TaskCompletionSource<IResult>()));
 
-	private TaskCompletionSource Enqueue(Request request)
+	private TaskCompletionSource<IResult> Enqueue(Request request)
 	{
 		lock (Lock)
 		{
@@ -71,7 +71,6 @@ public class BlockDownloadService : BackgroundService
 	/// </remarks>
 	public void RemoveBlocks(uint maxBlockHeight)
 	{
-		// TODO: Handle correctly TCS cancellation.
 		PriorityQueue<Request, Priority> tempQueue = new(Priority.Comparer);
 
 		lock (Lock)
@@ -88,6 +87,11 @@ public class BlockDownloadService : BackgroundService
 				if (priority.BlockHeight <= maxBlockHeight)
 				{
 					tempQueue.Enqueue(request, priority);
+				}
+				else
+				{
+					// The block might have been downloaded by now so just try to set the result.
+					_ = request.Tcs.TrySetResult(new ReorgOccurredResult(NewBlockchainHeight: maxBlockHeight));
 				}
 			}
 
@@ -126,7 +130,7 @@ public class BlockDownloadService : BackgroundService
 					for (int i = 0; i < toStart; i++)
 					{
 						// Dequeue does not provide priority value.
-						if (!BlocksToDownload.TryDequeue(out Request? request, out Priority? priority))
+						if (!BlocksToDownload.TryDequeue(out Request? queuedRequest, out Priority? priority))
 						{
 							throw new UnreachableException("Failed to dequeue block from the queue.");
 						}
@@ -136,13 +140,13 @@ public class BlockDownloadService : BackgroundService
 							{
 								try
 								{
-									Block? block = await BlockProvider.TryGetBlockAsync(request.BlockHash, cancellationToken).ConfigureAwait(false);
+									Block? block = await BlockProvider.TryGetBlockAsync(queuedRequest.BlockHash, cancellationToken).ConfigureAwait(false);
 
-									return new RequestResult(request, block);
+									return new RequestResult(queuedRequest, block);
 								}
 								catch (Exception ex)
 								{
-									Logger.LogError($"Exception thrown while getting block {request.BlockHash} (height: {request.Priority.BlockHeight})", ex);
+									Logger.LogError($"Exception thrown while getting block {queuedRequest.BlockHash} (height: {queuedRequest.Priority.BlockHeight})", ex);
 									throw;
 								}
 							},
@@ -161,27 +165,29 @@ public class BlockDownloadService : BackgroundService
 				activeTasks.Remove(task);
 
 				RequestResult result = await task.ConfigureAwait(false);
+				Request request = result.Request;
 
-				// If the block was downloaded OK, we suppose that it's stored on disk and it can be fetched fast.
-				if (result.Block is null)
+				if (result.Block is not null)
 				{
-					Request request = result.Request;
-
+					_ = request.Tcs.TrySetResult(new SuccessResult(result.Block));
+				}
+				else
+				{
 					if (request.Attempts >= MaxFailedAttempts)
 					{
 						Logger.LogInfo($"Attempt to download block {request.BlockHash} (height: {request.Priority.BlockHeight}) failed {MaxFailedAttempts} times. Dropping the request.");
+
+						// The block might have been removed concurrently if a reorg occurred.
+						if (!request.Tcs.TrySetResult(new FailureResult(request.Attempts)))
+						{
+							IResult opResult = await request.Tcs.Task.ConfigureAwait(false);
+							Logger.LogDebug($"Failed to set result for block '{request.BlockHash}' (height: {request.Priority.BlockHeight}). Result is already: {opResult}");
+						}
 					}
 					else
 					{
 						// Re-enqueue as we failed to download the block.
 						Enqueue(request with { Attempts = request.Attempts + 1 });
-					}
-				}
-				else
-				{
-					if (!result.Request.Tcs.TrySetResult())
-					{
-						throw new UnreachableException($"Request '{result.Request}' has already terminated.");
 					}
 				}
 			}
@@ -195,8 +201,46 @@ public class BlockDownloadService : BackgroundService
 			Logger.LogError(ex);
 			throw;
 		}
+		finally
+		{
+			// Mark everything as cancelled because the service is shutting down (either gracefully or forcibly).
+			lock (Lock)
+			{
+				while (BlocksToDownload.TryDequeue(out Request? request, out _))
+				{
+					_ = request.Tcs.TrySetResult(CancelledResult.Instance);
+				}
+			}
+		}
 	}
 
-	internal record Request(uint256 BlockHash, Priority Priority, int Attempts, TaskCompletionSource Tcs);
+	/// <param name="Tcs">By design, this task completion source is not supposed to be ended by </param>
+	internal record Request(uint256 BlockHash, Priority Priority, uint Attempts, TaskCompletionSource<IResult> Tcs);
 	private record RequestResult(Request Request, Block? Block);
+
+	/// <summary>Result object describing if/how object was downloaded using the block downloading service.</summary>
+	public interface IResult { }
+
+	/// <summary>Denotes a failure of getting a block.</summary>
+	public interface IFailureResult : IResult { }
+
+	/// <summary>Block was downloaded successfully.</summary>
+	public record SuccessResult(Block Block) : IResult;
+
+	/// <summary>Block could not be get because a blockchain reorg occurred.</summary>
+	/// <remarks>
+	/// The result announces that getting a certain block does not really makes sense because it got evicted by other nodes.
+	/// <para>There is a chance that one downloads given block sooner than a reorg is announced so that possibility must be taken into account by the caller.</para>
+	/// </remarks>
+	public record ReorgOccurredResult(uint NewBlockchainHeight) : IFailureResult;
+
+	/// <summary>Block could not be get for an unknown reason from any block providing source.</summary>
+	/// <remarks>Trying to get the block later might help or it might not. There is no guarantee.</remarks>
+	public record FailureResult(uint Attempts) : IFailureResult;
+
+	/// <summary>Block could not be get because the service is shutting down.</summary>
+	public record CancelledResult() : IFailureResult
+	{
+		public static readonly CancelledResult Instance = new();
+	}
 }
