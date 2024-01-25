511f7af16dc9a325a7aa0557b6cbf28ffe3e3012
Turbolay <clement.ogame@gmail.com>
2024-01-24 21:29:21 -0600

    Set max filters in memory to 20000 for more impressive results on testnet

--- WalletWasabi/Wallets/FilterProcessor/BlockFilterIterator.cs
+++ WalletWasabi/Wallets/FilterProcessor/BlockFilterIterator.cs
@@ -13,7 +13,7 @@ namespace WalletWasabi.Wallets.FilterProcessor;
 /// </summary>
 public class BlockFilterIterator
 {
-	public BlockFilterIterator(IIndexStore indexStore, int maxNumberFiltersInMemory = 2000)
+	public BlockFilterIterator(IIndexStore indexStore, int maxNumberFiltersInMemory = 20000)
 	{
 		IndexStore = indexStore;
 		MaxNumberFiltersInMemory = maxNumberFiltersInMemory;
