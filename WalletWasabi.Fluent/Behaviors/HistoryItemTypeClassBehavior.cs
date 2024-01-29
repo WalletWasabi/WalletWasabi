cd06a116665f527add4b788be1988fd13a29a4a2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-01-29 16:21:37 +0100

    Use different color for the Amount field in the case if it is positive

--- WalletWasabi.Fluent/Behaviors/HistoryItemTypeClassBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/HistoryItemTypeClassBehavior.cs
@@ -2,6 +2,7 @@ using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using NBitcoin;
 using WalletWasabi.Fluent.ViewModels.Wallets.Home.History.HistoryItems;
 
 namespace WalletWasabi.Fluent.Behaviors;
@@ -15,6 +16,8 @@ public class HistoryItemTypeClassBehavior : AttachedToVisualTreeBehavior<Avaloni
 	private const string CoinJoinsClass = "CoinJoins";
 
 	private const string SpeedUpClass = "SpeedUp";
+
+	private const string PositiveAmountClass = "PositiveAmount";
 
 	protected override void OnAttachedToVisualTree(CompositeDisposable disposable)
 	{
@@ -42,6 +45,13 @@ public class HistoryItemTypeClassBehavior : AttachedToVisualTreeBehavior<Avaloni
 
 	private void AddClasses(TreeDataGridRow row)
 	{
+		if (row.DataContext is HistoryItemViewModelBase historyItemViewModelBase)
+		{
+			if (historyItemViewModelBase.Transaction.DisplayAmount > Money.Zero)
+			{
+				row.Classes.Add(PositiveAmountClass);
+			}
+		}
 		switch (row.DataContext)
 		{
 			case TransactionHistoryItemViewModel:
@@ -64,6 +74,7 @@ public class HistoryItemTypeClassBehavior : AttachedToVisualTreeBehavior<Avaloni
 
 	private void RemoveClasses(TreeDataGridRow row)
 	{
+		_ = row.Classes.Remove(PositiveAmountClass);
 		_ = row.Classes.Remove(TransactionClass);
 		_ = row.Classes.Remove(CoinJoinClass);
 		_ = row.Classes.Remove(CoinJoinsClass);
