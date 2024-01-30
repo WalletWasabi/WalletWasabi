9cefc9d2c5b95e4eebef2a61ff888afc0302dd60
Turbolay <clement.ogame@gmail.com>
2024-01-30 11:11:32 -0600

    Improve batch fetching algo

--- WalletWasabi.Backend/Controllers/BlockchainController.cs
+++ WalletWasabi.Backend/Controllers/BlockchainController.cs
@@ -456,50 +456,44 @@ public class BlockchainController : ControllerBase
 
 		while (toFetchFeeList.Count > 0)
 		{
+			var currentTx = toFetchFeeList.First();
+
 			List<Coin> inputs = new();
 			HashSet<Transaction> parentTxs = new();
 
-			// Collect which transactions needs to be fetched.
-			List<TxIn> inputsToFetch = new();
+			void AddParentTx(Transaction tx, OutPoint prevOut)
+			{
+				parentTxs.Add(tx);
+				TxOut txOut = tx.Outputs[prevOut.N];
+				inputs.Add(new Coin(prevOut, txOut));
+			}
 
-			var currentTx = toFetchFeeList.First();
+			// Collect which transactions needs to be fetched.
+			List<OutPoint> prevOutToFetchFromRPC = new();
+
 			foreach (var input in currentTx.Inputs)
 			{
-				if (!transactionsLocalCache.TryGetValue(input.PrevOut.Hash, out var parentTx))
+				if (transactionsLocalCache.TryGetValue(input.PrevOut.Hash, out var parentTx))
 				{
-					inputsToFetch.Add(input);
+					AddParentTx(parentTx, input.PrevOut);
 				}
 				else
 				{
-					parentTxs.Add(parentTx);
+					prevOutToFetchFromRPC.Add(input.PrevOut);
 				}
 			}
 
-			if (inputsToFetch.Count == 1)
+			if (prevOutToFetchFromRPC.Count > 0)
 			{
-				var input = inputsToFetch.Single();
-				var missingTx = await RpcClient.GetRawTransactionAsync(input.PrevOut.Hash, true, cancellationToken);
-				parentTxs.Add(missingTx);
-			}
-			else
-			{
-				var missingTxs = await RpcClient.GetRawTransactionsAsync(inputsToFetch.Select(x => x.PrevOut.Hash), cancellationToken);
+				var missingTxs = prevOutToFetchFromRPC.Count == 1 ?
+					new [] { await RpcClient.GetRawTransactionAsync(prevOutToFetchFromRPC.Single().Hash, true, cancellationToken) } :
+					await RpcClient.GetRawTransactionsAsync(prevOutToFetchFromRPC.Select(x => x.Hash), cancellationToken);
+
 				foreach (var tx in missingTxs)
 				{
-					parentTxs.Add(tx);
+					var prevOut = prevOutToFetchFromRPC.First(x => x.Hash == tx.GetHash());
+					AddParentTx(tx, prevOut);
 				}
-			}
-
-			foreach (var parentTx in parentTxs)
-			{
-				if (!transactionsLocalCache.ContainsKey(parentTx.GetHash()))
-				{
-					transactionsLocalCache.Add(parentTx.GetHash(), parentTx);
-				}
-
-				var input = currentTx.Inputs.Where(input => input.PrevOut.Hash == parentTx.GetHash()).First();
-				TxOut txOut = parentTx.Outputs[input.PrevOut.N];
-				inputs.Add(new Coin(input.PrevOut, txOut));
 			}
 
 			var unconfirmedParents = parentTxs.Where(x =>
