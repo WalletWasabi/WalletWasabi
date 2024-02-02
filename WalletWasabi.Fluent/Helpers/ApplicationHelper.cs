a14e9b8a8233908644e2c1dfc829154a673503e7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-02-02 19:40:36 +0100

    Encapsulate clipboard access in ApplicationHelper and make sure always run on main UI thread

--- WalletWasabi.Fluent/Helpers/ApplicationHelper.cs
+++ WalletWasabi.Fluent/Helpers/ApplicationHelper.cs
@@ -1,23 +1,49 @@
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Input.Platform;
+using Avalonia.Threading;
 using Avalonia.VisualTree;
 using ReactiveUI;
-using WalletWasabi.Fluent.Views;
 
 namespace WalletWasabi.Fluent.Helpers;
 
 public static class ApplicationHelper
 {
 	private static readonly TimeSpan PollingInterval = TimeSpan.FromSeconds(0.2);
+
 	public static Window? MainWindow => (Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow;
 
-	public static IClipboard? Clipboard => GetClipboard();
+	public static async Task<string> GetTextAsync()
+	{
+		if (GetClipboard() is { } clipboard)
+		{
+			return await Dispatcher.UIThread.InvokeAsync(async () => await clipboard.GetTextAsync() ?? "");
+		}
 
-	public static IClipboard? GetClipboard()
+		return await Task.FromResult("");
+	}
+
+	public static async Task SetTextAsync(string? text)
+	{
+		if (GetClipboard() is { } clipboard && text is not null)
+		{
+			await Dispatcher.UIThread.InvokeAsync(async () => await clipboard.SetTextAsync(text));
+		}
+	}
+
+	public static async Task ClearAsync()
+	{
+		if (GetClipboard() is { } clipboard)
+		{
+			await Dispatcher.UIThread.InvokeAsync(async () => await clipboard.ClearAsync());
+		}
+	}
+
+	private static IClipboard? GetClipboard()
 	{
 		if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime { MainWindow: { } window })
 		{
@@ -59,14 +85,9 @@ public static class ApplicationHelper
 
 	public static IObservable<string?> ClipboardTextChanged(IScheduler? scheduler = default)
 	{
-		if (Clipboard == null)
-		{
-			return Observable.Return<string?>(null);
-		}
-
 		return Observable.Timer(PollingInterval, scheduler ?? Scheduler.Default)
 			.Repeat()
-			.Select(_ => Observable.FromAsync(() => Clipboard.GetTextAsync(), RxApp.MainThreadScheduler))
+			.Select(_ => Observable.FromAsync(GetTextAsync, RxApp.MainThreadScheduler))
 			.Merge(1)
 			.DistinctUntilChanged();
 	}
