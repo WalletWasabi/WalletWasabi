192cd8903e9dc068b188533bb25a6fb655fec26e
Roland Soós <soos.roland93@gmail.com>
2024-02-04 14:57:05 +0100

    Move record to separate file

--- WalletWasabi.Fluent/Models/Currency/CurrencyFormat.cs
+++ WalletWasabi.Fluent/Models/Currency/CurrencyFormat.cs
@@ -1,7 +1,4 @@
-using System.Linq;
 using ReactiveUI;
-using WalletWasabi.Fluent.Models.Wallets;
-using WalletWasabi.Userfacing;
 
 namespace WalletWasabi.Fluent.Models.Currency;
 
@@ -47,50 +44,4 @@ public partial class CurrencyFormat : ReactiveObject
 	public int? MaxLength { get; init; }
 	public string? DefaultWatermark { get; set; }
 	public bool ForceFractionalFormatToMaxFractionalDigits { get; init; }
-}
-
-public abstract record CurrencyValue
-{
-	public static readonly Empty EmptyValue = new();
-
-	public static CurrencyValue FromBtc(Amount? amount)
-	{
-		return amount switch
-		{
-			null => EmptyValue,
-			Amount x when x == Amount.Invalid => new Invalid(),
-			Amount x => new Valid(x.Btc.ToDecimal(NBitcoin.MoneyUnit.BTC))
-		};
-	}
-
-	public static CurrencyValue FromUsd(Amount? amount)
-	{
-		return amount switch
-		{
-			null => EmptyValue,
-			Amount x when x == Amount.Invalid => new Invalid(),
-			Amount x => new Valid(x.UsdValue)
-		};
-	}
-
-	public record Valid(decimal Value) : CurrencyValue;
-
-	public record Invalid : CurrencyValue;
-
-	public record Empty : CurrencyValue;
-
-	public decimal? ToDecimal()
-	{
-		return this switch
-		{
-			Valid v => v.Value,
-			_ => null
-		};
-	}
-
-	public string ToInvariantFormatString()
-	{
-		var decimalValue = ToDecimal();
-		return CurrencyInput.Format(decimalValue);
-	}
 }
