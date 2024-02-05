f94435296bf27b852d06f0962e0a8fdc24ad5d05
Lucas Ontivero <lucasontivero@gmail.com>
2024-02-05 12:25:34 -0300

    Satoshi WebSocket Handler

---
+++ WalletWasabi/Synchronizarion/FilterMessage.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using WalletWasabi.Backend.Models;
+using WalletWasabi.Extensions;
+
+namespace WalletWasabi.Synchronizarion;
+
+public enum RequestMessage
+{
+	BestKnowBlockHash
+}
+
+public enum ResponseMessage
+{
+	Filter,
+	HandshakeError
+}
+
+public record FilterMessage(FilterModel filterModel)
+{
+	public byte[] ToByteArray()
+	{
+		using var mem = new MemoryStream();
+		using var writer = new BinaryWriter(mem);
+
+		writer.Write((byte)ResponseMessage.Filter);
+		writer.Write(filterModel);
+
+		return mem.ToArray();
+	}
+}
