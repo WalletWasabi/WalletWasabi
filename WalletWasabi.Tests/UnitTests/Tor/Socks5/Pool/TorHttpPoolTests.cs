d497fb97dcce424663dc92108cd08c4a874f8789
Dávid Molnár <molnardavid84@gmail.com>
2024-02-06 11:10:18 +0100

    Restore double line

--- WalletWasabi.Tests/UnitTests/Tor/Socks5/Pool/TorHttpPoolTests.cs
+++ WalletWasabi.Tests/UnitTests/Tor/Socks5/Pool/TorHttpPoolTests.cs
@@ -280,9 +280,8 @@ public class TorHttpPoolTests
 				Content-Length: 0
 				X-GitHub-Request-Id: AFCA:0EE6:208343C:21F2436:6395A726
 
-				"""
-				+ "\r\n" // Adding the newline like this otherwise CodeMaid removes the double empty lines.
-				.ReplaceLineEndings("\r\n");
+				
+				""".ReplaceLineEndings("\r\n");
 
 			await serverWriter1.WriteAsync(serverResponse1.AsMemory(), timeoutCts.Token);
 			await serverWriter1.FlushAsync().WaitAsync(timeoutCts.Token);
