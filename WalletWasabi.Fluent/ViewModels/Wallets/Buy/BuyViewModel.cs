eb38cbf4586afd175c4f3354f5dbf90edc802117
Roland Soós <soos.roland93@gmail.com>
2024-02-12 16:27:30 +0100

    fix solution

--- WalletWasabi.Fluent/ViewModels/Wallets/Buy/BuyViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Buy/BuyViewModel.cs
@@ -35,8 +35,8 @@ public partial class BuyViewModel : RoutableViewModel, IOrderManager
 	private readonly ReadOnlyObservableCollection<OrderViewModel> _orders;
 	private readonly SourceCache<OrderViewModel, int> _ordersCache;
 	private readonly Wallet _wallet;
-	[AutoNotify] private OrderViewModel? _emptyOrder; // Used to track the "Empty" order (with empty ConversationId)
 
+	[AutoNotify] private OrderViewModel? _emptyOrder; // Used to track the "Empty" order (with empty ConversationId)
 	[AutoNotify] private OrderViewModel? _selectedOrder;
 
 	public BuyViewModel(UiContext uiContext, WalletViewModel walletVm)
@@ -67,7 +67,12 @@ public partial class BuyViewModel : RoutableViewModel, IOrderManager
 			.Where(x => x.Id != ConversationId.Empty)
 			.Do(_ => EmptyOrder = NewEmptyOrder())
 			.Subscribe();
+
+		HasNonEmptyOrder = this.WhenAnyValue(x => x.Orders.Count)
+			.Select(_ => Orders.Any(x => x.ConversationId != ConversationId.Empty));
 	}
+
+	public IObservable<bool> HasNonEmptyOrder { get; }
 
 	public ReadOnlyObservableCollection<OrderViewModel> Orders => _orders;
 
