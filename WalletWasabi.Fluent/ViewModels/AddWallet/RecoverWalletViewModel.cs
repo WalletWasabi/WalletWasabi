e6da3262179f12d25334ab15b3e287dde6ae1da4
Yahia Chiheb <chihebyahia@gmail.com>
2024-03-05 22:18:15 +0100

    Improve text

--- WalletWasabi.Fluent/ViewModels/AddWallet/RecoverWalletViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/AddWallet/RecoverWalletViewModel.cs
@@ -60,7 +60,7 @@ public partial class RecoverWalletViewModel : RoutableViewModel
 		var (walletName, _, _, _) = options;
 		ArgumentException.ThrowIfNullOrEmpty(walletName);
 
-		var password = await Navigate().To().CreatePasswordDialog("Add Passphrase", "If you used a passphrase when you created the wallet type it below, otherwise leave this empty.").GetResultAsync();
+		var password = await Navigate().To().CreatePasswordDialog("Add Passphrase", "If you used a passphrase when you created your wallet you must type it below, otherwise leave this empty.").GetResultAsync();
 		if (password is not { } || CurrentMnemonics is not { IsValidChecksum: true } currentMnemonics)
 		{
 			return;
