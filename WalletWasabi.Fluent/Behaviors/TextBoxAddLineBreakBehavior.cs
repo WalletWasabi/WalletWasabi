4cc625549dba85ad288d5a70f9bd0a8421b9b236
ichthus1604 <ichthus1604@gmail.com>
2024-03-05 12:23:19 -0300

    merge

--- WalletWasabi.Fluent/Behaviors/TextBoxAddLineBreakBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/TextBoxAddLineBreakBehavior.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactions.Custom;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using WalletWasabi.Fluent.Extensions;
