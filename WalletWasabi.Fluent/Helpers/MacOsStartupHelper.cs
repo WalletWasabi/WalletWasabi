9a7bdbc86c39e7c000ee6e364e5b1f4363d5f70f
Balázs Faragó <54551879+Szpoti@users.noreply.github.com>
2024-03-06 12:08:48 +0100

    Change script strings format

--- WalletWasabi.Fluent/Helpers/MacOsStartupHelper.cs
+++ WalletWasabi.Fluent/Helpers/MacOsStartupHelper.cs
@@ -9,13 +9,13 @@ namespace WalletWasabi.Fluent.Helpers;
 public static class MacOsStartupHelper
 {
 	private static readonly string AddCmd = $"osascript -e \' tell application \"System Events\" to make new login item at end with properties {{path:\"/Applications/{Constants.AppName}.app/Contents/MacOS/Microservices/Binaries/osx64/{Constants.SilentExecutableName}.app\", hidden:true}} \'";
-	private static readonly string DeleteCmd = $"""osascript -e 'tell application "System Events" to delete login item '{Constants.SilentExecutableName}'""";
-	private static readonly string ListCmd = $"""osascript -e 'tell application "System Events" to get the name of every login item'""";
+	private static readonly string DeleteCmd = $@"osascript -e 'tell application ""System Events"" to delete login item ""{Constants.SilentExecutableName}""'";
+	private static readonly string ListCmd = $@"osascript -e 'tell application ""System Events"" to get the name of every login item'";
 
 	public static async Task AddOrRemoveLoginItemAsync(bool runOnSystemStartup)
 	{
 		string result = await EnvironmentHelpers.ShellExecAndGetResultAsync(ListCmd).ConfigureAwait(false);
-		bool loginItemExists = result.Contains(Constants.AppName);
+		bool loginItemExists = result.Contains(Constants.SilentExecutableName);
 
 		if (!loginItemExists && runOnSystemStartup)
 		{
