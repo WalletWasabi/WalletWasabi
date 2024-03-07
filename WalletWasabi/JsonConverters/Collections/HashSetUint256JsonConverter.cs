f18f00842b49098266d6820ad97a7b85f158fd21
Balázs Faragó <54551879+Szpoti@users.noreply.github.com>
2024-03-07 15:40:31 +0100

    Fix parsing

--- WalletWasabi/JsonConverters/Collections/HashSetUint256JsonConverter.cs
+++ WalletWasabi/JsonConverters/Collections/HashSetUint256JsonConverter.cs
@@ -10,9 +10,19 @@ public class HashSetUint256JsonConverter : JsonConverter<HashSet<uint256>>
 {
 	public override HashSet<uint256>? ReadJson(JsonReader reader, Type objectType, HashSet<uint256>? existingValue, bool hasExistingValue, JsonSerializer serializer)
 	{
-		var stringValue = reader.Value as string;
+		JToken token = JToken.Load(reader);
+		if (token.Type == JTokenType.Array)
+		{
+			var value = new HashSet<uint256>();
+			var set = token.ToObject<HashSet<string>>();
+			foreach (var item in set)
+			{
+				value.Add(new(item));
+			}
+			return value;
+		}
 
-		return new HashSet<uint256>() { new uint256(stringValue) };
+		return new HashSet<uint256>();
 	}
 
 	public override void WriteJson(JsonWriter writer, HashSet<uint256>? value, JsonSerializer serializer)
