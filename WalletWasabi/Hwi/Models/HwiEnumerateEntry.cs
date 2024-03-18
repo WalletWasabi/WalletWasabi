856b070a702145a2c930c3bea63a1261bd53b424
Nagy Szabolcs <birkusz20@gmail.com>
2024-03-18 14:16:37 +0100

    added tz3 to hwiEnumerateEntry

--- WalletWasabi/Hwi/Models/HwiEnumerateEntry.cs
+++ WalletWasabi/Hwi/Models/HwiEnumerateEntry.cs
@@ -46,7 +46,7 @@ public class HwiEnumerateEntry
 		{
 			HardwareWalletModels.Coldcard or HardwareWalletModels.Coldcard_Simulator => WalletType.Coldcard,
 			HardwareWalletModels.Ledger_Nano_S or HardwareWalletModels.Ledger_Nano_X or HardwareWalletModels.Ledger_Nano_S_Plus => WalletType.Ledger,
-			HardwareWalletModels.Trezor_1 or HardwareWalletModels.Trezor_1_Simulator or HardwareWalletModels.Trezor_T or HardwareWalletModels.Trezor_T_Simulator => WalletType.Trezor,
+			HardwareWalletModels.Trezor_1 or HardwareWalletModels.Trezor_1_Simulator or HardwareWalletModels.Trezor_T or HardwareWalletModels.Trezor_T_Simulator or HardwareWalletModels.Trezor_Safe_3 => WalletType.Trezor,
 			HardwareWalletModels.Jade => WalletType.Jade,
 			HardwareWalletModels.BitBox02_BTCOnly => WalletType.BitBox,
 			_ => WalletType.Hardware
@@ -62,10 +62,12 @@ public class HwiEnumerateEntry
 			HardwareWalletModels.Coldcard => true,
 			HardwareWalletModels.Ledger_Nano_S or HardwareWalletModels.Ledger_Nano_X or HardwareWalletModels.Ledger_Nano_S_Plus => false,
 			HardwareWalletModels.Trezor_1 => false,
-			HardwareWalletModels.Trezor_T => true,
+			HardwareWalletModels.Trezor_T => false,
+			HardwareWalletModels.Trezor_Safe_3 => false,
 			HardwareWalletModels.Trezor_1_Simulator or HardwareWalletModels.Trezor_T_Simulator or HardwareWalletModels.Coldcard_Simulator => false,
 			HardwareWalletModels.Jade => false,
 			HardwareWalletModels.BitBox02_BTCOnly => false,
+
 			_ => false
 		};
 	}
