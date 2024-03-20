6cdad9b4e16ae3cab6df7b9c8b9d96a954b5e008
Kiminuo <kiminuo@protonmail.com>
2024-03-20 10:42:17 +0100

    Revert "WalletFilterProcessor: handle exceptions better"
    This reverts commit 91b600118b132d10753d09a99192aacd4937d685.

--- WalletWasabi/Wallets/WalletFilterProcessor.cs
+++ WalletWasabi/Wallets/WalletFilterProcessor.cs
@@ -180,7 +180,7 @@ public class WalletFilterProcessor : BackgroundService
 				}
 			}
 		}
-		catch (Exception ex) when (ex is OperationCanceledException or TaskCanceledException)
+		catch (OperationCanceledException)
 		{
 			Logger.LogDebug("Filter processor's execution was stopped.");
 		}
@@ -295,11 +295,6 @@ public class WalletFilterProcessor : BackgroundService
 			return successFullNodeResult.Block;
 		}
 
-		if (fullNodeResult is BlockDownloadService.CanceledResult)
-		{
-			throw new OperationCanceledException();
-		}
-
 		while (true)
 		{
 			BlockDownloadService.IResult p2pNodeResult = await BlockDownloadService.TryGetBlockAsync(P2pSourceRequest.Automatic, blockHash, priority, cancellationToken)
@@ -308,11 +303,6 @@ public class WalletFilterProcessor : BackgroundService
 			if (p2pNodeResult is BlockDownloadService.SuccessResult successP2pResult)
 			{
 				return successP2pResult.Block;
-			}
-
-			if (p2pNodeResult is BlockDownloadService.CanceledResult)
-			{
-				throw new OperationCanceledException();
 			}
 		}
 	}
