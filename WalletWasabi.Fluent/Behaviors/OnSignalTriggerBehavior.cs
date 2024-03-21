d517c148dc5fc9e0cdf3162a8474b1d60ddc5c82
Yahia Chiheb <chihebyahia@gmail.com>
2024-03-21 17:54:09 +0100

    Fix CodeFactor

--- WalletWasabi.Fluent/Behaviors/OnSignalTriggerBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/OnSignalTriggerBehavior.cs
@@ -8,15 +8,14 @@ namespace WalletWasabi.Fluent.Behaviors;
 
 public class OnSignalTriggerBehavior : Trigger
 {
+	public static readonly StyledProperty<IObservable<Unit>> TriggerProperty = AvaloniaProperty.Register<OnSignalTriggerBehavior, IObservable<Unit>>(nameof(Trigger));
+
 	public OnSignalTriggerBehavior()
 	{
 		this.WhenAnyObservable(x => x.Trigger)
 			.Do(_ => Interaction.ExecuteActions(this, Actions, null))
 			.Subscribe();
 	}
-
-	public static readonly StyledProperty<IObservable<Unit>> TriggerProperty = AvaloniaProperty.Register<OnSignalTriggerBehavior, IObservable<Unit>>(
-		nameof(Trigger));
 
 	public IObservable<Unit> Trigger
 	{
