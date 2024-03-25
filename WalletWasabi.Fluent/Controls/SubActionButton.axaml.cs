4d960099273e7b7cb7736b41c444abb04207a6ae
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-03-25 15:09:38 +0100

    Add ItemsSource property

--- WalletWasabi.Fluent/Controls/SubActionButton.axaml.cs
+++ WalletWasabi.Fluent/Controls/SubActionButton.axaml.cs
@@ -1,5 +1,17 @@
+using System.Collections;
+using Avalonia;
 using Avalonia.Controls;
 
 namespace WalletWasabi.Fluent.Controls;
 
-public class SubActionButton : SplitButton;
+public class SubActionButton : SplitButton
+{
+	public static readonly StyledProperty<IEnumerable?> ItemsSourceProperty =
+		AvaloniaProperty.Register<SubActionButton, IEnumerable?>(nameof(ItemsSource));
+
+	public IEnumerable? ItemsSource
+	{
+		get => GetValue(ItemsSourceProperty);
+		set => SetValue(ItemsSourceProperty, value);
+	}
+}
