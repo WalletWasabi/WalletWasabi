34ecb717ed09ad1ac7a4b2bbdc079d2337789565
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-04-02 22:40:15 +0200

    Add RecoverWordViewModel

---
+++ WalletWasabi.Fluent/ViewModels/AddWallet/RecoverWordViewModel.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NBitcoin;
+using ReactiveUI;
+
+namespace WalletWasabi.Fluent.ViewModels.AddWallet;
+
+public partial class RecoverWordViewModel : ViewModelBase
+{
+	[AutoNotify] private bool _isSelected;
+	[AutoNotify] private bool _isConfirmed;
+	[AutoNotify] private bool _isEnabled;
+	[AutoNotify] private string? _selectedWord;
+	[AutoNotify] private string _word;
+	[AutoNotify] private bool _isMnemonicsValid;
+	[AutoNotify] private IEnumerable<string>? _suggestions;
+
+	public RecoverWordViewModel(int index, string word)
+	{
+		Index = index;
+		Word = word;
+
+		Suggestions = new Mnemonic(Wordlist.English, WordCount.Twelve).WordList.GetWords();
+
+		this.WhenAnyValue(x => x.SelectedWord)
+			.Subscribe(_ => ValidateWord());
+	}
+
+	public int Index { get; }
+
+	public ObservableCollection<string> Mnemonics { get; } = new();
+
+	private void ValidateWord()
+	{
+		IsConfirmed = Word.Equals(SelectedWord, StringComparison.InvariantCultureIgnoreCase);
+	}
+
+	public override string ToString()
+	{
+		return $"{Index}. {Word}";
+	}
+}
