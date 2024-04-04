3ad9047e336946714fccafc71f5dd0803a220459
Balázs Faragó <54551879+Szpoti@users.noreply.github.com>
2024-04-04 13:57:34 +0200

    Finalize

--- WalletWasabi.Fluent/Helpers/MacOsStartupHelper.cs
+++ WalletWasabi.Fluent/Helpers/MacOsStartupHelper.cs
@@ -11,10 +11,11 @@ public static class MacOsStartupHelper
 {
 	private static readonly string PlistContent = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\r\n<plist version=\"1.0\">\r\n<dict>\r\n    <key>Label</key>\r\n    <string>com.wasabiwallet.startup</string>\r\n    <key>ProgramArguments</key>\r\n    <array>\r\n        <string>/Applications/Wasabi Wallet.app/Contents/MacOS/wassabee</string>\r\n        <string>startsilent</string>\r\n    </array>\r\n    <key>RunAtLoad</key>\r\n    <true/>\r\n</dict>\r\n</plist>";
 
-	public static async Task AddOrRemoveLoginItemAsync(bool runOnSystemStartup)
+	public static async Task AddOrRemoveStartupItemAsync(bool runOnSystemStartup)
 	{
-		string dataDir = "~/Library/LaunchAgents/";
-		string plistPath = Path.Combine(dataDir, Constants.SilentPlistName);
+		string homeDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+		string plistPath = Path.Combine(homeDir, "Library/LaunchAgents/", Constants.SilentPlistName);
+
 		var fileExists = File.Exists(plistPath);
 		if (!fileExists && runOnSystemStartup)
 		{
