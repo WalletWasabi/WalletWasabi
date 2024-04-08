6779d6fb84faaf6a027a05ab7705b5b9f87de125
Nagy Szabolcs <birkusz20@gmail.com>
2024-04-08 14:45:33 +0200

    fix windows part

--- WalletWasabi.Tests/Helpers/HwiValidationHelper.cs
+++ WalletWasabi.Tests/Helpers/HwiValidationHelper.cs
@@ -16,11 +16,11 @@ public static class HwiValidationHelper
 		string pattern = model switch
 		{
 			HardwareWalletModels.Trezor_T => "^webusb:",
-			HardwareWalletModels.Trezor_1 => @"(^hid:\\\\.*?vid_534c&pid_0001&mi_00)|(DevSrvsID:\d+)|(\d+(-\d+\.\d+)+:\d+\.\d+)",
-			HardwareWalletModels.Coldcard => @"(^hid:\\\\.*?vid_d13e&pid_cc10&mi_00)|(DevSrvsID:\d+)|(\d+(-\d+\.\d+)+:\d+\.\d+)",
-			HardwareWalletModels.Ledger_Nano_S or HardwareWalletModels.Ledger_Nano_X => @"(^hid:\\\\.*?vid_2c97&pid_0001&mi_00)|(DevSrvsID:\d+)|(\d+(-\d+\.\d+)+:\d+\.\d+)",
+			HardwareWalletModels.Trezor_1 => @"(^\\\\\?\\HID#VID_534C&PID_0001)|(DevSrvsID:\d+)|(\d+(-\d+\.\d+)+:\d+\.\d+)",
+			HardwareWalletModels.Coldcard => @"(^\\\\\?\\HID#VID_D13E&PID_CC10)|(DevSrvsID:\d+)|(\d+(-\d+\.\d+)+:\d+\.\d+)",
+			HardwareWalletModels.Ledger_Nano_S or HardwareWalletModels.Ledger_Nano_X => @"(^\\\\\?\\HID#VID_2C97&PID_0001)|(DevSrvsID:\d+)|(\d+(-\d+\.\d+)+:\d+\.\d+)",
 			HardwareWalletModels.Jade => @"(^COM\d+)|(/dev/cu\.usbserial-[A-Za-z0-9]+)|(/dev/ttyACM\d+)",
-			HardwareWalletModels.BitBox02_BTCOnly => @"(^\\\\\?\\hid#vid_03eb&pid_2403)|(DevSrvsID:\d+)|(\d+(-\d+\.\d+)+:\d+\.\d+)",
+			HardwareWalletModels.BitBox02_BTCOnly => @"(^\\\\\?\\HID#VID_03EB&PID_2403)|(DevSrvsID:\d+)|(\d+(-\d+\.\d+)+:\d+\.\d+)",
 			_ => "",
 		};
 		return Regex.IsMatch(path, pattern);
