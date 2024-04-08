0cf80a60a71e14157a1bafbcab407b112df51cd7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-04-08 13:35:54 +0200

    Add RecoverWalletSummaryView stub

---
+++ WalletWasabi.Fluent/Views/AddWallet/RecoverWalletSummaryView.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Views.AddWallet;
+
+public class RecoverWalletSummaryView : UserControl
+{
+	public RecoverWalletSummaryView()
+	{
+		InitializeComponent();
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+}
