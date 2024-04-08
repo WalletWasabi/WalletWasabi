9e7a353e2e3379de7d12dcc4f1f51961657d7538
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-04-08 23:18:49 +0200

    Refactor summary view

--- WalletWasabi.Fluent/ViewModels/AddWallet/RecoverWalletSummaryViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/AddWallet/RecoverWalletSummaryViewModel.cs
@@ -20,26 +20,12 @@ namespace WalletWasabi.Fluent.ViewModels.AddWallet;
 [NavigationMetaData(Title = "Recovery Words")]
 public partial class RecoverWalletSummaryViewModel : RoutableViewModel
 {
-	private readonly List<RecoverWordViewModel> _words;
-	[AutoNotify] private RecoverWordViewModel _currentWord;
 	[AutoNotify] private IEnumerable<string>? _suggestions;
 	[AutoNotify] private Mnemonic? _currentMnemonics;
 	[AutoNotify] private bool _isMnemonicsValid;
-	[AutoNotify] private string? _confirmPassphrase;
-	[AutoNotify] private bool _focusConfirmPassphrase;
-	[AutoNotify(SetterModifier = AccessModifier.Private)] private bool _allWordsConfirmed;
-	[AutoNotify(SetterModifier = AccessModifier.Private)] private bool _passphraseConfirmed;
 
 	private RecoverWalletSummaryViewModel(WalletCreationOptions.RecoverWallet options)
 	{
-		var words = Enumerable
-			.Range(1, 12)
-			.Select(x => new RecoverWordViewModel(x, ""));
-
-		_words = words.OrderBy(x => x.Index).ToList();
-		_currentWord = _words.First();
-		_currentWord.IsSelected = true;
-
 		Suggestions = new Mnemonic(Wordlist.English, WordCount.Twelve).WordList.GetWords();
 
 		Mnemonics.ToObservableChangeSet().ToCollection()
@@ -55,14 +41,10 @@ public partial class RecoverWalletSummaryViewModel : RoutableViewModel
 
 		EnableBack = true;
 
-		var nextCanExecute = this.WhenAnyValue(x => x.IsMnemonicsValid);
-
 		NextCommand = ReactiveCommand.CreateFromTask(
 			async () => await OnNextAsync(options),
-			canExecute: nextCanExecute);
+			canExecute: this.WhenAnyValue(x => x.IsMnemonicsValid));
 	}
-
-	public ObservableCollectionExtended<RecoverWordViewModel> ConfirmationWords { get; } = new();
 
 	private int MinGapLimit { get; set; } = 114;
 
@@ -73,7 +55,7 @@ public partial class RecoverWalletSummaryViewModel : RoutableViewModel
 		var (walletName, _, _, _) = options;
 		ArgumentException.ThrowIfNullOrEmpty(walletName);
 
-		var password = ConfirmPassphrase;
+		var password = await Navigate().To().CreatePasswordDialog("Add Passphrase", "If you used a passphrase when you created your wallet you must type it below, otherwise leave this empty.").GetResultAsync();
 		if (password is not { } || CurrentMnemonics is not { IsValidChecksum: true } currentMnemonics)
 		{
 			return;
@@ -122,31 +104,9 @@ public partial class RecoverWalletSummaryViewModel : RoutableViewModel
 		return string.Join(' ', Mnemonics);
 	}
 
-	[System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Uses DisposeWith()")]
 	protected override void OnNavigatedTo(bool isInHistory, CompositeDisposable disposables)
 	{
 		base.OnNavigatedTo(isInHistory, disposables);
-
-		ConfirmationWords.Clear();
-
-		var confirmationWordsSourceList = new SourceList<RecoverWordViewModel>();
-
-		confirmationWordsSourceList
-			.DisposeWith(disposables)
-			.Connect()
-			.ObserveOn(RxApp.MainThreadScheduler)
-			.Bind(ConfirmationWords)
-			//.OnItemAdded(x => x.Reset())
-			.Subscribe()
-			.DisposeWith(disposables);
-
-		confirmationWordsSourceList
-			.Connect()
-			.WhenValueChanged(x => x.IsConfirmed)
-			.Subscribe(_ => AllWordsConfirmed = confirmationWordsSourceList.Items.All(x => x.IsConfirmed))
-			.DisposeWith(disposables);
-
-		confirmationWordsSourceList.AddRange(_words);
 
 		var enableCancel = UiContext.WalletRepository.HasWallet;
 		SetupCancel(enableCancel: enableCancel, enableCancelOnEscape: enableCancel, enableCancelOnPressed: false);
