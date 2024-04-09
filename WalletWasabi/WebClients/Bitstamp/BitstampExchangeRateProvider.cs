2c450b24392f66632a669b103690dfe6140d6e85
Kiminuo <kiminuo@protonmail.com>
2024-04-09 16:03:36 +0200

    Support for reporting of dangerous API; the test project is excluded as it is not a part of the product

--- WalletWasabi/WebClients/Bitstamp/BitstampExchangeRateProvider.cs
+++ WalletWasabi/WebClients/Bitstamp/BitstampExchangeRateProvider.cs
@@ -12,10 +12,14 @@ public class BitstampExchangeRateProvider : IExchangeRateProvider
 {
 	public async Task<IEnumerable<ExchangeRate>> GetExchangeRateAsync(CancellationToken cancellationToken)
 	{
+		// Only used by the Backend.
+#pragma warning disable RS0030 // Do not use banned APIs
 		using var httpClient = new HttpClient
 		{
 			BaseAddress = new Uri("https://www.bitstamp.net")
 		};
+#pragma warning restore RS0030 // Do not use banned APIs
+
 		using var response = await httpClient.GetAsync("api/v2/ticker/btcusd", cancellationToken).ConfigureAwait(false);
 		using var content = response.Content;
 		var rate = await content.ReadAsJsonAsync<BitstampExchangeRate>().ConfigureAwait(false);
