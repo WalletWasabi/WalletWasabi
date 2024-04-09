96ddebf3cd5f65557b7162b68ba46f90521a7840
ichthus1604 <ichthus1604@gmail.com>
2024-04-09 18:57:55 -0300

    Move DataTrigger

--- WalletWasabi.Fluent/Behaviors/FocusControlActionCustom.cs
+++ WalletWasabi.Fluent/Behaviors/FocusControlActionCustom.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Threading;
+using Avalonia.Xaml.Interactions.Custom;
 using Avalonia.Xaml.Interactivity;
 
 namespace WalletWasabi.Fluent.Behaviors;
