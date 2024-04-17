a7947ecb3202d5379d524c6cb8d5018a0de322fe
ichthus1604 <ichthus1604@gmail.com>
2024-04-17 19:43:44 -0300

    Minimal Decoupling TransactionSummaryViewModel

--- WalletWasabi.Fluent/ViewModels/Wallets/Send/TransactionSummaryViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Send/TransactionSummaryViewModel.cs
@@ -10,7 +10,7 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Send;
 
 public partial class TransactionSummaryViewModel : ViewModelBase
 {
-	private readonly Wallet _wallet;
+	private readonly IWalletModel _wallet;
 	private BuildTransactionResult? _transaction;
 	[AutoNotify] private bool _transactionHasChange;
 	[AutoNotify] private TimeSpan? _confirmationTime;
@@ -24,7 +24,7 @@ public partial class TransactionSummaryViewModel : ViewModelBase
 	[AutoNotify] private double? _amountDiff;
 	[AutoNotify] private double? _feeDiff;
 
-	private TransactionSummaryViewModel(TransactionPreviewViewModel parent, Wallet wallet, TransactionInfo info, bool isPreview = false)
+	private TransactionSummaryViewModel(TransactionPreviewViewModel parent, IWalletModel wallet, TransactionInfo info, bool isPreview = false)
 	{
 		Parent = parent;
 		_wallet = wallet;
@@ -48,8 +48,7 @@ public partial class TransactionSummaryViewModel : ViewModelBase
 	{
 		_transaction = transactionResult;
 
-		TransactionFeeHelper.TryEstimateConfirmationTime(_wallet, info.FeeRate, out var estimate);
-		ConfirmationTime = estimate;
+		ConfirmationTime = _wallet.Transactions.TryEstimateConfirmationTime(info);
 
 		var destinationAmount = _transaction.CalculateDestinationAmount(info.Destination);
 
