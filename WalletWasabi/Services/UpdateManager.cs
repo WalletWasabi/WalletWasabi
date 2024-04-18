6d52ad18812a086a5c4848f5f6e1141b15491696
Balázs Faragó <54551879+Szpoti@users.noreply.github.com>
2024-04-18 12:51:09 +0200

    Invoke UpdateAvailableToGet with new UpdateStatus instance

--- WalletWasabi/Services/UpdateManager.cs
+++ WalletWasabi/Services/UpdateManager.cs
@@ -65,7 +65,7 @@ public class UpdateManager : IDisposable
 			Cleanup();
 			return;
 		}
-
+		bool readyToInstall = false;
 		if (DownloadNewVersion)
 		{
 			do
@@ -73,11 +73,10 @@ public class UpdateManager : IDisposable
 				tries++;
 				try
 				{
-					(string installerPath, Version newVersion) = await GetInstallerAsync(targetVersion, CancellationToken).ConfigureAwait(false);
+					(string installerPath, targetVersion) = await GetInstallerAsync(targetVersion, CancellationToken).ConfigureAwait(false);
 					InstallerPath = installerPath;
-					Logger.LogInfo($"Version {newVersion} downloaded successfully.");
-					updateStatus.IsReadyToInstall = true;
-					updateStatus.ClientVersion = newVersion;
+					Logger.LogInfo($"Version {targetVersion} downloaded successfully.");
+					readyToInstall = true;
 					break;
 				}
 				catch (OperationCanceledException ex)
@@ -101,8 +100,13 @@ public class UpdateManager : IDisposable
 				}
 			} while (tries < MaxTries);
 		}
-
-		UpdateAvailableToGet?.Invoke(this, updateStatus);
+		var newStatus = new UpdateStatus(updateStatus.BackendCompatible,
+			updateStatus.ClientUpToDate,
+			updateStatus.LegalDocumentsVersion,
+			updateStatus.CurrentBackendMajorVersion,
+			targetVersion)
+		{ IsReadyToInstall = readyToInstall };
+		UpdateAvailableToGet?.Invoke(this, newStatus);
 	}
 
 	/// <summary>
