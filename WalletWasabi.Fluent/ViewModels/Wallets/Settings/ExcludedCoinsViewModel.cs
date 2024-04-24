d7504a2b280c3610672df817658e352ac0b77186
José Manuel Nieto <superjmn@outlook.com>
2024-04-24 11:17:49 +0200

    Implement select all/none

--- WalletWasabi.Fluent/ViewModels/Wallets/Settings/ExcludedCoinsViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Settings/ExcludedCoinsViewModel.cs
@@ -1,5 +1,11 @@
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Windows.Input;
+using DynamicData;
+using DynamicData.Aggregation;
+using DynamicData.Binding;
 using ReactiveUI;
 using WalletWasabi.Fluent.Infrastructure;
 using WalletWasabi.Fluent.Models.Wallets;
@@ -13,6 +19,9 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Settings;
 public partial class ExcludedCoinsViewModel : DialogViewModelBase<Unit>
 {
 	private readonly IWalletModel _wallet;
+	private readonly CompositeDisposable _disposables = new();
+
+	[AutoNotify] private bool? _areAllCoinsSelected;
 
 	public ExcludedCoinsViewModel(IWalletModel wallet)
 	{
@@ -25,6 +34,36 @@ public partial class ExcludedCoinsViewModel : DialogViewModelBase<Unit>
 				ExcludeSelectedCoins();
 				Close();
 			});
+
+		ToggleSelectionCommand = ReactiveCommand.Create(() => SelectAll(!CoinList.Selection.Any()));
+	}
+
+	private void SelectAll(bool value)
+	{
+		foreach (var coin in CoinList.CoinItems)
+		{
+			coin.IsSelected = value;
+		}
+	}
+	
+	public ICommand ToggleSelectionCommand { get; }
+
+	protected override void OnNavigatedTo(bool isInHistory, CompositeDisposable disposables)
+	{
+		CoinList.CoinItems
+			.ToObservableChangeSet()
+			.WhenPropertyChanged(x => x.IsSelected)
+			.Select(_ => CoinList.Selection.Count == CoinList.CoinItems.Count ? true : CoinList.Selection.Count == 0 ? false : (bool?)null)
+			.BindTo(this, x => x.AreAllCoinsSelected)
+			.DisposeWith(_disposables);
+	}
+
+	protected override void OnNavigatedFrom(bool isInHistory)
+	{
+		if (!isInHistory)
+		{
+			_disposables.Dispose();
+		}
 	}
 
 	public CoinListViewModel CoinList { get; set; }
