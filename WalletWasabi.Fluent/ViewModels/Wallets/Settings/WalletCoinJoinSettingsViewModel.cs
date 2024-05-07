efe5d58e3a721a7b1beb4e235d47a6350d3c60b6
Szabolcs Nagy <birkusz20@gmail.com>
2024-05-07 17:42:11 +0200

    only show logged in wallets

--- WalletWasabi.Fluent/ViewModels/Wallets/Settings/WalletCoinJoinSettingsViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Settings/WalletCoinJoinSettingsViewModel.cs
@@ -122,7 +122,8 @@ public partial class WalletCoinJoinSettingsViewModel : RoutableViewModel
 
 		UiContext.WalletRepository.Wallets
 			.Connect()
-			.Filter(x => x.Id == _wallet.Id || x.Settings.OutputWalletId != _wallet.Id)
+			.Filter(x => (x.Id == _wallet.Id || x.Settings.OutputWalletId != _wallet.Id) && x.IsLoggedIn)
+
 			.SortBy(i => i.Name)
 			.Bind(out var wallets)
 			.Subscribe()
