43b72d850fbeecd2c05e95806d4294044e5235f0
Lucas Ontivero <lontivero@users.noreply.github.com>
2024-05-10 14:55:09 -0300

    Remove status API (#13018)
    It is not used by the wallet.

--- WalletWasabi/Backend/Models/Responses/StatusResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace WalletWasabi.Backend.Models.Responses;
-
-public class StatusResponse
-{
-	public bool FilterCreationActive { get; set; }
-	public bool WabiSabiCoinJoinCreationActive { get; set; }
-}
