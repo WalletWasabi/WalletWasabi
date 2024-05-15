88a03e7f61581206b02aa10b7cdf50d4c79c764f
Csiki <andras.csikvari@gmail.com>
2024-05-15 16:12:28 +0200

    Removing last comment
    + recheck after the change

--- WalletWasabi.Tests/UnitTests/Wallet/FilterProcessor/BlockDownloadServiceTests.cs
+++ WalletWasabi.Tests/UnitTests/Wallet/FilterProcessor/BlockDownloadServiceTests.cs
@@ -104,7 +104,7 @@ public class BlockDownloadServiceTests
 			}
 
 			// Add small delay to make sure that things stabilize.
-			await Task.Delay(2000); // fake comment for the check
+			await Task.Delay(2000);
 
 			// Allow downloading of the block1.
 			{
