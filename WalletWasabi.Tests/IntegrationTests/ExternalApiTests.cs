b2b6e8d15fcbb6af040301753bc5bb81bb5b79ff
Lucas Ontivero <lucasontivero@gmail.com>
2024-05-15 11:05:02 -0300

    Add `FeeRateProvider`

--- WalletWasabi.Tests/IntegrationTests/ExternalApiTests.cs
+++ WalletWasabi.Tests/IntegrationTests/ExternalApiTests.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using WalletWasabi.ExchangeRate;
 using WalletWasabi.WebClients;
 using Xunit;
+using WalletWasabi.FeeRateEstimation;
 
 namespace WalletWasabi.Tests.IntegrationTests;
 
@@ -31,5 +32,21 @@ public class ExternalApiTests
 		var userAgent = UserAgent.GetNew(Random.Shared.Next());
 		var rate = await provider.GetExchangeRateAsync(providerName, userAgent, timeoutCts.Token).ConfigureAwait(false);
 		Assert.NotEqual(0m, rate.Rate);
+	}
+
+	[Fact]
+	public async Task BlockstreamFeeRateProviderTestsAsync() =>
+		await AssertFeeProviderAsync("Blockstream");
+
+	[Fact]
+	public async Task MempoolSpaceRateProviderTestsAsync() =>
+		await AssertFeeProviderAsync("MempoolSpace");
+
+	private async Task AssertFeeProviderAsync(string providerName)
+	{
+		using CancellationTokenSource timeoutCts = new(TimeSpan.FromMinutes(3));
+		var provider = new FeeRateProvider();
+		var estimations = await provider.GetFeeRateEstimationsAsync(providerName, timeoutCts.Token).ConfigureAwait(false);
+		Assert.NotEmpty(estimations.Estimations);
 	}
 }
---
+++ WalletWasabi/FeeRateEstimation/FeeRateProvider.cs
@@ -0,0 +1,75 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using WalletWasabi.Blockchain.Analysis.FeesEstimation;
+
+namespace WalletWasabi.FeeRateEstimation;
+using FeeRateProviderInfo = (string Name, string ApiUrl, Func<string, AllFeeEstimate> Extractor);
+
+public class FeeRateProvider(EndPoint? socksProxyEndPoint = null)
+{
+	private static FeeRateProviderInfo[] Providers = [
+		("Blockstream", "https://blockstream.info/api/fee-estimates", BlockstreamHandler()),
+		("MempoolSpace", "https://mempool.space/api/v1/fees/recommended", MempoolSpaceHandler()),
+	];
+
+	public async Task<AllFeeEstimate> GetFeeRateEstimationsAsync(string providerName, CancellationToken cancellationToken)
+	{
+		var providerInfo = Providers.FirstOrDefault(x => x.Name.Equals(providerName, StringComparison.InvariantCultureIgnoreCase));
+		if (providerInfo == default)
+		{
+			throw new NotSupportedException($"Fee rate estimations provider '{providerName}' is not supported.");
+		}
+		var url = new Uri(providerInfo.ApiUrl);
+
+#pragma warning disable RS0030 // Do not use banned APIs
+		using var httpClientHandler = new HttpClientHandler();
+		using var httpClient = new HttpClient(httpClientHandler);
+		httpClientHandler.Proxy = GetWebProxy();
+		httpClient.BaseAddress = new Uri($"{url.Scheme}://{url.Host}");
+#pragma warning restore RS0030 // Do not use banned APIs
+
+		using var response = await httpClient.GetAsync(url.PathAndQuery, cancellationToken).ConfigureAwait(false);
+		var json = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+		var estimations = providerInfo.Extractor(json);
+		return estimations;
+	}
+
+	private static Func<string, AllFeeEstimate> BlockstreamHandler() =>
+		json => new AllFeeEstimate(
+			JsonDocument.Parse(json).RootElement
+				.EnumerateObject()
+				.Select(o => (int.Parse(o.Name), (int) Math.Ceiling(o.Value.GetDouble())))
+				.ToDictionary());
+
+	private static Func<string, AllFeeEstimate> MempoolSpaceHandler() =>
+		json =>
+			new AllFeeEstimate(
+				JsonDocument.Parse(json).RootElement
+					.EnumerateObject()
+					.Select(o => (o.Name, o.Value.GetDouble()) switch {
+							("fastestFee", var feeRate) => (Target: 2, feeRate),
+							("halfHourFee", var feeRate) => (Target: 3, feeRate),
+							("hourFee", var feeRate) => (Target: 6, feeRate),
+							("economyFee", var feeRate) => (Target: 72, feeRate),
+							_ => (0, 0)
+						})
+					.Where(x => x.Target > 0)
+					.Select(o => (o.Target, (int) Math.Ceiling(o.feeRate)))
+					.ToDictionary());
+
+	private WebProxy? GetWebProxy()
+	{
+		return socksProxyEndPoint switch
+		{
+			DnsEndPoint dns => TorWebProxy(dns.Host, dns.Port),
+			IPEndPoint ip => TorWebProxy(ip.Address.ToString(), ip.Port),
+			null => null,
+			_ => throw new NotSupportedException("The endpoint type is not supported.")
+		};
+		static WebProxy TorWebProxy(string host, int port) => new(new UriBuilder("socks5", host, port).Uri);
+	}
+}
