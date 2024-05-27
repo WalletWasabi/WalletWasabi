c28e73cdb79004aec86da300437965c9a94412c7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-05-27 10:50:16 +0200

    Jump back to the previous word when user presses BACKSPACE

--- WalletWasabi.Fluent/Converters/IntConverter.cs
+++ WalletWasabi.Fluent/Converters/IntConverter.cs
@@ -6,4 +6,7 @@ public static class IntConverter
 {
 	public static readonly IValueConverter ToOrdinalString =
 		new FuncValueConverter<int, string>(x => $"{x}.");
+
+	public static readonly IValueConverter IsZero =
+		new FuncValueConverter<int, bool>(x => x == 0);
 }
