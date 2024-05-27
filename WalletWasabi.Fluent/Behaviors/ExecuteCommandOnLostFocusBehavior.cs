be6c817ce2a6f3c08f8ebc0568dd7fd332b57658
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-05-27 09:39:50 +0200

    Add ExecuteCommandOnLostFocusBehavior

---
+++ WalletWasabi.Fluent/Behaviors/ExecuteCommandOnLostFocusBehavior.cs
@@ -0,0 +1,30 @@
+using System.Reactive.Disposables;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactions.Custom;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+public class ExecuteCommandOnLostFocusBehavior : ExecuteCommandBaseBehavior
+{
+	protected override void OnAttached(CompositeDisposable disposables)
+	{
+		InputElement.LostFocusEvent.AddClassHandler<InputElement>(
+				(x, _) =>
+				{
+					if (Equals(x, AssociatedObject))
+					{
+						var parameter = CommandParameter;
+						if (Command is { } cmd && cmd.CanExecute(parameter))
+						{
+							cmd.Execute(parameter);
+						}
+					}
+				},
+				RoutingStrategies.Bubble)
+			.DisposeWith(disposables);
+	}
+}
