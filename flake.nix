140cafc6fd9326904b21440b10327544f7cc9960
Lucas Ontivero <lontivero@users.noreply.github.com>
2024-05-29 16:29:20 -0300

    Configure CI (#13097)

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
     let
-        pkgs = import nixpkgs { system = "x86_64-linux"; }; #nixpkgs.legacyPackages.x86_64-linux;
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
         deployScript = pkgs.writeScriptBin "deploy" (builtins.readFile ./Contrib/deploy.sh);
         gitRev = if (builtins.hasAttr "rev" self) then self.rev else "dirty";
         backend-build = pkgs.buildDotnetModule rec {
@@ -22,6 +22,53 @@
           postInstall = ''
             ln -s ${deployScript}/bin/deploy $out
           '';
+        };
+
+        build-all = pkgs.buildDotnetModule rec {
+          pname = "WalletWasabi";
+          version = "2.0.0-${builtins.substring 0 8 (self.lastModifiedDate or self.lastModified or "19700101")}-${gitRev}";
+          nugetDeps = ./deps-all.nix; # nix build .#packages.x86_64-linux.default.passthru.fetch-deps
+          runtimeDeps = with pkgs; [
+             # for backend
+             openssl zlib
+             # for client
+             tor hwi bitcoind-knots
+             xorg.libX11 xorg.libXrandr xorg.libX11.dev xorg.libICE xorg.libSM fontconfig.lib ];
+          dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0;
+          selfContainedBuild = true;
+
+
+          src = ./.;
+
+          projectFile = ["WalletWasabi.Backend/WalletWasabi.Backend.csproj" "WalletWasabi.Fluent.Desktop/WalletWasabi.Fluent.Desktop.csproj"];
+          testProjectFile = "WalletWasabi.Tests/WalletWasabi.Tests.csproj";
+          executables = [ "WalletWasabi.Backend" "WalletWasabi.Fluent.Desktop" ];
+          doCheck = true;
+          dotnetTestFlags = ["--filter \"UnitTests\"" "--logger \"console;verbosity=detailed\""];
+
+          # wrap manually, because we want not so ugly executable names
+          dontDotnetFixup = true;
+
+          preFixup = ''
+            wrapDotnetProgram $out/lib/${pname}/WalletWasabi.Fluent.Desktop $out/bin/wasabi
+            wrapDotnetProgram $out/lib/${pname}/WalletWasabi.Backend $out/bin/wbend
+          '';
+
+          microservices = "./WalletWasabi/Microservices/Binaries/lin64/";
+          preBuild = ''
+            cp -r ${pkgs.tor}/bin/tor ${microservices}/Tor/tor
+            cp ${pkgs.hwi}/bin/hwi ${microservices}/hwi
+            cp ${pkgs.bitcoind-knots}/bin/bitcoind ${microservices}/bitcoind
+          '';
+
+          skiaSharp = toString ./. + "WalletWasabi.Fluent.Desktop/bin/Debug/net8.0/runtimes/linux-x64/native";
+          LD_LIBRARY_PATH = "${skiaSharp}";
+
+          #meta = with lib; {
+          #  homepage = "https://wasabiwallet.io";
+          #  description = "The full Wasabi Wallet software";
+          #  license = licenses.mit;
+          #};
         };
 
         # dotnet trace
@@ -67,6 +114,7 @@
     in
     {
       packages.x86_64-linux.default = backend-build;
+      packages.x86_64-linux.all = build-all;
       devShells.x86_64-linux.default = wasabi-shell;
     };
 }
