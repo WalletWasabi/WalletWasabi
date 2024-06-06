b9bf0c1e7517314866721c354ab419bcd2fc4487
Yahia Chiheb <chihebyahia@gmail.com>
2024-06-06 16:16:04 +0100

    Fix formatting

--- WalletWasabi.Fluent/Behaviors/WhitespacePasteRemovalBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/WhitespacePasteRemovalBehavior.cs
@@ -19,11 +19,12 @@ public class WhitespacePasteRemovalBehavior : DisposingBehavior<TextBox>
 		{
 			var pasteEvents = Observable.FromEventPattern<EventHandler<RoutedEventArgs>, RoutedEventArgs>(
 				eh => tb.PastingFromClipboard += eh, eh => tb.PastingFromClipboard -= eh);
+
 			pasteEvents
-				.Do(args => args.EventArgs.Handled = true)	// Always mark the attempt to paste as handled, so we'll always use the customized paste.
+				.Do(args => args.EventArgs.Handled = true) // Always mark the attempt to paste as handled, so we'll always use the customized paste.
 				.Select(_ => Observable.FromAsync(ApplicationHelper.GetTextAsync, scheduler: RxApp.MainThreadScheduler)) // Executes get text asynchronously using the UI thread
-				.Concat()	// Concatenates the  results of the requests into a single observable
-				.Do(clipboardText => Paste(clipboardText, tb))	// Pastes the text
+				.Concat() // Concatenates the results of the requests into a single observable
+				.Do(clipboardText => Paste(clipboardText, tb)) // Pastes the text
 				.Subscribe()
 				.DisposeWith(disposables);
 		}
