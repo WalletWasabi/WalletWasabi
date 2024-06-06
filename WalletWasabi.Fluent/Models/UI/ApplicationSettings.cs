bb59e23904edbecb57ea11504dc3f8cfa98cb675
Roland Soós <soos.roland93@gmail.com>
2024-06-06 16:22:46 +0200

    fix saving

--- WalletWasabi.Fluent/Models/UI/ApplicationSettings.cs
+++ WalletWasabi.Fluent/Models/UI/ApplicationSettings.cs
@@ -115,7 +115,8 @@ public partial class ApplicationSettings : ReactiveObject
 		_windowState = (WindowState)Enum.Parse(typeof(WindowState), _uiConfig.WindowState);
 
 		// Save on change
-		this.WhenAnyValue(
+
+		var configChangeTrigger1 = this.WhenAnyValue(
 			x => x.EnableGpu,
 			x => x.Network,
 			x => x.StartLocalBitcoinCoreOnStartup,
@@ -129,7 +130,12 @@ public partial class ApplicationSettings : ReactiveObject
 			x => x.TerminateTorOnExit,
 			x => x.DownloadNewVersion,
 			(_, _, _, _, _, _, _, _, _, _, _, _) => Unit.Default)
-			.Skip(1)
+			.Skip(1);
+
+		var configChangeTrigger2 = this.WhenAnyValue(x => x.BackendUri).ToSignal().Skip(1);
+
+		configChangeTrigger1
+			.Merge(configChangeTrigger2)
 			.ObserveOn(RxApp.MainThreadScheduler)
 			.Throttle(TimeSpan.FromMilliseconds(ThrottleTime))
 			.Do(_ => Save())
