ace312b7a13a2eb252c04340ea3d7e04641ae1aa
ichthus1604 <ichthus1604@gmail.com>
2024-06-11 11:19:13 -0300

    SelectAll Button

--- WalletWasabi.Fluent/ViewModels/Wallets/Send/ManualControlDialogViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Send/ManualControlDialogViewModel.cs
@@ -3,7 +3,9 @@ using DynamicData.Binding;
 using ReactiveUI;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Windows.Input;
 using WalletWasabi.Blockchain.TransactionOutputs;
 using WalletWasabi.Fluent.Models.Transactions;
 using WalletWasabi.Fluent.Models.Wallets;
@@ -21,6 +23,8 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Send;
     NavigationTarget = NavigationTarget.DialogScreen)]
 public partial class ManualControlDialogViewModel: DialogViewModelBase<IEnumerable<SmartCoin>>
 {
+	[AutoNotify] private bool _hasSelection;
+
 	private readonly IWalletModel _walletModel;
 	private readonly Wallet _wallet;
 
@@ -44,6 +48,8 @@ public partial class ManualControlDialogViewModel: DialogViewModelBase<IEnumerab
 			        .ToCollection()
 			        .Select(c => c.Any() ? walletModel.AmountProvider.Create(c.TotalAmount()) : null);
 
+		ToggleSelectionCommand = ReactiveCommand.Create(() => SelectAll(!CoinList.Selection.Any()));
+
 		SetupCancel(true, true, true);
 		_walletModel = walletModel;
 		_wallet = wallet;
@@ -51,7 +57,19 @@ public partial class ManualControlDialogViewModel: DialogViewModelBase<IEnumerab
 
 	public CoinListViewModel CoinList { get; }
 
+	public ICommand ToggleSelectionCommand { get; }
+
 	public IObservable<Amount?> SelectedAmount { get; }
+
+	protected override void OnNavigatedTo(bool isInHistory, CompositeDisposable disposables)
+	{
+		CoinList.CoinItems
+				.ToObservableChangeSet()
+				.WhenPropertyChanged(x => x.IsSelected)
+				.Select(_ => CoinList.Selection.Count > 0)
+				.BindTo(this, x => x.HasSelection)
+				.DisposeWith(disposables);
+	}
 
 	protected override void OnNavigatedFrom(bool isInHistory)
 	{
@@ -68,5 +86,13 @@ public partial class ManualControlDialogViewModel: DialogViewModelBase<IEnumerab
 		var sendParameters = new SendFlowModel(_wallet, _walletModel, coins);
 
 		Navigate().To().Send(_walletModel, sendParameters);
+	}
+
+	private void SelectAll(bool value)
+	{
+		foreach (var coin in CoinList.CoinItems)
+		{
+			coin.IsSelected = value;
+		}
 	}
 }
