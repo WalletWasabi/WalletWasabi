49e6c84ab25005aeee3d1ba92c4cd4589cbf83ce
Roland Soós <soos.roland93@gmail.com>
2024-07-02 06:15:20 +0200

    make link clickable

--- WalletWasabi.Fluent/ViewModels/Dialogs/NewCoordinatorConfirmationDialogViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Dialogs/NewCoordinatorConfirmationDialogViewModel.cs
@@ -1,4 +1,5 @@
 using System.Reactive.Concurrency;
+using System.Windows.Input;
 using ReactiveUI;
 using WalletWasabi.Fluent.Helpers;
 using WalletWasabi.Fluent.ViewModels.Dialogs.Base;
@@ -9,21 +10,25 @@ namespace WalletWasabi.Fluent.ViewModels.Dialogs;
 [NavigationMetaData(Title = "Coordinator detected")]
 public partial class NewCoordinatorConfirmationDialogViewModel : DialogViewModelBase<bool>
 {
-	public NewCoordinatorConfirmationDialogViewModel(CoordinatorConnectionString coordinatorConnection)
+	private NewCoordinatorConfirmationDialogViewModel(CoordinatorConnectionString coordinatorConnection)
 	{
 		CoordinatorConnection = coordinatorConnection;
 		EnableBack = false;
 		SetupCancel(enableCancel: false, enableCancelOnEscape: true, enableCancelOnPressed: true);
 
 		NextCommand = ReactiveCommand.Create(() => Close(result: true));
+		OpenReadMoreCommand = ReactiveCommand.CreateFromTask(async () => await UiContext.FileSystem.OpenBrowserAsync(coordinatorConnection.ReadMore.ToString()));
 
 		RxApp.MainThreadScheduler.Schedule(ClearClipboardAsync);
 	}
+
+	public CoordinatorConnectionString CoordinatorConnection { get; }
+
+	public ICommand OpenReadMoreCommand { get; }
 
 	private async void ClearClipboardAsync()
 	{
 		await ApplicationHelper.SetTextAsync("");
 	}
 
-	public CoordinatorConnectionString CoordinatorConnection { get; }
 }
