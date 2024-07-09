40ab4a169cdb2df57dffb47aed558fff64e03f4c
Turbolay <turbolay@proton.me>
2024-07-09 06:48:05 -0600

    Coordinator Connection String (#13190)
    * Introduce CoordinatorConfigString
    
    * implement basic UI for the feature
    
    * Rename + Wipe clipboard on DialogCreation
    
    * Write todo NewCoordinatorConfirmationDialog refresh
    
    * Fix stupid mistakes
    
    * Add sanity checks
    
    * Add Name
    
    * Don't display dialog if new string doesn't change anything
    
    * refactoring
    
    * Polishing
    
    * make link clickable
    
    * simplification
    
    * Small refactoring
     * Move `CoordinatorConnectionString` to Discoverability directory
     * Refactor `ToString`
     * Rename `CoordinatorFeePercentage` to `CoordinationFeeRate`
    
    * Minor improvements
    
    * Minor fixes
    
    * Get navigation target from the dialog
    
    * Add constants
    
    * Rename Endpoint to CoordinatorUri
    
    ---------
    
    Co-authored-by: Turbolay <clement.ogame@gmail.com>
    Co-authored-by: Roland Soós <soos.roland93@gmail.com>
    Co-authored-by: Lucas Ontivero <lucasontivero@gmail.com>

---
+++ WalletWasabi/Discoverability/CoordinatorConnectionString.cs
@@ -0,0 +1,85 @@
+using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using NBitcoin;
+
+namespace WalletWasabi.Discoverability;
+
+public record CoordinatorConnectionString(
+	string Name,
+	Network Network,
+	Uri CoordinatorUri,
+	decimal CoordinationFeeRate,
+	int AbsoluteMinInputCount,
+	Uri ReadMore)
+{
+	public override string ToString()
+	{
+		var builder = new UriBuilder
+		{
+			Query = new NameValueCollection
+			{
+				["name"] = Uri.EscapeDataString(Name),
+				["network"] = Network.Name,
+				["coordinatorUri"] = Uri.EscapeDataString(CoordinatorUri.ToString()),
+				["coordinationFeeRate"] = CoordinationFeeRate.ToString(CultureInfo.InvariantCulture),
+				["absoluteMinInputCount"] = AbsoluteMinInputCount.ToString(),
+				["readMore"] = Uri.EscapeDataString(ReadMore.ToString())
+			}.ToString()
+		};
+		return builder.ToString();
+	}
+
+	public static bool TryParse(string s, [NotNullWhen(true)] out CoordinatorConnectionString? coordinatorConnectionString)
+	{
+		coordinatorConnectionString = null;
+
+		var queryString = HttpUtility.ParseQueryString(s);
+		string[] requiredParams = ["name", "network", "CoordinatorUri", "coordinationFeeRate", "absoluteMinInputCount", "readMore"];
+
+		if (requiredParams.Any(param => string.IsNullOrEmpty(queryString[param])))
+		{
+			return false;
+		}
+
+		var name = queryString["name"]!;
+
+		var network = Network.GetNetwork(queryString["network"]!);
+		if (network is null)
+		{
+			return false;
+		}
+
+		if (!Uri.TryCreate(queryString["coordinatorUri"], UriKind.Absolute, out var coordinatorUri))
+		{
+			return false;
+		}
+
+		if (!decimal.TryParse(queryString["coordinationFeeRate"], NumberStyles.Any, CultureInfo.InvariantCulture, out var coordinationFeeRate))
+		{
+			return false;
+		}
+
+		if (!int.TryParse(queryString["absoluteMinInputCount"], out var absoluteMinInputCount))
+		{
+			return false;
+		}
+
+		if (!Uri.TryCreate(queryString["readMore"], UriKind.Absolute, out var readMore))
+		{
+			return false;
+		}
+
+		coordinatorConnectionString = new CoordinatorConnectionString(
+			name,
+			network,
+			coordinatorUri,
+			coordinationFeeRate,
+			absoluteMinInputCount,
+			readMore);
+
+		return true;
+	}
+}
