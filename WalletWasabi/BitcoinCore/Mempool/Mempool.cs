7f947861f7b6972e547abf0ff1f821ccbf963cbb
Turbolay <turbolay@proton.me>
2024-08-15 10:33:32 -0600

    Remove usages of getrawtransaction

--- WalletWasabi/BitcoinCore/Mempool/Mempool.cs
+++ WalletWasabi/BitcoinCore/Mempool/Mempool.cs
@@ -8,10 +8,7 @@ namespace WalletWasabi.BitcoinCore.Mempool;
 public class Mempool
 {
 	/// <summary>Mapping of transaction hashes to the corresponding transactions.</summary>
-	private Dictionary<uint256, Transaction> TransactionIndex { get; init; } = new();
-
-	/// <summary>Mapping of UTXOs to the corresponding spending transactions.</summary>
-	private Dictionary<OutPoint, Transaction> PrevOutsIndex { get; init; } = new();
+	private Dictionary<uint256, byte> TransactionIndex { get; init; } = new();
 
 	/// <summary>
 	/// Checks whether a transaction with the given hash is in the mempool.
@@ -23,46 +20,24 @@ public class Mempool
 
 	/// <summary>
 	/// Adds a new transaction to the mempool.
-	/// If <paramref name="tx"/> spends an <see cref="OutPoint"/> that is already spent by a different mempool transaction
-	/// in the mempool, we remove the mempool transaction(s).
 	/// </summary>
-	/// <exception cref="ArgumentException">If the transaction is already in the mempool.</exception>
-	/// <exception cref="InvalidOperationException">If there is an internal issue.</exception>
-	public void AddTransaction(Transaction tx)
-	{
-		// Evict double spends.
-		foreach (TxIn txInput in tx.Inputs)
-		{
-			// tx spends a UTXO which some *other* transaction in the mempool *already* spends.
-			if (PrevOutsIndex.TryGetValue(txInput.PrevOut, out Transaction? mempoolTx))
-			{
-				// Remove the old transaction from our mempool snapshot.
-				if (!TryRemoveTransaction(mempoolTx.GetHash()))
-				{
-					throw new InvalidOperationException($"Failed to remove '{mempoolTx.GetHash()}' transaction.");
-				}
-			}
-		}
-
-		TransactionIndex.Add(tx.GetHash(), tx);
-
-		foreach (TxIn txInput in tx.Inputs)
-		{
-			PrevOutsIndex.Add(txInput.PrevOut, tx);
-		}
-	}
+	/// <remarks>
+	/// Double spends are not evicted, but they will be invalidated at every call of GetRawMempoolAsync
+	/// </remarks>
+	public bool AddTransaction(uint256 txHash)
+		=> TransactionIndex.TryAdd(txHash, new byte());
 
 	/// <summary>
 	/// Adds the given transactions to the mempool.
 	/// <para>If the transaction is already in the mempool, it is not added.</para>
 	/// </summary>
 	/// <returns>Number of added transactions.</returns>
-	public int AddMissingTransactions(IEnumerable<Transaction> txs)
+	public int AddMissingTransactions(IEnumerable<uint256> txsHash)
 	{
 		int added = 0;
-		foreach (Transaction tx in txs.Where(x => !ContainsTransaction(x.GetHash())))
+		foreach (uint256 txHash in txsHash.Where(x => !ContainsTransaction(x)))
 		{
-			AddTransaction(tx);
+			TransactionIndex.TryAdd(txHash, new byte());
 			added++;
 		}
 
@@ -72,21 +47,8 @@ public class Mempool
 	/// <summary>
 	/// Removes the transaction with the given transaction hash from the mempool.
 	/// </summary>
-	public bool TryRemoveTransaction(uint256 txHash)
-	{
-		if (TransactionIndex.Remove(txHash, out Transaction? transaction))
-		{
-			// Remove all UTXOs of the removed transaction from our helper index.
-			foreach (TxIn txInput in transaction.Inputs)
-			{
-				PrevOutsIndex.Remove(txInput.PrevOut);
-			}
-
-			return true;
-		}
-
-		return false;
-	}
+	public bool RemoveTransaction(uint256 txHashToRemove)
+		=> TransactionIndex.Remove(txHashToRemove, out _);
 
 	/// <summary>
 	/// Gets a copy of all transaction hashes in the mempool.
@@ -94,26 +56,5 @@ public class Mempool
 	public ISet<uint256> GetMempoolTxids()
 	{
 		return TransactionIndex.Keys.ToHashSet();
-	}
-
-	/// <summary>
-	/// Gets transactions from the mempool that spends <paramref name="txOuts"/>.
-	/// </summary>
-	public IReadOnlySet<Transaction> GetSpenderTransactions(IEnumerable<OutPoint> txOuts)
-	{
-		HashSet<OutPoint> txOutsSet = txOuts.ToHashSet();
-
-		return txOutsSet.Where(prevOut => PrevOutsIndex.ContainsKey(prevOut))
-			.Select(prevOut => PrevOutsIndex[prevOut])
-			.ToHashSet();
-	}
-
-	public Mempool Clone()
-	{
-		return new Mempool()
-		{
-			TransactionIndex = new Dictionary<uint256, Transaction>(TransactionIndex),
-			PrevOutsIndex = new Dictionary<OutPoint, Transaction>(PrevOutsIndex)
-		};
 	}
 }
