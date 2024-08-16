02e71e8abfcab0e52d0b3947e812edea99fe8d02
José Manuel Nieto <superjmn@outlook.com>
2024-08-16 12:37:18 +0200

    Use local time

--- WalletWasabi.Fluent/Extensions/DateTimeOffsetExtensions.cs
+++ WalletWasabi.Fluent/Extensions/DateTimeOffsetExtensions.cs
@@ -2,9 +2,9 @@ namespace WalletWasabi.Fluent.Extensions;
 
 public static class DateTimeOffsetExtensions
 {
-	public static string ToUserFacingString(this DateTimeOffset value, bool withTime = true) => value.DateTime.ToUserFacingString(withTime);
+	public static string ToUserFacingString(this DateTimeOffset value, bool withTime = true) => value.LocalDateTime.ToUserFacingString(withTime);
 
-	public static string ToUserFacingFriendlyString(this DateTimeOffset value) => value.DateTime.ToUserFacingFriendlyString();
+	public static string ToUserFacingFriendlyString(this DateTimeOffset value) => value.LocalDateTime.ToUserFacingFriendlyString();
 
 	public static string ToOnlyTimeString(this DateTimeOffset value) => value.ToString("HH:mm");
 }
