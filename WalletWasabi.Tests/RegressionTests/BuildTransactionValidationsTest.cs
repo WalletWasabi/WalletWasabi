00288b29d2114fc0c1d5192d8b59843a3893f8e0
Lucas Ontivero <lontivero@users.noreply.github.com>
2024-12-12 19:52:05 -0500

    Wire components using IoC (#13076)

--- WalletWasabi.Tests/RegressionTests/BuildTransactionValidationsTest.cs
+++ WalletWasabi.Tests/RegressionTests/BuildTransactionValidationsTest.cs
@@ -43,13 +43,12 @@ public class BuildTransactionValidationsTest : IClassFixture<RegTestFixture>
 		IRPCClient rpc = setup.RpcClient;
 		Network network = setup.Network;
 		BitcoinStore bitcoinStore = setup.BitcoinStore;
-		using Backend.Global global = setup.Global;
 		string password = setup.Password;
 		ServiceConfiguration serviceConfiguration = setup.ServiceConfiguration;
 
 		// Create the services.
 		// 1. Create connection service.
-		NodesGroup nodes = new(global.Config.Network, requirements: Constants.NodeRequirements);
+		NodesGroup nodes = new(setup.Network, requirements: Constants.NodeRequirements);
 		nodes.ConnectedNodes.Add(await RegTestFixture.BackendRegTestNode.CreateNewP2pNodeAsync());
 
 		// 2. Create mempool service.
--- WalletWasabi.Tests/RegressionTests/MaxFeeTests.cs
+++ WalletWasabi.Tests/RegressionTests/MaxFeeTests.cs
@@ -45,7 +45,6 @@ public class MaxFeeTests : IClassFixture<RegTestFixture>
 		IRPCClient rpc = setup.RpcClient;
 		Network network = setup.Network;
 		BitcoinStore bitcoinStore = setup.BitcoinStore;
-		using Backend.Global global = setup.Global;
 		ServiceConfiguration serviceConfiguration = setup.ServiceConfiguration;
 		string password = setup.Password;
 
@@ -53,7 +52,7 @@ public class MaxFeeTests : IClassFixture<RegTestFixture>
 
 		// Create the services.
 		// 1. Create connection service.
-		NodesGroup nodes = new(global.Config.Network, requirements: Constants.NodeRequirements);
+		NodesGroup nodes = new(setup.Network, requirements: Constants.NodeRequirements);
 		nodes.ConnectedNodes.Add(await RegTestFixture.BackendRegTestNode.CreateNewP2pNodeAsync());
 
 		// 2. Create mempool service.
