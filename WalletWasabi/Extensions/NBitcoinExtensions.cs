249ad6be8a83ce41a9f868b3d84bbbe9f2d5fb85
Lucas Ontivero <lucasontivero@gmail.com>
2025-01-06 12:58:21 -0300

    Add P2SH as unknown script types

--- WalletWasabi/Extensions/NBitcoinExtensions.cs
+++ WalletWasabi/Extensions/NBitcoinExtensions.cs
@@ -403,12 +403,12 @@ public static class NBitcoinExtensions
 
 	public static ScriptType GetScriptType(this Script script)
 	{
-		return TryGetScriptType(script) ?? throw new NotImplementedException($"Unsupported script type.");
+		return TryGetScriptType(script) ?? throw new NotImplementedException("Unknown script type.");
 	}
 
 	public static ScriptType? TryGetScriptType(this Script script)
 	{
-		foreach (ScriptType scriptType in new[] { ScriptType.P2WPKH, ScriptType.P2PKH, ScriptType.P2PK, ScriptType.P2WSH, ScriptType.Taproot })
+		foreach (ScriptType scriptType in new[] { ScriptType.P2WPKH, ScriptType.P2PKH, ScriptType.P2PK, ScriptType.P2WSH, ScriptType.Taproot, ScriptType.P2SH })
 		{
 			if (script.IsScriptType(scriptType))
 			{
