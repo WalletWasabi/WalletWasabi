3205f92aba757b1cf513f875f2b85684c0aaa065
Lucas Ontivero <lontivero@users.noreply.github.com>
2025-03-17 11:24:17 -0300

    Remove unused methods in `WasabiClient` (#13790)

--- WalletWasabi/WebClients/Wasabi/WasabiClient.cs
+++ WalletWasabi/WebClients/Wasabi/WasabiClient.cs
@@ -1,7 +1,5 @@
 using NBitcoin;
 using NBitcoin.RPC;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -12,8 +10,6 @@ using WalletWasabi.Blockchain.Transactions;
 using WalletWasabi.Extensions;
 using WalletWasabi.Logging;
 using WalletWasabi.Serialization;
-using WalletWasabi.Services;
-using WalletWasabi.Tor.Http;
 
 namespace WalletWasabi.WebClients.Wasabi;
 
@@ -26,9 +22,6 @@ public class WasabiClient
 
 	private readonly HttpClient _httpClient;
 
-	public static Dictionary<uint256, Transaction> TransactionCache { get; } = new();
-	private static Queue<uint256> TransactionIdQueue { get; } = new();
-	public static object TransactionCacheLock { get; } = new();
 	public static ushort ApiVersion { get; private set; } = ushort.Parse(Helpers.Constants.BackendMajorVersion);
 
 	#region batch
@@ -86,76 +79,15 @@ public class WasabiClient
 		return ret;
 	}
 
-	public async Task<IEnumerable<Transaction>> GetTransactionsAsync(Network network, IEnumerable<uint256> txHashes, CancellationToken cancel)
+	public async Task BroadcastAsync(SmartTransaction transaction, CancellationToken cancellationToken)
 	{
-		var allTxs = new List<Transaction>();
-		var txHashesToQuery = new List<uint256>();
-		lock (TransactionCacheLock)
-		{
-			var cachedTxs = TransactionCache.Where(x => txHashes.Contains(x.Key));
-			allTxs.AddRange(cachedTxs.Select(x => x.Value));
-			txHashesToQuery.AddRange(txHashes.Except(cachedTxs.Select(x => x.Key)));
-		}
-
-		foreach (IEnumerable<uint256> chunk in txHashesToQuery.ChunkBy(10))
-		{
-			cancel.ThrowIfCancellationRequested();
-
-			using HttpRequestMessage request = new(
-				HttpMethod.Get,
-				$"api/v{ApiVersion}/btc/blockchain/transaction-hexes?&transactionIds={string.Join("&transactionIds=", chunk.Select(x => x.ToString()))}");
-			using HttpResponseMessage response = await _httpClient.SendAsync(request, cancel).ConfigureAwait(false);
-
-			if (response.StatusCode != HttpStatusCode.OK)
-			{
-				await response.ThrowRequestExceptionFromContentAsync(cancel).ConfigureAwait(false);
-			}
-
-			using HttpContent content = response.Content;
-			var retString = await content.ReadAsJsonAsync(Decode.Array(Decode.String)).ConfigureAwait(false);
-			var ret = retString.Select(x => Transaction.Parse(x, network)).ToList();
-
-			lock (TransactionCacheLock)
-			{
-				foreach (var tx in ret)
-				{
-					tx.PrecomputeHash(false, true);
-					if (TransactionCache.TryAdd(tx.GetHash(), tx))
-					{
-						TransactionIdQueue.Enqueue(tx.GetHash());
-						if (TransactionCache.Count > 1000) // No more than 1000 txs in cache
-						{
-							var toRemove = TransactionIdQueue.Dequeue();
-							TransactionCache.Remove(toRemove);
-						}
-					}
-				}
-			}
-			allTxs.AddRange(ret);
-		}
-
-		return allTxs.ToDependencyGraph().OrderByDependency();
-	}
-
-	public async Task BroadcastAsync(string hex, CancellationToken cancellationToken)
-	{
-		using var content = new StringContent($"\"{hex}\"", Encoding.UTF8, "application/json");
+		using var content = new StringContent($"\"{transaction.Transaction.ToHex()}\"", Encoding.UTF8, "application/json");
 		using HttpResponseMessage response = await _httpClient.PostAsync($"api/v{ApiVersion}/btc/blockchain/broadcast", content, cancellationToken).ConfigureAwait(false);
 
 		if (response.StatusCode != HttpStatusCode.OK)
 		{
 			await response.ThrowRequestExceptionFromContentAsync(CancellationToken.None).ConfigureAwait(false);
 		}
-	}
-
-	public async Task BroadcastAsync(Transaction transaction, CancellationToken cancellationToken)
-	{
-		await BroadcastAsync(transaction.ToHex(), cancellationToken).ConfigureAwait(false);
-	}
-
-	public async Task BroadcastAsync(SmartTransaction transaction, CancellationToken cancellationToken)
-	{
-		await BroadcastAsync(transaction.Transaction, cancellationToken).ConfigureAwait(false);
 	}
 
 	public async Task<ushort> GetBackendMajorVersionAsync(CancellationToken cancel)
