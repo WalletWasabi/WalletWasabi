de29050c605648cf1dc4aad03b385576a0885dc0
Lucas Ontivero <lucasontivero@gmail.com>
2025-03-18 17:17:13 -0300

    Sync from rpc

--- WalletWasabi/BitcoinRpc/RpcMonitor.cs
+++ WalletWasabi/BitcoinRpc/RpcMonitor.cs
@@ -1,39 +1,42 @@
 using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.Bases;
-using WalletWasabi.Extensions;
+using WalletWasabi.Logging;
+using WalletWasabi.Services;
 
 namespace WalletWasabi.BitcoinRpc;
 
 public class RpcMonitor : PeriodicRunner
 {
-	private RpcStatus _rpcStatus;
+	private readonly EventBus _eventBus;
+	private readonly IRPCClient _rpcClient;
 
-	public RpcMonitor(TimeSpan period, IRPCClient rpcClient) : base(period)
+	public RpcMonitor(TimeSpan period, IRPCClient rpcClient, EventBus eventBus) : base(period)
 	{
-		_rpcStatus = RpcStatus.Connecting;
-		RpcClient = rpcClient;
+		_eventBus = eventBus;
+		_rpcClient = rpcClient;
 	}
 
-	public event EventHandler<RpcStatus>? RpcStatusChanged;
-
-	public IRPCClient RpcClient { get; set; }
-
-	public RpcStatus RpcStatus
-	{
-		get => _rpcStatus;
-		private set
-		{
-			if (value != _rpcStatus)
-			{
-				_rpcStatus = value;
-				RpcStatusChanged?.Invoke(this, value);
-			}
-		}
-	}
 
 	protected override async Task ActionAsync(CancellationToken cancel)
 	{
-		RpcStatus = await RpcClient.GetRpcStatusAsync(cancel).ConfigureAwait(false);
+		var rpcStatus = await GetRpcStatusAsync(cancel).ConfigureAwait(false);
+		_eventBus.Publish(new RpcStatusChanged(rpcStatus));
+	}
+
+
+	private async Task<RpcStatus> GetRpcStatusAsync(CancellationToken cancel)
+	{
+		try
+		{
+			var bci = await _rpcClient.GetBlockchainInfoAsync(cancel).ConfigureAwait(false);
+			var pi = await _rpcClient.GetPeersInfoAsync(cancel).ConfigureAwait(false);
+			return new RpcStatus.Responsive(bci.Headers, bci.Blocks, pi.Length, bci.BestBlockHash, bci.Pruned, bci.InitialBlockDownload);
+		}
+		catch (Exception ex) when (ex is not OperationCanceledException and not TimeoutException)
+		{
+			Logger.LogTrace(ex);
+			return new RpcStatus.Unresponsive();
+		}
 	}
 }
