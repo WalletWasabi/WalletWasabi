2346bb234c919df4f788ba3f2ce995b838ddf264
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-03-20 20:54:02 +0100

    Update Avalonia and related NuGet packages to 11.2.5

--- WalletWasabi.Fluent/ApplicationStateManager.cs
+++ WalletWasabi.Fluent/ApplicationStateManager.cs
@@ -1,8 +1,10 @@
 using System.ComponentModel;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Platform;
 using ReactiveUI;
 using WalletWasabi.Fluent.Extensions;
 using WalletWasabi.Fluent.Helpers;
@@ -27,14 +29,16 @@ public class ApplicationStateManager : IMainWindowService
 	private bool _hideRequest;
 	private bool _isShuttingDown;
 	private bool _restartRequest;
-	private IActivatableApplicationLifetime? _activatable;
+	private IActivatableLifetime? _activatable;
 
 	internal ApplicationStateManager(IClassicDesktopStyleApplicationLifetime lifetime, UiContext uiContext, bool startInBg)
 	{
 		_lifetime = lifetime;
 		_stateMachine = new StateMachine<State, Trigger>(State.InitialState);
 
-		if (_lifetime is IActivatableApplicationLifetime activatableLifetime)
+		var activatableLifetime = Application.Current?.TryGetFeature<IActivatableLifetime>();
+
+		if (activatableLifetime is not null)
 		{
 			if (startInBg)
 			{
@@ -185,9 +189,9 @@ public class ApplicationStateManager : IMainWindowService
 
 		MainViewModel.Instance.ApplyUiConfigWindowState();
 
-		if (_lifetime is IActivatableApplicationLifetime activatable)
+		if (_activatable is not null)
 		{
-			activatable.TryLeaveBackground();
+			_activatable.TryLeaveBackground();
 		}
 
 		var result = new MainWindow
