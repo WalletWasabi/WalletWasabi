ac0dd353f5a8fec6b947c0a77b08797bedbb9af1
Lucas Ontivero <lucasontivero@gmail.com>
2025-03-23 00:29:11 -0300

    Fix status icon for rpc

--- WalletWasabi/BitcoinRpc/RpcStatus.cs
+++ WalletWasabi/BitcoinRpc/RpcStatus.cs
@@ -4,22 +4,28 @@ namespace WalletWasabi.BitcoinRpc;
 
 public abstract record RpcStatus
 {
-	public record Unresponsive : RpcStatus;
+	public record Unresponsive : RpcStatus
+	{
+		public override string? ToString() => "is unresponsive";
+	}
 
-	public record Responsive(ulong Headers, ulong Blocks, int PeersCount, uint256 BestBlockHash, bool Pruned, bool InitialBlockDownload) : RpcStatus;
+	public record Responsive(
+		ulong Headers,
+		ulong Blocks,
+		int PeersCount,
+		uint256 BestBlockHash,
+		bool Pruned,
+		bool InitialBlockDownload) : RpcStatus
+	{
+		public override string? ToString() =>
+			(PeersCount, Synchronized) switch
+			{
+				(0, _) => "is connecting...",
+				(_, true) => "is synchronized",
+				(_, false) => "is synchronizing..."
+			};
+	}
+
 	public bool Synchronized => this is Responsive r && r.Blocks == r.Headers;
 
-	public override string ToString()
-	{
-		if (this is Responsive r)
-		{
-			if (r.PeersCount == 0)
-			{
-				return "is connecting...";
-			}
-
-			return Synchronized ? "is synchronized" : "is synchronizing...";
-		}
-		return "is unresponsive";
-	}
 }
