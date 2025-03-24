764ea91f822374ca592578126411cf21f8c0783c
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-03-24 22:19:41 +0100

    Navigate to Multi-share view

--- WalletWasabi.Fluent/ViewModels/AddWallet/Create/MultiShareOptionsViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/AddWallet/Create/MultiShareOptionsViewModel.cs
@@ -51,7 +51,7 @@ public partial class MultiShareOptionsViewModel : RoutableViewModel
 			}
 		};
 
-		Navigate().To().ConfirmMultiShare(options);
+		Navigate().To().MultiShare(options);
 	}
 
 	private void OnCancel()
