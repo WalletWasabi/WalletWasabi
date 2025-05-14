119e9bfc6c57eec07d2ac6a56983c6afed200925
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-05-14 20:29:04 +0200

    Move after starting code into AfterStarting method

--- WalletWasabi.Fluent.Desktop/WasabiAppExtensions.cs
+++ WalletWasabi.Fluent.Desktop/WasabiAppExtensions.cs
@@ -19,54 +19,55 @@ public static class WasabiAppExtensions
 {
 	public static async Task<ExitCode> RunAsGuiAsync(this WasabiApplication app)
 	{
-		return await app.RunAsync(
-			afterStarting: () =>
+		return await app.RunAsync(afterStarting: () => AfterStarting(app));
+	}
+
+	private static Task AfterStarting(WasabiApplication app)
+	{
+		RxApp.DefaultExceptionHandler = Observer.Create<Exception>(ex =>
+		{
+			if (Debugger.IsAttached)
 			{
-				RxApp.DefaultExceptionHandler = Observer.Create<Exception>(ex =>
+				Debugger.Break();
+			}
+
+			Logger.LogError(ex);
+
+			RxApp.MainThreadScheduler.Schedule(() => throw new ApplicationException("Exception has been thrown in unobserved ThrownExceptions", ex));
+		});
+
+		Logger.LogInfo("Wasabi GUI started.");
+		bool runGuiInBackground = app.AppConfig.Arguments.Any(arg => arg.Contains(StartupHelper.SilentArgument));
+		UiConfig uiConfig = LoadOrCreateUiConfig(Config.DataDir);
+		Services.Initialize(app.Global!, uiConfig, app.SingleInstanceChecker, app.TerminateService);
+
+		using CancellationTokenSource stopLoadingCts = new();
+
+		AppBuilder appBuilder = AppBuilder
+			.Configure(() => new App(
+				backendInitialiseAsync: async () =>
 				{
-					if (Debugger.IsAttached)
-					{
-						Debugger.Break();
-					}
+					// macOS require that Avalonia is started with the UI thread. Hence this call must be delayed to this point.
+					await app.Global!.InitializeNoWalletAsync(initializeSleepInhibitor: true, app.TerminateService, stopLoadingCts.Token).ConfigureAwait(false);
 
-					Logger.LogError(ex);
+					// Make sure that wallet startup set correctly regarding RunOnSystemStartup
+					await StartupHelper.ModifyStartupSettingAsync(uiConfig.RunOnSystemStartup).ConfigureAwait(false);
+				}, startInBg: runGuiInBackground))
+			.UseReactiveUI()
+			.SetupAppBuilder()
+			.AfterSetup(_ => ThemeHelper.ApplyTheme(uiConfig.DarkModeEnabled ? Theme.Dark : Theme.Light));
 
-					RxApp.MainThreadScheduler.Schedule(() => throw new ApplicationException("Exception has been thrown in unobserved ThrownExceptions", ex));
-				});
+		if (app.TerminateService.CancellationToken.IsCancellationRequested)
+		{
+			Logger.LogDebug("Skip starting Avalonia UI as requested the application to stop.");
+			stopLoadingCts.Cancel();
+		}
+		else
+		{
+			appBuilder.StartWithClassicDesktopLifetime(app.AppConfig.Arguments);
+		}
 
-				Logger.LogInfo("Wasabi GUI started.");
-				bool runGuiInBackground = app.AppConfig.Arguments.Any(arg => arg.Contains(StartupHelper.SilentArgument));
-				UiConfig uiConfig = LoadOrCreateUiConfig(Config.DataDir);
-				Services.Initialize(app.Global!, uiConfig, app.SingleInstanceChecker, app.TerminateService);
-
-				using CancellationTokenSource stopLoadingCts = new();
-
-				AppBuilder appBuilder = AppBuilder
-					.Configure(() => new App(
-						backendInitialiseAsync: async () =>
-						{
-							// macOS require that Avalonia is started with the UI thread. Hence this call must be delayed to this point.
-							await app.Global!.InitializeNoWalletAsync(initializeSleepInhibitor: true, app.TerminateService, stopLoadingCts.Token).ConfigureAwait(false);
-
-							// Make sure that wallet startup set correctly regarding RunOnSystemStartup
-							await StartupHelper.ModifyStartupSettingAsync(uiConfig.RunOnSystemStartup).ConfigureAwait(false);
-						}, startInBg: runGuiInBackground))
-					.UseReactiveUI()
-					.SetupAppBuilder()
-					.AfterSetup(_ => ThemeHelper.ApplyTheme(uiConfig.DarkModeEnabled ? Theme.Dark : Theme.Light));
-
-				if (app.TerminateService.CancellationToken.IsCancellationRequested)
-				{
-					Logger.LogDebug("Skip starting Avalonia UI as requested the application to stop.");
-					stopLoadingCts.Cancel();
-				}
-				else
-				{
-					appBuilder.StartWithClassicDesktopLifetime(app.AppConfig.Arguments);
-				}
-
-				return Task.CompletedTask;
-			});
+		return Task.CompletedTask;
 	}
 
 	private static UiConfig LoadOrCreateUiConfig(string dataDir)
