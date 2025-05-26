6a2c47d6ec6a558e82eeb130246b8928dfaea8ca
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-05-26 16:11:24 +0200

    add decoders

--- WalletWasabi/Serialization/External.cs
+++ WalletWasabi/Serialization/External.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using WalletWasabi.Backend.Models;
 using WalletWasabi.Models;
+using WalletWasabi.Tor.StatusChecker;
 
 namespace WalletWasabi.Serialization;
 
@@ -49,5 +50,37 @@ public static partial class Decode
 		Object(get => new ExchangeRate{
 			Ticker = "USD",
 			Rate = get.Required("USD", Field("Sell", Decimal))
+		});
+
+	public static readonly Decoder<TorIssue> TorIssueDecoder =
+		Object(get => new TorIssue
+		{
+			Is = get.Required("is", String),
+			Title = get.Required("title", String),
+			CreatedAt = get.Required("createdAt", String),
+			LastMod = get.Required("lastMod", String),
+			Permalink = get.Required("permalink", String),
+			Severity = get.Required("severity", String),
+			Resolved = get.Required("resolved", Bool),
+			Informational = get.Required("informational", Bool),
+			ResolvedAt = get.Required("resolvedAt", String),
+			Affected = get.Required("affected", Array(String)).ToList(),
+			Filename = get.Required("filename", String)
+		});
+
+	public static readonly Decoder<SystemItem> TorSystemItemDecoder =
+		Object(get => new SystemItem
+		{
+			Name = get.Required("name", String),
+			Description = get.Required("description", String),
+			Category = get.Required("category", String),
+			Status = get.Required("status", String),
+			UnresolvedIssues = get.Required("unresolvedIssues", Array(TorIssueDecoder)).ToList()
+		});
+
+	public static readonly Decoder<TorNetworkStatus> TorStatus =
+		Object(get => new TorNetworkStatus
+		{
+			Systems = get.Required("systems", Array(TorSystemItemDecoder)).ToList()
 		});
 }
