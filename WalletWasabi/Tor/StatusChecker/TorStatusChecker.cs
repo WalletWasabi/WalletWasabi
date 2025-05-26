6ba661724ad8a6365f629d5a3d6072c93e520557
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-05-26 16:11:52 +0200

    modify Tor Status Checker

--- WalletWasabi/Tor/StatusChecker/TorStatusChecker.cs
+++ WalletWasabi/Tor/StatusChecker/TorStatusChecker.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using WalletWasabi.Bases;
+using WalletWasabi.Extensions;
 using WalletWasabi.Logging;
+using WalletWasabi.Serialization;
 using WalletWasabi.Services;
 
 namespace WalletWasabi.Tor.StatusChecker;
@@ -13,14 +16,12 @@ namespace WalletWasabi.Tor.StatusChecker;
 /// </summary>
 public class TorStatusChecker : PeriodicRunner
 {
-	private readonly XmlIssueListParser _parser;
 	private readonly EventBus _eventBus;
-	private static readonly Uri TorStatusUri = new("https://status.torproject.org/index.xml");
+	private static readonly Uri TorStatusUri = new("https://status.torproject.org/index.json");
 
-	public TorStatusChecker(TimeSpan period, HttpClient httpClient, XmlIssueListParser parser, EventBus eventBus)
+	public TorStatusChecker(TimeSpan period, HttpClient httpClient, EventBus eventBus)
 		: base(period)
 	{
-		_parser = parser;
 		_eventBus = eventBus;
 		_httpClient = httpClient;
 	}
@@ -34,16 +35,38 @@ public class TorStatusChecker : PeriodicRunner
 		{
 			using HttpRequestMessage request = new(HttpMethod.Get, TorStatusUri);
 			using HttpResponseMessage response = await _httpClient.SendAsync(request, cancellationToken).ConfigureAwait(false);
+			using var content = response.Content;
 
-			string xml = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
-			var issues = _parser.Parse(xml);
+			TorNetworkStatus systemsStatus = await content.ReadAsJsonAsync(Decode.TorStatus).ConfigureAwait(false);
+			Issue[] issues = CheckForTorIssues(systemsStatus);
 
 			// Fire event.
-			_eventBus.Publish(new TorNetworkStatusChanged(issues.ToArray()));
+			_eventBus.Publish(new TorNetworkStatusChanged(issues));
 		}
 		catch (Exception ex)
 		{
 			Logger.LogDebug("Failed to get/parse Tor status page.", ex);
 		}
+	}
+
+	/// <summary>
+	/// Checks the relevant systems for any issues.
+	/// Relevant systems are: v3 Onion Services, Directory Authorities and DNS.
+	/// </summary>
+	private Issue[] CheckForTorIssues(TorNetworkStatus systemsStatus)
+	{
+		string[] systemNames = { "v3 Onion Service", "Directory Authorities", "DNS" };
+
+		var issues = systemsStatus.Systems
+			.Where(system => systemNames.Contains(system.Name))
+			.Where(system => system.Status != "ok" || system.UnresolvedIssues.Count != 0)
+			.Select(system =>
+			{
+				bool isResolved = system.UnresolvedIssues.Any(issue => !issue.Resolved);
+				return new Issue(system.Name, resolved: isResolved);
+			})
+			.ToArray();
+
+		return issues;
 	}
 }
