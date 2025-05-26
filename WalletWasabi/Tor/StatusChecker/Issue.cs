8b0f72ed49b4efb8fd84a2aa2c7a75772a3b66de
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-05-26 18:49:16 +0200

    expand Issue class, show IssueTitle on UI

--- WalletWasabi/Tor/StatusChecker/Issue.cs
+++ WalletWasabi/Tor/StatusChecker/Issue.cs
@@ -2,17 +2,19 @@ namespace WalletWasabi.Tor.StatusChecker;
 
 public class Issue
 {
-	public Issue(string title, bool resolved)
+	public Issue(string systemName, string issueTitle,bool resolved)
 	{
-		Title = title;
+		SystemName = systemName;
+		IssueTitle = issueTitle;
 		Resolved = resolved;
 	}
 
-	public string Title { get; }
+	public string SystemName { get; }
+	public string IssueTitle { get; }
 	public bool Resolved { get; }
 
 	public override string ToString()
 	{
-		return $"{nameof(Title)}: {Title}, {nameof(Resolved)}: {Resolved}";
+		return $"{nameof(SystemName)}: {SystemName}, {nameof(IssueTitle)}: {IssueTitle} ,{nameof(Resolved)}: {Resolved}";
 	}
 }
