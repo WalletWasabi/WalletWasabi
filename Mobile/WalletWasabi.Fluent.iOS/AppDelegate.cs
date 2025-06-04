cad87010f124b9ea4e8e73e83273c0dbf8f5dfe5
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-06-04 22:47:54 +0200

    Add WalletWasabi.Fluent.iOS project stub

---
+++ Mobile/WalletWasabi.Fluent.iOS/AppDelegate.cs
@@ -0,0 +1,23 @@
+using Foundation;
+using UIKit;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.iOS;
+using Avalonia.Media;
+
+namespace WalletWasabi.Fluent.iOS;
+
+// The UIApplicationDelegate for the application. This class is responsible for launching the 
+// User Interface of the application, as well as listening (and optionally responding) to 
+// application events from iOS.
+[Register("AppDelegate")]
+#pragma warning disable CA1711 // Identifiers should not have incorrect suffix
+public partial class AppDelegate : AvaloniaAppDelegate<App>
+#pragma warning restore CA1711 // Identifiers should not have incorrect suffix
+{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont();
+    }
+}
