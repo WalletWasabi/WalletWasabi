3b911422976983e3b284d6939de9d535aa1ee017
Dmitry Astakhin <dmitry.astakhin@gmail.com>
2025-06-21 13:26:15 +0300

    Moved RenameViewModel to WalletSettingsViewModel

--- WalletWasabi.Fluent/ViewModels/Wallets/Settings/WalletSettingsViewModel.cs
+++ WalletWasabi.Fluent/ViewModels/Wallets/Settings/WalletSettingsViewModel.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
+using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using NBitcoin;
 using ReactiveUI;
@@ -9,6 +9,7 @@ using WalletWasabi.Fluent.Helpers;
 using WalletWasabi.Fluent.Models.UI;
 using WalletWasabi.Fluent.Infrastructure;
 using WalletWasabi.Fluent.Models.Wallets;
+using WalletWasabi.Fluent.Validation;
 using WalletWasabi.Fluent.ViewModels.Navigation;
 using WalletWasabi.Models;
 using ScriptType = WalletWasabi.Fluent.Models.Wallets.ScriptType;
@@ -17,146 +18,174 @@ namespace WalletWasabi.Fluent.ViewModels.Wallets.Settings;
 
 [AppLifetime]
 [NavigationMetaData(
-	Title = "Wallet Settings",
-	Caption = "Display wallet settings",
-	IconName = "nav_wallet_24_regular",
-	Order = 2,
-	Category = "Wallet",
-	Keywords = new[] { "Wallet", "Settings", },
-	NavBarPosition = NavBarPosition.None,
-	NavigationTarget = NavigationTarget.DialogScreen,
-	Searchable = false)]
+    Title = "Wallet Settings",
+    Caption = "Display wallet settings",
+    IconName = "nav_wallet_24_regular",
+    Order = 2,
+    Category = "Wallet",
+    Keywords = new[] { "Wallet", "Settings", },
+    NavBarPosition = NavBarPosition.None,
+    NavigationTarget = NavigationTarget.DialogScreen,
+    Searchable = false)]
 public partial class WalletSettingsViewModel : RoutableViewModel
 {
-	private readonly IWalletModel _wallet;
-	[AutoNotify] private bool _preferPsbtWorkflow;
-	[AutoNotify] private string _walletName;
-	[AutoNotify] private int _selectedTab;
-	[AutoNotify] private ScriptType _defaultReceiveScriptType;
-	[AutoNotify] private bool _isSegWitDefaultReceiveScriptType;
-	[AutoNotify] private WalletWasabi.Models.PreferredScriptPubKeyType _changeScriptPubKeyType;
-	[AutoNotify] private WalletWasabi.Models.SendWorkflow _defaultSendWorkflow;
-	[AutoNotify] private bool _isAutomaticDefaultSendWorkflow;
+    private readonly IWalletModel _wallet;
+    [AutoNotify] private bool _preferPsbtWorkflow;
+    [AutoNotify] private string _walletName;
+    [AutoNotify] private string _newWalletName;
+    [AutoNotify] private int _selectedTab;
+    [AutoNotify] private ScriptType _defaultReceiveScriptType;
+    [AutoNotify] private bool _isSegWitDefaultReceiveScriptType;
+    [AutoNotify] private WalletWasabi.Models.PreferredScriptPubKeyType _changeScriptPubKeyType;
+    [AutoNotify] private WalletWasabi.Models.SendWorkflow _defaultSendWorkflow;
+    [AutoNotify] private bool _isAutomaticDefaultSendWorkflow;
 
-	public WalletSettingsViewModel(UiContext uiContext, IWalletModel walletModel)
-	{
-		UiContext = uiContext;
-		_wallet = walletModel;
-		_walletName = walletModel.Name;
-		_preferPsbtWorkflow = walletModel.Settings.PreferPsbtWorkflow;
-		_selectedTab = 0;
-		IsHardwareWallet = walletModel.IsHardwareWallet;
-		IsWatchOnly = walletModel.IsWatchOnlyWallet;
+    public WalletSettingsViewModel(UiContext uiContext, IWalletModel walletModel)
+    {
+        UiContext = uiContext;
+        _wallet = walletModel;
+        _walletName = walletModel.Name;
+        _newWalletName = walletModel.Name;
+        _preferPsbtWorkflow = walletModel.Settings.PreferPsbtWorkflow;
+        _selectedTab = 0;
+        IsHardwareWallet = walletModel.IsHardwareWallet;
+        IsWatchOnly = walletModel.IsWatchOnlyWallet;
 
-		DefaultReceiveScriptType = walletModel.Settings.DefaultReceiveScriptType;
-		this.WhenAnyValue(x => x.DefaultReceiveScriptType)
-			.Subscribe(value => IsSegWitDefaultReceiveScriptType = value == ScriptType.SegWit);
+        // Валидация имени кошелька
+        this.ValidateProperty(
+            x => x.NewWalletName,
+            errors =>
+            {
+                if (_wallet.Name == NewWalletName)
+                {
+                    return;
+                }
 
-		_changeScriptPubKeyType = walletModel.Settings.ChangeScriptPubKeyType switch
-		{
-			PreferredScriptPubKeyType.Specified s => s.ScriptType switch
-			{
-				ScriptPubKeyType.TaprootBIP86 => PreferredScriptPubKeyType.Specified.Taproot,
-				ScriptPubKeyType.Segwit => PreferredScriptPubKeyType.Specified.SegWit,
-				_ => throw new ArgumentOutOfRangeException()
-			},
-			_ => walletModel.Settings.ChangeScriptPubKeyType
-		};
+                if (UiContext.WalletRepository.ValidateWalletName(NewWalletName) is { } error)
+                {
+                    errors.Add(error.Severity, error.Message);
+                }
+            });
 
-		DefaultSendWorkflow = walletModel.Settings.DefaultSendWorkflow;
-		this.WhenAnyValue(x => x.DefaultSendWorkflow)
-			.Subscribe(value => IsAutomaticDefaultSendWorkflow = value == SendWorkflow.Automatic);
+        DefaultReceiveScriptType = walletModel.Settings.DefaultReceiveScriptType;
+        this.WhenAnyValue(x => x.DefaultReceiveScriptType)
+            .Subscribe(value => IsSegWitDefaultReceiveScriptType = value == ScriptType.SegWit);
 
-		WalletCoinJoinSettings = new WalletCoinJoinSettingsViewModel(UiContext, walletModel);
+        _changeScriptPubKeyType = walletModel.Settings.ChangeScriptPubKeyType switch
+        {
+            PreferredScriptPubKeyType.Specified s => s.ScriptType switch
+            {
+                ScriptPubKeyType.TaprootBIP86 => PreferredScriptPubKeyType.Specified.Taproot,
+                ScriptPubKeyType.Segwit => PreferredScriptPubKeyType.Specified.SegWit,
+                _ => throw new ArgumentOutOfRangeException()
+            },
+            _ => walletModel.Settings.ChangeScriptPubKeyType
+        };
 
-		SetupCancel(enableCancel: false, enableCancelOnEscape: true, enableCancelOnPressed: true);
+        DefaultSendWorkflow = walletModel.Settings.DefaultSendWorkflow;
+        this.WhenAnyValue(x => x.DefaultSendWorkflow)
+            .Subscribe(value => IsAutomaticDefaultSendWorkflow = value == SendWorkflow.Automatic);
 
-		NextCommand = CancelCommand;
+        WalletCoinJoinSettings = new WalletCoinJoinSettingsViewModel(UiContext, walletModel);
 
-		VerifyRecoveryWordsCommand = ReactiveCommand.Create(() => Navigate().To().WalletVerifyRecoveryWords(walletModel));
+        SetupCancel(enableCancel: false, enableCancelOnEscape: true, enableCancelOnPressed: true);
+        NextCommand = CancelCommand;
 
-		ResyncWalletCommand = ReactiveCommand.CreateFromTask(async () =>
-		{
-			int? heightToResync = await UiContext.Navigate().To().ResyncWallet().GetResultAsync();
-			if (heightToResync is not null)
-			{
-				walletModel.Settings.RescanWallet((int)heightToResync);
-				UiContext.Navigate(MetaData.NavigationTarget).Clear();
-				AppLifetimeHelper.Shutdown(withShutdownPrevention: true, restart: true);
-			}
-		});
+        VerifyRecoveryWordsCommand = ReactiveCommand.Create(() => Navigate().To().WalletVerifyRecoveryWords(walletModel));
 
-		this.WhenAnyValue(x => x.DefaultSendWorkflow)
-			.Skip(1)
-			.Subscribe(value =>
-			{
-				walletModel.Settings.DefaultSendWorkflow = value;
-				walletModel.Settings.Save();
-			});
+        ResyncWalletCommand = ReactiveCommand.CreateFromTask(async () =>
+        {
+            int? heightToResync = await UiContext.Navigate().To().ResyncWallet().GetResultAsync();
+            if (heightToResync is not null)
+            {
+                walletModel.Settings.RescanWallet((int)heightToResync);
+                UiContext.Navigate(MetaData.NavigationTarget).Clear();
+                AppLifetimeHelper.Shutdown(withShutdownPrevention: true, restart: true);
+            }
+        });
 
-		this.WhenAnyValue(x => x.DefaultReceiveScriptType)
-			.Skip(1)
-			.Subscribe(value =>
-			{
-				walletModel.Settings.DefaultReceiveScriptType = value;
-				walletModel.Settings.Save();
-			});
+        // Обработка изменения имени
+        this.WhenAnyValue(x => x.NewWalletName)
+            .Skip(1)
+            .Where(_ => !Validations.Any)
+            .Subscribe(newName =>
+            {
+                if (_wallet.Name != newName)
+                {
+                    try
+                    {
+                        _wallet.Rename(newName);
+                        _walletName = newName;
+                    }
+                    catch
+                    {
+                        NewWalletName = _wallet.Name;
+                        UiContext.Navigate().To().ShowErrorDialog(
+                            $"The wallet cannot be renamed to {newName}",
+                            "Invalid name",
+                            "Cannot rename the wallet",
+                            NavigationTarget.CompactDialogScreen);
+                    }
+                }
+            });
 
-		this.WhenAnyValue(x => x.ChangeScriptPubKeyType)
-			.Skip(1)
-			.Subscribe(value =>
-			{
-				walletModel.Settings.ChangeScriptPubKeyType = value;
-				walletModel.Settings.Save();
-			});
+        this.WhenAnyValue(x => x.DefaultSendWorkflow)
+            .Skip(1)
+            .Subscribe(value =>
+            {
+                walletModel.Settings.DefaultSendWorkflow = value;
+                walletModel.Settings.Save();
+            });
 
-		this.WhenAnyValue(x => x.PreferPsbtWorkflow)
-			.Skip(1)
-			.Subscribe(value =>
-			{
-				walletModel.Settings.PreferPsbtWorkflow = value;
-				walletModel.Settings.Save();
-			});
+        this.WhenAnyValue(x => x.DefaultReceiveScriptType)
+            .Skip(1)
+            .Subscribe(value =>
+            {
+                walletModel.Settings.DefaultReceiveScriptType = value;
+                walletModel.Settings.Save();
+            });
 
-		this.WhenAnyValue(x => x._wallet.Name).BindTo(this, x => x.WalletName);
+        this.WhenAnyValue(x => x.ChangeScriptPubKeyType)
+            .Skip(1)
+            .Subscribe(value =>
+            {
+                walletModel.Settings.ChangeScriptPubKeyType = value;
+                walletModel.Settings.Save();
+            });
 
-		RenameCommand = ReactiveCommand.CreateFromTask(OnRenameWalletAsync);
-	}
+        this.WhenAnyValue(x => x.PreferPsbtWorkflow)
+            .Skip(1)
+            .Subscribe(value =>
+            {
+                walletModel.Settings.PreferPsbtWorkflow = value;
+                walletModel.Settings.Save();
+            });
 
-	public ICommand RenameCommand { get; set; }
+        this.WhenAnyValue(x => x._wallet.Name).BindTo(this, x => x.WalletName);
+    }
 
-	public bool IsHardwareWallet { get; }
+    public bool IsHardwareWallet { get; }
+    public bool IsWatchOnly { get; }
+    public bool SeveralReceivingScriptTypes => _wallet.SeveralReceivingScriptTypes;
+    public bool IsDefaultSendWorkflowSettingVisible => !(IsWatchOnly || IsHardwareWallet);
 
-	public bool IsWatchOnly { get; }
+    public IEnumerable<ScriptType> ReceiveScriptTypes { get; } = [ScriptType.SegWit, ScriptType.Taproot];
+    public IEnumerable<PreferredScriptPubKeyType> ChangeScriptPubKeyTypes { get; } =
+    [
+        PreferredScriptPubKeyType.Unspecified.Instance,
+        PreferredScriptPubKeyType.Specified.SegWit,
+        PreferredScriptPubKeyType.Specified.Taproot
+    ];
 
-	public bool SeveralReceivingScriptTypes => _wallet.SeveralReceivingScriptTypes;
-	public bool IsDefaultSendWorkflowSettingVisible => !(IsWatchOnly || IsHardwareWallet);
+    public IEnumerable<SendWorkflow> SendWorkflows { get; } = Enum.GetValues<SendWorkflow>();
 
-	public IEnumerable<ScriptType> ReceiveScriptTypes { get; } = [ScriptType.SegWit, ScriptType.Taproot];
-	public IEnumerable<PreferredScriptPubKeyType> ChangeScriptPubKeyTypes { get; } =
-	[
-		PreferredScriptPubKeyType.Unspecified.Instance,
-		PreferredScriptPubKeyType.Specified.SegWit,
-		PreferredScriptPubKeyType.Specified.Taproot
-	];
+    public WalletCoinJoinSettingsViewModel WalletCoinJoinSettings { get; private set; }
+    public ICommand VerifyRecoveryWordsCommand { get; }
+    public ICommand ResyncWalletCommand { get; }
 
-	public IEnumerable<SendWorkflow> SendWorkflows { get; } = Enum.GetValues<SendWorkflow>();
-
-	public WalletCoinJoinSettingsViewModel WalletCoinJoinSettings { get; private set; }
-
-	public ICommand VerifyRecoveryWordsCommand { get; }
-	public ICommand ResyncWalletCommand { get; }
-
-	private async Task OnRenameWalletAsync()
-	{
-		await Navigate().To().WalletRename(_wallet).GetResultAsync();
-		UiContext.WalletRepository.StoreLastSelectedWallet(_wallet);
-	}
-
-	protected override void OnNavigatedTo(bool isInHistory, CompositeDisposable disposables)
-	{
-		base.OnNavigatedTo(isInHistory, disposables);
-
-		WalletCoinJoinSettings.ManuallyUpdateOutputWalletList();
-	}
+    protected override void OnNavigatedTo(bool isInHistory, CompositeDisposable disposables)
+    {
+        base.OnNavigatedTo(isInHistory, disposables);
+        WalletCoinJoinSettings.ManuallyUpdateOutputWalletList();
+    }
 }
