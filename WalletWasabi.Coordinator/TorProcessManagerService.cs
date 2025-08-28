c46d835e60fbb7751e678e64d800fa700c6be0bf
Lucas Ontivero <lucasontivero@gmail.com>
2025-08-28 18:19:27 -0300

    Do not hardcode ports

--- WalletWasabi.Coordinator/TorProcessManagerService.cs
+++ WalletWasabi.Coordinator/TorProcessManagerService.cs
@@ -1,6 +1,7 @@
-using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using WalletWasabi.Logging;
 using WalletWasabi.Services;
@@ -9,27 +10,37 @@ using WalletWasabi.WabiSabi.Coordinator;
 
 namespace WalletWasabi.Coordinator;
 
-public class TorProcessManagerService(TorSettings torSettings, WabiSabiConfig config) : IHostedService
+public class TorProcessManagerService(TorSettings torSettings, WabiSabiConfig config, IConfiguration configuration) : IHostedService
 {
 	private readonly TorProcessManager _torManager = new(torSettings, new EventBus());
 
 	public async Task StartAsync(CancellationToken cancellationToken)
 	{
+		var urls = configuration["urls"];
+		if (urls is null)
+		{
+			Logger.LogWarning("The coordinator doesn't have urls configured!");
+			return;
+		}
+
 		var (_, torControlClient) = await _torManager.StartAsync(attempts: 3, cancellationToken).ConfigureAwait(false);
 		Logger.LogInfo($"{nameof(TorProcessManager)} is initialized.");
 
 		if (torControlClient is { } nonNullTorControlClient)
 		{
+			// multiple urls can be configured but that would need to create multiple onion services and we don't want to do that.
+			var address = urls.Split(";").First();
+			var uri = new Uri(address);
 			string onionServiceId;
 			if (!string.IsNullOrWhiteSpace(config.OnionServicePrivateKey))
 			{
 				onionServiceId = await nonNullTorControlClient
-					.CreateOnionServiceAsync(config.OnionServicePrivateKey, 80, 37126, cancellationToken).ConfigureAwait(false);
+					.CreateOnionServiceAsync(config.OnionServicePrivateKey, 80, uri.Port, cancellationToken).ConfigureAwait(false);
 			}
 			else
 			{
 				(onionServiceId, var privateKey) = await nonNullTorControlClient
-					.CreateKeylessOnionServiceAsync(80, 37126, cancellationToken).ConfigureAwait(false);
+					.CreateKeylessOnionServiceAsync(80, uri.Port, cancellationToken).ConfigureAwait(false);
 				config.OnionServicePrivateKey = privateKey;
 				config.AnnouncerConfig.CoordinatorUri = $"http://{onionServiceId}.onion";
 				config.AnnouncerConfig.ReadMoreUri = $"http://{onionServiceId}.onion";
