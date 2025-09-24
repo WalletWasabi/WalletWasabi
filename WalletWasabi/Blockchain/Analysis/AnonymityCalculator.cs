d6f035013bd369af23c33a920bc7b47b9cfe271a
Lucas Ontivero <lucasontivero@gmail.com>
2025-09-23 23:39:53 -0300

    Replicate tests

---
+++ WalletWasabi/Blockchain/Analysis/AnonymityCalculator.cs
@@ -0,0 +1,171 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using WalletWasabi.Blockchain.Keys;
+using WalletWasabi.Blockchain.TransactionOutputs;
+using WalletWasabi.Blockchain.Transactions;
+using AnonymityScore = decimal;
+
+namespace WalletWasabi.Blockchain.Analysis;
+
+public static class SmartTransactionExtensions
+{
+	public static bool IsReceive(this SmartTransaction tx) => tx.ForeignInputs.Count > 0 && tx.WalletInputs.Count == 0 && tx.WalletOutputs.Count > 0;
+	public static bool IsSpend(this SmartTransaction tx) => tx.ForeignInputs.Count == 0 && tx.ForeignOutputs.Count > 0;
+	public static bool IsSelfSpend(this SmartTransaction tx) => tx.ForeignInputs.Count == 0 && tx.ForeignOutputs.Count == 0;
+	public static bool IsMultiparty(this SmartTransaction tx) => tx.ForeignInputs.Count > 0 && tx.WalletInputs.Count > 0;
+}
+
+public static class XConstants
+{
+	public const AnonymityScore CertainlyKnown = 1m;
+}
+
+public record AnonymityScoreDb
+{
+	private ImmutableHashSet<SmartCoin> SmartCoinScores { get; init; } =
+		ImmutableHashSet<SmartCoin>.Empty;
+
+	private ImmutableDictionary<HdPubKey, AnonymityScore> PubKeyScores { get; init; } =
+		ImmutableDictionary<HdPubKey, AnonymityScore>.Empty;
+
+	private ImmutableList<(HdPubKey Source, SmartCoin SmartCoin)> Dependencies { get; init; } =
+		ImmutableList<(HdPubKey Source, SmartCoin SmartCoin)>.Empty;
+
+	public static readonly AnonymityScoreDb Empty = new();
+
+	public bool TryGetAnonymityScore(SmartCoin coin, out AnonymityScore anonymityScore)
+	{
+		if (SmartCoinScores.Contains(coin))
+		{
+			anonymityScore = PubKeyScores[coin.HdPubKey];
+			return true;
+		}
+
+		anonymityScore = XConstants.CertainlyKnown;
+		return false;
+	}
+
+	public bool TryGetAnonymityScore(HdPubKey pubkey, out AnonymityScore anonymityScore)
+	{
+		if (PubKeyScores.TryGetValue(pubkey, out anonymityScore ))
+		{
+			return true;
+		}
+
+		anonymityScore = XConstants.CertainlyKnown;
+		return false;
+	}
+
+	public AnonymityScoreDb SetAnonymityScore (SmartCoin coin, AnonymityScore anonymityScore)
+	{
+		var invalidated = InvalidateCacheEntries(coin.HdPubKey);
+		return invalidated with
+		{
+			SmartCoinScores = invalidated.SmartCoinScores.Add(coin),
+			PubKeyScores = invalidated.PubKeyScores.SetItem(coin.HdPubKey, anonymityScore),
+			Dependencies =
+				invalidated.Dependencies.AddRange(coin.Transaction.WalletInputs.Select(scoin => (scoin.HdPubKey, coin)))
+		};
+	}
+
+	private AnonymityScoreDb InvalidateCacheEntries(HdPubKey pubKey)
+	{
+		var dependencies = Dependencies.Where(x => x.Source == pubKey).ToArray();
+		var validDependencies = dependencies.Aggregate(this, (db, dependency) =>
+			db.InvalidateCacheEntries(dependency.SmartCoin.HdPubKey));
+
+		return validDependencies with
+		{
+			Dependencies = Dependencies.RemoveRange(dependencies),
+			SmartCoinScores = SmartCoinScores.Except(dependencies.Select(x => x.SmartCoin)),
+			PubKeyScores = PubKeyScores.RemoveRange(dependencies.Select(x => x.SmartCoin.HdPubKey))
+		};
+	}
+}
+
+public static class AnonymityCalculator
+{
+	public static (AnonymityScore AnonymityScore, AnonymityScoreDb Db) GetAnonymityScore(SmartCoin coin, AnonymityScoreDb db)
+	{
+		if (db.TryGetAnonymityScore(coin, out var anonymityScore))
+		{
+			return (anonymityScore, db);
+		}
+
+		var (calculatedScore, pdb) = AnalyzeCoinAnonymity(coin, db); // Calculate the anonscore
+		if (db.TryGetAnonymityScore(coin.HdPubKey, out var storedAnonymityScore))
+		{
+			if (calculatedScore > storedAnonymityScore)
+			{
+				pdb = pdb.SetAnonymityScore(coin, calculatedScore); // Stores it if is less than value already known
+				return (anonymityScore, pdb);
+			}
+
+			return (storedAnonymityScore, pdb);
+		}
+
+		pdb = pdb.SetAnonymityScore(coin, calculatedScore); // Stores it if is less than value already known
+		return (calculatedScore, pdb);
+	}
+
+
+	private static (AnonymityScore, AnonymityScoreDb) AnalyzeCoinAnonymity(SmartCoin coin, AnonymityScoreDb db)
+	{
+		var tx = coin.Transaction;
+		if (tx.IsSelfSpend())
+		{
+			var (updatedDb, anonymityScores) = tx.WalletVirtualInputs.Aggregate(
+				(scoreDb: db, anonymityScoreList: ImmutableList<AnonymityScore>.Empty), (acc, virtualInput) =>
+				{
+					var (anonymityScore, scoreDb) = GetAnonymityScore(virtualInput.Coins.First(), acc.scoreDb);
+					return (scoreDb, acc.anonymityScoreList.Add(anonymityScore));
+				});
+			return (Addition(anonymityScores), updatedDb);
+		}
+
+		if (tx.IsMultiparty())
+		{
+			var (updatedDb, anonymityScores) = tx.WalletVirtualInputs.Aggregate(
+				(scoreDb: db, anonymityScoreList: ImmutableList<AnonymityScore>.Empty), (acc, virtualInput) =>
+				{
+					var (anonymityScore, scoreDb) = GetAnonymityScore(virtualInput.Coins.First(), acc.scoreDb);
+					return (scoreDb, acc.anonymityScoreList.Add(anonymityScore));
+				});
+			var minimumAnonScore = Addition(anonymityScores);
+
+			var anonymityGain = Math.Max(ComputeAnonymityContribution(coin), 1m / tx.ForeignInputs.Count );
+			anonymityGain = anonymityGain > 1 ? 1 / anonymityGain : anonymityGain;
+
+			return (minimumAnonScore * anonymityGain, updatedDb);
+		}
+
+		return (XConstants.CertainlyKnown, db);
+	}
+
+	private static AnonymityScore Addition(IEnumerable<AnonymityScore> anonscore)
+	{
+		// Knowledge about coins is not mutually exclusive, and then addition can result in numbers bigger than one.
+		return Math.Min(XConstants.CertainlyKnown, anonscore.Sum());
+	}
+
+	private static AnonymityScore ComputeAnonymityContribution(SmartCoin transactionOutput)
+	{
+		var walletVirtualOutputs = transactionOutput.Transaction.WalletVirtualOutputs;
+		var foreignVirtualOutputs = transactionOutput.Transaction.ForeignVirtualOutputs;
+
+		var amount = walletVirtualOutputs.First(o => o.Coins.Select(c => c.Outpoint).Contains(transactionOutput.Outpoint)).Amount;
+
+		// Count the outputs that have the same value as our transactionOutput.
+		var equalValueWalletVirtualOutputCount = walletVirtualOutputs.Count(o => o.Amount == amount);
+		var equalValueForeignRelevantVirtualOutputCount = foreignVirtualOutputs.Count(o => o.Amount == amount);
+
+		// The anonymity set should increase by the number of equal-valued foreign outputs.
+		// If we have multiple equal-valued wallet outputs, then we divide the increase evenly between them.
+		// The rationale behind this is that picking randomly an output would make our anonset:
+		// total/ours = 1 + foreign/ours, so the increase in anonymity is foreign/ours.
+		return equalValueForeignRelevantVirtualOutputCount > 0
+			? (AnonymityScore)equalValueWalletVirtualOutputCount / equalValueForeignRelevantVirtualOutputCount
+			: XConstants.CertainlyKnown;
+	}
+}
