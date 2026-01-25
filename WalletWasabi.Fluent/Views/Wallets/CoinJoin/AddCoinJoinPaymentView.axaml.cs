9016f64a8c71fc58bbc68791d3e1fc1e35d17d0d
Coinjoiner <coinjoiner.com@protonmail.com>
2026-01-25 14:14:07 +0200

    Added GUI for batched payment in coinjoin

---
+++ WalletWasabi.Fluent/Views/Wallets/CoinJoin/AddCoinJoinPaymentView.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WalletWasabi.Fluent.Views.Wallets.CoinJoin;
+
+public partial class AddCoinJoinPaymentView : UserControl
+{
+	public AddCoinJoinPaymentView()
+	{
+		InitializeComponent();
+	}
+
+	private void InitializeComponent()
+	{
+		AvaloniaXamlLoader.Load(this);
+	}
+}
