5fbbb8dbb3c4f062b57a7213eb5c21ecf6e852d8
Carti <carti_it@proton.me>
2026-02-04 12:45:45 +0100

    WalletCoinsModel: Implement `IWalletCoinsModel`

--- WalletWasabi.Fluent/Models/Wallets/IWalletCoinsModel.cs
+++ WalletWasabi.Fluent/Models/Wallets/IWalletCoinsModel.cs
@@ -1,7 +1,16 @@
 using DynamicData;
-using WalletWasabi.Blockchain.Analysis.Clustering;
-using WalletWasabi.Blockchain.TransactionOutputs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WalletWasabi.Blockchain.TransactionBuilding;
+using WalletWasabi.Fluent.ViewModels.Wallets.Send;
 
 namespace WalletWasabi.Fluent.Models.Wallets;
 
-public partial interface IWalletCoinsModel: ICoinListModel;
+public partial interface IWalletCoinsModel : ICoinListModel
+{
+	Task UpdateExcludedCoinsFromCoinjoinAsync(ICoinModel[] coinsToExclude);
+
+	List<ICoinModel> GetSpentCoins(BuildTransactionResult? transaction);
+
+	bool AreEnoughToCreateTransaction(TransactionInfo transactionInfo, IEnumerable<ICoinModel> coins);
+}
