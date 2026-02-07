b1051cf6997e65f670d9976af522b30c0cab08fb
Carti <carti_it@proton.me>
2026-02-07 18:58:40 +0100

    PasteButtonFlashBehavior: Make sure that code runs on the UI thread

--- WalletWasabi.Fluent/Behaviors/PasteButtonFlashBehavior.cs
+++ WalletWasabi.Fluent/Behaviors/PasteButtonFlashBehavior.cs
@@ -1,13 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Threading;
+using Avalonia.Xaml.Interactions.Custom;
+using ReactiveUI;
 using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
 using System.Reactive.Disposables.Fluent;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Xaml.Interactions.Custom;
-using ReactiveUI;
 using WalletWasabi.Fluent.Controls;
 using WalletWasabi.Fluent.Extensions;
 using WalletWasabi.Fluent.Helpers;
@@ -43,30 +44,26 @@ public class PasteButtonFlashBehavior : AttachedToVisualTreeBehavior<AnimatedBut
 
 		var disposables = new CompositeDisposable();
 
-		if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime)
+		if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime lifetime && lifetime.MainWindow is { } mainWindow)
 		{
-			var mainWindow = lifetime.MainWindow;
-
 			Observable
 				.FromEventPattern(mainWindow, nameof(mainWindow.Activated)).ToSignal()
 				.Merge(this.WhenAnyValue(x => x.CurrentAddress).ToSignal())
 				.Throttle(TimeSpan.FromMilliseconds(100))
-				.ObserveOn(RxApp.MainThreadScheduler)
 				.SubscribeAsync(async _ => await CheckClipboardForValidAddressAsync(forceCheck: true))
 				.DisposeWith(disposables);
 
 			Observable
 				.Interval(TimeSpan.FromMilliseconds(500))
-				.ObserveOn(RxApp.MainThreadScheduler)
-				.SubscribeAsync(async _ =>
-				{
-					if (!mainWindow.IsActive)
+				.SubscribeAsync(async _ => await Dispatcher.UIThread.InvokeAsync(async () =>
 					{
-						return;
-					}
+						if (!mainWindow.IsActive)
+						{
+							return;
+						}
 
-					await CheckClipboardForValidAddressAsync();
-				})
+						await CheckClipboardForValidAddressAsync();
+					}).ConfigureAwait(false))
 				.DisposeWith(disposables);
 		}
 
@@ -80,40 +77,43 @@ public class PasteButtonFlashBehavior : AttachedToVisualTreeBehavior<AnimatedBut
 
 	private async Task CheckClipboardForValidAddressAsync(bool forceCheck = false)
 	{
-		var clipboardValue = await ApplicationHelper.GetTextAsync();
-
-		// Yes, it can be null, the software crashed without this condition.
-		if (clipboardValue is null)
+		await Dispatcher.UIThread.InvokeAsync(async () =>
 		{
-			return;
-		}
+			var clipboardValue = await ApplicationHelper.GetTextAsync();
 
-		if (AssociatedObject is null)
-		{
-			return;
-		}
+			// Yes, it can be null, the software crashed without this condition.
+			if (clipboardValue is null)
+			{
+				return;
+			}
 
-		if (_lastFlashedOn == clipboardValue && !forceCheck)
-		{
-			return;
-		}
+			if (AssociatedObject is null)
+			{
+				return;
+			}
 
-		AssociatedObject.Classes.Remove(FlashAnimation);
+			if (_lastFlashedOn == clipboardValue && !forceCheck)
+			{
+				return;
+			}
 
-		clipboardValue = clipboardValue.Trim();
-		var addressParsingResult = AddressParser.Parse(clipboardValue, Services.WalletManager.Network);
+			AssociatedObject.Classes.Remove(FlashAnimation);
 
-		// ClipboardValue might not match CurrentAddress, but it might be a PayJoin address pointing to the CurrentAddress
-		// Hence we need to compare both string value and parse result
-		if (clipboardValue != CurrentAddress && addressParsingResult.IsOk && CurrentAddress != addressParsingResult.Value.ToWif(Services.WalletManager.Network))
-		{
-			AssociatedObject.Classes.Add(FlashAnimation);
-			_lastFlashedOn = clipboardValue;
-			ToolTip.SetTip(AssociatedObject, $"Paste BTC Address:\r\n{clipboardValue}");
-		}
-		else
-		{
-			ToolTip.SetTip(AssociatedObject, "Paste");
-		}
+			clipboardValue = clipboardValue.Trim();
+			var addressParsingResult = AddressParser.Parse(clipboardValue, Services.WalletManager.Network);
+
+			// ClipboardValue might not match CurrentAddress, but it might be a PayJoin address pointing to the CurrentAddress
+			// Hence we need to compare both string value and parse result
+			if (clipboardValue != CurrentAddress && addressParsingResult.IsOk && CurrentAddress != addressParsingResult.Value.ToWif(Services.WalletManager.Network))
+			{
+				AssociatedObject.Classes.Add(FlashAnimation);
+				_lastFlashedOn = clipboardValue;
+				ToolTip.SetTip(AssociatedObject, $"Paste BTC Address:\r\n{clipboardValue}");
+			}
+			else
+			{
+				ToolTip.SetTip(AssociatedObject, "Paste");
+			}
+		});
 	}
 }
