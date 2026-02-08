cbc0216fc819a1d7cefbbbe0f204e06bc01150a8
Carti <carti_it@proton.me>
2026-02-08 15:00:50 +0100

    Convert `UiContextConstructorGenerator` to `IIncrementalGenerator`

--- WalletWasabi.Fluent.Generators/Abstractions/GeneratorStepContext.cs
+++
@@ -1,5 +0,0 @@
-using Microsoft.CodeAnalysis;
-
-namespace WalletWasabi.Fluent.Generators.Abstractions;
-
-internal record GeneratorStepContext(GeneratorExecutionContext Context, Compilation Compilation);
