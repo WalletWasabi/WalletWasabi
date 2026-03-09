f6be83dcaf58a3373a9395706faefe6cb8e67a7b
Carti <carti_it@proton.me>
2026-03-09 20:06:52 +0100

    Remove BoolToGenericConverter

--- WalletWasabi.Fluent/Converters/BooleanValueConverters.cs
+++ WalletWasabi.Fluent/Converters/BooleanValueConverters.cs
@@ -1,5 +1,25 @@
+using Avalonia.Data.Converters;
+using System.Globalization;
+
 namespace WalletWasabi.Fluent.Converters;
 
-public class BoolStringConverter : BoolToGenericConverter<string>
+public class BoolStringConverter : IValueConverter
 {
+	public string True { get; set; } = "True";
+	public string False { get; set; } = "False";
+
+	public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		if (value is not bool boolValue)
+		{
+			return False;
+		}
+
+		return boolValue ? True : False;
+	}
+
+	public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		throw new NotSupportedException();
+	}
 }
