722cba3e7cf9badf1510fbd0e32902df8330644b
Carti-it <carti_it@proton.me>
2026-04-12 20:54:20 +0000

    Send dialog: Disable paste button when there is nothing to paste / clipboard empty (again) (#14491)
    * Fix warning
    
    * Implement `ShowTargetControlWhenClipboardHasValueBehavior`
    
    * CR

---
+++ WalletWasabi.Fluent/Behaviors/ShowWhenClipboardNotEmptyBehavior.cs
@@ -0,0 +1,54 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+using WalletWasabi.Fluent.Helpers;
+
+namespace WalletWasabi.Fluent.Behaviors;
+
+/// <summary>
+/// User clicks a text box and if the clipboard has a value (not whitespace), the target control (e.g. a paste button) will be shown.
+/// </summary>
+public class ShowWhenClipboardNotEmptyBehavior : Behavior<TextBox>
+{
+	public static readonly StyledProperty<Control?> TargetControlProperty =
+		AvaloniaProperty.Register<ShowWhenClipboardNotEmptyBehavior, Control?>(nameof(TargetControl));
+
+	public Control? TargetControl
+	{
+		get => GetValue(TargetControlProperty);
+		set => SetValue(TargetControlProperty, value);
+	}
+
+	protected override void OnAttachedToVisualTree()
+	{
+		base.OnAttachedToVisualTree();
+		AssociatedObject?.GotFocus += OnGotFocusAsync;
+	}
+
+	protected override void OnDetachedFromVisualTree()
+	{
+		base.OnDetachedFromVisualTree();
+		AssociatedObject?.GotFocus -= OnGotFocusAsync;
+	}
+
+	private async void OnGotFocusAsync(object? sender, GotFocusEventArgs e)
+	{
+		if (AssociatedObject is null || TargetControl is null)
+		{
+			return;
+		}
+
+		try
+		{
+			var text = await ApplicationHelper.GetTextAsync();
+
+			// Leading and trailing whitespace is removed for the check purposes.
+			TargetControl.IsVisible = !string.IsNullOrEmpty(text?.Trim());
+		}
+		catch
+		{
+			TargetControl.IsVisible = false;
+		}
+	}
+}
