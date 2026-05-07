2ba14113192a8b9b44052d15f2fd2e2e82c8d79b
Carti-it <carti_it@proton.me>
2026-05-07 10:58:51 +0000

    CoreNode: Clean up (#14571)

--- WalletWasabi.Tests/BitcoinCore/Processes/BitcoindRpcProcessBridge.cs
+++ WalletWasabi.Tests/BitcoinCore/Processes/BitcoindRpcProcessBridge.cs
@@ -6,7 +6,6 @@ using NBitcoin;
 using WalletWasabi.BundledApps;
 using WalletWasabi.Bases;
 using WalletWasabi.BitcoinRpc;
-using WalletWasabi.Helpers;
 using WalletWasabi.Logging;
 using WalletWasabi.Tests.BitcoinCore.Configuration;
 
@@ -37,7 +36,7 @@ public class BitcoindRpcProcessBridge
 	public IRPCClient RpcClient { get; }
 	public string DataDir { get; }
 	public bool PrintToConsole { get; }
-	public PidFile PidFile { get; }
+	private PidFile PidFile { get; }
 	private ProcessAsync? Process { get; set; }
 	private int? CachedPid { get; set; }
 
@@ -52,8 +51,8 @@ public class BitcoindRpcProcessBridge
 
 		// On Windows, if DataDir ends with '\', the Process can't be started.
 		string dataDir = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && DataDir.EndsWith('\\')
-					 ? DataDir[..^1]
-					 : DataDir;
+			? DataDir[..^1]
+			: DataDir;
 
 		string args = $"{networkArgument} -datadir=\"{dataDir}\" -printtoconsole={ptcv}";
 
@@ -138,7 +137,7 @@ public class BitcoindRpcProcessBridge
 		int? pid = await PidFile.TryReadAsync().ConfigureAwait(false);
 
 		// If the cached PID is PID, then we own the process.
-		if (pid.HasValue && (!onlyOwned || CachedPid == pid))
+		if (pid is not null && (!onlyOwned || CachedPid == pid))
 		{
 			Logger.LogDebug($"User is responsible for the daemon process with PID {pid}. Stop it.");
 
@@ -162,13 +161,13 @@ public class BitcoindRpcProcessBridge
 
 				if (!isKilled)
 				{
-					Logger.LogDebug($"Wait until the process is stopped.");
+					Logger.LogDebug("Wait until the process is stopped.");
 					await process.WaitForExitAsync(cts.Token).ConfigureAwait(false);
 				}
 			}
 			finally
 			{
-				Logger.LogDebug($"Wait until the process is stopped.");
+				Logger.LogDebug("Wait until the process is stopped.");
 				process.Dispose();
 				Process = null;
 				PidFile.TryDelete();
