6e2e3f4dcf18e4ac7f5491109823824b0d499807
Carti <carti_it@proton.me>
2026-07-06 15:11:54 +0200

    WW 💚 Arti

--- WalletWasabi.Client/Global.cs
+++ WalletWasabi.Client/Global.cs
@@ -58,6 +58,7 @@ public class Global
 		DataDir = dataDir;
 		Config = config;
 		TorSettings = new TorSettings(
+			config.TorBackend,
 			DataDir,
 			distributionFolderPath: EnvironmentHelpers.GetFullBaseDirectory(),
 			terminateOnExit: Config.TerminateTorOnExit,
