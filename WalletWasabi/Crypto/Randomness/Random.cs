d78046e0fc230558114d883e6d14672623965396
Carti <carti_it@proton.me>
2026-07-14 22:10:23 +0200

    Random: Remove unnecessary allocations

--- WalletWasabi/Crypto/Randomness/Random.cs
+++ WalletWasabi/Crypto/Randomness/Random.cs
@@ -2,20 +2,14 @@ using System.Security.Cryptography;
 
 namespace WalletWasabi.Crypto.Randomness;
 
-public delegate byte[] RandomnessProvider(int length);
+public delegate void RandomnessProvider(Span<byte> span);
 
 public static class RandomnessProviders
 {
-	public static RandomnessProvider Secure =
-		length => RandomNumberGenerator.GetBytes(length);
+	private static readonly RandomNumberGenerator SecureRandomNumberGenerator = RandomNumberGenerator.Create();
 
-	public static RandomnessProvider Insecure =
-		length =>
-		{
-			var buffer = new byte[length];
-			Random.Shared.NextBytes(buffer);
-			return buffer;
-		};
+	public static readonly RandomnessProvider Secure = SecureRandomNumberGenerator.GetBytes;
+	public static readonly RandomnessProvider Insecure = Random.Shared.NextBytes;
 }
 
 public static class RandomnessProviderExtensions
@@ -27,16 +21,18 @@ public static class RandomnessProviderExtensions
 			throw new ArgumentOutOfRangeException(nameof(maxExclusive), "maxExclusive must be greater than 0");
 		}
 
+		Span<byte> bytes = stackalloc byte[4];
+		generator(bytes);
+
+		var value = BitConverter.ToUInt32(bytes);
+
 		var range = (uint)maxExclusive;
-
-		var bytes = generator(4);
-		var value = BitConverter.ToUInt32(bytes, 0);
-
 		var max = uint.MaxValue - (uint.MaxValue % range);
+
 		while (value >= max)
 		{
-			bytes = generator(4);
-			value = BitConverter.ToUInt32(bytes, 0);
+			generator(bytes);
+			value = BitConverter.ToUInt32(bytes);
 		}
 
 		return (int)(value % range);
