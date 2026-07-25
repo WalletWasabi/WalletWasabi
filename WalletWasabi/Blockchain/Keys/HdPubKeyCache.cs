07c3e2a887c262326923e9a77867c07370822e6b
Carti <carti_it@proton.me>
2026-07-25 22:35:57 +0200

    KeyManager: Fix more synchronization issues

--- WalletWasabi/Blockchain/Keys/HdPubKeyCache.cs
+++ WalletWasabi/Blockchain/Keys/HdPubKeyCache.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using NBitcoin;
 
 namespace WalletWasabi.Blockchain.Keys;
 
