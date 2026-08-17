1356b0e54f0c3be082fa315275805f96cf575ca6
Lucas Ontivero <lontivero@users.noreply.github.com>
2026-08-17 13:51:29 -0300

    Revert "Redo Calculation for Anonymity Score (#14117)" (#14944)
    This reverts commit c31c3b60829b31f2ce80a7fde5b1f4d6b617dc67.

--- WalletWasabi.Tests/UnitTests/WabiSabi/Integration/Participant.cs
+++ WalletWasabi.Tests/UnitTests/WabiSabi/Integration/Participant.cs
@@ -86,6 +86,7 @@ internal class Participant
 		static HdPubKey CreateHdPubKey(ExtPubKey extPubKey)
 		{
 			var hdPubKey = new HdPubKey(extPubKey.PubKey, KeyPath.Parse($"m/84'/0/0/0/{extPubKey.Child}"), LabelsArray.Empty, KeyState.Clean);
+			hdPubKey.SetAnonymitySet(1); // bug if not settled
 			return hdPubKey;
 		}
 
